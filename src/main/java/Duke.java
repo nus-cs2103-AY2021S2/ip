a295f59ce9d96ac3770c82594c4a303b867ce002
Chong Jing Quan <jingquan0302@gmail.com>
2021-01-15 17:22:15 +0800

    add storing and listing function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,14 +10,26 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("------------------------");
-        System.out.println("Hello! I'm Duke's friend, Ekud. \nDuke's dead, so I'm here to take this job. \nWhat can I do for you?");
+        System.out.println("Hello! I'm Duke's friend, Ekud." +
+                "\nDuke's dead, so I'm here to take his job." +
+                "\nWhat can I do for you?");
+
+        ArrayList<String> listOfInputs = new ArrayList<>(100);
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while(!input.equals("bye")){
-            System.out.println(input);
+            if(input.equals("list")){
+                for (int i = 0; i < listOfInputs.size(); i++) {
+                    System.out.println((i + 1)+ ". " + listOfInputs.get(i));
+                }
+            } else {
+                listOfInputs.add(input);
+                System.out.println("added: " + input);
+            }
             input = sc.nextLine();
         }
-        System.out.println("Bye Bye. Please give me 5-star rating, I still need this job.\nMuch thanks.");
+        System.out.println("Bye Bye. Please give me 5-star rating, I still need this job." +
+                "\nMuch thanks.");
     }
 }
