9af6ab45b98b55089c8927831fec8d847f5a04a8
Chong Jing Quan <jingquan0302@gmail.com>
2021-01-15 17:13:54 +0800

    Add greeting, echo, and exit functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("------------------------");
+        System.out.println("Hello! I'm Duke's friend, Ekud. \nDuke's dead, so I'm here to take this job. \nWhat can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while(!input.equals("bye")){
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+        System.out.println("Bye Bye. Please give me 5-star rating, I still need this job.\nMuch thanks.");
     }
 }
