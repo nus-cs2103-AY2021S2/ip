41952b9cd10dd9eabe5dd948698885465d7384c2
Tomashiwa <wongqinliang@gmail.com>
2021-01-15 23:50:04 +0800

    Add Level 2. Add, List
    * Text entered by the user are stored into a seperate list via
    addItem
    
    * These items can be displayed to the user via listItems

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     private static final String CHATBOT_NAME = "Mantaro";
     private static boolean isActive = true;
+
+    private static List<String> items = new ArrayList<>();
+
+    /**
+     * Add item to internal list
+     * @param name Name of the item
+     */
+    private static void addItem(String name) {
+        items.add(name);
+        System.out.println("___________________________________________________________");
+        System.out.printf("added: %s\n", name);
+        System.out.println("___________________________________________________________\n");
+    }
+
+    /**
+     * List down all items in the internal list
+     */
+    private static void listItems() {
+        System.out.println("___________________________________________________________");
+        for(int i = 0; i < items.size(); i++) {
+            System.out.printf("%d. %s\n", i + 1, items.get(i));
+        }
+        System.out.println("___________________________________________________________");
+    }
 
     public static void main(String[] args) {
         // Opening message
@@ -14,16 +40,16 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         String command = "";
 
-        // Echo user's commands but exit when user enters bye
+        // Respond to user's commands but exit when user enters bye
         while(isActive) {
             command = scanner.nextLine();
 
-            if(command.equals("bye")) {
+            if(command.equals("list")) {
+                listItems();
+            } else if(command.equals("bye")) {
                 isActive = false;
             } else {
-                System.out.println("___________________________________________________________");
-                System.out.println(command);
-                System.out.println("___________________________________________________________\n");
+                addItem(command);
             }
         }
 
