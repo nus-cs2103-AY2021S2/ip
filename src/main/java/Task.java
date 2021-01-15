95bc91eb20b48126451ccd72b8639451c7bfdb52
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-15 23:13:04 +0800

    Change the strings returned from getStatusIcon function.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -9,7 +9,7 @@ public class Task {
 
     //Return tick or X symbols according to task completion status.
     public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718");
+        return (isDone ? "\u2718" : " ");
     }
 
     //Change the completion status of a task to done.
