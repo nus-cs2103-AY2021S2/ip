8cbaef113f8d91396a4e3b569b0961f2a0815314
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-15 22:38:00 +0800

    Added Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    //Return tick or X symbols according to task completion status.
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    //Change the completion status of a task to done.
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    //Change the completion status of a task to not done.
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+}
