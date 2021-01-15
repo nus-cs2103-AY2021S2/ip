ceac4510a88717a6867ddeb7efc453f750369dd7
Nicholas Tanvis <e0424661@u.nus.edu>
2021-01-15 13:43:05 +0800

    feat(Duke): implement chatbot skeleton

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String GREETING = "Hi, I am Sonia! How can I help you?";
+
+    public static void echo(String message) {
+        System.out.println("Sonia: " + message);
+    }
+
+    public static void parse(String command) {
+        if (command.equals("bye")) {
+            echo("Goodbye!");
+            System.exit(0);
+        } else {
+            echo(command);
+        }
+    }
+
+    public static String prompt() {
+        System.out.print("You: ");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        echo(GREETING);
+        while (true) {
+            String command = prompt();
+            parse(command);
+        }
     }
 }
