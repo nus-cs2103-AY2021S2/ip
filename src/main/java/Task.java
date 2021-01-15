7cdfe6ee87519a9daa270edda56143e47c871912
Chong Jing Quan <jingquan0302@gmail.com>
2021-01-15 23:21:03 +0800

    Create Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    private String taskName;
+    private boolean done;
+
+    Task (String taskName){
+        this(taskName, false);
+    }
+
+    Task(String taskName, boolean done) {
+        this.taskName = taskName;
+        this.done = done;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public String toString() {
+        String checkBox = this.done ? "[X]" : "[ ]";
+        return checkBox + " " + this.taskName;
+    }
+}
