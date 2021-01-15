4f0e2950c1795afdf29d94b6b4462cb2111be7e0
enhao25 <larloller@gmail.com>
2021-01-15 23:32:46 +0800

    Implement Duke  that greets user, echo commands and exits when user types bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Output initial greeting before asking for input
+        String initialGreeting = "Hello I'm Duke \n"
+                + "What can I do for you? \n";
+        System.out.println(initialGreeting);
+
+        // Standard Input Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // User input data
+        String userInput;
+
+        // Loop through and echo the input until the user inputs "bye"
+        while (true) {
+            userInput = sc.nextLine();
+
+            if (userInput.equals("bye")) {
+                // Exit when the user inputs "bye"
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                // Otherwise, echo the userInput
+                System.out.println(userInput);
+            }
+
+        }
     }
 }
