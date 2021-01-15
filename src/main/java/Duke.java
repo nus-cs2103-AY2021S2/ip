7299f5af473cb70ea4c253c0987aa96582e7b648
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-15 21:51:46 +0800

    Added greet and echo function in Duke for level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,28 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        greet();
+        echo(sc);
+    }
+
+    public static void greet() {
+        System.out.println("\t____________________________________________________________\n"
+                        + "\tHello! I'm Duke\n\tWhat can I do for you?\n"
+                        + "\t____________________________________________________________\n");
+    }
+
+    public static void echo(Scanner sc) {
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("\t____________________________________________________________\n"
+                            + "\t" + input + "\n"
+                            + "\t____________________________________________________________\n");
+            input = sc.nextLine();
+        }
+        System.out.println("\t____________________________________________________________\n"
+                        + "\tBye. Hope to see you again soon!\n"
+                        + "\t____________________________________________________________\n"
+        );
     }
 }
