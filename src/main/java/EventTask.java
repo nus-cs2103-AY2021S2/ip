f7b843ed955037d6617d0dced31ec8608a7cbf77
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-15 23:47:14 +0800

    Added DeadlineTask.java, EventTask.java, TodoTask.java.

---
+++ src/main/java/EventTask.java
@@ -0,0 +1,13 @@
+public class EventTask extends Task {
+    protected String date;
+
+    public EventTask(String description, String date) {
+        super(description);
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + this.date + ")";
+    }
+}
