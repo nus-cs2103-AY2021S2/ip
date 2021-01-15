86e450af727e21342067a2d05f2a1393d06251c3
Chong Jing Quan <jingquan0302@gmail.com>
2021-01-15 23:22:29 +0800

    implement done function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,20 +12,30 @@ public class Duke {
         System.out.println("------------------------");
         System.out.println("Hello! I'm Duke's friend, Ekud." +
                 "\nDuke's dead, so I'm here to take his job." +
-                "\nWhat can I do for you?");
+                "\nYou want to jot down some tasks?");
 
-        ArrayList<String> listOfInputs = new ArrayList<>(100);
+        ArrayList<Task> listOfInputs = new ArrayList<>(100);
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while(!input.equals("bye")){
-            if(input.equals("list")){
+            if (input.equals("list")){
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < listOfInputs.size(); i++) {
                     System.out.println((i + 1)+ ". " + listOfInputs.get(i));
                 }
+            } else if (input.startsWith("done")) {
+                int index = Integer.parseInt(input.split(" ")[1]);
+                try{
+                    listOfInputs.get(index - 1).setDone(true);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(listOfInputs.get(index - 1));
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("You have " + listOfInputs.size() + " tasks in your list. Try again.");
+                }
             } else {
-                listOfInputs.add(input);
-                System.out.println("added: " + input);
+                listOfInputs.add(new Task(input));
+                System.out.println("Added: " + input);
             }
             input = sc.nextLine();
         }
