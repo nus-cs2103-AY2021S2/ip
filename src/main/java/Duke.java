4b9eb079937f8e0c796d032a0b2af7485d549875
Nicholas Tanvis <e0424661@u.nus.edu>
2021-01-15 14:30:12 +0800

    feat(Duke): implement non-persistent storage for texts
    BREAKING CHANGE: no longer echo commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    public static ArrayList<String> tasks = new ArrayList<>();
     public static String GREETING = "Hi, I am Sonia! How can I help you?";
 
     public static void echo(String message) {
@@ -11,8 +13,22 @@ public class Duke {
         if (command.equals("bye")) {
             echo("Goodbye!");
             System.exit(0);
+        } else if (command.equals("list")) {
+            echo("Here are your tasks!");
+            prettyPrint(tasks);
         } else {
-            echo(command);
+            tasks.add(command);
+            echo("I added \"" + command + "\" to the task list!");
+        }
+    }
+
+    public static void prettyPrint(ArrayList<String> tasks) {
+        if (tasks.size() > 0) {
+            for (int i = 1; i <= tasks.size(); i++) {
+                System.out.println("   " + i + ": " + tasks.get(i - 1));
+            }
+        } else {
+            System.out.println("   Looks like you haven't added any tasks.");
         }
     }
 
