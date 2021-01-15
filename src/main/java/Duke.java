12ebf34cd17850e613659c812186c8ca775352a3
enhao25 <larloller@gmail.com>
2021-01-16 00:06:15 +0800

    Stores the user input and outputs the listing accordingly when needed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -12,6 +13,7 @@ public class Duke {
 
         // User input data
         String userInput;
+        ArrayList<String> userInputs = new ArrayList();
 
         // Loop through and echo the input until the user inputs "bye"
         while (true) {
@@ -21,9 +23,15 @@ public class Duke {
                 // Exit when the user inputs "bye"
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if(userInput.equals("list")) {
+                // Shows the list of user inputs with its numbering starting from 1
+                for (int index = 0; index < userInputs.size(); index++) {
+                    System.out.println(index + 1 + ". " + userInputs.get(index));
+                }
             } else {
-                // Otherwise, echo the userInput
-                System.out.println(userInput);
+                // Otherwise, store the user input into an arraylist
+                userInputs.add(userInput);
+                System.out.println("added: " + userInput);
             }
 
         }
