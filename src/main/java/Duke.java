5c3a6ebbf342c41e1a079a500d4997bbb573c274
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-15 22:28:19 +0800

    Added addList and showList function for Duke Level 2.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class Duke {
     static Scanner sc = new Scanner(System.in);
@@ -9,29 +12,61 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
+        List<String> list = new ArrayList<>();
 
         greet();
-        echo();
+        addList(list);
     }
 
+    //Duke greets the user.
     public static void greet() {
         System.out.println("\t____________________________________________________________\n"
                         + "\tHello! I'm Duke\n\tWhat can I do for you?\n"
                         + "\t____________________________________________________________\n");
     }
 
+    //Echos the input back to the user.
     public static void echo() {
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             System.out.println("\t____________________________________________________________\n"
-                            + "\t" + input + "\n"
-                            + "\t____________________________________________________________\n");
+                    + "\t" + input + "\n"
+                    + "\t____________________________________________________________\n");
             input = sc.nextLine();
         }
         System.out.println("\t____________________________________________________________\n"
-                        + "\tBye. Hope to see you again soon!\n"
-                        + "\t____________________________________________________________\n"
+                + "\tBye. Hope to see you again soon!\n"
+                + "\t____________________________________________________________\n"
         );
+    }
+
+    //Adds the input in a list and echo it back to the user.
+    //Prints the list if input is "list"
+    public static void addList(List list) {
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                showList(list);
+            } else {
+                list.add(input);
+                System.out.println("\t____________________________________________________________\n"
+                                + "\tadded: " + input + "\n"
+                                + "\t____________________________________________________________\n");
+            }
+            input = sc.nextLine();
+        }
+        System.out.println("\t____________________________________________________________\n"
+                + "\tBye. Hope to see you again soon!\n"
+                + "\t____________________________________________________________\n"
+        );
+    }
+
+    //Prints out the items in the list.
+    public static void showList(List list) {
+        System.out.println("\t____________________________________________________________");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.printf("\t%d. %s\n", i + 1, list.get(i));
+        }
+        System.out.println("\t____________________________________________________________\n");
     }
 }
