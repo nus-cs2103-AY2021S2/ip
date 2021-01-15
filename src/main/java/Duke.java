02ab6e4d65392bbdd5cd6b9d8c4d58ae4fe1abbe
ampan98 <e0407566@u.nus.edu>
2021-01-15 22:38:03 +0800

    Implement skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+package main.java;
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +10,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Please input a command.");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            System.out.println("  ~~~");
+            if (input.equals("bye")) {
+                System.out.println("  See you next time.");
+                System.out.println("  ~~~");
+                break;
+            }
+            System.out.println("  " + input);
+            System.out.println("  ~~~");
+        }
     }
 }
