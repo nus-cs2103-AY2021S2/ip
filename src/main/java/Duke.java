9f2727775f2d749c361f9229804fbf036dab154b
Tomashiwa <wongqinliang@gmail.com>
2021-01-15 23:14:13 +0800

    Add Level-1. Greet Echo Exit
    Implements the required skeletal version of Duke that starts by
    greeting the user, simply echos commands entered by the user,
    and exits when the user types bye.
    
    Chatbot's name can be modified through the variable CHATBAT_NAME.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String CHATBOT_NAME = "Mantaro";
+    private static boolean isActive = true;
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Opening message
+        System.out.println("___________________________________________________________");
+        System.out.printf("Meow, I'm %s\nWhat can I do for you today?\n", CHATBOT_NAME);
+        System.out.println("___________________________________________________________\n");
+
+        // Scan user input as a command
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+
+        // Echo user's commands but exit when user enters bye
+        while(isActive) {
+            command = scanner.nextLine();
+
+            if(command.equals("bye")) {
+                isActive = false;
+            } else {
+                System.out.println("___________________________________________________________");
+                System.out.println(command);
+                System.out.println("___________________________________________________________\n");
+            }
+        }
+
+        // Closing message
+        System.out.println("___________________________________________________________");
+        System.out.println("Meow. Hope to see you again soon!");
+        System.out.println("___________________________________________________________");
     }
 }
