cde3c87f196d7c2d5869587f23d15ed176a6bfb2
SoulUseless <jkyap99@gmail.com>
2021-01-15 15:51:46 +0800

    Duke: add echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
 
     /**
      *  Greeter for Duke
     */
     public static void greet() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "     ____        _        \n"
+                + "    |  _ \\ _   _| | _____ \n"
+                + "    | | | | | | | |/ / _ \\\n"
+                + "    | |_| | |_| |   <  __/\n"
+                + "    |____/ \\__,_|_|\\_\\___|\n";
 
-        System.out.println("---------------------------------------");
+        System.out.println("    ---------------------------------------");
         System.out.println(logo);
-        System.out.println("---------------------------------------");
-        System.out.println("Hello! This is Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("---------------------------------------");
+        System.out.println("    ---------------------------------------");
+        System.out.println("    Hello! This is Duke");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ---------------------------------------");
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         greet();
+
+        boolean done = false;
+        while (!done) {
+            String command = sc.next();
+            System.out.println("    ---------------------------------------");
+            if (command.equals("bye")) {
+                done = true;
+                System.out.println("Bye bye!");
+            } else {
+                System.out.println("    " + command);
+            }
+            System.out.println("    ---------------------------------------");
+        }
     }
 }
