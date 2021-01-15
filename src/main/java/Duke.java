a6f86695dccd3b8b1154f6556d17fc4275a3cffc
zatkiller <gheedylan@yahoo.com.sg>
2021-01-15 23:34:24 +0800

    Added divider lines, welcome, message and bye functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void printDivider() {
+        String divider = "    ___________________________________________";
+        System.out.println(divider);
+    }
+
+    public static void welcome() {
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+    }
+
+    public static void printMessage(String message) {
+        System.out.println("     " + message);
+    }
+
+    public static void bye() {
+        System.out.println("     Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printDivider();
+        welcome();
+        printDivider();
+
+        Scanner sc = new Scanner(System.in);
+        String action = sc.nextLine();
+
+        while (!action.equals("bye")) {
+            printDivider();
+            printMessage(action);
+            printDivider();
+            action = sc.nextLine();
+        }
+
+        printDivider();
+        bye();
+        printDivider();
+
+        sc.close();
     }
 }
