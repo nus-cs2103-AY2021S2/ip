f8852bf4cd9c2e113b888dcd4f74e7b7b216aa3a
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-15 22:04:12 +0800

    Changed scanner to global variable and changed echo to accept no arguments.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,9 +9,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        Scanner sc = new Scanner(System.in);
+
+
         greet();
-        echo(sc);
+        echo();
     }
 
     public static void greet() {
@@ -19,7 +21,7 @@ public class Duke {
                         + "\t____________________________________________________________\n");
     }
 
-    public static void echo(Scanner sc) {
+    public static void echo() {
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             System.out.println("\t____________________________________________________________\n"
