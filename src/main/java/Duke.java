7ebfda2c4f6cc3d31cde39351651d450f45774df
zatkiller <gheedylan@yahoo.com.sg>
2021-01-15 23:49:52 +0800

    Added add and list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
+
+    public static List<String> inputList = new ArrayList<>();
+
 
     public static void printDivider() {
         String divider = "    ___________________________________________";
@@ -20,19 +25,40 @@ public class Duke {
         System.out.println("     Bye. Hope to see you again soon!");
     }
 
+    public static void printStored() {
+        for (int i = 0; i < inputList.size(); i++) {
+            System.out.println("      " + (i + 1) + ". " + inputList.get(i));
+        }
+    }
+
     public static void main(String[] args) {
         printDivider();
         welcome();
         printDivider();
 
         Scanner sc = new Scanner(System.in);
-        String action = sc.nextLine();
 
-        while (!action.equals("bye")) {
-            printDivider();
-            printMessage(action);
-            printDivider();
-            action = sc.nextLine();
+        boolean carryOn = true;
+
+        while (carryOn) {
+            String action = sc.nextLine();
+
+            switch(action) {
+                case "list":
+                    printDivider();
+                    printStored();
+                    printDivider();
+                    break;
+                case "bye":
+                    carryOn = false;
+                    break;
+                default:
+                    inputList.add(action);
+                    printDivider();
+                    printMessage("added: " + action);
+                    printDivider();
+                    break;
+            }
         }
 
         printDivider();
