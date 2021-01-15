8a7ffad0e153cfd4b5f83f1bf11e71f0c60b2094
Glenn Lee <glenn@Glenns-MacBook-Pro.local>
2021-01-15 15:33:32 +0800

    Added Level-1 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(makeOutput("Hello! I'm Duke\n\t What can I do for you?"));
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(makeOutput(input));
+            input = sc.nextLine();
+        }
+        System.out.println(makeOutput("Bye. Hope to see you again soon!"));
     }
+
+    public static String makeOutput(String input) {
+        return "\t____________________________________________________________"
+            + "\n\t " + input
+            + "\n\t____________________________________________________________";
+    }
+
 }
