5d633d7c60c28d9dd2168b8b29f6edd9ce414369
eksinyue <xinyuehehxy@gmail.com>
2021-01-15 22:45:29 +0800

    Allow users to add and list tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Duke {
 
+    public static List<String> userInputItems = new ArrayList<>();
+
     public static void greeting() {
         partition();
         String logo = "    __  _____ _  ___   ___   _ ___ \n" +
@@ -23,6 +25,21 @@ public class Duke {
         System.out.println("    ---------------------------");
     }
 
+    public static void addItem(String userInput) {
+        userInputItems.add(userInput);
+        partition();
+        System.out.println("    added: " + userInput);
+        partition();
+    }
+
+    public static void listItems() {
+        partition();
+        for (int i = 1; i <= userInputItems.size(); ++i) {
+            System.out.println("    " + i + ". " + userInputItems.get(i - 1));
+        }
+        partition();
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         greeting();
@@ -31,11 +48,13 @@ public class Duke {
             String userInput = sc.nextLine();
             if (userInput.equals("bye")) {
                 break;
+            } else if (userInput.equals("list")) {
+                listItems();
+            } else {
+                addItem(userInput);
             }
-            partition();
-            System.out.println("    " + userInput);
-            partition();
         }
+
         farewell();
         sc.close();
     }
