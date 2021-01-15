453aab4162bf1eb0cb0f792f2c921f4c5b417b78
yaowei-soc <e0407296@u.nus.edu>
2021-01-15 22:39:27 +0800

    Level 2 complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,25 +3,38 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        String userInput = "";
+        int index = 0;
 
-        System.out.println("\t____________________________________________________________");
+        printSegment();
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
-        System.out.println("\t____________________________________________________________");
-
-        String userInput = "";
+        printSegment();
 
         while (!userInput.equals("bye")) {
             userInput = scanner.nextLine();
-            if (!userInput.equals("bye")) {
-                System.out.println("\t____________________________________________________________");
-                System.out.println("\t" + userInput);
-                System.out.println("\t____________________________________________________________");
+            if (userInput.equals("list")) {
+                printSegment();
+                for (int i = 0; i < index; i++) {
+                    System.out.println("\t" + (i + 1) + ". " + tasks[i]);
+                }
+                printSegment();
+            } else if (!userInput.equals("bye")) {
+                tasks[index] = userInput;
+                index++;
+                printSegment();
+                System.out.println("\t added: " + userInput);
+                printSegment();
             }
         }
 
-        System.out.println("\t____________________________________________________________");
+        printSegment();
         System.out.println("\tBye. Hope to see you again soon!");
+        printSegment();
+    }
+
+    static void printSegment() {
         System.out.println("\t____________________________________________________________");
     }
 }
