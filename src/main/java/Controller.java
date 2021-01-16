aed33ee479c5046e431ce32697e1615c3aec273f
Marcus Ong Jing Keat <markuz5116@gmail.com>
2021-01-16 20:32:51 +0800

    Add function to mark task as done

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -6,7 +6,7 @@ public class Controller {
     private final static String LINE = "\t__________________________________\n";
     private final static String GREETING = "\t Hello! I'm Duke\n\t What can I do for you? \n";
     private final static String ENDDUKE = "\t Bye. Hope to see you again soon!\n";
-    private final List<String> list;
+    private final List<Task> list;
 
     public Controller() {
         list = new ArrayList<>();
@@ -20,11 +20,7 @@ public class Controller {
 
         while(!input.equals("bye")) {
             System.out.print(LINE);
-            if (input.equals("list")) {
-                printList();
-            } else {
-                addTask(input);
-            }
+            handleInput(input);
             System.out.print(LINE);
             input = sc.nextLine();
         }
@@ -32,16 +28,34 @@ public class Controller {
         System.out.println(LINE + ENDDUKE + LINE);
     }
 
+    private void handleInput(String input) {
+        if (input.startsWith("done")) {
+            doneTask(input);
+        } else if(input.equals("list")) {
+            printList();
+        } else {
+            addTask(input);
+        }
+    }
+
+    private void doneTask(String input) {
+        int index = Integer.parseInt(input.substring(5)) - 1;
+        Task task = list.get(index);
+        task.done();
+        String output = String.format("\t Nice! I've marked this task as done: \n \t\t %s", task);
+        System.out.println(output);
+    }
+
     private void addTask(String task) {
-        list.add(task);
+        list.add(new Task(task));
         String output = String.format("\t added: %s", task);
         System.out.println(output);
     }
 
     private void printList() {
         int num = 1;
-        for (String task : list) {
-            String output = String.format("\t %d. %s", num, task);
+        for (Task task : list) {
+            String output = String.format("\t %d.%s", num, task);
             System.out.println(output);
             num++;
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,6 @@
 public class Task {
     private boolean done;
-    private String description;
+    private final String description;
 
     public Task(String description) {
         this.description = description;
