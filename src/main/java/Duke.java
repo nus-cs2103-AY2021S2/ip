b04441d84caf7486a2d7d55d879f57b18b1f1114
laurenlhy <e0424895@u.nus.edu>
2021-01-16 22:57:56 +0800

    Add deadline, event, todo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,13 +12,28 @@ public class Duke {
             if (userInput.equals("bye")) {
                 Response.bye();
                 break;
-            } else if (userInput.equals("list")) {
+            }
+
+            if (userInput.equals("list")) {
                 Response.listTasks();
             } else if (userInput.contains("done")) {
                 int taskNumber = Integer.parseInt(userInput.substring(5));
                 Response.markAsDone(taskNumber);
+            } else if (userInput.contains("todo")) {
+                Task currentTask = new Todo(userInput.substring(5));
+                Response.addTask(currentTask);
+            } else if (userInput.contains("deadline")) {
+                int temp = userInput.indexOf('/') - 1;
+                Task currentTask = new Deadline(userInput.substring(9, temp),
+                        userInput.substring(temp + 4));
+                Response.addTask(currentTask);
+            } else if (userInput.contains("event")) {
+                int temp = userInput.indexOf('/') - 1;
+                Task currentTask = new Event(userInput.substring(6, temp),
+                        userInput.substring(temp + 4));
+                Response.addTask(currentTask);
             } else {
-                Response.addTask(userInput);
+                System.out.println("Error\n");
             }
         }
     }
--- src/main/java/Response.java
+++ src/main/java/Response.java
@@ -19,25 +19,26 @@ public class Response {
         System.out.println(line);
 
         for (int i = 1; i < numTasks + 1; i++) {
-            System.out.println(i + ". [" + tasks[i - 1].getStatusIcon()
-                    + "] " + tasks[i - 1].description);
+            System.out.println(i + ". " + tasks[i - 1].toString());
         }
 
         System.out.println(line + "\n");
     }
 
-    public static void addTask(String input) {
-        myPrint("added: " + input);
-        tasks[numTasks] = new Task(input);
+    public static void addTask(Task task) {
+        System.out.println(line + "\nokay, added this task:");
+        tasks[numTasks] = task;
         numTasks++;
+        System.out.println(task.toString());
+        System.out.println("you now have a total of " + numTasks + " tasks.");
+        System.out.println(line + "\n");
     }
 
     public static void markAsDone(int num) {
         System.out.println(line + "\nokay, this task has been marked as done:");
         Task currentTask = tasks[num - 1];
         currentTask.markAsDone();
-        System.out.println(num + ". [" + currentTask.getStatusIcon()
-                + "] " + currentTask.description);
+        System.out.println(num + ". " + currentTask.toString());
         System.out.println(line + "\n");
     }
 }
