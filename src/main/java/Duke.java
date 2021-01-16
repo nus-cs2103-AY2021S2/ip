c74eab6ea35dc70f932201b18bc2bcf303907312
enhao25 <larloller@gmail.com>
2021-01-16 13:05:45 +0800

    Add the ability to mark tasks as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,7 +13,7 @@ public class Duke {
 
         // User input data
         String userInput;
-        ArrayList<String> userInputs = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         // Loop through and echo the input until the user inputs "bye"
         while (true) {
@@ -25,12 +25,27 @@ public class Duke {
                 break;
             } else if(userInput.equals("list")) {
                 // Shows the list of user inputs with its numbering starting from 1
-                for (int index = 0; index < userInputs.size(); index++) {
-                    System.out.println(index + 1 + ". " + userInputs.get(index));
+                for (int index = 0; index < taskList.size(); index++) {
+                    System.out.println(index + 1 + ". " + taskList.get(index).getTask());
+                }
+            } else if (userInput.length() > 4 && userInput.startsWith("done")) {
+                // Mark the task as done based on the number given after the "done" input
+                int taskIndex = Integer.parseInt(userInput.substring(5));
+
+                // Print a response to the user
+                try {
+                    // Successfully marked the task as done
+                    System.out.println("Nice! I've marked this task as done:\n "
+                        + "   " + taskList.get(taskIndex - 1).markAsDone());
+                } catch (IndexOutOfBoundsException exception) {
+                    // Print an error message if the task number is invalid
+                    System.out.println("The task number you have entered in invalid. \n" +
+                        "Please try again later.");
                 }
             } else {
-                // Otherwise, store the user input into an arraylist
-                userInputs.add(userInput);
+                // Otherwise, store the user input as task into an arraylist
+                Task newTask = new Task(userInput);
+                taskList.add(newTask);
                 System.out.println("added: " + userInput);
             }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,9 +7,20 @@ public class Task {
         this.isDone = false;
     }
 
-    public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    // Mark the task as done and return a success message from getTask()
+    public String markAsDone() {
+        this.isDone = true;
+        return getTask();
     }
 
-    //...
+    // Get the status icon depending on whether the task is done or not
+    public String getStatusIcon() {
+        return (isDone ? "\u2718" : " "); //return X symbols if done, else return blank space
+    }
+
+    // Return the task in the format [tick / X] <Task Description>
+    public String getTask() {
+        return String.format("[%s] %s", getStatusIcon(), description);
+    }
+
 }
