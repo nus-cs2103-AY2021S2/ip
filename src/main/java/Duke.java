118b17b46b41d07f8d18f53bfe1fddc5cf32bf10
kaixiangtay <taykaixiang97@gmail.com>
2021-01-16 23:42:40 +0800

    Created Duke Project Level 1 with greet, echo and exit features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        DukeBot dukeBot = new DukeBot();
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.next();
+
+        while(dukeBot.echo(userInput)) {
+            userInput = sc.next();
+        }
     }
 }
---
+++ src/main/java/DukeBot.java
@@ -0,0 +1,25 @@
+public class DukeBot {
+    private final String border = "\t___________________________________\n";
+    private String output;
+
+    public DukeBot() {
+        greeting();
+    }
+
+    public void greeting() {
+        output = border + "\t Hello! I'm Duke\n" + "\t What can I do for you?\n" + border;
+        System.out.println(output);
+    }
+
+    public boolean echo(String command) {
+        boolean continueInput = true;
+        if(command.equals("bye")) {
+            output = border + "\t" + "Bye. Hope to see you again soon!\n" + border;
+            continueInput = false;
+        } else {
+            output = border + "\t" + command + "\n" + border;
+        }
+        System.out.println(output);
+        return continueInput;
+    }
+}
