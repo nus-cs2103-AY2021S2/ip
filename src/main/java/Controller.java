96517fcfa8047c7c849fc7090ffdff2354c8490a
Marcus Ong Jing Keat <markuz5116@gmail.com>
2021-01-16 19:01:20 +0800

    Add List functionality and adding items to the list

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Controller {
     private final static String LINE = "\t__________________________________\n";
     private final static String GREETING = "\t Hello! I'm Duke\n\t What can I do for you? \n";
     private final static String ENDDUKE = "\t Bye. Hope to see you again soon!\n";
+    private final List<String> list;
+
+    public Controller() {
+        list = new ArrayList<>();
+    }
 
     public void run() {
         String startMsg = LINE + GREETING + LINE;
@@ -13,11 +20,30 @@ public class Controller {
 
         while(!input.equals("bye")) {
             System.out.print(LINE);
-            System.out.println("\t " + input);
+            if (input.equals("list")) {
+                printList();
+            } else {
+                addTask(input);
+            }
             System.out.print(LINE);
             input = sc.nextLine();
         }
 
         System.out.println(LINE + ENDDUKE + LINE);
+    }
+
+    private void addTask(String task) {
+        list.add(task);
+        String output = String.format("\t added: %s", task);
+        System.out.println(output);
+    }
+
+    private void printList() {
+        int num = 1;
+        for (String task : list) {
+            String output = String.format("\t %d. %s", num, task);
+            System.out.println(output);
+            num++;
+        }
     }
 }
