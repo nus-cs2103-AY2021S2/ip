7ac6da9e7bb45a80029cdfbe4d00fbd1d88f904a
SiTingST <33442908+SiTingST@users.noreply.github.com>
2021-01-17 02:54:40 +0800

    Completed Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+    private Scanner sc = new Scanner(System.in);
+    final String lines = "----------------------------------------";
+
+    public void displayWelcomeMessage(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hi There! Greetings from \n" + logo);
+        System.out.println(lines + "\n" + " Good day! I'm Duke" + "\n" + " How can I help you today? " + "\n" + lines);
+
+    }
+
+    public void displayEndMessage(){
+        sc.close();
+        System.out.println( lines + "\n" + " Bye. Hope to see you again!" + "\n" + lines);
+    }
+
+    public void main(String[] args) {
+
+        displayWelcomeMessage();
+
+        String userInput = sc.nextLine();
+
+        while(!userInput.equals("bye")) {
+            System.out.println("\t" + lines + "\n\t" + " " + userInput + "\n\t" + lines);
+            userInput = sc.nextLine();
+        }
+        displayEndMessage();
     }
 }
