a504d67b0e1195f1907f6f2950bb1ce138a26f6f
darkdestry-t <tanki123+temp@hotmail.com>
2021-01-16 18:09:48 +0800

    Add chat loop

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,53 @@
+
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import java.io.IOException;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+
+    public static void main(String[] args) throws IOException {
+        String logo = " ____        _        \n" + "|  _ \\ _   _| | _____ \n" + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(greetingMessage);
+
+        chatLoop(System.in, System.out);
+    }
+
+    public static String greetingMessage = "Hello! I'm Duke\n" + "What can I do for you?";
+
+    public static void chatLoop(InputStream in, OutputStream out) throws IOException {
+        BufferedReader reader = getReader(in);
+        PrintStream writer = getWriter(out);
+        String line = reader.readLine();
+        while (line != null) {
+            writer.println(parseInput(line));
+            if (line.equals("bye")) {
+                break;
+            }
+            line = reader.readLine();
+        }
+    }
+
+    private static PrintStream getWriter(OutputStream out) {
+        return new PrintStream(out);
+    }
+
+    private static BufferedReader getReader(InputStream in) {
+        return new BufferedReader(new InputStreamReader(in));
     }
 
     public static String parseInput(String input) {
-        switch(input) {
+        switch (input) {
             case "bye":
                 return "Bye. Hope to see you again soon!";
+            default:
+                return input;
         }
-        return input;
     }
+
 }
--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +15,30 @@ public class DukeTest {
     @Test
     public void parseInputByeOutputFarewell() {
         assertEquals("Bye. Hope to see you again soon!", Duke.parseInput("bye"));
+    }
+
+    @Test
+    public void chatInputByeExitsLoop() throws IOException {
+        String testInput = "bye\n" + "hello\n" + "bye\n";
+
+        InputStream ins = new ByteArrayInputStream(testInput.getBytes(StandardCharsets.UTF_8));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Duke.chatLoop(ins, out);
+
+        assertEquals("Bye. Hope to see you again soon!\n", new String(out.toByteArray()));
+    }
+
+    @Test
+    public void chatLoopWritesToOutput() throws IOException {
+        String testInput = "echoThis\n" + "echoThisAsWell\n" + "echoThisFinally\n";
+
+        InputStream ins = new ByteArrayInputStream(testInput.getBytes(StandardCharsets.UTF_8));
+        BufferedReader in = new BufferedReader(new InputStreamReader(ins));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        Duke.chatLoop(ins, out);
+
+        assertEquals(null, in.readLine());
+        assertEquals(testInput, new String(out.toByteArray()));
     }
 }
