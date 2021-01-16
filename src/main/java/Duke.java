e2ce8263bfcb7ea65f15800852b23239d1cfd4f0
Zong Yu <hanzongyu@live.com>
2021-01-17 01:28:36 +0800

    add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.*;
+
 public class Duke {
+    public static String border = "    ********************************* \n";
+
+    public static void process(String input) {
+        System.out.println(border + "     " + input + "\n" + border);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcomeMsg = "         █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█ \n" + "         █░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█ \n"
+                + "         █░░║║║╠─║─║─║║║║║╠─░░█ \n" + "         █░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█ \n"
+                + "         █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ \n";
+        String logo = "         ──────▄▀▄─────▄▀▄ \n" + "         ─────▄█░░▀▀▀▀▀░░█▄ \n"
+                + "         ─▄▄──█░░░░░░░░░░░█──▄▄ \n" + "         █▄▄█─█░░▀░░┬░░▀░░█─█▄▄█";
+
+        String greeting = "     Hey there! I'm Kawaii Kat \n" + "     How can i assist you nya~? \n";
+
+        String exitMessage = "     Farewell. See you soon :)! \n";
+
+        System.out.println(welcomeMsg + logo + "\n" + border + greeting + border);
+
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        while (!input.equals(("bye"))) {
+            process(input);
+            input = sc.nextLine();
+        }
+
+        System.out.println(border + exitMessage + border);
+
     }
 }
