5a16ac275f106e2537ccea2aca05fece8416fdd3
zoeykobe <kelvinzhou981209@gmail.com>
2021-01-17 00:38:33 +0800

    Level 2. Add, List
    Implementing adding and listing of to-dos

--- src/main/java/Kelbot.java
+++ src/main/java/Kelbot.java
@@ -1,13 +1,24 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Kelbot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String greeting = "Hello! I'm Kelbot\n" + "What can I do for you?";
-        System.out.println(greeting);
+        System.out.println("Hello! I'm Kelbot\n" + "What can I do for you?");
+        List<String> toDoList = new ArrayList<>();
         String input = sc.nextLine();
-        while (!input.equals("bye")) {
-            System.out.println(input);
+        while (true) {
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                // List out all the to-dos
+                for (int i = 1; i <= toDoList.size(); i++) {
+                    System.out.println(i + ". " + toDoList.get(i - 1));
+                }
+            } else {
+                // Add to-do to list
+                toDoList.add(input);
+                System.out.println("added: " + input);
+            }
             input = sc.nextLine();
         }
         System.out.println("Bye le Bye!");
