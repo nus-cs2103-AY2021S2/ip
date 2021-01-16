07ac7070aa03aabe8850f671a974055e179167a5
NBH99 <lametiger@hotmail.com>
2021-01-16 23:44:52 +0800

    Added Level-1 in Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke\n"
+                + "What can I do for you?");
+
+        Scanner scan = new Scanner(System.in);
+        String string = scan.nextLine();
+
+        while (!string.equals("bye")) {
+            System.out.println(string);
+            string = scan.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon");
+
     }
+
 }
