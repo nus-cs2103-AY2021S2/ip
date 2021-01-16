b83c7a957bffa7cf10c5a450a4bcb5bce3bc72e3
Zong Yu <hanzongyu@live.com>
2021-01-17 01:30:29 +0800

    add Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
 import java.util.*;
 
 public class Duke {
-    public static String border = "    ********************************* \n";
+    public static String topBorder = "\n    ********************************* \n";
+    public static String btmBorder = "    ********************************* \n";
+    public static ArrayList<String> list = new ArrayList<>();
 
-    public static void process(String input) {
-        System.out.println(border + "     " + input + "\n" + border);
+    public static void printBox(String input) {
+        System.out.println(topBorder + "     " + input + "\n" + btmBorder);
+    }
+
+    public static void process(String command) {
+        if (command.equals("list")) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= list.size(); i++) {
+                if (i == list.size()) {
+                    sb.append("     " + i + ") " + list.get(i - 1));
+                } else {
+                    if (i == 1) {
+                        sb.append(i + ") " + list.get(i - 1) + "\n");
+                    } else {
+                        sb.append("     " + i + ") " + list.get(i - 1) + "\n");
+
+                    }
+                }
+            }
+            printBox(sb.toString());
+        } else {
+            list.add(command);
+            printBox("added: " + command);
+        }
     }
 
     public static void main(String[] args) {
@@ -12,13 +36,14 @@ public class Duke {
                 + "         █░░║║║╠─║─║─║║║║║╠─░░█ \n" + "         █░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█ \n"
                 + "         █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ \n";
         String logo = "         ──────▄▀▄─────▄▀▄ \n" + "         ─────▄█░░▀▀▀▀▀░░█▄ \n"
-                + "         ─▄▄──█░░░░░░░░░░░█──▄▄ \n" + "         █▄▄█─█░░▀░░┬░░▀░░█─█▄▄█";
+                + "         ─▄▄──█░░░░░░░░░░░█──▄▄ \n" + "         █▄▄█─█░░▀░░┬░░▀░░█─█▄▄█ ";
 
-        String greeting = "     Hey there! I'm Kawaii Kat \n" + "     How can i assist you nya~? \n";
+        String greeting = "Hey there! I'm Kawaii Kat \n" + "     How can i assist you nya~?";
 
-        String exitMessage = "     Farewell. See you soon :)! \n";
+        String exitMessage = "Farewell. See you soon :)!";
 
-        System.out.println(welcomeMsg + logo + "\n" + border + greeting + border);
+        System.out.println(welcomeMsg + logo);
+        printBox(greeting);
 
         Scanner sc = new Scanner(System.in);
 
@@ -29,7 +54,7 @@ public class Duke {
             input = sc.nextLine();
         }
 
-        System.out.println(border + exitMessage + border);
+        printBox(exitMessage);
 
     }
 }
