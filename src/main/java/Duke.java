6eee86b274f9ca45a905c6bd14fd6304a7913352
picasdan9 <picasdan99@gmail.com>
2021-01-16 21:14:30 +0800

    Implement greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        greeting();
+        
+        String userCmd = sc.nextLine();
+        while (!userCmd.equals("bye")) {
+            responseWrapper(userCmd);
+            userCmd = sc.nextLine();
+        }
+        exit();
+        sc.close();
+    }
+
+    public static void printLineBreak() {
+        System.out.println("\t____________________________________________________________");
+    }
+
+    public static void responseWrapper(String msg) {
+        printLineBreak();
+        System.out.println(String.format("\t%s", msg));
+        printLineBreak();
+    }
+
+    public static void greeting() {
+        printLineBreak();
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+        printLineBreak();
+    }
+
+    public static void exit() {
+        responseWrapper("Bye. Hope to see you again soon!");
     }
 }
