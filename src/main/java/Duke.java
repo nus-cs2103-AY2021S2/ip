029c6bd2fe690b68371c0fef787dd475731011f0
mabel-kang <e0425446@u.nus.edu>
2021-01-16 18:51:36 +0800

    Completed Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,15 +11,23 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        List<String> store = new ArrayList<String>();
         Scanner scan = new Scanner(System.in);
         while(true) {
             String command = scan.nextLine();
             if (command.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (command.equalsIgnoreCase("list")) {
+                for(int i =0; i < store.size(); i++) {
+                    System.out.println((i + 1) + ". " + store.get(i));
+                }
             } else {
-                System.out.println(command);
+                store.add(command);
+                System.out.println("added: " + command);
             }
         }
+
     }
+
 }
