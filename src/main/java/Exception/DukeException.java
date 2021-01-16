6d1e7d344fb23ffcc16b74424efe3913d4420820
BennyPhoe <bennyphoe1998@gmail.com>
2021-01-17 02:44:02 +0800

    level 5 error handling and level 6 delete

---
+++ src/main/java/Exception/DukeException.java
@@ -0,0 +1,7 @@
+package Exception;
+
+public class DukeException extends Exception {
+    public DukeException(String errorMessage) {
+        super(errorMessage);
+    }
+}
--- src/main/java/UserInput.java
+++ src/main/java/UserInput.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import Exception.*;
 
 public class UserInput {
     private Scanner sc;
@@ -14,22 +15,30 @@ public class UserInput {
     public void executeInput() {
         while (sc.hasNextLine()) {
             String userInput = sc.nextLine();
-            String[] inputBreakdown = userInput.split(" ");
-            if (inputBreakdown[0].equals("bye")) {
-                indentInput("Bye. Hope to see you again!");
-                break;
-            } else if (inputBreakdown[0].equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < this.newStorage.size(); i++) {
-                    printTask(i, this.newStorage.get(i));
+            try {
+                checkInput(userInput);
+
+                String[] inputBreakdown = userInput.split(" ");
+                if (inputBreakdown[0].equals("bye")) {
+                    indentInput("Bye. Hope to see you again!");
+                    break;
+                } else if (inputBreakdown[0].equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < this.newStorage.size(); i++) {
+                        printTask(i, this.newStorage.get(i));
+                    }
+                } else if (inputBreakdown[0].equals("done") && Integer.valueOf(inputBreakdown[1]) > 0 && Integer.valueOf(inputBreakdown[1]) <= this.newStorage.size() + 1) {
+                    System.out.println("Nice! I've marked this task as done:");
+                    int selectedIndex = Integer.valueOf(inputBreakdown[1]) - 1;
+                    this.newStorage.get(selectedIndex).setDone(true);
+                    printTask(selectedIndex, this.newStorage.get(selectedIndex));
+                } else if (inputBreakdown[0].equals("delete")) {
+                    deleteTask(userInput);
+                } else {
+                    addTask(userInput);
                 }
-            } else if (inputBreakdown[0].equals("done") && Integer.valueOf(inputBreakdown[1]) > 0 && Integer.valueOf(inputBreakdown[1]) <= this.newStorage.size() + 1) {
-                System.out.println("Nice! I've marked this task as done:");
-                int selectedIndex = Integer.valueOf(inputBreakdown[1]) - 1;
-                this.newStorage.get(selectedIndex).setDone(true);
-                printTask(selectedIndex, this.newStorage.get(selectedIndex));
-            } else {
-                addTask(userInput);
+            } catch (DukeException ex) {
+                System.out.println(ex.getMessage());
             }
         }
     }
@@ -75,5 +84,35 @@ public class UserInput {
         System.out.println("Got it. I've added this task: ");
         printTaskWithNoNum(task);
         System.out.println("Now you have " + this.newStorage.size() + " tasks in the list.");
+    }
+
+    public void deleteTask(String userInput) {
+        String[] inputBreakdown = userInput.split(" ");
+        int taskToBeDeleted = Integer.valueOf(inputBreakdown[1]) - 1;
+        Task task = this.newStorage.get(taskToBeDeleted);
+        this.newStorage.remove(taskToBeDeleted);
+        System.out.println("Noted. I've removed this task:");
+        printTaskWithNoNum(task);
+        System.out.println("Now you have " + this.newStorage.size() + " tasks in the list.");
+    }
+
+    public void checkInput (String userInput) throws DukeException {
+        String[] input = userInput.split(" ");
+        List<String> possibleActionInputs = new ArrayList<>();
+        List<String> possibleSingleInputs = new ArrayList<>();
+        possibleActionInputs.add("done");
+        possibleSingleInputs.add("delete");
+        possibleSingleInputs.add("bye");
+        possibleActionInputs.add("todo");
+        possibleActionInputs.add("event");
+        possibleActionInputs.add("deadline");
+        possibleSingleInputs.add("list");
+        if (!possibleActionInputs.contains(input[0]) && !possibleSingleInputs.contains(input[0])) {
+            throw new DukeException("user action is not recognised!");
+        } else {
+            if (possibleActionInputs.contains(input[0]) && input.length == 1) {
+                throw new DukeException("no description added!");
+            }
+        }
     }
 }
