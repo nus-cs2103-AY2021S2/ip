3f2c5db9d1f42da84b42bc9b73df8d58240d6912
Marcus Ong Jing Keat <markuz5116@gmail.com>
2021-01-16 20:12:00 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    private boolean done;
+    private String description;
+
+    public Task(String description) {
+        this.description = description;
+        done = false;
+    }
+
+    public void done() {
+        done = true;
+    }
+
+    @Override
+    public String toString() {
+        String output;
+        if (done) {
+            output = String.format("[X] %s", description);
+        } else {
+            output = String.format("[ ] %s", description);
+        }
+        return output;
+    }
+}
