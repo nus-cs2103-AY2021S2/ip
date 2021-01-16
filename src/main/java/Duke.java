7e3e0d7e8b211981ce980ed3bc9711ddd9976c48
Tomashiwa <wongqinliang@gmail.com>
2021-01-16 13:00:30 +0800

    Integrate ToDo, Deadline and Event into Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,16 +9,47 @@ public class Duke {
     private static List<Task> tasks = new ArrayList<>();
 
     /**
-     * Add a task to internal list
-     * @param desc Description of the task
+     * Print an adding message corresponding to the added Task's type
+     * @param task Task that was added
      */
-    private static void addTask(String desc) {
-        Task newTask = new Task(desc);
-        tasks.add(newTask);
-
+    private static void printTaskAdding(Task task) {
         System.out.println("___________________________________________________________");
-        System.out.printf("added: %s\n", desc);
+        System.out.println("Got it meow. I've added this task: ");
+        System.out.printf("  [%s][%s] %s\n", task.getTypeSymbol(), task.getStatusSymbol(), task.getDesc());
+        System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
         System.out.println("___________________________________________________________\n");
+    }
+
+    /**
+     * Add a ToDo into the internal list
+     * @param desc Description of the ToDo
+     */
+    private static void addToDo(String desc) {
+        ToDo newToDo = new ToDo(desc);
+        tasks.add(newToDo);
+        printTaskAdding(newToDo);
+    }
+
+    /**
+     * Add a Deadline into the internal list
+     * @param desc Description of the Deadline
+     * @param dateTime Date/Time that the Deadline must be done
+     */
+    private static void addDeadline(String desc, String dateTime) {
+        Deadline newDeadline = new Deadline(desc, dateTime);
+        tasks.add(newDeadline);
+        printTaskAdding(newDeadline);
+    }
+
+    /**
+     * Add an Event into the internal list
+     * @param desc Description of the Event
+     * @param dateTimeRange Date/Time range when the Event take place
+     */
+    private static void addEvent(String desc, String dateTimeRange) {
+        Event newEvent = new Event(desc, dateTimeRange);
+        tasks.add(newEvent);
+        printTaskAdding(newEvent);
     }
 
     /**
@@ -34,7 +65,8 @@ public class Duke {
 
             System.out.println("___________________________________________________________");
             System.out.println("Good job meow, I've marked this task as done:");
-            System.out.printf("%d.[%s] %s\n", index + 1, task.getStatusSymbol(), task.getDesc());
+            System.out.printf("%d.[%s][%s] %s\n", index + 1, task.getTypeSymbol(), task.getStatusSymbol(),
+                    task.getDesc());
             System.out.println("___________________________________________________________\n");
         }
     }
@@ -47,7 +79,7 @@ public class Duke {
         System.out.println("Meow, here are the tasks in your list:");
         for(int i = 0; i < tasks.size(); i++) {
             Task task = tasks.get(i);
-            System.out.printf("%d.[%s] %s\n", i + 1, task.getStatusSymbol(), task.getDesc());
+            System.out.printf("%d.[%s][%s] %s\n", i + 1, task.getTypeSymbol(), task.getStatusSymbol(), task.getDesc());
         }
         System.out.println("___________________________________________________________\n");
     }
@@ -70,7 +102,16 @@ public class Duke {
         while(isActive) {
             command = scanner.nextLine();
 
-            if(command.matches("^done ((100)|[1-9][0-9]|[0-9])$")) {
+            if(command.matches("^todo .+$")) {
+                String desc = command.substring(5);
+                addToDo(desc);
+            } else if(command.matches("^deadline .+$")) {
+                String[] splits = command.substring(9).split(" /by ");
+                addDeadline(splits[0], splits[1]);
+            } else if(command.matches("^event .+$")) {
+                String[] splits = command.substring(6).split(" /at ");
+                addEvent(splits[0], splits[1]);
+            } else if(command.matches("^done ((100)|[1-9][0-9]|[0-9])$")) {
                 String[] splits = command.split(" ");
                 try {
                     completeTask(Integer.valueOf(splits[1]) - 1);
@@ -91,7 +132,9 @@ public class Duke {
             } else if(command.equals("bye")) {
                 isActive = false;
             } else {
-                addTask(command);
+                System.out.println("___________________________________________________________");
+                System.out.println("No such command, Please try again with another command meow.");
+                System.out.println("___________________________________________________________\n");
             }
         }
 
