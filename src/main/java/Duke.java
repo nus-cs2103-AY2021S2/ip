279fb5146f4b276602193fbd8378f7100ed257ed
Marcus Lee <marcus_lee@u.nus.edu>
2021-01-16 15:42:03 +0800

    modified Duke.java to provide functionality for Level-2
    Implemented a String array (named `records`) to contain user entries.
    Since it can be assumed that there will be at most 100 entries, a fixed size
    array initialised to 100 was implemented.
    
    Modified the while-loop in Duke.main() to add entries to `records`.
    
    Modified the while-loop in Duke.main() to list out entries in `records`.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,34 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static final String BORDER = "---------------";
+    private static final String BORDER = "-------------------------------------";
 
     public static void main(String[] args) {
         Duke.printWithBorders("Hello! I'm Duke\nWhat can I do for you?");
 
         Scanner sc = new Scanner(System.in);
 
+        String[] records = new String[100];
+        int numberOfRecords = 0;
+
         while (sc.hasNextLine()) {
             String userInput = sc.nextLine();
             if (userInput.equals("bye")) {
                 Duke.printWithBorders("Bye. Hope to see you soon!");
                 return;
+            } else if (userInput.equals("list")) {
+                String output = "";
+                for (int i = 0; i < numberOfRecords; i++) {
+                    output += (i + 1) + ". " + records[i];
+                    if (i != numberOfRecords - 1) {
+                        output += "\n";
+                    }
+                }
+                Duke.printWithBorders(output);
             } else {
-                Duke.printWithBorders(userInput);
+                records[numberOfRecords++] = userInput;
+                String output = "added: " + userInput;
+                Duke.printWithBorders(output);
             }
         }
     }
