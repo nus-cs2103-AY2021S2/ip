f4d2870c6f6f43472435ab67041b4e209cc9cb00
JayChenYJ <jaychenyj@gmail.com>
2021-01-16 23:26:14 +0800

    Level 1 completed, to add javadoc comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+
+        Scanner sc = new Scanner(System.in);
+        Boolean endOfCycle = false;
+
+        while(!endOfCycle) {
+            String nextCommand = sc.nextLine();
+            if (nextCommand.equals("bye")) {
+                bye();
+                endOfCycle = true;
+            } else {
+                echo(nextCommand);
+            }
+        }
+
+        sc.close();
+    }
+
+    public static void greet() {
+        System.out.println("Hello! I'm Jay \n" + "What can I do for you?");
+    }
+
+    public static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void echo(String command) {
+        System.out.println(command);
     }
 }
+
