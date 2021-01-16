c298509eadbfed02ba9dfce66813ff447bc866a5
Marcus Ong Jing Keat <markuz5116@gmail.com>
2021-01-16 18:43:34 +0800

    Add echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,8 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //System.out.println("Hello from\n" + logo);
+        Controller c = new Controller();
+        c.run();
     }
 }
