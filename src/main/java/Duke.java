c8ce555d838c34f9f0108c1232a10534311adc02
yienyoong <yoong.yi.en.2000@gmail.com>
2021-01-17 01:17:52 +0800

    Added Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,50 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String TOP_BORDER = "╭--------------------------------------------╮";
+    private static final String BTM_BORDER = "╰|╱ -----------------------------------------╯";
+    private static final String PADDING = "  ";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printGreeting();
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            echoInput(input);
+            input = sc.nextLine();
+        }
+        sc.close();
+
+        printBye();
+    }
+
+    public static void printGreeting() {
+        String logo = "              .--.    .-.         .-.   \n" +
+                "             : .-'    : :         : :   \n" +
+                " .--.  .--.  : `;.--. : `-.  .--. : `-. \n" +
+                "'  ..'' .; ; : :' '_.'' .; :' '_.'' .; :\n" +
+                "`.__.'`.__,_;:_;`.__.'`.__.'`.__.'`.__.'";
+        System.out.println(logo);
+        String greeting = " ╭------------------------------------------------------------------╮\n"
+                        + " |  Hello! I'm cafebeb, here to help you keep track of measly tasks | \n"
+                        + " |  in your mundane human life. How may I help you today?           |\n"
+                        + " ╰|╱ ---------------------------------------------------------------╯  \n";
+
+        System.out.println(greeting);
+    }
+
+    public static void printBye() {
+        String farewell = " ╭---------------------------------------╮\n"
+                        + " |  Bye! Hope you complete your tasks!   |\n"
+                        + " ╰|╱ ------------------------------------╯\n";
+        System.out.println(farewell);
+    }
+
+    public static void echoInput(String input) {
+        System.out.println(TOP_BORDER);
+        System.out.println(PADDING + input);
+        System.out.println(BTM_BORDER);
     }
 }
+
