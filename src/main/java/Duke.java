f053179d7b9f0292e5a9adaf61d4ec4ee828a4ae
NBH99 <lametiger@hotmail.com>
2021-01-17 00:25:31 +0800

    Added Level-3 in Duke and Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,21 +9,43 @@ public class Duke {
 
         Scanner scan = new Scanner(System.in);
         String string = scan.nextLine();
-        String[] str = new String[100];
+        Task[] tasks = new Task[100];
         int k, i = 0;
 
         while (!string.equals("bye")) {
+
             if (string.equals("list")) {
+
+                System.out.println("Here are the tasks in your list:");
                 for (int j = 0; j < i; j++) {
                     k = j + 1;
-                    System.out.println(k + ". " + str[j]);
+                    System.out.println(k + ".[" + tasks[j].getStatusIcon()
+                            + "] " + tasks[j].description);
                 }
-                string = scan.nextLine();
+
             } else {
-                System.out.println("added: " + string);
-                str[i++] = string;
-                string = scan.nextLine();
+
+                String front = " ", back = " ";
+                if (string.contains(" ")) {
+                    String[] str = string.split(" ");
+                    front = str[0];
+                    back = str[1];
+                }
+
+                if (front.equals("done")) {
+                    k = Integer.parseInt(back) - 1;
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  [" + tasks[k].getStatusIcon()
+                            + "] " + tasks[k].description);
+                    tasks[k].markAsDone();
+                } else {
+                    System.out.println("added: " + string);
+                    tasks[i++] = new Task(string);
+                }
+
             }
+            string = scan.nextLine();
+
         }
 
         System.out.println("Bye. Hope to see you again soon");
