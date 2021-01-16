4aec1c21c65fc077e32cf0b090ca16f86982e024
kaixiangtay <taykaixiang97@gmail.com>
2021-01-17 01:16:28 +0800

    Created Duke Project Level 2 with add and list features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,10 +4,10 @@ public class Duke {
     public static void main(String[] args) {
         DukeBot dukeBot = new DukeBot();
         Scanner sc = new Scanner(System.in);
-        String userInput = sc.next();
+        String userInput = sc.nextLine();
 
         while(dukeBot.echo(userInput)) {
-            userInput = sc.next();
+            userInput = sc.nextLine();
         }
     }
 }
--- src/main/java/DukeBot.java
+++ src/main/java/DukeBot.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class DukeBot {
     private static final String BORDER = "\t___________________________________\n";
     private String output;
+    private final ArrayList<String> taskList = new ArrayList<>();
 
     public DukeBot() {
         greeting();
@@ -11,16 +14,33 @@ public class DukeBot {
         System.out.println(output);
     }
 
-    public boolean echo(String command) {
+    public boolean echo(String input) {
         boolean continueInput = true;
 
-        if(command.equals("bye")) {
-            output = BORDER + "\t" + "Bye. Hope to see you again soon!\n" + BORDER;
-            continueInput = false;
-        } else {
-            output = BORDER + "\t" + command + "\n" + BORDER;
+        switch (input) {
+            case "bye":
+                output = BORDER + "\t" + " Bye. Hope to see you again soon!\n" + BORDER;
+                continueInput = false;
+                break;
+            case "list":
+                output = retrieveList(taskList);
+                break;
+            default:
+                output = BORDER + "\t added: " + input + "\n" + BORDER;
+                taskList.add(input);
+                break;
         }
         System.out.println(output);
         return continueInput;
+    }
+
+    public String retrieveList(ArrayList<String> taskList) {
+        StringBuilder str = new StringBuilder(BORDER);
+
+        for (int num = 1; num <= taskList.size(); num++) {
+            str.append("\t ").append(num).append(". ").append(taskList.get(num - 1)).append("\n");
+        }
+        str.append(BORDER);
+        return str.toString();
     }
 }
