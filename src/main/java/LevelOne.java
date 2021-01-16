7a1dae4643c486572bb749805b74daa4be24cfa6
hengyongming <e0425848@u.nus.edu>
2021-01-16 16:00:37 +0800

    Level-1 completed

---
+++ src/main/java/LevelOne.java
@@ -0,0 +1,12 @@
+public class LevelOne {
+
+     public String echo(String input){
+         return input;
+     }
+
+     @Override
+    public String toString(){
+         return "Hello! I'm Duke \nWhat can I do for you?";
+
+     }
+}
