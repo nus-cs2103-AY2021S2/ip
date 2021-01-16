15a112519b8278acd8e7f78ce220057487f894e1
Jellybeano <lanyuxuan98@gmail.com>
2021-01-17 00:49:59 +0800

    Automated Text UI Testing (runtest.bat reports difference when there is none)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -187,6 +187,7 @@ public class Duke {
                     break;
                 default:
                     System.out.println("uwu");
+                    break;
             }
         }
     }
