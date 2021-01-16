1ee7a8bc35354d9ad697bdd5fb909df39081c6b7
Samuel Fang <samuelfangjw@gmail.com>
2021-01-16 20:09:16 +0800

    Implemented Level 4 and A-Inheritance

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,19 @@
+import Task.*;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static String LINE_BREAK = "------------------------------------------------------------";
-    private static String INDENT = "    ";
-    private static String BOT_NAME = "Chip the Squirrel";
-    private static ArrayList<Task> tasks = new ArrayList<>();
+    private static final String LINE_BREAK = "------------------------------------------------------------";
+    private static final String INDENT = "    ";
+    private static final String BOT_NAME = "Chip the Squirrel";
+    private static final ArrayList<Task> tasks = new ArrayList<>();
+
+    enum TaskType {
+        DEADLINE,
+        TODO,
+        EVENT
+    }
 
     public static void printWithIndentation(String ... strings) {
         System.out.println(INDENT + LINE_BREAK);
@@ -31,10 +39,35 @@ public class Duke {
         }
     }
 
-    public static void addTask(String task) {
-        tasks.add(new Task(task));
+    public static void addTask(TaskType taskType, String input) {
+        String[] tokens;
+        Task task;
 
-        printWithIndentation("added: " + task);
+        switch (taskType) {
+            case TODO:
+                task = new Todo(input);
+                break;
+            case DEADLINE:
+                tokens = input.split(" /by ", 2);
+                input = tokens[0];
+                task = new Deadline(input, tokens[1]);
+                break;
+            case EVENT:
+                tokens = input.split(" /at ", 2);
+                input = tokens[0];
+                task = new Event(input, tokens[1]);
+                break;
+            default:
+                printWithIndentation("I do not understand.");
+                return;
+        }
+
+        tasks.add(task);
+        int numTasks =  tasks.size();
+        String formattedTasksCount = numTasks > 1 ? String.format("%d tasks", numTasks) : "1 task";
+        printWithIndentation("Got it! I've added this task: ",
+                "  " + task.toString(),
+                "Now you have " + formattedTasksCount + " in the list.");
     }
 
     public static void main(String[] args) {
@@ -43,7 +76,7 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNext()) {
-            String input = sc.nextLine();
+            String input = sc.nextLine().trim();
             String[] tokens = input.split(" ", 2);
             String command = tokens[0];
 
@@ -51,6 +84,15 @@ public class Duke {
                 case "bye":
                     printWithIndentation("Bye! Hope to see you again soon!");
                     System.exit(0);
+                    break;
+                case "todo":
+                    addTask(TaskType.TODO, tokens[1]);
+                    break;
+                case "event":
+                    addTask(TaskType.EVENT, tokens[1]);
+                    break;
+                case "deadline":
+                    addTask(TaskType.DEADLINE, tokens[1]);
                     break;
                 case "done":
                     int idx = Integer.parseInt(tokens[1]) - 1;
@@ -62,7 +104,7 @@ public class Duke {
                     printTasks();
                     break;
                 default:
-                    addTask(input);
+                    printWithIndentation("I do not understand.");
             }
         }
     }
