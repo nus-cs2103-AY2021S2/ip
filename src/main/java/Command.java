5fdc65463275d77ce04f4eca9397c5c826c566ad
WANG_YUANZHE <e0411258@u.nus.edu>
2021-01-16 21:27:11 +0800

    Exception message updated

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -7,7 +7,7 @@ public class Command {
             return CommandType.LIST;
         } else if (str.length() >= 8 && str.substring(0, 8).equalsIgnoreCase("deadline")) {
             if (str.length() == 8 || (str.length() <= 9 && str.charAt(8) != ' ')) {
-                throw new DukeException("No argument");
+                throw new DukeException("No argument: Argument field cannot be empty");
             } else if (str.charAt(8) != ' ') {
                 return CommandType.OTHER;
             } else {
@@ -16,25 +16,25 @@ public class Command {
                         && str.charAt(indexOfBy + 3) == ' ') {
                     return CommandType.DEADLINE;
                 } else {
-                    throw new DukeException("Invalid argument");
+                    throw new DukeException("Invalid argument: '/by' is not detected");
                 }
             }
         } else if (strLength >= 6 && str.substring(0, 6).equalsIgnoreCase("delete")) {
             if (str.length() == 6 || (str.length() <= 7 && str.charAt(6) == ' ')) {
-                throw new DukeException("No argument");
+                throw new DukeException("No argument: Argument field cannot be empty");
             } else if (str.charAt(6) != ' ') {
                 return CommandType.OTHER;
             } else {
                 try {
                     Integer.parseInt(str.substring(7));
                 } catch (NumberFormatException e) {
-                    throw new DukeException("Invalid argument");
+                    throw new DukeException("Invalid argument: Argument must be an integer");
                 }
                 return CommandType.DELETE;
             }
         } else if (strLength >= 5 && str.substring(0, 5).equalsIgnoreCase("event")) {
             if (str.length() == 5 || (str.length() <= 6 && str.charAt(5) == ' ')) {
-                throw new DukeException("No argument");
+                throw new DukeException("No argument: Argument field cannot be empty");
             } else if (str.charAt(5) != ' ') {
                 return CommandType.OTHER;
             } else {
@@ -43,12 +43,12 @@ public class Command {
                         && str.charAt(indexOfAt + 3) == ' ') {
                     return CommandType.EVENT;
                 } else {
-                    throw new DukeException("Invalid argument");
+                    throw new DukeException("Invalid argument: '/at' is not detected");
                 }
             }
         } else if (strLength >= 4 && str.substring(0, 4).equalsIgnoreCase("todo")) {
             if (str.length() == 4 || (str.length() <= 5 && str.charAt(4) == ' ')) {
-                throw new DukeException("No argument");
+                throw new DukeException("No argument: Argument field cannot be empty");
             } else if (str.charAt(4) != ' ') {
                 return CommandType.OTHER;
             } else {
@@ -56,14 +56,14 @@ public class Command {
             }
         } else if (strLength >= 4 && str.substring(0, 4).equalsIgnoreCase("done")) {
             if (str.length() == 4 || (str.length() <= 5 && str.charAt(4) == ' ')) {
-                throw new DukeException("No argument");
+                throw new DukeException("No argument: Argument field cannot be empty");
             } else if (str.charAt(4) != ' ') {
                 return CommandType.OTHER;
             } else {
                 try {
                     Integer.parseInt(str.substring(5));
                 } catch (NumberFormatException e) {
-                    throw new DukeException("Invalid argument");
+                    throw new DukeException("Invalid argument: Argument must be an integer");
                 }
                 return CommandType.DONE;
             }
@@ -77,7 +77,7 @@ public class Command {
             case TODO -> {
                 String subStr = command.substring(5);
                 if (StringParser.isBlank(subStr)) {
-                    throw new DukeException("Void argument");
+                    throw new DukeException("Void argument: Content field is blank");
                 } else {
                     return new Todo(command.substring(5));
                 }
@@ -87,9 +87,9 @@ public class Command {
                 String subStrContent = command.substring(9, indexOfBy - 1);
                 String subStrTime = command.substring(indexOfBy + 4);
                 if (StringParser.isBlank(subStrContent)) {
-                    throw new DukeException("Void argument");
+                    throw new DukeException("Void argument: Content field is blank");
                 } else if (StringParser.isBlank(subStrTime)) {
-                    throw new DukeException("Void argument");
+                    throw new DukeException("Void argument: Time field is blank");
                 } else {
                     return new Deadline(subStrContent, subStrTime);
                 }
@@ -99,18 +99,16 @@ public class Command {
                 String subStrContent = command.substring(6, indexOfAt - 1);
                 String subStrTime = command.substring(indexOfAt + 4);
                 if (StringParser.isBlank(subStrContent)) {
-                    throw new DukeException("Void argument");
+                    throw new DukeException("Void argument: Content field is blank");
                 } else if (StringParser.isBlank(subStrTime)) {
-                    throw new DukeException("Void argument");
+                    throw new DukeException("Void argument: Time field is blank");
                 } else {
                     return new Event(subStrContent, subStrTime);
                 }
             }
             default -> {
-                throw new DukeException("Invalid command");
+                throw new DukeException("Invalid command: Unknown reason");
             }
         }
     }
-
-
 }
