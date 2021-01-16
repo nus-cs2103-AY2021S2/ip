132c44f88e54affaa7934e9a64766ff4813e01ed
yhtMinceraft1010X <tayyihsuen@gmail.com>
2021-01-16 20:52:43 +0800

    Level 2 changes: -Removed echo functionality of Meme Man. -Added functionality for Meme Man to note down tasks and display them when prompted. -Update README.md user guide to reflect the above changes.

--- docs/README.md
+++ docs/README.md
@@ -1,18 +1,24 @@
 # User Guide
 
 ## Introduction
-Introducing... a custom chatbot based on Meme Man and the Surreal Memes. 
-Meme Man is sure to entertain you every step of the way!
+Introducing... a custom chatbot based on Meme Man and the Surreal Memes.
+Not only will Meme Man entertain you every step of the way, Meme Man will also help you keep track of your tasks.
 
-## Features 
+## Features
 
-### Early Functionality
-At this point, Meme Man is just going to echo after you, yah? (NS reference here)
-Not to worry, if you say "bye" to Meme Man, he will EJECT you... er... I mean, leave.
+### Add tasks
+You can key in a task and Meme Man will record it down for you.
+
+### List tasks
+You can key in "list" and Meme Man will display the tasks in the order that you have keyed them in.
 
 ## Usage
 
 ### `bye` - Exit command
 
 Key this command in to cause Meme Man to EJECT you... er... I mean leave...
+
+### `list` - List command
+
+Key this command in to cause Meme Man to show you your tasks in order.
 
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,34 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static String[] taskArray = new String[100];
+    public static int taskArraySize = 0;
+
     public static void initialGreeting() {
         System.out.println("I am Meme Man. Whoms't be entering the VIMension?");
     }
 
     public static String inputCommand(Scanner sc) {
-        String command = sc.next();
+        String command = sc.nextLine();
         return command;
     }
 
-    public static void executeCommand(String command) {
-        System.out.println(command);
+    public static void addItem(String command) {
+        taskArray[taskArraySize] = command;
+        taskArraySize++;
+        System.out.println("Meme Man is now adding task: " + command);
+    }
+
+    public static void printList() {
+        if (Duke.taskArraySize == 0) {
+            System.out.println("I have nothing to print. Not stonks");
+        } else {
+            System.out.println("I print the tasks:");
+            for (int i = 1; i <= taskArraySize; i++) {
+                System.out.println(i + ". " + taskArray[i - 1]);
+            }
+            System.out.println("Hmmst've... Stonks");
+        }
     }
 
     public static void exitProgram() {
@@ -26,8 +43,10 @@ public class Duke {
             userCommand = inputCommand(sc);
             if (userCommand.equals("bye")) {
                 break;
+            } else if (userCommand.equals("list")){
+                Duke.printList();
             } else {
-                Duke.executeCommand(userCommand);
+                Duke.addItem(userCommand);
             }
         }
         sc.close();
