f918d40a5c9e55759a06536ba284789de10317e1
BennyPhoe <bennyphoe1998@gmail.com>
2021-01-16 19:20:31 +0800

    Level 1 Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String line = "";
+        for (int i = 0; i < 50; i++) {
+            line = line.concat("_");
+        }
+        System.out.println(line);
+        System.out.println("Hello! I'm Benny");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+
+        while(sc.hasNextLine()) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                indentInput("Hope to see you again!");
+                break;
+            }
+            indentInput(userInput);
+        }
+
+    }
+
+    public static void indentInput(String input) {
+        String line = "";
+        for (int i = 0; i < 50; i++) {
+            line = line.concat("_");
+        }
+        System.out.println(line);
+        System.out.println(input);
+        System.out.println(line);
     }
 }
