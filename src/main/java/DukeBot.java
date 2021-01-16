3c70f87f6b069bac4d3f8cc32f8ca7b24312ff33
Marcus Lee Eugene <marcus19787@gmail.com>
2021-01-16 20:55:10 +0800

    Clean switch case indentations

--- src/main/java/DukeBot.java
+++ src/main/java/DukeBot.java
@@ -15,24 +15,24 @@ public class DukeBot {
         boolean isExit = false;
 
         switch (command) {
-            case "welcome":
-                commandOutput += "Hello! I'm Duke\n"
-                        + "\t What can I do for you?";
-                break;
-            case "list":
-                commandOutput = getMemoryContents();
-                break;
-            case "blah":
-                commandOutput += "blah";
-                break;
-            case "bye":
-                commandOutput += "Bye. Hope to see you again soon!";
-                isExit = true;
-                break;
-            default:
-                commandOutput += "added: " + command;
-                memory.add(command);
-                break;
+        case "welcome":
+            commandOutput += "Hello! I'm Duke\n"
+                    + "\t What can I do for you?";
+            break;
+        case "list":
+            commandOutput = getMemoryContents();
+            break;
+        case "blah":
+            commandOutput += "blah";
+            break;
+        case "bye":
+            commandOutput += "Bye. Hope to see you again soon!";
+            isExit = true;
+            break;
+        default:
+            commandOutput += "added: " + command;
+            memory.add(command);
+            break;
         }
         commandOutput += "\n";
 
