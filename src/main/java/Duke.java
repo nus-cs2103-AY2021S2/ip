1d3e9e25323411c5b70e1a7d652b37cb25abe7bd
gycc7253 <guo7253@gmail.com>
2021-01-16 11:02:26 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+package main.java;
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +9,15 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("This is\n " + logo);
+        String greetings = "Dear user, welcome to the world of duke!";
+        System.out.println(greetings);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.toLowerCase().equals("bye")) {
+            System.out.println("    " + input);
+            input = sc.nextLine();
+        }
+        System.out.println("Bye. Until next time!");
     }
 }
