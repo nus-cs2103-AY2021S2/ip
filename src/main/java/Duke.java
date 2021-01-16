a1ef2f3130c036a2eabd3be02cbb7af14ab9b387
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-01-16 22:47:03 +0800

    Level 1 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
+    private static String LOGO =
+              " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private static String EXITCOMMAND = "bye";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        IO.printBotMessage("Hello from\n" + LOGO +"What can I do for you?");
+
+        while(true){
+            String reply = IO.readLine();
+            if(reply.equals(EXITCOMMAND)) {
+                break;
+            }
+            IO.printBotMessage(reply);
+        }
+        IO.printBotMessage("Bye. Hope to see you again soon!");
     }
 }
--- src/main/java/IO.java
+++ src/main/java/IO.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class IO {
-    private static String DIVIDER = "____________________________________________________________";
+    private static String DIVIDER = "____________________________________________________________\n";
     public static void printBotMessage(String message){
-        String printMessage = DIVIDER + message + DIVIDER;
-        print(message);
+        String printMessage = DIVIDER + message + "\n"+DIVIDER;
+        print(printMessage);
     }
     private static void print(String message) {
         System.out.println(message);
+    }
+    public static String readLine(){
+        Scanner scanf = new Scanner(System.in);
+        String input = scanf.nextLine();
+        return input;
     }
 }
