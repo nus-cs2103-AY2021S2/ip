5fb1035171d1baa4112ecc318291fb272998a684
Samuel Fang <samuelfangjw@gmail.com>
2021-01-16 18:17:39 +0800

    Implemented Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String LINE_BREAK = "------------------------------------------------------------";
+    private static String BOT_NAME = "Chip the Squirrel";
+
+    public static void printWithIndentation(String s) {
+        String indent = "    ";
+        System.out.println(indent + s);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printWithIndentation(LINE_BREAK);
+
+        printWithIndentation("Hello! I'm " + BOT_NAME + "!");
+        printWithIndentation("What can I do for you today?");
+        printWithIndentation(LINE_BREAK);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String command = sc.next();
+
+            switch(command) {
+                case "bye":
+                    printWithIndentation(LINE_BREAK);
+                    printWithIndentation("Bye! Hope to see you again soon!");
+                    printWithIndentation(LINE_BREAK);
+                    System.exit(0);
+                    break;
+                default:
+                    printWithIndentation(LINE_BREAK);
+                    printWithIndentation(command);
+                    printWithIndentation(LINE_BREAK);
+            }
+
+        }
     }
 }
