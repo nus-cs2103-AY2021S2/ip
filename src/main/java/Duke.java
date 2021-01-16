eeb397e7388e7fd445fbb01709df4763bb77dbfd
JonahhGohh <jonahgoh@outlook.com>
2021-01-16 16:24:27 +0800

    Add Greet, Echo and Exit messages.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Start up greeting message
+        String greetingMessage = "Hello! I'm a Chat bot and my name " +
+                "is Joe \nHow may I help you?";
+        System.out.println(messageFormatter(greetingMessage));
+
+        boolean isChatBotOnline = true;
+        while (isChatBotOnline) {
+            // Listen to input
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+
+            // Echoing the input
+            if (input.equals("bye")) {
+                String byeMessage = "Goodbye, hope you had a great time!";
+                System.out.println(messageFormatter(byeMessage));
+                isChatBotOnline = false;
+            } else {
+                System.out.println(messageFormatter(input));
+            }
+        }
+    }
+
+
+    public static String messageFormatter(String str) {
+        return "____________________________________________________________" +
+                "\n" + str + "\n" +
+                "____________________________________________________________\n";
     }
 }
