42b1b6b98df63456d218bb350a48e9b6c98dd621
Marcus Lee <marcus_lee@u.nus.edu>
2021-01-16 15:12:01 +0800

    Add basic functionality
    Duke class: add printWithBorders() method
    
    This method takes a string as input and prints it within borders before
    and after the string.
    
    Duke class: add while-loop within main() to provide basic functionality
    
    The while-loop receives inputs from the user and echos it back.
    If the string "bye" is given, the program terminates with a goodbye message.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String BORDER = "---------------";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke.printWithBorders("Hello! I'm Duke\nWhat can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                Duke.printWithBorders("Bye. Hope to see you soon!");
+                return;
+            } else {
+                Duke.printWithBorders(userInput);
+            }
+        }
+    }
+
+    public static void printWithBorders(String message) {
+        System.out.println(Duke.BORDER);
+        System.out.println(message);
+        System.out.println(Duke.BORDER);
     }
 }
