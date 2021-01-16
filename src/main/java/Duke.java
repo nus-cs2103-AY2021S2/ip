e9f1d064a406571d1a99b85af6fccc5146603968
rachel <rachel.ljt@gmail.com>
2021-01-16 16:00:52 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        welcomeMessage();
+        String input = sc.next();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = sc.next();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+        sc.close();
+    }
+
+    public static void welcomeMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
     }
 }
