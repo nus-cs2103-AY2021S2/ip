d54bd4c2ae0a9b53bcd191471b0ffdad2652054f
Md-Fazil <fazil@Mohameds-MacBook-Pro.local>
2021-01-16 21:03:34 +0800

    Level-1 Completed: Echo back commands feature added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,15 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "\n" + "What can I do for you? \n");
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        while(!command.equals("bye")){
+            System.out.println(command);
+            System.out.println("\n");
+            command = sc.nextLine();
+
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
