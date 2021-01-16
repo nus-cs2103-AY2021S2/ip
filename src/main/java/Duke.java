7947ae5f5217d028a8e37c730010261441f8151b
ssoonwee <38690268+ssoonwee@users.noreply.github.com>
2021-01-16 21:18:55 +0800

    Modified Duke for Level-1 task.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        performFirstTask();
+    }
+
+    public static void performFirstTask() {
+        String line = "---------------------------";
+        String greet = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(line);
+        System.out.println(greet);
+        System.out.println(line);
+        String cmd = "";
+        Scanner input = new Scanner(System.in);
+        while(!cmd.equals("bye")) {
+            cmd = input.next();
+            System.out.println(line);
+            System.out.println(cmd);
+            System.out.println(line);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
