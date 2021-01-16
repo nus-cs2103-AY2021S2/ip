9e3afa39d253cd1f364ef4cec3e0f1c33a2794b8
Jellybeano <lanyuxuan98@gmail.com>
2021-01-16 21:51:24 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,67 @@
+import java.util.*;
+import java.io.*;
+
 public class Duke {
+
+    static class FastIO extends PrintWriter
+    {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastIO()
+        {
+            super(new BufferedOutputStream(System.out));
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements())
+            {
+                try
+                {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException  e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +69,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duwuke\n What can I do for you? uwu");
+
+        FastIO fio = new FastIO();
+
+        while(true) {
+
+            String input = fio.nextLine();
+
+            switch (input) {
+                case "bye":
+                    System.out.println("Bye, hope to see you again! uwu");
+                    return;
+                default:
+                    System.out.println(input + " uwu");
+            }
+        }
+
     }
 }
