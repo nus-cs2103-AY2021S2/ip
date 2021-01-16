18c8d51e435abc98f49ef53bd70ba30e86a842fc
gycc7253 <guo7253@gmail.com>
2021-01-16 11:37:46 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
 package main.java;
 
+import main.java.Entity.Task;
+
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
@@ -14,7 +15,8 @@ public class Duke {
         System.out.println("This is\n " + logo);
         String greetings = "Dear user, welcome to the world of duke!";
         System.out.println(greetings);
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
+        Set<Integer> done = new HashSet<>();
         Scanner sc = new Scanner(System.in);
         String input;
         while (true) {
@@ -23,13 +25,25 @@ public class Duke {
                 break;
             }
             if (input.toLowerCase().equals("list")) {
+                if (list.size() == 0) {
+                    System.out.println("    Currently there is nothing on your list.");
+                    continue;
+                }
                 for (int i = 0; i < list.size(); i++) {
                     System.out.println("    " + (i + 1) + ". " + list.get(i));
                 }
                 continue;
             }
+            if (input.toLowerCase().split(" ")[0].equals("done")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                Task task = list.get(index);
+                task.markAsDone();
+                System.out.println("    Nice! I've mark this task as done:");
+                System.out.println("        " + task);
+                continue;
+            }
             System.out.println("    added: " + input);
-            list.add(input);
+            list.add(new Task(input));
         }
         System.out.println("Bye. Until next time!");
     }
---
+++ src/main/java/Entity/Task.java
@@ -0,0 +1,19 @@
+package main.java.Entity;
+
+public class Task {
+    protected String name;
+    protected boolean isDone;
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public boolean markAsDone() {
+        this.isDone = true;
+        return true;
+    }
+
+    public String toString() {
+        return "[" + (this.isDone ? "X" : " ") + "]" + this.name;
+    }
+}
