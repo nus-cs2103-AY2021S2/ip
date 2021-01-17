2a592e9edbac178869427f480272cf39502016fd
JayChenYJ <jaychenyj@gmail.com>
2021-01-17 12:17:13 +0800

    Level-3 completed in terms of functionality. Delay in done method has to be fixed. Javadoc comments need to be added/changed.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * The Duke project
  */
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         greet();
 
         Scanner sc = new Scanner(System.in);
@@ -13,20 +13,24 @@ public class Duke {
         nextGreet(username);
 
         boolean endOfCycle = false;
-        String[] list = new String[100];
+        Task[] tasks = new Task[100];
         int count = 0;
 
         while(!endOfCycle) {
             System.out.print(username + ": ");
             String nextCommand = sc.nextLine();
+            String[] commandToWords = nextCommand.split(" ");
             if (nextCommand.equals("bye")) {
                 bye(username);
                 endOfCycle = true;
-            } else if(nextCommand.equals("list")) {
-                list(list, count);
+            } else if (nextCommand.equals("list")) {
+                list(tasks, count);
+            } else if (commandToWords[0].equals("done")) {
+                int itemNum = Integer.parseInt(commandToWords[1]);
+                done(tasks, itemNum);
             }
             else {
-                add(nextCommand, list, count);
+                add(nextCommand, tasks, count);
                 count++;
             }
         }
@@ -58,24 +62,33 @@ public class Duke {
      * Add what the user typed as input by printing out what the user keyed in.
      * @param command The user input.
      */
-    public static void add(String command, String[] list, int count) {
+    public static void add(String command, Task[] tasks, int count) {
         System.out.println("----------------------------------------------------------------------------------------");
         System.out.println("added: " + command);
         System.out.println("----------------------------------------------------------------------------------------");
-        list[count] = command;
+        tasks[count] = new Task(command);
     }
 
     /**
      * List out all user inputs in sequence.
-     * @param list A list of user inputs in sequence, up to 100.
+     * @param tasks A list of user inputs in sequence, up to 100.
      * @param count The current number of user inputs stored inside the list.
      */
-    public static void list(String[] list, int count) {
+    public static void list(Task[] tasks, int count) {
         System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < count; i++){
             int listNum = i + 1;
-            System.out.println(listNum + ". " + list[i]);
+            System.out.println(listNum + ". " + tasks[i].display());
         }
+        System.out.println("----------------------------------------------------------------------------------------");
+    }
+
+    public static void done(Task[] tasks, int itemNum) throws InterruptedException {
+        tasks[itemNum - 1].isDone = true;
+        Thread.sleep(500);
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("Nice! I've marked this task as done:\n" + tasks[itemNum - 1].display());
         System.out.println("----------------------------------------------------------------------------------------");
     }
 
