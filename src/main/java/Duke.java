0337108ef5520eea30b4a21e75de2750330c9a68
Zen Lee Yijie <iarezenly@gmail.com>
2021-01-17 19:55:56 +0800

    Implement Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,23 +4,31 @@ import java.util.Scanner;
 
 public class Duke {
 
+    private static List<String> list;
+
     /**
-     * An application that reads user inputs and echoes back to them.
+     * An application that stores and displays user inputs.
      * Exit the program by entering "bye".
      */
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in).useDelimiter("\n");
+        list = new ArrayList<>();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("POWERED BY\n" + logo);
         greet();
         String userInput = sc.next();
         while (!userInput.equals("bye")) {
-            echo(userInput);
+            if (userInput.equals("list")) {
+                print(list);
+            } else {
+                addToList(userInput);
+            }
             userInput = sc.next();
         }
         exit();
@@ -31,16 +39,7 @@ public class Duke {
      */
 
     private static void greet() {
-        print("Hello! I'm Duke.\n\t  How can I help you?");
-    }
-
-    /**
-     * Echoes the user input.
-     * @param message the message to echo back to the user.
-     */
-
-    private static void echo(String message) {
-        print(message);
+        print("Hello! I'm Duke.\n\t  How may I help you?");
     }
 
     /**
@@ -52,12 +51,38 @@ public class Duke {
     }
 
     /**
-     * Formats the user input or welcome/exit message and print it.
-     * @param message the user input or welcome/exit message
+     * Adds user input to a list.
+     * @param userInput Text entered by the user.
      */
 
-    private static void print(String message) {
-        String horizLine = "\n\t____________________________________________________________\n";
-        System.out.println(horizLine + "\t  " + message + horizLine);
+    private static void addToList(String userInput) {
+        list.add(userInput);
+        print("added: " + userInput);
+    }
+
+    /**
+     * Prints message to user.
+     * @param message Welcome/Goodbye message or a message showing what the user has added to the list.
+     */
+
+    public static void print(String message) {
+        System.out.println("\n\t____________________________________________________________");
+        System.out.println("\n\t  " + message);
+        System.out.println("\n\t____________________________________________________________\n");
+    }
+
+    /**
+     * Prints all text entered by user.
+     * @param list A list of text entered by the user.
+     */
+
+    public static void print(List<String> list) {
+        System.out.println("\n\t____________________________________________________________\n");
+        int listCounter = 1;
+        for (String userInput : list) {
+            System.out.println("  \t  " + listCounter + ". " + userInput);
+            listCounter++;
+        }
+        System.out.println("\n\t____________________________________________________________\n");
     }
 }
