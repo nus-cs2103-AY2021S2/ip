abf7cb17bb29912827ce2cc028c3b9558ff17603
NBH99 <lametiger@hotmail.com>
2021-01-17 13:28:46 +0800

    Added A-TextUiTesting in test-ui-test

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -40,7 +40,7 @@ public class Duke {
                     k = Integer.parseInt(back) - 1;
                     tasks[k].markAsDone();
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(tasks[k]);
+                    System.out.println("  " + tasks[k]);
                 } else {
 
                     String task = "", time = "";
