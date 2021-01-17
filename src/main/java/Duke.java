965d3de469659303baf6b43f263e456597126ad8
pavitra <pavirajan02@gmail.com>
2021-01-17 21:37:28 +0800

    Finished level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,11 +8,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
+        String[] storage = new String[100];
+        int count = 0;
+
         System.out.println("     Heyyoo! I am Luna!\n     What can I echo for you today? :D\n" + logo);
         Scanner sc = new Scanner(System.in);
         String inp = sc.nextLine();
+
         while (!inp.equals("bye")) {
-            System.out.println("     " + inp);
+            if (inp.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    int next = i + 1;
+                    System.out.println("     " + next + ". " + storage[i]);
+                }
+            } else {
+                storage[count] = inp;
+                count++;
+                System.out.println("     Done adding: " + inp);
+            }
             inp = sc.nextLine();
         }
         System.out.println("     Byeee! It was wonderful to have you here!");
