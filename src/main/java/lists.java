bb221968b6557bdc37e0e39515dadb43e72a54c8
markmcwong@yahoo.com <markwong@Marks-iMac.local>
2021-01-17 16:51:43 +0800

    create custom listItem class to have ability to mark as done

--- src/main/java/lists.java
+++ src/main/java/lists.java
@@ -2,31 +2,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class lists {
-    List<String> dukeList;
+    List<listItem> dukeList;
+    class listItem{
+        private final boolean doneStatus;
+        private final String task;
+        public listItem(inputCommand command){
+            this.task = command.getCommand();
+            this.doneStatus = false;
+        }
+
+        public listItem(inputCommand command, boolean isDone){
+            this.task = command.getCommand();
+            this.doneStatus = isDone;
+        }
+
+        public listItem markAsDone(){
+            return new listItem(new inputCommand(this.task), true);
+        }
+    }
+
     public lists(){
         this.dukeList = new ArrayList<>();
     }
 
     public lists(inputCommand command){
-        this.dukeList = new ArrayList<String>();
-        dukeList.add(command.getCommand());
+        this.dukeList = new ArrayList<listItem>();
+        dukeList.add(new listItem(command));
     }
 
-    public lists(List<String> inputList){
-        this.dukeList = new ArrayList<>(inputList);
+    public lists(List<listItem> inputList){
+        this.dukeList = new ArrayList<listItem>(inputList);
     }
 
     public lists addCommand(inputCommand command){
-        List<String> tempList = new ArrayList<>(this.dukeList);
-        tempList.add(command.getCommand());
+        List<listItem> tempList = new ArrayList<>(this.dukeList);
+        tempList.add(new listItem(command));
         return new lists(tempList);
     }
 
     public void addCommandMutable(inputCommand command){
-        this.dukeList.add(command.getCommand());
+        this.dukeList.add(new listItem(command));
     }
 
-    public List<String> getDukeList(){
+    public List<listItem> getDukeList(){
         return this.dukeList;
     }
 }
