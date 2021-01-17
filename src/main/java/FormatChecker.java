611c63adb9d861277d9619fe08e4cf412e4ac244
Cheng20010201 <e0493630@u.nus.edu>
2021-01-17 22:05:07 +0800

    Update/Add helper class for user input checking and extracting

--- src/main/java/FormatChecker.java
+++ src/main/java/FormatChecker.java
@@ -10,7 +10,72 @@ public class FormatChecker {
      */
     public static boolean isDoneFormat(String userInput) {
         String[] splited = userInput.split(" ");
-        return splited[0].equals("done") && isInteger(splited[1]);
+        boolean isDone = splited[0].equals("done");
+        boolean hasInteger = isInteger((splited[1]));
+        return isDone && hasInteger;
+    }
+
+    /**
+     * Check whether the user input string is in the format of listing tasks
+     * @param userInput user input string
+     * @return true if the input string is in the format of getting task done
+     */
+    public static boolean isListFormat(String userInput) {
+        return userInput.equals("list");
+    }
+
+    /**
+     * Check whether the user input string is in the format of adding new ToDo task
+     * @param userInput user input string
+     * @return true if the input string is in the format of adding new ToDo task
+     */
+    public static boolean isToDoFormat(String userInput) {
+        String[] splited = userInput.split(" ");
+        boolean isToDo = splited[0].equals("todo");
+        boolean notSingle = splited.length > 1;
+        return isToDo && notSingle;
+    }
+
+    /**
+     * Check whether the user input string is in the format of adding new deadline task
+     * @param userInput user input string
+     * @return true if the input string is in the format of adding new deadline task
+     */
+    public static boolean isDeadLineFormat(String userInput) {
+        String[] splited = userInput.split(" ");
+        boolean isDeadline = splited[0].equals("deadline");
+        if (!isDeadline) {
+            return false;
+        }
+
+        userInput = InputInformationExtractor.getDeadlineDetail(userInput);
+        splited = userInput.split(" /by ");
+        if (splited.length != 2) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check whether the user input string is in the format of adding new event task
+     * @param userInput user input string
+     * @return true if the input string is in the format of adding new event task
+     */
+    public static boolean isEventFormat(String userInput) {
+        String[] splited = userInput.split(" ");
+        boolean isEvent = splited[0].equals("event");
+        if (!isEvent) {
+            return false;
+        }
+
+        userInput = InputInformationExtractor.getEventDetail(userInput);
+        splited = userInput.split(" /at ");
+        if (splited.length != 2) {
+            return false;
+        }
+
+        return true;
     }
 
     private static boolean isInteger(String toCheck) {
---
+++ src/main/java/InputInformationExtractor.java
@@ -0,0 +1,68 @@
+/**
+ * Provide methods for extracting information
+ * from user input string
+ */
+public class InputInformationExtractor {
+    /**
+     * Get the content of the todo item from user input
+     * @param userInput User input is assumed to be in todo format
+     * @return Content of the todo item
+     */
+    public static String getToDoContent(String userInput) {
+        return userInput.substring(5);
+    }
+
+    /**
+     * Get the detail of the deadline item from user input
+     * @param userInput User input is assumed to be in deadline format
+     * @return Detail of the deadline item
+     */
+    public static String getDeadlineDetail(String userInput) {
+        return userInput.substring(9);
+    }
+
+    /**
+     * Get the content of the deadline item from user input
+     * @param userInput User input is assumed to be in deadline format
+     * @return Content of the deadline item
+     */
+    public static String getDeadlineContent(String userInput) {
+        return userInput.split(" /by ")[0].substring(9);
+    }
+
+    /**
+     * Get the time of the deadline item from user input
+     * @param userInput User input is assumed to be in deadline format
+     * @return Time of the deadline item
+     */
+    public static String getDeadlineTime(String userInput) {
+        return userInput.split(" /by ")[1];
+    }
+
+    /**
+     * Get the detail of the event item from user input
+     * @param userInput User input is assumed to be in event format
+     * @return detail of the event item
+     */
+    public static String getEventDetail(String userInput) {
+        return userInput.substring(6);
+    }
+
+    /**
+     * Get the content of the deadline item from user input
+     * @param userInput User input is assumed to be in deadline format
+     * @return Content of the deadline item
+     */
+    public static String getEventContent(String userInput) {
+        return userInput.split(" /at ")[0].substring(6);
+    }
+
+    /**
+     * Get the time of the deadline item from user input
+     * @param userInput User input is assumed to be in deadline format
+     * @return Time of the deadline item
+     */
+    public static String getEventTime(String userInput) {
+        return userInput.split(" /at ")[1];
+    }
+}
