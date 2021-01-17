551e4af566a203770dd7ba6a65b9e8e6790f66b9
natosy <natosyy@gmail.com>
2021-01-17 12:49:20 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Duke {
+
+    static void display(String str) {
+        String[] strings = str.split("\n");
+        ArrayList<String> displayedStrings = new ArrayList<>();
+        int longest = 0;
+
+        for (String s : strings) {
+            if (s.length() > longest) longest = s.length();
+            displayedStrings.add("    | " + s);
+        }
+
+        String border = "     " + "-".repeat(longest + 2);
+        System.out.println(border);
+
+        for (String s : displayedStrings) {
+            String rightSpace = " ".repeat(longest - s.length() + 7);
+            System.out.println(s + rightSpace + "|");
+        }
+        System.out.println(border);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        display("Hello! I'm Duke\nWhat can I do for you?");
+        while (sc.hasNextLine()) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                display("Bye. Hope to see you again soon!");
+                return;
+            } else {
+                display(command);
+            }
+        }
     }
 }
