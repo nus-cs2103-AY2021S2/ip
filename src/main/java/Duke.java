7151aa32f29ed62a8c0850b0744d9a7f25b3839b
Zen Lee Yijie <iarezenly@gmail.com>
2021-01-17 22:18:59 +0800

    Implement Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private static List<String> list;
+    private static List<Task> list;
 
     /**
-     * An application that stores and displays user inputs.
+     * An application that serves as a to-do list.
      * Exit the program by entering "bye".
      */
 
@@ -23,19 +23,29 @@ public class Duke {
         System.out.println("POWERED BY\n" + logo);
         greet();
         String userInput = sc.next();
+        String[] parseInput = userInput.split(" ");
+        String command = parseInput[0];
         while (!userInput.equals("bye")) {
-            if (userInput.equals("list")) {
-                print(list);
-            } else {
-                addToList(userInput);
+            switch (command) {
+                case "list":
+                    print(list);
+                    break;
+                case "done":
+                    int taskIndex = Integer.parseInt(parseInput[1])-1;
+                    markTaskAsDone(list.get(taskIndex));
+                    break;
+                default:
+                    addTask(new Task(userInput));
             }
             userInput = sc.next();
+            parseInput = userInput.split(" ");
+            command = parseInput[0];
         }
         exit();
     }
 
     /**
-     * Greets the user when he/she starts the application.
+     * Greet the user when he/she starts the application.
      */
 
     private static void greet() {
@@ -43,7 +53,7 @@ public class Duke {
     }
 
     /**
-     * Says goodbye to the user when he/she exits the application.
+     * Say goodbye to the user when he/she exits the application.
      */
 
     private static void exit() {
@@ -51,36 +61,49 @@ public class Duke {
     }
 
     /**
-     * Adds user input to a list.
-     * @param userInput Text entered by the user.
+     * Add task to the list.
+     * @param task Task entered by the user.
      */
 
-    private static void addToList(String userInput) {
-        list.add(userInput);
-        print("added: " + userInput);
+    private static void addTask(Task task) {
+        list.add(task);
+        print("added: " + task);
     }
 
     /**
-     * Prints message to user.
-     * @param message Welcome/Goodbye message or a message showing what the user has added to the list.
+     * Mark a task as completed.
+     * @param task Task entered by the user.
+     */
+
+    private static void markTaskAsDone(Task task) {
+        task.markDone();
+        print("Nice! I have marked this task as done:\n\t\t "
+                + "[" + task.getStatusIcon() + "] " + task);
+    }
+
+    /**
+     * Print message to user.
+     * @param message Welcome/Goodbye message or a description of the task added.
      */
 
     public static void print(String message) {
-        System.out.println("\n\t____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
         System.out.println("\n\t  " + message);
         System.out.println("\n\t____________________________________________________________\n");
     }
 
     /**
-     * Prints all text entered by user.
-     * @param list A list of text entered by the user.
+     * Print all tasks.
+     * @param list A list of tasks entered by the user.
      */
 
-    public static void print(List<String> list) {
-        System.out.println("\n\t____________________________________________________________\n");
+    public static void print(List<Task> list) {
+        System.out.println("\t____________________________________________________________\n");
+        System.out.println("\t  Your tasks:");
         int listCounter = 1;
-        for (String userInput : list) {
-            System.out.println("  \t  " + listCounter + ". " + userInput);
+        for (Task task : list) {
+            System.out.println("  \t  " + listCounter +
+                    ".[" + task.getStatusIcon() + "] " + task);
             listCounter++;
         }
         System.out.println("\n\t____________________________________________________________\n");
---
+++ src/main/java/Task.java
@@ -0,0 +1,36 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Constructor of the Task class.
+     * @param description A brief description of the task.
+     */
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Display whether a task is completed.
+     * @return A cross symbol if task is incomplete, a tick symbol if task is completed.
+     */
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    /**
+     * Update the task as completed.
+     */
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
+}
