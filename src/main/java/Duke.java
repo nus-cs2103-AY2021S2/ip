b5dcf2713c095957400c603304d0826ddfa1f8b8
Nanxi-Huang <e0426038@u.nus.edu>
2021-01-17 22:19:22 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,48 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    private static ArrayList<String> taskList = new ArrayList<>();
+
+    public static void getTasks() {
+        System.out.println("____________________________________________________________\n");
+
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.println(i+1 + ". " + taskList.get(i));
+        }
+
+        System.out.println("____________________________________________________________\n");
+
+        storeTask();
+    }
 
     public static void Greet() {
         String greeting = "____________________________________________________________\n"
-                + "Hello! I'm Duke, your favourite penguin.\n"
+                + "Hello! I'm Duke, 恭喜发财.\n"
                 + "What can I do for you?\n"
                 + "____________________________________________________________\n";
         System.out.println(greeting);
 
     }
 
-    public static void Echo() {
+    public static void storeTask() {
         Scanner sc = new Scanner(System.in);
-        String command = sc.nextLine();
+        String task = sc.nextLine();
+        String listCommand = "list";
         String exitCommand = "bye";
-        if (!(command.toLowerCase()).equals(exitCommand)) {
-            System.out.println("____________________________________________________________\n"
-                    + command
-                    + "\n____________________________________________________________\n");
-            Echo();
-        } else {
+
+        if (task.equals(listCommand)) {
+            getTasks();
+
+        } else if ((task.toLowerCase()).equals(exitCommand)) {
             Exit();
+
+        } else{
+            taskList.add(task);
+            System.out.println("____________________________________________________________\n"
+                    + "added: " + task
+                    + "\n____________________________________________________________\n");
+            storeTask();
         }
     }
 
@@ -35,7 +56,6 @@ public class Duke {
 
         }
 
-
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -46,6 +66,6 @@ public class Duke {
 
         Greet();
 
-        Echo();
+        storeTask();
     }
 }
