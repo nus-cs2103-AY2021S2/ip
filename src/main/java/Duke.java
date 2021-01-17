c644425750addcbc0d880f669a1193c38f53ab01
noelmathewisaac <noelmathewisaac@gmail.com>
2021-01-17 22:13:44 +0800

    Implemented Greet, Echo, Exit features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,20 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("**************************\nHey, I am Duke.\nHow can I help you?\n" +
+                "**************************");
+
+        Scanner sc= new Scanner(System.in);
+        String command = sc.nextLine();
+
+        while(!command.equals("bye")){
+            System.out.println("   >>> " + command);
+            command = sc.nextLine();
+        }
+
+        System.out.println("**************************\n" +
+                       "Goodbye and see you soon!\n" +
+                       "**************************");
+
     }
 }
