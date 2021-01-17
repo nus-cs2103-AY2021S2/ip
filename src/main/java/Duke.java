1bb6256baf7bdeb130ed642c169a5166168d6b13
CSmortal <jovenpua51@gmail.com>
2021-01-17 21:29:52 +0800

    Level 1 1st change

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
+        String indent = "         ";
+        String horizSep = indent + "________________________________________________\n";
+
+        String greeting = indent + " Hello! I'm Duke\n" + indent + " What can I do for you?\n";
+        String farewell = indent + " Bye. Hope to see you again soon!\n";
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(horizSep + greeting + horizSep);
+
+        while (sc.hasNextLine()) {
+            String next = sc.nextLine();
+
+            if (next.equals("bye")) {
+
+                System.out.println(horizSep + farewell + horizSep);
+                sc.close();
+                return;
+            } else {
+                System.out.println(horizSep + indent + " " + next + "\n" + horizSep);
+            }
+        }
+
     }
 }
