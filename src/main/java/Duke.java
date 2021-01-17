2e08e744f4abadd7ea7913341238f78990ed31b2
vvan-essa <vtsy2000@yahoo.com.sg>
2021-01-17 11:01:48 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -6,6 +7,7 @@ import java.util.Scanner;
  * @author Damith C. Rajapakse, Jeffry Lum, Vanessa Tay
  */
 public class Duke {
+    public static ArrayList<String> userCommands = new ArrayList<>();
 
     /**
      * Greets the user and begins listening to user commands
@@ -26,7 +28,7 @@ public class Duke {
     }
 
     /**
-     * Takes in and echoes user's command line arguments until
+     * Takes in user's command line arguments and treats them accordingly
      * user inputs "bye", then Chatbot will end the session
      */
     public static void listenToUserCommand() {
@@ -36,17 +38,47 @@ public class Duke {
             if (input.equals("bye")) {
                 stopListeningToCommand();
                 break;
+            } else if (input.equals("list")) {
+                displayUserCommands();
             } else {
+                addUserCommand(input);
                 echoUserCommand(input);
             }
         }
     }
 
+    /**
+     * Chatbot prints out a farewell message before ending the session
+     */
     public static void stopListeningToCommand() {
         System.out.println("Bye. Hope to see you soon!\n");
     }
 
+    /**
+     * Chatbot prints out what the user has just fed in as command-line input
+     * @param input command-line input
+     */
     public static void echoUserCommand(String input) {
         System.out.println(input + "\n");
+    }
+
+    /**
+     * Chatbot adds the user's command-line input into an arraylist
+     * @param input command-line input
+     */
+    public static void addUserCommand(String input) {
+        userCommands.add(input);
+        System.out.print("added: ");
+    }
+
+    /**
+     * Chatbot prints out line-by-line all of the user's command-line
+     * inputs in that given session
+     */
+    public static void displayUserCommands() {
+        for(int i = 1; i <= userCommands.size(); i++) {
+            System.out.println(i + ". " + userCommands.get(i-1));
+        }
+        System.out.println();
     }
 }
