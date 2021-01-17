ca0265f1e3c6554023f4f9ba3b898f9f9dd0e628
mabel-kang <e0425446@u.nus.edu>
2021-01-17 23:04:31 +0800

    Completed Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -13,55 +14,73 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         List<Task> store = new ArrayList<Task>();
         Scanner scan = new Scanner(System.in);
+        Validation validate = new Validation();
         while(true) {
             String command = scan.nextLine();
-            int index = command.indexOf(' ');
-            int findSlash = command.indexOf('/');
-            if (index > -1) {
-                String type = command.substring(0, index);
-                switch (type) {
-                    case "todo":
-                        String descriptionTask = command.substring(index+1);
-                        ToDo newToDo = new ToDo(descriptionTask);
-                        store.add(newToDo);
-                        System.out.println("Got it. I've added this task:\n" + newToDo.toString() + "\nNow you have "
-                        + store.size() + " tasks in the list");
-                        break;
-                    case "deadline":
-                        String descriptionDeadline = command.substring(index+1, findSlash-1);
-                        String date = command.substring(findSlash+4);
-                        Deadline newDeadline = new Deadline(descriptionDeadline, date);
-                        store.add(newDeadline);
-                        System.out.println("Got it. I've added this task:\n" + newDeadline.toString() +
-                                "\nNow you have " + store.size() + " tasks in the list");
-                        break;
-                    case "event":
-                        String descriptionEvent = command.substring(index+1, findSlash-1);
-                        String time = command.substring(findSlash+4);
-                        Event newEvent = new Event(descriptionEvent, time);
-                        store.add(newEvent);
-                        System.out.println("Got it. I've added this task:\n" + newEvent.toString() +
-                                "\nNow you have " + store.size() + " tasks in the list");
-                        break;
-                    case "done":
-                        int number = Integer.parseInt(command.substring(index+1));
-                        Task toMark = store.get(number - 1);
-                        String response = toMark.markAsDone();
-                        System.out.println(response);
-                        break;
+            try {
+                validate.checkValidCommand(command);
+                int index = command.indexOf(' ');
+                int findSlash = command.indexOf('/');
+                if (index > -1) {
+                    String type = command.substring(0, index);
+                    switch (type) {
+                        case "todo":
+                            String descriptionTask = command.substring(index+1);
+                            ToDo newToDo = new ToDo(descriptionTask);
+                            store.add(newToDo);
+                            System.out.println("Got it. I've added this task:\n" + newToDo.toString() + "\nNow you have "
+                                        + store.size() + " tasks in the list");
+                            break;
+                        case "deadline":
+                            try {
+                                validate.checkForSchedule(command, findSlash);
+                                String descriptionDeadline = command.substring(index+1, findSlash-1);
+                                String date = command.substring(findSlash+4);
+                                Deadline newDeadline = new Deadline(descriptionDeadline, date);
+                                store.add(newDeadline);
+                                System.out.println("Got it. I've added this task:\n" + newDeadline.toString() +
+                                        "\nNow you have " + store.size() + " tasks in the list");
+                                break;
+                            } catch (DukeException e) {
+                                System.out.println(e);
+                                break;
+                            }
+                        case "event":
+                            try {
+                                validate.checkForSchedule(command, findSlash);
+                                String descriptionEvent = command.substring(index + 1, findSlash - 1);
+                                String time = command.substring(findSlash + 4);
+                                Event newEvent = new Event(descriptionEvent, time);
+                                store.add(newEvent);
+                                System.out.println("Got it. I've added this task:\n" + newEvent.toString() +
+                                        "\nNow you have " + store.size() + " tasks in the list");
+                                break;
+                            } catch (DukeException e) {
+                                System.out.println(e);
+                                break;
+                            }
+                        case "done":
+                            int number = Integer.parseInt(command.substring(index+1));
+                            Task toMark = store.get(number - 1);
+                            String response = toMark.markAsDone();
+                            System.out.println(response);
+                            break;
+                    }
+                } else {
+                    switch (command) {
+                        case "bye":
+                            System.out.println("Bye. Hope to see you again soon!");
+                            break;
+                        case "list":
+                            System.out.println("Here are the tasks in your list:");
+                            for(int i =0; i < store.size(); i++) {
+                                System.out.println((i + 1) + "." + store.get(i).toString());
+                            }
+                            break;
+                    }
                 }
-            } else {
-                switch (command) {
-                    case "bye":
-                        System.out.println("Bye. Hope to see you again soon!");
-                        break;
-                    case "list":
-                        System.out.println("Here are the tasks in your list:");
-                        for(int i =0; i < store.size(); i++) {
-                            System.out.println((i + 1) + "." + store.get(i).toString());
-                        }
-                        break;
-                }
+            } catch (DukeException e){
+                System.out.println(e);
             }
             if (command.equals("bye")) {
                 break;
---
+++ src/main/java/Validation.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Validation {
+
+    public void checkValidCommand(String command) throws DukeException {
+        String[] validCommands = new String[]{"todo", "deadline", "event", "list", "bye", "done"};
+        List<String> list = Arrays.asList(validCommands);
+        int index = command.indexOf(' ');
+        String first;
+        if (index > -1) {
+            first = command.substring(0, index);
+        } else {
+            first = command;
+        }
+        if(!list.contains(first.toLowerCase())){
+            throw new DukeException(":( OOPS! I'm sorry, but I don't know what that means :-(");
+        } else {
+            String[] secondValidation = new String[]{"todo", "deadline", "event", "done"};
+            List<String> secondList = Arrays.asList(secondValidation);
+            if (secondList.contains(first.toLowerCase())) {
+                if(index <= -1 || command.substring(index).isBlank()) {
+                    throw new DukeException(":( OOPS! The description of a todo/deadline/event/done cannot be empty!!");
+                }
+            }
+        }
+    }
+
+    public void checkForSchedule(String command, int findSlash) throws DukeException {
+        if ((findSlash <= -1) || command.endsWith("/") || command.substring(findSlash + 1).isBlank()) {
+            throw new DukeException(":( OOPS! Please input a valid time/date");
+        }
+    }
+}
