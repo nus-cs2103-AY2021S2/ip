9c206954c8a581f01d0494c4fa7ce4682bbc3529
Marcus Ong Jing Keat <markuz5116@gmail.com>
2021-01-17 13:30:12 +0800

    Added DukeUnknownArgumentsException class for unknown commands

---
+++ src/main/java/DukeUnknownArgumentsException.java
@@ -0,0 +1,6 @@
+public class DukeUnknownArgumentsException extends DukeException {
+
+    public String toString() {
+        return "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+    }
+}
