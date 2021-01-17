1ab0b78474d270a11e7c555d925721bd75088f56
Marcus Lee Eugene <marcus19787@gmail.com>
2021-01-18 00:55:43 +0800

    Separated out chunks of code from handleCommand function

--- src/main/java/DukeBot.java
+++ src/main/java/DukeBot.java
@@ -4,36 +4,37 @@ import java.util.List;
 public class DukeBot {
     private List<Task> taskList;
     private boolean isExit;
+    private String commandOutput;
 
     public DukeBot() {
         taskList = new ArrayList<>();
         taskList.add(null);
         isExit = false;
-        respondToCommand("Hello! I'm Duke\n"
-                + "\tWhat can I do for you?");
+        commandOutput = "Hello! I'm Duke\n"
+                + "\tWhat can I do for you?";
+        respondToCommand(commandOutput);
     }
 
     public void handleCommand(String text) throws DukeException {
+        commandOutput = "";
         String[] commandLine = text.split(" "); //entire line of command in String array
         String command = commandLine[0];
-        String commandOutput, taskName, date;
-        int taskNum;
-        Task task;
+        String taskInfo = text.replaceFirst(command + " ", "");
 
         if (command.equals("todo") || command.equals("event") || command.equals("deadline")) {
-            String errMsg = "";
-            if (commandLine.length < 2) {
+            String errMsg;
+            if (taskInfo.equals(command)) {
                 errMsg = "☹ OOPS!!! The description of a " + command + " cannot be empty.";
                 throw new DukeException(errMsg);
             }
         }
 
         if (command.equals("done") || command.equals("delete")) {
-            String errMsg = "";
-            if (commandLine.length < 2) {
+            String errMsg;
+            if (taskInfo.equals(command)) {
                 errMsg = "☹ OOPS!!! The selection for " + command + " cannot be empty.";
                 throw new DukeException(errMsg);
-            } else if (!isNumeric(text.split(" ")[1])) {
+            } else if (!isNumeric(taskInfo)) {
                 errMsg = "☹ OOPS!!! The selection for " + command + " should be a valid Integer.";
                 throw new DukeException(errMsg);
             }
@@ -41,54 +42,81 @@ public class DukeBot {
 
         switch (command) {
             case "list":
-                commandOutput = getTaskListContents();
+                listProcess();
                 break;
             case "bye":
-                isExit = true;
-                commandOutput = "Bye. Hope to see you again soon!";
+                byeProcess();
                 break;
             case "done":
-                taskNum = Integer.parseInt(text.split(" ")[1]);
-                task = taskList.get(taskNum);
-                task.markAsDone();
-                commandOutput = "Nice! I've marked this task as done:\n"
-                        + "\t" + task.toString();
+                doneProcess(taskInfo);
                 break;
             case "delete":
-                taskNum = Integer.parseInt(text.split(" ")[1]);
-                task = taskList.get(taskNum);
-                taskList.remove(task);
-                commandOutput = "Noted. I've removed this task: \n\t\t"
-                        + task.toString() + getRemainingTasks();
+                deleteProcess(taskInfo);
                 break;
             case "event":
-                taskName = text.split(" /at")[0].replaceFirst("event ", "");
-                date = text.split(" /at ")[1];
-                task = new Event(taskName, date);
-                taskList.add(task);
-                commandOutput = "Got it. I've added this task: \n\t\t"
-                        + task.toString() + getRemainingTasks();
+                eventProcess(taskInfo);
                 break;
             case "deadline":
-                taskName = text.split(" /by")[0].replaceFirst("deadline ", "");
-                date = text.split(" /by ")[1];
-                task = new Deadline(taskName, date);
-                taskList.add(task);
-                commandOutput = "Got it. I've added this task: \n\t\t"
-                        + task.toString() + getRemainingTasks();
+                deadlineProcess(taskInfo);
                 break;
             case "todo":
-                taskName = text.split("todo ")[1];
-                task = new ToDo(taskName);
-                taskList.add(task);
-                commandOutput = "Got it. I've added this task: \n\t\t"
-                        + task.toString() + getRemainingTasks();
+                todoProcess(taskInfo);
                 break;
             default:
                 throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
 
         respondToCommand(commandOutput);
+    }
+
+    private void listProcess() {
+        commandOutput = getTaskListContents();
+    }
+
+    private void byeProcess() {
+        isExit = true;
+        commandOutput = "Bye. Hope to see you again soon!";
+    }
+
+    private void doneProcess(String selection) {
+        int taskNum = Integer.parseInt(selection);
+        Task task = taskList.get(taskNum);
+        task.markAsDone();
+        commandOutput = "Nice! I've marked this task as done:\n\t  "
+                + task.toString();
+    }
+
+    private void deleteProcess(String selection) {
+        int taskNum = Integer.parseInt(selection);
+        Task task = taskList.get(taskNum);
+        taskList.remove(task);
+        commandOutput = "Noted. I've removed this task: \n\t  "
+                + task.toString() + getRemainingTasks();
+    }
+
+    private void eventProcess(String taskInfo) {
+        String taskName = taskInfo.split(" /at")[0].replaceFirst("event ", "");
+        String date = taskInfo.split(" /at ")[1];
+        Task task = new Event(taskName, date);
+        taskList.add(task);
+        commandOutput = "Got it. I've added this task: \n\t  "
+                + task.toString() + getRemainingTasks();
+    }
+
+    private void deadlineProcess(String taskInfo) {
+        String taskName = taskInfo.split(" /by")[0].replaceFirst("deadline ", "");
+        String date = taskInfo.split(" /by ")[1];
+        Task task = new Deadline(taskName, date);
+        taskList.add(task);
+        commandOutput = "Got it. I've added this task: \n\t  "
+                + task.toString() + getRemainingTasks();
+    }
+
+    private void todoProcess(String taskName) {
+        Task task = new ToDo(taskName);
+        taskList.add(task);
+        commandOutput = "Got it. I've added this task: \n\t  "
+                + task.toString() + getRemainingTasks();
     }
 
     private String getTaskListContents() {
@@ -106,9 +134,9 @@ public class DukeBot {
         return "\n\tNow you have " + (taskList.size() - 1) + " tasks in the list.";
     }
 
-    public void respondToCommand(String commandOutput) {
+    public void respondToCommand(String selectedOutput) {
         String responseMsg = "\t____________________________________________________________\n"
-                + "\t" + commandOutput + "\n"
+                + "\t" + selectedOutput + "\n"
                 + "\t____________________________________________________________\n";
 
         System.out.println(responseMsg);
