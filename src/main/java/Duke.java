a5961211bb002048b7d8e1a21f53779f69adf367
jamesleeht <jameshtlee@live.com.sg>
2021-01-18 02:43:22 +0800

    Setup Duke and implemented level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,53 @@
+import java.util.Scanner;
+
 public class Duke {
+    
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.start();
+    }
+    
+    // Constants
+    private static final String line = "____________________________________________________________";
+
+    // Attributes
+    private Scanner scanner;
+    private boolean isActive;
+
+    public Duke() {
+        this.scanner = new Scanner(System.in);
+        this.isActive = true;
+    }
+
+    public void start() {
+        greet();
+        while(isActive) {
+            listen();
+        }
+    }
+    
+    private void greet() {
+        String greeting = "Hello! I'm Duke" + "\n"
+        + "What can I do for you?";
+        print(greeting);
+    }
+
+    private void listen() {
+        String input = scanner.nextLine();
+        switch(input) {
+            case "bye":
+                isActive = false;
+                String output = "Bye. Hope to see you again soon!";
+                print(output);
+                break;
+            default:
+                print(input);
+                break;
+        }
+    }
+
+    private void print(String input) {
+        System.out.println(line + "\n" + input + "\n" + line + "\n");
     }
 }
+
