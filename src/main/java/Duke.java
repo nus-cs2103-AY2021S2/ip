f870809f9fa1dbf7143088c6ef4ad04f8ccd74f4
Vinleon <kohvinleon@gmail.com>
2021-01-17 22:05:46 +0800

    Duke class: add deadline, event and todo methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,29 +15,11 @@ public class Duke {
         printHorizontalLine();
     }
 
-    private static void addCommand(String command) {
-        Task newTask = new Task(command);
-        System.out.printf("added: %s%n", command);
-        taskList.add(newTask);
-    }
-
     private static void printCommand() {
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < taskList.size(); i++) {
             System.out.printf("%d.%s%n", i + 1, taskList.get(i).toString());
         }
-    }
-
-    private static void echoCommand(String command) {
-        printHorizontalLine();
-        if (command.equals("list")) {
-            printCommand();
-        } else if (command.contains("done")) {
-            doneCommand(command);
-        } else {
-            addCommand(command);
-        }
-        printHorizontalLine();
     }
 
     private static void doneCommand(String command) {
@@ -47,6 +29,56 @@ public class Duke {
         Task selectedTask = taskList.get(itemPosition);
         selectedTask.setDone();
         System.out.println("   " + selectedTask.toString());
+    }
+
+    private static void todoCommand(String command) {
+        System.out.println("Got it. I've added this task: ");
+        ToDo newToDo = new ToDo(command.substring(5));
+        taskList.add(newToDo);
+
+        System.out.println("   " + newToDo.toString());
+        printSummary();
+    }
+
+    private static void deadlineCommand(String command) {
+        System.out.println("Got it. I've added this task: ");
+        int slashPosition = command.indexOf("/");
+        Deadline newDeadline = new Deadline(command.substring(9, slashPosition - 1), command.substring(slashPosition + 4));
+        taskList.add(newDeadline);
+
+        System.out.println("   " + newDeadline.toString());
+        printSummary();
+    }
+
+    private static void eventCommand(String command) {
+        System.out.println("Got it. I've added this task: ");
+
+        int slashPosition = command.indexOf("/");
+        Event newEvent = new Event(command.substring(6, slashPosition - 1), command.substring(slashPosition + 4));
+        taskList.add(newEvent);
+
+        System.out.println("   " + newEvent.toString());
+        printSummary();
+    }
+
+    private static void printSummary() {
+        System.out.printf("Now you have %d tasks in the list.%n", taskList.size());
+    }
+
+    private static void echoCommand(String command) {
+        printHorizontalLine();
+        if (command.equals("list")) {
+            printCommand();
+        } else if (command.contains("done")) {
+            doneCommand(command);
+        } else if (command.contains("todo")) {
+            todoCommand(command);
+        } else if (command.contains("deadline")) {
+            deadlineCommand(command);
+        } else if (command.contains("event")) {
+            eventCommand(command);
+        }
+        printHorizontalLine();
     }
 
     private static void exitCommand() {
