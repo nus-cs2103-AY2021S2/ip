8860a6860cd44836cc43491468fab3d8ff523145
Vinleon <kohvinleon@gmail.com>
2021-01-17 19:35:58 +0800

    Duke class: add echo, greeting and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static void printHorizontalLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void greetUser() {
+        printHorizontalLine();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        printHorizontalLine();
+    }
+
+    private static void echoCommand(String command) {
+        printHorizontalLine();
+        System.out.println(command);
+        printHorizontalLine();
+    }
+
+    private static void exitCommand() {
+        printHorizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLine();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greetUser();
+        Scanner sc = new Scanner(System.in);
+        String command;
+        while (!(command = sc.nextLine()).equals("bye")) {
+            echoCommand(command);
+        }
+        exitCommand();
+        sc.close();
     }
 }
