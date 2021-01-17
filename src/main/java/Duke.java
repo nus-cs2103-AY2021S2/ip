4df29486fa896c324511581c6e1c6c2a89cff24c
wongkokian <ianwong97@hotmail.sg>
2021-01-18 05:02:08 +0800

    Added add and list functions.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
 public class Duke {
+    public static ArrayList<String> data = new ArrayList<>();
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,22 +17,26 @@ public class Duke {
                 + "     What can I do for you?\n"
                 + "    ____________________________________________________________\n";
         System.out.println(greet);
-
-        Scanner sc = new Scanner(System.in);
-        echo(sc);
+        run(sc.nextLine());
         sc.close();
     }
 
-    public static void echo(Scanner sc) {
-        String cmd = sc.next();
+    public static void run(String cmd) {
         System.out.println("    ____________________________________________________________");
-        if(!cmd.equals("bye")) {
-            System.out.println("     " + cmd + "\n"
-                    + "    ____________________________________________________________\n");
-            echo(sc);
-        } else {
+        if(cmd.equals("bye")) {
             System.out.println("     Bye. Hope to see you again soon!" + "\n"
                     + "    ____________________________________________________________\n");
+        } else if (cmd.equals("list")) {
+            for(int i = 0; i < data.size(); i++) {
+                System.out.println("     " + (i + 1) + ". " + data.get(i));
+            }
+            System.out.println("    ____________________________________________________________\n");
+            run(sc.nextLine());
+        } else {
+            System.out.println("     added: " + cmd + "\n"
+                    + "    ____________________________________________________________\n");
+            data.add(cmd);
+            run(sc.nextLine());
         }
     }
 }
