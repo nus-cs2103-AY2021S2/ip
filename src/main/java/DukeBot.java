ce2e222d22c8d520481cea11431495fa0de6c323
kaixiangtay <taykaixiang97@gmail.com>
2021-01-17 22:29:32 +0800

    Created Duke Project Level 3 with mark as done feature and added Task class

--- src/main/java/DukeBot.java
+++ src/main/java/DukeBot.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class DukeBot {
     private static final String BORDER = "\t___________________________________\n";
     private String output;
-    private final ArrayList<String> taskList = new ArrayList<>();
+    private final ArrayList<Task> taskList = new ArrayList<>();
 
     public DukeBot() {
         greeting();
@@ -15,8 +15,13 @@ public class DukeBot {
     }
 
     public boolean echo(String input) {
+        String[] doneCommand = new String[2];
         boolean continueInput = true;
 
+        if(input.contains("done")) {
+            doneCommand = input.split(" ");
+            input = doneCommand[0];
+        }
         switch (input) {
             case "bye":
                 output = BORDER + "\t" + " Bye. Hope to see you again soon!\n" + BORDER;
@@ -25,20 +30,30 @@ public class DukeBot {
             case "list":
                 output = retrieveList(taskList);
                 break;
+            case "done":
+                Task doneTask = taskList.get(Integer.parseInt(doneCommand[1]) - 1);
+                doneTask.markAsDone();
+                output = BORDER +  "\t" + " Nice! I've marked this task as done:\n" +  "\t  "
+                        + doneTask.getStatusIcon() + doneTask.description + "\n" + BORDER;
+                break;
             default:
                 output = BORDER + "\t added: " + input + "\n" + BORDER;
-                taskList.add(input);
+                Task newTask = new Task(input);
+                taskList.add(newTask);
                 break;
         }
         System.out.println(output);
         return continueInput;
     }
 
-    public String retrieveList(ArrayList<String> taskList) {
-        StringBuilder str = new StringBuilder(BORDER);
+    public String retrieveList(ArrayList<Task> taskList) {
+        StringBuilder str = new StringBuilder(BORDER+ "\t" + " Here are the tasks in your list:\n");
 
         for (int num = 1; num <= taskList.size(); num++) {
-            str.append("\t ").append(num).append(". ").append(taskList.get(num - 1)).append("\n");
+            Task currentTask = taskList.get(num-1);
+            str.append("\t ").append(num).append(".").
+                    append(currentTask.getStatusIcon()).
+                    append(currentTask.description).append("\n");
         }
         str.append(BORDER);
         return str.toString();
