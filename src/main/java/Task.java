6a2325e8bb64c88864ec6aa01b7b0ec461a670db
Yong Kang <skykinetic@stevenkou.xyz>
2021-01-17 22:16:10 +0800

    Add Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,20 @@
+public class Task {
+    protected String description;
+    protected boolean isDone = false;
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); // return tick or cross symbols
+    }
+}
