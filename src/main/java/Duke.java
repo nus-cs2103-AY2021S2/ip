5f2a30f42da111d539d64f3acd216b9b5dbb1c88
simran-bhadani3 <simranbhadani333@gmail.com>
2021-01-18 02:25:39 +0800

    Add functionality to add tasks and list tasks, Level-2 complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo =
                 "     ______\n" +
-                        "    |___  /\n" +
-                        "       / / \n" +
-                        "      / /  \n" +
-                        "     / /__ \n" +
-                        "    /_____|\n";
-
+                "    |___  /\n" +
+                "       / / \n" +
+                "      / /  \n" +
+                "     / /__ \n" +
+                "    /_____|\n";
         System.out.println("\n~ Hello! I am Zee :) ~\n"
                 + logo + "\n"
                 + "~ What can I do for you today? ~\n");
-
+        List<String> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
         String command = scanner.nextLine();
-
-        while (!command.equals("bye")) {
-            System.out.println("\n~ " + command + " ~\n");
+        while(!command.equals("bye")) {
+            if(command.equals("list")) {
+                int count = 1;
+                System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                for(String string: tasks) {
+                    System.out.println("  " + count + ". " + string);
+                    count++;
+                }
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+            } else {
+                System.out.println("\n~ added: " + command + " ~\n");
+                tasks.add(command);
+            }
             command = scanner.nextLine();
         }
-
         scanner.close();
         System.out.println("\n~ Bye! Come back soon! UwU ~\n");
     }
