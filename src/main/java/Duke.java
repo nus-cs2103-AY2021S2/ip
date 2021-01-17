4acc222947dc0344af3d54073d1e5b2cb673ea08
Shuyang0 <l.shuyang98@gmail.com>
2021-01-17 19:13:36 +0800

    complete level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,17 +18,24 @@ public class Duke {
         ArrayList<Task> tasks = new ArrayList<Task>();
         Scanner sc = new Scanner(System.in);
         while(true) {
-            String msg = sc.next();
-            if(msg.equals("bye")) {
+            String msg = sc.nextLine();
+            String[] msgSplit = msg.split(" ");
+            if(msgSplit[0].equals("bye")) {
                 System.out.println(BORDER);
                 System.out.println(INDENT + "Bye. Hope to see you again!");
                 System.out.println(BORDER + "\n");
                 break;
-            } else if(msg.equals("list")){
+            } else if(msgSplit[0].equals("list")) {
                 System.out.println(BORDER);
-                for(int i = 0; i < tasks.size(); i++) {
-                    System.out.println(INDENT + (i+1)  + ". " + tasks.get(i).getName());
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(INDENT + (i + 1) + "." + tasks.get(i));
                 }
+                System.out.println(BORDER + "\n");
+            } else if(msgSplit[0].equals("done")) {
+                int taskIndex = Integer.parseInt(msgSplit[1]) - 1;
+                tasks.get(taskIndex).setDone();
+                System.out.println(BORDER);
+                System.out.println(INDENT + "Nice! I've marked this task as done:\n" + INDENT + tasks.get(taskIndex));
                 System.out.println(BORDER + "\n");
             } else {
                 tasks.add(new Task(msg));
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,22 @@
 public class Task {
     private final String name;
+    private boolean isDone;
 
     Task(String name) {
         this.name = name;
+        this.isDone = false;
     }
 
-    String getName() {
-        return this.name;
+    void setDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        if(isDone) {
+            return "[X] " + this.name;
+        } else {
+            return "[ ] " + this.name;
+        }
     }
 }
