9e11c22005b18885ab5e5a7c978b709d55ef0931
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-01-17 19:12:46 +0800

    Added Delete functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,6 +28,7 @@ public class Duke {
     private static String DEADLINESPLITREGEX = " /by ";
     private static String EVENTCOMMAND = "event";
     private static String EVENTSPLITREGEX = " /at ";
+    private static String DELETECOMMAND = "delete";
 
     private static IO io;
 
@@ -35,11 +36,11 @@ public class Duke {
         io = new IO();
         printHelloMessage();
 
-        List<Task> Tasks = new ArrayList<Task>();
+        List<Task> tasks = new ArrayList<Task>();
         while (true) {
             String reply = io.readLine();
             try {
-                validateCommand(reply, Tasks.size());
+                validateCommand(reply, tasks.size());
             } catch (CommandException e) {
                 io.printBotMessage(e.getMessage());
                 continue;
@@ -47,27 +48,34 @@ public class Duke {
             if (reply.equals(EXITCOMMAND)) {
                 break;
             } else if (reply.equals(LISTCOMMAND)) {
-                io.printTasks(Tasks);
+                io.printTasks(tasks);
             } else if (reply.startsWith(DONECOMMAND)) {
-                Integer index = Integer.parseInt(reply.substring(5));
-                Tasks.get(index - 1).markCompleted();
-                printDoneMessage(Tasks.get(index - 1));
+                Integer index = Integer.parseInt(reply.substring(DONECOMMAND.length()+1));
+                tasks.get(index - 1).markCompleted();
+                printDoneMessage(tasks.get(index - 1));
             } else if (reply.startsWith(TODOCOMMAND)) {
                 ToDos newTask = new ToDos(reply.substring(TODOCOMMAND.length() + 1));
-                Tasks.add(newTask);
-                printAddedTaskMessage(newTask, Tasks.size());
+                tasks.add(newTask);
+                printAddedTaskMessage(newTask, tasks.size());
             } else if (reply.startsWith(DEADLINECOMMAND)) {
                 String message = reply.substring(DEADLINECOMMAND.length() + 1);
                 String[] messages = message.split(DEADLINESPLITREGEX);
                 Deadlines newTask = new Deadlines(messages[0], messages[1]);
-                Tasks.add(newTask);
-                printAddedTaskMessage(newTask, Tasks.size());
+                tasks.add(newTask);
+                printAddedTaskMessage(newTask, tasks.size());
             } else if (reply.startsWith(EVENTCOMMAND)) {
                 String message = reply.substring(EVENTCOMMAND.length() + 1);
                 String[] messages = message.split(EVENTSPLITREGEX);
                 Events newTask = new Events(messages[0], messages[1]);
-                Tasks.add(newTask);
-                printAddedTaskMessage(newTask, Tasks.size());
+                tasks.add(newTask);
+                printAddedTaskMessage(newTask, tasks.size());
+            } else if (reply.startsWith(DELETECOMMAND)) {
+
+                Integer index = Integer.parseInt(reply.substring(DELETECOMMAND.length() + 1));
+                Task deletedTask = tasks.get(index-1);
+                tasks.remove(index-1);
+                printDeleteMessage(deletedTask,tasks.size());
+
             }
         }
         io.printBotMessage("Bye. Hope to see you again soon!");
@@ -81,6 +89,10 @@ public class Duke {
         io.printBotMessage("Nice! I've marked this task as done:\n " + task.toString());
     }
 
+    public static void printDeleteMessage(Task task, int count) {
+        io.printBotMessage("Noted. I've removed this task:\n " + task.toString()+ "\nNow you have " + count + " tasks in the list.");
+    }
+
     public static void printAddedTaskMessage(Task task, int count) {
         io.printBotMessage("Got it. I've added this task: \n  " + task.toString() + "\nNow you have " + count + " tasks in the list.");
     }
@@ -88,8 +100,8 @@ public class Duke {
     public static void validateCommand(String command, int taskCount) throws CommandException {
         if(command.equals(EXITCOMMAND)||command.equals(LISTCOMMAND))
             return;
-        if(command.equals(DONECOMMAND)) {
-            throw new CommandException("☹ OOPS!!! You must indicate the index of the Tasks to be done.");
+        if(command.equals(DONECOMMAND) || command.equals(DELETECOMMAND)) {
+            throw new CommandException("☹ OOPS!!! You must indicate the index of the Tasks to be "+command+".");
         }
 
         if(command.equals(TODOCOMMAND)||command.equals(DEADLINECOMMAND)||command.equals(EVENTCOMMAND)) {
@@ -104,10 +116,10 @@ public class Duke {
         String commandName = command.substring(0, firstSplit);
         String description= command.substring(firstSplit + 1);
 
-        if(commandName.equals(DONECOMMAND)||commandName.equals(TODOCOMMAND)||commandName.equals(EVENTCOMMAND)||commandName.equals(DEADLINECOMMAND)) {
+        if(commandName.equals(DONECOMMAND)||commandName.equals(DELETECOMMAND)||commandName.equals(TODOCOMMAND)||commandName.equals(EVENTCOMMAND)||commandName.equals(DEADLINECOMMAND)) {
             if(description.isEmpty()) {
                 throw new CommandException("☹ OOPS!!! The description of a "+commandName+" cannot be empty.");
-            }else if (commandName.equals(DONECOMMAND)) {
+            }else if (commandName.equals(DONECOMMAND)||commandName.equals(DELETECOMMAND)) {
                 try {
                     int index = Integer.parseInt(description);
                     if(index > taskCount || index < 1) {
