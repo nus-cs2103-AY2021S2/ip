8a775b5b47849151502672f1ae1924cfd1379257
cheeerynne <>
2021-01-17 20:11:42 +0800

    Implemented 'Add, List'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String horzLine = "    _________________________________________________";
+        List<String> list = new ArrayList<>();
+
         String logo = " ____        _        \n"
                 + "               |  _ \\ _   _| | _____ \n"
                 + "               | | | | | | | |/ / _ \\\n"
                 + "               | |_| | |_| |   <  __/\n"
                 + "               |____/ \\__,_|_|\\_\\___|\n";
 
-        System.out.println("    -------------------------------------------------\n"
-                + "     Hello! I'm"
-                + logo
+        System.out.println(horzLine
+                + "\n     Hello! I'm" + logo
                 + "\n     What can I do for you?\n"
-                + "    -------------------------------------------------");
+                + horzLine);
 
         boolean exit = false;
         while (!exit) {
@@ -21,14 +25,23 @@ public class Duke {
 
             if (userInput.equals("bye")) {
                 exit = true;
-                System.out.println("    -------------------------------------------------\n"
-                        + "     Bye. Hope to see you again soon!\n"
-                        + "    -------------------------------------------------");
+                System.out.println(horzLine
+                        + "\n     Bye. Hope to see you again soon!\n"
+                        + horzLine);
+            } else if (userInput.equals("list")) {
+                System.out.println(horzLine);
+
+                for (int i = 0; i < list.size(); i++) {
+                    int number = 1 + i;
+                    System.out.println("     " + number + ". " + list.get(i));
+                }
+
+                System.out.println(horzLine);
             } else {
-                System.out.println("    -------------------------------------------------\n"
-                        + "     "
-                        + userInput
-                        + "\n    -------------------------------------------------");
+                list.add(userInput);
+                System.out.println(horzLine
+                        + "\n      added: " + userInput + "\n"
+                        + horzLine);
             }
         }
 
