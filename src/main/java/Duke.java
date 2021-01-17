123a6b20480400b04764f0dde60f0c17b640824f
Stratostorm <h_weicong@outlook.com>
2021-01-17 18:08:30 +0800

    Replaced echo functionality with storage functionality. Implemented "list" command.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Duke {
+    private static ArrayList<String> storage;
+    private static void processCommand(String command) {
+        if (command.equals("list")) {
+            for (String str : storage) {
+                System.out.println(str);
+            }
+        } else {
+            System.out.println("I will remember: " + command);
+            storage.add(command);
+        }
+    }
+
     public static void main(String[] args) {
+        storage = new ArrayList<String>();
         Scanner sc =  new Scanner(System.in);
         System.out.println("Greetings. My name is I-01B, but you may call me CHEF. What can I assist you with?");
         while (sc.hasNext()) {
@@ -10,7 +23,7 @@ public class Duke {
                 System.out.println("I hope I have been of assistance. Goodbye. C:");
                 break;
             } else {
-                System.out.println(command);
+                processCommand(command);
             }
         }
     }
