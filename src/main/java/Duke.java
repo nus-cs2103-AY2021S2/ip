e2d982f1a6bfb387c4669fb1bddf1fddfa7a4895
jlxw48 <jlxw48@gmail.com>
2021-01-17 16:18:47 +0800

    Implement skeletal chatbot which responds to input from user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static void introduction() {
+        String loading = "Hello from\n"
+                + " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "| |_| | |_| |   <  /\n"
+                + "|____/ \\,_|_|\\_\\___|\n";
+        System.out.println(loading);
+        String welcomeMessage = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(welcomeMessage);
+    }
+
+    private static void endProgram() {
+        String endMessage = "Bye. Hope to see you again soon!";
+        System.out.println(endMessage);
+        scanner.close();
+    }
+
+    private static void run() {
+        introduction();
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                endProgram();
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        run();
     }
 }
