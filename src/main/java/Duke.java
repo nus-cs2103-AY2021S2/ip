692d990c869a525bbdcdd5c1261e5ef6c1441a9d
Azeem Vasanwala <azeem.iron@gmail.com>
2021-01-17 21:39:54 +0800

    Implement level-1 of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        Scanner scanner = new Scanner(System.in);
+        String logo = " _____      _               _   _             \n" +
+                "/  ___|    | |             | | (_)            \n" +
+                "\\ `--.  ___| |__   __ _ ___| |_ _  __ _ _ __  \n" +
+                " `--. \\/ _ \\ '_ \\ / _` / __| __| |/ _` | '_ \\ \n" +
+                "/\\__/ /  __/ |_) | (_| \\__ \\ |_| | (_| | | | |\n" +
+                "\\____/ \\___|_.__/ \\__,_|___/\\__|_|\\__,_|_| |_|\n" +
+                "                                              \n" +
+                "                                              ";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Enter a command below for me to assist you");
+
+        boolean shouldRun = true;
+        while (shouldRun) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                echo("Bye. Hope to see you again soon!");
+                shouldRun = false;
+            } else {
+                echo(input);
+            }
+        }
+    }
+
+    public static void echo(String input) {
+        System.out.println("------------------------------");
+        System.out.println(input);
+        System.out.println("------------------------------");
     }
 }
