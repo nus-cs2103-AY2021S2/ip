f02c9a890a9b023384371d505c63732e4d8989da
Yong Kang <skykinetic@stevenkou.xyz>
2021-01-17 22:07:06 +0800

    Duke.java: store the commands given

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,12 @@
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Duke {
     private static final String HORIZONTAL_LINE = "\t____________________________________________________________";
 
     /**
-     * Wrap single line with horizontal lines and indent using tab
-     */
-    private static void wrappedPrint(String line) {
-        wrappedPrint(new String[]{line});
-    }
-
-    /**
      * Wrap line(s) with horizontal lines and indent using tab
-     * @param lines
+     * @param lines: array of strings
      */
     private static void wrappedPrint(String[] lines) {
         System.out.println(HORIZONTAL_LINE);
@@ -22,17 +16,45 @@ public class Duke {
         System.out.println(HORIZONTAL_LINE);
     }
 
+    /**
+     * Wrap single line with horizontal lines and indent using tab
+     */
+    private static void wrappedPrint(String line) {
+        wrappedPrint(new String[]{line});
+    }
+
+    private static void printHistory(Vector<String> commandHistory) {
+        System.out.println(HORIZONTAL_LINE);
+        for (int i = 0; i < commandHistory.size(); i++) {
+            System.out.printf("\t %d: %s%n", i + 1, commandHistory.get(i));
+        }
+        System.out.println(HORIZONTAL_LINE);
+    }
+
     public static void main(String[] args) {
         // Initial greeting
         wrappedPrint(new String[]{"Yo! I'm Ekud!", "What you want?"});
 
-        // echo whatever is given until bye is detected
+        // store whatever is given until bye is detected
         Scanner input = new Scanner(System.in);
-        String command = input.next();
-        while (!command.equals("bye")) {
-            wrappedPrint(command);
-            command = input.next();
-        }
-        wrappedPrint("Bye bye. Anything call me ah!");
+        Vector<String> commandHistory = new Vector<>();
+        String command;
+        boolean active = true;
+
+        do {
+            command = input.nextLine();
+            switch (command) {
+                case "list":
+                    printHistory(commandHistory);
+                    break;
+                case "bye":
+                    wrappedPrint("Bye bye. Anything call me ah!");
+                    active = false;
+                    break;
+                default:
+                    commandHistory.add(command);
+                    wrappedPrint("added: " + command);
+            }
+        } while (active);
     }
 }
