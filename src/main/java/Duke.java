954e3ec5d5664748cd0907dde562b2edfe8c24af
wongkokian <ianwong97@hotmail.sg>
2021-01-18 03:26:00 +0800

    Changed chatbot name and added greet, echo and exit function.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,28 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String greet = "    ____________________________________________________________\n"
+                + "     Hello! I'm Duke\n"
+                + "     What can I do for you?\n"
+                + "    ____________________________________________________________\n";
+        System.out.println(greet);
+
+        Scanner sc = new Scanner(System.in);
+        echo(sc);
+        sc.close();
+    }
+
+    public static void echo(Scanner sc) {
+        String cmd = sc.next();
+        System.out.println("    ____________________________________________________________");
+        if(!cmd.equals("bye")) {
+            System.out.println("     " + cmd + "\n"
+                    + "    ____________________________________________________________\n");
+            echo(sc);
+        } else {
+            System.out.println("     Bye. Hope to see you again soon!" + "\n"
+                    + "    ____________________________________________________________\n");
+        }
     }
 }
