6e6e8c115ee1c4758c96151955d5437d93466b30
Huang Po-Wei <>
2021-01-17 11:24:53 +0800

    Implement greet, echo and exit
    Project starts with a basic skeletal version.
    
    Let's allow BLARB, which is this version of Duke to greet the users
    and exit when called, and to echo all other unrecognized commands.
    
    The skeletal version is to kick off the project without complications.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " ____            _      ____   ____\n" +
+                "|    \\ |        / \\    |    \\ |    \\\n" +
+                "|____/ |       /___\\   |____/ |____/\n" +
+                "|    \\ |      /     \\  |  \\   |    \\\n" +
+                "|____/ |____ /       \\ |   \\_ |____/\n";
+        System.out.println("You are now in the presence of\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+
+        String response = "%s\n"
+                + "\n"
+                + "> ";
+
+        String intro = "This is BLARB\n"
+                + "You may speak";
+
+        String esc = "Hasta la vida, baby.";
+
+        System.out.printf(response, intro);
+
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(esc);
+                break;
+            } else {
+                System.out.printf(response, input);
+            }
+        }
     }
 }
+
