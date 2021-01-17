c8433c731d423f5e635472f5bd64b0326b6dc8f6
cheeerynne <>
2021-01-17 19:06:37 +0800

    Implemented 'Greet, Echo, Exit'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "               |  _ \\ _   _| | _____ \n"
+                + "               | | | | | | | |/ / _ \\\n"
+                + "               | |_| | |_| |   <  __/\n"
+                + "               |____/ \\__,_|_|\\_\\___|\n";
+
+        System.out.println("    -------------------------------------------------\n"
+                + "     Hello! I'm"
+                + logo
+                + "\n     What can I do for you?\n"
+                + "    -------------------------------------------------");
+
+        boolean exit = false;
+        while (!exit) {
+            String userInput = sc.nextLine();
+
+            if (userInput.equals("bye")) {
+                exit = true;
+                System.out.println("    -------------------------------------------------\n"
+                        + "     Bye. Hope to see you again soon!\n"
+                        + "    -------------------------------------------------");
+            } else {
+                System.out.println("    -------------------------------------------------\n"
+                        + "     "
+                        + userInput
+                        + "\n    -------------------------------------------------");
+            }
+        }
+
+        sc.close();
     }
 }
