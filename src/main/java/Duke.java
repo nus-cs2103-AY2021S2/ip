dafd1a6553666c652af8820d81c602f385645b6a
natosy <natosyy@gmail.com>
2021-01-17 13:08:34 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Duke {
+
+    static ArrayList<String> list = new ArrayList<>();
 
     static void display(String str) {
         String[] strings = str.split("\n");
@@ -12,6 +15,14 @@ public class Duke {
         System.out.println("    " + "___________________________________________________________________");
     }
 
+    static void displayList() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= list.size(); i++) {
+            sb.append(i + ". " + list.get(i - 1) + "\n");
+        }
+        display(sb.toString());
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         display("Hello! I'm Duke\nWhat can I do for you?");
@@ -20,8 +31,11 @@ public class Duke {
             if (command.equals("bye")) {
                 display("Bye. Hope to see you again soon!");
                 return;
+            } else if (command.equals("list")) {
+                displayList();
             } else {
-                display(command);
+                list.add(command);
+                display("added: " + command);
             }
         }
     }
