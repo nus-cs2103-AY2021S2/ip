9e55bb8fd0ba8c86746b6a8892178c957fd5da20
Mok Bing Wei Maurice <Maurice2n97@gmail.com>
2021-01-17 17:02:46 +0800

    Added remember commands, or list them out

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private static List<String> listOfTasks = new ArrayList<>();
+
+    private static void printAllTasks() {
+        int counter = 1;
+        for (String s : listOfTasks) {
+            System.out.println(counter + ". " + s);
+            counter++;
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,13 +22,18 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("\n\n");
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        String input;
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        while (true) {
-            if (input.equals("bye")) break;
-            System.out.println(input + "\n");
+        do {
             input = sc.nextLine();
-        }
+            if (input.equals("bye")) break;
+            if (input.equals("list")) {
+                printAllTasks();
+            } else {
+                System.out.println("added: " + input + "\n");
+                listOfTasks.add(input);
+            }
+        } while(true);
         System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
