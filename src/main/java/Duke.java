1ec97e3eaaeb4df3547da6557c79a41385827236
Marcus Lee <marcus_lee@u.nus.edu>
2021-01-17 16:55:43 +0800

    modify Duke.java to handle exceptions
    Modify Duke.java to catch DukeExceptions thrown by methods in the
    program-loop when errors/incorrect inputs are encountered. Also modify program loop
    to throw DukeExceptions on unknown commands.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,37 +14,45 @@ public class Duke {
         while (sc.hasNextLine()) {
             String userInput = sc.nextLine();
             StringBuilder response = new StringBuilder();
-            if (userInput.equals("bye")) {
-                response.append("Bye. Hope to see you soon!");
-                Duke.printWithBorders(response.toString());
-                return;
-            } else if (userInput.equals("list")) {
-                response.append("Here are the tasks in your list:\n");
-                for (int i = 0; i < numberOfTasks; i++) {
-                    Task task = tasks[i];
-                    response.append(i + 1);
-                    response.append(".");
-                    response.append(task);
-                    if (i != numberOfTasks - 1) {
-                        response.append("\n");
+            try {
+                if (userInput.equals("bye")) {
+                    response.append("Bye. Hope to see you soon!");
+                    Duke.printWithBorders(response.toString());
+                    return;
+                } else if (userInput.equals("list")) {
+                    response.append("Here are the tasks in your list:\n");
+                    for (int i = 0; i < numberOfTasks; i++) {
+                        Task task = tasks[i];
+                        response.append(i + 1);
+                        response.append(".");
+                        response.append(task);
+                        if (i != numberOfTasks - 1) {
+                            response.append("\n");
+                        }
                     }
+                } else if (userInput.startsWith("done")) {
+                    int userChoice = Integer.valueOf(userInput.split(" ")[1]);
+                    Task task = tasks[userChoice - 1];
+                    task.markComplete();
+                    response.append("Nice! I've marked this task as done:\n");
+                    response.append(task);
+                } else if (userInput.startsWith("event") ||
+                        userInput.startsWith("todo") ||
+                        userInput.startsWith("deadline")) {
+                    Task task = Task.parseTask(userInput);
+                    tasks[numberOfTasks++] = task;
+                    response.append("Got it. I've added this task:\n  added: ");
+                    response.append(task);
+                    response.append("\nNow you have ");
+                    response.append(numberOfTasks);
+                    response.append(" tasks in the list.");
+                } else {
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-            } else if (userInput.startsWith("done")) {
-                int userChoice = Integer.valueOf(userInput.split(" ")[1]);
-                Task task = tasks[userChoice - 1];
-                task.markComplete();
-                response.append("Nice! I've marked this task as done:\n");
-                response.append(task);
-            } else {
-                Task task = Task.parseTask(userInput);
-                tasks[numberOfTasks++] = task;
-                response.append("Got it. I've added this task:\n  added: ");
-                response.append(task);
-                response.append("\nNow you have ");
-                response.append(numberOfTasks);
-                response.append(" tasks in the list.");
+                Duke.printWithBorders(response.toString());
+            } catch (DukeException e) {
+                Duke.printWithBorders(e.toString());
             }
-            Duke.printWithBorders(response.toString());
         }
     }
 
