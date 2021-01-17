b5bd5ddd8f72cd937bcc5bc8a5fe83aaee332ac2
Figo Lee Ze Xin <e0420458@u.nus.edu>
2021-01-18 00:46:29 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,31 +3,40 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> col = new ArrayList<>(100);
-        int i = 1;
+        ArrayList<Task> col = new ArrayList<>(100);
 
         System.out.println("----------------------------------------------");
         System.out.println("Hello I'm Duke\n" + "What can I do for you?");
         System.out.println("----------------------------------------------");
 
-        String next = sc.nextLine();
-        while(!next.equals("bye")) {
-            col.add(next);
-            System.out.println("----------------------------------------------");
-            System.out.println("added: " + next);
-            System.out.println("----------------------------------------------");
-            next = sc.nextLine();
-            while (next.equals("list")) {
+        String x = sc.nextLine();
+        String[] command = x.split(" ");
+        while(!x.equals("bye")) {
+            if(x.equals("list")) {
                 System.out.println("----------------------------------------------");
-                Object[] strarr = col.toArray();
-                for (Object s : strarr) {
-                    System.out.println(i + ". " + s);
-                    i++;
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < col.size(); i++) {
+                    System.out.println((i+1) + ". " + col.get(i));
                 }
                 System.out.println("----------------------------------------------");
-                i = 1;
-                next = sc.nextLine();
             }
+            else if(command[0].equals("done")) {
+                System.out.println("----------------------------------------------");
+                int index = Integer.parseInt(command[1]);
+                Task t = col.get(index - 1);
+                t.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(t);
+                System.out.println("----------------------------------------------");
+            }
+            else { //must be add task
+                col.add(new Task(x));
+                System.out.println("----------------------------------------------");
+                System.out.println("added: " + x);
+                System.out.println("----------------------------------------------");
+            }
+            x = sc.nextLine();
+            command = x.split(" ");
         }
         System.out.println("----------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
