bf287e78d4f3fad8a64625c1d8a9f89cecf42d02
ming-00 <e0470728@u.nus.edu>
2021-01-17 16:19:25 +0800

    Implement Level 3

--- src/main/java/Ellis.java
+++ src/main/java/Ellis.java
@@ -5,11 +5,40 @@ import java.util.Scanner;
 public class Ellis {
     List<Task> lst = new ArrayList<>();
 
+    /**
+     * Iterates and returns enumerated list of tasks.
+     */
     public void iterateList() {
+        System.out.println("Here are the items in your list:\n");
         int i = 1;
         for (Task item : lst) {
-            System.out.println(i + ". " + item.description);
+            System.out.println(i + ". " + item.getStatusIcon() + item.description);
             i++;
+        }
+    }
+
+    /**
+     * Formats text.
+     */
+    public void formatText(String str) {
+        System.out.println("**************************************************");
+        System.out.println(str);
+        System.out.println("**************************************************");
+    }
+
+    /**
+     * Checks for task in the given index and marks it as completed if valid.
+     *
+     * @param index task number given one-based indexing.
+     */
+    public void handleTaskStatus(int index) {
+        if (index >= lst.size()) {
+            formatText("Index out of bounds! You don't have so many items, hun.");
+        } else {
+            Task task = lst.get(index);
+            task.markAsDone();
+            formatText("Sweet! I have marked the following task as done:\n"
+                    + task.getStatusIcon() + task.description);
         }
     }
 
@@ -18,19 +47,22 @@ public class Ellis {
         while (true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
-                System.out.println("**********************");
-                System.out.println("Bye, see you soon!");
-                System.out.println("**********************");
+                // close program
+                formatText("Bye, see you soon!");
                 break;
             } else if (input.equals("list")) {
-                System.out.println("**********************");
+                // show everything in the list
+                System.out.println("**************************************************");
                 iterateList();
-                System.out.println("**********************");
+                System.out.println("**************************************************");
+            } else if (input.split(" ",2)[0].equals("done")){
+                // mark task with the given index as completed
+                int index = Integer.parseInt(input.split(" ",2)[1]) - 1;
+                handleTaskStatus(index);
             } else {
+                // add new task to list
                 lst.add(new Task(input));
-                System.out.println("**********************");
-                System.out.println("Added: " + input);
-                System.out.println("**********************");
+                formatText("Added: " + input);
             }
         }
         sc.close();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,15 +2,28 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructs a Task object.
+     *
+     * @param description information on task to be done.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Checks task completion status and returns formatted status icon.
+     *
+     * @return formatted status icon.
+     */
     public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        return (isDone ? "[X] " : "[ ] "); //return tick or X symbols
     }
 
+    /**
+     * Marks a task as completed.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
