dfdc2a6247537535941368d55acb4c2801e7404d
Huang Po-Wei <>
2021-01-17 11:47:47 +0800

    Add comments and change variable names
    Increase clarity on the functionality of the code.
    
    Variable names are changed to fit the problem statement.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        //Logo Display
         String logo = " ____            _      ____   ____\n" +
                 "|    \\ |        / \\    |    \\ |    \\\n" +
                 "|____/ |       /___\\   |____/ |____/\n" +
@@ -9,23 +10,27 @@ public class Duke {
                 "|____/ |____ /       \\ |   \\_ |____/\n";
         System.out.println("You are now in the presence of\n" + logo);
 
+        //Import IO
         Scanner sc = new Scanner(System.in);
 
+        //General template for Blarb's response
         String response = "%s\n"
                 + "\n"
                 + "> ";
 
-        String intro = "This is BLARB\n"
+        //Greeting and Exit Phrases
+        String greet = "This is BLARB\n"
                 + "You may speak";
 
-        String esc = "Hasta la vida, baby.";
+        String exit = "Hasta la vida, baby.";
 
-        System.out.printf(response, intro);
+        System.out.printf(response, greet);
 
+        //Implementation of Echoing and Exiting
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
-                System.out.println(esc);
+                System.out.println(exit);
                 break;
             } else {
                 System.out.printf(response, input);
