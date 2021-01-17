5f3d1fa5dacb90f9208d7aec52c7649413cf1756
simran-bhadani3 <simranbhadani333@gmail.com>
2021-01-18 02:19:32 +0800

    Add ability to echo messages, Level-1 complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo =
+                "     ______\n" +
+                        "    |___  /\n" +
+                        "       / / \n" +
+                        "      / /  \n" +
+                        "     / /__ \n" +
+                        "    /_____|\n";
+
+        System.out.println("\n~ Hello! I am Zee :) ~\n"
+                + logo + "\n"
+                + "~ What can I do for you today? ~\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String command = scanner.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println("\n~ " + command + " ~\n");
+            command = scanner.nextLine();
+        }
+
+        scanner.close();
+        System.out.println("\n~ Bye! Come back soon! UwU ~\n");
     }
 }
