2c6c8ad951ee905a7af0262290459d602f05b2d6
CSmortal <jovenpua51@gmail.com>
2021-01-17 21:57:55 +0800

    Level 2: 1st change

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,26 +13,36 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
-
+        List<String> myList = new ArrayList<>();
         String indent = "         ";
-        String horizSep = indent + "________________________________________________\n";
+        String horizSep = indent + "________________________________________________";
 
         String greeting = indent + " Hello! I'm Duke\n" + indent + " What can I do for you?\n";
         String farewell = indent + " Bye. Hope to see you again soon!\n";
         Scanner sc = new Scanner(System.in);
 
-        System.out.println(horizSep + greeting + horizSep);
+        System.out.println(horizSep + "\n" + greeting + horizSep + "\n");
 
         while (sc.hasNextLine()) {
             String next = sc.nextLine();
 
             if (next.equals("bye")) {
 
-                System.out.println(horizSep + farewell + horizSep);
+                System.out.println(horizSep + "\n" + farewell + horizSep + "\n");
                 sc.close();
                 return;
+            } else if (next.equals("list")) {
+                ListIterator iter = myList.listIterator();
+
+                System.out.println(horizSep);
+                while (iter.hasNext()) {
+                    System.out.println(indent + " " + String.valueOf(iter.nextIndex() + 1) + ". " + iter.next());
+                }
+                System.out.println(horizSep);
+
             } else {
-                System.out.println(horizSep + indent + " " + next + "\n" + horizSep);
+                myList.add(next);
+                System.out.println(horizSep + "\n" +  indent + " added: " + next + "\n" + horizSep + "\n");
             }
         }
 
