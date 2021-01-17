c1fd089c49c2d11c0edf15dc4669bdd4db760a27
ming-00 <e0470728@u.nus.edu>
2021-01-17 15:40:06 +0800

    Implement Level 2

--- src/main/java/Ellis.java
+++ src/main/java/Ellis.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Ellis {
+    List<String> lst = new ArrayList<>();
+
+    public void iterateList() {
+        int i = 1;
+        for (String item : lst) {
+            System.out.println(i + ". " + item);
+            i++;
+        }
+    }
 
     public void run() {
         Scanner sc = new Scanner(System.in);
@@ -11,10 +22,16 @@ public class Ellis {
                 System.out.println("Bye, see you soon!");
                 System.out.println("**********************");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("**********************");
+                iterateList();
+                System.out.println("**********************");
+            } else {
+                lst.add(input);
+                System.out.println("**********************");
+                System.out.println("Added: " + input);
+                System.out.println("**********************");
             }
-            System.out.println("**********************");
-            System.out.println(input);
-            System.out.println("**********************");
         }
         sc.close();
     }
