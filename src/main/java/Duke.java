6fe144c8d2a25051764498438ada8fb1fea407ea
Yong Kang <skykinetic@stevenkou.xyz>
2021-01-18 00:43:35 +0800

    Create Todo, Deadline and Event task types, refactored code to enable

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,7 +11,16 @@ public class Duke {
     private static void wrappedPrint(String[] lines) {
         System.out.println(HORIZONTAL_LINE);
         for (String l: lines) {
-            System.out.println("\t " + l);
+            printLine(l);
+        }
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    private static void wrappedPrint(Vector<Task> tasks) {
+        System.out.println(HORIZONTAL_LINE);
+        printLine("Go do work! You need finish these things:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.printf("\t %d. %s%n", i + 1, tasks.get(i).toString());
         }
         System.out.println(HORIZONTAL_LINE);
     }
@@ -23,45 +32,73 @@ public class Duke {
         wrappedPrint(new String[]{line});
     }
 
-    private static void printHistory(Vector<Task> tasks) {
+    private static void printLine(String line) {
+        System.out.println("\t " + line);
+    }
+
+    private static void processDescription(String full, String sep, String[] description) {
+        int i = full.indexOf(" " + sep);
+        description[0] = full.substring(0, i);
+        description[1] = full.substring(i + sep.length() + 2);
+    }
+
+    private static void postAddTaskSummary(Vector<Task> tasks) {
         System.out.println(HORIZONTAL_LINE);
-        for (int i = 0; i < tasks.size(); i++) {
-            Task currentTask = tasks.get(i);
-            System.out.printf("\t %d. %s%n", i + 1, currentTask);
-        }
+        printLine("Okay I remember for you liao:");
+        printLine("\t" + tasks.lastElement());
+        printLine(String.format("Eh you got %d task%s in total leh", tasks.size(), tasks.size() == 1 ? "" : "s"));
         System.out.println(HORIZONTAL_LINE);
     }
 
     public static void main(String[] args) {
         // Initial greeting
-        wrappedPrint(new String[]{"Yo! I'm Ekud!", "What you want?"});
+        wrappedPrint(new String[]{
+                "Yo! I'm Ekud!",
+                "What you want?"
+        });
 
         // store whatever is given until bye is detected
         Scanner input = new Scanner(System.in);
         Vector<Task> tasks = new Vector<>();
-        String description;
         boolean active = true;
 
+        // process input and act accordingly until bye is encountered
         do {
-            description = input.nextLine();
-            if (description.startsWith("done")) {
-                int index = Integer.parseInt(description.substring(5)) - 1;
-                Task currentTask = tasks.get(index);
-                currentTask.markAsDone();
-                wrappedPrint(new String[]{"Good job! The task below is marked done!", currentTask.toString()});
-                continue;
-            }
-            switch (description) {
-                case "list":
-                    printHistory(tasks);
+            String firstWord = input.next();
+            switch (firstWord) {
+                case "list":  // list all stored tasks
+                    wrappedPrint(tasks);
+                    break;
+                case "done":  // mark a single task as done
+                    int index = input.nextInt() - 1;
+                    Task currentTask = tasks.get(index);
+                    currentTask.markAsDone();
+                    wrappedPrint(new String[]{"Good job! The task below is marked done!", currentTask.toString()});
                     break;
                 case "bye":
                     wrappedPrint("Bye bye. Anything call me ah!");
                     active = false;
                     break;
+                case "todo":
+                    tasks.add(new ToDo(input.nextLine()));
+                    postAddTaskSummary(tasks);
+                    break;
+                case "deadline": {
+                    String[] descriptions = new String[2];
+                    processDescription(input.nextLine(), "/by", descriptions);
+                    tasks.add(new Deadline(descriptions[0], descriptions[1]));
+                    postAddTaskSummary(tasks);
+                    break;
+                }
+                case "event": {
+                    String[] descriptions = new String[2];
+                    processDescription(input.nextLine(), "/at", descriptions);
+                    tasks.add(new EventTask(descriptions[0], descriptions[1]));
+                    postAddTaskSummary(tasks);
+                    break;
+                }
                 default:
-                    tasks.add(new Task(description));
-                    wrappedPrint("added: " + tasks.lastElement().description);
+                    wrappedPrint("Huh?! What talking you?");
             }
         } while (active);
     }
