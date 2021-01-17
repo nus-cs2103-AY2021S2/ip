213d8b2b6329bedf7c5f805aa5f0b3fe8311ba8c
jlxw48 <jlxw48@gmail.com>
2021-01-17 22:38:01 +0800

    Create custom Exception classes

---
+++ src/main/java/EmptyDescriptionException.java
@@ -0,0 +1,8 @@
+public class EmptyDescriptionException extends Exception {
+    public EmptyDescriptionException(String message) {
+        super(message);
+    }
+    public EmptyDescriptionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
