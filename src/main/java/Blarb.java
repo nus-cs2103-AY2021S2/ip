98d62c13ec8e2b234554a76c71eba68bb71fc4bd
Huang Po-Wei <>
2021-01-17 16:05:36 +0800

    Abstract chat functions to a separate class
    The Duke class is messy and not written in an object-oriented style.
    
    Abstracted all chatting functions to new class so that the Duke
    class only does the logo printing and input looping.
    
    The main class is made cleaner and the chatting functions are
    rewritten in OOP style to clear the code and enhance readability.

---
+++ src/main/java/Blarb.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * {@code Blarb} is the chat bot in used in the main {@code Duke} program.
+ */
+public class Blarb {
+    private final List<Task> list;
+
+    /**
+     * Initializes Blarb
+     */
+    public Blarb() {
+        list = new ArrayList<>(100);
+        blurt("This is BLARB.\nYou may speak.");
+    }
+
+    /**
+     * Prints output in response format.
+     * @param output Output string
+     */
+    private void blurt(String output) {
+        String response = "%s\n\n> ";
+        System.out.printf(response, output);
+    }
+
+    /**
+     * Prints the task list.
+     */
+    private void list() {
+        if (list.size() < 1) {
+            blurt("You have nothing on your list.");
+        } else {
+            int i = 1;
+            StringBuilder sb = new StringBuilder("Here are your tasks: \n");
+            for (Task task : list) {
+                sb.append(String.format("\n%d. %s", i++, task.toString()));
+            }
+            blurt(sb.substring(1));
+        }
+    }
+
+    /**
+     * Changes the indexed task to a completed state.
+     * @param index The index of the task.
+     */
+    private void done(Integer index) {
+        try {
+            list.get(index).markAsDone();
+            String done = "I've marked this task as done:\n %s";
+            blurt(String.format(done, list.get(index)));
+        } catch (IndexOutOfBoundsException ex) {
+            blurt("There is no such task.");
+        }
+    }
+
+    /**
+     * Adds a new Task to the task list.
+     * @param task Task to be added.
+     */
+    private void add(Task task) {
+        String addTask = "Affirmative. I've added this task: \n %s \n"
+                + "Now you have %d tasks in the list.";
+        list.add(task);
+        blurt(String.format(addTask, task.toString(), list.size()));
+    }
+
+    /**
+     * Parses and determines course of action for Blurb.
+     * @param input The inputted command.
+     * @return A boolean value that shows the availability for the next command intake.
+     */
+    public boolean execute(String input) {
+        if (input.equalsIgnoreCase("bye")) {
+            System.out.println("Hasta la vida, baby.");
+            return false;
+        }
+
+        //Splits the input into two parts -- the command and the subsidiary.
+        String[] tokens = input.split(" ", 2);
+
+        if (tokens[0].equalsIgnoreCase("list") && tokens.length < 2) {
+            list();
+        } else if (tokens[0].equalsIgnoreCase("done")) {
+            try {
+                int idx = Integer.parseInt(tokens[1]) - 1;
+                done(idx);
+            } catch (NumberFormatException ex) {
+                blurt(String.format("I have detailed files on human anatomy, but not %s.", input));
+            }
+        } else if (tokens[0].equalsIgnoreCase("todo")) {
+            Task task = new ToDo(tokens[1]);
+            add(task);
+        } else if (tokens[0].equalsIgnoreCase("deadline")) {
+            String[] fragments = tokens[1].split(" /by ");
+            Task task = new Deadline(fragments[0], fragments[1]);
+            add(task);
+        } else if (tokens[0].equalsIgnoreCase("event")) {
+            String[] fragments = tokens[1].split(" /at ");
+            Task task = new Event(fragments[0], fragments[1]);
+            add(task);
+        } else {
+            blurt(String.format("I have detailed files on human anatomy, but not %s.", input));
+        }
+
+        return true;
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
 
 /**
  * Main Class of Execution for IP.
@@ -21,73 +19,11 @@ public class Duke {
 
         //Import IO
         Scanner sc = new Scanner(System.in);
+        Blarb blarb = new Blarb();
 
-        //General templates for Blarb's response
-        String response = "%s\n"
-                + "\n"
-                + "> ";
-
-        String addTask = "Affirmative. I've added this task: \n %s \n"
-                + "Now you have %d tasks in the list.";
-
-        //Greeting
-        System.out.printf(response, "This is BLARB.\nYou may speak.");
-
-        //Implementation of Leaving, Adding, Listing and Marking.
-        //Commandized Task Initialization
-        //Uses ArrayList to store actions
-        List<Task> list = new ArrayList<>(100);
         while (sc.hasNextLine()) {
-            //Reads input into two tokens, first part command, second part details.
-            String input = sc.nextLine();
-            String[] tokens = input.split(" ", 2);
-
-            //Executes action depending on parsed command
-            if (tokens[0].equalsIgnoreCase("bye") && tokens.length < 2) {
-                System.out.println("Hasta la vida, baby.");
+            if (!blarb.execute(sc.nextLine())) {
                 break;
-            } else if (tokens[0].equalsIgnoreCase("list") && tokens.length < 2) {
-                if (list.size() < 1) {
-                    System.out.printf(response, "You have nothing on your list.");
-                } else {
-                    int i = 1;
-                    StringBuilder sb = new StringBuilder("Here are your tasks: \n");
-                    for (Task task : list) {
-                        sb.append(String.format("\n%d. %s", i++, task.toString()));
-                    }
-                    System.out.printf(response, sb.substring(1));
-                }
-            } else if (tokens[0].equalsIgnoreCase("done")) {
-                try {
-                    int idx = Integer.parseInt(tokens[1]) - 1;
-                    list.get(idx).markAsDone();
-                    String done = "I've marked this task as done:\n %s";
-                    System.out.printf(response, String.format(done, list.get(idx)));
-                } catch (IndexOutOfBoundsException ex) {
-                    System.out.printf(response, "There is no such task.");
-                } catch (NumberFormatException ex) {
-                    System.out.printf(response,
-                            String.format("I have detailed files on human anatomy, but not %s.",
-                                    input));
-                }
-            } else if (tokens[0].equalsIgnoreCase("todo")) {
-                Task task = new ToDo(tokens[1]);
-                list.add(task);
-                System.out.printf(response, String.format(addTask, task.toString(), list.size()));
-            } else if (tokens[0].equalsIgnoreCase("deadline")) {
-                String[] fragments = tokens[1].split(" /by ");
-                Task task = new Deadline(fragments[0], fragments[1]);
-                list.add(task);
-                System.out.printf(response, String.format(addTask, task.toString(), list.size()));
-            } else if (tokens[0].equalsIgnoreCase("event")) {
-                String[] fragments = tokens[1].split(" /at ");
-                Task task = new Event(fragments[0], fragments[1]);
-                list.add(task);
-                System.out.printf(response, String.format(addTask, task.toString(), list.size()));
-            } else {
-                System.out.printf(response,
-                        String.format("I have detailed files on human anatomy, but not %s.",
-                                input));
             }
         }
     }
