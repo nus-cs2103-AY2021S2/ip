75ea242d9a07b56f5f9dfba9bc46fc273412d3cd
Yong Kang <skykinetic@stevenkou.xyz>
2021-01-17 18:56:02 +0800

    Duke.java: implement skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String HORIZONTAL_LINE = "\t____________________________________________________________";
+
+    /**
+     * Wrap single line with horizontal lines and indent using tab
+     */
+    private static void wrappedPrint(String line) {
+        wrappedPrint(new String[]{line});
+    }
+
+    /**
+     * Wrap line(s) with horizontal lines and indent using tab
+     * @param lines
+     */
+    private static void wrappedPrint(String[] lines) {
+        System.out.println(HORIZONTAL_LINE);
+        for (String l: lines) {
+            System.out.println("\t " + l);
+        }
+        System.out.println(HORIZONTAL_LINE);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Initial greeting
+        wrappedPrint(new String[]{"Yo! I'm Ekud!", "What you want?"});
+
+        // echo whatever is given until bye is detected
+        Scanner input = new Scanner(System.in);
+        String command = input.next();
+        while (!command.equals("bye")) {
+            wrappedPrint(command);
+            command = input.next();
+        }
+        wrappedPrint("Bye bye. Anything call me ah!");
     }
 }
