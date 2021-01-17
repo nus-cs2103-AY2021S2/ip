549154b209c3fe913dd2edfc6efdf7ca1611670c
Stratostorm <h_weicong@outlook.com>
2021-01-17 17:40:46 +0800

    Changed greeting. Added echo functionality and exit commands.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Greetings. My name is I-01B, but you may call me CHEF. What can I assist you with?");
+        while (sc.hasNext()) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("I hope I have been of assistance. Goodbye.");
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
     }
 }
