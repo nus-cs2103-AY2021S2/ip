696e580ab970c89917496917257aee983c7efd91
laurenlhy <e0424895@u.nus.edu>
2021-01-17 17:44:49 +0800

    Add exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,39 +1,66 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void startResponse(String userInput) throws Exception {
+        if (userInput.equals("list")) {
+            if (Response.numTasks == 0) {
+                throw new NullPointerException();
+            }
+            Response.listTasks();
+        } else if (userInput.contains("done")) {
+            int taskNumber = Integer.parseInt(userInput.substring(5));
+            if (taskNumber <= 0 || taskNumber > Response.numTasks) {
+                throw new IndexOutOfBoundsException();
+            }
+            Response.markAsDone(taskNumber);
+        } else if (userInput.contains("todo")) {
+            if (userInput.length() <= 5) {
+                throw new IllegalArgumentException();
+            }
+            Task currentTask = new Todo(userInput.substring(5));
+            Response.addTask(currentTask);
+        } else if (userInput.contains("deadline")) {
+            if (userInput.length() <= 9 || !userInput.contains("/")) {
+                throw new IllegalArgumentException();
+            }
+            int temp = userInput.indexOf('/') - 1;
+            Task currentTask = new Deadline(userInput.substring(9, temp),
+                    userInput.substring(temp + 4));
+            Response.addTask(currentTask);
+        } else if (userInput.contains("event")) {
+            if (userInput.length() <= 5 || !userInput.contains("/")) {
+                throw new IllegalArgumentException();
+            }
+            int temp = userInput.indexOf('/') - 1;
+            Task currentTask = new Event(userInput.substring(6, temp),
+                    userInput.substring(temp + 4));
+            Response.addTask(currentTask);
+        } else {
+            throw new Exception();
+        }
+    }
 
     public static void main(String[] args) {
         Response.hello();
 
-        Scanner sc = new Scanner(System.in);
-
         while (true) {
-            String userInput = sc.nextLine();
-            if (userInput.equals("bye")) {
-                Response.bye();
-                break;
-            }
-
-            if (userInput.equals("list")) {
-                Response.listTasks();
-            } else if (userInput.contains("done")) {
-                int taskNumber = Integer.parseInt(userInput.substring(5));
-                Response.markAsDone(taskNumber);
-            } else if (userInput.contains("todo")) {
-                Task currentTask = new Todo(userInput.substring(5));
-                Response.addTask(currentTask);
-            } else if (userInput.contains("deadline")) {
-                int temp = userInput.indexOf('/') - 1;
-                Task currentTask = new Deadline(userInput.substring(9, temp),
-                        userInput.substring(temp + 4));
-                Response.addTask(currentTask);
-            } else if (userInput.contains("event")) {
-                int temp = userInput.indexOf('/') - 1;
-                Task currentTask = new Event(userInput.substring(6, temp),
-                        userInput.substring(temp + 4));
-                Response.addTask(currentTask);
-            } else {
-                System.out.println("Error\n");
+            try {
+                String userInput = sc.nextLine();
+                if (userInput.equals("bye")) {
+                    Response.bye();
+                    break;
+                }
+                startResponse(userInput);
+            } catch (IllegalArgumentException e) {
+                System.out.println("\nerror: not enough information to create task!\n");
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("\nerror: taskd does not exist!\n");
+            } catch (NullPointerException e) {
+                System.out.println("\nerror: there is nothing in the list!\n");
+            } catch (Exception e) {
+                System.out.println("\nerror: sorry, I don't understand :(\n");
             }
         }
     }
--- src/main/java/Response.java
+++ src/main/java/Response.java
@@ -1,44 +1,47 @@
+import java.util.ArrayList;
+
 public class Response {
     public static int numTasks = 0;
-    public static Task[] tasks = new Task[100]; // Array of tasks
-    public static String line = "---------------------------";
+    public static ArrayList<Task> tasks = new ArrayList<>();
+    //public static Task[] tasks = new Task[100]; // Array of tasks
+    //public static String line = "---------------------------";
 
-    public static void myPrint(String str) {
-        System.out.println(line + "\n" + str + "\n" + line + "\n");
-    }
+//    public static void myPrint(String str) {
+//        System.out.println(line + "\n" + str + "\n" + line + "\n");
+//    }
 
     public static void hello() {
-        myPrint("what's up? :)");
+        System.out.println("\nwhat's up? :)\n");
     }
 
     public static void bye() {
-        myPrint("bye, have a good day! :)");
+        System.out.println("\nbye, have a good day! :)\n");
     }
 
     public static void listTasks() {
-        System.out.println(line);
+        System.out.println("");
 
         for (int i = 1; i < numTasks + 1; i++) {
-            System.out.println(i + ". " + tasks[i - 1].toString());
+            System.out.println(i + ". " + tasks.get(i - 1).toString());
         }
 
-        System.out.println(line + "\n");
+        System.out.println("");
     }
 
     public static void addTask(Task task) {
-        System.out.println(line + "\nokay, added this task:");
-        tasks[numTasks] = task;
+        System.out.println("\nokay, added this task:");
+        tasks.add(numTasks, task); // [numTasks] = task;
         numTasks++;
         System.out.println(task.toString());
-        System.out.println("you now have a total of " + numTasks + " tasks.");
-        System.out.println(line + "\n");
+        System.out.println("you now have a total of " + numTasks + " tasks.\n");
+        //System.out.println(line + "\n");
     }
 
     public static void markAsDone(int num) {
-        System.out.println(line + "\nokay, this task has been marked as done:");
-        Task currentTask = tasks[num - 1];
+        System.out.println("\nokay, this task has been marked as done:");
+        Task currentTask = tasks.get(num - 1);
         currentTask.markAsDone();
-        System.out.println(num + ". " + currentTask.toString());
-        System.out.println(line + "\n");
+        System.out.println(num + ". " + currentTask.toString() + "\n");
+        //System.out.println("\n");
     }
 }
