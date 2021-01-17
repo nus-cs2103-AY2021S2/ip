8051e6d0a278da965a74194fccd874052002f81a
Figo Lee Ze Xin <e0420458@u.nus.edu>
2021-01-17 22:00:39 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,18 +3,42 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello I'm Duke\n" + "What can I do for you\n" + logo);
+        ArrayList<String> col = new ArrayList<>(100);
+        int i = 1;
 
-        String next = sc.next();
+        System.out.println("----------------------------------------------");
+        System.out.println("Hello I'm Duke\n" + "What can I do for you?");
+        System.out.println("----------------------------------------------");
+
+        String next = sc.nextLine();
         while(!next.equals("bye")) {
-            System.out.println(next);
-            next = sc.next();
+            col.add(next);
+            System.out.println("----------------------------------------------");
+            System.out.println("added: " + next);
+            System.out.println("----------------------------------------------");
+            next = sc.nextLine();
+            while (next.equals("list")) {
+                System.out.println("----------------------------------------------");
+                Object[] strarr = col.toArray();
+                for (Object s : strarr) {
+                    System.out.println(i + ". " + s);
+                    i++;
+                }
+                System.out.println("----------------------------------------------");
+                i = 1;
+                next = sc.nextLine();
+            }
         }
+        System.out.println("----------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("----------------------------------------------");
     }
 }
+
+
+
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
