b8a389637f153215f3e1be644d320fae3c97cf52
Vinleon <kohvinleon@gmail.com>
2021-01-17 20:25:44 +0800

    Duke class: add mark as done methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    private static List<String> stringList;
+    private static List<Task> taskList;
     private static void printHorizontalLine() {
         System.out.println("____________________________________________________________");
     }
@@ -15,25 +15,38 @@ public class Duke {
         printHorizontalLine();
     }
 
-    private static void printList() {
-        for (int i = 0; i < stringList.size(); i++) {
-            System.out.printf("%d. %s%n", i+1, stringList.get(i));
-        }
+    private static void addCommand(String command) {
+        Task newTask = new Task(command);
+        System.out.printf("added: %s%n", command);
+        taskList.add(newTask);
     }
 
-    private static void addItem(String command) {
-        System.out.printf("added: %s%n", command);
-        stringList.add(command);
+    private static void printCommand() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.printf("%d.%s%n", i + 1, taskList.get(i).toString());
+        }
     }
 
     private static void echoCommand(String command) {
         printHorizontalLine();
-        if(command.equals("list")) {
-            printList();
+        if (command.equals("list")) {
+            printCommand();
+        } else if (command.contains("done")) {
+            doneCommand(command);
         } else {
-            addItem(command);
+            addCommand(command);
         }
         printHorizontalLine();
+    }
+
+    private static void doneCommand(String command) {
+        System.out.println("Nice! I've marked this task as done: ");
+        String[] inputs = command.split(" ");
+        int itemPosition = Integer.parseInt(inputs[1]) - 1;
+        Task selectedTask = taskList.get(itemPosition);
+        selectedTask.setDone();
+        System.out.println("   " + selectedTask.toString());
     }
 
     private static void exitCommand() {
@@ -44,7 +57,7 @@ public class Duke {
 
     public static void main(String[] args) {
         greetUser();
-        stringList = new ArrayList<>();
+        taskList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         String command;
         while (!(command = sc.nextLine()).equals("bye")) {
