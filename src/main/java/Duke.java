70385f0b050782c7d2d2f948736c0052f14cd6db
noelmathewisaac <noelmathewisaac@gmail.com>
2021-01-17 23:50:51 +0800

    Added functionality to add and list tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,38 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("**************************\nHey, I am Duke.\nHow can I help you?\n" +
+        System.out.println("**************************\nHey, " +
+                "I am Duke.\nHow can I help you?\n" +
                 "**************************");
 
-        Scanner sc= new Scanner(System.in);
-        String command = sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        ArrayList<String> tasks = new ArrayList<>();
 
-        while(!command.equals("bye")){
-            System.out.println("   >>> " + command);
-            command = sc.nextLine();
+        while (!input.equals("bye")) {
+
+            if (input.equals("list")) {
+                System.out.println("**************************");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(i + 1 + ". " + tasks.get(i));
+
+                }
+                System.out.println("**************************");
+
+            } else {
+                System.out.println("   >>> " + input);
+                tasks.add(input);
+            }
+            input = sc.nextLine();
+
         }
 
+
         System.out.println("**************************\n" +
-                       "Goodbye and see you soon!\n" +
-                       "**************************");
+                "Goodbye and see you soon!\n" +
+                "**************************");
 
     }
 }
