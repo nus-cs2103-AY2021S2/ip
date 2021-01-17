0c7db3e252430162f49f58c5a4a89bf0dfd25d13
Marcus Ong Jing Keat <markuz5116@gmail.com>
2021-01-17 12:46:04 +0800

    Add INDENT, NEWLINE, and ENDCOMMAND feature

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -3,9 +3,13 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Controller {
-    private final static String LINE = "\t__________________________________\n";
-    private final static String GREETING = "\t Hello! I'm Duke\n\t What can I do for you?\n";
-    private final static String ENDDUKE = "\t Bye. Hope to see you again soon!\n";
+    private final static String INDENT = "\t";
+    private final static String NEWLINE = System.lineSeparator();
+    private final static String LINE = INDENT + "__________________________________" + NEWLINE;
+    private final static String GREETING = INDENT + " Hello! I'm Duke\n\t What can I do for you?"
+            + NEWLINE;
+    private final static String ENDDUKE = INDENT + " Bye. Hope to see you again soon!" + NEWLINE;
+    private final static String ENDCOMMAND = "bye";
     private final List<Task> list;
 
     public Controller() {
@@ -18,7 +22,7 @@ public class Controller {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        while(!input.equals("bye")) {
+        while(!input.equals(ENDCOMMAND)) {
             System.out.print(LINE);
             handleInput(input);
             System.out.print(LINE);
@@ -42,7 +46,8 @@ public class Controller {
         int index = Integer.parseInt(input.substring(5)) - 1;
         Task task = list.get(index);
         task.done();
-        String output = String.format("\t Nice! I've marked this task as done:\n \t\t %s", task);
+        String output = String.format(INDENT + " Nice! I've marked this task as done:" + NEWLINE
+                + INDENT + INDENT + " %s", task);
         System.out.println(output);
     }
 
@@ -60,16 +65,17 @@ public class Controller {
             t = new Todo("");
         }
         list.add(t);
-        String output = String.format("\t Got it. I've added this task:\n\t\t %s\n\t "
-                + "Now you have %d tasks in the list.", t, list.size());
+        String output = String.format(INDENT + " Got it. I've added this task:" + NEWLINE + INDENT
+                + INDENT + " %s" + NEWLINE + INDENT + "Now you have %d tasks in the list."
+                , t, list.size());
         System.out.println(output);
     }
 
     private void printList() {
-        System.out.println("\t Here are the tasks in your list:");
+        System.out.println(INDENT + " Here are the tasks in your list:");
         int num = 1;
         for (Task task : list) {
-            String output = String.format("\t %d.%s", num, task);
+            String output = String.format(INDENT + " %d.%s", num, task);
             System.out.println(output);
             num++;
         }
