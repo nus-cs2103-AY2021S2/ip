894da1ed3c264d868ad3da4959aef34722a097c7
vvan-essa <vtsy2000@yahoo.com.sg>
2021-01-17 14:41:28 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * @author Damith C. Rajapakse, Jeffry Lum, Vanessa Tay
  */
 public class Duke {
-    public static ArrayList<String> userCommands = new ArrayList<>();
+    public static ArrayList<Task> taskList = new ArrayList<>();
 
     /**
      * Greets the user and begins listening to user commands
@@ -29,7 +29,7 @@ public class Duke {
 
     /**
      * Takes in user's command line arguments and treats them accordingly
-     * user inputs "bye", then Chatbot will end the session
+     * until user inputs "bye", then Chatbot will end the session
      */
     public static void listenToUserCommand() {
         Scanner sc = new Scanner(System.in);
@@ -40,6 +40,10 @@ public class Duke {
                 break;
             } else if (input.equals("list")) {
                 displayUserCommands();
+            } else if (input.startsWith("done ")) {
+                int taskNumber = Integer.parseInt(input.substring(5));
+                Task task = taskList.get(taskNumber-1);
+                markTaskDone(task);
             } else {
                 addUserCommand(input);
                 echoUserCommand(input);
@@ -63,22 +67,40 @@ public class Duke {
     }
 
     /**
-     * Chatbot adds the user's command-line input into an arraylist
+     * Chatbot creates new task with the user's command-line input
+     * and stores it in a list
      * @param input command-line input
      */
     public static void addUserCommand(String input) {
-        userCommands.add(input);
+       Task task = new Task(input);
+        taskList.add(task);
         System.out.print("added: ");
     }
 
     /**
-     * Chatbot prints out line-by-line all of the user's command-line
-     * inputs in that given session
+     * Chatbot prints out line-by-line all of the user's tasks
+     * stored in the list in that given session.
+     * Completed tasks will be marked with a tick and uncompleted
+     * ones with a cross.
      */
     public static void displayUserCommands() {
-        for(int i = 1; i <= userCommands.size(); i++) {
-            System.out.println(i + ". " + userCommands.get(i-1));
+        System.out.println("Here are your tasks!");
+        for(int i = 1; i <= taskList.size(); i++) {
+            Task task = taskList.get(i-1);
+            System.out.println(i + ". [" + task.getStatusIcon()
+                    + "]" + task.description);
         }
+        System.out.println();
+    }
+
+    /**
+     * Mark task as done and notify user of that change
+     * @param task supplied from the user's command-line input
+     */
+    public static void markTaskDone(Task task) {
+        task.isDone = true;
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("[" + task.getStatusIcon() + "]" + task.description);
         System.out.println();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,18 @@
+/**
+ * Represents a task that is created from the user's command line input
+ * @author Damith C. Rajapakse, Jeffry Lum
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+}
