6084ecc2fd89edb11cde7f72fdb43934c01d0561
Lee Han Bin <hanbin55@hotmail.com>
2021-01-18 13:46:27 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,22 +2,30 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
+        List<String> taskList= new ArrayList<String>(100);
         Scanner sc= new Scanner(System.in);
-        Boolean continueProgram = true;
         String logo = "Hello! I'm Duke\n"
                 + "What can I do for you?\n";
         System.out.println(logo);
 
 
         while (sc.hasNext()) {
-            String command = sc.next();
+            String command = sc.nextLine();
             if (command.equals("bye")){
                 break;
             } else {
-                System.out.println(
-                        "____________________________________________________________\n"
-                                + command
-                                + "\n____________________________________________________________");
+                if(command.equals("list")){
+                    System.out.println("____________________________________________________________");
+                    for(int i=1;i<=taskList.size();i++){
+                        System.out.println(i+". "+taskList.get(i-1));
+                    }
+                    System.out.println("____________________________________________________________");
+                }
+                else{
+                    taskList.add(command);
+                    System.out.println("____________________________________________________________\n"+
+                            "added: "+ command+"\n____________________________________________________________");
+                }
             }
         }
         System.out.println("____________________________________________________________\n"
