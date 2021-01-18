e3148a28f9f432185153f39c71ead4d374530264
shaelynl <shaelyn.lamxy@gmail.com>
2021-01-18 23:02:14 +0800

    added Level 2

--- src/main/java/Level1.java
+++ src/main/java/Level1.java
@@ -9,19 +9,29 @@ public class Level1 {
         System.out.println("     What can I do for you?");
         System.out.println("    _________________________________________________");
 
+        String[] tasksArray = new String[100];
+        int count = 0;
+
         while (sc.hasNext()) {
-            String input = sc.next();
+            String input = sc.nextLine();
 
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("    _________________________________________________");
                 System.out.println("     Bye. Hope to see you again soon! :)");
                 System.out.println("    _________________________________________________");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println("    _________________________________________________");
+                for (int i = 0; i < count; i++) {
+                    System.out.println(i+1 + ". " + tasksArray[i]);
+                }
+                System.out.println("    _________________________________________________");
             } else {
+                tasksArray[count] = input;
                 System.out.println("    _________________________________________________");
-                System.out.println("     " + input);
+                System.out.println("     " + "added : " + input);
                 System.out.println("    _________________________________________________");
-                System.out.println();
+                count++;
             }
         }
     }
