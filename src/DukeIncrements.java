1842cb23c0608e923f3867cda393928efc7f0232
wei-yutong <weiyutongggg@gmail.com>
2021-01-18 23:44:52 +0800

    added Level-2: add, list

--- src/DukeIncrements.java
+++ src/DukeIncrements.java
@@ -8,18 +8,27 @@ public class DukeIncrements {
         System.out.println(line);
 
         Scanner sc = new Scanner(System.in);
-
+        int count = 0;
+        String[] arr = new String[100];
         while (sc.hasNext()) {
-            String echo = sc.nextLine();
-            if (echo.equalsIgnoreCase("bye")) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
                 System.out.println(line);
                 System.out.println("sayonara nerd! c ya soon ;)");
                 System.out.println(line);
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println(line);
+                for (int i = 0; i < count; i++) {
+                    System.out.println(i + 1 + ". " + arr[i]);
+                }
+                System.out.println(line);
             } else {
                 System.out.println(line);
-                System.out.println(echo);
+                System.out.println("added: " + input);
                 System.out.println(line);
+                arr[count] = input;
+                count++;
+                }
             }
         }
-    }
 }
