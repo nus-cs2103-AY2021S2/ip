47bbde0b2b84e09cb8fdd644579b06fb414d7027
Swa Yong Shen <=>
2021-01-18 10:49:47 +0800

    Added level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,43 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public Duke() {
+        greet();
+    }
+
+    private void printWithStyle(String output) {
+        System.out.println("    ________________________________________________________________");
+        System.out.println("    " + output);
+        System.out.println("    ________________________________________________________________");
+    }
+    void greet() {
+        printWithStyle("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    void echo(String userInput) {
+        printWithStyle(userInput);
+    }
+
+    void bye() {
+        printWithStyle("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        Duke duke = new Duke();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            duke.echo(input);
+            input = scanner.nextLine();
+        }
+        duke.bye();
+
     }
 }
