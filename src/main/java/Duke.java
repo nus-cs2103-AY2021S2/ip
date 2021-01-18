14f121855fb6253b5007cfbc960587ae9b94c1eb
Keith Low <keithlow98@gmail.com>
2021-01-18 23:36:09 +0800

    Added a List to store the string inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> storageList = new ArrayList<>();
+        int listCounter = 1;
         String name = "Link";
         String userInput;
         System.out.println("Hello! I'm " + name);
@@ -9,8 +12,18 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         userInput = sc.nextLine();
         while (!userInput.equals("bye")) {
-            System.out.println(userInput);
-            userInput = sc.nextLine();
+            if (userInput.equals("list")) {
+                while (storageList.size() != listCounter) {
+                    System.out.println(listCounter + ". " + storageList.get(listCounter - 1));
+                    listCounter++;
+                }
+                System.out.println(listCounter + ". " + storageList.get(listCounter - 1));
+                userInput = sc.nextLine();
+            } else {
+                storageList.add(userInput);
+                System.out.println("added: " + userInput);
+                userInput = sc.nextLine();
+            }
         }
         System.out.println("Bye. Talk to me anytime!");
     }
