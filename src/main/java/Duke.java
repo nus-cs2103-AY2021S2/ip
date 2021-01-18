27ebbeef4299e51a2847b6d9709f63b755a3e45e
Assyarul <muhdassyarul@gmail.com>
2021-01-18 15:41:18 +0800

    Level 2 initial

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
+    static ArrayList<Task> list = new ArrayList<Task>();
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -18,8 +21,12 @@ public class Duke {
             if (input.matches("bye")) {
                 hasExited = true;
                 printWithBorders("Bye. Hope to see you again soon");
-            } else {
-                printWithBorders(input);
+            } else if (input.matches("list")) {
+                printWithBorders(listToString());
+            }
+            else {
+                addTask(input);
+                printWithBorders("added: "+input);
             }
 
         }
@@ -29,6 +36,20 @@ public class Duke {
         System.out.println("-".repeat(25));
         System.out.println(string);
         System.out.println("-".repeat(25));
+    }
+    private static String listToString() {
+        String content="";
+        Integer count = 1;
+        for(Task t:list) {
+            content += count.toString() + ".";
+            content += t.getContent();
+            content += "\n";
+            count++;
+        }
+        return content;
+    }
+    private static void addTask(String content) {
+        list.add(new Task(content));
     }
 
 
