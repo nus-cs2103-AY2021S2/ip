37a2d05e3fe7c6e349748de0a3a8caf9d023a648
Charles Lee Lin Ta <e0484321@u.nus.edu>
2021-01-18 23:34:48 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,60 +2,94 @@ import java.util.*;
 
 public class Duke {
 
-    static Task[] tasks = new Task[100];
-    static int numTask = 0;
+    static List<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         greet();
         while (sc.hasNextLine()) {
             String s = sc.nextLine();
-            if (s.equals("bye")) {
+            String key = getKeyword(s);
+            if (key.equals("bye")) {
                 bye();
                 break;
+            } else if (key.equals("list")) {
+                list();
+            } else if (key.equals("done")) {
+                done(Integer.parseInt(s.substring(5)));
+            } else if (key.equals("todo") || key.equals("event") || key.equals("deadline")) {
+                addTask(getTask(s, key));
             }
-            if (s.length() >= 6) {
-                if (s.substring(0, 4).equals("done")) {
-                    int num = Integer.parseInt(s.substring(5));
-                    done(num);
-                    continue;
-                }
-            }
-            store(s);
         }
     }
 
+    static public Task getTask(String s, String key) {
+        String desc = "";
+        if (key.equals("todo")) {
+            return new ToDo(s.substring(5));
+        }
+        int keyLen = key.length();
+        for (int i = keyLen + 1; i < s.length(); i++) {
+            if (s.charAt(i) == ' ' && s.charAt(i + 1) == '/') {
+                break;
+            }
+            desc += s.charAt(i);
+        }
+        String time = s.substring(keyLen + desc.length() + 6);
+        if (key.equals("event")) {
+            return new Event(desc, time);
+        } else {
+            return new Deadline(desc, time);
+        }
+    }
+
+    static public void list() {
+        String s = "";
+        if (tasks.size() == 0) {
+            System.out.println("    ____________________________________________________________\n     " +
+                    "Here are the tasks in your list:\n    " +
+                    "____________________________________________________________\n");
+            return;
+        }
+        for (int i = 0; i < tasks.size(); i++) {
+            s += (i + 1) + "." + tasks.get(i) + "\n";
+            if (i != tasks.size() - 1) {
+                s += "          ";
+            }
+        }
+        System.out.println("    ____________________________________________________________\n     " +
+                "     Here are the tasks in your list:\n          " + s +
+                "    ____________________________________________________________\n");
+    }
+
+    static public void addTask(Task task) {
+        tasks.add(task);
+        System.out.println("    ____________________________________________________________\n" +
+                "     Got it. I've added this task: \n" +
+                "       " + task + "\n" +
+                "     Now you have " + tasks.size() + " tasks in the list.\n" +
+                "    ____________________________________________________________\n");
+    }
+
+    static public String getKeyword(String s) {
+        String str = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                str += s.charAt(i);
+            } else {
+                break;
+            }
+        }
+        return str;
+    }
+
     static public void done(int num) {
-        Task task = tasks[num - 1];
+        Task task = tasks.get(num - 1);
         task.markAsDone();
         System.out.println("    ____________________________________________________________\n" +
                 "     Nice! I've marked this task as done: \n" +
                 "       " + task + "\n" +
                 "    ____________________________________________________________\n");
-    }
-
-    static public void store(String str) {
-        if (str.equals("list")) {
-            String printList = "";
-            int idx = 1;
-            for (int i = 0; i < numTask; i++) {
-                Task task = tasks[idx - 1];
-                printList += idx + "." + task + "\n    ";
-                if (i != numTask - 1) {
-                    printList += " ";
-                }
-                idx++;
-            }
-            System.out.println("    ____________________________________________________________\n     " +
-                    printList +
-                    "____________________________________________________________\n");
-        } else {
-            tasks[numTask] = new Task(str);
-            numTask++;
-            System.out.println("    ____________________________________________________________\n     " +
-                    "added: " + str + "\n" +
-                    "    ____________________________________________________________\n");
-        }
     }
 
     static public void bye() {
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -19,4 +19,5 @@ public class Task {
     public String toString() {
         return "[" + this.getStatusIcon() + "] " + this.description;
     }
+
 }
