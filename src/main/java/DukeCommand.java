a1052eaee48f8d35ca1230de8e1eefc94d8050b4
Vinleon <kohvinleon@gmail.com>
2021-01-19 01:21:05 +0800

    Add DukeCommand.java and DukeException.java

---
+++ src/main/java/DukeCommand.java
@@ -0,0 +1,86 @@
+public enum DukeCommand {
+    DEADLINE {
+        @Override
+        public void runCommand(String actions) throws DukeException {
+            if (actions.isEmpty() || actions.isBlank()) {
+                throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+            }
+            if (!actions.contains("/by")) {
+                throw new DukeException("☹ OOPS!!! You have entered an invalid format.");
+            }
+
+            String[] inputs = actions.split("/by");
+            if (inputs.length < 2) {
+                throw new DukeException("☹ OOPS!!! You have entered an invalid format.");
+            }
+
+            Deadline newDeadline = new Deadline(inputs[0].trim(), inputs[1].trim());
+            Duke.addCommand(newDeadline);
+        }
+    },
+    TODO {
+        @Override
+        public void runCommand(String actions) throws DukeException {
+            if (actions.isEmpty() || actions.isBlank()) {
+                throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+            }
+
+            ToDo newToDo = new ToDo(actions);
+            Duke.addCommand(newToDo);
+        }
+    },
+    EVENT {
+        @Override
+        public void runCommand(String actions) throws DukeException {
+            if (actions.isEmpty() || actions.isBlank()) {
+                throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+            }
+            if (!actions.contains("/at")) {
+                throw new DukeException("☹ OOPS!!! You have entered an invalid format.");
+            }
+
+            String[] inputs = actions.split("/at");
+            if (inputs.length < 2) {
+                throw new DukeException("☹ OOPS!!! You have entered an invalid format.");
+            }
+
+            Event newEvent = new Event(inputs[0].trim(), inputs[1].trim());
+            Duke.addCommand(newEvent);
+        }
+    },
+    LIST {
+        @Override
+        public void runCommand(String actions) throws DukeException {
+            int listSize = Duke.taskList.size();
+            if (listSize <= 0) {
+                throw new DukeException("☹ OOPS!!! Your task list is empty.");
+            }
+
+            Duke.listCommand();
+        }
+    },
+    DONE {
+        @Override
+        public void runCommand(String actions) throws DukeException {
+            int index = Integer.parseInt(actions) - 1;
+            int listSize = Duke.taskList.size();
+
+            if (index < 0 || index >= listSize) {
+                throw new DukeException("☹ OOPS!!! The number you entered is invalid.");
+            }
+
+            Duke.doneCommand(index);
+        }
+    };
+
+    public abstract void runCommand(String actions) throws DukeException;
+
+    public static boolean isContains(String value) {
+        for (DukeCommand cmd : values()) {
+            if (cmd.name().equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
