98e47a43a6dcf52263a68cf2da2e01e232d9edb3
Azeem Vasanwala <azeem.iron@gmail.com>
2021-01-18 23:41:59 +0800

    Implement A-TextUiTesting for Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -26,7 +26,7 @@ public class Duke {
             case "done": {
                 int taskIdx = Integer.parseInt(input.split(" ")[1]) - 1;
                 tasks.get(taskIdx).setDone();
-                echo(String.format("Nice! This task is done :) \n%s", tasks.get(taskIdx).toString()));
+                echo(String.format("Nice! This task is done :)\n%s", tasks.get(taskIdx).toString()));
                 break;
             }
             case "todo": {
@@ -61,14 +61,14 @@ public class Duke {
     }
 
     public static void greet() {
-        String logo = "______                    _       _           _     \n" +
-                "|  ___|                  (_)     | |         | |    \n" +
-                "| |_ _ __ __ _ _ __   ___ _ ___  | |     ___ | |__  \n" +
-                "|  _| '__/ _` | '_ \\ / __| / __| | |    / _ \\| '_ \\ \n" +
+        String logo = "______                    _       _           _\n" +
+                "|  ___|                  (_)     | |         | |\n" +
+                "| |_ _ __ __ _ _ __   ___ _ ___  | |     ___ | |__\n" +
+                "|  _| '__/ _` | '_ \\ / __| / __| | |    / _ \\| '_ \\\n" +
                 "| | | | | (_| | | | | (__| \\__ \\ | |___| (_) | | | |\n" +
                 "\\_| |_|  \\__,_|_| |_|\\___|_|___/ \\_____/\\___/|_| |_|\n" +
-                "                                                    \n" +
-                "                                                    \n";
+                "\n" +
+                "\n";
         System.out.println("Hello from\n" + logo + "(a.k.a Loh Jing Yen)");
         System.out.println("What can I do for you?");
         System.out.println("Enter a command below for me to assist you");
