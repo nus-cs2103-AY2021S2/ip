29c3fd86fce06856e14853828fa4167d610e9131
Keith Low <keithlow98@gmail.com>
2021-01-18 23:15:16 +0800

    Customized chatbot words

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "Link";
+        String userInput;
+        System.out.println("Hello! I'm " + name);
+        System.out.println("How are you feeling today?");
+        Scanner sc = new Scanner(System.in);
+        userInput = sc.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = sc.nextLine();
+        }
+        System.out.println("Bye. Talk to me anytime!");
     }
 }
