80921802e86628977670fb4954baf11f8d271956
candyhy <candiceyhy@gmail.com>
2021-01-18 16:46:26 +0800

    Add greet, echo, exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,42 @@
+import java.io.*;
+
 public class Duke {
-    public static void main(String[] args) {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+    public static void main(String[] args) throws IOException{
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
+
+        while(true) {
+            String string = br.readLine();
+            if(!string.equals("bye")) {
+                echo(string);
+            } else {
+                exit();
+                break;
+            }
+        }
+        pw.close();
+    }
+
+    public static void greet() {
+        pw.println("Hello! I'm Duke");
+        pw.println("What can I do for you?");
+        pw.flush();
+    }
+
+    public static void echo(String string) {
+        pw.println(string);
+        pw.flush();
+    }
+
+    public static void exit() {
+        pw.println("Bye. Hope to see you again soon!");
     }
 }
