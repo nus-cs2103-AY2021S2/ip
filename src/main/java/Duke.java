232d8454d55185e33362a86f461157b9d56d16a6
OhJunMing <ohjunming1998@gmail.com>
2021-01-18 22:15:21 +0800

    L1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+    Duke(){
+
+    }
+    public static void Greet() {
+        System.out.println("---------------------------------------");
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println("---------------------------------------");
+    }
+
+    public static void echo(String msg) {
+        System.out.println("---------------------------------------");
+        System.out.println(msg);
+        System.out.println("---------------------------------------");
+    }
+
+    public static void exit() {
+        System.out.println("---------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("---------------------------------------");
     }
 }
