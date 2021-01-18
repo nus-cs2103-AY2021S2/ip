8c7d1782986f6b96265648d610182e93ba8a9b7d
Charles Lee Lin Ta <e0484321@u.nus.edu>
2021-01-18 23:11:52 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        greet();
+        while (sc.hasNextLine()) {
+            String s = sc.nextLine();
+            if (s.equals("bye")) {
+                bye();
+                break;
+            }
+            command(s);
+        }
+    }
+
+    static public void command(String str) {
+        System.out.println("    ____________________________________________________________\n     " +
+                str + "\n" +
+                "    ____________________________________________________________\n");
+    }
+
+    static public void bye() {
+        System.out.println("    ____________________________________________________________\n     " +
+                "Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________\n");
+    }
+
+    static public void greet() {
+        System.out.println("    ____________________________________________________________\n     " +
+                " ____        _        \n" +
+                "     |  _ \\ _   _| | _____ \n" +
+                "     | | | | | | | |/ / _ \\\n" +
+                "     | |_| | |_| |   <  __/\n" +
+                "     |____/ \\__,_|_|\\_\\___|\n\n     " +
+                "Hello! I'm Duke\n     " +
+                "What can I do for you?\n" +
+                "    ____________________________________________________________\n");
     }
 }
