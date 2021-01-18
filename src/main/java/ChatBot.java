b1ab09afe4615b349017e883f7197ee48624508a
david <dvdweien@gmail.com>
2021-01-18 10:18:49 +0800

    Level 2. Add, List
    Add the ability to store whatever text entered by the user and
    display them back to the user when requested.

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,18 +1,43 @@
+import java.util.ArrayList;
+
 class ChatBot {
+
+	private final ArrayList<String> mem;
 
 	ChatBot() {
 		System.out.println("    ____________________________________________________________");
 		System.out.println("");
-		System.out.println("    Hello! I'm Duke");
-		System.out.println("    What can I do for you?");
+		System.out.println("      Hello! I'm Duke");
+		System.out.println("      What can I do for you?");
+		System.out.println("    ____________________________________________________________");
+		System.out.println("");
+		this.mem = new ArrayList<String>();
+	}
+
+	void parser(String s) {
+		if (s.equals("list")) {
+			this.list();
+		} else {
+			this.store(s);
+		}
+	}
+
+	void store(String s) {
+		this.mem.add(s);
+		System.out.println("    ____________________________________________________________");
+		System.out.println("");
+		System.out.println("      added:  " + s);
 		System.out.println("    ____________________________________________________________");
 		System.out.println("");
 	}
 
-	void parser(String s) {
+	void list() {
 		System.out.println("    ____________________________________________________________");
 		System.out.println("");
-		System.out.println("    " + s);
+		for (int i = 1; i <= this.mem.size(); i += 1) {
+			System.out.println("      " + i + ".  " + this.mem.get(i - 1));
+		}
+		
 		System.out.println("    ____________________________________________________________");
 		System.out.println("");
 	}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,7 +23,7 @@ public class Duke {
 
         System.out.println("    ____________________________________________________________");
 		System.out.println("");
-		System.out.println("    Bye. Hope to see you again soon!");
+		System.out.println("      Bye. Hope to see you again soon!");
 		System.out.println("    ____________________________________________________________");
 		System.out.println("");
     }
