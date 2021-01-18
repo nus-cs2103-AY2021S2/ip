8a3ca6d1acf4f84866d3b82cf7505e5c98d925cb
Lim Li Quan <linliquan1996@Hotmail.com>
2021-01-18 16:16:24 +0800

    Duke.java : Added feature to mark task as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Duke {
-    private static ArrayList<String> command_history = new ArrayList<>();
+    private static ArrayList<Task> taskList = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         //Greet User
@@ -11,29 +11,58 @@ public class Duke {
         String command = scanner.nextLine();
         while (!command.equalsIgnoreCase("bye")) {
             printLine();
-            switch (command) {
-                case "list" :
-                    //Display history of commands
-                    listCommands();
+            // Split command to check if first word is done
+            // and also to extract the option
+            String[] commandArr = command.split(" ");
+            switch (commandArr[0]) {
+                case "list":
+                    //Display all task added
+                    listTasks();
+                    break;
+                case "done":
+                    int option = Integer.parseInt(commandArr[commandArr.length - 1]) - 1;
+                    //Mark task of choice as done
+                    completeTask(option);
                     break;
                 default:
-                    //Adding commands into history for listing later
-                    command_history.add(command);
-                    //Echo commands
+                    //Adding command as task into taskList
+                    taskList.add(new Task(command));
+                    //Echo command
                     System.out.printf("added: %s\n", command);
                     break;
             }
             printLine();
-            command = scanner.nextLine();
+            command = scanner.nextLine().toLowerCase();
         }  //Exits the program
         printLine();
         System.out.println("Bye. Hope to see you again soon!");
         printLine();
     }
 
-    public static void listCommands() {
-        for(int i = 0; i < command_history.size(); i++) {
-            System.out.printf("%d. %s\n", (i+1) , command_history.get(i));
+    public static void completeTask(int option) {
+        boolean res = false;
+        String message = "Invalid option";
+        // Option validation (More than 0 and not more than the number of task)
+        if (option >= 0 && option < taskList.size()) {
+            res = taskList.get(option).markAsDone();
+            message = String.format("Task %d is already been marked as done", option + 1);
+        }
+
+        if (res) {
+            System.out.println("Nice! I`ve marked this task as done:");
+            System.out.println(taskList.get(option));
+        } else {
+            System.out.println(message);
+        }
+    }
+
+    public static void printTask(String numbering, Task task) {
+        System.out.printf("%s %s\n", numbering , task);
+    }
+
+    public static void listTasks() {
+        for(int i = 0; i < taskList.size(); i++) {
+            System.out.printf("%d. %s\n", (i+1) , taskList.get(i));
         }
     }
 
