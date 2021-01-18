9e3e5b185f93fee017a0f429bdf39c45cb27df72
Lee Han Bin <hanbin55@hotmail.com>
2021-01-18 16:15:15 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
-        List<String> taskList= new ArrayList<String>(100);
+        List<Task> taskList= new ArrayList<Task>(100);
         Scanner sc= new Scanner(System.in);
         String logo = "Hello! I'm Duke\n"
                 + "What can I do for you?\n";
@@ -10,21 +10,33 @@ public class Duke {
 
 
         while (sc.hasNext()) {
-            String command = sc.nextLine();
-            if (command.equals("bye")){
+            String line = sc.nextLine();
+            String[] command = line.split(" ");
+            if (command[0].equals("bye")){
                 break;
             } else {
-                if(command.equals("list")){
-                    System.out.println("____________________________________________________________");
+                if(command[0].equals("list")){
+                    System.out.println("____________________________________________________________\n"+
+                            "Here are the tasks in your list");
                     for(int i=1;i<=taskList.size();i++){
-                        System.out.println(i+". "+taskList.get(i-1));
+                        Task curTask = taskList.get(i-1);
+                        System.out.println(i+". ["+curTask.getStatusIcon()+"] "+curTask.getDescription());
                     }
                     System.out.println("____________________________________________________________");
+                } else if(command[0].equals("done")){
+                    int index = Integer.valueOf(command[1])-1;
+                    Task curTask = taskList.get(index);
+                    curTask.markAsDone();
+                    System.out.println("____________________________________________________________\n"+
+                            "Nice! I've marked this task as done:\n"+index+". ["+curTask.getStatusIcon()+"] "+curTask.getDescription()+
+                            "\n____________________________________________________________");
+
                 }
                 else{
-                    taskList.add(command);
+                    Task newTask = new Task(line);
+                    taskList.add(newTask);
                     System.out.println("____________________________________________________________\n"+
-                            "added: "+ command+"\n____________________________________________________________");
+                            "added: "+ line +"\n____________________________________________________________");
                 }
             }
         }
@@ -34,4 +46,26 @@ public class Duke {
 
 
     }
+}
+class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2718" : " "); //return tick or X symbols
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void markAsDone(){
+        this.isDone = true;
+    }
+
 }
