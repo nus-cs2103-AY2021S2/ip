d38e1e1a15144f197daa524470b8ce7a3fd9d3ae
yungweezy <choonweiw@gmail.com>
2021-01-19 01:33:31 +0800

    Add emoji and color enums

--- src/main/java/misc/Color.java
+++ src/main/java/misc/Color.java
@@ -4,73 +4,22 @@ package misc;
  * The enum Color.
  */
 public enum Color {
-    /**
-     * Reset color.
-     */
     RESET("\033[0m"),
-    /**
-     * Black color.
-     */
     BLACK("\033[0;30m"),
-    /**
-     * Red color.
-     */
     RED("\033[0;31m"),
-    /**
-     * Green color.
-     */
     GREEN("\033[0;32m"),
-    /**
-     * Yellow color.
-     */
     YELLOW("\033[0;33m"),
-    /**
-     * Blue color.
-     */
     BLUE("\033[0;34m"),
-    /**
-     * Magenta color.
-     */
     MAGENTA("\033[0;35m"),
-    /**
-     * Cyan color.
-     */
     CYAN("\033[0;36m"),
-    /**
-     * White color.
-     */
     WHITE("\033[0;37m"),
-    /**
-     * Black bold color.
-     */
     BLACK_BOLD("\033[1;30m"),
-    /**
-     * Red bold color.
-     */
     RED_BOLD("\033[1;31m"),
-    /**
-     * Green bold color.
-     */
     GREEN_BOLD("\033[1;32m"),
-    /**
-     * Yellow bold color.
-     */
     YELLOW_BOLD("\033[1;33m"),
-    /**
-     * Blue bold color.
-     */
     BLUE_BOLD("\033[1;34m"),
-    /**
-     * Magenta bold color.
-     */
     MAGENTA_BOLD("\033[1;35m"),
-    /**
-     * Cyan bold color.
-     */
     CYAN_BOLD("\033[1;36m"),
-    /**
-     * White bold color.
-     */
     WHITE_BOLD("\033[1;37m");
 
     /**
