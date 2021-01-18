26141e2a05680fa39db3bea743999ab7f69c1fb7
david <dvdweien@gmail.com>
2021-01-18 08:34:32 +0800

    Level 1. Greet, Echo, Exit

---
+++ src/main/java/ChatBot.java
@@ -0,0 +1,19 @@
+class ChatBot {
+
+	ChatBot() {
+		System.out.println("    ____________________________________________________________");
+		System.out.println("");
+		System.out.println("    Hello! I'm Duke");
+		System.out.println("    What can I do for you?");
+		System.out.println("    ____________________________________________________________");
+		System.out.println("");
+	}
+
+	void parser(String s) {
+		System.out.println("    ____________________________________________________________");
+		System.out.println("");
+		System.out.println("    " + s);
+		System.out.println("    ____________________________________________________________");
+		System.out.println("");
+	}
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+    	/*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        */
+        ChatBot c = new ChatBot();
+
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        while(!input.equals("bye")) {
+        	c.parser(input);
+        	input = sc.nextLine();
+        }
+
+        System.out.println("    ____________________________________________________________");
+		System.out.println("");
+		System.out.println("    Bye. Hope to see you again soon!");
+		System.out.println("    ____________________________________________________________");
+		System.out.println("");
     }
 }
