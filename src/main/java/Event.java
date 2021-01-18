7c390fbf862bd9807d2b2a8f9bc1c1da9b003807
Swa Yong Shen <=>
2021-01-18 16:36:26 +0800

    Level-5

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -8,6 +8,6 @@ public class Event extends Task{
 
     @Override
     public String toString() {
-        return "[E] " + super.toString() + "(at: " + this.date + ")";
+        return "[E] " + super.toString() + " (at: " + this.date + ")";
     }
 }
