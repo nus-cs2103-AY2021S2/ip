5b226b1bcf56707d57efd1b99d60d96189238111
CSmortal <jovenpua51@gmail.com>
2021-01-18 17:46:11 +0800

    added Event.java

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,25 @@
-package main.java;public class Event {
+package main.java;
+
+public class Event extends Task {
+    private String timeRange;
+
+    public Event(String description, String timeRange) {
+        super(description);
+        this.timeRange = timeRange;
+    }
+
+    public Event(String description, String timeRange, boolean isDone) {
+        super(description, isDone);
+        this.timeRange = timeRange;
+    }
+
+    @Override
+    public Event markAsDone() {
+        return new Event(description, timeRange, true);
+    }
+
+    @Override
+    public String toString() {
+        return "[E][" + getStatusIcon() + "] " + description + " (at: " + timeRange + ")";
+    }
 }
