8e5452b2723d6dd7c167a853772cbff555ead9c9
JayChenYJ <jaychenyj@gmail.com>
2021-01-18 23:46:01 +0800

    Fixed a serious bug relating to task count increment and task count display. Level-5 completed.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
  * The Duke project
  */
 public class Duke {
+    static int totalTasks = 0;
+
     public static void main(String[] args) {
         greet();
 
@@ -14,7 +16,7 @@ public class Duke {
 
         boolean endOfCycle = false;
         Task[] tasks = new Task[100];
-        int count = 0;
+//        int count = 0;
 
         while(!endOfCycle) {
             System.out.print(username + ": ");
@@ -25,18 +27,15 @@ public class Duke {
                     bye(username);
                     endOfCycle = true;
                 } else if (nextCommand.equals("list")) {
-                    list(tasks, count);
+                    list(tasks, totalTasks);
                 } else if (commandToWords[0].equals("done")) {
-                    done(nextCommand, tasks, count);
+                    done(nextCommand, tasks, totalTasks);
                 } else if (commandToWords[0].equals("todo")) {
-                    todo(nextCommand, tasks, count);
-                    count++;
+                    todo(nextCommand, tasks);
                 } else if (commandToWords[0].equals("deadline")) {
-                    deadline(nextCommand, tasks, count);
-                    count++;
+                    deadline(nextCommand, tasks);
                 } else if (commandToWords[0].equals("event")){
-                    event(nextCommand, tasks, count);
-                    count++;
+                    event(nextCommand, tasks);
                 } else {
                     wrongCommand();
                 }
@@ -46,6 +45,13 @@ public class Duke {
         }
 
         sc.close();
+    }
+
+    /**
+     * Increase the total number of tasks in the list by 1.
+     */
+    public static void taskAdded() {
+        totalTasks++;
     }
 
     /**
@@ -72,17 +78,18 @@ public class Duke {
      * Adds a Todo task.
      * @param nextCommand The description of the task.
      * @param tasks The Task array containing user tasks in sequence, up to 100.
-     * @param count The current number of tasks stored inside the Task array.
+     * @throws DukeException Exception thrown if the command given is invalid.
      */
-    public static void todo(String nextCommand, Task[] tasks, int count) throws DukeException{
+    public static void todo(String nextCommand, Task[] tasks) throws DukeException{
         if (nextCommand.length() < 6) {
             throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
         }
         String command = nextCommand.substring(5);
-        tasks[count] = new Todo(command);
+        tasks[totalTasks] = new Todo(command);
         System.out.println("----------------------------------------------------------------------------------------");
-        System.out.println("Got it. I've added this task: \n" + "    " + tasks[count].toString());
-        System.out.println("Now you have " + count + 1 + " tasks in the list.");
+        System.out.println("Got it. I've added this task: \n" + "    " + tasks[totalTasks].toString());
+        taskAdded();
+        System.out.println("Now you have " + totalTasks + " tasks in the list.");
         System.out.println("----------------------------------------------------------------------------------------");
     }
 
@@ -90,9 +97,9 @@ public class Duke {
      * Adds a Deadline task.
      * @param nextCommand The description of the task.
      * @param tasks The Task array containing user tasks in sequence, up to 100.
-     * @param count The current number of tasks stored inside the Task array.
+     * @throws DukeException Exception thrown if the command given is invalid.
      */
-    public static void deadline(String nextCommand, Task[] tasks, int count) throws DukeException{
+    public static void deadline(String nextCommand, Task[] tasks) throws DukeException{
         if (nextCommand.length() < 10) {
             throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
         } else if (!nextCommand.contains("/")){
@@ -100,10 +107,11 @@ public class Duke {
         }
         String command = nextCommand.substring(9, nextCommand.indexOf("/") - 1);
         String dateInfo = nextCommand.substring(nextCommand.indexOf("/") + 4);
-        tasks[count - 1] = new Deadline(command, dateInfo);
+        tasks[totalTasks] = new Deadline(command, dateInfo);
         System.out.println("----------------------------------------------------------------------------------------");
-        System.out.println("Got it. I've added this task: \n" + "    " + tasks[count - 1].toString());
-        System.out.println("Now you have " + count + " tasks in the list.");
+        System.out.println("Got it. I've added this task: \n" + "    " + tasks[totalTasks - 1].toString());
+        taskAdded();
+        System.out.println("Now you have " + totalTasks + " tasks in the list.");
         System.out.println("----------------------------------------------------------------------------------------");
     }
 
@@ -111,9 +119,9 @@ public class Duke {
      * Adds an Event task.
      * @param nextCommand The description of the task.
      * @param tasks The Task array containing user tasks in sequence, up to 100.
-     * @param count The current number of tasks stored inside the Task array.
+     * @throws DukeException Exception thrown if the command given is invalid.
      */
-    public static void event(String nextCommand, Task[] tasks, int count) throws DukeException{
+    public static void event(String nextCommand, Task[] tasks) throws DukeException{
         if (nextCommand.length() < 7) {
             throw new DukeException("OOPS!!! The description of an event cannot be empty.");
         } else if (!nextCommand.contains("/")) {
@@ -121,16 +129,17 @@ public class Duke {
         }
         String command = nextCommand.substring(6, nextCommand.indexOf("/") - 1);
         String dateInfo = nextCommand.substring(nextCommand.indexOf("/") + 4);
-        tasks[count - 1] = new Event(command, dateInfo);
+        tasks[totalTasks] = new Event(command, dateInfo);
         System.out.println("----------------------------------------------------------------------------------------");
-        System.out.println("Got it. I've added this task: \n" + "    " + tasks[count - 1].toString());
-        System.out.println("Now you have " + count + " tasks in the list.");
+        System.out.println("Got it. I've added this task: \n" + "    " + tasks[totalTasks - 1].toString());
+        taskAdded();
+        System.out.println("Now you have " + totalTasks + " tasks in the list.");
         System.out.println("----------------------------------------------------------------------------------------");
     }
 
     /**
      * List out all user inputs in sequence.
-     * @param tasks An array of tasks in sequence, up to 100.
+     * @param tasks The Task array containing user tasks in sequence, up to 100.
      * @param count The current number of tasks stored inside the Task array.
      */
     public static void list(Task[] tasks, int count) {
@@ -145,8 +154,10 @@ public class Duke {
 
     /**
      * Mark the task in the given task number as done.
-     * Tell the user that the task has been marked done.
-     * @param tasks An array of tasks in sequence, up to 100.
+     * @param command The command given by user input.
+     * @param tasks The Task array containing user tasks in sequence, up to 100.
+     * @param count The current number of tasks stored inside the Task array.
+     * @throws DukeException Exception thrown if the number given is out of range.
      */
     public static void done(String command, Task[] tasks, int count) throws DukeException{
         String[] commandToWords = command.split(" ");
@@ -160,6 +171,10 @@ public class Duke {
         System.out.println("----------------------------------------------------------------------------------------");
     }
 
+    /**
+     * Tells the user that the input given is invalid.
+     * @throws DukeException Exception thrown if the user input is invalid.
+     */
     public static void wrongCommand() throws  DukeException{
         throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
     }
