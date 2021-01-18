f144a0c58a2035a2b62509ff9c43b7bd221d330b
RuiXiong2211 <ruixiong8@hotmail.com>
2021-01-18 20:17:39 +0800

    -Added Exceptions - 1) unknown description 2) unknown command 3) done (x) - where x does not exist

--- src/main/java/Skeleton.java
+++ src/main/java/Skeleton.java
@@ -19,28 +19,36 @@ public class Skeleton {
         String rest = "";
         greet();
         while (true) {
-            cmd = sc.nextLine();
-            int i = cmd.indexOf(' ');
-            if (i >= 0) {
-                word = cmd.substring(0, i);
-                rest = cmd.substring(i);
+            try {
+                cmd = sc.nextLine();
+                int i = cmd.indexOf(' ');
+                if (i >= 0) {
+                    word = cmd.substring(0, i);
+                    rest = cmd.substring(i);
+                }
+                if (cmd.equals("bye")) {
+                    terminate();
+                } else if (cmd.equals("list")) {
+                    listItems();
+                } else if (word.equals("done")) {
+                    int value = Integer.parseInt(rest.replaceAll("[^0-9]", ""));
+                    done(value);
+                } else {
+                    add(word, rest);
+                }
             }
-            if (cmd.equals("bye")) {
-                terminate();
-            } else if (cmd.equals("list")) {
-                listItems();
-            } else if (word.equals("done")) {
-                int value = Integer.parseInt(rest.replaceAll("[^0-9]", ""));
-                done(value);
-            }  else {
-                add(word, rest);
+            catch (DukeException E) {
+                //System.out.println("Caught DukeException.");
+                System.out.println(E);
             }
         }
     }
 
     /** function that adds to a storage list the type of task the user has input. */
-    static void add(String type, String rest) {
-        System.out.println("Wagata. Mou added shi mashita.");
+    static void add(String type, String rest) throws DukeException {
+        if (type.equals("todo") && rest.equals("")) {
+            throw new DukeException("Description cannot be empty");
+        }
         if (type.equals("todo")) {
             storage.add(new Todo(rest));
         } else if (type.equals("deadline")) {
@@ -53,14 +61,20 @@ public class Skeleton {
             String by = rest.substring(i);
             rest = rest.substring(0, i - 3);
             storage.add(new Event(rest, by));
+        } else {
+            throw new DukeException("Command UNKNOWN");
         }
+        System.out.println("Wagata. Mou added shi mashita.");
         System.out.println(storage.get(current));
         System.out.println("Now you have " + storage.size() + " tasks in the list.");
         System.out.println();
         current++;
     }
 
-    static void done(int value) {
+    static void done(int value) throws DukeException {
+        if (value <= 0 || value > storage.size()) {
+            throw new DukeException("No such list item.");
+        }
         storage.get(value-1).markAsDone();
         System.out.println("YOSHA! KONNO TASK GA OWARIMASHITA! GANBARE");
         System.out.println(storage.get(value-1));
