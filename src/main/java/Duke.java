01ec46f6217c9038952ec3a653cd6a0fc7f2c80a
mechastriker3 <33394879+mechastriker3@users.noreply.github.com>
2021-01-19 00:06:40 +0800

    first commit!! added user input and echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(input);
+            }
+
+        }
     }
 }
