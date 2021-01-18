a2e53392a8712bebd8a7bbbfc167d71d8aa02306
LI <gabriellee0903@gmail.com>
2021-01-18 21:58:55 +0800

    Level 2 added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -16,12 +17,21 @@ public class Duke {
         System.out.println("What do you want?");
         System.out.println(partingLine);
 
+        ArrayList<String> items = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
         while (!input.equals("bye")) {
             System.out.println(partingLine);
-            System.out.println(" " + input);
+            if (input.equals("list")) {
+                for (int i = 1; i <= items.size(); i++) {
+                    System.out.println(" " + i + ". "
+                            + items.get(i - 1));
+                }
+            } else {
+                items.add(input);
+                System.out.println(" added: " + input);
+            }
             System.out.println(partingLine);
             input = sc.nextLine();
         }
