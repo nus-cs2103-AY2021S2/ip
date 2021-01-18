c9d341fa96e21ca9fb82d8d7adfd4d39a18b3a0a
wei-yutong <weiyutongggg@gmail.com>
2021-01-19 00:50:22 +0800

    added Level-3: mark as done

--- src/DukeIncrements.java
+++ src/DukeIncrements.java
@@ -9,26 +9,52 @@ public class DukeIncrements {
 
         Scanner sc = new Scanner(System.in);
         int count = 0;
-        String[] arr = new String[100];
+        Task[] arr = new Task[100];
         while (sc.hasNext()) {
             String input = sc.nextLine();
+            String[] str = input.split(" ");
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println(line);
                 System.out.println("sayonara nerd! c ya soon ;)");
                 System.out.println(line);
             } else if (input.equalsIgnoreCase("list")) {
                 System.out.println(line);
+                System.out.println("get to work! these are the tasks in your list: ");
                 for (int i = 0; i < count; i++) {
-                    System.out.println(i + 1 + ". " + arr[i]);
+                    System.out.println(i + 1 + ".[" + arr[i].getStatus() + "] " + arr[i].description);
                 }
+                System.out.println(line);
+            } else if (str[0].equalsIgnoreCase("done")) {
+                System.out.println(line);
+                int taskNum = Integer.parseInt(str[1]);
+                Task temp = arr[taskNum - 1];
+                temp.markDone();
+                System.out.println("cool! this task is marked as done:");
+                System.out.println("[" + temp.getStatus() + "] " + temp.description);
                 System.out.println(line);
             } else {
                 System.out.println(line);
                 System.out.println("added: " + input);
                 System.out.println(line);
-                arr[count] = input;
+                arr[count] = new Task(input);
                 count++;
                 }
             }
         }
+}
+
+class Task {
+    String description;
+    boolean isDone;
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getStatus() {
+        return (isDone ? "\u2718" : " ");
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
 }
