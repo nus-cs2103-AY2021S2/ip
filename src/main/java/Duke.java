25c8df0bbd2c05d183f20c2960ed569bf80826fd
austenjs <e0407786u.nus.edu>
2021-01-18 14:31:06 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,17 +7,33 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String lineAfterCommand = "____________________________________________________________\n";
+        String lineAfterCommand = "____________________________________________________________";
         System.out.println("Hello from\n" + logo);
         Scanner scanner = new Scanner(System.in);
-        System.out.println(lineAfterCommand + "Hello! I'm  Duke");
-        System.out.println("What can I do for you?\n" + lineAfterCommand);
-        String command = scanner.next();
+        System.out.println(lineAfterCommand + "\nHello! I'm  Duke");
+        System.out.println("What can I do for you?\n" + lineAfterCommand + "\n");
         String[] tasks = new String[100];
-        while (!command.equals("bye")) {
-            System.out.println("\n" + lineAfterCommand + command + "\n" + lineAfterCommand);
-            command = scanner.next();
+        int index = 0;
+        while (scanner.hasNext()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")){
+                break;
+            }
+            System.out.println(lineAfterCommand);
+            if (command.equals("list")){
+                for (int i = 0; i < index; i++){
+                    System.out.println(String.valueOf(i + 1) + ". " + tasks[i]);
+                }
+                //System.out.println("\n");
+                System.out.println(lineAfterCommand + "\n");
+            }
+            else {
+                tasks[index] = command;
+                index++;
+                System.out.println("added: " + command);
+                System.out.println(lineAfterCommand + "\n");
+            }
         }
-        System.out.println(lineAfterCommand + "Bye. Hope to see you again soon!\n" + lineAfterCommand);
+        System.out.println(lineAfterCommand + "\nBye. Hope to see you again soon!\n" + lineAfterCommand + "\n");
     }
 }
