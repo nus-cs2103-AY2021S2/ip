8afe52fddf6249bd26c945077f15dbf892b27f9a
Vevek Selvam <vevekselvam@gmail.com>
2021-01-18 20:38:57 +0800

    Implemented Level-1 of ip.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,49 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String BOT_NAME = "Apollo the Robot!";
+    public static String INDENTATION = "    ";
+    public static String logo = INDENTATION
+            + "     \\    ___\\   ___\\  \\     \\       ___\n"
+            + INDENTATION
+            + "   /  \\  |    |/     \\ |     |     /     \\ \n"
+            + INDENTATION
+            + "  ------ |___/ |      ||     |    |       |\n"
+            + INDENTATION
+            + " /      \\|     \\     / |     |     \\     / \n"
+            + INDENTATION
+            + "/        |      \\___/  \\_____\\_____ \\___/ \n";
+
+    public static void printlnWithIndentation(String s) {
+        System.out.println(INDENTATION + " " + s);
+    }
+
+    public static void printHorizontalLine() {
+        String line = "____________________________________________________________";
+        System.out.println(INDENTATION + line);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printHorizontalLine();
+        printlnWithIndentation("Hello from " + BOT_NAME + "\n" + logo);
+        printlnWithIndentation("What would you like to do today?\n");
+        printHorizontalLine();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNext()) {
+            String command = scanner.next();
+
+            if (command.equals("bye")) {
+                printHorizontalLine();
+                printlnWithIndentation("Bye. Hope to see you again soon!");
+                printHorizontalLine();
+                System.exit(0);
+            } else {
+                printHorizontalLine();
+                printlnWithIndentation(command);
+                printHorizontalLine();
+            }
+        }
     }
 }
