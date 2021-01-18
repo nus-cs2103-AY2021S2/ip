2892b12a0b16e401b6910b91c2e58d715f93be8b
austenjs <e0407786u.nus.edu>
2021-01-18 14:04:45 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,16 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String lineAfterCommand = "____________________________________________________________\n";
         System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(lineAfterCommand + "Hello! I'm  Duke");
+        System.out.println("What can I do for you?\n" + lineAfterCommand);
+        String command = scanner.next();
+        while (!command.equals("bye")) {
+            System.out.println("\n" + lineAfterCommand + command + "\n" + lineAfterCommand);
+            command = scanner.next();
+        }
+        System.out.println(lineAfterCommand + "Bye. Hope to see you again soon!\n" + lineAfterCommand);
     }
 }
