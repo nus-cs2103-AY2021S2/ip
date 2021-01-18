066f537a9c661c8e045c699af7c7d83995110065
Samuel Lee <samuel.lwy@live.com.sg>
2021-01-19 00:07:29 +0800

    Level 2 - Add to list capability

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,14 +6,26 @@ public class Duke {
         String welcomeMsg = "Olly: Hey! Welcome to the chatbot. What can I do for you today?";
         System.out.println(welcomeMsg);
 
+        String[] texts = new String[100];
+        int textCounter = 0;
+
         while (sc.hasNext()) {
-            String input = sc.next();
+            String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Olly: Goodbye for now, we will meet again.");
                 break;
             }
-            System.out.print("You: ");
-            System.out.println(input);
+
+            if (input.equals("list")) {
+                System.out.println("Olly: Here you go! Your list of items:");
+                for (int i = 0; i < textCounter; i++) {
+                    System.out.println(i+1 + ". " + texts[i]);
+                }
+            } else {
+                texts[textCounter] = input;
+                textCounter++;
+                System.out.println("[Added to list] " + input);
+            }
         }
     }
 }
