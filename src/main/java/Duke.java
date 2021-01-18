298f780df699bf8d01d4e6dd5c2b5c1d27b5af2e
xiinweii98 <xiinweii98@gmail.com>
2021-01-18 20:13:34 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String command;
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String partition = "\n/**********************************************************/\n";
+
+        System.out.println(logo + "\nHello! I'm Duke.\n" + "What can I do for you?\n" + partition);
+        command = sc.nextLine();
+
+        while(!command.equals("bye")) {
+            System.out.println("Your command is: " + command + "\n" + partition);
+            command = sc.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!\n" + logo);
     }
 }
