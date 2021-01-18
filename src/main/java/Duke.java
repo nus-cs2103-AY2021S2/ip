5159d8dba8ca7ea6fe54183ad4a4543426d38ea4
Samuel Lee <samuel.lwy@live.com.sg>
2021-01-18 23:59:28 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String welcomeMsg = "Olly: Hey! Welcome to the chatbot. What can I do for you today?";
+        System.out.println(welcomeMsg);
+
+        while (sc.hasNext()) {
+            String input = sc.next();
+            if (input.equals("bye")) {
+                System.out.println("Olly: Goodbye for now, we will meet again.");
+                break;
+            }
+            System.out.print("You: ");
+            System.out.println(input);
+        }
     }
 }
