f0583e06289b97a405df83d7f059a7038a4f7e85
OhJunMing <ohjunming1998@gmail.com>
2021-01-18 23:12:17 +0800

    L2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
+    private static ArrayList<String> lst = new ArrayList<>();
 
-    Duke(){
+    public static void initiate(Scanner s) {
+        Duke.Greet();
+        String input = s.nextLine();
 
+        while(!input.equals("bye")){
+            if (input.equals("list")) {
+                Duke.list();
+            } else {
+                Duke.add(input);
+            }
+            input = s.nextLine();
+        }
+        Duke.exit();
     }
+
+    public static void add(String msg) {
+        lst.add(msg);
+        System.out.println("---------------------------------------");
+        System.out.println("added: " + msg);
+        System.out.println("---------------------------------------");
+    }
+
+    public static void list() {
+        System.out.println("---------------------------------------");
+        for(int i = 0; i < lst.size(); i++) {
+           System.out.println((i + 1) + ". " + lst.get(i));
+        }
+        System.out.println("---------------------------------------");
+    }
+
     public static void Greet() {
         System.out.println("---------------------------------------");
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        Duke chatbot = new Duke();
-        chatbot.Greet();
-        String input = s.next();
-
-        while(!input.equals("bye")){
-            chatbot.echo(input);
-            input = s.next();
-        }
-        chatbot.exit();
+        Duke.initiate(s);
     }
 }
