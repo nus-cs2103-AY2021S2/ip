263e9087dc9dd5ae5a8efd2c0c5eeb5e98e4dee8
wangtao0717 <e0688546@u.nus.edu>
2021-01-18 22:55:22 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,15 +3,32 @@ import java.util.Scanner;
 public class Duke {
     private static String indentation = "    ";
     private static String horizon = "-------------------------------------";
+    private static String[] taskList = new String[100];
+    private static int countTask = 0;
     private static void printReply(String reply){
         System.out.println(indentation+horizon);
-        if (!reply.equals("bye")) {
-            System.out.println(indentation + reply);
-        }
-        else {
-            System.out.println(indentation + "Bye. (>_<) Hope to see you again soon! ");
-        }
+        switch (reply) {
+            case "hello":
+                System.out.println(indentation + "Hello! I'm Duke Y(^_^)Y");
+                System.out.println(indentation+"What can I do for you?\n");
+                break;
+            case "bye":
+                System.out.println(indentation + "Bye. (>_<) Hope to see you again soon! ");
+                break;
+
+            case "list":
+                for (int i = 0; i < countTask; i++) {
+                    System.out.println(indentation + (i+1) + "." + taskList[i]);
+                }
+                break;
+            default:
+                System.out.println(indentation + "added:" + reply);
+            }
         System.out.println(indentation+horizon);
+    }
+    private static void taskAdd(String task){
+        taskList[countTask] = task;
+        countTask++;
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -19,17 +36,18 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-
-        String hello = "Hello! I'm Duke Y(^_^)Y\n"+indentation+"What can I do for you?\n";
-        String name = "";
+        String command = "";
 
         System.out.println("Hello from\n" + logo);
-        printReply(hello);
+        printReply("hello");
 
         Scanner scanner = new Scanner(System.in);
         do {
-            name = scanner.nextLine();
-            printReply(name);
-        }while(!name.equals("bye"));
+            command = scanner.nextLine();
+            if (!command.equals("bye") && !command.equals("list")){
+                taskAdd(command);
+            }
+            printReply(command);
+        }while(!command.equals("bye"));
     }
 }
