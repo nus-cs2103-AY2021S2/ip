1c18b0c08b4a1bdbb9bab3f8fd2fc1fe8c6bba6c
Lee Han Bin <hanbin55@hotmail.com>
2021-01-18 12:35:04 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc= new Scanner(System.in);
+        Boolean continueProgram = true;
+        String logo = "Hello! I'm Duke\n"
+                + "What can I do for you?\n";
+        System.out.println(logo);
+
+
+        while (sc.hasNext()) {
+            String command = sc.next();
+            if (command.equals("bye")){
+                break;
+            } else {
+                System.out.println(
+                        "____________________________________________________________\n"
+                                + command
+                                + "\n____________________________________________________________");
+            }
+        }
+        System.out.println("____________________________________________________________\n"
+                +"Bye. Hope to see you again soon!"
+                + "\n____________________________________________________________");
+
+
     }
 }
