e0e52eb95c3406ed67fb6cda7702edf37c12e4fb
lirc572 <admin@lirc572.com>
2021-01-18 11:21:30 +0800

    feat: implement greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,49 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void printLine(String line) {
+        System.out.println("     " + line);
+    }
+
+    public static void printHorizontalLine() {
+        System.out.println("    " + "____________________________________________________________");
+    }
+
+    public static void printEmptyLine() {
+        System.out.println();
+    }
+
+    public static boolean processCommand(String command) {
+        printHorizontalLine();
+        if (command.equals("bye")) {
+            printLine("Bye. Hope to see you again soon!");
+        } else {
+            printLine(command);
+        }
+        printHorizontalLine();
+        printEmptyLine();
+        if (command.equals("bye")) {
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printHorizontalLine();
+        for (String line : logo.split("\n")) {
+            printLine(line);
+        }
+        printEmptyLine();
+        printLine("Hello! I'm Duke");
+        printLine("What can I do for you?");
+        printHorizontalLine();
+        printEmptyLine();
+        while (processCommand(sc.nextLine())) ;
     }
 }
