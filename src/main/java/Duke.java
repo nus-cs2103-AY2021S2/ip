ed8625f9fecf9db138dd4ccef1c3db0fe75c132c
dlau <danielau88@gmail.com>
2021-01-18 15:16:16 +0800

    Complete level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static void printIndentOutput(String output) {
+        System.out.println('\t' + output);
+    }
+
+    private static void printHorizontalLine() {
+        printIndentOutput("_____________________________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +16,30 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        printHorizontalLine();
+        printIndentOutput("What can I do for you?");
+        printHorizontalLine();
+
+        Scanner stdin = new Scanner(System.in);
+        String line = stdin.nextLine();
+        boolean end = false;
+
+        while (line != null) {
+            printHorizontalLine();
+
+            if (line.equals("bye")) {
+                printIndentOutput("Bye. Hope to see you again soon!");
+                end = true;
+            } else {
+                printIndentOutput(line);
+            }
+
+            printHorizontalLine();
+
+            if (end) break;
+
+            line = stdin.nextLine();
+        }
     }
 }
