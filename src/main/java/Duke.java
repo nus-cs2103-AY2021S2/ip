63410e2ed6cf027f75a42668a8bba52165013050
darkdestry-t <tanki123+temp@hotmail.com>
2021-01-19 01:17:22 +0800

    Add todo, deadline and event to parser

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,16 +5,17 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.concurrent.Flow.Subscriber;
 import java.io.IOException;
 
 public class Duke {
 
     public static ArrayList<Task> tasks;
 
+    private static final String greetingMessage = "Hello! I'm Duke\n" + "What can I do for you?";
+    private static final String logo = " ____        _        \n" + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n" + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
+
     public static void main(String[] args) throws IOException {
-        String logo = " ____        _        \n" + "|  _ \\ _   _| | _____ \n" + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
         System.out.println(greetingMessage);
@@ -24,8 +25,6 @@ public class Duke {
         chatLoop(System.in, System.out);
 
     }
-
-    public static String greetingMessage = "Hello! I'm Duke\n" + "What can I do for you?";
 
     public static void chatLoop(InputStream in, OutputStream out) throws IOException {
         BufferedReader reader = getReader(in);
@@ -49,10 +48,36 @@ public class Duke {
                 return executeList();
             case "done":
                 return executeDone(tokenizedInput);
+            case "todo":
+                return executeTodo(input);
+            case "deadline":
+                return executeDeadline(input);
+            case "event":
+                return executeEvent(input);
             default:
-                tasks.add(new Task(input));
-                return "added: " + input;
+                return addTaskAndReturnMessage(new Task(input));
         }
+    }
+
+    private static String executeEvent(String input) {
+        String[] data = input.substring(6).split("/at");
+        return addTaskAndReturnMessage(new EventTask(data[0].trim(), data[1].trim()));
+    }
+
+    private static String executeDeadline(String input) {
+        String[] data = input.substring(9).split("/by");
+        return addTaskAndReturnMessage(new DeadlineTask(data[0].trim(), data[1].trim()));
+    }
+
+    private static String executeTodo(String input) {
+        String info = input.substring(5);
+        return addTaskAndReturnMessage(new TodoTask(info));
+    }
+
+    private static String addTaskAndReturnMessage(Task task) {
+        tasks.add(task);
+        return String.format("Got it. I've added this task:\n  %s\nNow you have %d tasks in the list.", task.toString(),
+                tasks.size());
     }
 
     private static String executeList() {
--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -9,21 +10,126 @@ import org.junit.jupiter.api.Test;
 
 public class DukeTest {
     @Test
-    public void chatLoopParseDoneSetsTaskAsDone() throws IOException {
+    public void parseInputNewDeadlineTask() {
         // Setup
         Duke.tasks = new ArrayList<>(100);
 
         // Test
-        String testInput = "help people\n" + "help myself\n" + "done 1\n" + "list\n";
+        final String expectedOutput = "Got it. I've added this task:\n  [D][✗] return book (by: Sunday)\nNow you have 1 tasks in the list.";
+        assertEquals(expectedOutput, Duke.parseInput("deadline return book /by Sunday"));
+
+        assertEquals("return book", Duke.tasks.get(0).getTaskInfo());
+        assertEquals(false, Duke.tasks.get(0).getCompletionState());
+        assertEquals("[D][✗] return book (by: Sunday)", Duke.tasks.get(0).toString());
+        assertTrue(Duke.tasks.get(0) instanceof DeadlineTask);
+    }
+
+    @Test
+    public void parseInputNewEventTask() {
+        // Setup
+        Duke.tasks = new ArrayList<>(100);
+
+        // Test
+        final String expectedOutput = "Got it. I've added this task:\n  [E][✗] project meeting (at: Mon 2-4pm)\nNow you have 1 tasks in the list.";
+        assertEquals(expectedOutput, Duke.parseInput("event project meeting /at Mon 2-4pm"));
+
+        assertEquals("project meeting", Duke.tasks.get(0).getTaskInfo());
+        assertEquals(false, Duke.tasks.get(0).getCompletionState());
+        assertEquals("[E][✗] project meeting (at: Mon 2-4pm)", Duke.tasks.get(0).toString());
+        assertTrue(Duke.tasks.get(0) instanceof EventTask);
+    }
+
+    @Test
+    public void parseInputNewTodoTask() {
+        // Setup
+        Duke.tasks = new ArrayList<>(100);
+
+        // Test
+        final String expectedOutput = "Got it. I've added this task:\n  [T][✗] borrow book\nNow you have 1 tasks in the list.";
+        assertEquals(expectedOutput, Duke.parseInput("todo borrow book"));
+
+        assertEquals("borrow book", Duke.tasks.get(0).getTaskInfo());
+        assertEquals(false, Duke.tasks.get(0).getCompletionState());
+        assertEquals("[T][✗] borrow book", Duke.tasks.get(0).toString());
+        assertTrue(Duke.tasks.get(0) instanceof TodoTask);
+    }
+
+    @Test
+    public void chatLoopParseEventTaskInteractionOutput() throws IOException {
+        // Setup
+        Duke.tasks = new ArrayList<>(100);
+
+        // Test
+        String testInput = "event project meeting /at Mon 2-4pm\nlist\ndone 1\nlist\n";
 
         InputStream in = makeInputStreamFromString(testInput);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
         Duke.chatLoop(in, out);
 
-        final String expectedOutput = "added: help people\n" + "added: help myself\n"
-                + "Nice! I've marked this task as done:\n  [✓] help people\n" + "1.[✓] help people\n"
-                + "2.[✗] help myself\n";
+        final String expectedOutput = "Got it. I've added this task:\n  [E][✗] project meeting (at: Mon 2-4pm)\nNow you have 1 tasks in the list.\n"
+                + "1.[E][✗] project meeting (at: Mon 2-4pm)\n"
+                + "Nice! I've marked this task as done:\n  [E][✓] project meeting (at: Mon 2-4pm)\n"
+                + "1.[E][✓] project meeting (at: Mon 2-4pm)\n";
+        assertEquals(expectedOutput, new String(out.toByteArray()));
+    }
+
+    @Test
+    public void chatLoopParseDeadlineTaskInteractionOutput() throws IOException {
+        // Setup
+        Duke.tasks = new ArrayList<>(100);
+
+        // Test
+        String testInput = "deadline return book /by Sunday\nlist\ndone 1\nlist\n";
+
+        InputStream in = makeInputStreamFromString(testInput);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        Duke.chatLoop(in, out);
+
+        final String expectedOutput = "Got it. I've added this task:\n  [D][✗] return book (by: Sunday)\nNow you have 1 tasks in the list.\n"
+                + "1.[D][✗] return book (by: Sunday)\n"
+                + "Nice! I've marked this task as done:\n  [D][✓] return book (by: Sunday)\n"
+                + "1.[D][✓] return book (by: Sunday)\n";
+        assertEquals(expectedOutput, new String(out.toByteArray()));
+    }
+
+    @Test
+    public void chatLoopParseTodoTaskInteractionOutput() throws IOException {
+        // Setup
+        Duke.tasks = new ArrayList<>(100);
+
+        // Test
+        String testInput = "todo borrow book\nlist\ndone 1\nlist\n";
+
+        InputStream in = makeInputStreamFromString(testInput);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        Duke.chatLoop(in, out);
+
+        final String expectedOutput = "Got it. I've added this task:\n  [T][✗] borrow book\nNow you have 1 tasks in the list.\n"
+                + "1.[T][✗] borrow book\n" + "Nice! I've marked this task as done:\n  [T][✓] borrow book\n"
+                + "1.[T][✓] borrow book\n";
+        assertEquals(expectedOutput, new String(out.toByteArray()));
+    }
+
+    @Test
+    public void chatLoopParseDoneSetsTaskAsDone() throws IOException {
+        // Setup
+        Duke.tasks = new ArrayList<>(100);
+
+        // Test
+        String testInput = "todo help people\n" + "todo help myself\n" + "done 1\n" + "list\n";
+
+        InputStream in = makeInputStreamFromString(testInput);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        Duke.chatLoop(in, out);
+
+        final String expectedOutput = "Got it. I've added this task:\n  [T][✗] help people\nNow you have 1 tasks in the list.\n"
+                + "Got it. I've added this task:\n  [T][✗] help myself\nNow you have 2 tasks in the list.\n"
+                + "Nice! I've marked this task as done:\n  [T][✓] help people\n" + "1.[T][✓] help people\n"
+                + "2.[T][✗] help myself\n";
         assertEquals(expectedOutput, new String(out.toByteArray()));
     }
 
@@ -31,7 +137,7 @@ public class DukeTest {
     public void parseInputDoneSetsTaskAsDone() {
         // Setup
         Duke.tasks = new ArrayList<>(100);
-        assertEquals("added: help people", Duke.parseInput("help people"));
+        Duke.parseInput("help people");
 
         // Test
         assertEquals("help people", Duke.tasks.get(0).getTaskInfo());
@@ -50,7 +156,7 @@ public class DukeTest {
     public void parseInputAddToList() {
         int tasksStateLength = Duke.tasks.size();
 
-        assertEquals("added: help people", Duke.parseInput("help people"));
+        Duke.parseInput("help people");
 
         assertEquals(tasksStateLength + 1, Duke.tasks.size());
 
@@ -63,9 +169,9 @@ public class DukeTest {
     public void parseInputAddToListMaintainsOrder() {
         int tasksStateLength = Duke.tasks.size();
 
-        assertEquals("added: help people", Duke.parseInput("help people"));
-        assertEquals("added: blah", Duke.parseInput("blah"));
-        assertEquals("added: read book", Duke.parseInput("read book"));
+        Duke.parseInput("help people");
+        Duke.parseInput("blah");
+        Duke.parseInput("read book");
 
         assertEquals(tasksStateLength + 3, Duke.tasks.size());
 
@@ -78,9 +184,9 @@ public class DukeTest {
     public void parseInputListOutputTasksState() {
         // Setup
         Duke.tasks = new ArrayList<>(100);
-        assertEquals("added: help people", Duke.parseInput("help people"));
-        assertEquals("added: blah", Duke.parseInput("blah"));
-        assertEquals("added: read book", Duke.parseInput("read book"));
+        Duke.parseInput("help people");
+        Duke.parseInput("blah");
+        Duke.parseInput("read book");
 
         final String expectedString = "1.[✗] help people\n2.[✗] blah\n3.[✗] read book";
 
