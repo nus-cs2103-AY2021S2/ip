dc6d7bc406e96e6394728e18015822021f4f0a93
shaelynl <shaelyn.lamxy@gmail.com>
2021-01-19 00:43:05 +0800

    changed file name to DukeLevel

--- src/main/java/DukeLevel.java
+++ src/main/java/DukeLevel.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Level1 {
+public class DukeLevel {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String line = "    _________________________________________________";
