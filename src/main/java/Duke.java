16bbac1661756917498f1cd40c9ac0ff771d2ed9
unknown <varcolac01@gmail.com>
2021-01-18 23:23:25 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,29 +13,28 @@ public class Duke {
         System.out.println(lines);
         System.out.println("     Hello! I'm Duke\n     What can I do for you?");
         System.out.println(lines);
-/*
+
         String[] list = new String[101];
         int index = 1;
-        boolean[] done = new boolean[101];
-*/
+//        boolean[] done = new boolean[101];
+
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             System.out.println(lines);
-            /*if (input.equals("list")) {
+            if (input.equals("list")) {
                 for (int i = 1; i < index; i++) {
                     String output = ". " + list[i];
                     System.out.println(indentation + i + output);
                 }
-            } else if (input.contains("done")){
+            /*} else if (input.contains("done")){
                 int number = Integer.parseInt(input.substring(5));
-                done[number] = true;
+                done[number] = true;*/
             } else {
                 System.out.println(indentation + "added: " + input);
                 list[index] = input;
                 index++;
-            }*/
-            System.out.println(indentation + input);
+            }
             System.out.println(lines);
             input = sc.nextLine();
         }
