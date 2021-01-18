8918036bc50659e3aa15f7f81d5f2089b80615f5
wongkokian <ianwong97@hotmail.sg>
2021-01-18 10:41:26 +0800

    Modified Duke to handle different types of tasks.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,30 +23,40 @@ public class Duke {
 
     public static void run(String cmd) {
         System.out.println("    ____________________________________________________________");
+
         if(cmd.equals("bye")) {
-            System.out.println("     Bye. Hope to see you again soon!" + "\n"
-                    + "    ____________________________________________________________\n");
+            System.out.println("     Bye. Hope to see you again soon!");
         } else if (cmd.equals("list")) {
             System.out.println("     Here are the tasks in your list:");
             for (int i = 0; i < tasks.size(); i++) {
                 Task curr = tasks.get(i);
-                System.out.println("     " + (i + 1) + "."
-                        + curr.getStatusIcon() + curr.getDescription());
+                System.out.println("     " + (i + 1) + "." + curr.toString());
             }
-            System.out.println("    ____________________________________________________________\n");
-            run(sc.nextLine());
         } else if (cmd.startsWith("done")) {
-            int index = Integer.parseInt(cmd.substring(5)) - 1;
-            tasks.get(index).markAsDone();
-            Task curr = tasks.get(index);
+            int i = Integer.parseInt(cmd.substring(5)) - 1;
+            tasks.get(i).markAsDone();
+            Task curr = tasks.get(i);
             System.out.println("     Nice! I've marked this task as done:\n"
-                    + "       " + curr.getStatusIcon() + curr.getDescription() + "\n"
-                    + "    ____________________________________________________________\n");
-            run(sc.nextLine());
+                    + "       " + curr.toString());
         } else {
-            System.out.println("     added: " + cmd + "\n"
-                    + "    ____________________________________________________________\n");
-            tasks.add(new Task(cmd));
+            Task curr;
+            if (cmd.startsWith("todo")) {
+                curr = new Todo(cmd.substring(5));
+            } else if (cmd.startsWith("deadline")) {
+                int end = cmd.indexOf('/') - 1;
+                curr = new Deadline(cmd.substring(9, end), cmd.substring(end + 5));
+            } else {
+                int end = cmd.indexOf('/') - 1;
+                curr = new Event(cmd.substring(6, end), cmd.substring(end + 5));
+            }
+            tasks.add(curr);
+            System.out.println("     Got it. I've added this task:\n"
+                    + "       " + curr.toString() + "\n"
+                    + "     Now you have " + tasks.size() + " tasks in the list.");
+        }
+        System.out.println("    ____________________________________________________________\n");
+
+        if(!cmd.equals("bye")) {
             run(sc.nextLine());
         }
     }
