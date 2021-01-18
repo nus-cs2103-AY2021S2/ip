9e63a3e1eb0c57713d9721d5f09404d7ce2759f6
Lim Li Quan <linliquan1996@Hotmail.com>
2021-01-18 15:14:17 +0800

    Duke.java : Add List command history and add command to command history features.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,40 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    private static ArrayList<String> command_history = new ArrayList<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         //Greet User
         printGreetings();
         String command = scanner.nextLine();
         while (!command.equalsIgnoreCase("bye")) {
-            //Echo commands
             printLine();
-            System.out.println(command);
+            switch (command) {
+                case "list" :
+                    //Display history of commands
+                    listCommands();
+                    break;
+                default:
+                    //Adding commands into history for listing later
+                    command_history.add(command);
+                    //Echo commands
+                    System.out.printf("added: %s\n", command);
+                    break;
+            }
             printLine();
             command = scanner.nextLine();
         }  //Exits the program
         printLine();
         System.out.println("Bye. Hope to see you again soon!");
         printLine();
+    }
+
+    public static void listCommands() {
+        for(int i = 0; i < command_history.size(); i++) {
+            System.out.printf("%d. %s\n", (i+1) , command_history.get(i));
+        }
     }
 
     public static void printLine() {
