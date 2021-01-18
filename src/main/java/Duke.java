6aa276dc5767afe4a90622905dc963af76d95fa3
Vevek Selvam <vevekselvam@gmail.com>
2021-01-18 23:10:08 +0800

    Implemented Level-2 of ip.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static String BOT_NAME = "Apollo the Robot!";
+    public static String BOT_NAME = "Apollo the Robot";
     public static String INDENTATION = "    ";
+    public static ArrayList<String> taskList = new ArrayList<>();
 
     public static void printlnWithIndentation(String s) {
         System.out.println(INDENTATION + " " + s);
@@ -11,6 +14,24 @@ public class Duke {
     public static void printHorizontalLine() {
         String line = "____________________________________________________________";
         System.out.println(INDENTATION + line);
+    }
+
+    public static void addTask(String task, ArrayList<String> taskList) {
+        taskList.add(task);
+        printHorizontalLine();
+        printlnWithIndentation("added: " + task);
+        printHorizontalLine();
+    }
+
+    public static void listTasks(ArrayList<String> taskList) {
+        printHorizontalLine();
+
+        for(int i = 0; i < taskList.size(); i++ ) {
+            int index = i + 1;
+            printlnWithIndentation(index + ". " + taskList.get(i));
+        }
+
+        printHorizontalLine();
     }
 
     public static void main(String[] args) {
@@ -22,17 +43,19 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
 
         while(scanner.hasNext()) {
-            String command = scanner.next();
+            String input = scanner.nextLine();
+            String command = input.split(" ", 1)[0];
 
             if (command.equals("bye")) {
                 printHorizontalLine();
                 printlnWithIndentation("Bye. Hope to see you again soon!");
                 printHorizontalLine();
                 System.exit(0);
-            } else {
-                printHorizontalLine();
-                printlnWithIndentation(command);
-                printHorizontalLine();
+            } else if (command.equals("list")) {
+                listTasks(taskList);
+            }
+            else {
+                addTask(input, taskList);
             }
         }
     }
