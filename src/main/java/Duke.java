93abe2707f1660e6bfe84936a8a0ec2c59fe989d
Otto Alexander <godjuansan88@gmail.com>
2021-01-18 13:39:25 +0800

    level1 duke week 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greetings = "Hi, I am a self-centered bot. I can't do anything for you.";
+        String bye = "Goodbye, hope to not see you again.";
+//        System.out.println("Hello from\n" + logo);
+        System.out.println(greetings);
+        while (sc.hasNext()) {
+            String word = sc.nextLine();
+            if (word.equals("bye")) {
+                System.out.println(bye);
+                break;
+            }
+            else {
+                System.out.println(word);
+            }
+        }
     }
 }
