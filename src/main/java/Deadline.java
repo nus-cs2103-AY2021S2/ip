cf00b8f273a29c8e8b3a667be21cda6a94b67dc7
CSmortal <jovenpua51@gmail.com>
2021-01-18 17:45:59 +0800

    added Deadline.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,2 +1,26 @@
-package main.java;public class Deadline {
+package main.java;
+
+public class Deadline extends Task {
+    private String doneBy;
+
+    public Deadline(String description, String doneBy) {
+        super(description);
+        this.doneBy = doneBy;
+    }
+
+    public Deadline(String description, String doneBy, boolean isDone) {
+        super(description, isDone);
+        this.doneBy = doneBy;
+    }
+
+    @Override
+    public Deadline markAsDone() {
+        return new Deadline(description, doneBy, true);
+    }
+
+    @Override
+    public String toString() {
+        return "[D][" + getStatusIcon() + "] " + description + " (by: " + doneBy + ")";
+    }
+
 }
