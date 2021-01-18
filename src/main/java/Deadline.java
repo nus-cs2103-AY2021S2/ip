3f365ac7806de73db6c98313afc11de9f0baee7e
Lim Li Quan <linliquan1996@Hotmail.com>
2021-01-18 17:54:11 +0800

    Add Deadline.java

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,13 @@
+public class Deadline extends Task{
+    private String deadline;
+
+    public Deadline(String desc, String deadline) {
+        super(desc);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[D] %s (by : %s)", super.toString(), this.deadline);
+    }
+}
