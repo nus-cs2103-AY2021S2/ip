7de349ef72d4b5c768cae065827030677f367569
branzuelajohn <branzuelajohn@gmail.com>
2021-01-19 17:09:27 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,16 +3,27 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] list = new String[100];
+        int counter = 0;
         Duke.printHorizontalLine();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         Duke.printHorizontalLine();
         while(true) {
             String s = sc.nextLine();
-            if(!s.equals("bye")) {
+            if(!s.equals("bye") && !s.equals("list")) {
                 Duke.printHorizontalLine();
-                System.out.println(s);
+                System.out.printf("added: %s%n",s);
                 Duke.printHorizontalLine();
+                list[counter++] = s;
+            } else if(s.equals("list")) {
+                Duke.printHorizontalLine();
+                for(int i = 0; i < counter; i++) {
+                    int j = i + 1;
+                    System.out.printf("%d. %s%n", j, list[i]);
+                }
+                Duke.printHorizontalLine();
+
             } else {
                 Duke.printHorizontalLine();
                 System.out.println("Bye. Hope to see you again soon!");
@@ -25,13 +36,4 @@ public class Duke {
     public static void printHorizontalLine() {
         System.out.println("____________________________________________________________");
     }
-
-
-    /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello from\n" + logo);*/
 }
