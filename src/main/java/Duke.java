1055a40223e3f79f4cd57060b75c3e61f94ce8cc
joncao98 <joncao98@gmail.com>
2021-01-19 15:41:19 +0800

    add Mark as Done and Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,21 +12,28 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
          */
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         while (true) {
             String s = sc.nextLine();
+            String[] arr = s.split(" ");
             if (s.equals("list")) {
                 int index = 1;
-                for (String text : list) {
-                    System.out.println(index + ". " + text);
+                for (Task task : list) {
+                    System.out.println(index + "." + task);
                     index++;
                 }
+            } else if (arr[0].equals("done")) {
+                int index = Integer.parseInt(arr[1]);
+                Task doneTask = list.get(index - 1).markAsDone();
+                list.set(index - 1, doneTask);
+                System.out.println("Nice! I've marked this task as done:\n" + doneTask);
             } else if (s.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else {
-                list.add(s);
+                Task newTask = new Task(s);
+                list.add(newTask);
                 System.out.println("added: " + s);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    private Task(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2718" : " "); //return tick or X symbols
+    }
+
+    public Task markAsDone() {
+        return new Task(this.description, true);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
