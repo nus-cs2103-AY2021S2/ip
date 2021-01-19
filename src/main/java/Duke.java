aa5ccbf0f7b19b31232b541d23b172613c4ec7d3
vivegank <77456578+vivegank@users.noreply.github.com>
2021-01-19 22:47:27 +0800

    Creating Duke Object

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    List<String> tasks;
+
+    public Duke() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void runDuke() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("How can I help you?");
+        scan();
+    }
+
+    public void scan() {
+        Scanner s = new Scanner(System.in);
+        String instruction = s.nextLine();
+        while(!instruction.equals("bye")) {
+            echo(instruction);
+            instruction = s.nextLine();
+        }
+        echo("Bye. Cya");
+    }
 
     public static void echo(String instruction) {
         System.out.println("     --------------------------------");
@@ -9,19 +37,7 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        Scanner s = new Scanner(System.in);
-        System.out.println("Hello from\n" + logo);
-        System.out.println("How can I help you?");
-        String instruction = s.nextLine();
-        while(!instruction.equals("bye")) {
-            echo(instruction);
-            instruction = s.nextLine();
-        }
-        echo("Bye. Cya");
+        Duke duke = new Duke();
+        duke.runDuke();
     }
 }
