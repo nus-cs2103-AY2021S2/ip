8c235e7da00511f4d0a3a372f60fbbb4e9130cf5
Eriksen2411 <ducdanh24112001@gmail.com>
2021-01-19 16:03:18 +0800

    add a-

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,28 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("    ____________________________________________________________");
+        System.out.println(logo);
+        System.out.println("Hello! I'm Danh's Duke\nWhat can I do for you, Mr Danh?");
+        System.out.println("    ____________________________________________________________\n");
+        Scanner input = new Scanner(System.in);
+        boolean signalToExit = false;
+        while (!signalToExit && input.hasNextLine()) {
+            String command = input.nextLine();
+            echo(command);
+            if (command.equals("bye")) {
+                signalToExit = true;
+            }
+        }
+    }
+
+    public static void echo(String command) {
+        System.out.println("    ____________________________________________________________");
+        if (command.equals("bye")) {
+            System.out.println("     Bye. Hope to see you again soon!");
+        } else {
+            System.out.println("     " + command);
+        }
+        System.out.println("    ____________________________________________________________\n");
     }
 }
