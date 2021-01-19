230dc3631f27d21fb44cae2170c869f8c542d110
ZhangAnli <zhanganli1998@hotmail.com>
2021-01-19 14:59:41 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,15 +7,29 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
+        // Initialise list
+        String[] list = new String[100];
+        int numOfItems = 0;
+
         while (!input.equals("bye")) {
-            System.out.println(input);
+            System.out.println("_________________________________________________________");
+            if (input.equals("list")) {
+                for (int i = 1; i <= numOfItems; i++) {
+                    System.out.println(i + ". " + list[i - 1]);
+                }
+            } else {
+                list[numOfItems] = input;
+                numOfItems++;
+                System.out.println("added: " + input);
+            }
+            System.out.println("_________________________________________________________");
             input = sc.nextLine();
         }
 
-        if (input.equals("bye")) {
-            System.out.println("Bye. Hope to see you again soon!");
-        }
+        System.out.println("_________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_________________________________________________________");
+
         sc.close();
-        return;
     }
 }
