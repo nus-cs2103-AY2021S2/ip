d73350045d034186fa8b8225e1902f72f0d5604e
Stratostorm <h_weicong@outlook.com>
2021-01-19 20:59:00 +0800

    Added error handling for deadline, event

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,30 +11,60 @@ public class Duke {
                 System.out.println((i+1) + ". " + todoList.get(i));
             }
         } else if (strippedCommand.startsWith("done")) {
-            String indexString = strippedCommand.substring(5).strip();
+            String indexString = strippedCommand.substring(4).strip();
             int index = Integer.parseInt(indexString) - 1;
             Task toDo = todoList.get(index);
             toDo.doTask();
             System.out.println("Affirmative. The following task has been marked as done: \n" + toDo);
         } else if (strippedCommand.startsWith("todo")) {
-            String cmd = strippedCommand.substring(5).strip();
-            Task newTask = new Todo(cmd);
-            todoList.add(newTask);
-            System.out.println("Added to to-do list: \n" + newTask);
+            String cmd = strippedCommand.substring(4).strip();
+            if (cmd.isBlank()) {
+                String msg = "I apologize, please input description for 'todo'.";
+                DukeException exception = new DukeException(msg);
+                System.out.println(exception);
+            } else {
+                Task newTask = new Todo(cmd);
+                todoList.add(newTask);
+                System.out.println("Added to to-do list: \n" + newTask);
+            }
         } else if (strippedCommand.startsWith("deadline")) {
-            String cmd = strippedCommand.substring(9).strip();
-            String[] split = cmd.split("/by");
-            Task newTask = new Deadline(split[0].strip(), split[1].strip());
-            todoList.add(newTask);
-            System.out.println("Added to to-do list: \n" + newTask);
+            String cmd = strippedCommand.substring(8).strip();
+            if (cmd.isBlank()) {
+                String msg = "I apologize, please input description and time for 'deadline'.";
+                DukeException exception = new DukeException(msg);
+                System.out.println(exception);
+            } else {
+                String[] split = cmd.split("/by");
+                if (cmd.equals(split[0])) {
+                    String msg = "I apologize, please use '/by' argument to specify time for 'deadline'.";
+                    DukeException exception = new DukeException(msg);
+                    System.out.println(exception);
+                } else {
+                    Task newTask = new Deadline(split[0].strip(), split[1].strip());
+                    todoList.add(newTask);
+                    System.out.println("Added to to-do list: \n" + newTask);
+                }
+            }
         } else if (strippedCommand.startsWith("event")) {
-            String cmd = strippedCommand.substring(6).strip();
-            String[] split = cmd.split("/at");
-            Task newTask = new Event(split[0].strip(), split[1].strip());
-            todoList.add(newTask);
-            System.out.println("Added to to-do list: \n" + newTask);
+            String cmd = strippedCommand.substring(5).strip();
+            System.out.println(cmd);
+            if (cmd.isBlank()) {
+                String msg = "I apologize, please input description and time for 'event'.";
+                DukeException exception = new DukeException(msg);
+                System.out.println(exception);
+            } else {
+                String[] split = cmd.split("/at");
+                if (cmd.equals(split[0])) {
+                    String msg = "I apologize, please use '/at' argument to specify time for 'event'.";
+                    DukeException exception = new DukeException(msg);
+                    System.out.println(exception);
+                } else {
+                    Task newTask = new Event(split[0].strip(), split[1].strip());
+                    todoList.add(newTask);
+                    System.out.println("Added to to-do list: \n" + newTask);
+                }
+            }
         } else {
-            //error handling
             String msg = "I apologize, I do not comprehend your command.";
             DukeException exception = new DukeException(msg);
             System.out.println(exception);
