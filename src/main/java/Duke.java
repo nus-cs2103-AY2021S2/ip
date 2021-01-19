06e18c505ff2d1ff3ea927e8cc60a9f9971afa19
CSjiade <jdlian505@gmail.com>
2021-01-19 22:30:39 +0800

    Duke Level2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
+  private ArrayList<String> tasklist;
+
+  public Duke() {
+    this.tasklist = new ArrayList<>();
+  }
+
+  public void list() {
+    int i = 1;
+    for (String task : tasklist) {
+      System.out.println(i + ". " + task + "\n");
+      i++;
+    }
+  }
+
+  public void addList(String input) {
+    this.tasklist.add(input);
+    System.out.println(" ___________________________________________");
+    System.out.println("added: " + input);
+    System.out.println(" ___________________________________________");
+  }
+
+
+
   public static void main(String[] args) {
+    Duke duke = new Duke();
     String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -14,15 +39,20 @@ public class Duke {
     System.out.println(" ___________________________________________");
     Scanner scan = new Scanner(System.in);
     while (scan.hasNext()) {
-      String input = scan.next();
+      String input = scan.nextLine();
       if (input.equals("bye")) {
         System.out.println(" ___________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(" ___________________________________________");
+
+      } else if (input.equals("list")) {
+        System.out.println(" ___________________________________________");
+        duke.list();
+        System.out.println(" ___________________________________________");
+
       } else {
-        System.out.println(" ___________________________________________");
-        System.out.println(input);
-        System.out.println(" ___________________________________________");
+        duke.addList(input);
+
       }
 
     }
