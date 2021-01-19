baaaa504cfef9f4c2b8a137a96ff2c3ecf280cae
shaelynl <shaelyn.lamxy@gmail.com>
2021-01-19 14:47:55 +0800

    added Level 4

--- src/main/java/DukeLevel.java
+++ src/main/java/DukeLevel.java
@@ -15,7 +15,7 @@ public class DukeLevel {
 
         while (sc.hasNext()) {
             String input = sc.nextLine();
-            String[] strArray = input.split(" ");
+            String[] strArray = input.split(" ", 2);
             String cmd = strArray[0];
 
             if (input.equalsIgnoreCase("bye")) {
@@ -27,20 +27,45 @@ public class DukeLevel {
                 System.out.println(line);
                 System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < count; i++) {
-                    System.out.println("      " + (i+1) + ".[" + tasksArray[i].getStatusIcon() + "] " + tasksArray[i].description);
+                    System.out.println("      " + (i+1) + "." + tasksArray[i].toString());
                 }
                 System.out.println(line);
             } else if (cmd.equalsIgnoreCase("done")) {
                 int cmdNum = Integer.parseInt(strArray[1]);
+                tasksArray[cmdNum-1].markAsDone();
                 System.out.println(line);
                 System.out.println("     Nice! I've marked this task as done:");
-                System.out.println("       [" + tasksArray[cmdNum-1].getStatusIcon() + "] " + tasksArray[cmdNum-1].description);
+                System.out.println("       " + tasksArray[cmdNum-1].toString());
                 System.out.println(line);
-                tasksArray[cmdNum-1].markAsDone();
             } else {
                 tasksArray[count] = new Task(input);
                 System.out.println(line);
-                System.out.println("     " + "added : " + input);
+                System.out.println("     Got it. I've added this task: ");
+                if (cmd.equalsIgnoreCase("todo")) {
+                    String cmdTask = strArray[1];
+                    ToDo tempTask = new ToDo(cmdTask);
+                    tasksArray[count] = tempTask;
+                    System.out.println("       " + tempTask.toString());
+                } else if (cmd.equalsIgnoreCase("deadline")) {
+                    String cmdTask = strArray[1];
+                    String[] tempStrArray = cmdTask.split("/by", 2);
+                    Deadline tempTask = new Deadline(tempStrArray[0], tempStrArray[1]);
+                    tasksArray[count] = tempTask;
+                    System.out.println("       " + tempTask.toString());
+                } else if (cmd.equalsIgnoreCase("event")) {
+                    String cmdTask = strArray[1];
+                    String[] tempStrArray = cmdTask.split("/at", 2);
+                    Event tempTask = new Event(tempStrArray[0], tempStrArray[1]);
+                    tasksArray[count] = tempTask;
+                    System.out.println("       " + tempTask.toString());
+                } else {
+                    System.out.println("          added: " + input);
+                }
+                if (count == 0) {
+                    System.out.println("     Now you have " + (count+1) + " task in the list.");
+                } else {
+                    System.out.println("     Now you have " + (count+1) + " tasks in the list.");
+                }
                 System.out.println(line);
                 count++;
             }
@@ -58,10 +83,52 @@ class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        return (isDone ? "\u2713" : " "); //return tick or X symbols
     }
 
     public void markAsDone() {
         isDone = true;
+    }
+
+    @Override
+    public String toString() { return "[" + getStatusIcon() + "] " + description; }
+}
+
+class ToDo extends Task {
+    public ToDo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + "(by:" + by + ")";
+    }
+}
+
+class Event extends Task {
+    protected String at;
+
+    public Event(String description, String at) {
+        super(description);
+        this.at = at;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + "(at:" + at + ")";
     }
 }
