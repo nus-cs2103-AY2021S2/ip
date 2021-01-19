826bf71865f1566184c4a3e5e20d49f206c0f0ae
umergta <umergta@gmail.com>
2021-01-19 19:16:57 +0800

    added level 3 function: marking task as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
-import javax.net.ssl.HostnameVerifier;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private static final String HORIZONTAL_RULE = "____________________________________________________________";
-    private static ArrayList<String> userList = new ArrayList<>();
+    private static ArrayList<Task> userList = new ArrayList<>();
     public static void main(String[] args) {
         Duke.basicProgram();
     }
@@ -12,10 +11,12 @@ public class Duke {
         String greeting = HORIZONTAL_RULE + "\nHello! I am Duke\n" + "What can I do for you?\n" + HORIZONTAL_RULE;
         String exitCommand = "bye";
         String listCommand = "list";
+        String doneCommand = "done";
         System.out.println(greeting);
         Scanner sc = new Scanner(System.in);
         boolean isBye = false;
         while (!isBye){
+            //task taken in
             String command = sc.nextLine();
             System.out.println(HORIZONTAL_RULE);
             if(command.equals(exitCommand)){
@@ -27,15 +28,30 @@ public class Duke {
                 Duke.printUserList();
                 System.out.println(HORIZONTAL_RULE);
             }
+            else if(command.split(" ")[0].equals(doneCommand)){
+                int taskNumber = Integer.parseInt(command.split(" ")[1]);
+                Duke.printDoneTask(taskNumber);
+            }
             else{
-                Duke.userList.add(command);
+                //actual task
+                Task newTask = new Task(command);
+                Duke.userList.add(newTask);
                 System.out.println("added: " + command +"\n"+ HORIZONTAL_RULE);
             }
         }
     }
     private static void printUserList(){
         for(int i = 0; i< userList.size();i++){
-            System.out.println(i+1 + ". " + userList.get(i));
+            System.out.println(i+1 + ".[" + userList.get(i).getStatusIcon() + "] " + userList.get(i).getTaskDetail());
         }
     }
+
+    private static void printDoneTask(int taskNumber){
+        Task doneTask = userList.get(taskNumber - 1);
+        doneTask.markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("[" + doneTask.getStatusIcon() + "] " + doneTask.getTaskDetail());
+        System.out.println(HORIZONTAL_RULE);
+    }
+
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    private boolean isDone;
+    private String taskDetail;
+    Task(String taskDetail){
+        this.taskDetail = taskDetail;
+        this.isDone = false;
+    }
+
+    public boolean getTaskStatus(){
+        return this.isDone;
+    }
+
+    public String getTaskDetail(){
+        return this.taskDetail;
+    }
+
+    public void markAsDone(){
+        this.isDone = true;
+    }
+
+    public void markAsUndone(){
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+}
