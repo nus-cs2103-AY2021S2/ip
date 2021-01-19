fb0d124fc4f744f913ebf1ac9d14cc3cc9f08ba6
github-amanda <57126871+github-amanda@users.noreply.github.com>
2021-01-19 18:48:22 +0800

    Level-2, A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,8 +4,21 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
+        String[] list = new String[100];
+        int index = 0;
         while (!command.equals("bye")) {
-            System.out.println(command);
+            String[] tokens = command.split(" ");
+            String commandType = tokens[0];
+            if (commandType.equals("list")) {
+                // SHOW LIST
+                for (int i = 0; i < index; i++) {
+                    System.out.printf("%d. " + list[i] + "\n", i + 1);
+                }
+            } else {
+                list[index] = command;
+                System.out.println("added: " + command);
+                index++;
+            }
             command = scanner.nextLine();
         }
         if (command.equals("bye")) {
@@ -13,4 +26,3 @@ public class Duke {
         }
     }
 }
-
