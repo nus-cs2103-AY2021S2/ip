4e6860de28fb73d4198cfadb977ee960c735cfc2
clarlzx <clarlzx@gmail.com>
2021-01-19 16:09:06 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,42 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void lines() {
+        System.out.println("__________________________" +
+                "__________________________________");
+    }
+
+    public static void greet() {
+        lines();
+        String greetingCat = " ฅ(=^・ω・^=ฅ)";
+        System.out.println(greetingCat);
+        System.out.println("Mew! I'm Chat the Cat");
+        System.out.println("What can I do for you?");
+        lines();
+    }
+
+    public static void echo(String s) {
+        lines();
+        System.out.println(s);
+        lines();
+    }
+
+    public static void goodbye() {
+        lines();
+        String goodByeCat = "（=^・ω・^=❁）";
+        System.out.println("✧･ﾟ:* Goodbye *:･ﾟ✧" + goodByeCat);
+        lines();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        while (!str.equals("bye")) {
+            echo(str);
+            str = sc.next();
+        }
+        goodbye();
     }
 }
