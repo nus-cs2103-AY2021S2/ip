ec4f8b9520b0b5b67ae85296d1025287ce22899c
umergta <umergta@gmail.com>
2021-01-19 14:08:17 +0800

    completed level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String HORIZONTAL_RULE = "____________________________________________________________";
+    private static final String indentation = "    ";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = HORIZONTAL_RULE + "\nHello! I am Duke\n" + "What can I do for you?\n" + HORIZONTAL_RULE;
+        System.out.println(greeting);
+        Scanner sc = new Scanner(System.in);
+        boolean isBye = false;
+        while (!isBye){
+            String command = sc.nextLine();
+            System.out.println(HORIZONTAL_RULE);
+            if(command.equals("bye")){
+                System.out.println(command + ". Hope to see you again soon!\n"+ HORIZONTAL_RULE);
+                sc.close();
+                isBye = true;
+            }
+            else{
+                System.out.println(command +"\n"+ HORIZONTAL_RULE);
+            }
+        }
     }
 }
