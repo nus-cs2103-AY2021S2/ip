1e50839c1b91a602161e4b3b8a61f45ec1ef0be8
ZhangAnli <zhanganli1998@hotmail.com>
2021-01-19 19:11:57 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -7,7 +9,7 @@ public class Duke {
         String input = sc.nextLine().trim();
 
         // Initialise list
-        Task[] tasks = new Task[100];
+        List<Task> tasks = new ArrayList<>();
         int numOfItems = 0;
         String[] command = {"list", "bye", "todo", "deadline", "event"};
 
@@ -19,7 +21,7 @@ public class Duke {
                     case "list":
                         System.out.println("Here are the tasks on your list:");
                         for (int i = 1; i <= numOfItems; i++) {
-                            String output = String.format("%s. %s", i, tasks[i - 1].toString());
+                            String output = String.format("%s. %s", i, tasks.get(i - 1).toString());
                             System.out.println(output);
                         }
                         break;
@@ -29,22 +31,29 @@ public class Duke {
                     case "todo":
                         toDoCommand(inputArr, tasks, numOfItems, input);
                         System.out.println("Got it. I've added this task:");
-                        System.out.println(tasks[numOfItems].toString());
+                        System.out.println(tasks.get(numOfItems).toString());
                         numOfItems++;
                         System.out.println("Now you have " + numOfItems + " tasks in the list.");
                         break;
                     case "deadline":
                         deadlineCommand(inputArr, tasks, numOfItems, input);
                         System.out.println("Got it. I've added this task:");
-                        System.out.println(tasks[numOfItems].toString());
+                        System.out.println(tasks.get(numOfItems).toString());
                         numOfItems++;
                         System.out.println("Now you have " + numOfItems + " tasks in the list.");
                         break;
                     case "event":
                         eventCommand(inputArr, tasks, numOfItems, input);
                         System.out.println("Got it. I've added this task:");
-                        System.out.println(tasks[numOfItems].toString());
+                        System.out.println(tasks.get(numOfItems).toString());
                         numOfItems++;
+                        System.out.println("Now you have " + numOfItems + " tasks in the list.");
+                        break;
+                    case "delete":
+                        System.out.println("Noted. I've removed this task: ");
+                        System.out.println(tasks.get(Integer.parseInt(inputArr[1]) - 1));
+                        deleteCommand(inputArr, tasks);
+                        numOfItems--;
                         System.out.println("Now you have " + numOfItems + " tasks in the list.");
                         break;
                     default:
@@ -65,32 +74,35 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public static void doneCommand(Task[] list, String[] inputArr ){
+    public static void doneCommand(List<Task>tasks, String[] inputArr ){
         System.out.println("Nice! I've marked this task as done:");
-        list[Integer.parseInt(inputArr[1]) - 1].markAsDone();
-        System.out.println(list[Integer.parseInt(inputArr[1]) - 1].toString());
+        Task doneTask = tasks.get(Integer.parseInt(inputArr[1]) - 1);
+        doneTask.markAsDone();
+        System.out.println(tasks.get(Integer.parseInt(inputArr[1]) - 1).toString());
     }
 
-    public static void toDoCommand(String[] inputArr, Task[] list, int numOfItems, String input) throws DukeMissingInputException {
+    public static void toDoCommand(String[] inputArr, List<Task> tasks, int numOfItems, String input)
+            throws DukeMissingInputException {
         String description = "";
         if (input.equals("todo")) {
-            throw new DukeMissingInputException("OOPS!!! The description of a event cannot be empty.");
+            throw new DukeMissingInputException("OOPS!!! The description of a todo cannot be empty.");
         } else {
             for (int i = 1; i < inputArr.length; i++) {
                 description += (inputArr[i] + " ");
             }
         }
         description = description.trim();
-        list[numOfItems] = new Todo(description);
-        list[numOfItems].toString();
+        tasks.add(new Todo(description)) ;
+        tasks.get(numOfItems).toString();
     }
 
-    public static void deadlineCommand(String[] inputArr, Task[] list, int numOfItems, String input) throws DukeMissingInputException {
+    public static void deadlineCommand(String[] inputArr, List<Task> tasks, int numOfItems, String input)
+            throws DukeMissingInputException {
         String description = "";
         String deadline = "";
         boolean foundBy = false;
         if (input.equals("deadline")) {
-            throw new DukeMissingInputException("OOPS!!! The description of a event cannot be empty.");
+            throw new DukeMissingInputException("OOPS!!! The description of a deadline cannot be empty.");
         } else {
             for (int i = 1; i < inputArr.length; i++) {
                 if (inputArr[i].equals("/by")) {
@@ -105,15 +117,16 @@ public class Duke {
             }
         }
         deadline = deadline.trim();
-        list[numOfItems] = new Deadline(description, deadline);
+        tasks.add(new Deadline(description, deadline));
     }
 
-    public static void eventCommand(String[] inputArr, Task[] list, int numOfItems, String input) throws DukeMissingInputException {
+    public static void eventCommand(String[] inputArr, List<Task> tasks, int numOfItems, String input)
+            throws DukeMissingInputException {
         String description = "";
         String time = "";
         boolean foundAt = false;
         if (input.equals("event")) {
-            throw new DukeMissingInputException("OOPS!!! The description of a event cannot be empty.");
+            throw new DukeMissingInputException("OOPS!!! The description of an event cannot be empty.");
         } else {
             for (int i = 1; i < inputArr.length; i++) {
                 if (inputArr[i].equals("/at")){
@@ -128,6 +141,10 @@ public class Duke {
             }
         }
         time = time.trim();
-        list[numOfItems] = new Event(description, time);
+        tasks.add(new Event(description, time));
+    }
+
+    public static void deleteCommand(String[] inputArr, List<Task> tasks){
+        tasks.remove(Integer.parseInt(inputArr[1]) - 1);
     }
 }
