9f55ef1916eb1d49c48dcda3cd196c6730d03247
Icelenaugust <dingyoujia0823@gmail.com>
2021-01-19 13:34:03 +0800

    A-TextUiTesting done

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -13,6 +13,7 @@ public class ChatBot {
         //Greeting the user
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        System.out.println();
 
         while (sc.hasNext()) {
             //split the input into an array
@@ -30,27 +31,29 @@ public class ChatBot {
                 for (int i = 0; i < taskList.size(); i++) {
                     System.out.println ((i+1) + ". " + (taskList.get(i)));
                 }
+                System.out.println();
             } else if (input.equals("done")) {
                 //input is to finish a task
                 int index = Integer.parseInt(inputWords[1]) - 1;
                 taskList.get(index).taskDone();
-                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("Nice! I've marked this task as done:");
                 System.out.println(taskList.get(index));
+                System.out.println();
             } else if (input.equals("todo")) {
                 //input is to add a todoTask
-                System.out.println("Got it. I've added this task: ");
+                System.out.println("Got it. I've added this task:");
                 String taskName = "";
                 for (int i = 1; i < inputWords.length; i++) {
                     taskName = taskName + inputWords[i] + " ";
                 }
-                taskList.add(new TodoTask(taskName));
+                taskList.add(new TodoTask(taskName.stripTrailing()));
                 int len = taskList.size();
                 System.out.println(taskList.get(len - 1));
                 System.out.println("Now you have " + len + " tasks in the list.");
-
+                System.out.println();
             } else if (input.equals("deadline")){
                 //input is to add a deadlineTask
-                System.out.println("Got it. I've added this task: ");
+                System.out.println("Got it. I've added this task:");
                 String taskName = "";
                 String date = "";
                 int dateIndex = 0;
@@ -76,9 +79,10 @@ public class ChatBot {
                 int len = taskList.size();
                 System.out.println(taskList.get(len - 1));
                 System.out.println("Now you have " + len + " tasks in the list.");
+                System.out.println();
             } else if (input.equals("event")){
             //input is to add a eventTask
-            System.out.println("Got it. I've added this task: ");
+            System.out.println("Got it. I've added this task:");
             String taskName = "";
             String time = "";
             int timeIndex = 0;
@@ -104,6 +108,7 @@ public class ChatBot {
             int len = taskList.size();
             System.out.println(taskList.get(len - 1));
             System.out.println("Now you have " + len + " tasks in the list.");
+            System.out.println();
         }
 
 
