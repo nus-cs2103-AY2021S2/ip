facc64e9fdc3b5caed72f528934fb08c2c1f2287
ampan98 <e0407566@u.nus.edu>
2021-01-19 17:45:48 +0800

    Add ability to handle and respond to errors in user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,46 +19,66 @@ public class Duke {
         while (true) {
             String input = sc.nextLine();
             System.out.println("  ~~~");
-            if (input.equals("bye")) {
-                System.out.println("  See you next time.");
-                System.out.println("  ~~~");
-                break;
-            } else if (input.equals("list")) {
-                System.out.println("  Tasks in list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println("  " + (i + 1) + "." + tasks.get(i));
-                }
-            } else if (input.split(" ")[0].equals("done")) {
-                int i = Integer.parseInt(input.split(" ")[1]);
-                tasks.get(i - 1).markAsDone();
-                System.out.println("  Successfully marked as done:");
-                System.out.println("  " + tasks.get(i - 1));
-            } else  {
-                Task task;
-                switch (input.split(" ")[0]) {
-                    case "todo":
-                        task = new Todo(input.substring(5));
-                        break;
-                    case "deadline": {
-                        String[] inputs = input.substring(9).split("/by");
-                        String name = inputs[0] + "(by:" + inputs[1] + ")";
-                        task = new Deadline(name);
-                        break;
+            try {
+                if (input.equals("bye")) {
+                    System.out.println("  See you next time.");
+                    System.out.println("  ~~~");
+                    break;
+                } else if (input.equals("list")) {
+                    System.out.println("  Tasks in list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println("  " + (i + 1) + "." + tasks.get(i));
                     }
-                    case "event": {
-                        String[] inputs = input.substring(6).split("/at");
-                        String name = inputs[0] + "(at:" + inputs[1] + ")";
-                        task = new Event(name);
-                        break;
+                } else if (input.split(" ")[0].equals("done")) {
+                    int i;
+                    try {
+                        i = Integer.parseInt(input.split(" ")[1]);
+                    } catch (Exception ex) {
+                        throw new DukeException("  Please provide an integer.");
                     }
-                    default:
-                        task = new Task(input);
-                        break;
+                    if (i > tasks.size() || i <= 0) {
+                        throw new DukeException("  That number is invalid.");
+                    }
+                    tasks.get(i - 1).markAsDone();
+                    System.out.println("  Successfully marked as done:");
+                    System.out.println("  " + tasks.get(i - 1));
+                } else {
+                    Task task;
+                    switch (input.split(" ")[0]) {
+                        case "todo":
+                            if (input.split(" ").length <= 1) {
+                                throw new DukeException("  Please describe the task.");
+                            }
+                            task = new Todo(input.substring(5));
+                            break;
+                        case "deadline": {
+                            if (input.split(" ").length <= 1) {
+                                throw new DukeException("  Please describe the task.");
+                            }
+                            String[] inputs = input.substring(9).split("/by");
+                            String name = inputs[0] + "(by:" + inputs[1] + ")";
+                            task = new Deadline(name);
+                            break;
+                        }
+                        case "event": {
+                            if (input.split(" ").length <= 1) {
+                                throw new DukeException("  Please describe the task.");
+                            }
+                            String[] inputs = input.substring(6).split("/at");
+                            String name = inputs[0] + "(at:" + inputs[1] + ")";
+                            task = new Event(name);
+                            break;
+                        }
+                        default:
+                            throw new DukeException("  That command is invalid.");
+                    }
+                    tasks.add(task);
+                    System.out.println("  Task added:");
+                    System.out.println("    " + task);
+                    System.out.println("  Total tasks in list: " + tasks.size());
                 }
-                tasks.add(task);
-                System.out.println("  Task added:");
-                System.out.println("    " + task);
-                System.out.println("  Total tasks in list: " + tasks.size());
+            } catch (DukeException ex) {
+                System.out.println(ex);
             }
             System.out.println("  ~~~");
         }
