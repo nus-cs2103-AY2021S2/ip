ce2c32208824e0bee4644cd914411db6cee08d83
wongkokian <ianwong97@hotmail.sg>
2021-01-19 23:21:33 +0800

    Modified Duke to handle DukeExceptions.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,7 @@
 import java.util.*;
 
 public class Duke {
-    public static ArrayList<Task> tasks = new ArrayList<>();
-    public static Scanner sc = new Scanner(System.in);
+    protected static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -17,47 +16,94 @@ public class Duke {
                 + "     What can I do for you?\n"
                 + "    ____________________________________________________________\n";
         System.out.println(greet);
-        run(sc.nextLine());
+
+        Scanner sc = new Scanner(System.in);
+        run(sc.nextLine(), sc);
         sc.close();
     }
 
-    public static void run(String cmd) {
+    private static void run(String input, Scanner sc) {
         System.out.println("    ____________________________________________________________");
-
-        if(cmd.equals("bye")) {
-            System.out.println("     Bye. Hope to see you again soon!");
-        } else if (cmd.equals("list")) {
-            System.out.println("     Here are the tasks in your list:");
-            for (int i = 0; i < tasks.size(); i++) {
-                Task curr = tasks.get(i);
-                System.out.println("     " + (i + 1) + "." + curr.toString());
-            }
-        } else if (cmd.startsWith("done")) {
-            int i = Integer.parseInt(cmd.substring(5)) - 1;
-            tasks.get(i).markAsDone();
-            Task curr = tasks.get(i);
-            System.out.println("     Nice! I've marked this task as done:\n"
-                    + "       " + curr.toString());
-        } else {
-            Task curr;
-            if (cmd.startsWith("todo")) {
-                curr = new Todo(cmd.substring(5));
-            } else if (cmd.startsWith("deadline")) {
-                int end = cmd.indexOf('/') - 1;
-                curr = new Deadline(cmd.substring(9, end), cmd.substring(end + 5));
+        String[] inputArr = input.split(" ");
+        try {
+            if (inputArr[0].equals("bye") && inputArr.length == 1) {
+                System.out.println("     Bye. Hope to see you again soon!");
+            } else if (inputArr[0].equals("list") && inputArr.length == 1) {
+                System.out.println("     Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    Task curr = tasks.get(i);
+                    System.out.println("     " + (i + 1) + "." + curr.toString());
+                }
+            } else if (inputArr[0].equals("done") && inputArr.length < 3) {
+                if (inputArr.length == 1) {
+                    throw new DukeException("       OOPS!!! The task number cannot be empty.");
+                } else if (!isNumeric(inputArr[1])) {
+                    throw new DukeException("       OOPS!!! The task number must be numeric.");
+                } else {
+                    int i = Integer.parseInt(inputArr[1]) - 1;
+                    if (i > (tasks.size() - 1) || i < 0) {
+                        throw new DukeException("       OOPS!!! The task number is out of range.");
+                    } else {
+                        tasks.get(i).markAsDone();
+                        Task curr = tasks.get(i);
+                        System.out.println("     Nice! I've marked this task as done:\n"
+                                + "       " + curr.toString());
+                    }
+                }
+            } else if (inputArr[0].equals("todo") || inputArr[0].equals("deadline") || inputArr[0].equals("event")) {
+                switch (inputArr[0]) {
+                    case "todo":
+                        if (inputArr.length == 1) {
+                            throw new DukeException("       OOPS!!! The description of a todo cannot be empty.");
+                        } else {
+                            tasks.add(new Todo(input.substring(5)));
+                        }
+                        break;
+                    case "deadline":
+                        if (inputArr.length == 1) {
+                            throw new DukeException("       OOPS!!! The description and due date of a deadline cannot be empty.");
+                        } else {
+                            String[] details = input.substring(9).split(" /by ");
+                            if (details.length == 1) {
+                                throw new DukeException("       OOPS!!! The due date of a deadline cannot be empty.");
+                            } else {
+                                tasks.add(new Deadline(details[0], details[1]));
+                            }
+                        }
+                        break;
+                    case "event":
+                        if (inputArr.length == 1) {
+                            throw new DukeException("       OOPS!!! The description and time frame of an event cannot be empty.");
+                        } else {
+                            String[] details = input.substring(6).split(" /at ");
+                            if (details.length == 1) {
+                                throw new DukeException("       OOPS!!! The time frame of an event cannot be empty.");
+                            } else {
+                                tasks.add(new Event(details[0], details[1]));
+                            }
+                        }
+                        break;
+                }
+                System.out.println("     Got it. I've added this task:\n"
+                        + "       " + tasks.get(tasks.size() - 1).toString() + "\n"
+                        + "     Now you have " + tasks.size() + " tasks in the list.");
             } else {
-                int end = cmd.indexOf('/') - 1;
-                curr = new Event(cmd.substring(6, end), cmd.substring(end + 5));
+                throw new DukeException("       OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
-            tasks.add(curr);
-            System.out.println("     Got it. I've added this task:\n"
-                    + "       " + curr.toString() + "\n"
-                    + "     Now you have " + tasks.size() + " tasks in the list.");
+        } catch (DukeException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            System.out.println("    ____________________________________________________________\n");
+            if (!input.equals("bye")) {
+                run(sc.nextLine(), sc);
+            }
         }
-        System.out.println("    ____________________________________________________________\n");
+    }
 
-        if(!cmd.equals("bye")) {
-            run(sc.nextLine());
+    private static boolean isNumeric(String str) {
+        if (str == null || str.length() == 0) {
+            return false;
         }
+        return str.chars().allMatch(Character::isDigit);
     }
 }
