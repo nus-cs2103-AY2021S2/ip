95ee94c9fc620f41857a03b0e928371f2118f794
LI <gabriellee0903@gmail.com>
2021-01-19 15:50:06 +0800

    More helper functions added to improve the readability

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,68 +2,87 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
+    private static String PARTING_LINE = "____________________________________________________________";
+    private static ArrayList<Task> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
+        printGreeting();
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(PARTING_LINE);
+            if (input.equals("list")) {
+                listTasks();
+            } else if (isDoneCommand(input)){
+                int index = Character.getNumericValue(input.charAt(5)) - 1;
+                markAsComplete(index);
+            } else if (isTask(input)) {
+                addThisTask(input);
+            }
+            System.out.println(PARTING_LINE);
+            input = sc.nextLine();
+        }
+        farewell();
+    }
+
+    public static void printGreeting() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String partingLine = "____________________________________________________________";
-
         System.out.println("Hello from\n" + logo);
-        System.out.println(partingLine);
+        System.out.println(PARTING_LINE);
         System.out.println("Sup. I am Duke.");
         System.out.println("What do you want?");
-        System.out.println(partingLine);
+        System.out.println(PARTING_LINE);
+    }
 
-        ArrayList<Task> tasks = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+    public static void farewell() {
+        System.out.println(PARTING_LINE);
+        System.out.println(" See you.");
+        System.out.println(PARTING_LINE);
+    }
 
-        while (!input.equals("bye")) {
-            System.out.println(partingLine);
-            if (input.equals("list")) {
-                System.out.println(" Here are the tasks: ");
-                for (int i = 0; i < tasks.size(); i++) {
-                    Task tempTask = tasks.get(i);
-                    System.out.println(" " + (i + 1) + "." + tempTask);
-                }
-            } else if (isDoneCommand(input)){
-                int index = Character.getNumericValue(input.charAt(5)) - 1;
-                if (index < tasks.size()) {
-                    Task completedTask = tasks.get(index);
-                    completedTask.complete();
-                    System.out.println(" Marked. How cool is that?");
-                    System.out.println("  " + completedTask);
-                }
-            } else if (isTask(input)) {
-                System.out.println(" Added: ");
-                Task thisTask;
-                String name;
-                if (isTodo(input)) {
-                    name = input.substring(5);
-                    thisTask = new Todo(name);
-                } else if (isDeadline(input)) {
-                    int byIndex = input.indexOf(" /by ");
-                    name = input.substring(9, byIndex);
-                    String ddl = input.substring(byIndex + 5);
-                    thisTask = new Deadline(name, ddl);
-                } else {
-                    int atIndex = input.indexOf(" /at ");
-                    name = input.substring(6, atIndex);
-                    String date = input.substring(atIndex + 5);
-                    thisTask = new Event(name, date);
-                }
-                tasks.add(thisTask);
-                System.out.println("  " + thisTask);
-                System.out.println(" Now you have " + tasks.size() + " tasks.");
-            }
-            System.out.println(partingLine);
-            input = sc.nextLine();
+    public static void listTasks() {
+        System.out.println(" Here are the tasks: ");
+        for (int i = 0; i < tasks.size(); i++) {
+            Task tempTask = tasks.get(i);
+            System.out.println(" " + (i + 1) + "." + tempTask);
         }
-        System.out.println(partingLine);
-        System.out.println(" Seeya.");
-        System.out.println(partingLine);
+    }
+
+    public static void markAsComplete(int index) {
+        if (index < tasks.size()) {
+            Task completedTask = tasks.get(index);
+            completedTask.complete();
+            System.out.println(" Marked. How cool is that?");
+            System.out.println("  " + completedTask);
+        }
+    }
+
+    public static void addThisTask(String input) {
+        System.out.println(" Added: ");
+        Task thisTask;
+        String name;
+        if (isTodo(input)) {
+            name = input.substring(5);
+            thisTask = new Todo(name);
+        } else if (isDeadline(input)) {
+            int byIndex = input.indexOf(" /by ");
+            name = input.substring(9, byIndex);
+            String ddl = input.substring(byIndex + 5);
+            thisTask = new Deadline(name, ddl);
+        } else {
+            int atIndex = input.indexOf(" /at ");
+            name = input.substring(6, atIndex);
+            String date = input.substring(atIndex + 5);
+            thisTask = new Event(name, date);
+        }
+        tasks.add(thisTask);
+        System.out.println("  " + thisTask);
+        System.out.println(" Now you have " + tasks.size() + " tasks.");
     }
 
     /**
