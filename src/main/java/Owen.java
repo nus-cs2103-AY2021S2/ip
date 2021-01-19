2a571a4b627c0e6ff5649237b63736ebe02caf64
Kingsley Kuan <kingsley@kingsleykuan.com>
2021-01-19 23:49:04 +0800

    Implement Add and List commands in Owen
    * Echo command is replaced by Add command
    * Add command adds items to a list that Owen keeps track of
    * List command lists out items that Owen is keeping track of

--- src/main/java/Owen.java
+++ src/main/java/Owen.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Owen is a personal assistant chatbot.
  * 
@@ -7,6 +10,7 @@
 public class Owen implements Chatbot {
     private final boolean isRunning;
     private final Response latestResponse;
+    private final List<String> items;
 
     /**
      * Creates an Owen chatbot with a hello response.
@@ -23,17 +27,20 @@ public class Owen implements Chatbot {
         stringBuilder.append(logo);
         stringBuilder.append("\nHello I am Owen the Owl!");
         this.latestResponse = new DefaultResponse(stringBuilder.toString());
+
+        this.items = new ArrayList<>();
     }
 
-    private Owen(boolean isRunning, Response latestResponse) {
+    private Owen(boolean isRunning, Response latestResponse, List<String> items) {
         this.isRunning = isRunning;
         this.latestResponse = latestResponse;
+        this.items = items;
     }
 
     @Override
     public Owen shutdown() {
         Response shutdownResponse = new DefaultResponse("Bye. Hope to see you again soon!");
-        return new Owen(false, shutdownResponse);
+        return new Owen(false, shutdownResponse, this.items);
     }
 
     @Override
@@ -49,11 +56,26 @@ public class Owen implements Chatbot {
     @Override
     public Owen parseCommand(String command) {
         switch (command) {
+        case "list":
+            Response listResponse = new DefaultResponse(this.getItemsListAsString());
+            return new Owen(this.isRunning, listResponse, this.items);
         case "bye":
             return this.shutdown();
         default:
-            Response response = new DefaultResponse(command);
-            return new Owen(true, response);
+            List<String> itemsCopy = new ArrayList<>(this.items);
+            itemsCopy.add(command);
+            String addedFormat = "added: %s";
+            Response addResponse = new DefaultResponse(String.format(addedFormat, command));
+            return new Owen(this.isRunning, addResponse, itemsCopy);
         }
+    }
+
+    private String getItemsListAsString() {
+        String[] listItemStrings = new String[this.items.size()];
+        String listItemFormat = "%d. %s";
+        for (int i = 0; i < this.items.size(); i++) {
+            listItemStrings[i] = String.format(listItemFormat, i + 1, this.items.get(i));
+        }
+        return String.join("\n", listItemStrings);
     }
 }
