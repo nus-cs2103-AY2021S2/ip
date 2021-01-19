f7703abf6a38e3783acaf154ac91878004fbf318
branzuelajohn <branzuelajohn@gmail.com>
2021-01-19 19:23:06 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int counter = 0;
         Duke.printHorizontalLine();
         System.out.println("Hello! I'm Duke");
@@ -11,29 +11,59 @@ public class Duke {
         Duke.printHorizontalLine();
         while(true) {
             String s = sc.nextLine();
-            if(!s.equals("bye") && !s.equals("list")) {
-                Duke.printHorizontalLine();
-                System.out.printf("added: %s%n",s);
-                Duke.printHorizontalLine();
-                list[counter++] = s;
+            if(s.equals("bye")) {
+                Duke.byeCommand();
+                break;
             } else if(s.equals("list")) {
                 Duke.printHorizontalLine();
-                for(int i = 0; i < counter; i++) {
-                    int j = i + 1;
-                    System.out.printf("%d. %s%n", j, list[i]);
-                }
+                Duke.listCommand(list, counter);
                 Duke.printHorizontalLine();
-
             } else {
-                Duke.printHorizontalLine();
-                System.out.println("Bye. Hope to see you again soon!");
-                Duke.printHorizontalLine();
-                break;
+                String[] array = s.split(" ");
+                if(array[0].equals("done")) {
+                    doneCommand(list,array);
+                } else {
+                    Task t = new Task(s);
+                    addCommand(t);
+                    list[counter++] = t;
+                }
             }
         }
     }
 
     public static void printHorizontalLine() {
         System.out.println("____________________________________________________________");
+    }
+
+    public static void byeCommand() {
+        Duke.printHorizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        Duke.printHorizontalLine();
+    }
+
+    public static void listCommand(Task[] list, int counter) {
+        System.out.println("Here are the tasks in your list:");
+        for(int i = 0; i < counter; i++) {
+            int j = i + 1;
+            Task t = list[i];
+            System.out.printf("[%s] %d. %s%n", t.getStatusIcon(), j , t.getDescription());
+        }
+    }
+
+    public static void doneCommand(Task[] list, String[] array) {
+        int m = Integer.valueOf(array[1]) - 1;
+        Task t  = list[m];
+        list[m] = t.markAsDone();
+        t = list[m];
+        Duke.printHorizontalLine();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.printf("[%s] %s%n", t.getStatusIcon(), t.getDescription());
+        Duke.printHorizontalLine();
+    }
+
+    public static void addCommand(Task t) {
+        Duke.printHorizontalLine();
+        System.out.printf("added: %s%n", t);
+        Duke.printHorizontalLine();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public Task(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public Task markAsDone() {
+        return new Task(this.description, true);
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    public String toString() {
+        return String.format(this.getDescription());
+    }
+}
