dbfb58e615a655487cdda239b0dbcc3e60bf5cf0
jellymias <51073704+jellymias@users.noreply.github.com>
2021-01-19 15:43:29 +0800

    Update Duke.java
    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        boolean exit = false;
+        while (!exit) {
+            String cmd = sc.next();
+            if (cmd.equals("bye")) {
+                exit = true;
+            } else {
+                System.out.println(cmd);
+                cmd = sc.nextLine();
+            }
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
