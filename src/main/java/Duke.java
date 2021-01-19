71f01da926664bbeed93a9daf03d8b96b35081e4
aaronsms <aaronsms@u.nus.edu>
2021-01-19 17:56:02 +0800

    Add echo functionality to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+    public static void main(final String[] args) {
+        final String logo = " ____        _        \n" + "|  _ \\ _   _| | _____ \n" + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        horizontalLine();
+
+        final Scanner scan = new Scanner(System.in);
+        while (true) {
+            final String input = scan.nextLine();
+            horizontalLine();
+            if (input.toLowerCase().equals("bye")) {
+                System.out.println("\tBye. Hope to see you again soon!");
+                horizontalLine();
+                break;
+            }
+            System.out.println("\t" + input);
+            horizontalLine();
+        }
+        scan.close();
+    }
+
+    private static void horizontalLine() {
+        System.out.println("─────────────────────────────────────────────────");
     }
 }
