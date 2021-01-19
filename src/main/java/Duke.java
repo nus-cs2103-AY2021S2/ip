bdaded0b93a4ab335750d5d99d4466759f2943d8
jxrrelo <jarylloh1997@gmail.com>
2021-01-19 13:26:25 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ public class Duke {
     public static void main(String[] args) {
         String inputArr[] = new String[100];
         Scanner sc = new Scanner(System.in);
-        System.out.println("    ----------------------------------\n    " + "Hello! I'm Duke\n    What can I do for you?" + "\n    ----------------------------------\n");
+        System.out.println("    ----------------------------------\n    Hello! I'm Duke\n    What can I do for you?\n    ----------------------------------\n");
         String input = "";
         int n = 0;
 
@@ -12,8 +12,16 @@ public class Duke {
             input = sc.nextLine();
 
             if(input.equals("bye")) break;
+            if(input.equals("list")) {
+                System.out.println("    ----------------------------------");
+                for(int i = 1; i <= n; i++) {
+                    System.out.println("    " + i + ". " + inputArr[i - 1]);
+                }
+                System.out.println("    ----------------------------------\n");
+            } else {
+                System.out.println("    ----------------------------------\n    " + "added: " + input + "\n    ----------------------------------\n");
+            }
             inputArr[n] = input;
-            System.out.println("    ----------------------------------\n    " + inputArr[n] + "\n    ----------------------------------\n");
             n++;
         }
         System.out.println("    ----------------------------------\n    " + "Bye. Hope to see you again soon!" + "\n    ----------------------------------\n");
