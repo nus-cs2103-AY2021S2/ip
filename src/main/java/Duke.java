e161a99aa465ca8ef66c46bbf4cc3d2f4356347e
DESKTOP-3V9VV9N\chlol <chloleila@yahoo.com.sg>
2021-01-19 17:49:11 +0800

    Implement Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,13 +15,28 @@ public class Duke {
         System.out.println(line);
         Scanner sc = new Scanner(System.in);
         String toPrint = "";
-        String input = sc.next();
+        String input = sc.nextLine();
+        List<String> list = new ArrayList<>();
         while (!input.equals("bye")) { //user input is not bye
             System.out.println(line);
-            toPrint = input;
-            System.out.println(align(toPrint));
+            if (input.equals("list")) {
+                toPrint = ""; //reinitialising toPrint
+                for (int i = 1; i < list.size() + 1; i++) {
+                    String point = i + ". " + list.get(i - 1);
+                    if (i != list.size()) {
+                        toPrint = toPrint + align(point) + "\n";
+                    } else {
+                        toPrint = toPrint + align(point);
+                    }
+                }
+                System.out.println(toPrint);
+            } else {
+                list.add(input);
+                toPrint = "added: " + input;
+                System.out.println(align(toPrint));
+            }
             System.out.println(line);
-            input = sc.next();
+            input = sc.nextLine();
         }
         System.out.println(line);
         System.out.println(align("Goodnight! MouMou will go back to sleep now."));
@@ -30,9 +45,9 @@ public class Duke {
 
     static String align(String s) { // to align
         int stringLength = s.length();
-        int lineLength = 73;
-        int left = (int) Math.ceil((lineLength - stringLength) / 2);
-        int right = (int) Math.floor((lineLength - stringLength) / 2);
+        int LINELENGTH = 73;
+        int left = (int) Math.ceil((LINELENGTH - stringLength) / 2);
+        int right = (int) Math.floor((LINELENGTH - stringLength) / 2);
         String newString = "";
         newString = String.format("%" + left + "s", " ") + s + String.format("%" + right + "s", " ");
         return newString;
