be334af2dec2acb30ca131a6a699467949e5c3dc
wei-yutong <weiyutongggg@gmail.com>
2021-01-19 14:18:14 +0800

    added Level-4: todos, events, deadlines

--- src/DukeIncrements.java
+++ src/DukeIncrements.java
@@ -12,7 +12,7 @@ public class DukeIncrements {
         Task[] arr = new Task[100];
         while (sc.hasNext()) {
             String input = sc.nextLine();
-            String[] str = input.split(" ");
+            String[] str = input.split(" ", 2);
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println(line);
                 System.out.println("sayonara nerd! c ya soon ;)");
@@ -21,7 +21,7 @@ public class DukeIncrements {
                 System.out.println(line);
                 System.out.println("get to work! these are the tasks in your list: ");
                 for (int i = 0; i < count; i++) {
-                    System.out.println(i + 1 + ".[" + arr[i].getStatus() + "] " + arr[i].description);
+                    System.out.println(i + 1 + "." + arr[i].toString());
                 }
                 System.out.println(line);
             } else if (str[0].equalsIgnoreCase("done")) {
@@ -30,7 +30,36 @@ public class DukeIncrements {
                 Task temp = arr[taskNum - 1];
                 temp.markDone();
                 System.out.println("cool! this task is marked as done:");
-                System.out.println("[" + temp.getStatus() + "] " + temp.description);
+                System.out.println(temp.toString());
+                System.out.println(line);
+            } else if (str[0].equalsIgnoreCase("todo")) {
+                System.out.println(line);
+                ToDo tempT = new ToDo(str[1]);
+                arr[count] = tempT;
+                System.out.println("ok! i've added this task:");
+                System.out.println(tempT.toString());
+                count++;
+                System.out.println("you have " + count + " tasks in your list! keep working!");
+                System.out.println(line);
+            } else if (str[0].equalsIgnoreCase("event")) {
+                System.out.println(line);
+                String[] strE = str[1].split("/at", 2);
+                Event tempE = new Event(strE[0], strE[1]);
+                arr[count] = tempE;
+                System.out.println("ok! i've added this task:");
+                System.out.println(tempE.toString());
+                count++;
+                System.out.println("you have " + count + " tasks in your list! keep working!");
+                System.out.println(line);
+            } else if (str[0].equalsIgnoreCase("deadline")) {
+                System.out.println(line);
+                String[] strD = str[1].split("/by", 2);
+                Deadline tempD = new Deadline(strD[0], strD[1]);
+                arr[count] = tempD;
+                System.out.println("ok! i've added this task:");
+                System.out.println(tempD.toString());
+                count++;
+                System.out.println("you have " + count + " tasks in your list! keep working!");
                 System.out.println(line);
             } else {
                 System.out.println(line);
@@ -44,8 +73,8 @@ public class DukeIncrements {
 }
 
 class Task {
-    String description;
-    boolean isDone;
+    protected String description;
+    protected boolean isDone;
     public Task(String description) {
         this.description = description;
         this.isDone = false;
@@ -56,5 +85,47 @@ class Task {
 
     public void markDone() {
         isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatus() + "] " + description;
+    }
+}
+
+class Deadline extends Task {
+    protected String by;
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+class ToDo extends Task {
+    public ToDo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Event extends Task {
+    protected String at;
+    public Event (String description, String at) {
+        super(description);
+        this.at = at;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + at + ")";
     }
 }
