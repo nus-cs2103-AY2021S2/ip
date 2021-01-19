6bb393ed993592dcc10e7d08456a51d62b95e754
DESKTOP-VHI0UFD\Lim Li Gang <ligangjurong8@gmail.com>
2021-01-19 12:22:03 +0800

    iP Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
 package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,14 +13,23 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Greetings and salutations! I am Duke, a helpful chatbot.\nPlease enter your command(s).");
+        System.out.println("To view the list of tasks, enter the command 'list'.");
         System.out.println("To exit, enter the command 'bye'.");
         Scanner sc = new Scanner(System.in);
+        List<String> list = new ArrayList<>(100);
         while (true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                StringBuilder output = new StringBuilder();
+                for (int i = 1; i <= list.size(); i++) {
+                    output.append(i).append(". ").append(list.get(i - 1)).append("\n");
+                }
+                System.out.println(output);
             } else {
-                System.out.println(input);
+                list.add(input);
+                System.out.println("added: " + input);
             }
         }
         System.out.println("Farewell. Hope that you will visit again soon!");
