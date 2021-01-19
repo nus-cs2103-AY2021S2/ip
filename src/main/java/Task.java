23f332621e5efb06dfaab6eec23ac86d0f0a18f7
CSjiade <jdlian505@gmail.com>
2021-01-20 00:22:53 +0800

    updated Level3

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -13,7 +13,7 @@ public class Task {
   }
 
   public String getStatusIcon() {
-    return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    return (isDone ? "\u2718" : " "); //return tick or X symbols
   }
 
   public void taskDone() {
