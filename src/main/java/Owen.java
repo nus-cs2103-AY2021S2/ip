7ba28b1ba58054b9b0296fa6b7f821aa7f203736
Kingsley Kuan <kingsley@kingsleykuan.com>
2021-01-19 17:52:47 +0800

    Rename bot name from Duke to Owen

---
+++ src/main/java/Owen.java
@@ -0,0 +1,11 @@
+public class Owen {
+    public static void main(String[] args) {
+        String logo = ""
+                + " /\\_/\\ \n"
+                + "((OvO))\n"
+                + "():::()\n"
+                + " VV-VV \n";
+        System.out.println(logo);
+        System.out.println("Hello I am Owen the Owl!");
+    }
+}
