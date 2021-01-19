af1fd114c8aea8cb2ec7f2ce9796b77a437d9819
jellymias <51073704+jellymias@users.noreply.github.com>
2021-01-19 15:58:56 +0800

    Update Duke.java
    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,14 +5,21 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         Scanner sc = new Scanner(System.in);
+        String[] commands = new String[100];
+        int index = 0;
         boolean exit = false;
         while (!exit) {
-            String cmd = sc.next();
+            String cmd = sc.nextLine();
             if (cmd.equals("bye")) {
                 exit = true;
+            } else if (cmd.equals("list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.println(String.valueOf(i + 1) + ". " + commands[i]);
+                }
             } else {
-                System.out.println(cmd);
-                cmd = sc.nextLine();
+                System.out.println("added: " + cmd);
+                commands[index] = cmd;
+                index++;
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
