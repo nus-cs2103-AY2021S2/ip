aef63690e11ba950f28c9fd701e3797c66c5a527
Kingsley Kuan <kingsley@kingsleykuan.com>
2021-01-19 22:16:10 +0800

    Implement main loop that handles user input / output

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Chatbot owen = new Owen();
         Response response = owen.getResponse();
         System.out.println(response);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (owen.isRunning()) {
+            String command = scanner.nextLine();
+            owen = owen.parseCommand(command);
+            System.out.println(owen.getResponse().getFormattedResponse());
+        }
+
+        scanner.close();
     }
 }
