b8a3d49dec9bccfecb23375f49137bea6f702977
github-amanda <57126871+github-amanda@users.noreply.github.com>
2021-01-19 19:04:11 +0800

    Level-3, A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,19 +4,25 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int index = 0;
         while (!command.equals("bye")) {
             String[] tokens = command.split(" ");
             String commandType = tokens[0];
             if (commandType.equals("list")) {
                 // SHOW LIST
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < index; i++) {
                     System.out.printf("%d. " + list[i] + "\n", i + 1);
                 }
+            } else if (commandType.equals("done")){
+                    // MARK TASK AS COMPLETE
+                    int taskIndex = Integer.parseInt(tokens[1]) - 1;
+                    list[taskIndex].markAsDone();
+                    System.out.println("Nice! I've marked this task as done: \n" +  list[taskIndex].toString());
             } else {
-                list[index] = command;
-                System.out.println("added: " + command);
+                list[index] = new Task(command);
+                System.out.println("added: " + list[index]);
                 index++;
             }
             command = scanner.nextLine();
