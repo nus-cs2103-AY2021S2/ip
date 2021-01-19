b3447fd4eeeabeec8059a6f91e486545575051fb
LI <gabriellee0903@gmail.com>
2021-01-19 15:27:15 +0800

    Updated and corrected some coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,8 +8,7 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String partingLine = "_____________________________"
-                + "_______________________________";
+        String partingLine = "____________________________________________________________";
 
         System.out.println("Hello from\n" + logo);
         System.out.println(partingLine);
@@ -24,21 +23,18 @@ public class Duke {
         while (!input.equals("bye")) {
             System.out.println(partingLine);
             if (input.equals("list")) {
-                System.out.println("Here are the tasks: ");
+                System.out.println(" Here are the tasks: ");
                 for (int i = 0; i < tasks.size(); i++) {
                     Task tempTask = tasks.get(i);
-                    System.out.println(" " + (i + 1) + "."
-                            + tempTask);
+                    System.out.println(" " + (i + 1) + "." + tempTask);
                 }
             } else if (isDoneCommand(input)){
-                int index = Character
-                        .getNumericValue(input.charAt(5)) - 1;
+                int index = Character.getNumericValue(input.charAt(5)) - 1;
                 if (index < tasks.size()) {
                     Task completedTask = tasks.get(index);
                     completedTask.complete();
-                    System.out.println("Marked. "
-                            + "How cool is that?");
-                    System.out.println(completedTask);
+                    System.out.println(" Marked. How cool is that?");
+                    System.out.println("  " + completedTask);
                 }
             } else if (isTask(input)) {
                 System.out.println(" Added: ");
@@ -60,8 +56,7 @@ public class Duke {
                 }
                 tasks.add(thisTask);
                 System.out.println("  " + thisTask);
-                System.out.println("Now you have "
-                        + tasks.size() + " tasks.");
+                System.out.println(" Now you have " + tasks.size() + " tasks.");
             }
             System.out.println(partingLine);
             input = sc.nextLine();
@@ -78,28 +73,23 @@ public class Duke {
      * @return true if the input is valid, false otherwise
      */
     public static boolean isDoneCommand(String input) {
-        return (input.length() > 5
-                && input.substring(0, 5).equals("done ")
+        return (input.length() > 5 && input.substring(0, 5).equals("done ")
                 && Character.isDigit(input.charAt(5)));
     }
 
     public static boolean isTask(String input) {
-        return (isTodo(input) || isDeadline(input)
-                || isEvent(input));
+        return (isTodo(input) || isDeadline(input) || isEvent(input));
     }
 
     public static boolean isTodo(String input) {
-        return (input.length() > 5
-                && input.substring(0, 5).equals("todo "));
+        return (input.length() > 5 && input.substring(0, 5).equals("todo "));
     }
 
     public static boolean isDeadline(String input) {
-        return (input.length() > 9
-                && input.substring(0, 9).equals("deadline "));
+        return (input.length() > 9 && input.substring(0, 9).equals("deadline "));
     }
 
     public static boolean isEvent(String input) {
-        return (input.length() > 6
-                && input.substring(0, 6).equals("event "));
+        return (input.length() > 6 && input.substring(0, 6).equals("event "));
     }
 }
