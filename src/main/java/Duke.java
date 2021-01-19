88c9e8cd30caae0be8203f936abd4e4c05f5e157
clarlzx <clarlzx@gmail.com>
2021-01-19 16:40:51 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+
+    private static ArrayList<String> strArr = new ArrayList<>();
 
     public static void lines() {
         System.out.println("__________________________" +
@@ -8,35 +11,58 @@ public class Duke {
     }
 
     public static void greet() {
-        lines();
         String greetingCat = " ฅ(=^・ω・^=ฅ)";
         System.out.println(greetingCat);
         System.out.println("Mew! I'm Chat the Cat");
         System.out.println("What can I do for you?");
-        lines();
     }
 
     public static void echo(String s) {
-        lines();
         System.out.println(s);
-        lines();
     }
 
     public static void goodbye() {
-        lines();
         String goodByeCat = "（=^・ω・^=❁）";
         System.out.println("✧･ﾟ:* Goodbye *:･ﾟ✧" + goodByeCat);
-        lines();
+    }
+
+    public static void add(String s) {
+        strArr.add(s);
+        String goCat = "(^・ω・^=)~~✧✧";
+        System.out.println(goCat);
+        System.out.println("Added: " + s);
+    }
+
+    public static void list() {
+        int i = 0;
+        System.out.println(" ฅ list ฅ ");
+        while (i < strArr.size()) {
+            System.out.println(Integer.toString(i + 1) + ": " + strArr.get(i));
+            i++;
+        }
     }
 
     public static void main(String[] args) {
+        //only add lines here
+        lines();
         greet();
+        lines();
+
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+        String str = sc.nextLine();
         while (!str.equals("bye")) {
-            echo(str);
-            str = sc.next();
+            lines();
+            if (str.equals("list")) {
+                list();
+            } else {
+                add(str);
+            }
+            lines();
+            str = sc.nextLine();
         }
+
+        lines();
         goodbye();
+        lines();
     }
 }
