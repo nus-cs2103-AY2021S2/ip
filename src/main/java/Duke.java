b9c5cd9840afa75fc2c17b9006ca1d7cb6c7fa05
vivegank <77456578+vivegank@users.noreply.github.com>
2021-01-20 02:05:03 +0800

    Level 3 (Mark as Done) done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    List<String> tasks;
+    List<Task> tasks;
 
     public Duke() {
         this.tasks = new ArrayList<>();
@@ -22,31 +22,37 @@ public class Duke {
 
     public void scan() {
         Scanner s = new Scanner(System.in);
-        String instruction = s.nextLine();
-        while(!instruction.equals("bye")) {
-            manageTask(instruction);
-            instruction = s.nextLine();
+        String description = s.nextLine();
+        while(!description.equals("bye")) {
+            manageTask(description);
+            description = s.nextLine();
         }
-        echo("Bye. Cya");
+        manageTask(description);
+        s.close();
     }
 
-    public void manageTask(String task) {
-        if(task.equals("list")) {
-            System.out.println("     --------------------------------");
-            for(int i = 0; i < tasks.size(); i++) {
-                String rank = String.valueOf(i + 1);
-                System.out.println("     " + rank + ". " + tasks.get(i));
-            }
-            System.out.println("     --------------------------------");
-        } else {
-            tasks.add(task);
-            echo(("added: " + task));
-        }
-    }
-
-    public static void echo(String instruction) {
+    public void manageTask(String description) {
         System.out.println("     --------------------------------");
-        System.out.println("     " + instruction);
+        if (description.equals("list")) {
+            System.out.println("     Here are the tasks in your list:");
+            for (int i = 0; i < tasks.size(); i++) {
+                String rank = String.valueOf(i + 1);
+                System.out.println("     " + rank + "." + tasks.get(i));
+            }
+        } else if (description.equals("bye")){
+            System.out.println("     Bye.Cya");
+        } else if (description.contains("done")) {
+            String taskLocation = description.substring(5);
+            int taskNumber = Integer.parseInt(taskLocation);
+            Task doneTask = tasks.get(taskNumber - 1);
+            doneTask.markAsDone();
+            System.out.println("     Noice! I've marked this task as done:");
+            System.out.println("     " + doneTask);
+        } else {
+            Task newTask = new Task(description);
+            tasks.add(newTask);
+            System.out.println("     added: " + newTask.getDescription());
+        }
         System.out.println("     --------------------------------");
     }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String checkStatus() {
+        return (isDone ? "\u2713" : " ");
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + checkStatus() + "] " + description;
+    }
+}
