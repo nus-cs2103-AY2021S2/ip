83bf6a590c1825131e26fc4824fb87070a89159f
DESKTOP-VHI0UFD\Lim Li Gang <ligangjurong8@gmail.com>
2021-01-20 00:32:17 +0800

    iP Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,7 +16,7 @@ public class Duke {
         System.out.println("To view the list of tasks, enter the command 'list'.");
         System.out.println("To exit, enter the command 'bye'.");
         Scanner sc = new Scanner(System.in);
-        List<String> list = new ArrayList<>(100);
+        List<Task> list = new ArrayList<>(100);
         while (true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
@@ -27,8 +27,20 @@ public class Duke {
                     output.append(i).append(". ").append(list.get(i - 1)).append("\n");
                 }
                 System.out.println(output);
+            } else if (input.substring(0, 4).equals("done")) {
+                try {
+                    int pointer = Integer.parseInt(input.substring(5)) - 1;
+                    Task task = list.get(pointer);
+                    task.markAsDone();
+                    list.set(pointer, task);
+                    System.out.println("Sweet! I've marked this task as done:\n" + list.get(pointer));
+                } catch (NumberFormatException e) {
+                    System.out.println("Number format exception, enter a number from 1-100!");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println ("Array index is out of bounds, enter a number from 1-100!");
+                }
             } else {
-                list.add(input);
+                list.add(new Task(input));
                 System.out.println("added: " + input);
             }
         }
