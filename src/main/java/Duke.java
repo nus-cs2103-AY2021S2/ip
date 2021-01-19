3285bf4d7bc9105f16a913ddba3a9ad912f1f780
maxxng <maxngkaishen@gmail.com>
2021-01-19 17:40:49 +0800

    add greet echo exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "   ___     ___    _  _     ___     ___     _     \n" +
+                "  |   \\   /   \\  | \\| |   |_ _|   | __|   | |    \n" +
+                "  | |) |  | - |  | .` |    | |    | _|    | |__  \n" +
+                "  |___/   |_|_|  |_|\\_|   |___|   |___|   | ____| \n" +
+                "_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| \n" +
+                "\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' \n";
+        System.out.println("Welcome back Max, I'm a Dumb Assistant " +
+                "Naively Intended (to) Ease Labour\n" + logo +
+                "____________________________________________________________\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n");
+
+        //++++++++++++++++++++++++++ IGNORE THE CODE ABOVE +++++++++++++++++++++++++++++
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        List<String> list = new ArrayList<>();
+
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                for (int i = 1; i <= list.size(); i++) {
+
+                }
+            }
+            System.out.println(
+                    "____________________________________________________________\n" +
+                    input + "\n" +
+                    "____________________________________________________________\n"
+            );
+            input = sc.next();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
