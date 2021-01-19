1dd8a55c4f7531f71b8440c73caabb81d4abd99e
DESKTOP-VHI0UFD\Lim Li Gang <ligangjurong8@gmail.com>
2021-01-19 12:03:29 +0800

    iP Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+package main.java;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Greetings and salutations! I am Duke, a helpful chatbot.\nPlease enter your command(s).");
+        System.out.println("To exit, enter the command 'bye'.");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+        System.out.println("Farewell. Hope that you will visit again soon!");
+
     }
 }
