88bb3523f1fd552ec981e266fa1cf13286205b39
Jonathan <banchiang98@gmail.com>
2021-01-19 17:35:54 +0800

    added the list and add function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,20 +15,36 @@ public class Duke {
                 + "    Hello! I'm Duke\n"
                 + "    What can I do for you?\n"
                 + line;
-        System.out.println(greeting);
+        System.out.print(greeting);
 
         Scanner sc = new Scanner(System.in);
         String command = "";
+
+        String[] toDo = new String[100];
+        int currIdx = 0;
         //while loop to echo commands of the user
+        command = sc.nextLine();
         while(!command.equals("bye")) {
+            //if command is list, we print out the to-do list.
+            if(command.equals("list")) {
+                int index = 1;
+                System.out.print(line);
+                for(int i = 0; i < currIdx; i++) {
+                    System.out.println(String.format("    %d. %s", i+1, toDo[i]));
+                }
+                System.out.print(line);
+                index++;
+            } else {
+                toDo[currIdx++] = command;
+                System.out.print(line + "    added: " + command + "\n" + line);
+            }
             command = sc.nextLine();
-            System.out.println(line + "    " + command + "\n" + line);
         }
 
         //print out the bye message
         String byeMessage = line
                 + "    Bye. Hope to see you again soon!\n"
                 + line;
-        System.out.println(byeMessage);
+        System.out.print(byeMessage);
     }
 }
