6f9d65a738dd9c6bd849a12c7cfd657a6d751eff
wei-yutong <weiyutongggg@gmail.com>
2021-01-19 14:39:02 +0800

    checked testing

--- src/DukeIncrements.java
+++ src/DukeIncrements.java
@@ -84,6 +84,7 @@ public class DukeIncrements {
 class Task {
     protected String description;
     protected boolean isDone;
+    
     public Task(String description) {
         this.description = description;
         this.isDone = false;
