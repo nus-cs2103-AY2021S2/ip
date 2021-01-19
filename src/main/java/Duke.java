8edc0c8e411c35c396bca29a11ebb52889f4e43b
soorya <32706641+soo-rya@users.noreply.github.com>
2021-01-19 19:29:22 +0800

    completed Level-1 with test cases

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,27 @@
-import java.io.BufferedReader;
+import java.io.*;
+import java.util.Locale;
 
 public class Duke {
-    public static void main(String[] args) {
-        String logo = "  ___  _           ,_,\n"
-                + "|  _  | |   ___   / / \n"
-                + "| | | | |  / _ | =; ;=,\n"
-                + "| |_| | |_| |_| \\ | | \n"
-                + " \\___/\\___\\---;_| |_| \n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String logo = "           ___  _           ,_,\n"
+                + "         |  _  | |   ___   / /\n"
+                + "         | | | | |  / _ \\ =; ;=,\n"
+                + "         | |_| | |_| |_| \\ | |\n"
+                + "          \\___/\\___/---;_| |_|\n";
+
+        String border = " --*---*---*---*---*---*---*---*---*---*--";
+        System.out.println(logo + "\n" + border + "\n  Hey there, Olaf here!\n  What can I do for you?");
+
+        while(true) {
+            String command = bf.readLine();
+            if(command.toLowerCase().equals("bye")){
+                String out = "Aww hope to see you soon, goodbye!";
+                System.out.println(border + "\n  " + out + "\n" + border);
+                break;
+            }
+            System.out.println(border + "\n  " + command + "\n" + border);
+        }
     }
 }
