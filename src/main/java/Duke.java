aea4f2b1cdcba4fbb9eb3b3649a8e24be7f3b387
wangtao0717 <e0688546@u.nus.edu>
2021-01-19 10:27:25 +0800

    Leve-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,10 @@
+import javax.lang.model.type.NullType;
 import java.util.Scanner;
 
 public class Duke {
     private static String indentation = "    ";
-    private static String horizon = "-------------------------------------";
-    private static String[] taskList = new String[100];
+    private static String horizon = "------------------------------------------------------";
+    private static Task[] taskList = new Task[100];
     private static int countTask = 0;
     private static void printReply(String reply){
         System.out.println(indentation+horizon);
@@ -17,8 +18,9 @@ public class Duke {
                 break;
 
             case "list":
-                for (int i = 0; i < countTask; i++) {
-                    System.out.println(indentation + (i+1) + "." + taskList[i]);
+                System.out.println(indentation + "Here are the tasks in your list:");
+                for (int i = 0; i < Task.countTask; i++) {
+                    System.out.println(indentation + (i+1) + "." + taskList[i].getTaskInfo());
                 }
                 break;
             default:
@@ -26,10 +28,18 @@ public class Duke {
             }
         System.out.println(indentation+horizon);
     }
-    private static void taskAdd(String task){
-        taskList[countTask] = task;
-        countTask++;
+
+    private  static void printDoneReply(int done){
+        System.out.println(indentation+horizon);
+        System.out.println(indentation+"Nice! I've marked this task as done:");
+        System.out.println(indentation + (done+1) + "." + taskList[done].getTaskInfo());
+        System.out.println(indentation+horizon);
     }
+
+    private static void taskAdd(String task){
+         taskList[Task.countTask] = new Task(task);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -40,14 +50,28 @@ public class Duke {
 
         System.out.println("Hello from\n" + logo);
         printReply("hello");
-
         Scanner scanner = new Scanner(System.in);
+
         do {
             command = scanner.nextLine();
-            if (!command.equals("bye") && !command.equals("list")){
-                taskAdd(command);
+            switch (command){
+                case "bye":
+                case "list":
+                case "hello":
+                    printReply(command);break;
+                default:
+                    String[] commandSplit = command.split(" ");
+                    if (commandSplit[0].equals("done") && commandSplit.length == 2){
+                        taskList[Integer.parseInt(commandSplit[1])-1].markAsDone();
+                        printDoneReply(Integer.parseInt(commandSplit[1])-1);
+                    }
+                    else {
+                        taskAdd(command);
+                        printReply(command);
+                    }
+
             }
-            printReply(command);
+
         }while(!command.equals("bye"));
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    public static int countTask ;
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+        this.countTask++;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[\u2713]" : "[\u2718] "); //return tick or X symbols
+    }
+    public String getDescription(){
+        return this.description;
+    }
+    public String getTaskInfo(){
+        return getStatusIcon()+getDescription();
+    }
+    public void markAsDone(){
+        this.isDone = true;
+    }
+}
