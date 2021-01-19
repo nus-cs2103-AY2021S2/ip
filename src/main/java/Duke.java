a63c640167403273d33a505f34eb5f7eb4e169a3
linhns <linhns59@gmail.com>
2021-01-19 12:49:20 +0800

    Level 2 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,13 +10,25 @@ public class Duke {
         String sep = "****************************\n";
         System.out.println(sep);
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> msgList = new ArrayList<>();
         while(true) {
-            String str = sc.next();
+            String str = sc.nextLine();
+
             if(str.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(str);
+            else if(str.equals("list")) {
+                int counter = 1;
+                for(String msg: msgList) {
+                    System.out.println(counter + ". " + msg);
+                    counter++;
+                }
+            }
+            else {
+                msgList.add(str);
+                System.out.println("added: " + str);
+            }
             System.out.println(sep);
         }
     }
