cbe983c80b9191f116821fba369493e2332ed586
xuanqi966 <xuanqi.wang966@gmail.com>
2021-01-19 17:17:11 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,7 +17,7 @@ public class Duke {
         return true;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException{
 
         // initialise all necessary variables
         Scanner inputScanner = new Scanner(System.in);
@@ -32,95 +32,126 @@ public class Duke {
 
             String userInput = inputScanner.nextLine();
 
-            if (userInput.equals("list")) {
-                System.out.println("    Here are the tasks in your list:");
-                // print out all tasks
-                for (int i = 0; i < taskCounter; i++) {
-                    // extract relevant information of each task
-                    Task currentTask = taskList[i];
-                    int taskID = i + 1;
-                    System.out.println("    " + taskID + "." + currentTask.toString());
-                }
+            try {
+                if (userInput.equals("list")) {
 
-            } else if (userInput.equals("bye")) {
-                // terminate the chat bot
-                System.out.println("    Goodbye comrade. Hope to see you again soon!");
-                terminate = true;
-            } else if (userInput.startsWith("done")) {
-                // check if nothing follows after done, in which case the input should be invalid
-                if (userInput.equals("done")) {
-                    System.out.println("    Invalid input. Please try again!");
-                    continue;
-                }
+                    System.out.println("    Here are the tasks in your list:");
+                    // print out all tasks
+                    for (int i = 0; i < taskCounter; i++) {
+                        // extract relevant information of each task
+                        Task currentTask = taskList[i];
+                        int taskID = i + 1;
+                        System.out.println("    " + taskID + "." + currentTask.toString());
+                    }
 
-                // mark the specific task as done
-                // extract task index, then mark task as done
-                String temp = userInput.substring(5);
-                int taskIndex;
+                } else if (userInput.equals("bye")) {
+                    // terminate the chat bot
+                    System.out.println("    Goodbye comrade. Hope to see you again soon!");
+                    terminate = true;
+                } else if (userInput.startsWith("done")) {
 
-                // check if the task number after done is a valid number
-                if (isNumber(temp)) {
-                    taskIndex = Integer.parseInt(temp);
+                    // check if nothing follows after done, in which case the input should be invalid
+                    if (userInput.equals("done")) {
+                        throw new DukeException("The task number of a done cannot be empty. Please try again");
+                    }
+
+                    // mark the specific task as done
+                    // extract task index, then mark task as done
+                    String temp = userInput.substring(5);
+                    int taskIndex;
+
+                    // check if the task number after done is a valid number
+                    if (isNumber(temp)) {
+                        taskIndex = Integer.parseInt(temp);
+                    } else {
+                        throw new DukeException("Please enter a numerical task number");
+                    }
+
+                    // check if the task number entered is valid
+                    if (taskIndex < 1 || taskIndex > 101 || taskList[taskIndex - 1] == null) {
+                        throw new DukeException("Your requested task does not exist!");
+                    }
+                    Task currentTask = taskList[taskIndex - 1];
+                    currentTask.markAsDone();
+
+                    // print out relevant information
+                    System.out.println("    Nice! I've marked this task as done:");
+                    System.out.println("     " + currentTask.toString());
+
+                } else if (userInput.startsWith("todo")) {
+                    // check if the todos description is provided
+                    if (userInput.equals("todo")) {
+                        throw new DukeException("The todo description should not be empty!");
+                    }
+
+                    // create new todos and put inside taskList
+                    String todoDescription = userInput.substring(5);
+                    Todo newTodo = new Todo(todoDescription);
+                    taskList[taskCounter] = newTodo;
+                    taskCounter++;
+
+                    // print out relevant information
+                    System.out.println("    Got it. I've added this task:");
+                    System.out.println("     " + newTodo.toString());
+                    System.out.println("    Now you have " + taskCounter + " tasks in the list.");
+
+                } else if (userInput.startsWith("deadline")) {
+                    // check if the deadline description is provided
+                    if (userInput.equals("deadline")) {
+                        throw new DukeException("The deadline description should not be empty!");
+                    }
+
+                    // create new deadline and put inside taskList
+                    String dlMsg = userInput.substring(9);
+                    String[] temp = dlMsg.split(" /by ");
+
+                    // check if the deadline time is provided
+                    if (temp.length == 1) {
+                        throw new DukeException("The deadline time should not be empty!");
+                    }
+
+                    String dlDescription = temp[0];
+                    String by = temp[1];
+                    Deadline newDL = new Deadline(dlDescription, by);
+                    taskList[taskCounter] = newDL;
+                    taskCounter++;
+
+                    // print out relevant information
+                    System.out.println("    Got it. I've added this task:");
+                    System.out.println("     " + newDL.toString());
+                    System.out.println("    Now you have " + taskCounter + " tasks in the list.");
+
+
+                } else if (userInput.startsWith("event")) {
+
+                    // create new event and put inside taskList
+                    if (userInput.equals("event")) {
+                        throw new DukeException("The event description should not be empty!");
+                    }
+
+                    String eMsg = userInput.substring(6);
+                    String[] temp = eMsg.split(" /at ");
+                    // check if the deadline time is provided
+                    if (temp.length == 1) {
+                        throw new DukeException("The event time should not be empty!");
+                    }
+
+                    String eDescription = temp[0];
+                    String at = temp[1];
+                    Event newEvent = new Event(eDescription, at);
+                    taskList[taskCounter] = newEvent;
+                    taskCounter++;
+
+                    // print out relevant information
+                    System.out.println("    Got it. I've added this task:");
+                    System.out.println("     " + newEvent.toString());
+                    System.out.println("    Now you have " + taskCounter + " tasks in the list.");
+
                 } else {
-                    System.out.println("    Invalid input. Please try again!");
-                    continue;
+                    throw new DukeException("I'm sorry, I don't understand what that means.");
                 }
-
-                // check if the task number entered is valid
-                if (taskIndex < 1 || taskIndex >101 || taskList[taskIndex-1] == null) {
-                    System.out.println("    Your requested task does not exist");
-                    continue;
-                }
-                Task currentTask = taskList[taskIndex-1];
-                currentTask.markAsDone();
-
-                // print out relevant information
-                System.out.println("    Nice! I've marked this task as done:");
-                System.out.println("     " + currentTask.toString());
-
-            } else if (userInput.startsWith("todo")){
-                // create new todos and put inside taskList
-                String todoDescription = userInput.substring(5);
-                Todo newTodo = new Todo(todoDescription);
-                taskList[taskCounter] = newTodo;
-                taskCounter++;
-
-                // print out relevant information
-                System.out.println("    Got it. I've added this task:");
-                System.out.println("     " + newTodo.toString());
-                System.out.println("    Now you have " + taskCounter + " tasks in the list.");
-
-            } else if (userInput.startsWith("deadline")) {
-                // create new deadline and put inside taskList
-                String dlMsg = userInput.substring(9);
-                String[] temp = dlMsg.split(" /by ");
-                String dlDescription = temp[0];
-                String by = temp[1];
-                Deadline newDL = new Deadline(dlDescription, by);
-                taskList[taskCounter] = newDL;
-                taskCounter++;
-
-                // print out relevant information
-                System.out.println("    Got it. I've added this task:");
-                System.out.println("     " + newDL.toString());
-                System.out.println("    Now you have " + taskCounter + " tasks in the list.");
-
-            } else if (userInput.startsWith("event")) {
-                // create new event and put inside taskList
-                String eMsg = userInput.substring(6);
-                String[] temp = eMsg.split(" /at ");
-                String eDescription = temp[0];
-                String at = temp[1];
-                Event newEvent = new Event(eDescription, at);
-                taskList[taskCounter] = newEvent;
-                taskCounter++;
-
-                // print out relevant information
-                System.out.println("    Got it. I've added this task:");
-                System.out.println("     " + newEvent.toString());
-                System.out.println("    Now you have " + taskCounter + " tasks in the list.");
-            } else {
-                System.out.println("    Invalid input. Please try again!");
+            } catch (DukeException e) {
+                System.out.println("    " + e.getMessage());
             }
         }
     }
