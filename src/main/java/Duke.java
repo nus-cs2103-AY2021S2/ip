dc5bca83e996a4fb05198316f4a846ae2a77dee0
soorya <32706641+soo-rya@users.noreply.github.com>
2021-01-19 18:20:43 +0800

    changed name to Olaf

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
+import java.io.BufferedReader;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "  ___  _           ,_,\n"
+                + "|  _  | |   ___   / / \n"
+                + "| | | | |  / _ | =; ;=,\n"
+                + "| |_| | |_| |_| \\ | | \n"
+                + " \\___/\\___\\---;_| |_| \n";
         System.out.println("Hello from\n" + logo);
     }
 }
