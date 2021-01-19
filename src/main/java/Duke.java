608d87167c1edce7cf30890ce81c2e5dc732d5cf
Donavan <donavanlim28@gmail.com>
2021-01-19 23:00:57 +0800

    Added Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
 public class Duke {
+    public static String ind = "    ";
+    public static String line = ind + "____________________________________________________________\n" + ind;
+
     public static void main(String[] args) {
 
 //        String logo = " ____        _        \n"
@@ -8,12 +11,29 @@ public class Duke {
 //                + "| | | | | | | |/ / _ \\\n"
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
-        String ind = "    ";
-        String line = ind + "____________________________________________________________\n";
 
-        System.out.println(line + ind + "Hello! I'm Kobe\n" + ind + "What can I do for you?\n" + line);
-//        Scanner sc = new Scanner(System.in);
-//        while (sc.next() != "bye")
-//        sc.next
+
+        System.out.println(line + "Hello! I'm Kobe\n" + ind + "What can I do for you?\n" + line);
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String text = sc.next();
+//            System.out.println(text);
+            if (text.equals("bye")) {
+                goodbye();
+                break;
+            } else {
+                echo(text);
+            }
+        }
+        sc.close();
     }
+
+    public static void echo(String echoedText) {
+        System.out.println(line + echoedText + "\n" + line);
+    }
+
+    public static void goodbye() {
+        System.out.println(line + "Bye. Kobe hopes to see you again soon!\n" + line);
+    }
+
 }
