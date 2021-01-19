d4ef7310da3a8f5566137c1f79c1c43e998d2bc2
github-amanda <57126871+github-amanda@users.noreply.github.com>
2021-01-19 19:41:29 +0800

    Level-6, A-TextUiTesting: Added remove function, replaced array with arraylist

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
-        Task[] list = new Task[100];
+        ArrayList<Task> list = new ArrayList<Task>();
         int index = 0;
         while (!command.equals("bye")) {
             String[] tokens = command.split(" ");
@@ -13,22 +13,24 @@ public class Duke {
                 if (commandType.equals("list")) {
                     // SHOW LIST
                     System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < index; i++) {
-                        System.out.printf("%d. " + list[i] + "\n", i + 1);
+                    int i = 1;
+                    for (Task x : list) {
+                        System.out.printf("%d. " + x + "\n", i);
+                        i++;
                     }
-                } else if (commandType.equals("done")){
-                        // MARK TASK AS COMPLETE
-                        int taskIndex = Integer.parseInt(tokens[1]) - 1;
-                        list[taskIndex].markAsDone();
-                        System.out.println("Nice! I've marked this task as done: \n" +  list[taskIndex].toString());
+                } else if (commandType.equals("done")) {
+                    // MARK TASK AS COMPLETE
+                    int taskIndex = Integer.parseInt(tokens[1]) - 1;
+                    list.get(taskIndex).markAsDone();
+                    System.out.println("Nice! I've marked this task as done: \n" +  list.get(taskIndex).toString());
                 } else if (commandType.equals("todo"))  {
-                // ADD TODO TASK
+                    // ADD TODO TASK
                     try {
                         String todo = command.substring(5);
                         Task task = new Todo(todo);
-                        list[index]= task;
+                        list.add(task);
                         index++;
-                        System.out.println("Got it. I've added this task: \n " + list[index - 1]);
+                        System.out.println("Got it. I've added this task: \n " + list.get(index - 1));
                         System.out.printf("Now you have %d task(s) in the list. \n", index);
                     } catch (StringIndexOutOfBoundsException e){
                         System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
@@ -38,19 +40,26 @@ public class Duke {
                     String description = command.substring(9);
                     String[] deadlineSplit = description.split("/by");
                     Task task = new Deadline(deadlineSplit[0], deadlineSplit[1]);
-                    list[index] = task;
+                    list.add(task);
                     index++;
-                    System.out.println("Got it. I've added this task: \n " + list[index - 1]);
+                    System.out.println("Got it. I've added this task: \n " + list.get(index - 1));
                     System.out.printf("Now you have %d task(s) in the list. \n", index);
                 } else if (commandType.equals("event")) {
                     // ADD EVENT TASK
                     String description = command.substring(6);
                     String[] eventSplit = description.split("/at");
                     Task task = new Event(eventSplit[0], eventSplit[1]);
-                    list[index] = task;
+                    list.add(task);
                     index++;
-                    System.out.println("Got it. I've added this task: \n " + list[index - 1]);
+                    System.out.println("Got it. I've added this task: \n " + list.get(index - 1));
                     System.out.printf("Now you have %d task(s) in the list. \n", index);
+                } else if (commandType.equals("delete")) {
+                    int taskIndex = Integer.parseInt(tokens[1]) - 1;
+                    Task task = list.get(taskIndex);
+                    list.remove(taskIndex);
+                    index--;
+                    System.out.println("Noted. I've removed this task: \n " + task.toString());
+                    System.out.printf("Now you have %d tasks in the list.", index);
                 } else {
                     throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
