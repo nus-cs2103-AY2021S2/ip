32d43b8d3c1e68a213b06c6c13e900636c370243
Shuyang0 <l.shuyang98@gmail.com>
2021-01-20 03:49:02 +0800

    complete level 6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Duke {
     static final String INDENT = "        ";
     static final String BORDER = INDENT + "__________________________________________________";
+    static ArrayList<Task> tasks = new ArrayList<Task>();
 
     public static void print(String s) {
         String[] sSplit = s.split("\n");
@@ -11,6 +12,10 @@ public class Duke {
             System.out.println(INDENT + line);
         }
         System.out.println(BORDER + "\n");
+    }
+
+    public static String getNumTasks() {
+        return "\nNow you have " + tasks.size() + " tasks in the list.";
     }
     public static void main(String[] args) {
         String logo = "      _       __  __  \n"
@@ -21,7 +26,6 @@ public class Duke {
                     + " \\____/ \\___|_| |_| \n";
         print("Hello I'm\n" + logo + "\nWhat can I do for you?");
 
-        ArrayList<Task> tasks = new ArrayList<Task>();
         Scanner sc = new Scanner(System.in);
         while(true) {
             String[] msg = sc.nextLine().split("/");
@@ -52,7 +56,7 @@ public class Duke {
                 } catch(IndexOutOfBoundsException e) {
                     print((new DukeException("task number does not exist")).toString());
                 } catch(NumberFormatException e) {
-                    print((new DukeException("indicate task number to be marked as done")).toString());
+                    print((new DukeException("indicate task number as an integer")).toString());
                 }
             } else if(cmd.equals("todo")) {
                 if (msg0.length < 2) {
@@ -61,7 +65,7 @@ public class Duke {
                 }
                 Task curr = new ToDo(msg0[1]);
                 tasks.add(curr);
-                print("Got it. I've added this task:\n" + curr + "\nNow you have " + tasks.size() + " tasks in the list.");
+                print("Got it. I've added this task:\n" + curr + getNumTasks());
             } else if(cmd.equals("deadline")) {
                 if (msg0.length < 2) {
                     print((new DukeException("please provide a description for deadline")).toString());
@@ -71,7 +75,7 @@ public class Duke {
                     String time = msg[1].split(" ", 2)[1];
                     Task curr = new Deadline(msg0[1], time);
                     tasks.add(curr);
-                    print("Got it. I've added this task:\n" + curr + "\nNow you have " + tasks.size() + " tasks in the list.");
+                    print("Got it. I've added this task:\n" + curr + getNumTasks());
                 } catch(ArrayIndexOutOfBoundsException e) {
                     print((new DukeException("please provide a time for deadline")).toString());
                 }
@@ -84,9 +88,24 @@ public class Duke {
                     String time = msg[1].split(" ", 2)[1];
                     Task curr = new Event(msg0[1], time);
                     tasks.add(curr);
-                    print("Got it. I've added this task:\n" + curr + "\nNow you have " + tasks.size() + " tasks in the list.");
-                } catch(ArrayIndexOutOfBoundsException e) {
+                    print("Got it. I've added this task:\n" + curr + getNumTasks());
+                } catch (ArrayIndexOutOfBoundsException e) {
                     print((new DukeException("please provide a time for event")).toString());
+                }
+            } else if (cmd.equals("delete")) {
+                if(msg0.length != 2) {
+                    print((new DukeException("wrong number of arguments for delete")).toString());
+                    continue;
+                }
+                try {
+                    int taskIndex = Integer.parseInt(msg0[1]) - 1;
+                    Task toRemove = tasks.get(taskIndex);
+                    tasks.remove(taskIndex);
+                    print("Noted. I've removed this task:\n" + toRemove + getNumTasks());
+                } catch(IndexOutOfBoundsException e) {
+                    print((new DukeException("task number does not exist")).toString());
+                } catch(NumberFormatException e) {
+                    print((new DukeException("indicate task number as an integer")).toString());
                 }
             } else {
                 print((new DukeException("I can't understand the message")).toString());
