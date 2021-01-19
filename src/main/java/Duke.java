9545b44519b7c03a585b084804462188e46dc35e
Donavanty <donavanlim28@gmail.com>
2021-01-19 22:35:43 +0800

    Kobe intro message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String ind = "    ";
+        String line = ind + "____________________________________________________________\n";
+
+        System.out.println(line + ind + "Hello! I'm Kobe\n" + ind + "What can I do for you?\n" + line);
+//        Scanner sc = new Scanner(System.in);
+//        while (sc.next() != "bye")
+//        sc.next
     }
 }
