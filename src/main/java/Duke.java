d836cc9318c9470b9a21a3891e0a645c637ac508
Benedict Khoo <benedictkhoo.mw@u.nus.edu>
2021-01-19 11:31:18 +0800

    Add level 2 functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private PrintStream printer;
     private Scanner input;
+    private ArrayList<Task> tasks;
 
     public static void main(String[] args) {
         Duke d = new Duke(System.out, System.in);
@@ -16,6 +18,7 @@ public class Duke {
     public Duke(PrintStream printer, InputStream inputStream) {
         this.printer = printer;
         this.input = new Scanner(inputStream);
+        tasks = new ArrayList<>();
     }
 
     public void displayGreeting() {
@@ -34,14 +37,23 @@ public class Duke {
 
     public void loop() {
         final String exitCmd = "bye";
+        final String listCmd = "list";
         String cmd;
-        boolean isDone;
+
         while (true) {
             cmd = input.nextLine();
             if (cmd.toLowerCase().equals(exitCmd)) {
                 break;
+            } else if (cmd.toLowerCase().equals(listCmd)) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    String desc = tasks.get(i).getDescription();
+                    printer.println(String.format("%d. %s", i + 1, desc));
+                }
+            } else {
+                printer.println(String.format("added: %s", cmd));
+                Task task = new Task(cmd);
+                tasks.add(task);
             }
-            printer.println(cmd);
         }
     }
 
