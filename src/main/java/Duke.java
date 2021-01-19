50779b1a95795cba7d318a2ae4feec656d4b8687
edelinetenges <edelinetenges@gmail.com>
2021-01-19 14:41:51 +0800

    echoes commands by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,10 +10,26 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         String tab = "     ";
-        String line = "____________________________________________________________\n";
-        System.out.println(tab + line
+        String line = "     ____________________________________________________________";
+        System.out.println(line + "\n"
                 + tab + "Hello! I'm Duke\n"
                 + tab + "What can I do for you?\n"
-                + tab + line);
+                + line);
+
+        Scanner scan = new Scanner(System.in);
+
+        while (scan.hasNext()) {
+            String input = scan.next();
+
+            System.out.println(line);
+
+            if (input.equals("bye")) {
+                System.out.println(tab + "Bye. Hope to see you again soon!");
+            } else {
+                System.out.println(tab + input);
+            }
+
+            System.out.println(line);
+        }
     }
 }
