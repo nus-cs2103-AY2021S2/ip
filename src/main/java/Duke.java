b0c2c9b1411cb7304fe1e88bde91b37ae86311b2
edelinetenges <edelinetenges@gmail.com>
2021-01-19 14:29:52 +0800

    added first greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String tab = "     ";
+        String line = "____________________________________________________________\n";
+        System.out.println(tab + line
+                + tab + "Hello! I'm Duke\n"
+                + tab + "What can I do for you?\n"
+                + tab + line);
     }
 }
