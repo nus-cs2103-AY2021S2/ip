1df5ab487d89dcb7142f3d78f244ab8d3e163033
linhns <linhns59@gmail.com>
2021-01-19 12:27:41 +0800

    Level 1 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String sep = "****************************\n";
+        System.out.println(sep);
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String str = sc.next();
+            if(str.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(str);
+            System.out.println(sep);
+        }
     }
 }
