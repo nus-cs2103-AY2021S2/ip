8db4dbbeb8cf8951c5afd100018ee7df99307f4b
ampan98 <e0407566@u.nus.edu>
2021-01-19 17:32:40 +0800

    Add DukeException class to represent exceptions specific to Duke

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,15 @@
+package main.java;
+
+public class DukeException extends Exception {
+    private final String message;
+
+    public DukeException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
