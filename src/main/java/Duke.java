17a66c1bd55f83166993c287df82b6867828550b
github-amanda <57126871+github-amanda@users.noreply.github.com>
2021-01-19 18:46:07 +0800

    Level-1, A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = scanner.nextLine();
+        }
+        if (command.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
     }
 }
+
