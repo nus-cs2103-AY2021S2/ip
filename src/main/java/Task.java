7ea1f7832881be7e696cbd90f8e787cd04019ff9
andrewzhang <zhangguangzesheng@gmail.com>
2021-01-20 03:47:26 +0800

    Level-3 implemented

---
+++ src/main/java/Task.java
@@ -0,0 +1,35 @@
+public class Task {
+
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public Task(String description, boolean isDone){
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public boolean getIsDone(){
+        return this.isDone;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public Task markAsDone(){
+        String description = this.getDescription();
+        //System.out.println(" Nice! I've marked this task as done: ");
+        return new Task(description,true);
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+
+}
