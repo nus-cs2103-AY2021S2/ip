98bc1c390eedea4d8411d671339098ac32bbadbf
mrweikiat <danielgohweikiat@yahoo.com>
2021-01-19 15:33:03 +0800

    level6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,17 +3,17 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
 
-        /*
-        ***VANILLA ip-master CODE***
+        //***VANILLA ip-master CODE***
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-         */
+
         Scanner sc = new Scanner(System.in);
 
         // Starting line for UI
+        System.out.println(logo);
         System.out.println("Hello I'm Duke");
         System.out.println("What can I do for you?");
 
@@ -21,11 +21,7 @@ public class Duke {
         boolean terminate = false;
 
         // create LinkedList to store information of user inputs
-        Task[] tasks = new Task[100];
-
-        // to keep track of num of task
-        int itemNum = 0; // starts from 1
-        int tasksNum = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         //Duke will keep repeating until command given "Bye"
         while (!terminate) {
@@ -33,30 +29,31 @@ public class Duke {
 
             // ***** level 5 *****
             try {
-                validate(text);// ****************************************** LEVEL 1 *************************************************
+                validate(text);
+                // ******* LEVEL 1 *****
                 if (text.equals("bye")) {
                     System.out.println("Bye. Hope to see you again soon!");
                     terminate = true; // terminates Duke
                 } else if (text.equals("list")) {
                     // print all the tasks stored currently in the list
-                    for (int i = 0; i < itemNum; i++) {
-                        System.out.println(i+1 + ". " + tasks[i].toString());
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(i+1 + ". " + tasks.get(i).toString());
                     }
                 } else if (text.contains("done")) {
-                    // ************************************** LEVEL 3 **********************************
+                    // ***** LEVEL 3 ******
                     String num = text.substring(5); // take out the int value of the task to be completed
                     int listNum = Integer.parseInt(num); // changes to int
-                    Task hold = tasks[listNum - 1];
+                    Task hold = tasks.get(listNum-1);
                     hold.markAsDone();
-                    // *********************** LEVEL 3 *********************************8
+
 
                     // format
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(tasks[listNum-1].toString());
-                    // ****************************************** LEVEL 1 *************************************************
+                    System.out.println(tasks.get(listNum-1).toString());
+                    // ***** LEVEL 1 *****
 
                 } else if (text.contains("deadline")) {
-                    // *********************************** LEVEL 4 ***************************************************
+                    // ****** LEVEL 4 *****
                     // insert code for deadline
                     String newText = text.substring(9); // remove deadline from the string text
                     //System.out.println(newText); // for debugging
@@ -71,44 +68,39 @@ public class Duke {
 
                     System.out.println("Got it. I've added this task:");
 
-                    for (int i = 0; i < itemNum; i++) {
-                        if (tasks[i].description.equals(description)) { // meaning this is the task we want to change
+                    for (int i = 0; i < tasks.size(); i++) {
+                        if (tasks.get(i).description.equals(description)) { // meaning this is the task we want to change
                             Deadline dl = new Deadline(description, date);
-                            if (tasks[i].isDone) {
+                            if (tasks.get(i).isDone) {
                                 dl.markAsDone();
                             }
 
-                            tasks[i] = dl; // insert into the list
+                            tasks.set(i, dl);// insert into the list
+
                             System.out.println(" " + dl.toString());
-                            tasksNum++;
-                            System.out.println("Now you have " + tasksNum + " tasks in the list");
+                            System.out.println("Now you have " + tasks.size() + " tasks in the list");
                         }
                     }
-                    // ****************************************** LEVEL 4 *************************************************8
                 } else if (text.contains("todo")) {
-                    // ************************************ LEVEL 4 *******************************************
                     String description = text.substring(text.indexOf(" ")+1); // take out the item from the text
-                    //String description = text.substring(4);
                     //System.out.println(description); // for debugging
 
-                    for (int i = 0; i < itemNum; i++) { // there is an instance of the item in list
-                        if (tasks[i].description.equals(description)) {
+                    for (int i = 0; i < tasks.size(); i++) { // there is an instance of the item in list
+                        if (tasks.get(i).description.equals(description)) {
                             Todo td = new Todo(description);
                             // bringing over information from superclass
-                            if (tasks[i].isDone) {
+                            if (tasks.get(i).isDone) {
                                 td.markAsDone();
                             }
-                            tasks[i] = td; // insert into list
+                            tasks.set(i, td); // insert into list
                             // formatting
                             System.out.println("Got it. I've added this task:");
                             System.out.println(" " + td.toString());
-                            tasksNum++; // increment
-                            System.out.println("Now you have " + tasksNum + " tasks in the list");
+                            System.out.println("Now you have " + tasks.size() + " tasks in the list");
                         }
                     }
-                    // ************************************** LEVEL 4 ************************************************
                 } else if (text.contains("event")) {
-                    // ************************************* LEVEL 4 **************************************************
+
                     String newText = text.substring(text.indexOf(" ")+1); // removing the event to get description
                     System.out.println(newText); // for debugging
 
@@ -118,38 +110,38 @@ public class Duke {
 
                     //System.out.println(description + " " + date); // for debugging
 
-                    for (int i = 0; i < itemNum; i++) {
-                        if(tasks[i].description.equals(description)) {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        if(tasks.get(i).description.equals(description)) {
                             Event e = new Event(description, date);
                             // bringing over info from superclass
-                            if (tasks[i].isDone) {
+                            if (tasks.get(i).isDone) {
                                 e.markAsDone();
                             }
 
-                            tasks[i] = e; // insert into list
+                            tasks.set(i, e); // insert into list
+
                             //formatting
                             System.out.println("Got it. I've added this task:");
                             System.out.println(" " + e.toString());
-                            tasksNum++; // increment
-                            System.out.println("Now you have " + tasksNum + " tasks in the list");
+                            //tasksNum++; // increment
+                            System.out.println("Now you have " + tasks.size() + " tasks in the list");
                         }
                     }
-                    // ************************************* LEVEL 4 **************************************************
+                }
+                else if (text.contains("delete")) {
+                    // ***** level 6 *****
+
                 }
                 // adding of tasks
                 else {
                     System.out.println("added: " + text);
                     // create new instance of task and add to the list
                     Task holder = new Task(text);
-                    tasks[itemNum] = holder; // position corresponds to item number
-                    itemNum++; // increase the item list count
-                    tasksNum++; // increase task list count
+                    tasks.add(holder); // position corresponds to item number
                 }
             } catch (Exception m) {
             System.out.println(m);
             }
-            // ***** level 5 *****
-
         }
 
         sc.close();
@@ -163,9 +155,9 @@ public class Duke {
         }
         else if (text.contains("blah")) { // case 2
             throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-        } else if (text.length() < 10 && text.contains("deadline")) {
+        } else if (text.length() < 10 && text.contains("deadline")) { // case 3
             throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
-        } else if (text.length() < 6 && text.contains("event")) {
+        } else if (text.length() < 6 && text.contains("event")) { // case 4
             throw new DukeException("☹ OOPS!!! The description of a event cannot be empty.");
         }
     }
