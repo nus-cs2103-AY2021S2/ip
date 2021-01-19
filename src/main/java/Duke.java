34f68b82eeaf8f53eaf115629e667bbb4b2f5ad6
umergta <umergta@gmail.com>
2021-01-19 14:54:48 +0800

    level 2: added store and display of user text functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,41 @@
+import javax.net.ssl.HostnameVerifier;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private static final String HORIZONTAL_RULE = "____________________________________________________________";
-    private static final String indentation = "    ";
+    private static ArrayList<String> userList = new ArrayList<>();
     public static void main(String[] args) {
+        Duke.basicProgram();
+    }
+    private static void basicProgram(){
         String greeting = HORIZONTAL_RULE + "\nHello! I am Duke\n" + "What can I do for you?\n" + HORIZONTAL_RULE;
+        String exitCommand = "bye";
+        String listCommand = "list";
         System.out.println(greeting);
         Scanner sc = new Scanner(System.in);
         boolean isBye = false;
         while (!isBye){
             String command = sc.nextLine();
             System.out.println(HORIZONTAL_RULE);
-            if(command.equals("bye")){
+            if(command.equals(exitCommand)){
                 System.out.println(command + ". Hope to see you again soon!\n"+ HORIZONTAL_RULE);
                 sc.close();
                 isBye = true;
             }
-            else{
-                System.out.println(command +"\n"+ HORIZONTAL_RULE);
+            else if(command.equals(listCommand)){
+                Duke.printUserList();
+                System.out.println(HORIZONTAL_RULE);
             }
+            else{
+                Duke.userList.add(command);
+                System.out.println("added: " + command +"\n"+ HORIZONTAL_RULE);
+            }
+        }
+    }
+    private static void printUserList(){
+        for(int i = 0; i< userList.size();i++){
+            System.out.println(i+1 + ". " + userList.get(i));
         }
     }
 }
