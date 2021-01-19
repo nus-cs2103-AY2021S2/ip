88c77d05463e888b32bcd2a63a057e8b27183334
LimJunxue <junxue1@hotmail.com>
2021-01-19 16:29:17 +0800

    Level-3 Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,31 +5,68 @@ import java.util.Scanner;
  * Encompasses the behavior of the Duke chat-bot.
  */
 public class Duke {
-    public static ArrayList<String> tasks = new ArrayList<>();
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     /**
      * Carries out the corresponding actions of the command.
+     *
      * @param command A string of the command to be carried out.
      */
-    public static void dispatch(String command) {
+    private static void dispatch(String command) {
         printLine();
-        switch (command) {
+        switch (command.split(" ")[0]) {
             case "bye":
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             case "list":
-                for (int i = 1; i <= tasks.size(); i++) {
-                    System.out.println(i + ". " + tasks.get(i-1));
+                if (tasks.size() == 0) {
+                    System.out.println("Your list is empty!");
+                } else {
+                    for (int i = 1; i <= tasks.size(); i++) {
+                        System.out.println(i + "." + tasks.get(i - 1));
+                    }
                 }
                 break;
+            case "done":
+                findAndMarkTask(command);
+                break;
             default:
-                System.out.println("added: " + command);
-                tasks.add(command);
+                if (command.trim().length() > 0) {
+                    System.out.println("added: " + command);
+                    tasks.add(new Task(command));
+                } else {
+                    System.out.println("Please enter something ...");
+                }
         }
         printLine();
     }
 
-    public static void printLine() {
+    /**
+     * Finds and marks task specified in the command string. Handles errors that include index out of bounds and number format.
+     *
+     * @param command Command inputted into the console handled by dispatch method.
+     */
+    private static void findAndMarkTask(String command) {
+        try {
+            Task currTask = tasks.get(Integer.parseInt(command.split(" ")[1]) - 1);
+            currTask.markTask();
+            System.out.println("Swee chai. It's done.\n" + currTask);
+        } catch (IndexOutOfBoundsException e) {
+            if (tasks.size() == 0) {
+                System.out.println("Your list is empty!");
+            } else {
+                System.out.println("The task number provided does not exist. :/");
+            }
+        } catch (Exception e) {
+            if (tasks.size() == 0) {
+                System.out.println("Your list is empty!");
+            } else {
+                System.out.println("Please enter a number from 1 to " + tasks.size());
+            }
+        }
+    }
+
+    private static void printLine() {
         System.out.println("------------------------------");
     }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,32 @@
+/**
+ * Represents a task listed in Duke and can be marked as done.
+ */
+public class Task {
+    protected boolean isDone;
+    protected String description;
+
+    /**
+     * Creates a task with the specified description.
+     * @param description The task's description.
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Marks this task as done.
+     */
+    public void markTask() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        String symbol = " ";
+        if (isDone) {
+            symbol = "X";
+        }
+        return "[" + symbol + "] " + this.description;
+    }
+}
