46b910df848c2b51c0f842871d2374a6758206f1
Prerthan <e0407819@u.nus.edu>
2021-01-20 13:07:31 +0800

    code update for Duke level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+
+        //task 1
+        //greeting line
+        System.out.println("Hello my name is Duke\n" + "What can i do for you");
+        Scanner sc = new Scanner(System.in); // takes in input
+        while(sc.hasNext()){ //while there is an input
+            String s = sc.next();// put the input into string
+            if(s.equals("bye")){ //if the srting is end word
+                System.out.println("Bye, hope to see you again soon"); //display terminating message
+                break; //break the loop
+            } else { //if the input is not the terminating word
+                System.out.println(s); ///print out the input
+            }
+        }
     }
 }
+
