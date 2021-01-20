fd4074bfeda8e80df943f5be0664c4100f94b0f4
pngsebastian <pngsebastian@gmail.com>
2021-01-21 00:35:27 +0800

    feat: greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static void replyFormat(String reply) {
+        String separator = "____________________________________________________________\n";
+        System.out.println(separator + reply + "\n" + separator);
+    }
+
+    private static void greet() {
+        String greeting = "Hello! I'm Duke\n" + "What can I do for you?";
+        replyFormat(greeting);
+    }
+
+    private static void echo(String userInput) {
+        replyFormat(userInput);
+    }
+
+    private static void bye() {
+        String byeMessage = "Bye. Hope to see you again soon!";
+        replyFormat(byeMessage);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        greet();
+        String userInput = sc.next();
+
+        while (!userInput.equals("bye")) {
+            echo(userInput);
+            userInput = sc.next();
+        }
+
+        bye();
     }
 }
