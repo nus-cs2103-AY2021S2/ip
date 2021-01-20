b349dc5cac51310f45749fa090944a92bccf0c7e
aaronsms <aaronsms@u.nus.edu>
2021-01-20 12:25:48 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected boolean isDone;
+    protected final String description;
+
+    public static Task create(final String description) {
+        return new Task(false, description);
+    }
+
+    private Task(final boolean isDone, final String description) {
+        this.isDone = isDone;
+        this.description = description;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "\u2713" : "\u2718");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+}
