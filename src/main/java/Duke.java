f634c80fb9dbe82e0d81a26fcca63cebeb560f60
geraldfan <geraldfan@gmail.com>
2021-01-20 16:17:11 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
+
 public class Duke {
+    private final StringBuilder lengthOfChatBox = new StringBuilder();
+
+    private void setLengthOfChatBox() {
+        lengthOfChatBox.append("\n");
+        lengthOfChatBox.append("-".repeat(50));
+        lengthOfChatBox.append("\n");
+    }
+
+    private void formatInChatBox(String s) {
+        System.out.println(lengthOfChatBox + s + lengthOfChatBox);
+    }
+
+    public void init() {
+        setLengthOfChatBox();
+        String greeting = "Hello I'm Mike\nWhat can I do for you?\n";
+        formatInChatBox(greeting);
+    }
+
+    public boolean chat(String s) {
+        if (s.toLowerCase().equals("bye")) {
+            String goodbyeMessage = "Bye. Hope to see you again soon!\n";
+            formatInChatBox(goodbyeMessage);
+            return false;
+        } else {
+            formatInChatBox(s);
+            return true;
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke mike = new Duke();
+        mike.init();
+        Scanner sc = new Scanner(System.in);
+        Boolean isChatting = true;
+
+        while (isChatting) {
+            String input = sc.next();
+            mike.chat(input);
+        }
     }
 }
