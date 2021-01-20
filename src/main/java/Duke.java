8a1b885db3dc9b354917c4350ba4d8ffd5879954
tanboonji <tanboonji@hotmail.com>
2021-01-21 07:25:18 +0800

    Implement mark as done and Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,16 +6,18 @@ public class Duke {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>(100);
+        ArrayList<Task> tasks = new ArrayList<>(100);
         boolean runDuke = true;
 
         greet();
 
         while (runDuke) {
             String input = scanner.nextLine();
+            String[] tokenizedInput = input.split(" ");
 
-            switch (input) {
+            switch (tokenizedInput[0]) {
                 case "list": StringBuilder builder = new StringBuilder();
+                    builder.append("Here are the tasks in your list:\n");
                     for (int i = 0; i < tasks.size(); i++) {
                         int numbering = i + 1;
                         builder.append(numbering);
@@ -28,11 +30,16 @@ public class Duke {
                     }
                     echo(builder.toString());
                     break;
+                case "done":
+                    int taskIndex = Integer.parseInt(tokenizedInput[1]) - 1;
+                    tasks.get(taskIndex).markAsDone();
+                    echo("Nice! I've marked this task as done:\n" + tasks.get(taskIndex));
+                    break;
                 case "bye": echo("Bye. Hope to see you again soon!");
                     runDuke = false;
                     break;
                 default:
-                    tasks.add(input);
+                    tasks.add(new Task(input));
                     echo("added: " + input);
             }
         }
@@ -49,8 +56,8 @@ public class Duke {
     }
 
     public static void echo(String input) {
-        System.out.println("==============================");
+        System.out.println("========================================");
         System.out.println(input);
-        System.out.println("==============================\n");
+        System.out.println("========================================\n");
     }
 }
