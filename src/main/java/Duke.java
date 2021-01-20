141da05eecf46e3aac5c06b40d771d8eb92d4cff
zhengruoxin <zhengruoxin99@gmail.com>
2021-01-20 14:39:39 +0800

    add ability to store and display text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,40 @@
 import java.util.*;
 
 public class Duke {
+
+	private ArrayList<String> store = new ArrayList<>();
+
+	public void addText (String text) {
+		this.store.add(text);
+	}
+
+	void greet() {
+		String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+		System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+	}
+
     void chat() {
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        this.greet();
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         while (!(input.equals("bye"))) {
-            System.out.println(input);
-            in = new Scanner(System.in);
-            input = in.nextLine();
+        	if (input.equals("list")) {
+        		int counter = 1;
+        		for (String elem: this.store) {
+        			System.out.println(counter + ". " + elem);
+        			counter += 1;
+        		}
+        	} else {
+	            this.addText(input);
+	            System.out.println("added: " + input);
+	        }
+	        in = new Scanner(System.in);
+	        input = in.nextLine();
         }
         if (input.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,5 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
         Duke chatbot = new Duke();
         chatbot.chat();
     }
