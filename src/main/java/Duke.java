84e3c9a4fa97b1f2c3946b24a04d660f61e943ae
jrvslam <jarveis.lamzx@gmail.com>
2021-01-20 11:39:09 +0800

    Added ability to store text entered by the user and return list of text given.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,18 +10,29 @@ public class Duke {
         System.out.println("   Hello There! I'm Duke, always here for you!");
         System.out.println("   How can I help you today?");
 
+        String[] arr = new String[100];
+        int counter = 0;
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
 
         while (!input.equals("bye")) {
             switch (input) {
                 case "list":
-                    System.out.println("___\n   list\n___");
+                    System.out.println("___");
+                    for (int i = 0; i < counter; i++) {
+                        System.out.println("   " + (i + 1) + ". " + arr[i]);
+                    }
+                    System.out.println("___");
                     input = sc.next();
                     break;
                 default:
-                    System.out.println("___\n   blah\n___");
+                    String item = sc.next();
+                    System.out.println("___\n added: read " + item + "\n___");
+                    input = input.concat(" " + item);
+                    arr[counter] = input;
+                    counter++;
                     input = sc.next();
+                    break;
             }
         }
 
