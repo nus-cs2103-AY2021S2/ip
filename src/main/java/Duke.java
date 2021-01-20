0c200eaa6cfb2d5e5b13bb5517a54c2dc29e7661
nicoleang09 <nicoleang09@gmail.com>
2021-01-20 15:03:10 +0800

    Add Duke Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! How can I help you?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye! See you soon :)");
+                scanner.close();
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
