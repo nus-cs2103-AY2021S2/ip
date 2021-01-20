3b89646713b3d931d61c02043c8159de8de1994f
simran-bhadani3 <simranbhadani333@gmail.com>
2021-01-20 08:14:58 +0800

    Add functionality to mark tasks as completed, Level 3 complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,21 +13,33 @@ public class Duke {
         System.out.println("\n~ Hello! I am Zee :) ~\n"
                 + logo + "\n"
                 + "~ What can I do for you today? ~\n");
-        List<String> tasks = new ArrayList<>();
+        List<String[]> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
         while(!command.equals("bye")) {
-            if(command.equals("list")) {
+            if (command.equals("list")) {
                 int count = 1;
                 System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                for(String string: tasks) {
-                    System.out.println("  " + count + ". " + string);
+                System.out.println("Here are the tasks in your list:");
+                for (String[] task : tasks) {
+                    if (task[1].equals("done")) {
+                        System.out.println("  " + count + ".[X] " + task[0]);
+                    } else {
+                        System.out.println("  " + count + ".[ ] " + task[0]);
+                    }
                     count++;
                 }
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+            } else if(command.split(" ")[0].equals("done")) {
+                int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("Good job, I have marked this task as done!");
+                System.out.println("[X] " + tasks.get(index)[0]);
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                tasks.set(index, new String[]{tasks.get(index)[0], "done"});
             } else {
                 System.out.println("\n~ added: " + command + " ~\n");
-                tasks.add(command);
+                tasks.add(new String[]{command, "not done"});
             }
             command = scanner.nextLine();
         }
