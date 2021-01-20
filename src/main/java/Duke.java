c880396c8dab8f6debe8e3abc5dfcd4376400188
hengyiqun <hengyiqun@gmail.com>
2021-01-21 00:59:52 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Duke {
     public static void main(String[] args) {
@@ -12,6 +14,7 @@ public class Duke {
         System.out.println("     Hello! I'm Duke");
         System.out.println("     What can I do for you?");
         System.out.println("    ____________________________________________________________");
+        List<String> store = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
@@ -20,10 +23,20 @@ public class Duke {
                 System.out.println("     Bye. Hope to see you again soon!");
                 System.out.println("    ____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                int count = 1;
+                for (String s: store) {
+                    System.out.println("     " + count + ". " + s);
+                    count++;
+                }
+                System.out.println("    ____________________________________________________________");
+            } else {
+                store.add(input);
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     added: " + input);
+                System.out.println("    ____________________________________________________________");
             }
-            System.out.println("    ____________________________________________________________");
-            System.out.println("     " + input);
-            System.out.println("    ____________________________________________________________");
         }
     }
 }
