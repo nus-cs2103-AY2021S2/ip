406306cbdeab9b36ec5a6d568cc08999b2efccd1
Stein121 <qscaxz0@gmail.com>
2021-01-20 13:20:33 +0800

    Add Level 1 Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,55 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    private static final String divider = "\t____________________________________________________________\n";
+
+    /**
+     * Entry point for Duke
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+            String[] greeting = {
+                "Hello! I'm Duke",
+                "What can I do for you?"
+            };
+            write(out, greeting);
+
+            String input = in.readLine();
+
+            while (!input.equals("bye")) {
+                write(out, input);
+                input = in.readLine();
+            }
+
+            write(out, "Bye. Hope to see you again soon!");
+        }
+    }
+
+    static void write(BufferedWriter out, String line) throws IOException {
+        out.write(divider);
+        out.write('\t');
+        out.write(line);
+        out.newLine();
+        out.write(divider);
+        out.flush();
+    }
+
+    static void write(BufferedWriter out, String[] lines) throws IOException {
+        out.write(divider);
+        for (String line : lines) {
+            out.write('\t');
+            out.write(line);
+            out.newLine();
+        }
+        out.write(divider);
+        out.flush();
     }
 }
