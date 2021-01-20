691306e479b98cec691a1174bf267f2a7b48688c
nighoggDatatype <hosherman1965@gmail.com>
2021-01-21 03:42:25 +0800

    Removed rendundant line

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,6 @@
 public abstract class Task {
     private final String description;
     protected boolean isDone;
-    private static int counter = 0;
 
     public Task(String description) {
         this.description = description;
