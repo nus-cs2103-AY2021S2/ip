f2431e7c00b8c9a15f53fd7f3b20f3af5a8c0602
BigDoot <glendonchua@gmail.com>
2021-01-20 18:08:20 +0800

    Implement Deadline and Event Tasks in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,8 +20,16 @@ public class  Duke {
         while (!str.equals("bye")) {
             Pattern pdone = Pattern.compile("^(done )([0-9]+)");
             Matcher mdone = pdone.matcher(str);
-            Pattern ptodo = Pattern.compile("^([Tt]odo )([a-zA-Z_0-9]+)");
+
+            Pattern ptodo = Pattern.compile("^([Tt]odo )([a-zA-Z_0-9 ]+)");
             Matcher mtodo = ptodo.matcher(str);
+
+            Pattern pdl = Pattern.compile("^([Dd]eadline )([a-zA-Z_0-9 ]+)(\\/by )([a-zA-Z_0-9 ]+)");
+            Matcher mdl = pdl.matcher(str);
+
+            Pattern pev = Pattern.compile("^([Ee]vent )([a-zA-Z_0-9 ]+)(\\/at )([a-zA-Z_0-9 ]+)");
+            Matcher mev = pev.matcher(str);
+
             if (str.equals("list")) {
                 System.out.println("Here are the tasks in your list!");
                 for (int i = 0; i < numitems; i++) {
@@ -40,14 +48,24 @@ public class  Duke {
                 System.out.println(list[numitems]);
                 numitems += 1;
                 System.out.println("Now you have " + numitems + " tasks in the list.");
-
-            }else {
+            } else if (mdl.find()) {
+                list[numitems] = new Deadline(mdl.group(2), mdl.group(4));
+                System.out.println("Got it!. I have added the following task:");
+                System.out.println(list[numitems]);
+                numitems += 1;
+                System.out.println("Now you have " + numitems + " tasks in the list.");
+            } else if (mev.find()) {
+                list[numitems] = new Event(mev.group(2), mev.group(4));
+                System.out.println("Got it!. I have added the following task:");
+                System.out.println(list[numitems]);
+                numitems += 1;
+                System.out.println("Now you have " + numitems + " tasks in the list.");
+            } else {
                 System.out.println("added: " + str);
                 list[numitems] = new Task(str);
                 numitems += 1;
             }
             str = sc.nextLine();
-
         }
         System.out.println("Bye friend, see you soon!");
     }
