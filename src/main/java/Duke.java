6c2b4870d068da9060079ce2404a9765e4a303df
IceBear789 <boreman90@gmail.com>
2021-01-20 19:52:36 +0800

    Implemented level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> storedText = new ArrayList<>();
         System.out.println("Hello! I'm Icebear");
         System.out.println("What can I do for you?");
         while (true) {
@@ -10,8 +12,13 @@ public class Duke {
             if (nextCommand.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (nextCommand.equals("list")){
+                for (int i = 0; i < storedText.size(); i++) {
+                    System.out.println(i + 1 + ". " + storedText.get(i));
+                }
             } else {
-                System.out.println(nextCommand);
+                storedText.add(nextCommand);
+                System.out.println("added: " + nextCommand);
             }
         }
     }
