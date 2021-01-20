c68579196671be89bc99477a9ee9d2484e04db66
VisnuRavi <visnu9016@gmail.com>
2021-01-20 11:00:02 +0800

    Level-1 added greet, echo, and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String line = "____________________________________________"+
+                "________________\n";
+        System.out.println(line + " Hey there! I'm Duke\n" +
+                " How can I help you?\n" + line);
+
+        while(sc.hasNext()) {
+            String str = sc.nextLine();
+            if(str.equals("bye")) {
+                break;
+            } else {
+                System.out.println(line + " " + str + "\n" + line);
+            }
+        }
+
+        System.out.println(line + " Bye, see you again!\n"
+                + line);
     }
 }
