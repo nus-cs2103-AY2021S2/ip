07936694cc00f9e7fa438b5dc28a3c718c78ca6a
yutingzou <zouyuting0801@gmail.com>
2021-01-20 17:15:00 +0800

    add Level 1 - Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,30 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Duke myDuke = new Duke();
+        myDuke.run();
     }
+
+    private void run(){
+        System.out.println("---------------------------------------------");
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println("---------------------------------------------");
+        Scanner input = new Scanner(System.in);
+
+        while(input.hasNextLine()){
+            String s = input.nextLine();
+            if(s.equals("bye")) {
+                System.out.println("---------------------------------------------");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("---------------------------------------------");
+                break;
+            } else{
+                System.out.println("---------------------------------------------");
+                System.out.println(s);
+                System.out.println("---------------------------------------------");
+            }
+        }
+    }
+
 }
