ba99afd4c362f5fbc53ae5b2ebc2b5e7ea8322c6
Guan Yang Ze <guanyangze@gmail.com>
2021-01-20 18:54:53 +0800

    clean up input loop + indentation and divider implementation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
-        String indent = "    ";
-        String divider = "____________________________________________________________";
+        String TEXT_INDENT = "     ";
+        String DIVIDER = "    ____________________________________________________________";
 
         ArrayList<String> tasks = new ArrayList<String>();
 
@@ -11,34 +11,32 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
 
         // welcome sequence
-        System.out.println(indent + divider);
-        System.out.println(indent + " Welcome to Duke!");
-        System.out.println(indent + " What can I do for you?");
-        System.out.println(indent + divider);
+        System.out.println(DIVIDER);
+        System.out.println(TEXT_INDENT + "Welcome to Duke!");
+        System.out.println(TEXT_INDENT + "What can I do for you?");
+        System.out.println(DIVIDER);
 
         // get user input
         String userInput = sc.nextLine();
         while (!userInput.toLowerCase().equals("bye")) {
+            System.out.println(DIVIDER);
+            // display list
             if (userInput.toLowerCase().equals("list")) {
-                System.out.println(indent + divider);
                 for (int i = 0; i < tasks.size(); i++) {
-                    String item = indent + " " + String.valueOf(i+1) + ". " + tasks.get(i);
+                    String item = TEXT_INDENT + String.valueOf(i + 1) + ". " + tasks.get(i);
                     System.out.println(item);
                 }
-                System.out.println(indent + divider);
-                userInput = sc.nextLine();
-            } else {
+            } else { // add task to list
                 tasks.add(userInput);
-                System.out.println(indent + divider);
-                System.out.println(indent + " added: " + userInput);
-                System.out.println(indent + divider);
-                userInput = sc.nextLine();
+                System.out.println(TEXT_INDENT + "added: " + userInput);
             }
+            System.out.println(DIVIDER);
+            userInput = sc.nextLine();
         }
 
         // exit sequence
-        System.out.println(indent + divider);
-        System.out.println(indent + " Bye! Hope you see you again :)");
-        System.out.println(indent + divider);
+        System.out.println(DIVIDER);
+        System.out.println(TEXT_INDENT + "Bye! Hope you see you again :)");
+        System.out.println(DIVIDER);
     }
 }
