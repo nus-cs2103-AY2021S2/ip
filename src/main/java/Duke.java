2c7094c04bc0f741bf1313f7943c377dff103bae
icytornado <perfecttzp@gmail.com>
2021-01-20 22:31:02 +0800

    Level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,8 +14,19 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
-            input = scanner.nextLine();
 
+                input = scanner.nextLine();
+            try {
+                if (!(input.contains("todo") || input.contains("event") || input.contains("deadline")|| input.contains("list") || input.contains("done") )) {
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                } else if (input.equals("todo") || input.equals("event") || input.equals("deadline") || input.equals("done") ) {
+                    throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                }                                 // can add in more exceptions
+            } catch (DukeException e) {
+                System.out.println("--------------------------");
+                System.out.println(e.getMessage());
+                System.out.println("--------------------------");
+            }
             if (input.equals("bye")) {
                 System.out.println("--------------------------");
                 System.out.println("Bye. Hope to see you again soon!");
@@ -30,15 +41,27 @@ public class Duke {
                 }
                 System.out.println("--------------------------");
 
-            } else if (input.contains("done")) {
-                int x = Integer.parseInt(input.substring(5));
-                System.out.println("--------------------------");
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(arrayList.get(x - 1).getStatusIcon() + " " + arrayList.get(x - 1).description);
-                arrayList.set(x - 1, arrayList.get(x - 1).markAsDone());
-                System.out.println("--------------------------");
+            } else if (input.contains("done") && !(input.equals("done"))) {
 
-            } else if (input.contains("todo")) {
+                    int x = Integer.parseInt(input.substring(5));
+                try {
+                    if (x > arrayList.size()) {
+                        throw new DukeException("☹ OOPS!!! Sorry item no found :-(");
+                    } else {
+                        System.out.println("--------------------------");
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println(arrayList.get(x - 1).markAsDone().getStatusIcon() + " " + arrayList.get(x - 1).description);
+                        arrayList.set(x - 1, arrayList.get(x - 1).markAsDone());
+                        System.out.println("--------------------------");
+                    }
+                } catch (DukeException e) {
+                    System.out.println("--------------------------");
+                    System.out.println(e.getMessage());
+                    System.out.println("--------------------------");
+                }
+
+
+            } else if (input.contains("todo") && !(input.equals("todo"))) {
                 System.out.println("--------------------------");
                 System.out.println("Got it. I've added this task: ");
                 Task task = new Todo(input.substring(5));
@@ -49,7 +72,7 @@ public class Duke {
                 System.out.println("Now you have " + arrayList.size() + " task(s) in the list");
                 System.out.println("--------------------------");
 
-            } else if (input.contains("deadline"))  {
+            } else if (input.contains("deadline") && !(input.equals("deadline")))  {
                 System.out.println("--------------------------");
                 System.out.println("Got it. I've added this task: ");
                 String[] parts = input.split("/");
@@ -63,7 +86,7 @@ public class Duke {
                 System.out.println("Now you have " + arrayList.size() + " task(s) in the list");
                 System.out.println("--------------------------");
 
-            } else if (input.contains("event")) {
+            } else if (input.contains("event") && !(input.equals("event"))) {
                 System.out.println("--------------------------");
                 System.out.println("Got it. I've added this task: ");
                 String[] parts = input.split("/");
@@ -77,18 +100,18 @@ public class Duke {
                 System.out.println("Now you have " + arrayList.size() + " task(s) in the list");
                 System.out.println("--------------------------");
 
-        } else {
+        }
+          /*  else {
                 Task task = new Task(input);
                 task.index = listNumber;
                 listNumber++;
                 arrayList.add(task);
                 System.out.println("added: "+ input);
                 System.out.println("--------------------------");
+               }
+        */
 
-        }
-    }
-
-        }
+    } }
 }
 
 
