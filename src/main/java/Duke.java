7f77c25ded08e745da482020cf53ccf5d6386373
nicoleang09 <nicoleang09@gmail.com>
2021-01-20 15:12:42 +0800

    Completed Duke Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    public static ArrayList<String> list;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -12,15 +15,22 @@ public class Duke {
         System.out.println("Hello! How can I help you?");
 
         Scanner scanner = new Scanner(System.in);
+        list  = new ArrayList<>();
 
         while (scanner.hasNext()) {
             String input = scanner.nextLine();
+
             if (input.equals("bye")) {
                 System.out.println("Bye! See you soon :)");
                 scanner.close();
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
             } else {
-                System.out.println(input);
+                list.add(input);
+                System.out.println("Added: " + input);
             }
         }
     }
