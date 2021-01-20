8bfe05d264aa822ddf007bb4906d8a15368014a4
ong6 <junxiongong2@gmail.com>
2021-01-20 23:48:24 +0800

    Level-6 and Fixed issues in Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,13 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        // String logo = " ____ _ \n"
-        // + "| _ \\ _ _| | _____ \n"
-        // + "| | | | | | | |/ / _ \\\n"
-        // + "| |_| | |_| | < __/\n"
-        // + "|____/ \\__,_|_|\\_\\___|\n";
-        // System.out.println("Hello from\n" + logo);
+    public static void main(String[] args) throws DukeException {
 
         List<Task> contentList = new ArrayList<>();
 
@@ -21,55 +15,69 @@ public class Duke {
             String input = sc.nextLine();
             StringBuilder output = new StringBuilder();
 
-            if (input.equals("bye")) {
-                textWarper("Bye. Hope to see you again soon!");
-                break;
-            } else if (input.equals("list")) {
-                output.append("Here are the tasks in your list: \n");
+            try {
+                if (input.equals("bye")) {
+                    textWarper("Bye. Hope to see you again soon!");
+                    break;
 
-                for (int i = 0; i < contentList.size(); i++) {
-                    output.append(String.format("%d.%s%n", i + 1, contentList.get(i).toString()));
-                }
-            } else if (input.startsWith("done")) {
-                output.append("Nice! I've marked this task as done: \n  ");
-
-                int index = Integer.parseInt(input.replaceAll("[^0-9]", "")) - 1;
-                contentList.get(index).setCompleted();
-
-                output.append(contentList.get(index));
-            } else if (input.startsWith("todo") || input.startsWith("deadline") || input.startsWith("event")) {
-                String[] arr = input.split(" ", 2);
-                Task temp;
-                if (input.endsWith("todo") || input.endsWith("deadline") || input.endsWith("event")) {
-                    output.append("☹ OOPS!!! The description of a todo cannot be empty.");
-                } else {
-                    switch (arr[0]) {
-                        case "todo":
-                            temp = new ToDo(arr[1]);
-                            break;
-                        case "deadline":
-                            temp = new Deadline(arr[1]);
-                            break;
-                        case "event":
-                            temp = new Event(arr[1]);
-                            break;
-                        default:
-                            temp = new Task(arr[1]);
+                } else if (input.equals("list")) {
+                    output.append("Here are the tasks in your list: \n");
+                    for (int i = 0; i < contentList.size(); i++) {
+                        output.append(String.format("%d.%s%n", i + 1, contentList.get(i).toString()));
                     }
+
+                } else if (input.startsWith("done")) {
+                    output.append("Nice! I've marked this task as done: \n  ");
+                    int index = Integer.parseInt(input.replaceAll("[^0-9]", "")) - 1;
+                    contentList.get(index).setCompleted();
+                    output.append(contentList.get(index));
+
+                } else if (input.endsWith("todo") || input.endsWith("deadline") || input.endsWith("event")) {
+                    throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+
+                } else if (input.startsWith("todo")) {
+                    String[] arr = input.split(" ", 2);
+                    Task temp = new ToDo(arr[1]);
                     contentList.add(temp);
-                    output.append("Got it. I've added this task:\n ");
-                    output.append(temp.toString() + "\n");
+                    taskAdded(output, contentList, temp);
+
+                } else if (input.startsWith("deadline")) {
+                    String[] arr = input.split(" ", 2);
+                    Task temp = new Deadline(arr[1]);
+                    contentList.add(temp);
+                    taskAdded(output, contentList, temp);
+
+                } else if (input.startsWith("event")) {
+                    String[] arr = input.split(" ", 2);
+                    Task temp = new Event(arr[1]);
+                    contentList.add(temp);
+                    taskAdded(output, contentList, temp);
+
+                } else if (input.startsWith("delete")) {
+                    int index = Integer.parseInt(input.replaceAll("[^0-9]", "")) - 1;
+                    output.append("Noted. I've removed this task: \n  ");
+                    output.append(contentList.get(index) + "\n");
+                    contentList.remove(index);
                     output.append(String.format("Now you have %d tasks in the list.", contentList.size()));
+
+                } else {
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-            } else {
-                textWarper("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                // contentList.add(new Task(input));
-                // output.append("added: " + input);
+            } catch (DukeException e) {
+                output.append(e.toString());
+            } catch (IndexOutOfBoundsException e) {
+                output.append("You have nothing in the list!");
             }
 
             textWarper(output.toString());
         }
 
+    }
+
+    public static void taskAdded(StringBuilder output, List<Task> contentList, Task temp) {
+        output.append("Got it. I've added this task:\n ");
+        output.append(temp.toString() + "\n");
+        output.append(String.format("Now you have %d tasks in the list.", contentList.size()));
     }
 
     public static void textWarper(String a) {
