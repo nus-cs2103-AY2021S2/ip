819aea77b72e08f798815e83abd4f58008123c07
nighoggDatatype <hosherman1965@gmail.com>
2021-01-21 05:00:05 +0800

    Added some error checking

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,49 +17,53 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         String line;
         List<Task> store = new ArrayList<>();
-        String tokens[];
+        String tokens[] = {"invalid"};
         do{
             System.out.print(separator + "Listening to your input: ");
             line = in.nextLine();
-            tokens = line.split(" ",2);
-            switch(tokens[0]){
-                case "bye":
-                    System.out.println(separator + "Goodbye from\n" + logo);
-                    break;
-                case "list":
-                    for (int i = 0 ; i < store.size(); i++) {
-                        System.out.println(formatOrderedPrint(store,i));
-                    }
-                    break;
-                case "done":
-                    int index = Integer.valueOf(tokens[1]) - 1;
-                    Task t = store.get(index);
-                    t.isDone = true;
-                    System.out.println("The following task is now marked as done:\n" +
-                            formatOrderedPrint(store,-1));
-                    break;
-                case "todo":
-                    store.add(new ToDos(tokens[1]));
-                    System.out.println("The following todo item has been added:\n" +
-                            formatOrderedPrint(store,-1));
-                    break;
-                case "deadline":
-                    tokens = tokens[1].split(" /by ",2);
-                    if (tokens.length < 2){
-
-                    }
-                    store.add(new Deadline(tokens[0],tokens[1]));
-                    System.out.println("The following deadline item has been added:\n" +
-                            formatOrderedPrint(store,-1));
-                    break;
-                case "event":
-                    tokens = tokens[1].split(" /at ",2);
-                    store.add(new Event(tokens[0],tokens[1]));
-                    System.out.println("The following event item has been added:\n" +
-                            formatOrderedPrint(store,-1));
-                    break;
-                default:
-                    break;
+            try {
+                tokens = splitTokenIntoTwo(line," ", "bye");
+                switch(tokens[0]){
+                    case "bye":
+                        System.out.println(separator + "Goodbye from\n" + logo);
+                        break;
+                    case "list":
+                        for (int i = 0 ; i < store.size(); i++) {
+                            System.out.println(formatOrderedPrint(store,i));
+                        }
+                        break;
+                    case "done":
+                        int index = Integer.valueOf(tokens[1]) - 1;
+                        Task t = store.get(index);
+                        t.isDone = true;
+                        System.out.println("The following task is now marked as done:\n" +
+                                formatOrderedPrint(store,-1));
+                        break;
+                    case "todo":
+                        store.add(new ToDos(tokens[1]));
+                        System.out.println("The following todo item has been added:\n" +
+                                formatOrderedPrint(store,-1));
+                        break;
+                    case "deadline":
+                        tokens = splitTokenIntoTwo(tokens[1]," /by ");
+                        store.add(new Deadline(tokens[0],tokens[1]));
+                        System.out.println("The following deadline item has been added:\n" +
+                                formatOrderedPrint(store,-1));
+                        break;
+                    case "event":
+                        tokens = splitTokenIntoTwo(tokens[1]," /by ");
+                        store.add(new Event(tokens[0],tokens[1]));
+                        System.out.println("The following event item has been added:\n" +
+                                formatOrderedPrint(store,-1));
+                        break;
+                    default:
+                        throw new InvalidCommandException(tokens[0]);
+                }
+            } catch (ParseException e) {
+                System.out.println("Command has invalid parsing.");
+                System.out.println(e.getMessage());
+            } catch (InvalidCommandException e){
+                System.out.println(e.getMessage());
             }
         }while(!tokens[0].equals("bye"));
     }
@@ -71,5 +76,20 @@ public class Duke {
             i -= size;
         }
         return "Entry " + (i+1) + "|" + tasks.get(i).toString();
+    }
+    private static String[] splitTokenIntoTwo(String parseTarget,String delimiter) throws ParseException{
+        String[] tokens = parseTarget.split(delimiter,2);
+        if (tokens.length < 2){
+            throw new ParseException("Expected deliminter '"+ delimiter +"'", tokens[0].length());
+        }
+        return tokens;
+    }
+    
+    private static String[] splitTokenIntoTwo(String parseTarget,String delimiter, String exception) throws ParseException{
+        String[] tokens = parseTarget.split(delimiter,2);
+        if (!tokens[0].equals(exception) && tokens.length < 2){
+            throw new ParseException("Expected deliminter '"+ delimiter +"'", tokens[0].length());
+        }
+        return tokens;
     }
 }
---
+++ src/main/java/InvalidCommandException.java
@@ -0,0 +1,15 @@
+public class InvalidCommandException extends Exception {
+
+    private String badCommand;
+
+    public InvalidCommandException(String badCommand) {
+        this.badCommand = badCommand;
+	}
+
+	private static final long serialVersionUID = 1L;
+    
+    @Override
+    public String getMessage() {
+        return "Command '" + badCommand + "' is not recognized.";
+    }
+}
