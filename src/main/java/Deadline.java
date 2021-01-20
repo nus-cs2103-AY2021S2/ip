df531f653c29a49f8a4345e2a6b4b19a83ecd1c4
oeiyiping <yiping426@hotmail.com>
2021-01-20 15:04:00 +0800

    Add subclasses of Task: ToDo, Event, Deadline

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,12 @@
+public class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String name, String by) {
+        super(name);
+        this.by = by;
+    }
+
+    public String toString() {
+        return "[D]" + super.toString() + "(by: " + by + ")";
+    }
+}
---
+++ src/main/java/Event.java
@@ -0,0 +1,12 @@
+public class Event extends Task {
+    protected String at;
+
+    public Event(String name, String at) {
+        super(name);
+        this.at = at;
+    }
+
+    public String toString() {
+        return "[E]" + super.toString() + "(at: " + at + ")";
+    }
+}
