823fbe31bdfe9ae8890f2286c8d53b67ed73898f
Winniehyx <e0425411@u.nus.edu>
2021-01-20 09:08:34 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+            String s = "";
+            while(!(s=sc.nextLine()).equals("bye")) {
+                System.out.println(s);
+            }
+            System.out.println("Bye. Hope to see you again soon!");
+        }
 }
