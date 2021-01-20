b6fe0dfef18811b859118fe703411414ca2d46bb
geraldfan <geraldfan@gmail.com>
2021-01-20 18:05:49 +0800

    Added ToDos, Events and Deadlines. Switched to using Enums

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -45,25 +45,41 @@ public class Duke {
     }
 
     public void mark(int index) {
-        Task taskToBeMarked = taskList.get(index-1);
+        Task taskToBeMarked = taskList.get(index - 1);
         taskToBeMarked.markAsDone();
         formatInChatBox("Nice! I've marked this task as done: \n" + taskToBeMarked);
     }
 
-    public boolean chat(String s, Duke d) {
-        if (s.toLowerCase().equals("bye")) {
-            String goodbyeMessage = "Bye. Hope to see you again soon!\n";
-            formatInChatBox(goodbyeMessage);
-            return false;
-        } else if (s.length() > 5 && s.substring(0, 5). equals("done ")) {
-            int index = Integer.parseInt(s.substring(5));
-            d.mark(index);
-        } else if (s.toLowerCase().equals("list")) {
-            d.list();
-        } else {
-            d.add(s);
+    public void exit() {
+        formatInChatBox("Bye. Hope to see you again soon!\n");
+    }
+
+    public void add(String task, String description) {
+        if (task.equals("TODO")) {
+            Task newToDo = new ToDo(description);
+            taskList.add(newToDo);
+            String output = "Got it. I've added this task:\n  " + newToDo + "\n Now you have "
+                    + taskList.size() + " tasks in the list.";
+            formatInChatBox(output);
+        } else if (task.equals("DEADLINE")) {
+            int endIndexOfDescription = description.indexOf("/by ");
+            String deadlineDescription = description.substring(0, endIndexOfDescription);
+            String deadline = description.substring(endIndexOfDescription + 4);
+            Task newDeadline = new Deadline(deadlineDescription, deadline);
+            taskList.add(newDeadline);
+            String output = "Got it. I've added this task:\n  " + newDeadline + "\n Now you have "
+                    + taskList.size() + " tasks in the list.";
+            formatInChatBox(output);
+        } else if (task.equals("EVENT")) {
+            int endIndexOfDescription = description.indexOf("/at ");
+            String eventDescription = description.substring(0, endIndexOfDescription);
+            String eventTime = description.substring(endIndexOfDescription + 4);
+            Task newEvent = new Event(eventDescription, eventTime);
+            taskList.add(newEvent);
+            String output = "Got it. I've added this task:\n  " + newEvent + "\n Now you have "
+                    + taskList.size() + " tasks in the list.";
+            formatInChatBox(output);
         }
-        return true;
     }
 
     public static void main(String[] args) {
@@ -73,8 +89,32 @@ public class Duke {
         Boolean isChatting = true;
 
         while (isChatting) {
-            String input = sc.nextLine();
-            isChatting = mike.chat(input, mike);
+            String input = sc.next();
+            Command c = Command.valueOf(input.toUpperCase());
+            switch (c) {
+                case BYE:
+                    mike.exit();
+                    return;
+                case LIST:
+                    mike.list();
+                    break;
+                case DONE:
+                    int i = sc.nextInt();
+                    mike.mark(i);
+                    break;
+                case TODO:
+                    String toDoDescription = sc.nextLine();
+                    mike.add("TODO", toDoDescription);
+                    break;
+                case DEADLINE:
+                    String deadlineDescription = sc.nextLine();
+                    mike.add("DEADLINE", deadlineDescription);
+                    break;
+                case EVENT:
+                    String eventDescription = sc.nextLine();
+                    mike.add("EVENT", eventDescription);
+                    break;
+            }
         }
         sc.close();
     }
