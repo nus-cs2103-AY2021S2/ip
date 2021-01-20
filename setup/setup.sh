448f1073bb65f5d63170962586b2fe11e151f1c4
Stein121 <qscaxz0@gmail.com>
2021-01-20 13:20:18 +0800

    Update setup files

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cp setup/pre-commit .git/hooks/pre-commit
-git config --add checkstyle.jar 'checkstyle/checkstyle-8.39-all.jar'
-git config --add checkstyle.checkfile 'checkstyle/checkstyle.xml'
+git config --add checkstyle.jar 'config/checkstyle/checkstyle-8.39-all.jar'
+git config --add checkstyle.checkfile 'config/checkstyle/checkstyle.xml'
+
