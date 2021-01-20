7f568a21a4a2356c41ab7aa03a6e484604342ebc
juhaerpasha <juhaeradittya200@gmail.com>
2021-01-20 23:50:50 +0800

    Start level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,6 @@
 public class Duke {
     //Main method where the classes are initialized
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         UI ui = new UI();
 
         String command;
@@ -12,21 +12,25 @@ public class Duke {
             command = ui.getCommand();
             String[] executable = new String[100];
             executable = command.split(" ");
-            if (command.equals("bye")) {
-                ui.goodbye();
-                break;
-            } else if (command.equals("list")) {
-                taskList.printTasks();
-            } else if (executable[0].equals("done")) {
-                taskList.markAsDone(Integer.parseInt(executable[1]));
-            } else if (executable[0].equals("todo")) {
-                taskList.addTask(new TodoTask(command));
-            } else if (executable[0].equals("deadline")) {
-                taskList.addTask(new DeadlineTask(command));
-            } else if (executable[0].equals("event")) {
-                taskList.addTask(new EventTask(command));
-            } else {
-                taskList.addTask(new Task(command));
+            try {
+                if (command.equals("bye")) {
+                    ui.goodbye();
+                    break;
+                } else if (command.equals("list")) {
+                    taskList.printTasks();
+                } else if (executable[0].equals("done")) {
+                    taskList.markAsDone(Integer.parseInt(executable[1]));
+                } else if (executable[0].equals("todo")) {
+                    taskList.addTask(new TodoTask(command));
+                } else if (executable[0].equals("deadline")) {
+                    taskList.addTask(new DeadlineTask(command));
+                } else if (executable[0].equals("event")) {
+                    taskList.addTask(new EventTask(command));
+                } else {
+                    taskList.addTask(new Task(command));
+                }
+            }catch(Exception e){
+                throw new DukeException("Oops! Invalid input!");
             }
         }
     }
