5605c9ff5ff2ceee45df7a23dac3c8c11274f083
Guan Yang Ze <guanyangze@gmail.com>
2021-01-20 16:21:44 +0800

    add greet, echo, exit functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String indent = "    ";
+        String divider = "____________________________________________________________";
+
+        // initialize scanner
+        Scanner sc = new Scanner(System.in);
+
+        // welcome sequence
+        System.out.println(indent + divider);
+        System.out.println(indent + " Welcome to Duke!");
+        System.out.println(indent + " What can I do for you?");
+        System.out.println(indent + divider);
+
+        // get user input
+        String userInput = sc.nextLine();
+        while (!userInput.toLowerCase().equals("bye")) {
+            System.out.println(indent + divider);
+            System.out.println(indent + userInput);
+            System.out.println(indent + divider);
+            userInput = sc.nextLine();
+        }
+
+        // exit sequence
+        System.out.println(indent + divider);
+        System.out.println(indent + " Bye! Hope you see you again :)");
+        System.out.println(indent + divider);
     }
 }
