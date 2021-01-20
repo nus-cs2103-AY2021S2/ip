e029359abeaddd845afee28203d737678c866310
jrvslam <jarveis.lamzx@gmail.com>
2021-01-20 11:30:45 +0800

    Implemented Skeletal structure for project

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,24 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("   Hello There! I'm Duke, always here for you!");
+        System.out.println("   How can I help you today?");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        while (!input.equals("bye")) {
+            switch (input) {
+                case "list":
+                    System.out.println("___\n   list\n___");
+                    input = sc.next();
+                    break;
+                default:
+                    System.out.println("___\n   blah\n___");
+                    input = sc.next();
+            }
+        }
+
+        System.out.println("___\n Bye! Hope to see you soon! :) \n___");
     }
 }
