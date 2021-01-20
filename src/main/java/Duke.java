455c411b3b03d214a90b2a0756d19b132f5980a5
Samuel Lee <samuel.lwy@live.com.sg>
2021-01-21 02:03:48 +0800

    Level 6 - added support for deleting tasks from list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,7 +13,7 @@ public class Duke {
         System.out.println("Olly: " + message);
     }
 
-    private static void inputHandler(String input) {
+    private static void inputHandler(String input) throws DukeException {
         if (input.equals("bye")) {
             ollySpeak("Goodbye for now, we will meet again.");
             System.exit(0);
@@ -23,18 +23,26 @@ public class Duke {
             printTasks();
         } else if (input.startsWith("todo")) {
             String[] command = input.split("todo ");
+            if (command.length == 1) throw new DukeException("The description of a todo cannot be empty.");
             Todo todo = new Todo(command[1]);
             addTask(todo);
         } else if (input.startsWith("deadline")) {
             String[] command = input.split("deadline ");
+            if (command.length == 1) throw new DukeException("The description of a deadline cannot be empty.");
             String deadlineArg = command[1];
+
             String[] byArgs = deadlineArg.split(" /by ");
+            if (byArgs.length < 2) throw new DukeException("There must be a date for deadline.");
+
             Deadline deadline = new Deadline(byArgs[0], byArgs[1]);
             addTask(deadline);
         } else if (input.startsWith("event")) {
             String[] command = input.split("event ");
+            if (command.length == 1) throw new DukeException("The description of a event cannot be empty.");
             String eventArg = command[1];
+
             String[] atArgs = eventArg.split(" /at ");
+            if (atArgs.length < 2) throw new DukeException("There must be a date for event.");
             Event event = new Event(atArgs[0], atArgs[1]);
             addTask(event);
         } else if (input.startsWith("done")) {
@@ -48,9 +56,20 @@ public class Duke {
             } else {
                 ollySpeak("The task number does not work, try again?");
             }
+        } else if (input.startsWith("delete")) {
+            String[] command = input.split(" ");
+            int index = Integer.parseInt(command[1]);
+            if (index > 0 && index <= tasks.size()) {
+                Task doneTask = tasks.get(index - 1);
+                tasks.remove(doneTask);
+                ollySpeak("Aww man.. I've removed this task:");
+                System.out.println(doneTask);
+                ollySpeak("Now you have " + getTaskCount() + " tasks left.");
+            } else {
+                ollySpeak("The task number does not work, try again?");
+            }
         } else {
-            Task newTask = new Task(input);
-            addTask(newTask);
+            throw new DukeException("I don't understand your language leh. Speak singlish?");
         }
     }
 
@@ -66,9 +85,13 @@ public class Duke {
     }
 
     private static void printTasks() {
-        ollySpeak("Here you go! Your list of items:");
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println(i+1 + ". " + tasks.get(i));
+        if (getTaskCount() == 0) {
+            ollySpeak("You currently have no tasks! Use todo, deadline or event.");
+        } else {
+            ollySpeak("Here you go! Your list of items:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println(i+1 + ". " + tasks.get(i));
+            }
         }
     }
 
@@ -78,7 +101,11 @@ public class Duke {
 
         while (sc.hasNext()) {
             String input = sc.nextLine();
-            inputHandler(input);
+            try {
+                inputHandler(input);
+            } catch (DukeException dukeEx) {
+                System.out.println(dukeEx);
+            }
         }
     }
 }
--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,4 +1,12 @@
 package main.java;
 
-public class DukeException {
+public class DukeException extends Exception {
+    DukeException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String toString() {
+        return "ALAMAK! " + getMessage();
+    }
 }
