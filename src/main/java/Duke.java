c9dc05bc0985bde31c3a36f3c5097f3fc1208224
zhengruoxin <zhengruoxin99@gmail.com>
2021-01-20 14:04:04 +0800

    add Main.java and message echo function in Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.*;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    void chat() {
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        while (!(input.equals("bye"))) {
+            System.out.println(input);
+            in = new Scanner(System.in);
+            input = in.nextLine();
+        }
+        if (input.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
     }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        Duke chatbot = new Duke();
+        chatbot.chat();
+    }
+}
