552688d93f49d7f07ceeaf94c81815ad8c858879
Daniel Ong <danielonges@Daniels-MacBook-Pro-3.local>
2021-01-20 15:48:49 +0800

    Create TaskManager object

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.ArrayList;
 
 public class TaskManager {
+    public List<Task> taskList;
+
+    public TaskManager() {
+        taskList = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        taskList.add(task);
+    }
+
+    public List getTaskList() {
+        return taskList;
+    }
+
+    public void printTasks() {
+        int count = 1;
+        for (Task task : taskList) {
+            System.out.println(count++ + ". " + task);
+        }
+    }
 }
