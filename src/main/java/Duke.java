a1fa3ee16ce70dd73fb46dfaf36dd09df374ec32
IanCKW <e0392531@u.nus.edu>
2021-01-20 21:55:27 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,34 @@
 import java.util.*;
 
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public List<String> list = new ArrayList<>();
 
+    public Duke(){
+    }
+
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n"
+                +"Hello! I'm Duke\n"
+                +"What can I do for you?\n"
+                + " ____________________________________________________________");
+        Duke D = new Duke();
         Scanner sc = new Scanner(System.in);
         String line = "____________________________________________________________";
         String currString = sc.nextLine();
         while(!currString.equals("bye")){
-
-            System.out.println( line + "\n" + currString +"\n" + line);
+            if(currString.equals("list")){
+                System.out.println(line);
+                int i = 1;
+                for(String s: D.list){
+                    System.out.format("%d. %s \n", i, s);
+                    i++;
+                }
+                System.out.println(line);
+            }
+            else {
+                D.list.add(currString);
+                System.out.println(line + "\n" + "added: " + currString + "\n" + line);
+            }
             currString = sc.nextLine();
         }
         System.out.println(line + "\n" + " Bye. Hope to see you again soon!"+"\n" + line);
