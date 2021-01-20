dbb733600789e4b8e7f77d7a9e9abb1cdd109f8a
Daniel Ong <danielonges@Daniels-MacBook-Pro-3.local>
2021-01-20 14:52:24 +0800

    Redesign greeting logo

--- src/main/java/ToDoBeast.java
+++ src/main/java/ToDoBeast.java
@@ -1,10 +1,20 @@
 public class ToDoBeast {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "\t____________________________________________________________\n";
+        String logo = "                                                     \n" +
+                "\t88                                                   \n" +
+                "\t88                                            ,d     \n" +
+                "\t88                                            88     \n" +
+                "\t88,dPPYba,   ,adPPYba, ,adPPYYba, ,adPPYba, MM88MMM  \n" +
+                "\t88P'    \"8a a8P_____88 \"\"     `Y8 I8[    \"\"   88     \n" +
+                "\t88       d8 8PP\"\"\"\"\"\"\" ,adPPPPP88  `\"Y8ba,    88     \n" +
+                "\t88b,   ,a8\" \"8b,   ,aa 88,    ,88 aa    ]8I   88,    \n" +
+                "\t8Y\"Ybbd8\"'   `\"Ybbd8\"' `\"8bbdP\"Y8 `\"YbbdP\"'   \"Y888  \n" +
+                "\t                                                     \n" +
+                "\t                                                     \n";
+        String greeting = line + logo + "\tWelcome to ToDoBeast, your best productivity task tracker tool!\n"
+                + "\tLet's get this bread! How would you like to be productive today?\n" + line;
+        System.out.println(greeting);
+
     }
 }
