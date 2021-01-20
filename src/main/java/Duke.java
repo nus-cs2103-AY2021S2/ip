d8b325f5c7848cc18ca4c85c1ebf98d684d26942
nighoggDatatype <hosherman1965@gmail.com>
2021-01-21 03:00:00 +0800

    Done Level-4, with some refactoring

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,26 +20,53 @@ public class Duke {
         do{
             System.out.print(separator + "Listening to your input: ");
             line = in.nextLine();
-            tokens = line.split(" ");
+            tokens = line.split(" ",2);
             switch(tokens[0]){
                 case "bye":
                     System.out.println(separator + "Goodbye from\n" + logo);
                     break;
                 case "list":
                     for (int i = 0 ; i < store.size(); i++) {
-                        System.out.println("Entry " + (i+1) + "|" + store.get(i).toString());
+                        System.out.println(formatOrderedPrint(store,i));
                     }
                     break;
                 case "done":
                     int index = Integer.valueOf(tokens[1]) - 1;
                     Task t = store.get(index);
                     t.isDone = true;
-                    System.out.println("The following task is now marked as done:\n" + t.toString());
+                    System.out.println("The following task is now marked as done:\n" +
+                            formatOrderedPrint(store,-1));
+                    break;
+                case "todo":
+                    store.add(new ToDos(tokens[0]));
+                    System.out.println("The following todo item has been added:\n" +
+                            formatOrderedPrint(store,-1));
+                    break;
+                case "deadline":
+                    tokens = tokens[1].split(" /by ",2);
+                    store.add(new Deadline(tokens[0],tokens[1]));
+                    System.out.println("The following deadline item has been added:\n" +
+                            formatOrderedPrint(store,-1));
+                    break;
+                case "event":
+                    tokens = tokens[1].split(" /at ",2);
+                    store.add(new Deadline(tokens[0],tokens[1]));
+                    System.out.println("The following event item has been added:\n" +
+                            formatOrderedPrint(store,-1));
                     break;
                 default:
-                    store.add(new Task(line));
-                    System.out.println("Added: " + line);
+                    break;
             }
         }while(!tokens[0].equals("bye"));
+    }
+    private static String formatOrderedPrint(List<Task> tasks, int i){
+        final int size = tasks.size();
+        while (i < 0){
+            i += size;
+        }
+        while (i >= size){
+            i -= size;
+        }
+        return "Entry " + (i+1) + "|" + tasks.get(i).toString();
     }
 }
