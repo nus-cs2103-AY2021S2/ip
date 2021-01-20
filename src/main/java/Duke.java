2ddb56c34d7a9b7d3fe8cf1adb794beb662201ae
ninijiaying <cooolnichole@gmail.com>
2021-01-21 02:31:52 +0800

    add Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.*;
+import java.io.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner in = new Scanner(System.in);
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        String commands = in.next();
+        while(!commands.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + commands);
+            System.out.println("    ____________________________________________________________");
+            commands = in.next();
+        }
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+
     }
 }
