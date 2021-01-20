d53ae9ce347d11eaeaaef32bde394892de482e9f
pngsebastian <pngsebastian@gmail.com>
2021-01-21 02:32:28 +0800

    feat: mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Duke {
     private static String separator = "____________________________________________________________";
-    private static ArrayList<String> tasksList;
+    private static ArrayList<Task> tasksList;
     private static int counter;
 
     private static void replyFormat(String reply) {
@@ -22,22 +22,39 @@ public class Duke {
     }
 
     // Adds a task to tasksList
-    private static void addTask(String task) {
-        tasksList.add(task);
+    private static void addTask(String userInput) {
+        Task newTask = new Task(userInput);
+        tasksList.add(newTask);
         counter++;
-        String addedMessage = "added: " + task;
+        String addedMessage = "added: " + newTask.description;
         replyFormat(addedMessage);
     }
 
+    // Marks a task as done and informs the user about it
+    private static void completeTask(int taskNo) {
+        if (taskNo > 0 & taskNo <= tasksList.size()) {
+            Task taskToComplete = tasksList.get(taskNo - 1);
+            taskToComplete.markAsDone();
+            String doneMessage = "Nice! I've marked this task as done:\n" + "  "
+                    + taskToComplete.getStatusIcon() + " " + taskToComplete.description;
+            replyFormat(doneMessage);
+        } else {
+            replyFormat("Task " + taskNo + " is not in the task list!");
+        }
+    }
+
     // Displays all the tasks in tasksList to the user
-    private static void displayTasks(ArrayList<String> tasksList) {
+    private static void displayTasks(ArrayList<Task> tasksList) {
         if (tasksList.size() <= 0) {
             replyFormat("There are no tasks at the moment.");
         } else {
             System.out.println(separator);
+            System.out.println("Here are the tasks in your list:");
 
             for (int i = 1; i <= tasksList.size(); i++) {
-                System.out.println(i + ". " + tasksList.get(i - 1));
+                Task currentTask = tasksList.get(i - 1);
+                System.out.println(i + ". " + currentTask.getStatusIcon()
+                                            + " " + currentTask.description);
             }
 
             System.out.println(separator + "\n");
@@ -53,8 +70,13 @@ public class Duke {
         String userInput = sc.nextLine();
 
         while (!userInput.equals("bye")) {
-            if (userInput.equals("list")) {
+            String[] inputArray = userInput.split(" ");
+            String prompt = inputArray[0];
+
+            if (prompt.equals("list")) {
                 displayTasks(tasksList);
+            } else if (prompt.equals("done")) {
+                completeTask(Integer.valueOf(inputArray[1]));
             } else {
                 addTask(userInput);
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,17 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return isDone ? "[X]" : "[ ]";
+    }
+}
