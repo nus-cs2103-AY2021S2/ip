14607e361859675b89963b4ef2ced744a3090da8
geraldfan <geraldfan@gmail.com>
2021-01-20 16:38:50 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private final StringBuilder lengthOfChatBox = new StringBuilder();
+    private final ArrayList<String> textList = new ArrayList<>();
 
     private void setLengthOfChatBox() {
         lengthOfChatBox.append("\n");
@@ -19,15 +21,38 @@ public class Duke {
         formatInChatBox(greeting);
     }
 
-    public boolean chat(String s) {
+    public void list() {
+        int numOfText = textList.size();
+        StringBuilder output = new StringBuilder();
+
+        if (textList.isEmpty()) {
+            output.append("No text saved\n");
+        } else {
+            int n = 1;
+            for (int i = 0; i < textList.size(); i++) {
+                output.append(i + 1).append(". ").append(textList.get(i)).append("\n");
+            }
+        }
+        formatInChatBox(output.toString());
+    }
+
+    public void add(String s) {
+        textList.add(s);
+        String output = "Added: " + s;
+        formatInChatBox(output);
+    }
+
+    public boolean chat(String s, Duke d) {
         if (s.toLowerCase().equals("bye")) {
             String goodbyeMessage = "Bye. Hope to see you again soon!\n";
             formatInChatBox(goodbyeMessage);
             return false;
+        } else if (s.toLowerCase().equals("list")) {
+            d.list();
         } else {
-            formatInChatBox(s);
-            return true;
+            d.add(s);
         }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -37,8 +62,9 @@ public class Duke {
         Boolean isChatting = true;
 
         while (isChatting) {
-            String input = sc.next();
-            mike.chat(input);
+            String input = sc.nextLine();
+            isChatting = mike.chat(input, mike);
         }
+        sc.close();
     }
 }
