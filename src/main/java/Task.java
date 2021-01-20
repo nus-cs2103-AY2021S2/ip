798b31f04b89378faedb9cf216f500180a0f1e93
Joel <joelhoengkiat@gmail.com>
2021-01-20 15:55:54 +0800

    Add Task skeleton

---
+++ src/main/java/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    public Task(String desc) {
+        this.description = desc;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%c] %s", isDone ? '✓' : '✘', description);
+    }
+}
