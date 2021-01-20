d2e21526b72e116c554053b24c25eb24c4b11081
lue97 <llue97@hotmail.com>
2021-01-20 23:13:38 +0800

    restructured code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -17,8 +19,12 @@ public class Duke {
 
     private final String GREETING = "Hello :)";
     private final String BYE = "See you next time :)";
-    private final String ADD_TASK = "Got it. I've added this task:\n%s\nNow you have %d task(s) in the list";
+    private final String TASK_ADD = "Got it. I've added this task:\n%s\nNow you have %d task(s) in the list";
     private final String TASK_DONE = "Nice! I've marked this task as done:\n%s";
+
+    private static final Pattern TODO_REGEX = Pattern.compile("todo\\s+(.*)");
+    private static final Pattern DEADLINE_REGEX = Pattern.compile("deadline\\s+(.*)\\s+/by\\s+(.*)");
+    private static final Pattern EVENT_REGEX = Pattern.compile("event\\s+(.*)\\s+/at\\s+(.*)");
 
     private final String currentMessage;
     private final List<Task> store;
@@ -27,87 +33,114 @@ public class Duke {
         this.currentMessage = currentMessage;
         this.store = newStore;
         this.done = done;
-        if (this.done) {
-            currentMessage = BYE;
-        }
     }
 
     private String getGreeting() {
         return GREETING;
     }
 
-    private Duke process(String input) {
+    private Duke processAdd(String command) {
         Duke newAgent;
-        String[] tokens = input.split(" ");
-        if (input.equals("bye")) {
-            newAgent = new Duke(BYE, this.store, true);
-        } else if (input.equals("list")) {
-            String response = IntStream.range(0, store.size())
-                    .mapToObj(i -> (i + 1) + ". " + this.store.get(i))
-                    .collect(Collectors.joining("\n"));
-            newAgent = new Duke(response, this.store, false);
-        } else if (tokens[0].equals("todo") || tokens[0].equals("deadline") || tokens[0].equals("event")) {
-            try {
-                List<Task> newStore = new ArrayList<>();
-                for (Task t : this.store) {
-                    newStore.add(t.clone());
-                }
-                Task t = null;
-                switch (tokens[0]) {
-                    case "todo":
-                        t = new TaskTodo(input.substring("todo".length() + 1).trim(), false);
-                        break;
-                    case "deadline":
-                        String deadline = input.substring(input.indexOf("/by ") + 4);
-                        t = new TaskDeadline(input.substring("deadline".length() + 1, input.indexOf(" /by ")).trim(),
-                                false, deadline);
-                        break;
-                    case "event":
-                        String time = input.substring(input.indexOf("/at ") + 4);
-                        t = new TaskEvent(input.substring("event".length() + 1, input.indexOf(" /at ")).trim(),
-                                false, time);
-                        break;
-                }
-                newStore.add(t);
-                String response = String.format(ADD_TASK, t, newStore.size());
-                newAgent = new Duke(response, newStore, false);
-            } catch (StringIndexOutOfBoundsException stringIndexOutOfBoundsException) {
-                newAgent = new Duke("Invalid command. Are you missing some arguments?",
-                        this.store,
-                        this.done);
-            }
-            return newAgent;
-        } else if (tokens[0].equals("done")) {
-            newAgent = this;
-            int index = 0;
-            try {
-                index = Integer.parseInt(tokens[1].trim());
-                if (index < 1 || index > this.store.size()) {
-                    throw new DukeException("Index " + index + " out of bounds for list of size " + this.store.size());
-                }
-                List<Task> newStore = new ArrayList<>();
-                String response = "";
-                Task t;
-                for (int i = 0; i <  this.store.size(); i++) {
-                    if (i + 1 == Integer.parseInt(tokens[1])) {
-                        newStore.add(this.store.get(i).setDone(true));
-                        response = String.format(TASK_DONE, newStore.get(i));
-                    } else {
-                        newStore.add(this.store.get(i).clone());
+        Matcher matcher;
+        String[] tokens = command.split("\\s+");
+        try {
+            Task task;
+            switch (tokens[0]) {
+                case "todo":
+                    matcher = TODO_REGEX.matcher(command);
+                    if (!matcher.find()) {
+                        throw new DukeException("todo Usage: todo [activity]");
                     }
-                }
-                newAgent = new Duke(response, newStore, false);
-            } catch (NumberFormatException numberFormatException) {
-                newAgent = new Duke("Please enter an integer", this.store, false);
-            } catch (DukeException dukeException) {
-                newAgent = new Duke(
-                        "Index " + index + " out of bounds for list of size " + this.store.size(),
-                        this.store, false);
+                    task = new TaskTodo(matcher.group(1), false);
+                    break;
+                case "deadline":
+                    matcher = DEADLINE_REGEX.matcher(command);
+                    if (!matcher.find()) {
+                        throw new DukeException("deadline Usage: deadline [activity] /by [deadline]");
+                    }
+                    task = new TaskDeadline(matcher.group(1), false, matcher.group(2));
+                    break;
+                case "event":
+                    matcher = EVENT_REGEX.matcher(command);
+                    if (!matcher.find()) {
+                        throw new DukeException("event Usage: event [activity] /at [time]");
+                    }
+                    task = new TaskEvent(matcher.group(1), false, matcher.group(2));
+                    break;
+                default: throw new IllegalStateException();
             }
-        } else {
-            newAgent = new Duke(input, this.store, false);
+            List<Task> newStore = this.store.stream().map(t -> t.clone()).collect(Collectors.toList());
+            newStore.add(task);
+            String response = String.format(TASK_ADD, task, newStore.size());
+            newAgent = new Duke(response, newStore, false);
+        }
+        catch (DukeException dukeException) {
+            newAgent = new Duke(dukeException.getMessage(), this.store, this.done);
+        } catch (IllegalStateException illegalStateException) {
+            newAgent = new Duke("Something that was not supposed to happen happened", this.store, this.done);
         }
         return newAgent;
+    }
+
+    private Duke processBye(String command) {
+        return new Duke(BYE, this.store, true);
+    }
+
+    private Duke processList(String command) {
+        String response;
+        if (this.store.size() <= 0) {
+            response = "Your schedule is empty";
+        } else {
+            response = IntStream.range(0, store.size())
+                    .mapToObj(i -> (i + 1) + "." + this.store.get(i))
+                    .collect(Collectors.joining("\n"));
+        }
+        return new Duke(response, this.store, false);
+    }
+
+    private Duke processDone(String command) {
+        Duke newAgent;
+        String[] tokens = command.split("\\s+");
+        try {
+            int index = Integer.parseInt(tokens[1]);
+            if (index < 0 || index > this.store.size()) {
+                throw new DukeException(
+                        String.format("Index %d out of bounds for schedule of size %d.", index, this.store.size()));
+            }
+            List<Task> newStore = this.store.stream().map(t -> t.clone()).collect(Collectors.toList());
+            newStore.set(index - 1, newStore.get(index - 1).setDone(true));
+            String response = String.format(TASK_DONE, this.store.get(index - 1));
+            newAgent = new Duke(TASK_DONE, newStore, this.done);
+        }
+        catch (NumberFormatException numberFormatException) {
+            newAgent = new Duke("Invalid number", this.store, this.done);
+        }
+        catch (DukeException dukeException) {
+            newAgent = new Duke(dukeException.getMessage(), this.store, this.done);
+        }
+        return newAgent;
+    }
+
+    private Duke processEcho(String command) {
+        return new Duke(command, this.store, this.done);
+    }
+
+    private Duke process(String input) {
+        String[] tokens = input.split("\\s+");
+        switch (tokens[0]) {
+            case "todo":
+            case "deadline":
+            case "event":
+                return processAdd(input);
+            case "done":
+                return processDone(input);
+            case "bye":
+                return processBye(input);
+            case "list":
+                return processList(input);
+            default:
+                return processEcho(input);
+        }
     }
 
     private String getCurrentMessage() {
