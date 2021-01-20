f8811371edceceb2495896d0a5d2b477929b372d
IanCKW <55784952+IanCKW@users.noreply.github.com>
2021-01-20 21:05:44 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+        String line = "____________________________________________________________";
+        String currString = sc.nextLine();
+        while(!currString.equals("bye")){
+
+            System.out.println( line + "\n" + currString +"\n" + line);
+            currString = sc.nextLine();
+        }
+        System.out.println(line + "\n" + " Bye. Hope to see you again soon!"+"\n" + line);
+
+
     }
 }
