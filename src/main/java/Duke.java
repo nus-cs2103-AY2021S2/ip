d9114511d4d1d5838f873aed6c234c402a1615fb
yifan <yifanqin@gmail.com>
2021-01-21 04:09:16 +0800

    first commit, level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import  java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        while(sc.hasNext()){
+            String out = sc.next();
+            System.out.println(out);
+        }
     }
 }
