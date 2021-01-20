ca92c56902efdebbecf18053f9f7aa035c9c387c
oeiyiping <yiping426@hotmail.com>
2021-01-20 11:47:52 +0800

    Duke class: Add greet() and listen() instance methods
    greet() prints the designated message.
    
    listen() accepts a user command and echoes said command.
    After the printing the previous command, Duke then waits for a new
    command. Active listening via a while-loop allows Duke to repeatedly
    carry out the above cycle until "bye" command is received.
    
    Upon initiating Duke object, greet() and listen() are called to
    immediately greet and accept commands.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
+    private final Scanner scanner;
+    private boolean isActive;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +11,34 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Duke duke = new Duke();
+
+    }
+
+    public Duke() {
+        isActive = true;
+        scanner = new Scanner(System.in);
+
+        greet();
+        listen();
+    }
+
+    private void greet() {
+        System.out.println("Hello, I'm Duke!\n" + "What can I do for you?\n");
+    }
+
+    private void listen() {
+        while (isActive) {
+            System.out.println("Let me know what to do!");
+            String command = scanner.next();
+
+            if (command.equals("bye")) {
+                isActive = false;
+                System.out.println("Bye bye, see you soon!\n");
+            } else {
+                System.out.println(command + "\n");
+            }
+        }
     }
 }
