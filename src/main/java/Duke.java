56ebbca8075ec53fa9917074cccc35362503b480
yifan <yifanqin@gmail.com>
2021-01-21 04:37:33 +0800

    level 1, added list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
 import  java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> arr = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,11 +17,16 @@ public class Duke {
 
         while(sc.hasNext()){
             String out = sc.next();
-            if(out.equals("bye")){
+            if(out.equals("bye")) {
                 System.out.println("Bye. Hope to see you again");
                 break;
+            }else if (out.equals("list")){
+                for(int i = 1; i <= arr.size();  i++){
+                    System.out.println(i + ". " + arr.get(i-1));
+                }
             }else {
-                System.out.println(out);
+                arr.add(out);
+                System.out.println("added: " + out);
             }
         }
     }
