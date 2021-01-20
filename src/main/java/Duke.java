8d97c6761c7c52b3e6af7a33dfc8bb704593ef9e
YuHeem <yuuuheem@gmail.com>
2021-01-21 00:32:02 +0800

    feat: add DukeException class to handle errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class Duke {
     public static boolean isRunning;
@@ -28,54 +27,103 @@ public class Duke {
 
     public static void waitForInput(Scanner in) {
         while (isRunning) {
-            String input = in.nextLine();
-            handleInput(input);
+            try {
+                String input = in.nextLine();
+                handleInput(input);
+            } catch (DukeException e) {
+                printDivider();
+                System.out.println(e.getMessage());
+                printDivider();
+            }
         }
     }
 
-    public static void handleInput(String input) {
+    public static void handleInput(String input) throws DukeException {
         String[] inputArr = input.split(" ", 2);
-        String command = inputArr[0];
-        if (command.equals("todo")) {
-            handleTodo(inputArr[1]);
-        } else if (command.equals("deadline")) {
-            handleDeadline(inputArr[1]);
-        } else if (command.equals("event")) {
-            handleEvent(inputArr[1]);
-        } else if (command.equals("bye")) {
-            handleBye();
-        } else if (command.equals("list")) {
-            handleList();
-        } else if (command.equals("done")) {
-            handleDone(inputArr[1]);
+
+        if (inputArr.length == 0) {
+            throw new DukeException("Please enter a command!");
+        } else if (inputArr.length == 1) {
+            String command = inputArr[0];
+            handleCommand(command, null);
+        } else {
+            String command = inputArr[0];
+            String rest = inputArr[1];
+            handleCommand(command, rest);
         }
     }
 
-    public static void handleTodo(String input) {
-        Todo todo = new Todo(input);
-        taskList.add(new Todo(input));
-        printAddedTask(todo);
+    public static void handleCommand(String command, String rest) throws DukeException {
+        switch (command) {
+            case "todo":
+                handleTodo(rest);
+                break;
+            case "deadline":
+                handleDeadline(rest);
+                break;
+            case "event":
+                handleEvent(rest);
+                break;
+            case "bye":
+                handleBye();
+                break;
+            case "list":
+                handleList();
+                break;
+            case "done":
+                handleDone(rest);
+                break;
+            default:
+                throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
     }
 
-    public static void handleDeadline(String input) {
-        String[] inputArr = input.split(" /by ", 2);
-        Deadline deadline = new Deadline(inputArr[0], inputArr[1]);
-        taskList.add(deadline);
-        printAddedTask(deadline);
+    public static void handleTodo(String input) throws DukeException {
+        if (input != null) {
+            Todo todo = new Todo(input);
+            taskList.add(new Todo(input));
+            printAddedTask(todo);
+        } else {
+            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+        }
     }
 
-    public static void handleEvent(String input) {
-        String[] inputArr = input.split(" /at ", 2);
-        Event event = new Event(inputArr[0], inputArr[1]);
-        taskList.add(event);
-        printAddedTask(event);
+    public static void handleDeadline(String input) throws DukeException {
+        if (input != null) {
+            String[] inputArr = input.split(" /by ", 2);
+            if (inputArr.length == 2) {
+                Deadline deadline = new Deadline(inputArr[0], inputArr[1]);
+                taskList.add(deadline);
+                printAddedTask(deadline);
+            } else {
+                throw new DukeException("☹ OOPS!!! The due date of a deadline cannot be empty.");
+            }
+        } else {
+            throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+        }
+    }
+
+    public static void handleEvent(String input) throws DukeException {
+        if (input != null) {
+            String[] inputArr = input.split(" /at ", 2);
+            if (inputArr.length == 2) {
+                Event event = new Event(inputArr[0], inputArr[1]);
+                taskList.add(event);
+                printAddedTask(event);
+            } else {
+                throw new DukeException("☹ OOPS!!! The occurrence time of an event cannot be empty.");
+            }
+
+        } else {
+            throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.");
+        }
     }
 
     public static void printAddedTask(Task task) {
         printDivider();
         System.out.println("Got it. I've added this task:");
         System.out.println(task);
-        System.out.println(String.format("Now you have %d tasks in the list", taskList.size()));
+        System.out.printf("Now you have %d tasks in the list%n", taskList.size());
         printDivider();
     }
 
@@ -93,17 +141,21 @@ public class Duke {
         int count = 1;
         if (taskList.size() > 0) {
             for (Task task : taskList) {
-                System.out.println(String.format("%d.%s", count++, task));
+                System.out.printf("%d.%s%n", count++, task);
             }
         }
         printDivider();
     }
 
-    public static void handleDone(String strIdx) {
-        Integer idx = Integer.parseInt(strIdx) - 1;
-        Task task = taskList.get(idx);
-        task.setDone(true);
-        printDoneTask(task);
+    public static void handleDone(String strIdx) throws DukeException {
+        if (strIdx != null) {
+            int idx = Integer.parseInt(strIdx) - 1;
+            Task task = taskList.get(idx);
+            task.setDone(true);
+            printDoneTask(task);
+        } else {
+            throw new DukeException("☹ OOPS!!! The id of a done command cannot be empty.");
+        }
     }
 
     public static void printDoneTask(Task task) {
@@ -111,19 +163,6 @@ public class Duke {
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(task);
         printDivider();
-    }
-
-    public static void handleCommand(String command) {
-        if (command.equals("bye")) {
-            isRunning = false;
-        } else if (command.equals("list")) {
-            IntStream.range(0, taskList.size())
-                    .forEach(idx -> {
-                        System.out.println(idx + 1 + "." + taskList.get(idx));
-                    });
-        } else if (command.equals("done")) {
-
-        }
     }
 
     public static void endDuke() {
