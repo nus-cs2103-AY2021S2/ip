007134a63c393d66c07c9f1735017f9acc950ec0
Prerthan <e0407819@u.nus.edu>
2021-01-20 13:31:08 +0800

    code update for Duke level 2 the code for Duke Level 1 is commented

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,6 +13,7 @@ public class Duke {
 
         //task 1
         //greeting line
+        /*
         System.out.println("Hello my name is Duke\n" + "What can i do for you");
         Scanner sc = new Scanner(System.in); // takes in input
         while(sc.hasNext()){ //while there is an input
@@ -22,6 +23,25 @@ public class Duke {
                 break; //break the loop
             } else { //if the input is not the terminating word
                 System.out.println(s); ///print out the input
+            }
+        }
+        */
+
+        //task 2
+        //add list
+        System.out.println("Hello! I am Duke\n" + "What can I do for you"); //print out greeting line
+        Scanner sc = new Scanner(System.in); //take in input
+        ArrayList xs = new ArrayList(); ///create arraylist to store words
+        while(sc.hasNextLine()){ //use next line so we store phrases instead of word
+            String s = sc.nextLine(); //store phrase as string
+            if(s.equals("bye")){ //if terminating word
+                System.out.println("Bye, hope to see you again soon!"); //display terminating msf
+                break; // terminate loop
+            } else if(!s.equals("list")){ //as long as we dont ask for the list
+                xs.add(s); //add the phrases into the list
+                System.out.println("added: " + s); //and display confirmation message
+            } else { //if we ask for the list
+                xs.stream().forEach(x->System.out.println(x)); // print out the list
             }
         }
     }
