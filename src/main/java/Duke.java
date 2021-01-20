9d13cdd2e88b50d09d37aaabffa556268e310f16
tanboonji <tanboonji@hotmail.com>
2021-01-21 06:29:26 +0800

    Implement add and list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,33 +1,51 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.lang.StringBuilder;
 
 public class Duke {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>(100);
+        boolean runDuke = true;
+
+        greet();
+
+        while (runDuke) {
+            String input = scanner.nextLine();
+
+            switch (input) {
+                case "list": StringBuilder builder = new StringBuilder();
+                    for (int i = 0; i < tasks.size(); i++) {
+                        int numbering = i + 1;
+                        builder.append(numbering);
+                        builder.append(". ");
+                        builder.append(tasks.get(i));
+
+                        if (numbering != tasks.size()) {
+                            builder.append("\n");
+                        }
+                    }
+                    echo(builder.toString());
+                    break;
+                case "bye": echo("Bye. Hope to see you again soon!");
+                    runDuke = false;
+                    break;
+                default:
+                    tasks.add(input);
+                    echo("added: " + input);
+            }
+        }
+    }
+
+    public static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _ ___ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo);
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("==============================");
-        System.out.println("Hello! I'm Duke.");
-        System.out.println("What can I do for you?");
-        System.out.println("==============================\n");
-
-        String input = "";
-
-        while (!input.equals("bye")) {
-            input = scanner.nextLine();
-
-            if (input.equals("bye")) {
-                echo("Bye. Hope to see you again soon!");
-            } else {
-                echo(input);
-            }
-        }
+        echo("Hello! I'm Duke.\nWhat can I do for you?");
     }
 
     public static void echo(String input) {
