b78adb134dcc4d0bc9522e1a8874b865b3803e0c
Huang Zhenxin <hzx649675498@gmail.com>
2021-01-20 16:40:19 +0800

    Bug fix for not adding event and deadline

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -13,17 +13,15 @@ public class Task {
     }
 
     private static void add(Task t) {
-        if (t.taskName.length() <= 38) {
-            taskList[capacity - 1] = t;
-        }
+        taskList[capacity - 1] = t;
     }
 
 
     public static final void done(int i) {
         if (taskList[i - 1] != null) {
             taskList[i - 1].done = "X";
-            System.out.println(Format.UPPER + " ".repeat(10) + "Wah~ You done the task: "
-                    + " ".repeat(10) + taskList[i - 1].toString() + Format.LOWER);
+            System.out.println(Format.UPPER + "Wah~ You done the task: "
+                    + " " + taskList[i - 1].toString() + Format.LOWER);
         } else {
             System.out.println(Format.chatBox("Walao!No such task!"));
         }
