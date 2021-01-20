6f5b754a770352216276d4c0c046b65332a707c1
BigDoot <glendonchua@gmail.com>
2021-01-20 16:53:50 +0800

    Add ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,33 +1,39 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class  Duke {
     public static void main(String[] args) {
-        String[] list = new String[100];
-        String[] done = new String[100];
+        Task[] list = new Task[100];
         int numitems = 0;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I am\n" + logo);
         System.out.println("What can I do for you?");
         System.out.println("---------------------------------");
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+
         while (!str.equals("bye")) {
+            Pattern p = Pattern.compile("(done )([0-9]+)");
+            Matcher m = p.matcher(str);
             if (str.equals("list")) {
+                System.out.println("Here are the tasks in your list!");
                 for (int i = 0; i < numitems; i++) {
-                    System.out.println("Here are the tasks in your list!");
-                    System.out.println(i+1 + "." + done[i] + list[i]);
+                    System.out.println(i+1 + "." + list[i].getStatusIcon() + list[i].description);
                 }
 
-            } else if (str.equals("done")) {
-
+            } else if (m.find()) {
+                    System.out.println("Good job, I've marked the task as done!");
+                    int n = Integer.parseInt(m.group(2)) - 1;
+                    list[n].markAsDone();
+                    System.out.println(list[n].getStatusIcon() + list[n].description);
             } else {
                 System.out.println("added: " + str);
-                done[numitems] = "[ ] ";
-                list[numitems] = str;
+                list[numitems] = new Task(str);
                 numitems += 1;
             }
             str = sc.nextLine();
