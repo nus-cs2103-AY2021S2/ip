97f693ac6d0d261f184ea58d7b8a3b1d02ee6d44
chenzaza <wxgych@gmail.com>
2021-01-21 17:16:21 +0800

    Week2-Level2-Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,12 +14,28 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+        String[] list = new String[100];
+        int count = 0;
 
-        //echos commands entered by the user
-        while(!str.equals("bye")){
-            System.out.println(str+"\n");
-            str = sc.nextLine();
-        }
+        //commands entered by the user
+        while(!str.equals("bye"))
+            if(str.equals("list")){
+                //display them back to the user when requested
+                for(int i=1; i<=count; i++){
+                    System.out.println(i + ". " + list[i-1]);
+                }
+                if(count==0){
+                    System.out.println("There is no task in the list.");
+                }
+                System.out.println();
+                str = sc.nextLine();
+            } else {
+                //store whatever text entered by the user
+                list[count] = str;
+                count++;
+                System.out.println("added: " + str +"\n");
+                str = sc.nextLine();
+            }
 
         //exits when the user types bye
         System.out.println("Bye. Hope to see you again soon!\n");
