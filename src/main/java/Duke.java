56655dbfa246e75ad717b79ec3434c3e45404c15
jingxueguo <guo.jingxuess@gmail.com>
2021-01-21 19:04:18 +0800

    Add function to greet user and echo user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! What can I do for you:>");
+
+        String input = sc.next();
+
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = sc.next();
+        }
+
+        System.out.println("Bye. See you again!");
     }
 }
