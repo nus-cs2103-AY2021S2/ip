67c8d0d1f364525840f711fca71a6d112c838e38
car155 <carissaying0000@gmail.com>
2021-01-21 18:15:32 +0800

    Duke skeleton. Added greeting, echo and exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(formatMessage("Hello! I'm Duke\n     What can I do for you?"));
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        while (!input.equals("bye")) {
+            System.out.println(formatMessage(input));
+            input = sc.next();
+        }
+
+        System.out.println(formatMessage("Bye. Hope to see you again soon!"));
+    }
+
+    public static String formatMessage(String message) {
+        return ("    ____________________________________________________________\n" + "     " + message + "\n"
+                + "    ____________________________________________________________\n");
     }
 }
