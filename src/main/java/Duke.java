3c4e80a0d5d922e99fed62bc1b1f28fa3b2c1612
hvrca <wang.haochen.c@gmail.com>
2021-01-21 22:18:59 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -47,64 +47,100 @@ public class Duke {
         }
     }
 
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        String introduction = "I'm Duke!\nWhat can I do for ya?\n";
-        formatBox(introduction);
-
-        Scanner sc = new Scanner(System.in);
-        Task[] taskArr = new Task[100];
-        String input;
-        int j = 0;
-
-        while (true) {
-            input = sc.nextLine();
-            String inputArr[] = input.split(" ");
-            if (input.equals("bye"))
-                break;
-            else if (input.equals("list")) {
-                System.out.println("------------------------------------");
-                System.out.println("Here are the tasks in your list: ");
-                for (Task t : taskArr) {
-                    if (t == null) break;
-                    System.out.println(t.toString());
-                }
-                System.out.println("------------------------------------");
-            } else if (inputArr[0].equals("done")) {
-                int taskNum = Integer.parseInt(inputArr[1]) - 1;
-                taskArr[taskNum].markDone();
-                formatBox("Nice! I've marked this task as done:\n" + taskArr[taskNum].toString());
-            }
-            else {
-                // add to list
-                String[] inputArrTasks = input.split("/");
-                String[] firstHalf = inputArrTasks[0].split(" ", 2);
-
-                if (inputArrTasks.length != 1) {
-                    String[] secondHalf = inputArrTasks[1].split(" ", 2);
-                    taskArr[j] = new Task(firstHalf[1], false, Type.valueOf(firstHalf[0].toUpperCase()), secondHalf[1]);
-                } else
-                    taskArr[j] = new Task(firstHalf[1], false, Type.valueOf(firstHalf[0].toUpperCase()), "");
-
-                j++;
-                // print output
-                String formattedInput = "Got it. I've added this task:\n  ";
-                formattedInput = formattedInput.concat(taskArr[j - 1].toString()).concat("\n");
-                formattedInput = formattedInput.concat("Now you have " + Integer.toString(j) + " tasks in the list.");
-                formatBox(formattedInput);
-            }
+    /**
+     * Exception class for missing todo descriptor
+     */
+    static class MissingTodoDescriptorException extends Exception {
+        public MissingTodoDescriptorException(String errorMessage) {
+            super(errorMessage);
         }
-        String bye = "Bye. Hope to see you again soon!";
-        formatBox(bye);
+    }
+
+    /**
+     * Exception class for unknown input parameters
+     */
+    static class UnknownInputParamException extends Exception {
+        public UnknownInputParamException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            String logo = " ____        _        \n"
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+            System.out.println("Hello from\n" + logo);
+            String introduction = "I'm Duke!\nWhat can I do for ya?\n";
+            formatBox(introduction);
+
+            Scanner sc = new Scanner(System.in);
+            Task[] taskArr = new Task[100];
+            String input;
+            int j = 0;
+
+            while (true) {
+                input = sc.nextLine();
+                String inputArr[] = input.split(" ");
+                if (inputArr[0].equals("bye"))
+                    break;
+                else if (inputArr[0].equals("list")) {
+                    System.out.println("------------------------------------");
+                    System.out.println("Here are the tasks in your list: ");
+                    for (Task t : taskArr) {
+                        if (t == null) break;
+                        System.out.println(t.toString());
+                    }
+                    System.out.println("------------------------------------");
+                } else if (inputArr[0].equals("done")) {
+                    int taskNum = Integer.parseInt(inputArr[1]) - 1;
+                    taskArr[taskNum].markDone();
+                    formatBox("Nice! I've marked this task as done:\n" + taskArr[taskNum].toString());
+                } else if (inputArr[0].equals("todo") || inputArr[0].equals("event") || inputArr[0].equals("deadline")) {
+                    // add to list
+                    String[] inputArrTasks = input.split("/");
+                    String[] firstHalf = inputArrTasks[0].split(" ", 2);
+
+                    if (inputArrTasks.length != 1) {
+                        String[] secondHalf = inputArrTasks[1].split(" ", 2);
+                        taskArr[j] = new Task(firstHalf[1], false, Type.valueOf(firstHalf[0].toUpperCase()), secondHalf[1]);
+                    } else {
+                        if (firstHalf.length == 1) {
+                            throw new MissingTodoDescriptorException("------------------------------------\n" +
+                                    ":( OOPS!!! The description of a todo cannot be empty\n" +
+                                    "------------------------------------");
+                        } else {
+                            taskArr[j] = new Task(firstHalf[1], false, Type.valueOf(firstHalf[0].toUpperCase()), "");
+                        }
+                    }
+
+                    j++;
+                    // print output
+                    String formattedInput = "Got it. I've added this task:\n  ";
+                    formattedInput = formattedInput.concat(taskArr[j - 1].toString()).concat("\n");
+                    formattedInput = formattedInput.concat("Now you have " + Integer.toString(j) + " tasks in the list.");
+                    formatBox(formattedInput);
+                } else {
+                    throw new UnknownInputParamException("------------------------------------\n" +
+                            ":( OOPS!!! I'm sorry, but I don't know what that means :-(\n" +
+                            "------------------------------------");
+                }
+            }
+            String bye = "Bye. Hope to see you again soon!";
+            formatBox(bye);
+
+        } catch (MissingTodoDescriptorException e) {
+            System.out.println(e.getMessage());
+        } catch (UnknownInputParamException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     /**
      * Duke speaks in chat boxes
+     *
      * @param str input string within chat boxes
      */
     public static void formatBox(String str) {
