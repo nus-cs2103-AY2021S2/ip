a4317aaf6b2ee1c3c3f1ec4dc8747f23443af795
XindiTian <xinditian@gmail.com>
2021-01-21 19:43:33 +0800

    duke 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,20 +11,31 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
-        ArrayList<String> lst = new ArrayList<>();
+        ArrayList<Task> lst = new ArrayList<>();
         while (sc.hasNext()) {
-            String input = sc.nextLine();
+            String input = sc.next();
             if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < lst.size(); i++) {
                     int count = i + 1;
-                    System.out.println(count + ". " + lst.get(i));
+                    String stats = lst.get(i).getStatus();
+                    System.out.println(count + ". " + stats + " " + lst.get(i).des);
                 }
             } else if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again!");
                 break;
+            } else if (input.equals("done")) {
+                int tag = sc.nextInt() - 1;
+                Task d = lst.get(tag);
+                d.markAsDone();
+                String stat = d.getStatus();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(stat + " " + d.des);
             } else {
-                lst.add(input);
-                System.out.println("added: " + input);
+                String ninput = input + sc.nextLine();
+                Task t = new Task(ninput);
+                lst.add(t);
+                System.out.println("added: " + t.des);
             }
         }
     }
