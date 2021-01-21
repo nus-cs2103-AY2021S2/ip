ff34f1b857c13645b40fcee6611de98a024f33a7
KnitidCeladon23 <kenny.chewpuyang23@gmail.com>
2021-01-21 19:13:10 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,9 +7,10 @@ public class Duke {
         String dash = "____________________________________________________________";
         String greeting1 = "Pai Kia Bot: Eh harlo! Call me Pai Kia Bot.";
         String greeting2 = "Pai Kia Bot: What you want?";
-
+        String greeting3 = "Pai Kia Bot: I will keep a list of your responses ah";
         String farewell = "Pai Kia Bot: Leave so soon ah? Limpeh sleep first, if got no issue don't disturb me.";
 
+        ArrayList<String> inputList = new ArrayList<>();
 
         System.out.println(dash);
         System.out.println(greeting1);
@@ -17,18 +18,34 @@ public class Duke {
         System.out.println(dash);
         System.out.print("Your response: ");
         String input = sc.next();
-
-
+        if (!input.equals("list")) {
+            inputList.add(input);
+        }
         System.out.println("");
 
         while (!input.equals("bye")) {
-            System.out.println(dash);
-            System.out.println("You: " + input);
-            System.out.println("Pai Kia Bot: ok can");
-            System.out.println(dash);
+            if (input.equals("list")) {
+                int counter = 1;
+                System.out.println(dash);
+                System.out.println("Pai Kia Bot: i send u your input list ah");
+
+                for (String s : inputList) {
+                    System.out.println(counter + ": " + s);
+                    counter++;
+                }
+                System.out.println(dash);
+            } else {
+                System.out.println(dash);
+                System.out.println("You: " + input);
+                System.out.println("Pai Kia Bot: ok can, added ur response chop chop fast game");
+                System.out.println(dash);
+            }
+
             System.out.print("Your response: ");
             input = sc.next();
-
+            if (!input.equals("list")) {
+                inputList.add(input);
+            }
             System.out.println("");
 
         }
