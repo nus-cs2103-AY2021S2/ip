9bdf86bf971ce47731f04ab2d90b984dda012460
Peeranat (ToTo) Tokaeo <totoyoyorocks@gmail.com>
2021-01-21 21:26:03 +0800

    Add greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,30 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String greeting = "Hello! I'm Duke\n" + "What can I do for you?";
+        dukePrint(greeting);
+        loop();
     }
+
+    public static void dukePrint(String s) {
+        System.out.println("\n----------------------");
+        System.out.println(s);
+        System.out.println("----------------------\n");
+    }
+
+    public static void loop() {
+        Scanner input = new Scanner(System.in);
+        boolean end = false;
+        while(!end) {
+            String s = input.nextLine();
+            if (s.equals("bye")) {
+                end = true;
+                dukePrint("Bye bye! See you later!");
+            } else {
+                dukePrint(s);
+            }
+        }
+    }
+
 }
