f23c88f6e0addf8a850991896b5fac43a4e155ef
KnitidCeladon23 <kenny.chewpuyang23@gmail.com>
2021-01-21 19:56:50 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,16 +10,16 @@ public class Duke {
         String greeting3 = "Pai Kia Bot: I will keep a list of your responses ah";
         String farewell = "Pai Kia Bot: Leave so soon ah? Limpeh sleep first, if got no issue don't disturb me.";
 
-        ArrayList<String> inputList = new ArrayList<>();
+        ArrayList<CheckList> inputList = new ArrayList<>();
 
         System.out.println(dash);
         System.out.println(greeting1);
         System.out.println(greeting2);
         System.out.println(dash);
         System.out.print("Your response: ");
-        String input = sc.next();
+        String input = sc.nextLine();
         if (!input.equals("list")) {
-            inputList.add(input);
+            inputList.add(new CheckList(input, false));
         }
         System.out.println("");
 
@@ -29,12 +29,27 @@ public class Duke {
                 System.out.println(dash);
                 System.out.println("Pai Kia Bot: i send u your input list ah");
 
-                for (String s : inputList) {
-                    System.out.println(counter + ": " + s);
+                for (CheckList c : inputList) {
+                    System.out.println(counter + c.toString());
                     counter++;
                 }
                 System.out.println(dash);
-            } else {
+            } else if (input.length() > 3 && input.substring(0,4).equals("done")) {
+                int index;
+                if (input.equals("done")) {
+                    System.out.println("Pai Kia Bot: which entry u wan to check as done ah?");
+                    index = Integer.parseInt(sc.nextLine());
+                } else {
+                    index = Integer.parseInt(input.substring(5));
+                }
+                if (index >= 0 && index <= inputList.size()) {
+                    inputList.set(index - 1, inputList.get(index-1).makeDone());
+                    System.out.println(dash);
+                    System.out.println("ok i give this task a done chop: " + inputList.get(index-1).toString().substring(1));
+                    System.out.println(dash);
+                }
+
+           } else {
                 System.out.println(dash);
                 System.out.println("You: " + input);
                 System.out.println("Pai Kia Bot: ok can, added ur response chop chop fast game");
@@ -42,9 +57,9 @@ public class Duke {
             }
 
             System.out.print("Your response: ");
-            input = sc.next();
-            if (!input.equals("list")) {
-                inputList.add(input);
+            input = sc.nextLine();
+            if (!input.equals("list") && !(input.length() > 3 && input.substring(0,4).equals("done"))) {
+                inputList.add(new CheckList(input, false));
             }
             System.out.println("");
 
@@ -55,5 +70,29 @@ public class Duke {
         System.out.println(farewell);
         System.out.println(dash);
         sc.close();
+    }
+}
+
+class CheckList {
+    String content;
+    boolean checked;
+
+    CheckList(String content, boolean checked) {
+        this.content = content;
+        this.checked = checked;
+    }
+
+    CheckList makeDone() {
+        return new CheckList(this.content, true);
+    }
+
+    CheckList makeUndone() {
+        return new CheckList(this.content, false);
+    }
+
+    @Override
+    public String toString() {
+        String checkbox = this.checked ? "[x]" : "[ ]";
+        return "." + checkbox + " " + this.content;
     }
 }
