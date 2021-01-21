ddda922913fe2dc8079400ea31fff0a034d94f4c
ALZAHRANI-PC\riyad <riyadh-h@outlook.sa>
2021-01-21 16:00:55 +0800

    Update Vergil.java

--- src/Vergil.java
+++ src/Vergil.java
@@ -1,31 +1,40 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Vergil {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String command;
+        ArrayList<String> tasks = new ArrayList<>();
 
-        // Chatbot replies are prefixed with V |,
-        // while user commands are prefixed with >>>.
-        System.out.println("V | Welcome! I'm Vergil!");
-        System.out.println("V | How may I help you?");
+        System.out.println("Welcome! I'm Vergil!");
+        System.out.println("How may I help you?");
         System.out.println();
 
         do {
+            // User commands are prefixed with >>>.
             System.out.print(">>> ");
             command = scan.nextLine();
-            System.out.print("V | ");
 
             switch (command) {
+                // List the current tasks.
+                case "list":
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
+                    break;
+
+                // Exit the program.
                 case "bye":
                     System.out.println("Bye. See you soon!");
                     break;
 
+                // Add a task to the list of tasks.
                 default:
-                    System.out.println(command);
-                    System.out.println();
+                    tasks.add(command);
+                    System.out.println("Added '" + command + "'.");
             }
-
+            System.out.println();
         } while (!command.equals("bye"));
     }
 }
