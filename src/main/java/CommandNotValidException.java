545dddccf9dff8547dccd6be778de57d7ecff12c
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-21 22:29:11 +0800

    Edit message of CommandNotValidExeception.java

--- src/main/java/CommandNotValidException.java
+++ src/main/java/CommandNotValidException.java
@@ -2,6 +2,7 @@ public class CommandNotValidException extends Exception {
     public CommandNotValidException() {
         super("Command not valid. Please use \"todo\", \"deadline\" "
                 + "or \"event\" followed by task description to add new tasks. "
+                + "Please use \"list\" to view your list of tasks. "
                 + "Please use \"done\" followed by index to mark completed tasks. "
                 + "Please use \"delete\" followed by index to delete tasks. "
                 + "Please use \"bye\" to exit.");
