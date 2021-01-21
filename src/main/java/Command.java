4379c6413946c7e3ac5bd1c4c5fe00b416067aa8
DESKTOP-3V9VV9N\chlol <chloleila@yahoo.com.sg>
2021-01-21 23:18:32 +0800

    Handle error of no user input for done, or no user input at all

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -9,6 +9,9 @@ public class Command {
 
     public List<Task> handleCommand(String command) throws DukeException {
         String[] inputs = command.split(" ");
+        if (inputs.length == 0) {
+            throw new DukeException("OOPS! Please enter a command or say bye so I can go back to sleep!");
+        }
         String action = inputs[0];
         if (action.equals("list")) {
             System.out.println(Aligner.align("Here are the tasks in your list:"));
@@ -16,42 +19,43 @@ public class Command {
                 System.out.println(Aligner.align(i + "." + list.get(i - 1).toString()));
             }
         } else if (action.equals("done")) {
+            if (inputs.length == 1) {
+                throw new DukeException("OOPS! Please tell me what to mark as done!");
+            }
             int taskNo = Integer.parseInt(String.valueOf(command.charAt(5))) - 1;
             list.get(taskNo).markAsDone();
             System.out.println(Aligner.align("Good job! I've marked this task as done:"));
             System.out.println(Aligner.align(list.get(taskNo).toString()));
-        } else if (action.equals("todo") || action.equals("deadline") ||
-                action.equals("event")) {
-            if (action.equals("todo")) {
-                if (inputs.length == 1) {
-                    throw new DukeException("OOPS! The description of a todo cannot be empty.");
-                }
-                System.out.println(Aligner.align("Sure! I've added this task:"));
-                ToDo newToDo = new ToDo(command.substring(5));
-                list.add(newToDo);
-                System.out.println(Aligner.align(newToDo.toString()));
-            } else {
-                int indexOfDate = command.indexOf("/");
-                if (action.equals("deadline")) {
-                    if (inputs.length == 1) {
-                        throw new DukeException("OOPS! The description of a deadline cannot be empty.");
-                    }
-                    System.out.println(Aligner.align("Sure! I've added this task:"));
-                    String date = command.substring(indexOfDate + 4);
-                    Deadline newDeadline = new Deadline(command.substring(9, indexOfDate), date);
-                    list.add(newDeadline);
-                    System.out.println(Aligner.align(newDeadline.toString()));
-                } else {
-                    if (inputs.length == 1) {
-                        throw new DukeException("OOPS! The description of an event cannot be empty.");
-                    }
-                    System.out.println(Aligner.align("Sure! I've added this task:"));
-                    String date = command.substring(indexOfDate + 4);
-                    Event newEvent = new Event(command.substring(6, indexOfDate), date);
-                    list.add(newEvent);
-                    System.out.println(Aligner.align(newEvent.toString()));
-                }
+        } else if (action.equals("todo")) {
+            if (inputs.length == 1) {
+                throw new DukeException("OOPS! The description of a todo cannot be empty.");
             }
+            System.out.println(Aligner.align("Sure! I've added this task:"));
+            ToDo newToDo = new ToDo(command.substring(5));
+            list.add(newToDo);
+            System.out.println(Aligner.align(newToDo.toString()));
+            System.out.println(Aligner.align("Now you have a whopping " + list.size() + " task(s) in the list."));
+        } else if (action.equals("deadline")) {
+            if (inputs.length == 1) {
+                throw new DukeException("OOPS! The description of a deadline cannot be empty.");
+            }
+            int indexOfDate = command.indexOf("/");
+            System.out.println(Aligner.align("Sure! I've added this task:"));
+            String date = command.substring(indexOfDate + 4);
+            Deadline newDeadline = new Deadline(command.substring(9, indexOfDate), date);
+            list.add(newDeadline);
+            System.out.println(Aligner.align(newDeadline.toString()));
+            System.out.println(Aligner.align("Now you have a whopping " + list.size() + " task(s) in the list."));
+        } else if (action.equals("event")) {
+            if (inputs.length == 1) {
+                throw new DukeException("OOPS! The description of an event cannot be empty.");
+            }
+            int indexOfDate = command.indexOf("/");
+            System.out.println(Aligner.align("Sure! I've added this task:"));
+            String date = command.substring(indexOfDate + 4);
+            Event newEvent = new Event(command.substring(6, indexOfDate), date);
+            list.add(newEvent);
+            System.out.println(Aligner.align(newEvent.toString()));
             System.out.println(Aligner.align("Now you have a whopping " + list.size() + " task(s) in the list."));
         } else if (action.equals("delete")) {
             int taskNo = Integer.parseInt(String.valueOf(command.charAt(7))) - 1;
@@ -62,6 +66,6 @@ public class Command {
         } else {
             throw new DukeException("OOPS! Sorry, I have no idea what that means :(");
         }
-    return list;
+        return list;
     }
 }
