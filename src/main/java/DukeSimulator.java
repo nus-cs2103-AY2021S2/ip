2de34d3117a7b7dde2b9b6af8da96ed1134d5002
Jonathan <banchiang98@gmail.com>
2021-01-21 16:50:00 +0800

    making sure testrun ui is okay (removed duke logo)

--- src/main/java/DukeSimulator.java
+++ src/main/java/DukeSimulator.java
@@ -18,12 +18,12 @@ public class DukeSimulator {
     }
 
     public void greeting() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
         String greeting = "     Hello! I'm Duke\n" + "     What can I do for you?\n";
         System.out.print(line + greeting + line);
     }
