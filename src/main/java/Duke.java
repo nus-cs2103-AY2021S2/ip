2c58bb0e5779311f78585d3c3208d7d78e615b44
frisciliasultan <frisciliasultan@gmail.com>
2021-01-21 12:39:38 +0800

    Add grettings, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,29 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+
+        System.out.println("    ____________________________________");
+        System.out.println("    Hello! I'm Duke \n    What can I do for you?");
+        System.out.println("    ____________________________________");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String command = sc.nextLine();
+          
+            System.out.println("    ____________________________________");
+
+            if (command.equals("bye")) {
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________");
+                break;
+            } else {
+                System.out.println("    " + command);
+                System.out.println("    ____________________________________");
+            }
+        }
+        
+        sc.close();
     }
 }
