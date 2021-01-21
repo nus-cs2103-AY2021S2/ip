34be3d8b586538d1d024e45b024423d5c11864aa
Yanneko <joel.wong.xue.rong@gmail.com>
2021-01-21 17:29:11 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,40 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________\n");
+
+        boolean Done = false;
+        while (!Done) {
+            switch (sc.next()) {
+                case "list":
+                    System.out.println("    ____________________________________________________________\n " +
+                            "    list\n" +
+                            "    ____________________________________________________________");
+                    break;
+
+                case "blah":
+                    System.out.println("    ____________________________________________________________\n " +
+                            "    blah\n" +
+                            "    ____________________________________________________________");
+                    break;
+
+                case "bye":
+                    System.out.println("    ____________________________________________________________\n " +
+                            "    Bye. Hope to see you again soon!\n" +
+                            "    ____________________________________________________________");
+                    Done = true;
+                    break;
+
+                default:
+                    System.out.println("What have you done...");
+                    break;
+            }
+        }
     }
 }
