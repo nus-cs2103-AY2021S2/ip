a6f598af545446e6f187654d9609ac24c706a9c7
soorya <32706641+soo-rya@users.noreply.github.com>
2021-01-21 13:05:20 +0800

    feat: added exception handling for possible scenarios

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,33 +17,67 @@ public class Duke {
                 + "          \\___/\\___/---;_| |_|\n";
 
         String border = " --*---*---*---*---*---*---*---*---*---*--";
+        String help = "  Please use the following to update me:\n\n" +
+                "    todo <task description>\n      \\_ adds a task to be done\n" +
+                "    deadline <task description> /by <DD-MM-YYYY>\n      \\_ adds a deadline\n" +
+                "    event <task description> /at <DD-MM-YYYY HHMM> to <DD-MM-YYYY HHMM>" +
+                "\n      \\_ adds an event\n" +
+                "    list\n      \\_ lists all tasks, deadlines and events\n" +
+                "    done <task number>\n      \\_ marks a task as done\n" +
+                "    bye\n      \\_ exists this application\n";
+
         System.out.println(logo + "\n" + border + "\n  Hey there, Olaf here!" +
                 "\n  What will we be doing today?");
 
         while(true) {
             String command = bf.readLine();
-            if(command.toLowerCase().equals("bye")){
+            if(command.equalsIgnoreCase("bye")){
                 String out = "Aww hope to see you soon, goodbye!";
                 System.out.println(border + "\n  " + out + "\n" + border);
                 break;
-            } else if(command.toLowerCase().equals("list")){
-                System.out.println(border + "\n  Here are your pending tasks:\n");
-                // adapted from: https://howtodoinjava.com/java/collections/arraylist/iterate-through-objects/
-                int idx = 0;
-                while (tasks.size() > idx){
-                    System.out.printf("  %s. %s\n",String.valueOf(idx+1),tasks.get(idx++));
+            } else if(command.equalsIgnoreCase("list")){
+                if (tasks.size()>0) {
+                    System.out.println(border + "\n  Here are all the tasks in your list:\n");
+                    int idx = 0;
+                    int undone = 0;
+                    // adapted from: https://howtodoinjava.com/java/collections/arraylist/iterate-through-objects/
+                    while (tasks.size() > idx) {
+                        Task toPrint = tasks.get(idx++);
+                        System.out.printf("  %s. %s\n", idx, toPrint);
+                        undone += toPrint.isNotDone();
+                    }
+                    System.out.printf("\n  Only %s tasks left to be done!\n%s\n", undone, border);
+                } else {
+                    System.out.println(border + "\n  You don't have any tasks yet :)" +
+                            "\n  Type 'help' to see how you can add a task\n" + border);
                 }
-                System.out.println(border);
             } else if(command.toLowerCase().startsWith("done")){
-                int idx = Integer.parseInt(command.split(" ")[1]);
-                tasks.get(idx-1).markAsDone();
-                int undone = tasks.stream()
-                        .mapToInt(Task::isNotDone)
-                        .reduce(0, Integer::sum);
-                System.out.println(border + "\n  Great job! You're done with:\n");
-                System.out.printf("  %s. %s\n",String.valueOf(idx), tasks.get(idx-1));
-                System.out.printf("\n  Now %o tasks are left to be done!\n", undone);
-                System.out.println(border);
+                try {
+                    int idx = Integer.parseInt(command.split(" ")[1]);
+                    tasks.get(idx - 1).markAsDone();
+                    int undone = tasks.stream()
+                            .mapToInt(Task::isNotDone)
+                            .reduce(0, Integer::sum);
+                    System.out.println(border + "\n  Great job! You're done with:\n");
+                    System.out.printf("  %s. %s\n", idx, tasks.get(idx - 1));
+                    System.out.printf("\n  Now %s tasks are left to be done!\n", undone);
+                    System.out.println(border);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println(border + "\n  Oops! Please state the task number to mark as done:\n");
+                    System.out.println("  done <task number>\n");
+                    System.out.println("  Type 'list' to view all tasks\n" +
+                            "  and their respective numbers\n" + border);
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println(border + "\n  Oops! No such task found...\n");
+                    System.out.printf("  There are only %s tasks in your list.\n", tasks.size());
+                    System.out.println(border);
+                } catch (NumberFormatException e) {
+                    System.out.println(border + "\n  Oops! No such task found...");
+                    System.out.println("  Please state the task number to mark as done:\n");
+                    System.out.println("  done <task number>\n");
+                    System.out.println("  Type 'list' to view all tasks\n" +
+                            "  and their respective numbers\n" + border);
+                }
             } else if(command.toLowerCase().startsWith("todo")){
                 try {
                     String expression = command.split(" ", 2)[1];
@@ -53,8 +87,8 @@ public class Duke {
                             .mapToInt(Task::isNotDone)
                             .reduce(0, Integer::sum);
                     System.out.println(border + "\n  Okie added new task:\n");
-                    System.out.printf("  %o. %s\n", total, tasks.get(total-1));
-                    System.out.printf("\n  Total %o tasks, only %o left to be done!\n", total, undone);
+                    System.out.printf("  %s. %s\n", total, tasks.get(total-1));
+                    System.out.printf("\n  Total %s tasks, only %s left to be done!\n", total, undone);
                     System.out.println(border);
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println(border + "\n  Oops! Please add a task description as follows:\n");
@@ -70,15 +104,15 @@ public class Duke {
                             .mapToInt(Task::isNotDone)
                             .reduce(0, Integer::sum);
                     System.out.println(border + "\n  Okie added new task:\n");
-                    System.out.printf("  %o. %s\n", total, tasks.get(total-1));
-                    System.out.printf("\n  Total %o tasks, only %o left to be done!\n", total, undone);
+                    System.out.printf("  %s. %s\n", total, tasks.get(total-1));
+                    System.out.printf("\n  Total %s tasks, only %s left to be done!\n", total, undone);
                     System.out.println(border);
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println(border + "\n  Oops! Please add a deadline using" +
                             "\n  the 24-hour time format as follows:\n");
                     System.out.println("  deadline <task description> /by <DD-MM-YYYY HHMM>\n" + border);
                 }
-            } else if(command.toLowerCase().startsWith("event")){
+            } else if(command.toLowerCase().startsWith("event")) {
                 try {
                     String expression = command.split(" ", 2)[1];
                     String[] parts = expression.split("/at", 2);
@@ -88,24 +122,19 @@ public class Duke {
                             .mapToInt(Task::isNotDone)
                             .reduce(0, Integer::sum);
                     System.out.println(border + "\n  Okie added new task:\n");
-                    System.out.printf("  %o. %s\n", total, tasks.get(total-1));
-                    System.out.printf("\n  Total %o tasks, only %o left to be done!\n", total, undone);
+                    System.out.printf("  %s. %s\n", total, tasks.get(total - 1));
+                    System.out.printf("\n  Total %s tasks, only %s left to be done!\n", total, undone);
                     System.out.println(border);
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println(border + "\n  Oops! Please add an event using" +
                             "\n  the 24-hour time format as follows:\n");
                     System.out.println("  event <task description> /at <DD-MM-YYYY HHMM> to <DD-MM-YYYY HHMM>\n" + border);
                 }
+            } else if (command.equalsIgnoreCase("help")) {
+                System.out.println(border + "\n" + help + border);
             } else {
-                System.out.println(border + "\n  Hmm I don't understand :(" +
-                        "\n  Please use the following to update me!\n");
-                System.out.println("    todo <task description>\n      \\_ adds a task to be done");
-                System.out.println("    deadline <task description> /by <DD-MM-YYYY>\n      \\_ adds a deadline");
-                System.out.println("    event <task description> /at <DD-MM-YYYY HHMM> to <DD-MM-YYYY HHMM>" +
-                        "\n      \\_ adds an event");
-                System.out.println("    list\n      \\_ lists all tasks, deadlines and events");
-                System.out.println("    done <task number>\n      \\_ marks a task as done");
-                System.out.println("    bye\n      \\_ exists this application\n" + border);
+                System.out.println(border + "\n  Hmm sorry I don't understand :(\n");
+                System.out.println(help + border);
             }
         }
     }
