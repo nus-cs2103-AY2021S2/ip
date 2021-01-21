36d0002118f76146a5758b2886ba0e0aff9999c6
pyuxiang <pyxelise@gmail.com>
2021-01-21 16:39:57 +0800

    Add Tasklist feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) throws IOException {
@@ -10,22 +11,45 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        Duke.respond(new String[]{ "Hello! I'm a customized Duke", "What can I do for you?" });
 
+        // Print introduction
+        ArrayList<String> intro = new ArrayList<>();
+        intro.add("Hello! I'm a customized Duke");
+        intro.add("What can I do for you?");
+        Duke.respond(intro);
+
+        ArrayList<String> outtro = new ArrayList<>();
+        outtro.add("Bye. Hope to see you again soon!");
+
+        // REPL
+        ArrayList<String> userData = new ArrayList<>();
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(System.in));
         while (true) {
             String s = in.readLine();
             if (s.equals("bye")) {
-                Duke.respond(new String[]{"Bye. Hope to see you again soon!"});
+                Duke.respond(outtro);
                 break;
+            } else if (s.equals("list")) {
+                Duke.respondList(userData);
             } else {
-                Duke.respond(new String[]{s});
+                ArrayList<String> line = new ArrayList<>();
+                line.add("added: " + s);
+                Duke.respond(line);
+                userData.add(s);
             }
         }
     }
 
-    private static void respond(String[] lines) {
+    private static void respondList(ArrayList<String> lines) {
+        ArrayList<String> copy = new ArrayList<>(lines);
+        for (int i = 0; i < copy.size(); i++) {
+            copy.set(i, (i+1) + ". " + copy.get(i));
+        }
+        Duke.respond(copy);
+    }
+
+    private static void respond(ArrayList<String> lines) {
         String border = "    ____________________________________________________________";
         String indent = "     ";
 
