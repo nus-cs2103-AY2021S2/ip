036227f19d69682cfa4a01a81e63256559a9ccbd
justgnohUG <justingnoh123@gmail.com>
2021-01-21 18:13:01 +0800

    Level-6: Delete
    - Added Delete command
    - Updated runtest.sh input.txt

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -8,14 +9,15 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("---------------------------------------\n" + logo);
-
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?" );
-
+        System.out.println("---------------------------------------\n");
+        System.out.println("What's up! I'm Duke\nPlease feed be commands :)" );
         System.out.println("---------------------------------------" );
 
-//        Initialize Task container of space 100
-        Task[] tasks = new Task[100];
+        Scanner scan = new Scanner(System.in);
+        String input = " ";
+
+//        Initialize Task container
+        ArrayList<Task> tasks = new ArrayList<>();
         int taskNumber = 0;
 
 
@@ -34,35 +36,34 @@ public class Duke {
 //                    Numbers should change accordingly when deleted (For future Ref.)
                     System.out.println("\n---------------------------------------" );
                     System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < taskNumber; i++) {
-                    System.out.println(String.valueOf(i+1) + "." + tasks[i].toString());
-                }
-                System.out.println("---------------------------------------" );
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(String.valueOf(i+1) + "." + tasks.get(i).toString());
+                    }
+                    System.out.println("---------------------------------------" );
                     break;
 
                 case "done":
 //                    Possible Error: index provided is out of bounds (NullPointerException)
                     int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                tasks[index].markDone();
+                    tasks.get(index).markDone();
 
-                System.out.println("\n---------------------------------------" );
-                System.out.println("Nice! I've marked this task as done:\n");
-                System.out.println("[" + tasks[index].getStatusIcon() + "] " + tasks[index].name);
-                System.out.println("---------------------------------------" );
+                    System.out.println("\n---------------------------------------" );
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[" + tasks.get(index).getStatusIcon() + "] " + tasks.get(index).name.trim());
+                    System.out.println("---------------------------------------" );
                     break;
 
                 case "todo":
                     try {
                         String name = getTodoName(input);
                         Todo todo = new Todo(name);
-                tasks[taskNumber] = todo;
-                taskNumber++;
+                        tasks.add(todo);
 
-                System.out.println("\n---------------------------------------" );
-                System.out.println("Got it. I've added this task: ");
-                System.out.println(todo.toString());
-                System.out.println("Now you have " + taskNumber + " tasks in the list");
-                System.out.println("---------------------------------------" );
+                        System.out.println("\n---------------------------------------" );
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(todo.toString());
+                        System.out.println("Now you have " + tasks.size() + " tasks in the list");
+                        System.out.println("---------------------------------------" );
                     } catch(DukeException e) {
                         e.printError("Come On Fella! Your ToDo description cannot be empty!");
                     }
@@ -73,14 +74,13 @@ public class Duke {
                         String name = getEventOrDeadlineName(input);
                         String by = getEventOrDeadlineAttribute(input);
                         Deadline deadline = new Deadline(name, by);
-                tasks[taskNumber] = deadline;
-                taskNumber++;
+                        tasks.add(deadline);
 
-                System.out.println("\n---------------------------------------" );
-                System.out.println("Got it. I've added this task: ");
-                System.out.println(deadline.toString());
-                System.out.println("Now you have " + taskNumber + " tasks in the list");
-                System.out.println("---------------------------------------" );
+                        System.out.println("\n---------------------------------------" );
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(deadline.toString());
+                        System.out.println("Now you have " + tasks.size() + " tasks in the list");
+                        System.out.println("---------------------------------------" );
                     } catch (DukeException e) {
                         e.printError("Hmm... You are either lacking a name or /by details!");
                     }
@@ -92,24 +92,28 @@ public class Duke {
                         String at = getEventOrDeadlineAttribute(input);
 
                         Event event = new Event(name,at);
-                tasks[taskNumber] = event;
-                taskNumber++;
+                        tasks.add(event);
 
-                System.out.println("\n---------------------------------------" );
-                System.out.println("Got it. I've added this task: ");
-                System.out.println(event.toString());
-                System.out.println("Now you have " + taskNumber + " tasks in the list");
-                System.out.println("---------------------------------------" );
+                        System.out.println("\n---------------------------------------" );
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(event.toString());
+                        System.out.println("Now you have " + tasks.size() + " tasks in the list");
+                        System.out.println("---------------------------------------" );
                     } catch (DukeException e) {
                         e.printError("Hmm... You are either lacking a name or /at details!");
                     }
                     break;
 
+                case "delete":
+                    int i = Integer.parseInt(input.split(" ")[1]) - 1;
 
-            } else {
-//            Add to list
-                tasks[taskNumber] = new Task(input);
-                taskNumber++;
+                    System.out.println("\n---------------------------------------" );
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println(tasks.get(i).toString());
+                    tasks.remove(i);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list");
+                    System.out.println("---------------------------------------" );
+                    break;
 
                 default:
 //                    Does not exactly throw an exception*
