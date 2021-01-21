80eae36bf5be2a7ab931e51867f1cec0e117a44a
BenedictBCJJ <bennyyxz@gmail.com>
2021-01-21 13:22:08 +0800

    Added deletion function, Level-6 done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -6,7 +7,7 @@ public class Duke {
         String goodbye = "    Bye bye, catch you soon.";
         Scanner sc = new Scanner(System.in);
         String input = "";
-        Task[] listOfTasks = new Task[100];
+        ArrayList<Task> listOfTasks = new ArrayList<>();
         int numberOfTasks = 0;
 
         System.out.println(greeting);
@@ -17,14 +18,21 @@ public class Duke {
             if (input.equals("list")) {
                 System.out.println("    Here are the tasks in your list:");
                 for (int i = 1; i <= numberOfTasks; i++) {
-                    System.out.println("    " + i + ". " + listOfTasks[i - 1].getStatus());
+                    System.out.println("    " + i + ". " + listOfTasks.get(i - 1).getStatus());
                 }
                 System.out.println();
             } else if (command.equals("done")) {
                 int taskNumber = Integer.parseInt(input.split(" ")[1]);
-                listOfTasks[taskNumber - 1].setDone(true);
+                listOfTasks.get(taskNumber - 1).setDone(true);
                 System.out.println("    Nice! I've marked this task as done: \n" + "      "
-                        + listOfTasks[taskNumber - 1].getStatus());
+                        + listOfTasks.get(taskNumber - 1).getStatus());
+            } else if (command.equals("delete")) {
+                int deleteIndex = Integer.parseInt(input.split(" ")[1]);
+                System.out.println("    Noted. I've removed this task:");
+                System.out.println("      " + listOfTasks.get(deleteIndex - 1).getStatus());
+                numberOfTasks--;
+                System.out.println("    Now you have " + numberOfTasks + " tasks in the list.\n");
+                listOfTasks.remove(deleteIndex - 1);
             } else {
                 try {
                     if (!command.equals("todo") && !command.equals("event") && !command.equals("deadline")) {
@@ -38,7 +46,7 @@ public class Duke {
                     }
 
                     if (command.equals("todo")) {
-                        listOfTasks[numberOfTasks] = new ToDo(taskInformation[1]);
+                        listOfTasks.add(new ToDo(taskInformation[1]));
                     } else {
                         String[] descriptionAndTime = taskInformation[1].split("/");
                         if (descriptionAndTime.length < 2) {
@@ -49,15 +57,15 @@ public class Duke {
                         String time = descriptionAndTime[1].split(" ", 2)[1];
                         if (command.equals("deadline")) {
 
-                            listOfTasks[numberOfTasks] = new Deadlines(description, time);
+                            listOfTasks.add(new Deadlines(description, time));
                         } else if (command.equals("event")) {
 
-                            listOfTasks[numberOfTasks] = new Event(description, time);
+                            listOfTasks.add(new Event(description, time));
                         }
                     }
 
                     System.out.println("    Got it. I've added this task: ");
-                    System.out.println("      " + listOfTasks[numberOfTasks].getStatus());
+                    System.out.println("      " + listOfTasks.get(numberOfTasks).getStatus());
                     numberOfTasks++;
                     System.out.println("    Now you have " + numberOfTasks + " tasks in the list.\n");
                 } catch (TaskException e) {
