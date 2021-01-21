8588ce3cc602684f87ba0879313b6aaf4b0d0634
jingxueguo <guo.jingxuess@gmail.com>
2021-01-21 18:52:36 +0800

    Revert "Add function to greet user and echo user input"
    This reverts commit 44763c2df39b93548d2bbe9c6da33e5b5fa3e18b.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,10 @@
-import java.util.Scanner;
-
 public class Duke {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
-        System.out.println("Hello! I'm Duke \n What can I do for you?");
-        String input = sc.next();
-        while (input != "bye") {
-            System.out.println(input);
-            input = sc.next();
-        }
-
-        System.out.println("Bye. Hope to see you again!");
-
     }
 }
