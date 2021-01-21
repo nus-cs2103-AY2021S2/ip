a8d4578306d161c6e5929dac5e3238e1ea199375
car155 <carissaying0000@gmail.com>
2021-01-21 18:44:55 +0800

    Added list, add function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,42 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println(formatMessage("Hello! I'm Duke\n     What can I do for you?"));
+        ArrayList<String> ls = new ArrayList<>();
+
+        System.out.println(formatMessage("Hello! I'm Duke\nWhat can I do for you?"));
 
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+        String input = sc.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.println(formatMessage(input));
-            input = sc.next();
+            if (input.equals("list")) {
+                printList(ls);
+            } else {
+                System.out.println(formatMessage("added: " + input));
+                ls.add(input);
+            }
+            input = sc.nextLine();
         }
 
         System.out.println(formatMessage("Bye. Hope to see you again soon!"));
+        sc.close();
     }
 
+    // prints list item number and string
+    public static void printList(ArrayList<String> ls) {
+        System.out.println("    ____________________________________________________________");
+        for (int i = 0; i < ls.size(); i++) {
+            System.out.println("     " + (i + 1) + ". " + ls.get(i));
+        }
+        System.out.println("    ____________________________________________________________\n");
+    }
+
+    // format for greeting, echo and exit
     public static String formatMessage(String message) {
-        return ("    ____________________________________________________________\n" + "     " + message + "\n"
+        String newMessage = message.replaceAll("\n", "\n     ");
+        return ("    ____________________________________________________________\n" + "     " + newMessage + "\n"
                 + "    ____________________________________________________________\n");
     }
 }
