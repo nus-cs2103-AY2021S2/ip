65470b2e23459f7de1cf90c674e2aff9d1e4655a
aw zhen yi <garythebob@gmail.com>
2021-01-21 18:07:06 +0800

    Test Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void greet() {
+        System.out.println("Hello! I'm Duke" + "\n" + "What can I do for you?");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +16,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        if(input.equals("bye")) {
+            exit();
+        } else {
+            System.out.println(input);
+        }
+
     }
 }
