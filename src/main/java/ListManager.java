078704b2581a71dc9a8830ba4ad255811cad01c5
frisciliasultan <frisciliasultan@gmail.com>
2021-01-21 23:53:41 +0800

    Add Javadocs comments in ListManager methods

--- src/main/java/ListManager.java
+++ src/main/java/ListManager.java
@@ -10,15 +10,29 @@ public class ListManager {
         this.listSize = 0;
     }
 
-    void addTask(Task t) {
-        list.add(t);
+    /**
+     * Adds the task argument into the task list.
+     * Prints out a response to indicate successful adding of task.
+     * Prints out the current number of tasks in the list.
+     * @param task
+     * @return void
+     */
+    void addTask(Task task) {
+        list.add(task);
         listSize++;
 
         System.out.println("    Got it. I've added this task:");
-        System.out.println("        " + t);
+        System.out.println("        " + task);
         System.out.printf("    Now you have %d tasks in the list.\n", listSize);
     }
 
+     /**
+     * Removes the task of the given index from the task list.
+     * Prints out a response to indicate successful removing of task.
+     * Prints out the current number of tasks in the list.
+     * @param index
+     * @return void
+     */
     void removeTask(int index) {
         Task temp = list.get(index - 1);
         list.remove(index - 1);
@@ -29,6 +43,11 @@ public class ListManager {
         System.out.printf("    Now you have %d tasks in the list.\n", listSize);
     }
 
+    /**
+     * Marks the task of the given index as done.
+     * Prints out a response to indicate successful marking of task.
+     * @param index
+     */
     void markTaskAsDone(int index) {
         Task temp = list.get(index - 1);
         temp.markAsDone();
@@ -36,6 +55,9 @@ public class ListManager {
         System.out.println("        " + temp);
     }
 
+    /**
+     * Displays list by iterating through the list members.
+     */
     void displayList() {
         System.out.println("    Here are the tasks in your list:");
         for (int i = 1; i <= listSize; i++) {
