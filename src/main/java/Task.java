a11a8a2e433bca70e0e38737d8a0853d178cdb59
umergta <umergta@gmail.com>
2021-01-21 11:12:26 +0800

    Level-6: added deletion of tasks functionality

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -23,7 +23,8 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        //return tick or X symbols
+        return (isDone ? "\u2713" : "\u2718");
     }
 
     @Override
