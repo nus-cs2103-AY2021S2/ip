975a05ddf4da7a5a73cd72163ca021a5824827c3
justgnohUG <justingnoh123@gmail.com>
2021-01-21 22:11:52 +0800

    Level 1: Greet, Echo, Exit
    Changed Level-1 author

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,29 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("---------------------------------------\n" + logo);
+
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?" );
+
+        System.out.println("---------------------------------------" );
+
+//        Level 1: Echo
+        while(true) {
+            Scanner scan = new Scanner(System.in);
+            String input;
+            input = scan.nextLine();
+
+            if (input.equals("bye")) {
+//            Terminate
+                System.out.println("\n---------------------------------------" );
+                System.out.println("Bye. Sayonara and goodbye!");
+                System.out.println("---------------------------------------" );
+                break;
+            }
+//            Echo:
+            System.out.println("\n---------------------------------------" );
+            System.out.println(input);
+            System.out.println("---------------------------------------" );
+        }
     }
 }
