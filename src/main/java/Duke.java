defb60494d1df49696c25826bd199af110936edb
yuchen <yuchenwang97@gmail.com>
2021-01-21 20:58:21 +0800

    Added Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String h_rule = "────────────────────────────────────────────────────────────────────";
+        System.out.println("Greetings! I am Duke! How may I assist you?\n" + h_rule);
+
+        Scanner sc = new Scanner(System.in);
+
+        String input;
+
+        while (true) {
+            input = sc.nextLine().trim();
+            System.out.println(h_rule);
+            if (input.equals("bye")) {
+                System.out.println("Goodbye! Have a nice day!\n");
+                break;
+            } else {
+                System.out.println(input);
+            }
+            System.out.println(h_rule);
+        }
+        sc.close();
     }
 }
