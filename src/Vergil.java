ffe5b86544f618eb389e1ffcbac90591e4dbb15d
ALZAHRANI-PC\riyad <riyadh-h@outlook.sa>
2021-01-21 21:24:03 +0800

    Update Vergil.java

--- src/Vergil.java
+++ src/Vergil.java
@@ -14,16 +14,20 @@ public class Vergil {
         do {
             // User commands are prefixed with >>>.
             System.out.print(">>> ");
-            command = scan.nextLine();
+            command = scan.nextLine().trim();
 
-            if (command.matches("bye")) {
-                System.out.println("Bye. See you soon!");
-            } else if (command.matches("list")) {
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.printf("%d. %s\n", i + 1, tasks.get(i));
-                }
-            } else if (command.matches("done \\d")) {
-                try {
+            try {
+                if (command.equals("bye")) {
+                    System.out.println("Bye. See you soon!");
+                } else if (command.equals("list")) {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.printf("%d. %s\n", i + 1, tasks.get(i));
+                    }
+                } else if (command.startsWith("done")) {
+                    if (command.equals("done")) {
+                        throw new VergilException(
+                                "Sorry! 'done' requires typing the list number of the task.");
+                    }
                     Task task = tasks.get(Integer.parseInt(command.split(" ")[1]) - 1);
                     if (task.isDone()) {
                         System.out.printf("You have already completed this task: %s\n",
@@ -33,33 +37,50 @@ public class Vergil {
                         System.out.println("Sweet! Task completed:");
                         System.out.printf("   %s\n", task);
                     }
-                } catch (IndexOutOfBoundsException e) {
-                    System.out.println("Sorry! There is no task with that list number.");
+                } else if (command.startsWith("todo")) {
+                    if (command.equals("todo")) {
+                        throw new VergilException(
+                                "Sorry! 'todo' requires typing a description of the task.");
+                    }
+                    String desc = command.split(" ", 2)[1];
+                    tasks.add(new Todo(desc));
+                    System.out.printf("Added '%s' as a ToDo task.\n", desc);
+                } else if (command.startsWith("deadline")) {
+                    if (command.equals("deadline")) {
+                        throw new VergilException(
+                                "Sorry! 'deadline' requires typing a description of," +
+                                " the keyword '/by', and a due time for the task.");
+                    }
+                    String[] bySplit = command.split(" /by ");
+                    String desc = bySplit[0].split(" ", 2)[1];
+                    String time = bySplit[1];
+
+                    tasks.add(new Deadline(desc, time));
+                    System.out.printf("Added '%s' as a deadline task by '%s'.\n",
+                            desc,
+                            time);
+                } else if (command.startsWith("event")) {
+                    if (command.equals("event")) {
+                        throw new VergilException(
+                                "Sorry! 'event' requires typing a description of," +
+                                        " the keyword '/at', and a scheduled time for the task.");
+                    }
+
+                    String[] atSplit = command.split(" /at ");
+                    String desc = atSplit[0].split(" ", 2)[1];
+                    String time = atSplit[1];
+
+                    tasks.add(new Event(desc, time));
+                    System.out.printf("Added '%s' as an event task by '%s'.\n",
+                            desc,
+                            time);
+                } else {
+                    throw new VergilException("Sorry! I cannot resolve this command.");
                 }
-            } else if (command.matches("todo .+")) {
-                String desc = command.split(" ", 2)[1];
-                tasks.add(new Todo(desc));
-                System.out.printf("Added '%s' as a ToDo task.\n", desc);
-            } else if (command.matches("deadline .+ /by .+")) {
-                String[] bySplit = command.split(" /by ");
-                String desc = bySplit[0].split(" ", 2)[1];
-                String time = bySplit[1];
-
-                tasks.add(new Deadline(desc, time));
-                System.out.printf("Added '%s' as a deadline task by '%s'.\n",
-                        desc,
-                        time);
-            } else if (command.matches("event .+ /at .+")) {
-                String[] atSplit = command.split(" /at ");
-                String desc = atSplit[0].split(" ", 2)[1];
-                String time = atSplit[1];
-
-                tasks.add(new Event(desc, time));
-                System.out.printf("Added '%s' as an event task by '%s'.\n",
-                        desc,
-                        time);
-            } else {
-                System.out.println("Sorry! This command is unrecognizable.");
+            } catch (VergilException e) {
+                System.out.println(e.getMessage());
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Sorry! The command's format is invalid.");
             }
             System.out.println();
         } while (!command.equals("bye"));
