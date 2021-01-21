8e450ce638afe3e1d0336a9b2b56a21dfdcb4000
Shaun Loh <sloh97@gmail.com>
2021-01-21 16:45:39 +0800

    Duke.java: change code to Greet, Echo and Exit add FastIO.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        FastIO reader = new FastIO();
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        String input = reader.nextLine();
+
+        while(true) {
+            if (input.equals("bye")) {
+                reader.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+                input = reader.nextLine();
+            }
+        }
+
+        reader.close();
     }
 }
+
---
+++ src/main/java/FastIO.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Fast I/O is a class to quicken I/O operations
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
