56d2f18b4df89e5dd32d45d6e1aba31b808e97c0
Jacob-109 <65383310+Jacob-109@users.noreply.github.com>
2021-01-21 21:09:53 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
+    public enum Tasktype {
+        DEADLINE,
+        EVENT,
+        TODO
+    }
     public static void main(String[] args) {
         System.out.println("Hello from Gambit, how may I assist you today?");
         Scanner sc = new Scanner(System.in);
@@ -24,9 +29,36 @@ public class Duke {
                 int value =Integer.parseInt(strArray[1]);
                 list.get(value - 1).markAsDone();
             } else {
-                Task t = new Task(word);
-                list.add(t);
-                System.out.println("added: " + word);
+                Tasktype newTask = null;
+                if (word.contains("deadline")) {
+                    newTask = Tasktype.DEADLINE;
+                } else if (word.contains("todo")) {
+                    newTask = Tasktype.TODO;
+                } else if (word.contains("event")) {
+                    newTask = Tasktype.EVENT;
+                }
+                System.out.println("Got it. I've added this task:");
+                switch (newTask)
+                {
+                    case(DEADLINE) :
+                        String[] input = word.split("/by");
+                        input[0] = input[0].replaceAll("deadline","");
+                        Deadline d = new Deadline(input[0],input[1]);
+                        System.out.println(d.toString());
+                        break;
+                    case(EVENT) :
+                        String[] info = word.split("/at");
+                        input[0] = input[0].replaceAll("event","");
+                        Event e = new Event(input[0],input[1]);
+                        System.out.println(e.toString());
+                        break;
+                    case(TODO) :
+                        word.replaceAll("todo","");
+                        ToDo t = new Todo(word);
+                        System.out.println(t.toString());
+                        break;
+                }
+                System.out.println("Now you have " + list.size() + "tasks in the list");
             }
         }
     }
