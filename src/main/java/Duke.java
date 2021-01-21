114540988f2104d319938f195b83d0488a4a5a92
ninijiaying <cooolnichole@gmail.com>
2021-01-21 11:24:31 +0800

    add Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,19 +3,38 @@ import java.io.*;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
         Scanner in = new Scanner(System.in);
         System.out.println("    ____________________________________________________________");
         System.out.println("     Hello! I'm Duke");
         System.out.println("     What can I do for you?");
         System.out.println("    ____________________________________________________________");
         System.out.print("Enter a command: ");
-        String commands = in.next();
+        String commands = in.nextLine();
+        int taskCount = 0;
         while(!commands.equals("bye")) {
-            System.out.println("    ____________________________________________________________");
-            System.out.println("    " + commands);
-            System.out.println("    ____________________________________________________________");
+            if(commands.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                int count = 1;
+                for(String s : tasks) {
+                    if(s != null) {
+                        System.out.println(count + ". " + s);
+                        count++;
+                    } else {
+                        break;
+                    }
+
+                }
+                System.out.println("    ____________________________________________________________");
+            } else {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     added: " +commands);
+                tasks[taskCount] = commands;
+                taskCount++;
+                System.out.println("    ____________________________________________________________");
+            }
             System.out.print("Enter a command: ");
-            commands = in.next();
+            commands = in.nextLine();
         }
         System.out.println("    ____________________________________________________________");
         System.out.println("    Bye. Hope to see you again soon!");
