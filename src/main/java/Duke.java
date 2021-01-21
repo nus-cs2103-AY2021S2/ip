bf935b0f0f12e0907491b24b61f8b76e0e182257
WYH <wyharry628@gmail.com>
2021-01-21 19:11:50 +0800

    level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,7 +7,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
         String line = "    ____________________________________________________________";
         String terminate_input = "bye";
-        String list = "list";
+        String show_list = "list";
+        String done = "done";
         Scanner sc = new Scanner(System.in);
         System.out.println(line);
         System.out.println("    Hello! I'm Duke");
@@ -24,13 +25,22 @@ public class Duke {
             if (input.equals(terminate_input)) {
                 System.out.println("    Bye. Hope to see you again soon!");
                 break;
-            } else if (input.equals(list)) {
+            } else if (input.equals(show_list)) {
+                System.out.println("    Here are the tasks in your list: ");
                 for (int i = 1; i < position + 1; i++) {
-                    System.out.println(i + ". " + todo[i - 1]);
+                    System.out.println("    " + i + ". " + todo[i - 1]);
                 }
+            } else if (input.length() > 4 && input.substring(0, 4).equals(done)) {
+                int job_done = Integer.valueOf(input.substring(5));
+                String job = todo[job_done - 1];
+                String done_job = "[" + "X" + "]" + job.substring(3);
+                todo[job_done - 1] = done_job;
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println("      " + done_job);
             } else {
                 System.out.println("    added: " + input);
-                todo[position] = input;
+                String job = "[ ] " + input;
+                todo[position] = job;
                 position += 1;
             }
             System.out.println(line);
