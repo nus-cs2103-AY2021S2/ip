c23a8cb36e3c5d260490949b518f49b1494604db
yuchen <yuchenwang97@gmail.com>
2021-01-21 22:59:34 +0800

    Added Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,21 +19,24 @@ public class Duke {
         String raw_in;
         String[] input;
         List<Task> itemList = new ArrayList<>();
+        int count = 0;
 
         while (true) {
             raw_in = sc.nextLine().trim();
             input = raw_in.split(" ");
             System.out.println(h_rule);
+            String joined = "";
+            String timing = "";
             if (raw_in.equals("bye")) {
                 System.out.println("Goodbye! Have a nice day!\n");
                 break;
             } else {
                 switch (input[0]) {
                     case "list":
-                        int i = 1;
+                        int index = 1;
                         for (Task s : itemList) {
-                            System.out.println(i + ". " + s.toString());
-                            i++;
+                            System.out.println(index + ". " + s.toString());
+                            index++;
                         }
                         break;
                     case "done":
@@ -41,9 +44,42 @@ public class Duke {
                         task.markDone();
                         System.out.println("Alright, I will mark this as done. \n" + input[1] + ". " + task.toString());
                         break;
-                    default:
-                        itemList.add(new Task(raw_in));
-                        System.out.println("Added " + raw_in);
+                    case "todo":
+                        for (int i = 1; i < input.length; i++) {
+                            joined = joined + " " + input[i];
+                        }
+                        itemList.add(new Todo(joined));
+                        count++;
+                        System.out.println("Added " + raw_in + "\nYou now have " + count + " items in your list. ");
+                        break;
+                    case "deadline":
+                        int seq = 0;
+                        while (!input[seq].equals("/by")) {
+                            joined = joined + " " + input[seq];
+                            seq++;
+                        }
+
+                        for (int i = seq + 1; i < input.length; i++) {
+                            timing = timing + " " + input[i];
+                        }
+
+                        itemList.add(new Deadline(joined, timing.trim()));
+                        count++;
+                        System.out.println("Added " + joined + "\nYou now have " + count + " items in your list. ");
+                        break;
+                    case "event":
+                        int seq2 = 0;
+                        while (!input[seq2].equals("/at")) {
+                            joined = joined + " " + input[seq2];
+                            seq2++;
+                        }
+                        for (int i = seq2 + 1; i < input.length; i++) {
+                            timing = timing + " " + input[i];
+                        }
+                        itemList.add(new Event(joined, timing.trim()));
+                        count++;
+                        System.out.println("Added " + joined + "\nYou now have " + count + " items in your list. ");
+                        break;
                 }
             }
             System.out.println(h_rule);
