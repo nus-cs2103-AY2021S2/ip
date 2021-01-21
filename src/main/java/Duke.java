ea64e59ae300405094116b7ea06e8f00f608ca29
Cp-John <e0376984@u.nus.edu>
2021-01-21 17:32:50 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] tasks = new String[100];
+    private static int counter = 0;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         greeting();
         while(true) {
-            String command = sc.next();
+            String command = sc.nextLine();
             if (!processCommand(command)) {
                 break;
             }
@@ -13,10 +16,22 @@ public class Duke {
         sc.close();
     }
 
+    /**
+     * print message with proper indentations
+     * @param msg
+     */
+    public static void printMsg(String msg) {
+        System.out.println("     " + msg);
+    }
+
+    public static void addTask(String task) {
+        tasks[counter] = task;
+        counter++;
+    }
+
     public static void printLine() {
         System.out.println("    ____________________________________________________________");
     }
-
 
     /**
      * print greeting message
@@ -29,8 +44,8 @@ public class Duke {
                 + "     | |_| | |_| |   <  __/\n"
                 + "     |____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo);
-        System.out.println("     Hello! I'm Duke");
-        System.out.println("     What can I do for you?");
+        printMsg("Hello! I'm Duke");
+        printMsg("What can I do for you?");
         printLine();
     }
 
@@ -46,17 +61,31 @@ public class Duke {
             printLine();
             return false;
         } else {
-            echo(command);
+            switch (command) {
+                case "list":
+                    printTasks();
+                    break;
+                default:
+                    addTask(command);
+                    echo(command);
+                    break;
+            }
             printLine();
             return true;
         }
     }
 
     public static void sayBye() {
-        System.out.println("     Bye. Hope to see you again soon!");
+        printMsg("Bye. Hope to see you again soon!");
     }
 
     public static void echo(String msg) {
-        System.out.println("     " + msg);
+        printMsg("added: " + msg);
+    }
+
+    public static void printTasks() {
+        for (int i = 0; i < counter; i++) {
+            printMsg(i + ". " + tasks[i]);
+        }
     }
 }
