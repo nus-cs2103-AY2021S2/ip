a6db28b67f828e01c8e42b3ed916aaed693f5135
jaredtengsw <jaredtengsw@u.nus.edu>
2021-01-21 21:52:23 +0800

    Updated to level 2: add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         String name = "Duke";
         String input;
-        String response;
+        ArrayList<Task> taskList = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -22,15 +23,27 @@ public class Duke {
             input = sc.nextLine();
 
             while (!input.equals("bye")) {
+                String[] inputWords = input.split(" ");
+                String action = inputWords[0];
+
                 if (input.equals("list")) {
-                    System.out.println("list");
+                    System.out.println("Here is a list of your tasks:");
+                    for (int i = 0; i < taskList.size(); i++) {
+                        System.out.println(i+1 + "." + taskList.get(i).toString());
+                    }
                     input = sc.nextLine();
                 } else if (input.equals("blah")) {
                     System.out.println("blah");
+                    input = sc.nextLine();
+                } else {
+                    Task newTask = new Task(input);
+                    taskList.add(newTask);
+                    System.out.println("added: " + newTask.getInfo());
                     input = sc.nextLine();
                 }
             }
             System.out.println("Goodbye! Have a nice day!");
         }
+        System.exit(1);
     }
 }
