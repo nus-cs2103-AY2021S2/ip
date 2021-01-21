4c32c01c128a3c01ee7f425cd7c4242711ff21d3
chenzaza <wxgych@gmail.com>
2021-01-21 17:11:57 +0800

    Week2-Level1-Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,6 +8,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        //test push
+        //test push, it doesnt work!
+        //starts by greeting the user
+        System.out.println("Hello! I'm Duke.\nWhat can I do for you?\n");
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        //echos commands entered by the user
+        while(!str.equals("bye")){
+            System.out.println(str+"\n");
+            str = sc.nextLine();
+        }
+
+        //exits when the user types bye
+        System.out.println("Bye. Hope to see you again soon!\n");
     }
+
 }
