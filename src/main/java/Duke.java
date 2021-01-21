271e2f4cb5c2409b0f0c9f5fe6aeca18112bd49e
Jacob-109 <65383310+Jacob-109@users.noreply.github.com>
2021-01-21 13:59:37 +0800

    Level 1.Greet,Echo,Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from Gambit, how may I assist you today?");
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNext()) {
+            String word = sc.next();
+            if(!word.equals("bye")) {
+                System.out.println(word);
+            } else {
+                System.out.println("Bye, hope to see you again!");
+                break;
+            }
+        }
     }
 }
