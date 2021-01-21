4ea22a36400851d8b0b86757bff47d7c6eae58b8
Marcus Lim <marc@Marcuss-MacBook-Pro-3.local>
2021-01-21 19:35:23 +0800

    [LEVEL-1] Added greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.lang.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,27 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        start();
+    }
+    
+    public static void start() {
+        Scanner sc = new Scanner(System.in);
+        String request = "";
+        dprint("Hello! I'm Duke\nWhat can I do for you?");
+        while (!request.equals("bye")) {
+            request = sc.nextLine();
+            if (!request.equals("bye")) {
+                dprint(request);
+            } else {
+                dprint("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
+    }
+
+    public static void dprint(String msg) {
+        String appendMsg = "____________________________________________________________\n"
+            + msg + "\n____________________________________________________________";
+        System.out.println(appendMsg);
     }
 }
