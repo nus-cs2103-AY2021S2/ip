d47901eef40e9c15454021c11dafbc06a4970f1c
WYH <wyharry628@gmail.com>
2021-01-21 19:37:26 +0800

    level 3 with class representation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,6 +9,7 @@ public class Duke {
         String terminate_input = "bye";
         String show_list = "list";
         String done = "done";
+        String indentation = "    ";
         Scanner sc = new Scanner(System.in);
         System.out.println(line);
         System.out.println("    Hello! I'm Duke");
@@ -16,30 +17,30 @@ public class Duke {
         System.out.println(line);
         System.out.println();
 
-        String[] todo = new String[100];
+        Task[] todo = new Task[100];
         int position = 0;
 
         while (true) {
             String input = sc.nextLine();
             System.out.println(line);
             if (input.equals(terminate_input)) {
-                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println(indentation + "Bye. Hope to see you again soon!");
                 break;
             } else if (input.equals(show_list)) {
                 System.out.println("    Here are the tasks in your list: ");
                 for (int i = 1; i < position + 1; i++) {
-                    System.out.println("    " + i + ". " + todo[i - 1]);
+                    System.out.println(indentation + i + ". " + todo[i - 1]);
                 }
             } else if (input.length() > 4 && input.substring(0, 4).equals(done)) {
                 int job_done = Integer.valueOf(input.substring(5));
-                String job = todo[job_done - 1];
-                String done_job = "[" + "X" + "]" + job.substring(3);
-                todo[job_done - 1] = done_job;
-                System.out.println("    Nice! I've marked this task as done:");
-                System.out.println("      " + done_job);
+                Task job = todo[job_done - 1];
+                job.markAsDone();
+                todo[job_done - 1] = job;
+                System.out.println(indentation + "Nice! I've marked this task as done:");
+                System.out.println(indentation + job);
             } else {
-                System.out.println("    added: " + input);
-                String job = "[ ] " + input;
+                Task job = new Task(input);
+                System.out.println(indentation + "added: " + input);
                 todo[position] = job;
                 position += 1;
             }
