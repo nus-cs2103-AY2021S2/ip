1e7422d61a7f68acf888523d978be22072ae7244
Onn Wei Sin <onnwards@gmail.com>
2021-01-21 17:36:14 +0800

    implement Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -7,20 +9,41 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String line = "    _______________________________________";
         Scanner sc = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
 
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm a bot called Duke. Beep boop. \nWhat do you want?\n");
         
-        String command = sc.next();
+
+        String command = sc.nextLine();
         while (!command.equals("bye")) {
-            sc.nextLine();
-            System.out.println(line + "\n    " + command + "\n" + line + "\n");
-            command = sc.next();
+            switch (command.split(" ")[0]) {
+                case "list":
+                    print(listToString(list));
+                    break;
+                default:
+                    list.add(command);
+                    print("Added: " + command);
+            }
+            command = sc.nextLine();
         }
-        System.out.println(line + "\n    BYE AND HAVE A GOOD DAY. Beep boop.\n" + line);
+        print("BYE AND HAVE A GOOD DAY. Beep boop.");
         sc.close();
         return;
+    }
+
+    public static void print(String text) {
+        String line = "    _______________________________________\n    ";
+        System.out.println(line + text + "\n" + line);
+    }
+
+    public static String listToString(List<String> list) {
+        String str = "";
+        for (int i = 0; i < list.size(); i++) {
+            str += String.valueOf(i+1) + ": " + list.get(i) + "\n    ";
+        }
+
+        return str.substring(0, str.length() - 5);
     }
 }
