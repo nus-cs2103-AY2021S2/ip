53acd06cbfb8390fd3385192f421ec4f3298da0d
Yanneko <joel.wong.xue.rong@gmail.com>
2021-01-21 23:10:34 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,8 +13,8 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("    ____________________________________________________________\n" +
-                "     Hello! I'm Duke\n" +
-                "     What can I do for you?\n" +
+                "     Duke... booted...\n" +
+                "     requesting tasks\n" +
                 "    ____________________________________________________________\n");
 
         boolean Done = false;
@@ -32,12 +32,19 @@ public class Duke {
                     break;
 
                 case "todo":
-                    ToDo nextToDo = new ToDo(sc.nextLine());
+                    String scanTest = sc.nextLine();
+                    if (scanTest.equals("") || scanTest.equals(" ")) {
+                        System.out.println("    ____________________________________________________________\n" +
+                                "     Error: ToDo entry format incorrect.\n" +
+                                "    ____________________________________________________________");
+                        break;
+                    }
+                    ToDo nextToDo = new ToDo(scanTest);
                     taskList.add(nextToDo);
                     System.out.println("    ____________________________________________________________\n" +
-                            "     Got it. I've added this task:\n");
+                            "     task.add:");
                     System.out.println("       " + nextToDo + "\n" +
-                            "     Now you have " + taskList.size() + " tasks in the list.");
+                            "     task.count = [" + taskList.size() + "].");
                     System.out.println("    ____________________________________________________________");
                     break;
 
@@ -48,13 +55,13 @@ public class Duke {
                         Deadline nextDeadLine = new Deadline(deadLineArgs[0], deadLineArgs[1]);
                         taskList.add(nextDeadLine);
                         System.out.println("    ____________________________________________________________\n" +
-                                "     Got it. I've added this task:\n");
+                                "     task.add:");
                         System.out.println("       " + nextDeadLine + "\n" +
-                                "     Now you have " + taskList.size() + " tasks in the list.");
+                                "     task.count = [" + taskList.size() + "].");
                         System.out.println("    ____________________________________________________________");
                     } catch (IndexOutOfBoundsException e) {
                         System.out.println("    ____________________________________________________________");
-                        System.out.println("     Error: Deadline entry format is wrong.");
+                        System.out.println("     Error: Deadline entry format incorrect.");
                         System.out.println("    ____________________________________________________________");
                     }
                     break;
@@ -66,20 +73,20 @@ public class Duke {
                         Event nextEvent = new Event(eventArgs[0], eventArgs[1]);
                         taskList.add(nextEvent);
                         System.out.println("    ____________________________________________________________\n" +
-                                "     Got it. I've added this task:\n");
+                                "     task.add:");
                         System.out.println("       " + nextEvent + "\n" +
-                                "     Now you have " + taskList.size() + " tasks in the list.");
+                                "     task.count = [" + taskList.size() + "].");
                         System.out.println("    ____________________________________________________________");
                     } catch (IndexOutOfBoundsException e) {
                         System.out.println("    ____________________________________________________________");
-                        System.out.println("     Error: Event entry format is wrong.");
+                        System.out.println("     Error: Event entry format incorrect.");
                         System.out.println("    ____________________________________________________________");
                     }
                     break;
 
                 case "done":
                     System.out.println("    ____________________________________________________________\n" +
-                            "     Nice! I've marked this task as done:");
+                            "     task.done = true:");
                     Task targetTask = taskList.get(sc.nextInt() - 1);
                     targetTask.MarkAsDone();
                     System.out.println("       " + targetTask);
@@ -88,22 +95,14 @@ public class Duke {
 
                 case "bye":
                     System.out.println("    ____________________________________________________________\n " +
-                            "     Bye. Hope to see you again soon!\n" +
+                            "     goodBye.\n" +
                             "    ____________________________________________________________");
                     Done = true;
                     break;
 
-                case "blah":
-                    System.out.println("    ____________________________________________________________\n " +
-                            "     blah\n" +
-                            "    ____________________________________________________________");
-                    break;
-
                 default:
-                    String nextTask = nextCommand + sc.nextLine();
                     System.out.println("    ____________________________________________________________");
-                    taskList.add(new Task(nextTask));
-                    System.out.println("     added: " + nextTask);
+                    System.out.println("     invalidCommand.");
                     System.out.println("    ____________________________________________________________");
                     break;
             }
