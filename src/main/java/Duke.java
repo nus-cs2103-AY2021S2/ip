6ce008b1321a4500a55c25c4797810d014f83116
Onn Wei Sin <onnwards@gmail.com>
2021-01-21 17:03:55 +0800

    Implement Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,20 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String line = "    _______________________________________";
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm a bot called Duke. Beep boop. \nWhat do you want?\n");
+        
+        String command = sc.next();
+        while (!command.equals("bye")) {
+            sc.nextLine();
+            System.out.println(line + "\n    " + command + "\n" + line + "\n");
+            command = sc.next();
+        }
+        System.out.println(line + "\n    BYE AND HAVE A GOOD DAY. Beep boop.\n" + line);
+        sc.close();
+        return;
     }
 }
