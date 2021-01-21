6050b37393a5a9ac733f24904f85ab7616bb8aaa
hvrca <wang.haochen.c@gmail.com>
2021-01-21 19:46:18 +0800

    first iteration

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,29 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String introduction = "I'm Duke!\nWhat can I do for ya?\n";
+        formatBox(introduction);
+
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = sc.nextLine();
+            if (input.equals("bye"))
+                break;
+            formatBox(input);
+        }
+        String bye = "Bye. Hope to see you again soon!";
+        formatBox(bye);
+    }
+
+    /**
+     * Duke speaks in chat boxes
+     * @param str input string within chat boxes
+     */
+    public static void formatBox(String str) {
+        System.out.println("------------------------------------\n");
+        System.out.println(str);
+        System.out.println("------------------------------------\n");
     }
 }
