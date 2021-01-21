fb4e096be91c48f58ec8f4755c64a4a5014994ee
Joel Ho <joelhoengkiat@gmail.com>
2021-01-21 12:09:38 +0800

    Update List message
    Made the list command more human friendly
    Default message when there are no tasks

--- src/main/java/DukeStrings.java
+++ src/main/java/DukeStrings.java
@@ -3,7 +3,9 @@ public class DukeStrings {
     public static final String MESSAGE_WELCOME = "Hello! I'm Duke\nWhat can I do for you?";
     public static final String MESSAGE_BYE = "Goodbye!";
     public static final String MESSAGE_DONE = "Nice! I've marked this as done: ";
-    public static final String MESSAGE_ADDED = "Got it. I've added this task:\n%s\nNow you have %d tasks.";
+    public static final String MESSAGE_ADDED = "Got it. I've added this task:\n\t%s\nNow you have %d tasks.";
+    public static final String MESSAGE_LIST = "Here are the tasks in your list:\n";
+    public static final String MESSAGE_LIST_EMPTY = "You have no tasks.";
 
     public static final String COMMAND_BYE = "bye";
     public static final String COMMAND_LIST = "list";
--- src/main/java/ListCommand.java
+++ src/main/java/ListCommand.java
@@ -2,6 +2,9 @@ public class ListCommand implements Command {
 
     @Override
     public String execute(TaskList taskList) {
-        return taskList.toString();
+        if (taskList.size() != 0) {
+            return DukeStrings.MESSAGE_LIST + taskList.toString();
+        }
+        return DukeStrings.MESSAGE_LIST_EMPTY;
     }
 }
