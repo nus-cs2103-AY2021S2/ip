98eb5c0aed452374dde8c220b39ba7b00facc470
nicholasnge <59093518+nicholasnge@users.noreply.github.com>
2021-01-21 18:51:34 +0800

    Level-3

--- src/main/java/Oracle.java
+++ src/main/java/Oracle.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Oracle {
+    private static ArrayList<Task> db = new ArrayList<>();
     public static void main(String[] args) {
         String logo =
                 "              $$$@@@@@@@\n"
@@ -26,26 +27,40 @@ public class Oracle {
                 + "        .-~~::::;:::::~:~--.\n";
         System.out.println(logo + "\nGreetings Neo, what can the Oracle do for you?");
 
-        ArrayList<Task> theOracleRemembers = new ArrayList<>();
-
         Scanner S = new Scanner(System.in);
         while(true) {
             String input = S.nextLine();  // Read user input
+            String[] split = input.split(" ", 2);
             if (input.equals("bye")){
                 System.out.println("Very well, we shall meet again");
                 break;
             }
-
             else if (input.equals("list")){
                 System.out.println("You have forgotten quickly, but the Oracle Remembers");
-                for (int i=0; i<theOracleRemembers.size(); i++){
-                    System.out.println((i+1) + ". " + theOracleRemembers.get(i).description);
+                for (int i = 0; i< db.size(); i++){
+                   printTask(i);
                 }
             }
+            else if (split[0].equals("done") && split.length > 1){
+                int i = Integer.parseInt(split[1]) - 1;
+                try {
+                    db.get(i).markDone();
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Invalid index of task, please try again");
+                    continue;
+                }
+                printTask(i);
+            }
             else {
-                theOracleRemembers.add(new Task(input));
+                db.add(new Task(input));
                 System.out.println("I will keep " + "\"" + input + "\" in mind");
             }
         }
+    }
+
+    private static void printTask(int i){
+        String index = (i+1) + ". ";
+        String doneStatus = "[" + db.get(i).getStatusIcon() + "] ";
+        System.out.println(index + doneStatus + db.get(i).description);
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -3,7 +3,16 @@
 public class Task {
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+    public String getStatusIcon() {
+        return (isDone ? "\u2718" : " "); //return tick or X symbols
+    }
+
+    public void markDone() {
+        this.isDone = !this.isDone;
     }
 
     String description;
+    boolean isDone;
 }
