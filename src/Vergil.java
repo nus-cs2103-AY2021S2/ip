2cdaeb51578759099afde495f36bbad2b5fd2899
ALZAHRANI-PC\riyad <riyadh-h@outlook.sa>
2021-01-21 16:50:20 +0800

    Update Vergil.java

--- src/Vergil.java
+++ src/Vergil.java
@@ -5,7 +5,7 @@ public class Vergil {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String command;
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         System.out.println("Welcome! I'm Vergil!");
         System.out.println("How may I help you?");
@@ -16,23 +16,29 @@ public class Vergil {
             System.out.print(">>> ");
             command = scan.nextLine();
 
-            switch (command) {
-                // List the current tasks.
-                case "list":
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println((i + 1) + ". " + tasks.get(i));
+            // List the current tasks.
+            if (command.matches("bye")) {
+                System.out.println("Bye. See you soon!");
+            } else if (command.matches("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.printf("%d. %s\n", i + 1, tasks.get(i));
+                }
+            } else if (command.matches("done \\d")) {
+                try {
+                    Task task = tasks.get(Integer.parseInt(command.split(" ")[1]) - 1);
+                    if (task.isDone()) {
+                        System.out.printf("You have already completed this task: %s\n",
+                                task);
+                    } else {
+                        task.doTask();
+                        System.out.printf("Sweet! Task completed: %s\n", task);
                     }
-                    break;
-
-                // Exit the program.
-                case "bye":
-                    System.out.println("Bye. See you soon!");
-                    break;
-
-                // Add a task to the list of tasks.
-                default:
-                    tasks.add(command);
-                    System.out.println("Added '" + command + "'.");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Sorry! There is no task with that list number.");
+                }
+            } else {
+                tasks.add(new Task(command));
+                System.out.printf("Added '%s'.\n", command);
             }
             System.out.println();
         } while (!command.equals("bye"));
