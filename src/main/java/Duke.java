7e441a3d7cf86b38444ea6ba84eda42856b7c124
ninijiaying <cooolnichole@gmail.com>
2021-01-21 22:51:28 +0800

    modify Duke.java using Java Collections class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Duke {
     public static void main(String[] args) {
-        Task[] tasks = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         System.out.println("    ____________________________________________________________");
         System.out.println("     Hello! I'm Duke");
@@ -16,9 +16,11 @@ public class Duke {
             if(commands.equals("list")) {
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     Here are the tasks in your list:");
+                int i = 1;
                 for(Task s : tasks) {
                     if(s != null) {
-                        System.out.println("     " + s.number + "." + s.toString());
+                        System.out.println("     " + i + "." + s.toString());
+                        i++;
                     } else {
                         break;
                     }
@@ -26,16 +28,16 @@ public class Duke {
                 System.out.println("    ____________________________________________________________");
             } else if(commands.split(" ")[0].equals("done")) {
                 int n = Integer.parseInt(commands.split(" ")[1]);
-                tasks[n-1].markAsDone();
+                tasks.get(n-1).markAsDone();
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     Nice! I've marked this task as done:");
-                System.out.println("       " + tasks[n-1].toString());
+                System.out.println("       " + tasks.get(n-1).toString());
                 System.out.println("    ____________________________________________________________");
             } else if(commands.split(" ")[0].equals("todo")) {
                 int iend = commands.indexOf(" ");
-                tasks[taskCount] = new ToDo(taskCount+1, commands.substring(iend+1));
+                tasks.add(new ToDo(taskCount+1, commands.substring(iend+1)));
                 try{
-                    tasks[taskCount].addTask(taskCount+1);
+                    tasks.get(taskCount).addTask(taskCount+1);
                     taskCount++;
                 } catch(DukeException e){
                     System.out.println("    ____________________________________________________________");
@@ -48,8 +50,8 @@ public class Duke {
                     int iend = commands.indexOf("/");
                     String subString1= commands.substring(iend1+1 , iend);
                     String subString2= commands.substring(iend+4);
-                    tasks[taskCount] = new Deadline(taskCount+1, subString1, subString2);
-                    tasks[taskCount].addTask(taskCount+1);
+                    tasks.add(new Deadline(taskCount+1, subString1, subString2));
+                    tasks.get(taskCount).addTask(taskCount+1);
                     taskCount++;
                 }
                 catch(DukeException e){
@@ -68,8 +70,8 @@ public class Duke {
                     int iend = commands.indexOf("/");
                     String subString1= commands.substring(iend1+1 , iend);
                     String subString2= commands.substring(iend+4);
-                    tasks[taskCount] = new Event(taskCount+1, subString1, subString2);
-                    tasks[taskCount].addTask(taskCount+1);
+                    tasks.add(new Event(taskCount+1, subString1, subString2));
+                    tasks.get(taskCount).addTask(taskCount+1);
                     taskCount++;
                 } catch(DukeException e){
                     System.out.println("    ____________________________________________________________");
@@ -81,7 +83,15 @@ public class Duke {
                     System.out.println("     ☹ OOPS!!! The start and end date of an event cannot be empty.(Format: /at + duration)");
                     System.out.println("    ____________________________________________________________");
                 }
-            } else {
+            } else if(commands.split(" ")[0].equals("delete")) {
+                int n = Integer.parseInt(commands.split(" ")[1]);
+                tasks.remove(n-1);
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Noted. I've removed this task:");
+                System.out.println("       " + tasks.get(n-1).toString());
+                System.out.println("     Now you have " + (taskCount-1) + " tasks in the list.");
+                System.out.println("    ____________________________________________________________");
+            }else {
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 System.out.println("    ____________________________________________________________");
