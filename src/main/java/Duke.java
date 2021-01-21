8ac883685b7d2ced03c859d8dc87f4c397670a63
Ho Jie Feng <hjf3108@gmail.com>
2021-01-21 22:34:02 +0800

    Implement Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+    static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello from\n" + Duke.logo);
+        System.out.println("What can I do for you?");
+
+        while(scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.compareTo("bye") == 0) {
+                System.out.println("\tDuke:");
+                System.out.println("\tBye!");
+                return;
+            }
+            System.out.println("\tDuke:");
+            System.out.println("\t" + line);
+        }
     }
 }
