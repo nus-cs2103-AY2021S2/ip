fc1eced47e43a8d0c78ebcd9d6e2663d077f0b2e
BenedictBCJJ <bennyyxz@gmail.com>
2021-01-21 13:12:40 +0800

    Add exception handling.

---
+++ src/main/java/TaskException.java
@@ -0,0 +1,8 @@
+public class TaskException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public TaskException(String message) {
+        super(message);
+    }
+}
