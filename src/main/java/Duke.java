a47d6971baace9b638feeb0d151c121bde2d9ec4
nowknowing <s.sunaga00@gmail.com>
2021-01-21 12:18:25 +0800

    Editted/added intro display

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String rightIndent = "   ";
+    private static final String textSpacer = rightIndent + "|" + " ";
+    private static final String responseBoxTop = rightIndent + "________________________________________\n";
+    private static final String responseBoxBottom = rightIndent + "-------------------------------------\n"
+            + rightIndent + "                                     \\|\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        displayIntro();
     }
+
+
+
+
+
+    private static void displayIntro() {
+        String logo = textSpacer + " ____        _        \n"
+                + textSpacer + "|  _ \\ _   _| | _____ \n"
+                + textSpacer + "| | | | | | | |/ / _ \\\n"
+                + textSpacer + "| |_| | |_| |   <  __/\n"
+                + textSpacer + "|____/ \\__,_|_|\\_\\___|\n"
+                + textSpacer + "\n";
+        String response = responseBoxTop
+                + textSpacer +"Hello from\n"
+                + logo
+                + textSpacer + "What can I do for you?\n"
+                + responseBoxBottom;
+        System.out.println(response);
+    }
+
 }
