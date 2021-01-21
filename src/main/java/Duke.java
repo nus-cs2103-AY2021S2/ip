7fdce46168b7556882205027fc1cb58727a38a5b
KnitidCeladon23 <kenny.chewpuyang23@gmail.com>
2021-01-21 20:35:05 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -49,21 +49,27 @@ public class Duke {
                     String s = input.substring(5);
                     CheckList c = new CheckList("T", s, false);
                     inputList.add(c);
+                    System.out.println(dash);
                     System.out.println("Pai Kia Bot: ok i added this task chop chop: " + c.toString().substring(1));
+                    System.out.println(dash);
                 }
             } else if (input.length() > 5 && input.substring(0, 6).equals("event ")) {
                 if (!input.equals("event ")) {
                     String[] s = input.substring(6).split("/at ");
                     CheckList c = new CheckList("E", s[0] + "(at: " + s[1] + ")", false);
                     inputList.add(c);
+                    System.out.println(dash);
                     System.out.println("Pai Kia Bot: ok i added this task chop chop: " + c.toString().substring(1));
+                    System.out.println(dash);
                 }
             } else if (input.length() > 8 && input.substring(0, 9).equals("deadline ")) {
                 if (!input.equals("deadline ")) {
                     String[] s = input.substring(9).split("/by ");
                     CheckList c = new CheckList("D", s[0] + "(by: " + s[1] + ")", false);
                     inputList.add(c);
+                    System.out.println(dash);
                     System.out.println("Pai Kia Bot: ok i added this task chop chop: " + c.toString().substring(1));
+                    System.out.println(dash);
                 }
             } else {
                 System.out.println(dash);
