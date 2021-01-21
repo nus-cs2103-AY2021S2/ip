74380bf34ec6f51f2483f13018f909da3837d6e6
ME <zhangspacexinyi@gmail.com>
2021-01-21 16:25:12 +0800

    Add basic greet, echo and exit features for Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "     ____        _        \n"
+                + "     |  _ \\ _   _| | _____ \n"
+                + "     | | | | | | | |/ / _ \\\n"
+                + "     | |_| | |_| |   <  __/\n"
+                + "     |____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("     Konichiwa~~ Watashi wa \n" + logo + " desu!\n     What can I do for you today?");
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+
+        while(!command.equals("bye")){
+            System.out.println("     Hai, "+ command);
+            command = sc.nextLine();
+        }
+        System.out.println("     Sayonara! Mata ne~ ;)");
     }
 }
