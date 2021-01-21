c0466429ffee63103fdd8fccddc542c06437580d
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-21 22:38:14 +0800

    Update Duke.java
    The message given when CommandNotValidException caught is not enough.
    Added one more line of message to show all available commands.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -66,6 +66,7 @@ public class Duke {
                     System.out.println("\t____________________________________________________________\n"
                                     + "\tCommand not valid. Please use \"todo\", \"deadline\"\n"
                                     + "\tor \"event\" followed by task description to add new tasks.\n"
+                                    + "\tPlease use \"list\" to view your list of tasks.\n"
                                     + "\tPlease use \"done\" followed by index to mark completed tasks.\n"
                                     + "\tPlease use \"delete\" followed by index to delete tasks.\n"
                                     + "\tPlease use \"bye\" to exit.\n"
