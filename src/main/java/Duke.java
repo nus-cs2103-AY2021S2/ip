ff0cc4c42f6a0170f228bea3a517485d7cc15e47
jingxueguo <guo.jingxuess@gmail.com>
2021-01-22 21:32:43 +0800

    Add input and output for automated text UI testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 import java.util.List;
 public class Duke {
     public static void main(String[] args) {
+        int counter = 0;
         ArrayList<Task> list = new ArrayList<>();
         Scanner sc = new Scanner (System.in);
         System.out.println("Hello! What can I do for you:>");
         String input;
         do {
             input = sc.nextLine();
+            if (input.equals("bye")){
+                break;
+            }
             if (input.equals("list")) {
                 for (int i = 0; i < list.size(); i++) {
                     System.out.println((i + 1) + "." + list.get(i));
@@ -24,22 +28,33 @@ public class Duke {
                         System.out.println(list.get(value - 1).toString());
                         break;
                     case "todo":
+                        counter++;
                         String name = String.join(" ", Arrays.copyOfRange(inputSplit, 1, inputSplit.length));
                         list.add(new ToDos(name));
-                        System.out.println("added: " + name);
+                        System.out.println("Got it. I have added this task:");
+                        System.out.println(list.get(counter - 1));
+                        System.out.println("You now have " + counter + " task(s) in the list!");
                         break;
                     case "deadline":
+                        counter++;
                         String deadlineName = String.join(" ", Arrays.copyOfRange(inputSplit, 1, inputSplit.length));
                         String[] deadlineSplit = deadlineName.split("/by ");
                         list.add(new Deadlines(deadlineSplit[0], deadlineSplit[1]));
-                        System.out.println("added: " + deadlineSplit[0]);
+                        System.out.println("Got it. I have added this task:");
+                        System.out.println(list.get(counter - 1));
+                        System.out.println("You now have " + counter + " task(s) in the list!");
                         break;
                     case "event":
+                        counter++;
                         String event = String.join(" ",Arrays.copyOfRange(inputSplit, 1, inputSplit.length));
                         String[] eventSplit = event.split("/at ");
                         list.add(new Events(eventSplit[0], eventSplit[1]));
-                        System.out.println("added: " + eventSplit[0]);
+                        System.out.println("Got it. I have added this task:");
+                        System.out.println(list.get(counter - 1));
+                        System.out.println("You now have " + counter + " task(s) in the list!");
                         break;
+                    default:
+                        System.out.println("You have entered invalid commands, please try again!");
                 }
             }
         } while (!input.equals("bye"));
