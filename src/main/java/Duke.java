9287a865e39580a946bc882f368b7472ff4d9628
Nguyen Son Linh <linhns59@gmail.com>
2021-01-22 19:25:25 +0800

    Redesign Duke and split functions into TaskManager class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,105 +1,31 @@
 import java.util.*;
 public class Duke {
-    public static void main(String[] args) throws DukeException{
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        String sep = "****************************\n";
-        System.out.println(sep);
+    static final String seperatorLine = "-----------------------------";
+    public static void main(String[] args) throws DukeException {
+        String welcome = "Hi, I'm Duke and I'm gonna be your assistant. Enjoy!!!";
+        TaskManager taskManager = new TaskManager();
+        System.out.println(welcome);
+        System.out.println();
+
         Scanner sc = new Scanner(System.in);
-        ArrayList<Task> taskLst = new ArrayList<>();
-        while(true) {
+        while (sc.hasNextLine()) {
             try {
-                String str = sc.nextLine();
-                Task task = null;
-                String[] components = str.split(" ");
-                if(str.equals("bye")) {
-                    System.out.println("Bye. Hope to see you again soon!");
-                    break;
+                String command = sc.nextLine();
+                if (command.equals("bye")) {
+                    System.out.println(seperatorLine);
+                    System.out.println("Bye! Hope to see you again soon!");
+                    System.out.println(seperatorLine);
+                } else if (command.equals("list")) {
+                    taskManager.listTask();
+                } else if (command.startsWith("done")) {
+                    taskManager.markDone(command.substring(5));
+                } else {
+                    taskManager.addTask(command);
                 }
-                else if(str.equals("list")) {
-                    int counter = 1;
-                    for(Task t: taskLst) {
-                        System.out.println(counter + "." + t.toString());
-                        counter++;
-                    }
-                }
-                else if (components[0].equals("done")) {
-                    System.out.println("Nice! I've marked this task as done: ");
-                    int index = Integer.parseInt(components[1]) - 1;
-                    taskLst.get(index).markDone();
-                    System.out.println("  " + taskLst.get(index).toString());
-                }
-                else {
-                    if(components[0].equals("deadline")) {
-                        StringBuilder desc = new StringBuilder(), time = new StringBuilder();
-                        int i = 1;
-                        while(!components[i].equals("/by")) {
-                            desc.append(components[i] + " ");
-                            i++;
-                        }
-                        i++;
-                        while(i < components.length) {
-                            time.append(components[i] + " ");
-                            i++;
-                        }
-                        time.deleteCharAt(time.length() - 1);
-                        task = new Deadline(desc.toString(), time.toString());
-                        taskLst.add(task);
-                    }
-                    else if (components[0].equals("todo")) {
-                        StringBuilder desc = new StringBuilder();
-                        if(components.length <= 1) throw new DukeException("The description of a todo cannot be empty.");
-                        int i = 1;
-                        while(i < components.length) {
-                            desc.append(components[i] + " ");
-                            i++;
-                        }
-                        task = new ToDo(desc.toString());
-                        taskLst.add(task);
-                    }
-                    else if (components[0].equals("event")) {
-                        StringBuilder desc = new StringBuilder(), time = new StringBuilder();
-                        int i = 1;
-                        while(!components[i].equals("/at")) {
-                            desc.append(components[i] + " ");
-                            i++;
-                        }
-                        i++;
-                        while(i < components.length) {
-                            time.append(components[i] + " ");
-                            i++;
-                        }
-                        time.deleteCharAt(time.length() - 1);
-                        task = new Event(desc.toString(), time.toString());
-                        taskLst.add(task);
-                    }
-                    else if (components[0].equals("delete")) {
-                        if (components.length == 1) throw new DukeException("Please insert an index!");
-                        int index = Integer.parseInt(components[1]);
-                        if (index > taskLst.size()) throw new DukeException("Index out of bounds!");
-                        Task removedTask = taskLst.remove(index);
-                        System.out.println("Noted. I've removed this task:\n" +
-                                removedTask.toString() + "\nNow you have " + (taskLst.size())
-                                + (taskLst.size() > 1 ? " tasks" : " task")
-                                + " in the list.");
-                        System.out.println(sep);
-                        continue;
-                    }
-                    else {
-                        throw new DukeException("I'm sorry, but I don't know what that means :-(");
-                    }
-                    System.out.println("Got it. Now I have added this " +
-                            "task:\n" + "  " + task.toString() + "\n" +
-                            "Now you have " + taskLst.size() + " tasks in the list.");
-                }
-                System.out.println(sep);
             } catch (DukeException err) {
+                System.out.println(seperatorLine);
                 System.out.println(err.getMessage());
-                System.out.println(sep);
+                System.out.println(seperatorLine);
             }
         }
     }
