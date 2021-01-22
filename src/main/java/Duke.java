fc96448a7fb32f23dd7920c44080bd60f50afd75
KevinLohJunYong <e0439232@u.nus.edu>
2021-01-22 15:16:56 +0800

    Done with level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ public class Duke {
     private static Scanner sc = new Scanner(System.in);
     private static int numOfItems = 0;
     private static String[] items = new String[100];
+    private static boolean[] doneItems = new boolean[100];
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _ \n"
@@ -14,6 +15,7 @@ public class Duke {
         while(true) {
             String firstWord = sc.next();
             if(firstWord.equals("list")) listItems();
+            else if(firstWord.equals("done")) doneItem();
             else if(firstWord.equals("bye")) {
                 byeUser();
                 break;
@@ -33,9 +35,16 @@ public class Duke {
     }
 
     private static void listItems() {
+          System.out.println("Here are the items in your list:");
          for(int itemNo=1;itemNo<=numOfItems;itemNo++) {
-             System.out.println(itemNo+". "+items[itemNo-1]);
+             System.out.println(itemNo+".["+(doneItems[itemNo-1]?'X':"")+"] "+items[itemNo-1]);
          }
+    }
+    private static void doneItem() {
+        int itemNo = sc.nextInt();
+        doneItems[itemNo-1] = true;
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.print("  "+"[X] "+ items[itemNo-1]);
     }
 
     private static void greetUser() {
