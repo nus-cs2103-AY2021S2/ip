108e7e30009d06c864145293b84176aaeb54694c
KevinLohJunYong <e0439232@u.nus.edu>
2021-01-22 16:02:55 +0800

    Done with level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ public class Duke {
     private static int numOfItems = 0;
     private static String[] items = new String[100];
     private static boolean[] doneItems = new boolean[100];
+    private static char[] typeOfItems = new char[100];
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _ \n"
@@ -26,18 +27,20 @@ public class Duke {
     }
 
     private static void addItems(String firstWord) {
-        StringBuilder item = new StringBuilder();
-        item.append(firstWord);
-        item.append(sc.nextLine());
+        StringBuilder item = new StringBuilder(sc.nextLine());
         items[numOfItems] = item.toString();
+        if(firstWord.charAt(0) == 'd' || firstWord.charAt(0) == 'D') typeOfItems[numOfItems] = 'D';
+        else if(firstWord.charAt(0) == 'e' || firstWord.charAt(0) == 'E') typeOfItems[numOfItems] = 'E';
+        else if(firstWord.charAt(0) == 't' || firstWord.charAt(0) == 'T') typeOfItems[numOfItems] = 'T';
         numOfItems++;
         System.out.println("added: "+item);
+        System.out.println("Now you have "+numOfItems+" tasks in your list");
     }
 
     private static void listItems() {
           System.out.println("Here are the items in your list:");
          for(int itemNo=1;itemNo<=numOfItems;itemNo++) {
-             System.out.println(itemNo+".["+(doneItems[itemNo-1]?'X':"")+"] "+items[itemNo-1]);
+             System.out.println(itemNo+"."+"["+ typeOfItems[itemNo-1]+"]"+"["+(doneItems[itemNo-1]?'X':"")+"] "+items[itemNo-1]);
          }
     }
     private static void doneItem() {
