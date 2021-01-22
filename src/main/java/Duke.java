c25f3b831941080f0b88cd38fbaadedc5a867489
jingxueguo <guo.jingxuess@gmail.com>
2021-01-22 14:39:47 +0800

    Add functionality to mark task as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Event> list = new ArrayList<>();
         Scanner sc = new Scanner (System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,19 +14,48 @@ public class Duke {
         System.out.println("Hello! What can I do for you:>");
 
         String input = sc.nextLine();
-
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println((i+1) + "." + list.get(i));
+                    System.out.println((i + 1) + "." + list.get(i));
                 }
             } else {
-                list.add(input);
-                System.out.println("added: " + input);
+                String[] inputSplit = input.split(" ");
+                if (inputSplit[0].equals("done")) {
+                    int value = Integer.valueOf(inputSplit[1]);
+                    list.get(value - 1).setDone();
+                    System.out.println("Nice, I have set this task as done!");
+                    System.out.println(list.get(value - 1).toString());
+                } else {
+                    list.add(new Event(false, input));
+                    System.out.println("added: " + input);
+                }
             }
             input = sc.nextLine();
         }
 
         System.out.println("Bye. See you again!");
+    }
+}
+
+class Event {
+    public boolean done = false;
+    public String eventName = "";
+
+    Event(boolean done, String eventName) {
+        this.done = done;
+        this.eventName = eventName;
+    }
+    public void setDone() {
+        this.done = true;
+    }
+
+    @Override
+    public String toString() {
+        if (this.done == true) {
+            return "[X] " + eventName;
+        } else {
+            return "[ ] " + eventName;
+        }
     }
 }
