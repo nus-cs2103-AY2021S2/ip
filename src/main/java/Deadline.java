ce7fffedc04a5ebc62c3afc49bfb60cdf49b6dca
JodyLorah <jodylorah@yahoo.com.sg>
2021-01-22 17:19:20 +0800

    Level 8. Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,18 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate date;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.date = LocalDate.parse(by);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " +
+                this.date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 
 }
