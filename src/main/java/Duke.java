cc1dd55331c9d5fa0b889ac27757699c0ed1ebb8
Chong Jing Quan <jingquan0302@gmail.com>
2021-01-22 22:19:33 +0800

    Refactor the code to make it more oop
    Currently the code is practically a monolithic mess.
    
    Splitting the code into more classes where each class
    controls one single component of the app makes it
    more readable and more maintainable.
    
    Let's break the code into manageable chunks and
    classes instead of cramming everything into one
    single main class.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,49 +1,25 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-
 import java.util.Scanner;
-import java.util.ArrayList;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-import task.ToDo;
-import task.Deadline;
-import task.Event;
-import task.Task;
 
 public class Duke {
     private static final String FILENAME = "duke.csv";
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("------------------------");
-        System.out.println("Hello! I'm Duke's friend, Ekud." +
-                "\nDuke's dead, so I'm here to take his job." +
-                "\nYou want to jot down some tasks?");
         run();
-        System.out.println("Bye Bye. Please give me 5-star rating, I still need this job." +
-                "\nMuch thanks.");
     }
 
     public static void run() {
-        ArrayList<Task> listOfTasks = handleLoad();
+        Ui ui = new Ui();
+        ui.initialize();
+        Storage storage = new Storage(FILENAME);
+        TaskList listOfTasks = storage.handleLoad();
+        Parser parser = new Parser(ui);
 
         Scanner sc = new Scanner(System.in);
         System.out.print(">>> ");
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                handleList(listOfTasks);
+                parser.handleList(listOfTasks);
             } else {
                 // handle the commands with arguments
                 int spaceIndex = input.indexOf(" ");
@@ -52,155 +28,28 @@ public class Duke {
 
                 switch (command) {
                     case "done":
-                        handleDone(input, listOfTasks);
+                        parser.handleDone(input, listOfTasks);
                         break;
                     case "todo":
-                        handleTodo(input, listOfTasks);
+                        parser.handleTodo(input, listOfTasks);
                         break;
                     case "delete":
-                        handleDelete(input, listOfTasks);
+                        parser.handleDelete(input, listOfTasks);
                         break;
                     case "deadline":
                     case "event":
-                        handleTasksWithTime(command, input, listOfTasks);
+                        parser.handleTasksWithTime(command, input, listOfTasks);
                         break;
                     default:
                         System.out.println("I have no idea what that means, what do you want?");
                         break;
                 }
             }
-            handleSave(listOfTasks);
+            storage.handleSave(listOfTasks);
 
             System.out.print(">>> ");
             input = sc.nextLine();
         }
-    }
-
-    public static void printUpdate(String preMessage, Task t, int size) {
-        System.out.println(preMessage);
-        System.out.println(t);
-        System.out.println("Now you have " + size + " tasks in the list.");
-    }
-
-    public static void handleList(ArrayList<Task> listOfTasks) {
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < listOfTasks.size(); i++) {
-            System.out.println((i + 1) + ". " + listOfTasks.get(i));
-        }
-    }
-
-    public static void handleDone(String input, ArrayList<Task> listOfTasks) {
-        try {
-            int index = Integer.parseInt(input.substring(input.indexOf(" ") + 1));
-            listOfTasks.get(index - 1).setDone(true);
-            System.out.println("Nice! I've marked this task as done:");
-            System.out.println(listOfTasks.get(index - 1));
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("You have " + listOfTasks.size() + " tasks in your list. Please check your input.");
-        } catch (NumberFormatException e) {
-            System.out.println("The input must be a positive integer!");
-        }
-    }
-
-    public static void handleTodo(String input, ArrayList<Task> listOfTasks) {
-        if (!input.contains(" ")) {
-            System.out.println("The description of a todo cannot be empty.");
-            return;
-        }
-        String task = input.substring(input.indexOf(" ") + 1);
-        Task temp = new ToDo(task);
-        listOfTasks.add(temp);
-        printUpdate("Got it. I have added the following task:", temp, listOfTasks.size());
-    }
-
-    public static void handleTasksWithTime(String command, String input, ArrayList<Task> listOfTasks) {
-        try {
-            String taskName = input.substring(input.indexOf(" ") + 1, input.indexOf("/") - 1);
-            Task temp;
-            String timing = input.substring(input.indexOf("/") + 4);
-            LocalDate date = LocalDate.parse(timing);
-            if (command.startsWith("deadline")) {
-                temp = new Deadline(taskName, date);
-            } else {
-                temp = new Event(taskName, date);
-            }
-            listOfTasks.add(temp);
-            printUpdate("Got it. I have added the following task:", temp, listOfTasks.size());
-        } catch (StringIndexOutOfBoundsException e) {
-            System.out.println("The timing of the task is not included. Please check your input.");
-        } catch (java.time.format.DateTimeParseException e) {
-            System.out.println("Please input a date with correct format (yyyy-mm-dd)");
-        }
-    }
-
-    public static void handleDelete(String input, ArrayList<Task> listOfTasks) {
-        try {
-            int index = Integer.parseInt(input.substring(input.indexOf(" ") + 1));
-            printUpdate(
-                    "Noted. I have removed the following task:",
-                    listOfTasks.remove(index - 1),
-                    listOfTasks.size()
-            );
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("You have " + listOfTasks.size() + " tasks in your list. Please check your input.");
-        } catch (NumberFormatException e) {
-            System.out.println("The input must be a positive integer!");
-        }
-    }
-
-    public static void handleSave(ArrayList<Task> data) {
-        try {
-            FileWriter csvWriter = new FileWriter("duke.csv");
-            for (Task task : data) {
-                csvWriter.append(task.parseToCSVRow());
-                csvWriter.append("\n");
-            }
-            csvWriter.flush();
-            csvWriter.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred while saving your changes. Please try again later.");
-        }
-    }
-
-    public static ArrayList<Task> handleLoad() {
-        try {
-            ArrayList<Task> listOfTasks =  new ArrayList<>();
-            BufferedReader reader = new BufferedReader(new FileReader(FILENAME));
-            String line = reader.readLine();
-            while(line != null) {
-                String[] details = line.split(",");
-                switch (details[0]){
-                    case "T":
-                        listOfTasks.add(new ToDo(details[2], Boolean.parseBoolean(details[1])));
-                        break;
-                    case "D":
-                        LocalDate dateD = LocalDate.parse(details[3]);
-                        listOfTasks.add(new Deadline(details[2], Boolean.parseBoolean(details[1]), dateD));
-                        break;
-                    case "E":
-                        LocalDate dateE = LocalDate.parse(details[3]);
-                        listOfTasks.add(new Event(details[2], Boolean.parseBoolean(details[1]), dateE));
-                        break;
-                }
-                line = reader.readLine();
-            }
-            return listOfTasks;
-        } catch (FileNotFoundException e) {
-            System.out.println("It seems like you do not have a save file. A new one will be created.");
-            makeNewFile();
-            return new ArrayList<>();
-        } catch (IOException e) {
-            System.out.println("An error occurred while loading. Please try again later.");
-            System.exit(1);
-            return null;
-        }
-    }
-
-    public static void makeNewFile() {
-        try{
-            new File(FILENAME).createNewFile();
-        } catch (IOException e) {
-            System.out.println("An error occurred while loading. Please try again later.");
-        }
+        ui.exit();
     }
 }
