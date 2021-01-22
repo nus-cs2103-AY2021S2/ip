a7b374a8a622540510094c21b84e11a172d0f0ba
sjq-sohjunqi <65146620+sjq-sohjunqi@users.noreply.github.com>
2021-01-22 11:52:29 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        chatBot();
+
+    }
+
+    public static void chatBot(){
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        String in = sc.nextLine();
+
+        while (!in.equals("bye")) {
+            System.out.println(in);
+            in = sc.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
