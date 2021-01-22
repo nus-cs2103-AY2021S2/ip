644f9eb2cef93f0aee6067cb1800f03b69d96197
Chong Jing Quan <jingquan0302@gmail.com>
2021-01-23 02:00:33 +0800

    DukeException: Change toString method
    After the user provides an incorrect input, the application
    shows the error message together with the exception class.
    
    This is rather ugly, from a user's perspective.
    
    Let's prevent the exception class from being printed out
    when an error is thrown.

--- src/main/java/controller/DukeException.java
+++ src/main/java/controller/DukeException.java
@@ -12,4 +12,8 @@ public class DukeException extends Exception{
     public DukeException(String errorMessage) {
         super(errorMessage);
     }
+
+    public String toString() {
+        return super.getMessage();
+    }
 }
