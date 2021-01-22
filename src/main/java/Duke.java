d68ec9667e747b5b1ec8b1ff001c9c3207b82e8b
sjq-sohjunqi <65146620+sjq-sohjunqi@users.noreply.github.com>
2021-01-22 17:32:01 +0800

    Add task types
    Different types of tasks not differentiated
    
    By introducing task types, different tasks can have specialised characteristics relevant to them

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -38,10 +38,39 @@ public class Duke {
 					System.out.println("Nice! I've marked this task as done:");
 					System.out.println(t);
 					break;
-				default:
-					tl.add(in);
-					System.out.println("added: "+in);
+				case "todo":
+					split = in.split("todo\\s");
+					
+					Task td = new Task(split[1]);
+					tl.add(td);
+					
+					System.out.println("Got it. I've added this task:");
+					System.out.println(td);
+					System.out.println("Now you have "+tl.count()+" tasks in the list.");
 					break;
+				case "deadline":
+					split = in.split("deadline\\s");
+					split = split[1].split("\\s/by\\s");
+					
+					Task dl = new Task(split[0], 'D', split[1]);
+					tl.add(dl);
+					
+					System.out.println("Got it. I've added this task:");
+					System.out.println(dl);
+					System.out.println("Now you have "+tl.count()+" tasks in the list.");
+					break;
+				case "event":
+					split = in.split("event\\s");
+					split = split[1].split("\\s/at\\s");
+					
+					Task ev = new Task(split[0], 'E', split[1]);
+					tl.add(ev);
+					
+					System.out.println("Got it. I've added this task:");
+					System.out.println(ev);
+					System.out.println("Now you have "+tl.count()+" tasks in the list.");
+					break;
+					
 			}
 			
 			in = sc.nextLine();
@@ -57,10 +86,20 @@ public class Duke {
 
 class Task {
 	private String name;
+	private char type;
+	private String dateTime;
 	private boolean done;
+	
+	public Task(String n, char t, String dt) {
+		name = n;
+		type = t;
+		dateTime = dt;
+		done = false;
+	}
 	
 	public Task(String n) {
 		name = n;
+		type = 'T';
 		done = false;
 	}
 	
@@ -69,10 +108,25 @@ class Task {
 	}
 	
 	public String toString() {
+		
+		String str = "["+type+"]";
+		
 		if (done)
-			return "[X] "+name;
+			str += "[X] "+name;
 		else
-			return "[ ] "+name;
+			str += "[ ] "+name;
+		
+		switch (type) {
+			case 'D':
+				str += " (by: "+dateTime+")";
+				break;
+			case 'E':
+				str += " (at: "+dateTime+")";
+				break;
+		}
+		
+		return str;
+		
 	}
 }
 
@@ -90,8 +144,8 @@ class TaskList {
 		}
 	}
 	
-	public void add(String obj) {
-		taskList.add(new Task(obj));
+	public void add(Task t) {
+		taskList.add(t);
 	}
 	
 	public Task markDone(int i) {
@@ -99,6 +153,10 @@ class TaskList {
 		t.mark();
 		
 		return t;
+	}
+	
+	public int count() {
+		return taskList.size();
 	}
 	
 }
