2a34aca41067c0bef1b6c24e77ecbe2b83de052d
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-23 21:56:26 +0800

    Update TodoTask.java

---
+++ src/main/java/tasks/TodoTask.java
@@ -0,0 +1,12 @@
+package tasks;
+
+public class TodoTask extends Task {
+    public TodoTask(String description) {
+        super(description, 'T');
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
