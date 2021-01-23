8ac4d567ec8194fa805f97eeb226d55f60594918
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-23 15:26:17 +0800

    Update DeadlineTask.java

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -1,13 +1,38 @@
-public class DeadlineTask extends Task {
-    protected String date;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    public DeadlineTask(String description, String date) {
+public class DeadlineTask extends Task {
+    protected LocalDate date;
+    protected LocalTime time;
+
+    public DeadlineTask(String description, LocalDate date) {
         super(description);
         this.date = date;
+        this.time = null;
+    }
+
+    public DeadlineTask(String description, LocalDate date, LocalTime time) {
+        super(description);
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getDateString() {
+        return this.date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    public String getTimeString() {
+        return this.time.format(DateTimeFormatter.ofPattern("HHmm"));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.date + ")";
+        return "[D]" + super.toString() + " (by: "
+                + this.date.format(DateTimeFormatter.ofPattern("dd MMM yyyy"))
+                + (this.time == null
+                ? ""
+                : " " + this.time.format(DateTimeFormatter.ofPattern("hh:mm a")))
+                + ")";
     }
 }
