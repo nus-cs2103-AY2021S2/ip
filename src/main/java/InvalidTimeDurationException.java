30ac6a266bf0ba63facded5abbb0d1993af86912
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-23 21:50:54 +0800

    Move InvalidTimeDurationException.java

--- src/main/java/InvalidTimeDurationException.java
+++
@@ -1,6 +0,0 @@
-//public class InvalidTimeDurationException extends Exception {
-public class InvalidTimeDurationException extends DukeException {
-    public InvalidTimeDurationException() {
-        super("\tPlease enter a valid start and end time duration\n\t(start time < end time).\n");
-    }
-}
