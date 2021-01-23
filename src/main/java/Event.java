f8e7b435bcebd90655a3630472df0908b8fabeaa
Nguyen Son Linh <linhns59@gmail.com>
2021-01-23 11:26:49 +0800

    add date/time support

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,12 +1,15 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
-    private String time;
+    private LocalDate time;
     public Event(String task, String time) {
         super(task);
-        this.time = time;
+        this.time = LocalDate.parse(time);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at: " + time + ")";
+        return "[E]" + super.toString() + "(at: " + time.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
     }
 }
