c54371ea9641aeda13b4df69b6d0d5f8683b1dc2
BennyPhoe <bennyphoe1998@gmail.com>
2021-01-23 20:09:39 +0800

    Add FileInput class to handle file related operations and to initialise data

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        FileInput fileInput = new FileInput();
+        fileInput.loadFile("./data", "./data/duke.txt");
+        File file = new File("./data/duke.txt");
         Scanner sc = new Scanner(System.in);
+        printStatements();
+        List<Task> loadedData = fileInput.initialiseStorage(file);
+        UserInput userInput = new UserInput(sc, loadedData);
+        userInput.executeInput();
+
+
+    }
+
+    private static void printStatements() {
         String line = "";
         for (int i = 0; i < 50; i++) {
             line = line.concat("_");
@@ -13,11 +26,6 @@ public class Duke {
         System.out.println("Hello! I'm Benny");
         System.out.println("What can I do for you?");
         System.out.println(line);
-
-        UserInput userInput = new UserInput(sc);
-        userInput.executeInput();
-
-
     }
 
 }
