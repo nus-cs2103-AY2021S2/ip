f40ce9513fc68b7b897a3c3b710e6f9f0c3b04d8
hengyongming <e0425848@u.nus.edu>
2021-01-23 22:47:05 +0800

    Implement parseFile method in Parser.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 public class Duke {
 
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -7,6 +7,37 @@ public class Parser {
         this.taskList = new TaskList();
     }
 
+    public void parseFile(String taskString) throws IOException{
+        String type = taskString.substring(1,2);
+        String isDone = taskString.substring(4,5);
+        if(type.equals("T")){
+            String description = taskString.substring(7);
+            ToDo task = new ToDo(description);
+            if(isDone.equals("X")){
+                task.markAsDone();
+            }
+            this.taskList.addTask(task);
+        }
+        else if(type.equals("D")){
+            String description = taskString.split(" \\(by: ")[0].substring(7);
+            String by = taskString.split(" \\(by: ")[1].replaceAll("\\)","");
+            Deadlines task = new Deadlines(description, by);
+            if(isDone.equals("X")){
+                task.markAsDone();
+            }
+            this.taskList.addTask(task);
+        }
+        else{
+            String description = taskString.split(" \\(at: ")[0].substring(7);
+            String by = taskString.split(" \\(at: ")[1].replaceAll("\\)","");
+            Events task = new Events(description, by);
+            if(isDone.equals("X")){
+                task.markAsDone();
+            }
+            this.taskList.addTask(task);
+        }
+    }
+
     public void parseUserCommand(String commandFromUser) throws IOException, DukeException {
         if(commandFromUser.equals("list")){
             this.taskList.listTask();
