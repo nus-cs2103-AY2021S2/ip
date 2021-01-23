6e8359c8285af3b8a09af0fa15853e9ac186edc0
Tomashiwa <wongqinliang@gmail.com>
2021-01-23 16:48:30 +0800

    Extract load tasks from hard disk to Parser class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,83 +1,23 @@
-import java.io.File;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     private static final String CHATBOT_NAME = "Mantaro";
-    private static final String TASKSLIST_PATH = "data/duke.txt";
-
     private static boolean isActive = true;
-    private static final List<Task> tasks = new ArrayList<>();
-
-    /**
-     * Load tasks from previous session that was saved into hard disk
-     * @throws DukeLoadException if there is an IO issue reading the save file or invalid task type was found
-     */
-    private static void loadTasks() throws DukeLoadException {
-        // Create the 'data' folder if missing
-        File dir = new File("data");
-        if(!dir.exists()) {
-            dir.mkdir();
-        }
-
-        // Load the save file or create one if missing
-        File file = new File(TASKSLIST_PATH);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy ha");
-
-        try {
-            // If the save file already exists, load its tasks
-            if(!file.createNewFile()) {
-                Scanner scanner = new Scanner(file);
-                while(scanner.hasNextLine()) {
-                    String[] splits = scanner.nextLine().split(" \\| ");
-                    boolean isDone = splits[1].equals("1");
-
-                    switch (splits[0]) {
-                    case "T":
-                        ToDo toDo = new ToDo(splits[2], isDone);
-                        tasks.add(toDo);
-                        break;
-                    case "D":
-                        LocalDateTime dateTime = LocalDateTime.parse(splits[3], formatter);
-                        Deadline deadline = new Deadline(splits[2], dateTime, isDone);
-                        tasks.add(deadline);
-                        break;
-                    case "E":
-                        LocalDateTime start = LocalDateTime.parse(splits[3], formatter);
-                        LocalDateTime end = LocalDateTime.parse(splits[4], formatter);
-                        Event event = new Event(splits[2], start, end, isDone);
-                        tasks.add(event);
-                        break;
-                    default:
-                        throw new DukeLoadException("Invalid task type found: " + splits[0]);
-                    }
-                }
-            }
-        } catch (IOException e) {
-            throw new DukeLoadException("Issue with IO while loading tasks");
-        }
-    }
 
     /**
      * Lifecycle of the chatbot
      * @param args Command line arguments
      */
     public static void main(String[] args) {
-//        try {
-//            loadTasks();
-//        } catch (DukeLoadException e) {
-//            System.out.println("___________________________________________________________");
-//            System.out.printf("There is a problem while loading your past tasks meow! %s\n", e.getMessage());
-//            System.out.println("___________________________________________________________\n");
-//        }
-
         Ui ui = new Ui();
         TaskManager taskManager = new TaskManager();
         Command.setup(ui, taskManager);
+
+        try {
+            Storage.loadTasksTo(taskManager);
+        } catch (DukeLoadException e) {
+            ui.printError(e.getMessage());
+        }
 
         // Opening message
         ui.printWelcomeMsg(CHATBOT_NAME);
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -34,49 +33,56 @@ public class Storage {
         }
     }
 
-    public static List<Task> loadTasks() throws DukeLoadException {
-        return new ArrayList<Task>();
-//        // Create the 'data' folder if missing
-//        File dir = new File("data");
-//        if(!dir.exists()) {
-//            dir.mkdir();
-//        }
-//
-//        // Load the save file or create one if missing
-//        File file = new File(PATH);
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy ha");
-//
-//        try {
-//            // If the save file already exists, load its tasks
-//            if(!file.createNewFile()) {
-//                Scanner scanner = new Scanner(file);
-//                while(scanner.hasNextLine()) {
-//                    String[] splits = scanner.nextLine().split(" \\| ");
-//                    boolean isDone = splits[1].equals("1");
-//
-//                    switch (splits[0]) {
-//                    case "T":
-//                        ToDo toDo = new ToDo(splits[2], isDone);
-//                        tasks.add(toDo);
-//                        break;
-//                    case "D":
-//                        LocalDateTime dateTime = LocalDateTime.parse(splits[3], formatter);
-//                        Deadline deadline = new Deadline(splits[2], dateTime, isDone);
-//                        tasks.add(deadline);
-//                        break;
-//                    case "E":
-//                        LocalDateTime start = LocalDateTime.parse(splits[3], formatter);
-//                        LocalDateTime end = LocalDateTime.parse(splits[4], formatter);
-//                        Event event = new Event(splits[2], start, end, isDone);
-//                        tasks.add(event);
-//                        break;
-//                    default:
-//                        throw new DukeLoadException("Invalid task type found: " + splits[0]);
-//                    }
-//                }
-//            }
-//        } catch (IOException e) {
-//            throw new DukeLoadException("Issue with IO while loading tasks");
-//        }
+    public static void loadTasksTo(TaskManager taskManager) throws DukeLoadException {
+        // Create the 'data' folder if missing
+        File dir = new File("data");
+        if(!dir.exists()) {
+            dir.mkdir();
+        }
+
+        // Load the save file or create one if missing
+        File file = new File(PATH);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy ha");
+
+        try {
+            // If the save file already exists, load its tasks
+            if(!file.createNewFile()) {
+                Scanner scanner = new Scanner(file);
+                while(scanner.hasNextLine()) {
+                    String[] splits = scanner.nextLine().split(" \\| ");
+                    boolean isDone = splits[1].equals("1");
+
+                    switch (splits[0]) {
+                    case "T":
+                        ToDo toDo = taskManager.addToDo(splits[2]);
+                        if(isDone) {
+                            toDo.markAsDone();
+                        }
+                        break;
+                    case "D":
+                        LocalDateTime dateTime = LocalDateTime.parse(splits[3], formatter);
+                        Deadline deadline = taskManager.addDeadline(splits[2], dateTime);
+                        if(isDone) {
+                            deadline.markAsDone();
+                        }
+                        break;
+                    case "E":
+                        LocalDateTime start = LocalDateTime.parse(splits[3], formatter);
+                        LocalDateTime end = LocalDateTime.parse(splits[4], formatter);
+                        Event event = taskManager.addEvent(splits[2], start, end);
+                        if(isDone) {
+                            event.markAsDone();
+                        }
+                        break;
+                    default:
+                        throw new DukeLoadException("Invalid task type found: " + splits[0]);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new DukeLoadException("Issue with IO while loading tasks");
+        } catch (DukeTaskException e) {
+            throw new DukeLoadException(e.getMessage());
+        }
     }
 }
