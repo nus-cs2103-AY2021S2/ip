2bbf3e47bbb5cc93513c951636379dcf8a4685a3
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-23 20:46:05 +0800

    Add TaskList.java

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class TaskList {
+    private List<Task> tasks;
+
+    public TaskList(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void add(Task task) {
+        this.tasks.add(task);
+    }
+
+    public void remove(int index) {
+        this.tasks.remove(index);
+    }
+
+    public Task get(int index) {
+        return this.tasks.get(index);
+    }
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public int size() {
+        return this.tasks.size();
+    }
+}
