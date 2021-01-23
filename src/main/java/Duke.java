50ca5afcec39d4b4f50cd546b8f47693a9cba677
shermanyuen <ysm@u.nus.edu>
2021-01-24 02:14:14 +0800

    Added Level-2 requirements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,32 +1,65 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static final String s = "     ";
+    public static final String line = "    _______________________________________________________________________\n";
+    public static String[] tasks = new String[100];
+    public static int numTasks;
+
+    public static void printMessage(String message) {
+        System.out.println(line);
+        message = message.replace("\n", "\n" + s);
+        System.out.println(s + message);
+        System.out.println(line);
+    }
+
+    public static void printWelcomeMessage() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        String welcomeMessage = "Hello from \n" + logo + "\n"
+                + "What can I do for you?";
+        printMessage(welcomeMessage);
+    }
+
+    public static void printGoodbyeMessage() {
+        printMessage("Bye. Hope to see you again soon!");
+    }
+
+    public static void listTasks() {
+        int i = 0;
+        String tasksMessage = "";
+        while (i < numTasks) {
+            tasksMessage = tasksMessage + String.valueOf(i + 1) + ". " + tasks[i] + "\n";
+            i++;
+        }
+        printMessage(tasksMessage);
+    }
+
+    public static void addTask(String taskString) {
+        tasks[numTasks] = taskString;
+        numTasks++;
+    }
+
     public static void main(String[] args) {
-        String s = "     ";
-        String logo = s + " ____        _        \n"
-                + s + "|  _ \\ _   _| | _____ \n"
-                + s + "| | | | | | | |/ / _ \\\n"
-                + s + "| |_| | |_| |   <  __/\n"
-                + s + "|____/ \\__,_|_|\\_\\___|\n";
-        String line = "    _______________________________________________________________________\n";
-        System.out.println(line);
-        System.out.println(s + "Hello from\n" + logo);
-        System.out.println(s + "What can I do for you?");
-        System.out.println(line);
+        printWelcomeMessage();
 
         String userInput;
         Scanner scan = new Scanner(System.in);
         userInput = scan.nextLine();
 
         while (String.valueOf(userInput).toLowerCase().equals("bye") == false) {
-            System.out.println(line);
-            System.out.println(s + userInput);
-            System.out.println(line);
+            if (String.valueOf(userInput).toLowerCase().equals("list") == true) {
+                listTasks();
+            } else {
+                addTask(userInput);
+                printMessage("added: " + userInput);
+            }
             userInput = scan.nextLine();
         }
         scan.close();
-        System.out.println(line);
-        System.out.println(s + "Bye. Hope to see you again soon!");
-        System.out.println(line);
+        printGoodbyeMessage();
     }
 }
