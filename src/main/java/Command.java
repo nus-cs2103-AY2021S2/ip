6d4713545b96a349678578d05b48174c3ac4961e
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-23 21:47:59 +0800

    Move Command.java

--- src/main/java/Command.java
+++
@@ -1,6 +0,0 @@
-import java.io.IOException;
-
-public abstract class Command {
-    public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws IOException, DukeException;
-    public abstract boolean isExit();
-}
