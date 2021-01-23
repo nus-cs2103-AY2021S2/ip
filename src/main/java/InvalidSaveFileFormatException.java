8a58b0802d47519fc5607c56511f3592ed449f01
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-23 21:50:10 +0800

    Move InvalidSaveFileFormatException.java

--- src/main/java/InvalidSaveFileFormatException.java
+++
@@ -1,6 +0,0 @@
-//public class InvalidSaveFileFormatException extends Exception {
-public class InvalidSaveFileFormatException extends DukeException {
-    public InvalidSaveFileFormatException() {
-        super("\tInvalid content format in save file.\t");
-    }
-}
