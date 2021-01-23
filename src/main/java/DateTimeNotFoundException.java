11b29ebf8ef292b25d7db753e9ed1dd337b9ae3c
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-23 21:48:24 +0800

    Move DeadlineTask.java

--- src/main/java/DateTimeNotFoundException.java
+++
@@ -1,9 +0,0 @@
-//public class DateTimeNotFoundException extends Exception {
-public class DateTimeNotFoundException extends DukeException {
-    public DateTimeNotFoundException() {
-        super("\tPlease enter the date (DD/MM/YYYY) with optional\n"
-                + "\ttime (in 24 hours format) after \"/by\" for Deadline Tasks\n"
-                + "\tor date with optional start and end time after \"/at\"\n"
-                + "\tfor Event Tasks.\n");
-    }
-}
