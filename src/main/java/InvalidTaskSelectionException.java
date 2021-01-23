3e2260ae10a61846c067464f4cf23d99502409fe
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-23 21:50:35 +0800

    Move InvalidTaskSelectionException.java

--- src/main/java/InvalidTaskSelectionException.java
+++
@@ -1,6 +0,0 @@
-//public class InvalidTaskSelectionException extends Exception {
-public class InvalidTaskSelectionException extends DukeException {
-    public InvalidTaskSelectionException() {
-        super("\tPlease enter task number after command.\n");
-    }
-}
