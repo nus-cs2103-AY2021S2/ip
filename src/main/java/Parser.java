3edac4f83bff6fe1bcd774ad7dd142640df36821
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-23 20:45:49 +0800

    Add Parser.java

---
+++ src/main/java/Parser.java
@@ -0,0 +1,22 @@
+public class Parser {
+    public Parser() {}
+
+    public static Command parse(String fullCommand) {
+        Command command;
+        if (fullCommand.equals("bye")) {
+            command = new ExitCommand();
+        } else if (fullCommand.equals("list")) {
+            command = new ListCommand();
+        } else {
+            String[] check = fullCommand.split(" ");
+            if (check[0].equals("done")) {
+                command = new DoneCommand(check);
+            } else if (check[0].equals("delete")) {
+                command = new DeleteCommand(check);
+            } else {
+                command = new AddCommand(fullCommand, check);
+            }
+        }
+        return command;
+    }
+}
