3ef0eed2094ad6ada23c257e74e8177fb076a66e
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-23 21:53:50 +0800

    Update ListCommand.java

---
+++ src/main/java/commands/ListCommand.java
@@ -0,0 +1,25 @@
+package commands;
+
+import tasks.TaskList;
+
+import ui.Ui;
+
+import storage.Storage;
+
+public class ListCommand extends Command {
+    public ListCommand() {}
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        if (tasks.size() == 0) {
+            ui.showEmptyList();
+        } else {
+            ui.showTasks(tasks);
+        }
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
