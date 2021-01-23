6c1d6463f434d705156ed920c891856c71e29858
hengyongming <e0425848@u.nus.edu>
2021-01-23 18:52:38 +0800

    format date and time

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,12 +1,26 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadlines extends Task{
-    public String by;
+    private final String by;
     Deadlines(String name, String by){
         super(name);
         this.by = by;
     }
 
+    public LocalDateTime parse(String dateTimeString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy Hmm");
+        return LocalDateTime.parse(dateTimeString, formatter);
+    }
+
+    public String parseDate() {
+        return this.parse(this.by).format(DateTimeFormatter.ofPattern("MMM d yyyy h:mm a"));
+    }
+
     @Override
     public String toString(){
-        return "[D]" + (this.done ? "[X] " : "[ ]") + this.getTaskName() +  " (by: " + this.by + ")";
+        return "[D]" + (this.done ? "[X] " : "[ ]") + this.getTaskName() +  " (by: " +
+                parseDate() + ")";
     }
 }
