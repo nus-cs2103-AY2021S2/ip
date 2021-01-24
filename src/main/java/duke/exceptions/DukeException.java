3a412c585f5a402c7f9039a879359b1e8f02891c
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-24 21:05:41 +0800

    Add javadoc for DukeException.java

--- src/main/java/duke/exceptions/DukeException.java
+++ src/main/java/duke/exceptions/DukeException.java
@@ -1,6 +1,14 @@
 package duke.exceptions;
 
+/**
+ * Error class for all exceptions thrown in Duke.
+ */
 public class DukeException extends Exception {
+    /**
+     * Constructs a DukeException with the specified message.
+     *
+     * @param message Message of exception.
+     */
     public DukeException(String message) {
         super(message);
     }
