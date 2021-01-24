22de2be8f110061a4b0eb6c3a04810717f3e9cea
enhao25 <larloller@gmail.com>
2021-01-24 23:20:15 +0800

    Merge from master to include functionality to load existing task from hard disk

--- src/main/java/Duke/DukeException.java
+++ src/main/java/Duke/DukeException.java
@@ -3,7 +3,7 @@ package Duke;
 public class DukeException extends Exception {
     
     /**
-	 * Used as exception for duke
+	 * Use as exception for duke
 	 */
 	private static final long serialVersionUID = -2154386125699489110L;
 
