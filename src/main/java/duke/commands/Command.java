15be5f6cbd88fab0204217f9972eef6bb594715d
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-24 21:04:23 +0800

    Add javadoc for Command.java

--- src/main/java/duke/commands/Command.java
+++ src/main/java/duke/commands/Command.java
@@ -10,7 +10,25 @@ import duke.storage.Storage;
 
 import java.io.IOException;
 
+/**
+ * Abstract class responsible for executing command line inputs.
+ */
 public abstract class Command {
+    /**
+     * Executes the command specified in the commannd line input.
+     *
+     * @param tasks TaskList containing tasks.
+     * @param ui Ui for system outputs.
+     * @param storage Storage for saving contents into file.
+     * @throws IOException If error happens while saving contents into save file.
+     * @throws DukeException If error happens while executing command.
+     */
     public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws IOException, DukeException;
+
+    /**
+     * Returns if program should exit after this command.
+     *
+     * @return true if should program should exit after command, false otherwise.
+     */
     public abstract boolean isExit();
 }
