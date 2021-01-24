63d156cd5dfa1b17d954e4fbc74c707414a758dd
picasdan9 <picasdan99@gmail.com>
2021-01-24 22:34:18 +0800

    Use LocalDate to represent date

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-  protected String deadline;
+  protected LocalDate deadline;
 
   public Deadline(String description, String deadline) {
     super(description);
-    this.deadline = deadline;
+    this.deadline = LocalDate.parse(deadline);
   }
 
   @Override
   public String toString() {
-    return String.format("[D]%s (by: %s)", super.toString(), deadline);
+    return String.format("[D]%s (by: %s)", super.toString(), deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
   }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-  protected String time;
+  protected LocalDate time;
 
   public Event(String description, String time) {
     super(description);
-    this.time = time;
+    this.time = LocalDate.parse(time);
   }
 
   @Override
   public String toString() {
-    return String.format("[E]%s (at: %s)", super.toString(), time);
+    return String.format("[E]%s (at: %s)", super.toString(), time.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
   }
 }
