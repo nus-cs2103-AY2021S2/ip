d0c518edd8edc63e04df8e81d0cb5a2540541920
YuFeng <leo20145kl@gmail.com>
2021-01-24 14:48:14 +0800

    Format switch case statement
    Remove the indentation for case statement in switch.

--- src/main/java/duke/Chatbot.java
+++ src/main/java/duke/Chatbot.java
@@ -14,6 +14,7 @@ public class Chatbot {
 
     /**
      * Constructs the chat bot.
+     *
      * @param taskList the task list given to chat bot
      */
     public Chatbot(TaskList taskList) {
@@ -39,62 +40,62 @@ public class Chatbot {
                 Command command = parser.parse(taskTypeSplit);
                 int tempOrder;
                 switch (command) {
-                    case LIST:
-                        taskList.printTaskList();
-                        break;
-                    case DONE:
-                        if (taskTypeSplit.length <= 1 || taskTypeSplit[1].isBlank()) {
-                            throw new DukeException("The description of done cannot be empty.");
-                        }
-                        tempOrder = Integer.parseInt(taskTypeSplit[1]);
-                        taskList.markDone(tempOrder - 1);
-                        break;
-                    case DELETE:
-                        if (taskTypeSplit.length <= 1 || taskTypeSplit[1].isBlank()) {
-                            throw new DukeException("The description of delete cannot be empty.");
-                        }
-                        tempOrder = Integer.parseInt(taskTypeSplit[1]);
-                        taskList.delete(tempOrder - 1);
-                        break;
-                    case FIND:
-                        if (taskTypeSplit.length <= 1 || taskTypeSplit[1].isBlank()) {
-                            throw new DukeException("The description of find cannot be empty.");
-                        }
-                        taskList.findTask(taskTypeSplit[1]);
-                        break;
-                    case TODO:
-                        if (taskTypeSplit.length <= 1 || taskTypeSplit[1].isBlank()) {
-                            throw new DukeException("The description of a todo cannot be empty.");
-                        }
-                        newTask = new ToDo(input.substring(5), TaskType.TODO);
-                        taskList.addTask(newTask);
-                        break;
-                    case DEADLINE:
-                        if (taskTypeSplit.length <= 1 || taskTypeSplit[1].isBlank()) {
-                            throw new DukeException("The description of a deadline cannot be empty.");
-                        }
-                        taskTimeSplit = input.split(" /by ");
-                        if (taskTimeSplit.length <= 1 || taskTimeSplit[1].isBlank()) {
-                            throw new DukeException("The time of a deadline cannot be empty.");
-                        }
-                        newTask = new Deadline(taskTimeSplit[0].substring(9),
-                                TaskType.DEADLINE, LocalDate.parse(taskTimeSplit[1]));
-                        taskList.addTask(newTask);
-                        break;
-                    case EVENT:
-                        if (taskTypeSplit.length <= 1 || taskTypeSplit[1].isBlank()) {
-                            throw new DukeException("The description of an event cannot be empty.");
-                        }
-                        taskTimeSplit = input.split(" /at ");
-                        if (taskTimeSplit.length <= 1 || taskTimeSplit[1].isBlank()) {
-                            throw new DukeException("The time of an event cannot be empty.");
-                        }
-                        newTask = new Event(taskTimeSplit[0].substring(6),
-                                TaskType.EVENT, LocalDate.parse(taskTimeSplit[1]));
-                        taskList.addTask(newTask);
-                        break;
-                    default:
-                        throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                case LIST:
+                    taskList.printTaskList();
+                    break;
+                case DONE:
+                    if (taskTypeSplit.length <= 1 || taskTypeSplit[1].isBlank()) {
+                        throw new DukeException("The description of done cannot be empty.");
+                    }
+                    tempOrder = Integer.parseInt(taskTypeSplit[1]);
+                    taskList.markDone(tempOrder - 1);
+                    break;
+                case DELETE:
+                    if (taskTypeSplit.length <= 1 || taskTypeSplit[1].isBlank()) {
+                        throw new DukeException("The description of delete cannot be empty.");
+                    }
+                    tempOrder = Integer.parseInt(taskTypeSplit[1]);
+                    taskList.delete(tempOrder - 1);
+                    break;
+                case FIND:
+                    if (taskTypeSplit.length <= 1 || taskTypeSplit[1].isBlank()) {
+                        throw new DukeException("The description of find cannot be empty.");
+                    }
+                    taskList.findTask(taskTypeSplit[1]);
+                    break;
+                case TODO:
+                    if (taskTypeSplit.length <= 1 || taskTypeSplit[1].isBlank()) {
+                        throw new DukeException("The description of a todo cannot be empty.");
+                    }
+                    newTask = new ToDo(input.substring(5), TaskType.TODO);
+                    taskList.addTask(newTask);
+                    break;
+                case DEADLINE:
+                    if (taskTypeSplit.length <= 1 || taskTypeSplit[1].isBlank()) {
+                        throw new DukeException("The description of a deadline cannot be empty.");
+                    }
+                    taskTimeSplit = input.split(" /by ");
+                    if (taskTimeSplit.length <= 1 || taskTimeSplit[1].isBlank()) {
+                        throw new DukeException("The time of a deadline cannot be empty.");
+                    }
+                    newTask = new Deadline(taskTimeSplit[0].substring(9),
+                            TaskType.DEADLINE, LocalDate.parse(taskTimeSplit[1]));
+                    taskList.addTask(newTask);
+                    break;
+                case EVENT:
+                    if (taskTypeSplit.length <= 1 || taskTypeSplit[1].isBlank()) {
+                        throw new DukeException("The description of an event cannot be empty.");
+                    }
+                    taskTimeSplit = input.split(" /at ");
+                    if (taskTimeSplit.length <= 1 || taskTimeSplit[1].isBlank()) {
+                        throw new DukeException("The time of an event cannot be empty.");
+                    }
+                    newTask = new Event(taskTimeSplit[0].substring(6),
+                            TaskType.EVENT, LocalDate.parse(taskTimeSplit[1]));
+                    taskList.addTask(newTask);
+                    break;
+                default:
+                    throw new DukeException("I'm sorry, but I don't know what that means :-(");
                 }
             } catch (DukeException ex) {
                 ui.printFormatMessage(ex.toString());
@@ -109,6 +110,7 @@ public class Chatbot {
 
     /**
      * Returns the task list in the chat bot.
+     *
      * @return the task list to be returned
      */
     public TaskList getTaskList() {
