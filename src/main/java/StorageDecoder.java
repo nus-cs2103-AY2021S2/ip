07bf47d9ddd6917a568d0424199f5d8e8e9a81a6
Marcus Ong Jing Keat <markuz5116@gmail.com>
2021-01-24 14:58:02 +0800

    Add Storage decoder class

---
+++ src/main/java/StorageDecoder.java
@@ -0,0 +1,59 @@
+import exceptions.DukeCorruptedStorageException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class StorageDecoder {
+
+    public static ArrayList<Task> decodeSave(ArrayList<String> encodeTasks) throws DukeCorruptedStorageException {
+        ArrayList<Task> tasks = new ArrayList<>();
+        for (String encodedTask : encodeTasks) {
+            AddCommandType command = Parser.parseCommandType(encodedTask);
+            Task task;
+            switch (command) {
+            case TODO:
+                task = decodeTodo(encodedTask);
+                break;
+            case EVENT:
+            case DEADLINE:
+                task = decodeTaskWithDate(command, encodedTask);
+                break;
+            default:
+                throw new DukeCorruptedStorageException();
+            }
+            tasks.add(task);
+        }
+        return tasks;
+    }
+
+    private static Task decodeTodo(String encodedTask) throws DukeCorruptedStorageException {
+        boolean isDone = Parser.isEncodedTaskDone(encodedTask);
+        String description = Parser.obtainDescription(encodedTask);
+        Task t = new Todo(description);
+        if (isDone) {
+            t.done();
+        }
+        return t;
+    }
+
+    private static Task decodeTaskWithDate(AddCommandType command, String encodedTask) throws DukeCorruptedStorageException {
+        boolean isDone = Parser.isEncodedTaskDone(encodedTask);
+        String description = Parser.obtainDescription(encodedTask);
+        LocalDate date = Parser.obtainEncodedDate(encodedTask);
+        Task t;
+        switch (command) {
+        case EVENT:
+            t = new Event(description, date);
+            break;
+        case DEADLINE:
+            t = new Deadline(description, date);
+            break;
+        default:
+            throw new DukeCorruptedStorageException();
+        }
+        if (isDone) {
+            t.done();
+        }
+        return t;
+    }
+}
