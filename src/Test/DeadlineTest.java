c36a010008f3fc58e731b985b09cce849266983d
vvan-essa <e0425352@u.nus.edu>
2021-01-24 16:10:30 +0800

    Tweak the code to comply with a coding standard

--- src/Test/DeadlineTest.java
+++ src/Test/DeadlineTest.java
@@ -1,8 +1,8 @@
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
 
 public class DeadlineTest {
     @Test
