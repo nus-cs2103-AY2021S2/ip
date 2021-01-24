eb4b28ab30207f8b4eeae48efe11dfde26e98ba9
ssoonwee <songsoonwee@hotmail.com>
2021-01-24 18:06:07 +0800

    Modify Duke and TaskList class by segregating methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,13 +3,16 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Duke {
+    public static final int TODO = 1;
+    public static final int DEADLINE = 2;
+    public static final int EVENT = 3;
+
     /**
      * Starts the program.
      */
     public static void main(String[] args) throws IOException {
         printGreeting();
-        //performFirstTask();
-        TaskList taskList = performSecondTask();
+        selectAction();
     }
 
     /**
@@ -32,6 +35,33 @@ public class Duke {
         System.out.println(printLine());
         System.out.println(greet);
         System.out.println(printLine());
+    }
+
+    public static void printBye() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(printLine());
+    }
+
+    public static void selectAction() throws IOException{
+        String prompt = "We have 2 functions in this bot, namely\n";
+        prompt = prompt + "1. Echo Bot\n2. Task List Bot\n";
+        String instruct = "Please key in 1 or 2 to select the bot.\n";
+        System.out.println(prompt);
+        System.out.println(instruct);
+        BufferedReader input = new
+            BufferedReader(new InputStreamReader(System.in));
+        String cmd = input.readLine();
+        switch (cmd) {
+            case "1":
+                performFirstTask();
+                break;
+            case "2":
+                TaskList temp = performSecondTask();
+                break;
+            default:
+                break;
+        }
+        input.close();
     }
 
     /**
@@ -63,7 +93,7 @@ public class Duke {
      */
     public static TaskList performSecondTask() throws IOException {
         String cmd = new String();
-        TaskList taskList = new TaskList();
+        TaskList taskList = new TaskList("tasks.txt");
         BufferedReader input = new
             BufferedReader(new InputStreamReader(System.in));
         while (true) {
@@ -75,90 +105,105 @@ public class Duke {
                 break;
             } else if (cmd.contains("done")) {
                 int index = Integer.parseInt(cmd.substring(5));
-                taskList.getTask(index - 1).setCompleted();
-                System.out.println("Nice! I've marked this task as done: ");
-                System.out.println(taskList.getTask(index - 1));
+                taskList.setTaskDone(index);
             } else if (cmd.contains("delete")){
                 int index = Integer.parseInt(cmd.substring(7));
-                Task temp = taskList.getTask(index - 1);
-                taskList.removeTask(index - 1);
-                System.out.println("Noted. I've removed this task: ");
-                System.out.println(temp);
-                System.out.println("Now you have "
-                    + taskList.getSize()
-                    + " tasks in the list.");
+                taskList.removeTask(index );
             } else {
                 taskList = performChildTask(taskList, cmd);
             }
             System.out.println(printLine());
         }
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(printLine());
+        printBye();
         input.close();
         return taskList;
     }
 
-    /**
-     * Performs the addition of the respective child task into Task List and
-     * returns the updated Task List.
-     *
-     * @param  taskList  previous Task List.
-     * @param  cmd       the command type for the respective task.
-     * @return           updated Task List.
-     */
     public static TaskList performChildTask(TaskList taskList, String cmd) {
+        int taskType = checkChildTask(cmd);
+        String task = checkInput(cmd, taskType);
+        int seg = cmd.indexOf("/");
+        if(!task.isEmpty()) {
+            try {
+                if (taskType == -1) {
+                    throw new
+                        DukeException("☹ OOPS!!! I'm sorry, " +
+                        "but I don't know what that means :-(");
+                } else {
+                    if (taskType == TODO) {
+                        ToDo newToDo = new ToDo(task);
+                        performAddTask(taskList, newToDo);
+                    } else if (taskType == DEADLINE) {
+                        String by = cmd.substring(seg + 4);
+                        Deadline newDeadLine = new Deadline(task, by);
+                        performAddTask(taskList, newDeadLine);
+                    } else {
+                        String at = cmd.substring(seg + 4);
+                        Event newEvent = new Event(task, at);
+                        performAddTask(taskList, newEvent);
+                    }
+                    printTotalTasks(taskList);
+                }
+            } catch (DukeException de) {
+                System.out.println(de.getMessage());
+            }
+        }
+        return taskList;
+    }
+
+    public static int checkChildTask(String cmd) {
+        if (cmd.indexOf("todo") == 0 && cmd.contains("todo")) {
+            return TODO;
+        } else if (cmd.indexOf("deadline") == 0 && cmd.contains("deadline")) {
+            return DEADLINE;
+        } else if (cmd.indexOf("event") == 0 && cmd.contains("event")) {
+            return EVENT;
+        } else {
+            return -1;
+        }
+    }
+
+    public static String checkInput(String cmd, int taskID) {
         String task = new String();
+        String type = new String();
         try {
-            if (cmd.contains("todo")) {
+            if (taskID == TODO) {
+                type = "todo";
                 if(cmd.length() > 4) {
                     task = cmd.substring(5);
                 }
-                if(task.equals("")){
-                    throw new DukeException(" ☹ OOPS!!! " +
-                        "The description of a todo cannot be empty.");
-                }
-                ToDo newToDo = new ToDo(task);
-                taskList.addTask(newToDo);
-                System.out.println("Got it. I've added this task: ");
-                System.out.println(newToDo);
-            } else if (cmd.contains("deadline")) {
+            } else if (taskID == DEADLINE) {
+                type = "deadline";
                 int seg = cmd.indexOf("/");
                 if(cmd.length() > 8 && seg != -1) {
                     task = cmd.substring(9, seg);
                 }
-                if(task.equals("")){
-                    throw new DukeException(" ☹ OOPS!!! " +
-                        "The description of a deadline cannot be empty.");
-                }
-                String by = cmd.substring(seg + 4);
-                Deadline newDeadline = new Deadline(task, by);
-                taskList.addTask(newDeadline);
-                System.out.println("Got it. I've added this task: ");
-                System.out.println(newDeadline);
-            } else if (cmd.contains("event")) {
+            } else {
+                type = "event";
                 int seg = cmd.indexOf("/");
                 if(cmd.length() > 5 && seg != -1) {
                     task = cmd.substring(6, seg);
-                } else if (task.equals("")) {
-                    throw new DukeException(" ☹ OOPS!!! " +
-                        "The description of a event cannot be empty.");
                 }
-                String at = cmd.substring(seg + 4);
-                Event newEvent = new Event(task, at);
-                taskList.addTask(newEvent);
-                System.out.println("Got it. I've added this task: ");
-                System.out.println(newEvent);
-            } else {
-                throw new
-                    DukeException("☹ OOPS!!! I'm sorry, " +
-                    "but I don't know what that means :-(");
             }
-            System.out.println("Now you have "
-                + taskList.getSize()
-                + " tasks in the list.");
+            if(task.equals("")){
+                throw new DukeException(" ☹ OOPS!!! " +
+                    "The description of a " + type + " cannot be empty.");
+            }
         } catch (DukeException de) {
             System.out.println(de.getMessage());
         }
-        return taskList;
+        return task;
+    }
+
+    public static void performAddTask(TaskList taskList, Task task) {
+        taskList.addTask(task);
+        System.out.println("Got it. I've added this task: ");
+        System.out.println(task);
+    }
+
+    public static void printTotalTasks(TaskList tasklist) {
+        System.out.println("Now you have "
+                + tasklist.getSize()
+                + " tasks in the list.");
     }
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,11 +1,50 @@
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class TaskList {
 
     public ArrayList<Task> tasksList;
+    public File fileObject;
+    public FileWriter fileWriter;
 
-    public TaskList(){
+    public TaskList(String directory){
         this.tasksList = new ArrayList<>();
+        this.fileObject = createFile(directory);
+    }
+
+    public File createFile(String directory) {
+        try {
+            File fileObject = new File(directory);
+            if (fileObject.createNewFile()) {
+                System.out.println("New file created: " 
+                    + fileObject.getName());
+            } else {
+                System.out.println("The file exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+        return fileObject;
+    }
+
+    public void writeFile(String text) {
+        try {
+            FileWriter fileWriter = new FileWriter(fileObject.getName());
+            fileWriter.write(text);
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public void setTaskDone(int index) {
+        this.getTask(index - 1).setCompleted();
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println(this.getTask(index - 1));
     }
 
     /**
@@ -42,7 +81,13 @@ public class TaskList {
      * @param  index task index.
      */
     public void removeTask(int index){
+        Task temp = this.getTask(index - 1);
         tasksList.remove(index - 1);
+        System.out.println("Noted. I've removed this task: ");
+        System.out.println(temp);
+        System.out.println("Now you have "
+            + this.getSize()
+            + " tasks in the list.");
     }
 
     /**
