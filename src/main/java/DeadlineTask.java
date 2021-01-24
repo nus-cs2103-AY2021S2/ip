0d943d119ff33eda204628d44cd98eff59bd9e7b
zoeykobe <47862816+zoeykobe@users.noreply.github.com>
2021-01-25 01:01:34 +0800

    Revert "Level 8. Dates and Times"

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -1,18 +1,14 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-
 public class DeadlineTask extends Task {
 
-  protected LocalDate date;
+  protected String by;
 
-  public DeadlineTask(String name, LocalDate date) {
+  public DeadlineTask(String name, String by) {
     super(name);
-    this.date = date;
+    this.by = by;
   }
 
   @Override
   public String toString() {
-    return "[D]" + super.toString() + " (by:" + this.date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)) + ")";
+    return "[D]" + super.toString() + " (by:" + this.by + ")";
   }
 }
