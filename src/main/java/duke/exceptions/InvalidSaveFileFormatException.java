b9c05c0f6446a9a530a8f91fbcee7e46bcecae03
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-24 21:05:54 +0800

    Add javadoc for InvalidSaveFileFormatException.java

--- src/main/java/duke/exceptions/InvalidSaveFileFormatException.java
+++ src/main/java/duke/exceptions/InvalidSaveFileFormatException.java
@@ -1,6 +1,13 @@
 package duke.exceptions;
 
+/**
+ * Error class for invalid content format in save files.
+ */
 public class InvalidSaveFileFormatException extends DukeException {
+    /**
+     * Constructs a InvalidSaveFileFormatException with specific message
+     * for invalid content format in save files.
+     */
     public InvalidSaveFileFormatException() {
         super("\tInvalid content format in save file.\t");
     }
