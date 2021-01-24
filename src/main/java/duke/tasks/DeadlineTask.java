1b2f2700fe10c2c47a9dcc80523695dc49f7b014
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-24 21:06:45 +0800

    Add javadoc for DeadlineTask.java

--- src/main/java/duke/tasks/DeadlineTask.java
+++ src/main/java/duke/tasks/DeadlineTask.java
@@ -4,37 +4,63 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Responsible for containing deadline tasks.
+ */
 public class DeadlineTask extends Task {
     protected LocalDate date;
     protected LocalTime time;
 
+    /**
+     * Constructs a DeadlineTask with the given description and deadline.
+     *
+     * @param description Description of task.
+     * @param date Deadline of task.
+     */
     public DeadlineTask(String description, LocalDate date) {
         super(description, 'D');
         this.date = date;
         this.time = null;
     }
 
+    /**
+     * Constructs a DeadlineTask with the given description, deadline and time.
+     *
+     * @param description Description of task.
+     * @param date Deadline of task.
+     * @param time Deadline timing of task.
+     */
     public DeadlineTask(String description, LocalDate date, LocalTime time) {
         super(description, 'D');
         this.date = date;
         this.time = time;
     }
 
-    public String getDateSaveString() {
+    private String getDateSaveString() {
         return this.date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
-    public String getTimeSaveString() {
+    private String getTimeSaveString() {
         return this.time == null
         ? ""
         : " | " + this.time.format(DateTimeFormatter.ofPattern("HHmm"));
     }
 
+    /**
+     * Returns the format of string of task to be saved.
+     *
+     * @return String of task to be saved.
+     */
     @Override
     public String getSaveString() {
         return super.getSaveString() + " | " + getDateSaveString() + getTimeSaveString();
     }
 
+    /**
+     * Returns the string of the task.
+     *
+     * @return string of the task.
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: "
