76af3373bec7c4adb22ba6d9b42d513149b84c21
SiTingST <33442908+SiTingST@users.noreply.github.com>
2021-01-24 20:37:21 +0800

    Add Level 7

--- src/main/java/duke/Events.java
+++ src/main/java/duke/Events.java
@@ -1,12 +1,24 @@
+package duke;
+
 public class Events extends Task {
 
     private String date;
   //  private String startTime;
  //   private String endTime;
 
+    public Events(String title, Boolean b, String data) {
+        super(title, b);
+        this.date = data;
+    }
+
     public Events(String title, String date) {
         super(title);
         this.date = date;
+    }
+
+    @Override
+    public String changeFormat(){
+        return "E" + super.changeFormat() + ":" + this.date;
     }
 
     @Override
---
+++ src/main/java/duke/Task.java
@@ -0,0 +1,62 @@
+package duke;
+
+public class Task {
+    protected String title;
+    protected boolean status;
+
+    public Task(){
+
+    }
+
+    public Task(String title){
+        this.title = title;
+        this.status = false;
+    }
+
+    public Task(String title, boolean b){
+        this.title = title;
+         this.status = b;
+    }
+
+    public void setCompleted(){
+        this.status = true;
+    }
+
+    public String isCompleted(){
+        return (this.status ? "\u2718" : " ");
+    }
+
+    public String getTaskName(){
+        return this.title;
+    }
+
+    public String changeFormat(){
+        return ":" +  this.status + ":" + this.getTaskName();
+    }
+
+    public Task changeToTaskFormat(String string_task) {
+
+        if(string_task.charAt(0) == 'T'){
+            String[] tasks = string_task.split(":");
+            System.out.println(tasks[1]);
+            System.out.println(tasks[2]);
+            return new ToDos(tasks[2], Boolean.parseBoolean(tasks[1]));
+
+        } else if(string_task.charAt(0) == 'D'){
+            String[] tasks = string_task.split(":");
+            return new Deadlines(tasks[2], Boolean.parseBoolean(tasks[1]), tasks[3]);
+
+        }else if(string_task.charAt(0) == 'E'){
+            String[] tasks = string_task.split(":");
+            return new Events(tasks[2], Boolean.parseBoolean(tasks[1]), tasks[3]);
+        }
+        else{
+            return new Task();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.isCompleted() + "] " + this.getTaskName();
+    }
+}
