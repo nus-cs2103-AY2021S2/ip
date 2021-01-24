510891a040a4b41d68ebbe6ad4302c16477aa863
SiTingST <33442908+SiTingST@users.noreply.github.com>
2021-01-24 21:41:54 +0800

    Added Level 8

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -1,16 +1,22 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Events extends Task {
 
-    private String date;
-  //  private String startTime;
- //   private String endTime;
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
 
-    public Events(String title, String date) {
+    public Events(String title, LocalDate date, LocalTime startTime, LocalTime endTime) {
         super(title);
         this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 
     @Override
     public String toString() {
-        return  "[E]" + super.toString() + "(at:" + this.date + ")";
+        return  "[E]" + super.toString() + "(at:" + this.date + " from " + this.startTime + " to"
+                + " " + this.endTime + ")";
     }
 }
