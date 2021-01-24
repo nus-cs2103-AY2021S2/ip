ede04756c309c38add99f2b7c1f7929dc3af5dce
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-24 21:07:11 +0800

    Add javadoc for EventTask.java

--- src/main/java/duke/tasks/EventTask.java
+++ src/main/java/duke/tasks/EventTask.java
@@ -4,11 +4,20 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Responsible for containing event tasks.
+ */
 public class EventTask extends Task {
     protected LocalDate date;
     protected LocalTime startTime;
     protected LocalTime endTime;
 
+    /**
+     * Constructs a EventTask with the given description and date.
+     *
+     * @param description Description of the event.
+     * @param date Date of the event.
+     */
     public EventTask(String description, LocalDate date) {
         super(description, 'E');
         this.date = date;
@@ -16,6 +25,13 @@ public class EventTask extends Task {
         this.endTime = null;
     }
 
+    /**
+     * Constructs a EventTask with the given description, date and starting time.
+     *
+     * @param description Description of the event.
+     * @param date Date of the event.
+     * @param startTime Starting time of the event.
+     */
     public EventTask(String description, LocalDate date, LocalTime startTime) {
         super(description, 'E');
         this.date = date;
@@ -23,6 +39,14 @@ public class EventTask extends Task {
         this.endTime = null;
     }
 
+    /**
+     * Constructs a EventTask with the given description, date, starting and ending time.
+     *
+     * @param description Description of the event.
+     * @param date Date of the event.
+     * @param startTime Starting time of the event.
+     * @param endTime Ending time of the event.
+     */
     public EventTask(String description, LocalDate date, LocalTime startTime, LocalTime endTime) {
         super(description, 'E');
         this.date = date;
@@ -30,28 +54,38 @@ public class EventTask extends Task {
         this.endTime = endTime;
     }
 
-    public String getDateSaveString() {
+    private String getDateSaveString() {
         return this.date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
-    public String getStartTimeString() {
+    private String getStartTimeString() {
         return this.startTime == null
                 ? ""
                 : " | " + this.startTime.format(DateTimeFormatter.ofPattern("HHmm"));
     }
 
-    public String getEndTimeString() {
+    private String getEndTimeString() {
         return this.endTime == null
                 ? ""
                 : " | " + this.endTime.format(DateTimeFormatter.ofPattern("HHmm"));
     }
 
+    /**
+     * Returns the format of string of event to be saved.
+     *
+     * @return String of event to be saved.
+     */
     @Override
     public String getSaveString() {
         return super.getSaveString() + " | " + getDateSaveString()
                 + getStartTimeString() + getEndTimeString();
     }
 
+    /**
+     * Returns the string of the event.
+     *
+     * @return string of the event.
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (at: "
