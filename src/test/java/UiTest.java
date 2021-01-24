13e3fca1756c417702bac6601f9380aaa6cc2787
mabel-kang <e0425446@u.nus.edu>
2021-01-24 22:52:46 +0800

    Complete A-JUnit

---
+++ src/test/java/UiTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+
+public class UiTest {
+
+    @Test
+    public void testResponseToList() {
+         PrintStream out = System.out;
+         ByteArrayOutputStream content = new ByteArrayOutputStream();
+         System.setOut(new PrintStream(content));
+         Ui ui = new Ui();
+         ui.responseToList(0);
+
+         assertEquals("Your list is currently empty! Let's start adding tasks!", content.toString()
+                .trim());
+
+         System.setOut(out);
+    }
+    
+
+}
