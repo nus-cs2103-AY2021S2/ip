de8c91449d60e36c2a59c234f1cf409a22010ced
Ivan Tan <ivantjh97@gmail.com>
2021-01-26 00:52:37 +0800

    Add tests for DateTime

--- src/test/java/duke/util/DateTimeTest.java
+++ src/test/java/duke/util/DateTimeTest.java
@@ -1,4 +1,22 @@
 package duke.util;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DateTimeTest {
+    @Test
+    void dateTime_acceptedInputs_noExceptionThrown() {
+        assertDoesNotThrow(() -> {
+            new DateTime("05/06/2021");
+            new DateTime("05/06/2021 1250");
+        });
+    }
+
+    @Test
+    void toString_acceptedInputs_printsCorrectly() {
+        assertEquals(new DateTime("12/12/2020").toString(), "12 Dec 2020");
+        assertEquals(new DateTime("06/08/2021 2359").toString(), "2359 06 Aug 2021");
+    }
 }
