9a437617a43fbdd9121b4c32dc3053905a00eaea
nhzaci <warrior.killz@gmail.com>
2021-01-26 00:47:27 +0800

    Update coding standard to follow guide

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,25 +2,25 @@ import controllers.Ui;
 import models.Storage;
 
 public class Duke {
-  /** Constant to store the database path for Duke's commands */
-  private static final String DATABASE_FILE_PATH = "data/duke.txt";
-  /** Constant storing database directory path */
-  private static final String DATABASE_DIRECTORY_PATH = "data/";
-  /** Storage object to be passed to AppController for reading / writing to db */
-  private final Storage storage;
+    /** Constant to store the database path for Duke's commands */
+    private static final String DATABASE_FILE_PATH = "data/duke.txt";
+    /** Constant storing database directory path */
+    private static final String DATABASE_DIRECTORY_PATH = "data/";
+    /** Storage object to be passed to AppController for reading / writing to db */
+    private final Storage storage;
 
-  public Duke(String filePath, String directoryPath) {
-    this.storage = new Storage(filePath, directoryPath);
-  }
+    public Duke(String filePath, String directoryPath) {
+        this.storage = new Storage(filePath, directoryPath);
+    }
 
-  public static void main(String[] args) {
-    new Duke(DATABASE_FILE_PATH, DATABASE_DIRECTORY_PATH).run();
-  }
+    public static void main(String[] args) {
+        new Duke(DATABASE_FILE_PATH, DATABASE_DIRECTORY_PATH).run();
+    }
 
-  public void run() {
-    Ui appController = new Ui(this.storage);
-    // start app logic
-    appController.start();
+    public void run() {
+        Ui appController = new Ui(this.storage);
+        // start app logic
+        appController.start();
 
-  }
+    }
 }
--- src/test/java/controllers/TaskListTest.java
+++ src/test/java/controllers/TaskListTest.java
@@ -18,101 +18,103 @@ import java.util.List;
 import java.util.Optional;
 
 public class TaskListTest {
-    private final TaskList taskList = new TaskList(new ArrayList<>());
+  private final TaskList taskList = new TaskList(new ArrayList<>());
 
-    @Test
-    void addTask() {
-        // command to pass into parser
-        String command = "todo hello world";
-        // initialise parser
-        Parser parser = new Parser(Arrays.asList(command.split(" ")));
-        // get command args from parser
-        List<String> commandArgs = null;
-        try {
-            commandArgs = parser.getCommandArgs();
-        } catch (DukeCommandNotFoundException e) {
-            e.printStackTrace();
-        }
-        // pass commandArgs into TasksList to create todo
-        List<Optional<? extends Todo>> todosList = null;
-        try {
-            assert commandArgs != null;
-            todosList = taskList.addTodos(commandArgs).getTodosList();
-        } catch (DukeBlankTaskException e) {
-            e.printStackTrace();
-        }
-        // get raw message of one item in todosList
-        assert todosList != null;
-        String rawMessage = todosList.get(0).map(Todo::getRawMessage).orElse("Something went wrong");
-        // rawMessage should be "hello world"
-        assertEquals("hello world", rawMessage);
+  @Test
+  void addTask() {
+    // command to pass into parser
+    String command = "todo hello world";
+    // initialise parser
+    Parser parser = new Parser(Arrays.asList(command.split(" ")));
+    // get command args from parser
+    List<String> commandArgs = null;
+    try {
+      commandArgs = parser.getCommandArgs();
+    } catch (DukeCommandNotFoundException e) {
+      e.printStackTrace();
     }
+    // pass commandArgs into TasksList to create todo
+    List<Optional<? extends Todo>> todosList = null;
+    try {
+      assert commandArgs != null;
+      todosList = taskList.addTodo(commandArgs).getTodosList();
+    } catch (DukeBlankTaskException e) {
+      e.printStackTrace();
+    }
+    // get raw message of one item in todosList
+    assert todosList != null;
+    String rawMessage = todosList.get(0).map(Todo::getRawMessage).orElse("Something went wrong");
+    // rawMessage should be "hello world"
+    assertEquals("hello world", rawMessage);
+  }
 
-    @Test
-    void addDeadline() {
-        // command to pass into parser
-        String command = "deadline finish hello world /by 21/12/2020 2359";
-        // initialise parser
-        Parser parser = new Parser(Arrays.asList(command.split(" ")));
-        // get command args from parser
-        List<String> commandArgs = null;
-        try {
-            commandArgs = parser.getCommandArgs();
-        } catch (DukeCommandNotFoundException e) {
-            e.printStackTrace();
-        }
-        // pass commandArgs into TasksList to create todo
-        List<Optional<? extends Todo>> todosList = null;
-        try {
-            assert commandArgs != null;
-            try {
-                todosList = taskList.addDeadline(commandArgs).getTodosList();
-            } catch (DukeBlankDetailsException | DukeDateTimeParseException e) {
-                e.printStackTrace();
-            }
-        } catch (DukeBlankTaskException e) {
-            e.printStackTrace();
-        }
-        // get raw message of one item in todosList
-        assert todosList != null;
-        String rawMessage = todosList.get(0).map(Todo::getRawMessage).orElse("Something went wrong");
-        String deadlineString = todosList.get(0).map(todo -> ((Deadline) todo).getDeadline()).orElse("Something went wrong");
-        // rawMessage should be "hello world"
-        assertEquals("finish hello world", rawMessage);
-        assertEquals("21/12/2020 2359", deadlineString);
+  @Test
+  void addDeadline() {
+    // command to pass into parser
+    String command = "deadline finish hello world /by 21/12/2020 2359";
+    // initialise parser
+    Parser parser = new Parser(Arrays.asList(command.split(" ")));
+    // get command args from parser
+    List<String> commandArgs = null;
+    try {
+      commandArgs = parser.getCommandArgs();
+    } catch (DukeCommandNotFoundException e) {
+      e.printStackTrace();
     }
+    // pass commandArgs into TasksList to create todo
+    List<Optional<? extends Todo>> todosList = null;
+    try {
+      assert commandArgs != null;
+      try {
+        todosList = taskList.addDeadline(commandArgs).getTodosList();
+      } catch (DukeBlankDetailsException | DukeDateTimeParseException e) {
+        e.printStackTrace();
+      }
+    } catch (DukeBlankTaskException e) {
+      e.printStackTrace();
+    }
+    // get raw message of one item in todosList
+    assert todosList != null;
+    String rawMessage = todosList.get(0).map(Todo::getRawMessage).orElse("Something went wrong");
+    String deadlineString = todosList.get(0).map(todo -> ((Deadline) todo).getDeadline())
+        .orElse("Something went wrong");
+    // rawMessage should be "hello world"
+    assertEquals("finish hello world", rawMessage);
+    assertEquals("21/12/2020 2359", deadlineString);
+  }
 
-    @Test
-    void addEvent() {
-        // command to pass into parser
-        String command = "event say hello world /at 21/12/2021 1300";
-        // initialise parser
-        Parser parser = new Parser(Arrays.asList(command.split(" ")));
-        // get command args from parser
-        List<String> commandArgs = null;
-        try {
-            commandArgs = parser.getCommandArgs();
-        } catch (DukeCommandNotFoundException e) {
-            e.printStackTrace();
-        }
-        // pass commandArgs into TasksList to create todo
-        List<Optional<? extends Todo>> todosList = null;
-        try {
-            assert commandArgs != null;
-            try {
-                todosList = taskList.addEvent(commandArgs).getTodosList();
-            } catch (DukeBlankDetailsException | DukeDateTimeParseException e) {
-                e.printStackTrace();
-            }
-        } catch (DukeBlankTaskException e) {
-            e.printStackTrace();
-        }
-        // get raw message of one item in todosList
-        assert todosList != null;
-        String rawMessage = todosList.get(0).map(Todo::getRawMessage).orElse("Something went wrong");
-        String dateTimeString = todosList.get(0).map(todo -> ((Event) todo).getEventTime()).orElse("Something went wrong");
-        // rawMessage should be "hello world"
-        assertEquals("say hello world", rawMessage);
-        assertEquals("21/12/2021 1300", dateTimeString);
+  @Test
+  void addEvent() {
+    // command to pass into parser
+    String command = "event say hello world /at 21/12/2021 1300";
+    // initialise parser
+    Parser parser = new Parser(Arrays.asList(command.split(" ")));
+    // get command args from parser
+    List<String> commandArgs = null;
+    try {
+      commandArgs = parser.getCommandArgs();
+    } catch (DukeCommandNotFoundException e) {
+      e.printStackTrace();
     }
+    // pass commandArgs into TasksList to create todo
+    List<Optional<? extends Todo>> todosList = null;
+    try {
+      assert commandArgs != null;
+      try {
+        todosList = taskList.addEvent(commandArgs).getTodosList();
+      } catch (DukeBlankDetailsException | DukeDateTimeParseException e) {
+        e.printStackTrace();
+      }
+    } catch (DukeBlankTaskException e) {
+      e.printStackTrace();
+    }
+    // get raw message of one item in todosList
+    assert todosList != null;
+    String rawMessage = todosList.get(0).map(Todo::getRawMessage).orElse("Something went wrong");
+    String dateTimeString =
+        todosList.get(0).map(todo -> ((Event) todo).getEventTime()).orElse("Something went wrong");
+    // rawMessage should be "hello world"
+    assertEquals("say hello world", rawMessage);
+    assertEquals("21/12/2021 1300", dateTimeString);
+  }
 }
