dfed7bc2da8d558a55437891a3b7cfb63c653976
nhzaci <warrior.killz@gmail.com>
2021-01-25 23:55:49 +0800

    Add addEvent() test to TaskListTest

--- src/test/java/controllers/TaskListTest.java
+++ src/test/java/controllers/TaskListTest.java
@@ -6,6 +6,7 @@ import exceptions.DukeCommandNotFoundException;
 import exceptions.DukeDateTimeParseException;
 import models.Parser;
 import models.Todo;
+import models.Event;
 import models.Deadline;
 import org.junit.jupiter.api.Test;
 
@@ -79,5 +80,39 @@ public class TaskListTest {
         // rawMessage should be "hello world"
         assertEquals("finish hello world", rawMessage);
         assertEquals("21/12/2020 2359", deadlineString);
+    }
+
+    @Test
+    void addEvent() {
+        // command to pass into parser
+        String command = "event say hello world /at 21/12/2021 1300";
+        // initialise parser
+        Parser parser = new Parser(Arrays.asList(command.split(" ")));
+        // get command args from parser
+        List<String> commandArgs = null;
+        try {
+            commandArgs = parser.getCommandArgs();
+        } catch (DukeCommandNotFoundException e) {
+            e.printStackTrace();
+        }
+        // pass commandArgs into TasksList to create todo
+        List<Optional<? extends Todo>> todosList = null;
+        try {
+            assert commandArgs != null;
+            try {
+                todosList = taskList.addEvent(commandArgs).getTodosList();
+            } catch (DukeBlankDetailsException | DukeDateTimeParseException e) {
+                e.printStackTrace();
+            }
+        } catch (DukeBlankTaskException e) {
+            e.printStackTrace();
+        }
+        // get raw message of one item in todosList
+        assert todosList != null;
+        String rawMessage = todosList.get(0).map(Todo::getRawMessage).orElse("Something went wrong");
+        String dateTimeString = todosList.get(0).map(todo -> ((Event) todo).getEventTime()).orElse("Something went wrong");
+        // rawMessage should be "hello world"
+        assertEquals("say hello world", rawMessage);
+        assertEquals("21/12/2021 1300", dateTimeString);
     }
 }
