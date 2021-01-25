c3380d560b7af4d441e80eb03cf59292cb876551
Dick Jessen William <jessenwilliam87@gmail.com>
2021-01-25 22:41:30 +0800

    Refactor to prepare for level 7

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import static java.lang.Integer.parseInt;
-
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -17,98 +16,129 @@ public class Duke {
         System.out.println("Duke: " + hello);
         String cmd = sc.nextLine();
         String[] pre = cmd.split("\\s+");
-        while(cmd.equals("bye") == false){
-            if(cmd.equals("list") == true) {
-                if(tasks.size() > 0) {
-                    System.out.println("Here are the tasks in your list:\n");
-                    for (int i = 1; i <= tasks.size(); i++) {
-                        System.out.println(i + ". " + tasks.get(i-1));
-                    }
-                }
-                else{
-                    System.out.println("No tasks");
-                }
+        while(!cmd.equals("bye")) {
+            int n;
+            switch (pre[0]) {
+                case "list":
+                    printTasks(tasks);
+                    break;
+                case "done":
+                    n = parseInt(pre[1]) - 1;
+                    doneTask(tasks, n);
+                    break;
+                case "delete":
+                    n = parseInt(pre[1]) - 1;
+                    deleteTask(tasks, n);
+                    break;
+                case "deadline":
+                    addDeadline(tasks, cmd);
+                    break;
+                case "event":
+                    addEvent(tasks, cmd);
+                    break;
+                case "todo":
+                    addToDo(tasks, pre);
+                    break;
+                default:
+                    addError();
+                    break;
             }
-            else{
-                try {
-                    if (pre[0].equals("done")) {
-                        if (parseInt(pre[1]) <= tasks.size()) {
-                            tasks.set(parseInt(pre[1]) - 1, tasks.get(parseInt(pre[1]) - 1).finish());
-                            System.out.println("Nice! I've marked this task as done: \n");
-                            System.out.println(tasks.get(parseInt(pre[1]) - 1));
-                        } else {
-                            System.out.println("error");
-                        }
-                    }
-                    else if(pre[0].equals("delete")){
-                        if(parseInt(pre[1]) <= tasks.size()){
-                            System.out.println("Noted. I've removed this task: ");
-                            System.out.println(tasks.get(parseInt(pre[1])-1));
-                            tasks.remove(parseInt(pre[1])-1);
-                            System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
 
-                        }
-                        else{
-                            System.out.println("error");
-                        }
-                    }
-                    else if (pre[0].equals("deadline")) {
-                        String s = "";
-                        String t = "";
-                        String[] pre2 = cmd.split("/by");
-                        try {
-                            s = pre2[0];
-                            t = pre2[1];
-                            System.out.println("Got it. I've added this task:");
-                            tasks.add(new Deadline(s, false, t));
-                            System.out.println(tasks.get(tasks.size() - 1));
-                            System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
-                        }
-                        catch (Exception e){
-                            System.out.println("Please enter a description for deadline");
-                        }
-                    } else if (pre[0].equals("event")) {
-                        String s = "";
-                        String t = "";
-                        String[] pre2 = cmd.split("/at");
-                        try {
-                            s = pre2[0];
-                            t = pre2[1];
-                            System.out.println("Got it. I've added this task:");
-                            tasks.add(new Deadline(s, false, t));
-                            System.out.println(tasks.get(tasks.size() - 1));
-                            System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
-                        }
-                        catch (Exception e){
-                            System.out.println("Please enter a description for event");
-                        }
-                    }
-                    else if (pre[0].equals("todo")) {
-                            String s = "";
-                            for (int i = 1; i < pre.length; i++) {
-                                s =
-                                        s + pre[i] + " ";
-                            }
-                            if (!s.equals("")) {
-                                tasks.add(new Todo(s,false));
-                                System.out.println("Got it. I've added this task:");
-                                System.out.println(tasks.get(tasks.size() - 1));
-                                System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
-                            } else {
-                                System.out.println("Please enter a description for todo");
-                            }
-                    }
-                    else{
-                        System.out.println("Command not understood");
-                    }
-                }
-                catch (Exception e){
-                    System.out.println("Invalid command");
-                }
-            }
             cmd = sc.nextLine();
             pre = cmd.split("\\s+");
         }
-        System.out.println("Duke: "+goodbye);
+        System.out.println("Duke: " + goodbye);
+    }
+
+    private static void deleteTask(ArrayList<Task> tasks, int n) {
+        try {
+            Task t = tasks.get(n);
+            System.out.println("Noted. I've removed this task: ");
+            tasks.remove(n);
+            System.out.println(t);
+            int size = tasks.size();
+            printTotalTasks(size);
+        }
+        catch(IndexOutOfBoundsException e){
+            System.out.println("Wrong task ID");
+        }
+    }
+
+    private static void printTotalTasks(int size) {
+        if(size != 1) {
+            System.out.printf("Now you have %d tasks in the list.\n", size);
+        }
+        else{
+            System.out.printf("Now you have %d task in the list.\n", size);
+        }
+    }
+
+    private static void doneTask(ArrayList<Task> tasks, int n) {
+        try {
+            tasks.set(n, tasks.get(n).finish());
+            System.out.println("Nice! I've marked this task as done: \n");
+            System.out.println(tasks.get(n));
+        }
+        catch(IndexOutOfBoundsException e){
+            System.out.println("Wrong task ID");
+        }
+    }
+
+    private static void addError() {
+        System.out.println("Command not understood");
+    }
+
+    private static void addToDo(ArrayList<Task> tasks, String[] pre) {
+        String s = "";
+        for (int i = 1; i < pre.length; i++) {
+            s = s + pre[i] + " ";
+        }
+        if (!s.equals("")) {
+            tasks.add(new Todo(s,false));
+            System.out.println("Got it. I've added this task:");
+            System.out.println(tasks.get(tasks.size() - 1));
+            printTotalTasks(tasks.size());
+        }
+        else {
+            System.out.println("Please add a description for todo.");
+        }
+    }
+
+    private static void addEvent(ArrayList<Task> tasks, String cmd) {
+        String[] pre2 = cmd.split("/at");
+        try {
+            String s = pre2[0];
+            String t = pre2[1];
+            System.out.println("Got it. I've added this task:");
+            tasks.add(new Event(s, false, t));
+            System.out.println(tasks.get(tasks.size() - 1));
+            printTotalTasks(tasks.size());
+        }
+        catch (ArrayIndexOutOfBoundsException  e){
+            System.out.println("Please enter a description for event");
+        }
+    }
+
+    private static void addDeadline(ArrayList<Task> tasks, String cmd) {
+        String[] pre2 = cmd.split("/by");
+        try {
+            String s = pre2[0];
+            String t = pre2[1];
+            System.out.println("Got it. I've added this task:");
+            tasks.add(new Deadline(s, false, t));
+            System.out.println(tasks.get(tasks.size() - 1));
+            printTotalTasks(tasks.size());
+        }
+        catch (ArrayIndexOutOfBoundsException  e){
+            System.out.println("Please enter a description for deadline");
+        }
+    }
+
+    private static void printTasks(ArrayList<Task> tasks) {
+        System.out.println("Here are the tasks in your list:\n");
+        for (int i = 1; i <= tasks.size(); i++) {
+            System.out.println(i + ". " + tasks.get(i-1));
+        }
+
     }
 }
