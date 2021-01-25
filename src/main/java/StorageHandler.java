3a123ccbb5e8df54570db93b6bf667ee7b80cef9
Zeche <zx3110@gmail.com>
2021-01-25 10:49:17 +0800

    Add Read and write tasklist to file functionality
    * Add updateListFile method in ReadWriteHandler that updates task list
    file on command exectution
    * Add readListFromFile method in ReadWriteHandler that generates a new
    TaskList object from the task list file
    * Add strToTaskList method in TaskList to translate the string
    representation of a task list into a TaskList object

--- src/main/java/StorageHandler.java
+++
@@ -1,7 +0,0 @@
-public class StorageHandler {
-    private final String filepath;
-
-    public StorageHandler (String filepath) {
-        this.filepath = filepath;
-    }
-}
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TaskList {
     private List<Task> taskList;
@@ -44,7 +46,34 @@ public class TaskList {
      * @param i the index of the task to be deleted
      */
     public void deleteNthTask(int i) {
+        this.numTasks--;
         this.taskList.remove(i - 1);
+    }
+
+    public void strToTask(String strTask) throws MikeInvalidInputException {
+        Pattern p = Pattern.compile("(\\d)\\. \\[([TDE])\\] \\[([X ])\\] (.+) \\(at: (.+)\\)");
+        Matcher m = p.matcher(strTask);
+        m.find();
+
+        switch(m.group(2)) {
+            case "T":
+                this.addTaskToList(new TodoTask(m.group(4)));
+                break;
+
+            case "E":
+                this.addTaskToList(new EventTask(m.group(4), m.group(5)));
+                break;
+
+            case "D":
+                this.addTaskToList(new DeadlineTask(m.group(4), m.group(5)));
+                break;
+
+            default:
+                throw new MikeInvalidInputException("No such task type");
+        }
+        if (!m.group(3).equals(" ")) {
+            this.getNthTask(Integer.parseInt(m.group(1))).isDone();
+        }
     }
 
     @Override
