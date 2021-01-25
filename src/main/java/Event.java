2e55a5e037edc1a2ef95bfe12779421b68993a56
geraldfan <geraldfan@gmail.com>
2021-01-25 14:53:21 +0800

    Change event.java toString()

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -13,6 +13,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (" + date.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
+        return "[E]" + super.toString() + " (at: " + date.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
     }
 }
