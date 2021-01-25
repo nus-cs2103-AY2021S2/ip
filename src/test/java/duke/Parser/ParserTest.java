b2cf440dd3ceb9b9e928278d9b342833c69240ff
SiTingST <33442908+SiTingST@users.noreply.github.com>
2021-01-26 00:34:42 +0800

    Add J-units test

---
+++ src/test/java/duke/Parser/ParserTest.java
@@ -0,0 +1,2 @@
+package duke.Parser;public class ParserTest {
+}
---
+++ src/test/java/duke/TaskList/TaskListTest.java
@@ -0,0 +1,11 @@
+package duke;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+    @Test
+    public void addingTaskTest() {
+        assertEquals(2, 2);
+    }
+}
