0a371bdc419fb2f82f998f485b4576fe512fc0a0
david <dvdweien@gmail.com>
2021-01-25 09:08:26 +0800

    Add ability to understand dates
    Program dates are simply stored as strings with no ability for
    manipulation
    
    Understanding dates is important for functionality such as
    listing tasks on a particular day
    
    Dates are now parsed as LocalDate objects

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,37 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 class Event extends Task {
-	private String time;
+	private LocalDate time;
 
 	Event(String a, String b) {
 		super(a);
-		this.time = b;
+		this.time = LocalDate.parse(b);
+	}
+
+	Event(String a, String b, boolean c) {
+		super(a, c);
+		this.time = LocalDate.parse(b);
+	}
+
+	@Override
+	String saveName() {
+		return String.format("event1!1%s1!1%s1!1%b", super.getName(), this.time.toString(), super.getIsDone());
+	}
+
+	@Override
+	boolean onDay(String s) {
+		LocalDate day = LocalDate.parse(s);
+		if (day.equals(this.time)) {
+			return !super.getIsDone();
+		} else {
+			return false;
+		}
+		
 	}
 
 	@Override
 	public String toString() {
-		return String.format("[E]%s (at: %s)", super.toString(), this.time);
+		return String.format("[E]%s (at: %s)", super.toString(), this.time.format(DateTimeFormatter.ofPattern("d MMM yyyy")));
 	}
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,19 +1,40 @@
 class Task {
 	private String name;
-	private boolean done;
+	private boolean isDone;
 
 	Task(String s) {
 		this.name = s;
-		this.done = false;
+		this.isDone = false;
+	}
+
+	Task(String s, boolean c) {
+		this.name = s;
+		this.isDone = c;
 	}
 
 	void finish() {
-		this.done = true;
+		this.isDone = true;
+	}
+
+	String getName() {
+		return this.name;
+	}
+
+	boolean getIsDone() {
+		return this.isDone;
+	}
+
+	String saveName() {
+		return String.format("task1!1%s1!1%b", this.name, this.isDone);
+	}
+
+	boolean onDay(String s) {
+		return !this.isDone;
 	}
 
 	public String toString() {
 		String check = " ";
-		if (this.done) {
+		if (this.isDone) {
 			check = "X";
 		}
 		return String.format("[%s]  %s", check, this.name);
