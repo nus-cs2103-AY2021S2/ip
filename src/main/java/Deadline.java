098b7b4c0db57a54906ed590fe82d7f76377d762
Onn Wei Sin <onnwards@gmail.com>
2021-01-26 04:13:50 +0800

    Add datetime parsing for /at and /by

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
 
     public Deadline(String description, TaskType taskType, String by) {
         super(description, taskType);
-        this.by = by;
+        this.by = LocalDate.parse(by);
     }
 
     @Override
     public String toString() {
-        return super.toString() + " (by: " + this.by + ")";
+        return super.toString() + " (by: " + this.by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String at;
+    protected LocalDate at;
 
     public Event(String description, TaskType taskType, String at) {
         super(description, taskType);
-        this.at = at;
+        this.at = LocalDate.parse(at);
     }
 
     @Override
     public String toString() {
-        return super.toString() + " (at: " + this.at + ")";
+        return super.toString() + " (at: " + this.at.format(DateTimeFormatter.ofPattern("dd MMM yyyy")) + ")";
     }
 }
