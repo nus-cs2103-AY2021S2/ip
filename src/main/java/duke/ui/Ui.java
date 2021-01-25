9fb51bc23964e26b7aebbed05c194ce5b9785753
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-25 12:58:59 +0800

    Update Ui.java to meet coding standard

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -37,7 +37,7 @@ public class Ui {
         System.out.println("\t____________________________________________________________\n"
                 + "\tHere are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
-            System.out.println("\t" + (i + 1) + "." + tasks.get(i).toString());
+            System.out.println("\t" + (i + 1) + "." + tasks.getTask(i).toString());
         }
         System.out.println("\t____________________________________________________________\n");
     }
@@ -89,7 +89,7 @@ public class Ui {
     public void showDoneTask(TaskList tasks, int index) {
         System.out.println("\t____________________________________________________________\n"
                 + "\tNice! I've marked this task as done:\n\t\t"
-                + tasks.get(index - 1).toString() + "\n"
+                + tasks.getTask(index - 1).toString() + "\n"
                 + "\t____________________________________________________________\n");
     }
 
