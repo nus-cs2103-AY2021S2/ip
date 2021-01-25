bed34db092544da660a1abb637c7fdc42b3847a1
Vevek Selvam <vevekselvam@gmail.com>
2021-01-25 17:43:23 +0800

    Reformat code style for switch statements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,8 +12,8 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Duke {
-    public static String BOT_NAME = "Apollo the Robot";
-    public static String INDENTATION = "    ";
+    public final static String BOT_NAME = "Apollo the Robot";
+    public final static String INDENTATION = "    ";
     public static ArrayList<Task> taskList = new ArrayList<>();
 
     public static void printlnWithIndentation(String s) {
@@ -25,7 +25,7 @@ public class Duke {
         System.out.println(INDENTATION + line);
     }
 
-    public static void printBetweenLines(String ... strings) {
+    public static void printBetweenLines(String... strings) {
         printHorizontalLine();
 
         for (int i = 0; i < strings.length; i++) {
@@ -39,49 +39,49 @@ public class Duke {
         int numberOfTasks = taskList.size();
 
         switch (command) {
-            case TODO:
-                Task todo = new Todo(input);
-                taskList.add(todo);
-                numberOfTasks += 1;
-                printBetweenLines("Got it. I've added this task:",
-                        INDENTATION + todo.toString(),
-                        "Now you have " + numberOfTasks + " tasks in the list."
-                );
-                break;
-            case DEADLINE:
-                int indexOfBy = input.trim().indexOf("/by");
+        case TODO:
+            Task todo = new Todo(input);
+            taskList.add(todo);
+            numberOfTasks += 1;
+            printBetweenLines("Got it. I've added this task:",
+                    INDENTATION + todo.toString(),
+                    "Now you have " + numberOfTasks + " tasks in the list."
+            );
+            break;
+        case DEADLINE:
+            int indexOfBy = input.trim().indexOf("/by");
 
-                if (indexOfBy == 0) {
-                    throw new InvalidOptionException("DEADLINE");
-                }
+            if (indexOfBy == 0) {
+                throw new InvalidOptionException("DEADLINE");
+            }
 
-                String deadlineMessage = input.substring(0, indexOfBy);
-                String by = input.substring(indexOfBy + 4);
-                Task deadline = new Deadline(deadlineMessage, by);
-                taskList.add(deadline);
-                numberOfTasks += 1;
-                printBetweenLines("Got it. I've added this task:",
-                        INDENTATION + deadline.toString(),
-                        "Now you have " + numberOfTasks + " tasks in the list."
-                );
-                break;
-            case EVENT:
-                int indexOfAt = input.trim().indexOf("/at");
+            String deadlineMessage = input.substring(0, indexOfBy);
+            String by = input.substring(indexOfBy + 4);
+            Task deadline = new Deadline(deadlineMessage, by);
+            taskList.add(deadline);
+            numberOfTasks += 1;
+            printBetweenLines("Got it. I've added this task:",
+                    INDENTATION + deadline.toString(),
+                    "Now you have " + numberOfTasks + " tasks in the list."
+            );
+            break;
+        case EVENT:
+            int indexOfAt = input.trim().indexOf("/at");
 
-                if (indexOfAt == 0) {
-                    throw new InvalidOptionException("EVENT");
-                }
+            if (indexOfAt == 0) {
+                throw new InvalidOptionException("EVENT");
+            }
 
-                String eventMessage = input.substring(0, indexOfAt);
-                String at = input.substring(indexOfAt + 4);
-                Task event = new Event(eventMessage, at);
-                taskList.add(event);
-                numberOfTasks += 1;
-                printBetweenLines("Got it. I've added this task:",
-                        INDENTATION + event.toString(),
-                        "Now you have " + numberOfTasks + " tasks in the list."
-                );
-                break;
+            String eventMessage = input.substring(0, indexOfAt);
+            String at = input.substring(indexOfAt + 4);
+            Task event = new Event(eventMessage, at);
+            taskList.add(event);
+            numberOfTasks += 1;
+            printBetweenLines("Got it. I've added this task:",
+                    INDENTATION + event.toString(),
+                    "Now you have " + numberOfTasks + " tasks in the list."
+            );
+            break;
         }
     }
 
@@ -92,7 +92,7 @@ public class Duke {
             printlnWithIndentation("You have not added any tasks yet.");
         }
 
-        for(int i = 0; i < taskList.size(); i++ ) {
+        for (int i = 0; i < taskList.size(); i++) {
             int index = i + 1;
             printlnWithIndentation(index + ". " + taskList.get(i).toString());
         }
@@ -122,7 +122,7 @@ public class Duke {
         String input = s.trim();
         Commands command;
 
-        if(input.equals("")) {
+        if (input.equals("")) {
             return;
         }
 
@@ -135,50 +135,50 @@ public class Duke {
         }
 
         switch (command) {
-            case LIST:
-                listTasks(taskList);
+        case LIST:
+            listTasks(taskList);
+            break;
+        case DONE:
+            try {
+                doneTask(inputArr[1]);
                 break;
-            case DONE:
-                try {
-                    doneTask(inputArr[1]);
-                    break;
-                } catch (IndexOutOfBoundsException | NumberFormatException e) {
-                    throw new InvalidOptionException(command.name());
-                }
-            case DELETE:
-                try {
-                    deleteTask(inputArr[1]);
-                    break;
-                } catch (IndexOutOfBoundsException | NumberFormatException e) {
-                    throw new InvalidOptionException(command.name());
-                }
-            case BYE:
-                printlnWithIndentation("Bye. Hope to see you again soon!");
-                System.exit(0);
+            } catch (IndexOutOfBoundsException | NumberFormatException e) {
+                throw new InvalidOptionException(command.name());
+            }
+        case DELETE:
+            try {
+                deleteTask(inputArr[1]);
                 break;
-            case TODO:
-                try {
-                    addTask(command, inputArr[1], taskList);
-                } catch (IndexOutOfBoundsException e) {
-                    throw new InvalidOptionException(command.name());
-                }
-                break;
-            case DEADLINE:
-                try {
-                    addTask(command, inputArr[1], taskList);
-                } catch (IndexOutOfBoundsException e) {
-                    throw new InvalidOptionException(command.name());
-                }
-                break;
-            case EVENT:
-                try {
-                    addTask(command, inputArr[1], taskList);
-                } catch (IndexOutOfBoundsException e) {
-                    throw new InvalidOptionException(command.name());
-                }
-                break;
-            default:
-                printBetweenLines("Invalid command, please try again!");
+            } catch (IndexOutOfBoundsException | NumberFormatException e) {
+                throw new InvalidOptionException(command.name());
+            }
+        case BYE:
+            printlnWithIndentation("Bye. Hope to see you again soon!");
+            System.exit(0);
+            break;
+        case TODO:
+            try {
+                addTask(command, inputArr[1], taskList);
+            } catch (IndexOutOfBoundsException e) {
+                throw new InvalidOptionException(command.name());
+            }
+            break;
+        case DEADLINE:
+            try {
+                addTask(command, inputArr[1], taskList);
+            } catch (IndexOutOfBoundsException e) {
+                throw new InvalidOptionException(command.name());
+            }
+            break;
+        case EVENT:
+            try {
+                addTask(command, inputArr[1], taskList);
+            } catch (IndexOutOfBoundsException e) {
+                throw new InvalidOptionException(command.name());
+            }
+            break;
+        default:
+            printBetweenLines("Invalid command, please try again!");
         }
     }
 
@@ -187,12 +187,12 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
 
-        while(scanner.hasNext()) {
+        while (scanner.hasNext()) {
             String input = scanner.nextLine();
 
             try {
                 handleInput(input);
-            } catch(Exception e) {
+            } catch (Exception e) {
                 printBetweenLines("An error occurred:", e.getMessage());
             }
         }
