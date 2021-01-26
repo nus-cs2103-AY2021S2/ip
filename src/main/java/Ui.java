0db653e071f0479782e45188301c0aafe55db694
CSjiade <jdlian505@gmail.com>
2021-01-26 18:04:24 +0800

    Update remaining missing JavaDoc comments

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -4,12 +4,19 @@ public class Ui {
 
   }
 
+  /**
+   * Outputs the customied bye message when user inputs bye command.
+   */
+
   public void bye() {
     System.out.println(" ___________________________________________");
     System.out.println("Bye. Hope to see you again soon!");
     System.out.println(" ___________________________________________");
   }
 
+  /**
+   * Outputs the customised greeting message when Duke object is instiatied.
+   */
   public void greeting() {
     String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
