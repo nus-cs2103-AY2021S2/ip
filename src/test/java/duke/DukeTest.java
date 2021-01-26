2deba0e35aa2201f629eb09787b12755c19ea77e
xyzhang00 <>
2021-01-26 21:33:41 +0800

    Add JUnit tests

---
+++ src/test/java/duke/DukeTest.java
@@ -0,0 +1,22 @@
+package duke;
+
+import duke.tasks.Deadline;
+import org.junit.jupiter.api.Test;
+import duke.tasks.Todo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+public class DukeTest {
+    @Test
+    public void testTodoString() {
+        assertEquals("[T][\u2717] sleep", new Todo("sleep", false).toString());
+    }
+
+    @Test
+    public void testDeadlineString() {
+        assertEquals("[D][\u2717] sleep (by: Mar 29 2000)",
+                new Deadline("sleep", false, "2000-03-29").toString());
+    }
+}
