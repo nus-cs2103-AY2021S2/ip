a1acbaf368d218635cf5761021d93b1c40144df2
zhengruoxin <zhengruoxin99@gmail.com>
2021-01-26 23:48:24 +0800

    Resolve conflict in TaskManager

---
+++ src/main/java/TaskManager.java
@@ -0,0 +1,82 @@
+package duke;
+
+import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Encapsulates information of all tasks entered by user.
+ * Supports operations to modify and display stored tasks.
+ */
+public class TaskManager {
+	/** List of all tasks entered by user. */
+    private ArrayList<Task> store = new ArrayList<>();
+
+    /**
+     * Adds new task to list of all tasks.
+     */
+    public void addTask(Task t) {
+        this.store.add(t);
+    }
+
+    /**
+     * Removes task from list of all tasks.
+     */
+    public void deleteTask(int n) {
+        this.store.remove(n - 1);
+    }
+
+    /**
+     * Prints the list of all tasks and their statuses.
+     */
+    public void displayTasks() {
+    	int counter = 1;
+        for (Task elem: this.store) {
+            System.out.println(counter + ". " + elem.toString());
+            counter += 1;
+        }
+    }
+
+    /**
+     * Accesses task at target index and mark it as done.
+     */
+    public void taskDone(int n) {
+        this.store.get(n - 1).markAsDone();
+    }
+
+    /**
+     * Find out the total number of tasks.
+     * @return total number of tasks
+     */
+    public int taskVolume() {
+        return this.store.size();
+    }
+
+    /**
+     * Prints the line(s) that contain the element user is finding.
+     */
+    public void find(String item) {
+        int counter = 1;
+        for (Task t: this.store) {
+            if (t.toString().contains(item)) {
+                System.out.println(counter + ". " + t.toString());
+            }
+            counter += 1;
+        }
+    }
+
+    /**
+     * Saves the latest list and states of tasks to a file.
+     */
+    public void writeToDisk() {
+        try {
+            FileWriter fw = new FileWriter("tasklist/mytasks.txt");
+            for (Task t: this.store) {
+                fw.write(t.toString() + "\n");
+            }
+            fw.close();
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+    }
+}
