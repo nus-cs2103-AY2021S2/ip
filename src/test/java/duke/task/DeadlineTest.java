6cd8f88ef1c25cb798d7de5bb24bbaa10f09c2ec
Stratostorm <h_weicong@outlook.com>
2021-01-27 00:55:03 +0800

    Add some unit tests for Deadline and Todo

---
+++ src/test/java/duke/task/DeadlineTest.java
@@ -0,0 +1,17 @@
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+    @Test
+    public void fileString_correctDateNoTime_outputCorrectly() {
+        assertEquals("D | false | test | 2020-01-27", new Deadline("test", "2020-01-27").fileString());
+    }
+
+    @Test
+    public void fileString_correctDateWithTime_outputCorrectly() {
+        assertEquals("D | false | test | 2020-01-27 0800", new Deadline("test", "2020-01-27 0800").fileString());
+    }
+}
