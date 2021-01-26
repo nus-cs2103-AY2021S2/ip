a27fb4b24614a7f4414542910fea5011f9654df1
DineshMagesvaran <dineshs97@gmail.com>
2021-01-26 14:36:22 +0800

    Add ability to save task data in hard disk automatically

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -10,4 +10,12 @@ public class Deadline extends Task {
     public String toString() {
         return "[D]" + super.toString() + " (" + dateInfo + ")";
     }
+
+    public String getData() {
+        if (isDone == true) {
+            return "D!@#1!@#" + taskInfo + "!@#" + dateInfo;
+        } else {
+            return "D!@#0!@#" + taskInfo + "!@#" + dateInfo;
+        }
+    }
 }
