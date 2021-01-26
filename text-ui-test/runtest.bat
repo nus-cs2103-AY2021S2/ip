73d48775daa89c3cd09005a9123a0aa84177740e
Onn Wei Sin <onnwards@gmail.com>
2021-01-26 16:31:32 +0800

    Modify tests for A-MoreOOP

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -5,6 +5,10 @@ if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
+if exist ACTUAL2.TXT del ACTUAL2.TXT
+
+REM to test if app can run without savefile existing
+if exist ..\bin\data del ..\bin\data
 
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
@@ -16,6 +20,8 @@ REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
-
+REM to test the persistence of the savefile
+java -classpath ..\bin Duke < input2.txt > ACTUAL2.TXT
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
+FC ACTUAL2.TXT EXPECTED2.TXT
