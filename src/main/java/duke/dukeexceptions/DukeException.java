00edc65f029bbab8289efcaf18dd7f442a4a551c
jlxw48 <jlxw48@gmail.com>
2021-01-26 16:20:40 +0800

    Align coding standard for custom Exception classes

--- src/main/java/duke/dukeexceptions/DukeException.java
+++ src/main/java/duke/dukeexceptions/DukeException.java
@@ -4,6 +4,7 @@ public abstract class DukeException extends Exception {
     public DukeException(String message) {
         super(message);
     }
+    
     public DukeException(String message, Throwable cause) {
         super(message, cause);
     }
