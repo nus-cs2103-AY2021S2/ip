2528630cdb4d0c683db90d570cfc264a349c939b
Lim Li Quan <linliquan1996@Hotmail.com>
2021-01-26 15:06:42 +0800

    Add save method for task format when saved to text file

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,10 +1,17 @@
 public class Todo extends Task {
+    private static final char SYMBOL = 'T';
+
     public Todo(String desc) {
         super(desc);
     }
 
     @Override
     public String toString() {
-        return String.format("[T] %s", super.toString());
+        return String.format("[%c] %s",SYMBOL, super.toString());
+    }
+
+    @Override
+    public String save() {
+        return String.format("%c,%s\n", SYMBOL, super.save());
     }
 }
