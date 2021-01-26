aba6cbf866f64572f256ac73de55626e654abbe6
gycc7253 <guo7253@gmail.com>
2021-01-26 10:58:59 +0800

    A-CodingStandard2

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -3,32 +3,32 @@ package main.java;
 import main.java.entity.Task;
 import java.util.List;
 import java.util.Scanner;
-public class Ui {
 
+public class Ui {
     private Scanner sc;
-    static final String greetings = "Dear user, welcome to the world of duke!";
-    static final String prefix = "    ";
-    static final String logo = " ____        _        \n"
+    static final String GREETINGS = "Dear user, welcome to the world of duke!";
+    static final String PREFIX = "    ";
+    static final String LOGO = " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
-    static final String displayEmptyList = prefix + "Currently there is nothing on your list.";
-    static final String added = prefix + "Got it. I've added this task:";
-    static final String markAsDone = prefix + "Nice! I've mark this task as done:";
-    static final String removeTask = prefix + "Noted, I've removed this task:";
+    static final String DISPLAY_EMPTY_LIST = PREFIX + "Currently there is nothing on your list.";
+    static final String DISPLAY_ADDED = PREFIX + "Got it. I've added this task:";
+    static final String DISPLAY_DONE = PREFIX + "Nice! I've mark this task as done:";
+    static final String DISPLAY_RENAME = PREFIX + "Noted, I've removed this task:";
 
     public Ui() {
         sc = new Scanner(System.in);
     }
 
     public void greeting() {
-        System.out.println("This is \n" + logo);
-        System.out.println(greetings);
+        System.out.println("This is \n" + LOGO);
+        System.out.println(GREETINGS);
     }
 
     public void displayExit() {
-        System.out.println(prefix + "Bye, until next time!");
+        System.out.println(PREFIX + "Bye, until next time!");
     }
 
     public void displayLine() {
@@ -40,18 +40,18 @@ public class Ui {
     }
 
     public void displayWrongCommand(String message) {
-        System.out.println(prefix + message);
-        System.out.println(prefix + "Sorry, but we could not parse your input");
-        System.out.println(prefix + "Please recheck its validity...");
+        System.out.println(PREFIX + message);
+        System.out.println(PREFIX + "Sorry, but we could not parse your input");
+        System.out.println(PREFIX + "Please recheck its validity...");
     }
 
     public void displayOutOfRange(int index) {
-        System.out.println(prefix + "There is no such index " + index + " in the task list.");
+        System.out.println(PREFIX + "There is no such index " + index + " in the task list.");
     }
 
     public void displayWrongCommand() {
-        System.out.println(prefix + "Sorry, but we could not parse your input");
-        System.out.println(prefix + "Please recheck its validity...");
+        System.out.println(PREFIX + "Sorry, but we could not parse your input");
+        System.out.println(PREFIX + "Please recheck its validity...");
     }
 
     public void displayError(String message) {
@@ -60,37 +60,37 @@ public class Ui {
     }
 
     public void displayTask(Task task) {
-        System.out.println(prefix + task);
+        System.out.println(PREFIX + task);
     }
 
     public void displayListSize(int size) {
-        System.out.println(prefix + "Now you have " + size + " tasks in the list.");
+        System.out.println(PREFIX + "Now you have " + size + " tasks in the list.");
     }
 
     public void displayEmptyList() {
-        System.out.println(displayEmptyList);
+        System.out.println(DISPLAY_EMPTY_LIST);
     }
 
     public void displayAllTasks(List<Task> list) {
         for (int i = 0; i < list.size(); i++) {
-            System.out.println(prefix + (i + 1) + ". " + list.get(i));
+            System.out.println(PREFIX + (i + 1) + ". " + list.get(i));
         }
     }
 
     public void displayAfterAdd(int size, Task task) {
-        System.out.println(added);
+        System.out.println(DISPLAY_ADDED);
         displayTask(task);
         displayListSize(size);
     }
 
     public void displayAfterDone(Task task) {
-        System.out.println(markAsDone);
+        System.out.println(DISPLAY_DONE);
         displayTask(task);
     }
 
     public void displayAfterDelete(int size, Task task) {
-        System.out.println(removeTask);
-        System.out.println(prefix + task);
+        System.out.println(DISPLAY_RENAME);
+        System.out.println(PREFIX + task);
         displayListSize(size);
     }
 
