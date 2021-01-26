d523dd03ff523ed6cbb74b1cc615ee933a3e999b
Mok Bing Wei Maurice <Maurice2n97@gmail.com>
2021-01-26 17:37:02 +0800

    Extract date from Event description
    For Deadline and Event classes, we need a way to parse and store the dates associated with these two classes.
    
    Static methods in Parser allows for parsing based on the date. So far the only recognisable format is  yyyy-MM-DD. F
    uture iterations could have more date formats.
    
    This would facilitate future features which can categorise events based on their dates.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,15 +1,27 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * A Task with a deadline to complete.
  */
 public class Deadline extends Task {
     protected String by;
+    private LocalDate dateOfDeadline;
     public Deadline(String description,String by) {
         super(description);
-        this.by = by;
+        String dateString = Parser.extractDate(by);
+        if (!dateString.equals("")) {
+            this.dateOfDeadline = Parser.parseDate(dateString);
+            String convertedDateString = dateOfDeadline.format(DateTimeFormatter.ofPattern("MMM d yyyy"));  // convert the format
+            this.by = by.replaceAll(dateString, convertedDateString);
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + by + ")";
     }
+
 }
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,5 +1,46 @@
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Parser {
     /**
      * A class to parse input String for more information of what type of event it is.
      */
+
+    /**
+     * finds date and time within the string
+     * @param input
+     * @return
+     */
+
+
+    public static String extractDate(String input){
+        String regex = "\\d+[-]\\d+[-]\\d+";
+        Pattern datePattern  = Pattern.compile(regex);
+        Matcher m = datePattern.matcher(input);
+        if(m.find()){
+            return m.group(0);
+        } else {
+            return "";
+        }
+    }
+
+    public static LocalDate parseDate (String input) {
+        String regex = "(?<year>\\d{4})-(?<month>\\d{2})-(?<day>\\d{2})"; // format YYYY-MM-DD
+        Pattern dateFormat = Pattern.compile(regex);
+        Matcher m = dateFormat.matcher(input);
+        m.find();
+        String day = m.group("day");
+        String month = m.group("month");
+        String year = m.group( "year");
+        return LocalDate.parse(year + '-' + month + '-' + day);
+    }
+
+    public static class ParseException extends Exception {
+        ParseException(String message){
+            super(message);
+        }
+    }
+
 }
