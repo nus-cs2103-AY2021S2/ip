d1c9b34b86b51367a3dd10f34cba6e00468b93a6
Assyarul <muhdassyarul@gmail.com>
2021-01-27 00:02:31 +0800

    Add JUnit test for EventFactory class

---
+++ src/main/java/EventFactoryTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+public class EventFactoryTest {
+    @Test
+    void read_emptyInput_exceptionThrown(){
+        try {
+            assertEquals("",new EventFactory().createTask("").toString());
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Error: Description of event cannot be empty",e.getMessage());
+        }
+    }
+    @Test
+    void read_wrongInput_exceptionThrown(){
+        try {
+            assertEquals("", new EventFactory().createTask("test").toString());
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Error: Please add a start time to your event",e.getMessage());
+        }
+    }
+}
