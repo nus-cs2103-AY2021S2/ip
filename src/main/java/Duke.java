5cd2d053a25b2599372b7b84d8da8b69e701bcbe
Jonathan-Cao <54720093+Jonathan-Cao@users.noreply.github.com>
2021-01-26 22:34:31 +0800

    add date and time

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,14 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+import java.time.format.DateTimeFormatter;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
+
+    public static DateTimeFormatter DATETIME_FORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm");
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Task> list = new ArrayList<>();
@@ -20,109 +27,115 @@ public class Duke {
                     throw new DukeException("OOPS!!! I'm sorry, but that is an invalid command :-(");
                 }
                 switch (command) {
-                    case TODO:
-                        if (arr.length < 2) {
-                            throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+                case TODO:
+                    if (arr.length < 2) {
+                        throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+                    }
+                    ToDo t = new ToDo(s.substring(5));
+                    list.add(t);
+                    System.out.println(taskConfirmation + t
+                            + "\nNow you have " + list.size()
+                            + (list.size() < 2 ? " task " : " tasks ") + "in the list.\n"
+                            + lineSpacing);
+                    break;
+                case DEADLINE:
+                    String[] arr2 = s.split(" /by ");
+                    if (arr.length < 2 || arr2[0].split(" ").length < 2) {
+                        throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+                    }
+                    if (arr2.length < 2) {
+                        throw new DukeException("OOPS!!! You did not set a deadline using '/by'.");
+                    }
+                    LocalDateTime deadlineDate;
+                    try {
+                        deadlineDate = LocalDateTime.parse(arr2[1], DATETIME_FORMAT);
+                    } catch (DateTimeParseException e) {
+                        throw new DukeException("OOPS!!! Please input date and time in the following format: dd-MM-yyyy HHmm.");
+                    }
+                    Deadline d = new Deadline(arr2[0].substring(9), deadlineDate);
+                    list.add(d);
+                    System.out.println(taskConfirmation + d
+                            + "\nNow you have " + list.size()
+                            + (list.size() < 2 ? " task " : " tasks ") + "in the list.\n"
+                            + lineSpacing);
+                    break;
+                case EVENT:
+                    String[] arr3 = s.split(" /at ");
+                    if (arr.length < 2 || arr3[0].split(" ").length < 2) {
+                        throw new DukeException("OOPS!!! The description of an event cannot be empty.");
+                    }
+                    if (arr3.length < 2) {
+                        throw new DukeException("OOPS!!! You did not set the date/time of the event using '/at'.");
+                    }
+                    LocalDateTime eventDate;
+                    try {
+                        eventDate = LocalDateTime.parse(arr3[1], DATETIME_FORMAT);
+                    } catch (DateTimeParseException e) {
+                        throw new DukeException("OOPS!!! Please input date and time in the following format: dd-MM-yyyy HHmm.");
+                    }
+                    Event e = new Event(arr3[0].substring(6), eventDate);
+                    list.add(e);
+                    System.out.println(taskConfirmation + e
+                            + "\nNow you have " + list.size()
+                            + (list.size() < 2 ? " task " : " tasks ") + "in the list.\n"
+                            + lineSpacing);
+                    break;
+                case LIST:
+                    if (list.isEmpty()) {
+                        System.out.println("There are no tasks in your list. Hooray!\n" + lineSpacing);
+                        continue;
+                    }
+                    System.out.println("Here are the tasks in your list:");
+                    int num = 1;
+                    for (Task task : list) {
+                        System.out.println(num + "." + task);
+                        num++;
+                    }
+                    System.out.println(lineSpacing);
+                    break;
+                case DONE:
+                    if (arr.length < 2) {
+                        throw new DukeException("OOPS!!! You did not enter the number corresponding to the task.");
+                    }
+                    try {
+                        int index = Integer.parseInt(arr[1]);
+                        if (index < 1 || index > list.size()) {
+                            throw new DukeException("OOPS!!! The numbered task does not exist.");
                         }
-                        ToDo t = new ToDo(s.substring(5));
-                        list.add(t);
-                        System.out.println(taskConfirmation + t
+                        list.get(index - 1).markAsDone();
+                        System.out.println("Nice! I've marked this task as done:\n" + list.get(index - 1)
+                                + "\n" + lineSpacing);
+                    } catch (NumberFormatException ex) {
+                        throw new DukeException("OOPS!!! You did not enter the number corresponding to the task.");
+                    }
+                    break;
+                case DELETE:
+                    if (arr.length < 2) {
+                        throw new DukeException("OOPS!!! You did not enter the number corresponding to the task.");
+                    }
+                    try {
+                        int index = Integer.parseInt(arr[1]);
+                        if (index < 1 || index > list.size()) {
+                            throw new DukeException("OOPS!!! The numbered task does not exist.");
+                        }
+                        Task task = list.get(index - 1);
+                        list.remove(index - 1);
+                        System.out.println("Noted! I've removed this task:\n" + task
                                 + "\nNow you have " + list.size()
-                                + (list.size() < 2 ? " task " : " tasks ") + "in the list.\n"
+                                + (list.size() == 1 ? " task " : " tasks ") + "in the list.\n"
                                 + lineSpacing);
-                        break;
-
-                    case DEADLINE:
-                        String[] arr2 = s.split(" /by ");
-                        if (arr.length < 2) {
-                            throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
-                        }
-                        if (arr2.length < 2) {
-                            throw new DukeException("OOPS!!! You did not set a deadline using '/by'.");
-                        }
-                        Deadline d = new Deadline(arr2[0].substring(9), arr2[1]);
-                        list.add(d);
-                        System.out.println(taskConfirmation + d
-                                + "\nNow you have " + list.size()
-                                + (list.size() < 2 ? " task " : " tasks ") + "in the list.\n"
-                                + lineSpacing);
-                        break;
-
-                    case EVENT:
-                        String[] arr3 = s.split(" /at ");
-                        if (arr.length < 2) {
-                            throw new DukeException("OOPS!!! The description of an event cannot be empty.");
-                        }
-                        if (arr3.length < 2) {
-                            throw new DukeException("OOPS!!! You did not set the date/time of the event using '/at'.");
-                        }
-                        Event e = new Event(arr3[0].substring(6), arr3[1]);
-                        list.add(e);
-                        System.out.println(taskConfirmation + e
-                                + "\nNow you have " + list.size()
-                                + (list.size() < 2 ? " task " : " tasks ") + "in the list.\n"
-                                + lineSpacing);
-                        break;
-
-                    case LIST:
-                        if (list.isEmpty()) {
-                            System.out.println("There are no tasks in your list. Hooray!\n" + lineSpacing);
-                            continue;
-                        }
-                        System.out.println("Here are the tasks in your list:");
-                        int num = 1;
-                        for (Task task : list) {
-                            System.out.println(num + "." + task);
-                            num++;
-                        }
-                        System.out.println(lineSpacing);
-                        break;
-
-                    case DONE:
-                        if (arr.length < 2) {
-                            throw new DukeException("OOPS!!! You did not enter the number corresponding to the task.");
-                        }
-                        try {
-                            int index = Integer.parseInt(arr[1]);
-                            if (index < 1 || index > list.size()) {
-                                throw new DukeException("OOPS!!! The numbered task does not exist.");
-                            }
-                            list.get(index - 1).markAsDone();
-                            System.out.println("Nice! I've marked this task as done:\n" + list.get(index - 1)
-                                                + "\n" + lineSpacing);
-                        } catch (NumberFormatException ex) {
-                            throw new DukeException("OOPS!!! You did not enter the number corresponding to the task.");
-                        }
-                        break;
-
-                    case DELETE:
-                        if (arr.length < 2) {
-                            throw new DukeException("OOPS!!! You did not enter the number corresponding to the task.");
-                        }
-                        try {
-                            int index = Integer.parseInt(arr[1]);
-                            if (index < 1 || index > list.size()) {
-                                throw new DukeException("OOPS!!! The numbered task does not exist.");
-                            }
-                            Task task = list.get(index - 1);
-                            list.remove(index - 1);
-                            System.out.println("Noted! I've removed this task:\n" + task
-                                    + "\nNow you have " + list.size()
-                                    + (list.size() == 1 ? " task " : " tasks ") + "in the list.\n"
-                                    + lineSpacing);
-                        } catch (NumberFormatException exception) {
-                            throw new DukeException("OOPS!!! You did not enter the number corresponding to the task.");
-                        }
-                        break;
-
-                    case BYE:
-                        System.out.println("Bye. Hope to see you again soon!\n" + lineSpacing);
-                        byeFlag = false;
-                        break;
+                    } catch (NumberFormatException exception) {
+                        throw new DukeException("OOPS!!! You did not enter the number corresponding to the task.");
+                    }
+                    break;
+                case BYE:
+                    System.out.println("Bye. Hope to see you again soon!\n" + lineSpacing);
+                    byeFlag = false;
+                    break;
                 }
             } catch (DukeException e) {
                 System.out.println("Duke has encountered an error: " + e.getMessage()
-                                    + "\n" + lineSpacing);
+                        + "\n" + lineSpacing);
             }
         }
     }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,19 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String at;
+    protected LocalDateTime at;
 
-    public Event(String description, String at) {
+    public Event(String description, LocalDateTime at) {
         super(description);
         this.at = at;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + this.at + ")";
+        return "[E]" + super.toString() + " (at: "
+                + this.at.format(DateTimeFormatter.ofPattern("d MMM yyyy hh:mma"))
+                + ")";
     }
 }
