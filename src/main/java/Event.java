ee2e6c15e94b72ee57c070fd641c82c8af89f5c5
khiaxeng <khiaxeng@gmail.com>
2021-01-26 20:56:49 +0800

    Add Level-8 requirements: Dates and times

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,16 @@
+import java.time.LocalDateTime;
+
 public class Event extends Task {
 
-    String time;
+    protected LocalDateTime time;
 
-    public Event(String description, String time) {
+    public Event(String description, LocalDateTime time) {
         super(description);
         this.time = time;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + time + ")";
+        return "[E]" + super.toString() + " (at: " + Parser.parseDateTimeToString(time) + ")";
     }
 }
