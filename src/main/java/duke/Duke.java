4f7577d41f184d454d6cf2c39772ca0845af6fc6
sjq-sohjunqi <65146620+sjq-sohjunqi@users.noreply.github.com>
2021-01-26 22:54:49 +0800

    Add JavaDoc comments
    JavaDoc header comments added to give specificity to the purpose of each function

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -17,7 +17,11 @@ public class Duke {
         
 		chatBot();
     }
-
+	
+	/**
+	* This method initiates the chatbot of the Duke program. It call upon various 
+	* classes responsibile for different functionalities of Duke.
+	*/
     public static void chatBot(){
 		
 		Ui ui = new Ui();
