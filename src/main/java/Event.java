3f6b327b021b6eb8d5ca2acb3f39d7af503c075e
CSmortal <jovenpua51@gmail.com>
2021-01-26 11:04:05 +0800

    modify Deadline and Event to store dates as LocalDate instead

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,25 +1,32 @@
 package main.java;
 
-public class Event extends Task {
-    private String timeRange;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String timeRange) {
+public class Event extends Task {
+    private final LocalDate date;
+    private final String timeRange;
+
+    public Event(String description, LocalDate date, String timeRange) {
         super(description);
+        this.date = date;
         this.timeRange = timeRange;
     }
 
-    public Event(String description, String timeRange, boolean isDone) {
+    public Event(String description, LocalDate date, String timeRange, boolean isDone) {
         super(description, isDone);
+        this.date = date;
         this.timeRange = timeRange;
     }
 
     @Override
     public Event markAsDone() {
-        return new Event(description, timeRange, true);
+        return new Event(description, date, timeRange, true);
     }
 
     @Override
     public String toString() {
-        return "[E][" + getStatusIcon() + "] " + description + " (at: " + timeRange + ")";
+        return "[E][" + getStatusIcon() + "] " + description + " (at: "
+                + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " " + timeRange + ")";
     }
 }
