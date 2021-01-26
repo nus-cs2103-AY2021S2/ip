ef0cfc5ecaf2d99e9371bf5ef725031562ef45b2
Lim Li Quan <linliquan1996@Hotmail.com>
2021-01-26 21:35:40 +0800

    Duke.java : Clean up of unused code , minor format changes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,3 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.Scanner;
-
 public class Duke {
     private Ui ui;
     private TaskList tasks;
@@ -28,7 +23,9 @@ public class Duke {
                 ui.showError(e.getMessage());
             } catch (DukeDeadlineException e) {
                 ui.showError(e.getMessage());
-            }finally {
+            } catch (NullPointerException e) {
+                ui.showError("OOPS!!! I`m sorry. but i don`t know what that means :-(");
+            } finally {
                 ui.showLine();
             }
         }
@@ -37,168 +34,4 @@ public class Duke {
     public static void main(String[] args) {
         new Duke().run();
     }
-
-    /*
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String task, keyword;
-        LocalDateTime deadline;
-        int firstSpace, firstSlash, option;
-
-        //Loading from file
-        System.out.println("Please enter file name : ");
-        fileName = scanner.nextLine();
-        loadTaskList();
-
-        //Greet User
-        printGreetings();
-        String command = scanner.nextLine();
-        while (!command.equalsIgnoreCase("bye")) {
-            boolean change = true;
-            printLine();
-
-            firstSpace = command.indexOf(" ");
-            keyword = firstSpace == -1 ? command : command.substring(0, firstSpace).toLowerCase();
-            firstSlash = command.indexOf("/");
-
-            try {
-                switch (keyword) {
-                case "list":
-                    //Display all task added
-                    listTasks();
-                    change = false;
-                    break;
-                case "done":
-                    // -1 as ArrayList starts from 0 , user input starts from 1
-                    option = getChoice(command, firstSpace);
-                    //Mark task of choice as done
-                    completeTask(option);
-                    break;
-                case "delete":
-                    option = getChoice(command, firstSpace);
-                    deleteTask(option);
-                    break;
-                case "todo":
-                    task = retrieveTask(command, firstSpace, command.length());
-                    addTask(new Todo(task));
-                    break;
-                case "deadline":
-                    task = retrieveTask(command, firstSpace, firstSlash);
-                    deadline = retrieveDeadline(command, firstSlash);
-                    addTask(new Deadline(task, deadline));
-                    break;
-                case "event":
-                    task = retrieveTask(command, firstSpace, firstSlash);
-                    LocalDateTime[] deadlines = retrieveEventDeadline(command, firstSlash);
-                    addTask(new Event(task,deadlines[0], deadlines[1]));
-                    break;
-                default:
-                    System.out.println("OOPS!!! I`m sorry. but i don`t know what that means :-(");
-                    break;
-                }
-                if (change) {
-                    saveTaskList();
-                }
-            } catch (DukeException e) {
-                System.out.printf("OOPS!!! %s %s cannot be empty.\n", e.getMessage(), keyword);
-            } catch (DukeDeadlineException e) {
-                System.out.println(e.getMessage());
-            }
-            printLine();
-            command = scanner.nextLine();
-        }
-        //Exits the program
-        printLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        printLine();
-    }
-
-    public static void deleteTask(int option) {
-        Task removed_task = taskList.get(option);
-        taskList.remove(option);
-        System.out.println("Noted. I`ve removed this task:");
-        printTask("", removed_task);
-        printTaskNum();
-    }
-
-    public static int getChoice(String command, int firstSpace) {
-        return Integer.parseInt(command.substring(firstSpace + 1)) - 1;
-    }
-
-    public static LocalDateTime retrieveDeadline(String command, int start) throws DukeDeadlineException{
-        int nextSpace = getNextSpace(command,start);
-        String deadline = command.substring(nextSpace+1);
-        try {
-            return parseDate(deadline);
-        } catch (DateTimeParseException e) {
-            throw new DukeDeadlineException("Format of the deadline of a deadline task should be (Year-Month-Day time (24 hours))");
-        }
-    }
-
-    public static int getNextSpace(String command, int start) {
-        return command.indexOf(" ",start);
-    }
-
-
-    public static LocalDateTime[] retrieveEventDeadline(String command, int start) throws DukeDeadlineException {
-        int firstSpace = getNextSpace(command,start) + 1;
-        int nextSpace = getNextSpace(command,firstSpace) + 1;
-        String date = command.substring(firstSpace, nextSpace);
-        String times = command.substring(nextSpace);
-        String[] timeArr = times.split("-");
-        if(timeArr.length != 2) {
-            throw new DukeDeadlineException("Format of the deadline of a Event task should be (Year-Month-Day Time(24 hours)-Time(24 hours)");
-        } else {
-            LocalDateTime[] deadlineArr = new LocalDateTime[2];
-            for (int i = 0; i < deadlineArr.length; i++) {
-                StringBuilder str = new StringBuilder(date);
-                str.append(timeArr[i]);
-                deadlineArr[i] = parseDate(str.toString());
-            }
-            return deadlineArr;
-        }
-    }
-
-    public static String retrieveTask(String command, int start, int end) throws DukeException {
-        // RetrieveTask starts from the space after the keyword
-        // RetrieveTask ends either end of the string or before / (deadline and event)
-        // E.g <keyword> <description> (datetime/time)
-        // if start == -1 --> e.g todo , deadline , event
-        // if end == -1 --> deadline buy book , event buy book
-        if (start == -1) {
-            throw new DukeException("The description of a");
-        } else if (end == -1) {
-            throw new DukeException("The deadline of a");
-        }
-        return command.substring(start, end);
-    }
-
-    public static void addTask(Task newTask) {
-        taskList.add(newTask);
-        System.out.println("Got it. I`ve added this task:");
-        printTask("", newTask);
-        printTaskNum();
-    }
-
-    public static void printTaskNum() {
-        System.out.printf("Now you have %d task in the list\n", taskList.size());
-    }
-
-    public static void completeTask(int option) {
-        boolean res = false;
-        String message = "Invalid option";
-        // Option validation (More than 0 and not more than the number of task)
-        if (option >= 0 && option < taskList.size()) {
-            res = taskList.get(option).markAsDone();
-            message = String.format("Task %d is already been marked as done", option + 1);
-        }
-
-        if (res) {
-            System.out.println("Nice! I`ve marked this task as done:");
-            printTask("", taskList.get(option));
-        } else {
-            System.out.println(message);
-        }
-    }
-    */
 }
