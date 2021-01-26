d4579d902d7f66cb037b38ac77e6d9b368e94d4f
ALZAHRANI-PC\riyad <riyadh-h@outlook.sa>
2021-01-26 23:28:07 +0800

    Add VergilTest.java

---
+++ src/VergilTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VergilTest {
+    TaskList taskList = new TaskList();
+
+    @Test
+    public void addTodo() {
+        taskList.add(new Todo("A to-do task"));
+        assertEquals(
+                "1. [T][ ] A to-do task\n",
+                taskList.toString());
+    }
+
+    @Test
+    public void completeTodo() {
+        taskList.add(new Todo("A to-do task"));
+        try {
+            taskList.complete(1);
+        } catch (VergilException e) {
+            // Do nothing.
+        } finally {
+            assertEquals(
+                    "1. [T][X] A to-do task\n",
+                    taskList.toString());
+        }
+    }
+}
