44600abaeb07b51af14436976f562be1fdebd29d
sjq-sohjunqi <65146620+sjq-sohjunqi@users.noreply.github.com>
2021-01-27 13:00:21 +0800

    Edit code to follow convention
    Edit code to follow convention
    
    The code did not follow the coding conventions, making it harder to read.
    
    Let's change code to follow standard Java coding standards.

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,12 +1,11 @@
 package duke;
 
-import java.util.*;
-import java.io.*;
-import java.time.*;
+import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.time.format.DateTimeFormatter;
 
 public class Parser {
+<<<<<<< Updated upstream
 	
 	private boolean end;
 	
@@ -156,4 +155,179 @@ public class Parser {
 	
 	}
 	
+=======
+    
+    private boolean end;
+    
+    /**
+    * Constructor for the Parser class
+    */
+    public Parser() {
+        end = false;
+    }
+    
+    /**
+    * Returns the End object which indicates whether the chatbot has been terminated by the user.
+    * @return boolean object. True - chatbot ended, False - chatbot still running
+    */
+    public boolean checkEnd() {
+        return end;
+    }
+    
+    /**
+    * Returns a String object which contains the output from processing user's input. This method will process
+    * the user's input and perform functions accordingly to the TaskList object supplied in the argument <p>
+    * The in String argument should be a valid command. The following are commands acceptable by the process function:<br/>
+    * <ul>
+    * <li>list - Displays current list items</li>
+    * <li>bye - Exits Duke program</li>
+    * <li>done <int:number> - Marks item number specified (1-based) as done</li>
+    * <li>todo <String:name> - Creates an item type of "todo" inside TaskList</li>
+    * <li>deadline <String:name> /by <String:date> - Creates an item type of "deadline" inside TaskList. The date should be specified in an YYYY-MM-DD format</li>
+    * <li>event <String:name> /at <String:date> - Creates an item type of "event" inside TaskList. The date should be specified in an YYYY-MM-DD format</li>
+    * <li>delete <int:number> - Deletes item number specified (1-based)</li>
+    * </ul>
+    * @param in the input command entered by user
+    * @param TaskList the TaskList object the commands will be processed on
+    * @return String object containing results of processing 
+    */
+    public String process(String in, TaskList tl){
+        
+        try {
+            
+            String res = "";
+            
+            String[] split = in.split("\\s");
+            
+            String cmd = split[0];
+            
+            switch(cmd) {
+            case "bye":
+                end = true;
+                res = "Bye. Hope to see you again soon!";
+                break;
+            case "list":
+                res = "Here are the tasks in your list:\n";
+                res += tl.printList();
+                break;
+            case "done":
+                if (split.length <= 1) {
+                    throw new DukeException("OOPS!!! A number needs to be specified.");
+                }
+                
+                try {
+                    Task t = tl.markDone(Integer.parseInt(split[1]));
+                    
+                    res = "Nice! I've marked this task as done:\n";
+                    res += t;
+                } catch(NumberFormatException ne) {
+                    throw new DukeException("OOPS!!! A number needs to be specified.");
+                }
+                
+                break;
+            case "todo":
+                split = in.split("todo\\s");
+                
+                if (split.length <= 1) {
+                    throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+                }
+                
+                Task td = new Task(split[1]);
+                tl.add(td);
+                
+                res = "Got it. I've added this task:\n";
+                res += td + "\n";
+                res += "Now you have "+tl.count()+" tasks in the list.";
+                break;
+            case "deadline":
+                split = in.split("deadline\\s");
+                
+                if (split.length <= 1) {
+                    throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+                }
+                
+                split = split[1].split("\\s/by\\s");
+                
+                if (split.length <= 1) {
+                    throw new DukeException("OOPS!!! A date and time is needed.");
+                }
+                
+                LocalDate dt = LocalDate.now();
+                
+                try {
+                
+                    dt = LocalDate.parse(split[1]);
+                
+                } catch(DateTimeParseException dte) {
+                    throw new DukeException("OOPS!!! The date format should be in YYYY-MM-DD");
+                }
+                
+                Task dl = new Task(split[0], 'D', dt);
+                tl.add(dl);
+                
+                res = "Got it. I've added this task:\n";
+                res += dl + "\n";
+                res += "Now you have "+tl.count()+" tasks in the list.\n";
+                break;
+            case "event":
+                split = in.split("event\\s");
+                
+                if (split.length <= 1) {
+                    throw new DukeException("OOPS!!! The description of a event cannot be empty.");
+                }
+                
+                split = split[1].split("\\s/at\\s");
+                
+                if (split.length <= 1) {
+                    throw new DukeException("OOPS!!! A date and time is needed.");
+                }
+                
+                LocalDate dtt = LocalDate.now();
+                
+                try {
+                
+                    dtt = LocalDate.parse(split[1]);
+                
+                } catch(DateTimeParseException e) {
+                    throw new DukeException("OOPS!!! The date format should be in YYYY-MM-DD");
+                }
+                
+                Task ev = new Task(split[0], 'E', dtt);
+                tl.add(ev);
+                
+                res = "Got it. I've added this task:\n";
+                res += ev + "\n";
+                res += "Now you have "+tl.count()+" tasks in the list.\n";
+                break;
+            case "delete":
+                if (split.length <= 1) {
+                    throw new DukeException("OOPS!!! A number needs to be specified.");
+                }
+                
+                try {
+                    Task t = tl.remove(Integer.parseInt(split[1]));
+                    
+                    res = "Noted. I've removed this task\n";
+                    res += t + "\n";
+                    res += "Now you have "+tl.count()+" tasks in the list.\n";
+                } catch(NumberFormatException ne) {
+                    throw new DukeException("OOPS!!! A number needs to be specified.");
+                }
+                
+                break;
+            default:
+                throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    
+            }
+            
+            return res;
+            
+        } catch(DukeException de) {
+            return de.getMessage();
+        }
+        
+    
+    }
+    
+>>>>>>> Stashed changes
 }
--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -1,12 +1,16 @@
 package duke;
 
-import java.util.*;
-import java.io.*;
-import java.time.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.time.format.DateTimeFormatter;
+import java.io.IOException;
 
 public class Storage {
+<<<<<<< Updated upstream
 	
 	private File f;
 	
@@ -107,4 +111,119 @@ public class Storage {
 		
 	}
 	
+=======
+    
+    private File file;
+    
+    /**
+    * Constructor for the Storage class. The filename argument will be used to create file for storing 
+    * and retrieving the user's TaskList.
+    * @param filename the name of the file to save TaskList information
+    */
+    public Storage(String filename) {
+        file = new File(filename);
+    }
+    
+    /**
+    * Returns the TaskList object previously saved by the chatbot when the user terminates the program.
+    * @return TaskList object
+    */
+    public TaskList loadTaskList() {
+        
+        try {
+            if (file.createNewFile()) {
+                return new TaskList();
+            } else {
+                
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                
+                TaskList tl = new TaskList();
+                
+                String line;
+                while((line = br.readLine()) != null) {
+                    String[] split = line.split("\\|");
+                    
+                    // Create new task for each line
+                    char type = split[0].charAt(0);
+                    
+                    boolean done = false;
+                    if (split[1].equals("D")) {
+                        done = true;
+                    }
+                    
+                    String name = split[2];
+                    
+                    if (type == 'D' || type == 'E') {
+                        LocalDate dateTime = LocalDate.parse(split[3]);
+                        
+                        Task t = new Task(name, type, dateTime);
+                        if (done)
+                            t.mark();
+                        
+                        tl.add(t);
+                        
+                    } else {
+                        Task t = new Task(name);
+                        if (done)
+                            t.mark();
+                        
+                        tl.add(t);
+                    }
+                }
+                
+                return tl;
+                    
+                
+            }
+            
+        } catch(IOException e) {
+            System.out.println(e.toString());
+            return new TaskList();
+        }
+        
+    }
+    
+    /**
+    * Saves the TaskList object provided in the argument into the file created by the constructor of this class.
+    * @param TaskList object to be saved
+    */
+    public void saveTaskList(TaskList tl){
+        
+        try {
+            FileWriter fw = new FileWriter(file, false);
+            
+            for (int i = 0; i < tl.count(); i++) {
+                
+                Task t = tl.getTask(i);
+                
+                String line = "" + t.getType() + "|";
+                if (t.getDone()) {
+                    line += "D|";
+                } else {
+                    line += "ND|";
+                }
+                
+                line += t.getName();
+                
+                if (t.getType() == 'D' | t.getType() == 'E') {
+                    line += "|" + t.getDate();
+                }
+                
+                line += "\n";
+                
+                fw.write(line);
+                
+            }
+            
+            fw.flush();
+            fw.close();
+            
+        } catch (IOException e) {
+            System.out.println("Error writing to file!");
+            
+        }
+        
+    }
+    
+>>>>>>> Stashed changes
 }
