64f2b3f2772a4b91797bbe94a9ff00cd050aa80d
sylviaokt <kaitingx3@gmail.com>
2021-01-27 23:04:07 +0800

    Complete Level 7.
    Save the tasks in the hard disk automatically whenever the task list changes. Load the data from the hard disk when Duke starts up.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -6,6 +6,19 @@ public class Task {
         this.name = name;
     }
 
+    public Task(String name, boolean done) {
+        this.name = name;
+        this.done = done;
+    }
+
+    public String getName(Task task) {
+        return task.name;
+    }
+
+    public boolean getStatus(Task task) {
+        return task.done;
+    }
+
     public void status(boolean done) {
         this.done = done;
     }
