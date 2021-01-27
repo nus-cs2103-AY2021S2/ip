35704c16b3257e4d9cf818ca7a253ef2f408ee24
lirc572 <admin@lirc572.com>
2021-01-28 00:27:32 +0800

    Feat: automatically load from file on startup

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -112,17 +112,48 @@ public class Duke {
             // Convert task list to a string
             StringBuilder data = new StringBuilder();
             for (Task task : tasks) {
-//                printLine(String.format("%d.%s", ++index, task.toString()));
                 data.append(task.toSavedString());
             }
             fileWriter.write(data.toString());
-//            Scanner fileReader = new Scanner(fileObj);
-//            StringBuilder data = new StringBuilder();
-//            while (fileReader.hasNextLine()) {
-//                data.append(fileReader.nextLine());
-//            }
-//            fileReader.close();
-//            String dataStr = data.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readFromFile() {
+        try {
+            File fileObj = new File(fileName);
+            fileObj.createNewFile();
+            Scanner fileReader = new Scanner(fileObj);
+            StringBuilder data = new StringBuilder();
+            while (fileReader.hasNextLine()) {
+                data.append(fileReader.nextLine());
+            }
+            fileReader.close();
+            tasks.clear();
+            String[] lines = data.toString().split("\n");
+            for (String line:lines) {
+                String[] sections = line.split(" | ");
+                if (sections[0].equals("T")) {
+                    Task task = new TodoTask(sections[2]);
+                    if (sections[1].equals("1")) {
+                        task.setIsDone(true);
+                    }
+                    tasks.add(task);
+                } else if (sections[0].equals("D")) {
+                    Task task = new DeadlineTask(sections[2], sections[3]);
+                    if (sections[1].equals("1")) {
+                        task.setIsDone(true);
+                    }
+                    tasks.add(task);
+                } else if (sections[0].equals("E")) {
+                    Task task = new EventTask(sections[2], sections[3]);
+                    if (sections[1].equals("1")) {
+                        task.setIsDone(true);
+                    }
+                    tasks.add(task);
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -276,6 +307,7 @@ public class Duke {
         printLine("Sou, watashi desu!");
         printHorizontalLine();
         printEmptyLine();
+        readFromFile();
         for (; ; ) {
             try {
                 if (!processCommand(sc.nextLine())) {
