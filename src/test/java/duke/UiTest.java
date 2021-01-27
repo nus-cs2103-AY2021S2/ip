50b10e0d5b12e46f785a988f2ed64abb0ae594bd
SoonKeatNeo <neo@soonkeat.sg>
2021-01-27 12:44:52 +0800

    Add initial JUnit tests for Ui

---
+++ src/test/java/duke/UiTest.java
@@ -0,0 +1,47 @@
+package duke;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UiTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        Ui ui = new Ui();
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    void greetUser_thenOutputCaptorSuccess() {
+        Ui.greetUser();
+        String expected = "Henlooooo~! My name is " + Duke.BOT_NAME + "\n" +
+                "    What can I do for you today? :)";
+        assertEquals(expected, outputStreamCaptor.toString()
+                .trim());
+    }
+
+    @Test
+    void showTestMessage_onPrintMessage_thenOutputCaptorSuccess() {
+        Ui.printMessage("Test");
+        String expected = "    Test\n";
+        assertEquals(expected, outputStreamCaptor.toString());
+    }
+
+    @Test
+    void showSeparators_onFunctionCall_thenOutputCaptorSuccess() {
+        Ui.printSeparators();
+        String expected = "~~~~~~~~~~~~~~~~~~~~~~\n";
+        assertEquals(expected, outputStreamCaptor.toString());
+    }
+}
