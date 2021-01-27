6196306cb2ec6ff1fae42ee1224076d95878535b
Ellevy <46534252+Ellevy@users.noreply.github.com>
2021-01-27 18:49:05 +0800

    Tidy up code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,64 +1,32 @@
-
-
 import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import java.util.Arrays;
 
 public class Duke {
-    private static TaskList tasks;
-    private static Storage storage;
-    private static Parser parser;
-    private static Ui ui;
+    private TaskList tasks;
+    private Storage storage;
+    private Parser parser;
+    private Ui ui;
+
+    public Duke() {
+        tasks = new TaskList();
+        storage = new Storage(tasks);
+        parser = new Parser(tasks);
+    }
 
     public static void main(String[] args) {
-        tasks = new TaskList();
-        storage = new Storage();
-        parser = new Parser(tasks);
+        Duke duke = new Duke();
         try {
-            storage.loadData();
-            parser.printList();
+            duke.storage.loadData();
+            duke.parser.printList();
         } catch (FileNotFoundException e) {
             System.out.println("File not found :(");
         }
-        ui = new Ui();
-        String userInput = ui.nextInput();
+        duke.ui = new Ui();
+        String userInput = duke.ui.nextInput();
         while (!"bye".equals(userInput)) {
-            parser.processCommand(userInput);
-            Duke.writeTaskList();
-            userInput = ui.nextInput();
+            duke.parser.processCommand(userInput);
+            duke.storage.writeTaskList();
+            userInput = duke.ui.nextInput();
         }
-        ui.close();
-    }
-
-    public static void readTaskList(String taskData) {
-        String[] splits = taskData.split(" \\| ");
-        if (splits[0].equals("T")) {
-            Todo addedTask = new Todo(Arrays.asList(splits).get(2), Arrays.asList(splits).get(1).equals("1"));
-            tasks.addTask(addedTask);
-        } else if (splits[0].equals("D")) {
-            try {
-                Deadline addedTask = new Deadline(Arrays.asList(splits).get(2), Arrays.asList(splits).get(1).equals("1"), Arrays.asList(splits).get(3));
-                tasks.addTask(addedTask);
-            } catch (DukeException e) {
-                System.out.println(e);
-            }
-        } else if (splits[0].equals("E")) {
-            try {
-                Event addedTask = new Event(Arrays.asList(splits).get(2), Arrays.asList(splits).get(1).equals("1"), Arrays.asList(splits).get(3));
-                tasks.addTask(addedTask);
-            } catch (DukeException e) {
-                System.out.println(e);
-            }
-        }
-    }
-
-    public static void writeTaskList() {
-        String userInput = tasks.joinToTxt();
-        try {
-            storage.writeNewFile(userInput);
-        } catch (IOException e) {
-            System.out.println("ERROR");
-        }
+        duke.ui.close();
     }
 }
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -4,13 +4,17 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Storage {
     private final String DATA_FOLDER_PATH = "data";
     private final String DUKE_FILE_PATH = "data/duke.txt";
+    private TaskList tasks;
 
-    public Storage() { }
+    public Storage(TaskList tasks) {
+        this.tasks = tasks;
+    }
 
     public void loadData() throws FileNotFoundException {
         File dataFolder = new File(DATA_FOLDER_PATH);
@@ -21,7 +25,29 @@ public class Storage {
         if (dukeFile.exists()) {
             Scanner s = new Scanner(dukeFile); // create a Scanner using the File as the source
             while (s.hasNext()) {
-                Duke.readTaskList(s.nextLine());
+                readTaskList(s.nextLine());
+            }
+        }
+    }
+
+    public void readTaskList(String taskData) {
+        String[] splits = taskData.split(" \\| ");
+        if (splits[0].equals("T")) {
+            Todo addedTask = new Todo(Arrays.asList(splits).get(2), Arrays.asList(splits).get(1).equals("1"));
+            tasks.addTask(addedTask);
+        } else if (splits[0].equals("D")) {
+            try {
+                Deadline addedTask = new Deadline(Arrays.asList(splits).get(2), Arrays.asList(splits).get(1).equals("1"), Arrays.asList(splits).get(3));
+                tasks.addTask(addedTask);
+            } catch (DukeException e) {
+                System.out.println(e);
+            }
+        } else if (splits[0].equals("E")) {
+            try {
+                Event addedTask = new Event(Arrays.asList(splits).get(2), Arrays.asList(splits).get(1).equals("1"), Arrays.asList(splits).get(3));
+                tasks.addTask(addedTask);
+            } catch (DukeException e) {
+                System.out.println(e);
             }
         }
     }
@@ -30,6 +56,15 @@ public class Storage {
         FileWriter fw = new FileWriter(DUKE_FILE_PATH);
         fw.write(userInput);
         fw.close();
+    }
+
+    public void writeTaskList() {
+        String userInput = tasks.joinToTxt();
+        try {
+            writeNewFile(userInput);
+        } catch (IOException e) {
+            System.out.println("ERROR");
+        }
     }
 
 }
