b055c99ecf56c18c9a521b1e025279858a062dfe
Otto Alexander <godjuansan88@gmail.com>
2021-01-27 19:17:38 +0800

    Add JUnit tests

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,9 +1,22 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
     @Test
-    public void doCommandTest_Deadline(){
-        assertEquals(2, 2);
+    public void taskListSizeTest() {
+        List<Task> lst = new ArrayList<>();
+        TaskList taskList = new TaskList(lst);
+        assertEquals(taskList.size(), 0);
+    }
+
+    @Test
+    public void taskListAddTest() {
+        List<Task> lst = new ArrayList<>();
+        TaskList taskList = new TaskList(lst);
+        taskList.add(new ToDo("task 1"));
+        assertEquals(taskList.get(0).getJob(), "task 1");
     }
 }
