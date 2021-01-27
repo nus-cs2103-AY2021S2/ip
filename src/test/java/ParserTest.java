7db39e924d0406cd54b9a327a3ac4df0ec3823c6
Benedict Khoo <benedictkhoo.mw@u.nus.edu>
2021-01-27 23:33:17 +0800

    Add tests for Parser and ToDo

---
+++ src/test/java/ParserTest.java
@@ -0,0 +1,22 @@
+import duke.commands.AddToDoCommand;
+import duke.commands.InvalidCommand;
+import duke.parser.Parser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ParserTest {
+    @Test
+    public void parser_addToDoCmdEmptyDescription_returnsInvalidCommand() {
+        Parser p = new Parser();
+
+        assertTrue(p.parseCmd("todo") instanceof InvalidCommand);
+    }
+
+    public void parser_addToDoCmdProper_returnsAddToDoCmd() {
+        Parser p = new Parser();
+
+        assertTrue(p.parseCmd("todo something big") instanceof AddToDoCommand);
+    }
+}
---
+++ src/test/java/ToDoTest.java
@@ -0,0 +1,46 @@
+import duke.task.TaskParseException;
+import duke.task.ToDo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ToDoTest {
+
+    @Test
+    public void toDo_deserializeBadlySerializedToDo_throwsTaskParseException() {
+        String description = "a random description";
+        boolean isDone = true;
+        String badlySerializedToDo = new ToDo(description, isDone).serialize().substring(1);
+
+        assertThrows(TaskParseException.class, () -> {
+            ToDo.deserialize(badlySerializedToDo);
+        });
+    }
+
+    @Test
+    public void toDo_deserializeSerializedToDo_correctlyDeserialized() {
+        String description = "something";
+        boolean isDone = true;
+        ToDo toDo = new ToDo(description, isDone);
+
+        assertDoesNotThrow(() -> {
+            ToDo deserialized = ToDo.deserialize(toDo.serialize());
+            assertEquals(deserialized.getDescription(), toDo.getDescription());
+            assertEquals(deserialized.getType(), toDo.getType());
+            assertEquals(deserialized.getStatusIcon(), toDo.getStatusIcon());
+        });
+    }
+
+    @Test
+    public void toDo_serializedToDo_recognizedAsToDo() {
+        String description = "something else";
+        boolean isDone = false;
+        String toDo = new ToDo(description, isDone).serialize();
+
+        assertTrue(ToDo.isToDo(toDo));
+    }
+}
