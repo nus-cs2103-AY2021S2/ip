fa68f9f6983b2e0295392a8a4a5f571e533ac473
chenzaza <wxgych@gmail.com>
2021-01-28 03:22:37 +0800

    Understand dates and times

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,16 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 public class Event extends Task {
-    protected String at;
+    protected LocalDateTime at;
 
-    public Event(String description, String at) {
+    public Event(String description, LocalDateTime at) {
         super(description);
         this.at = at;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " + at.format(DateTimeFormatter.ofPattern("HHmm, MMM dd yyyy")) + ")";
     }
 }
