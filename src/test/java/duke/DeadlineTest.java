1128a8abbed86b76fff5630d9bb21b3b0bca4dda
SoonKeatNeo <neo@soonkeat.sg>
2021-01-27 13:07:39 +0800

    Add JUnit tests for Deadlines and Events

---
+++ src/test/java/duke/DeadlineTest.java
@@ -0,0 +1,29 @@
+package duke;
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import java.time.format.DateTimeParseException;
+import duke.task.Deadline;
+import org.junit.jupiter.api.Test;
+
+public class DeadlineTest {
+    @Test
+    public void createDeadline_validDate_success() {
+        assertDoesNotThrow(() -> new Deadline("test", "1999-12-25"));
+    }
+
+    @Test
+    public void createDeadline_invalidDateFormat_throwsException() {
+        assertThrows(java.time.format.DateTimeParseException.class, () -> {
+            new Deadline("test", "abcd");
+        });
+    }
+
+    @Test
+    public void toString_validInputs_outputsCorrectly() {
+        String expected = new Deadline("some item", "2021-01-01").toString();
+        assertEquals("[D][ ] some item (by: Friday, Jan 1 2021)", expected);
+    }
+
+}
---
+++ src/test/java/duke/EventTest.java
@@ -0,0 +1,29 @@
+package duke;
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import java.time.format.DateTimeParseException;
+import duke.task.Event;
+import org.junit.jupiter.api.Test;
+
+public class EventTest {
+    @Test
+    public void createEvent_validDate_success() {
+        assertDoesNotThrow(() -> new Event("test", "1999-12-25"));
+    }
+
+    @Test
+    public void createDeadline_invalidDateFormat_throwsException() {
+        assertThrows(java.time.format.DateTimeParseException.class, () -> {
+            new Event("test", "abcd");
+        });
+    }
+
+    @Test
+    public void toString_validInputs_outputsCorrectly() {
+        String expected = new Event("some item", "2021-01-01").toString();
+        assertEquals("[E][ ] some item (at: Friday, Jan 1 2021)", expected);
+    }
+
+}
