db9809929011371ffa193caf4c3ba7753d9aa4e2
tanboonji <tanboonji@hotmail.com>
2021-01-27 20:41:14 +0800

    Add InvalidCommand class

--- src/main/java/CommandParser.java
+++ src/main/java/CommandParser.java
@@ -5,7 +5,7 @@ public class CommandParser {
 
     private static final Pattern COMMAND_FORMAT = Pattern.compile("(\\S+)\\W*(.*)");
 
-    public static Command parse(String input) throws DukeException {
+    public static Command parse(String input) {
         Matcher matcher = COMMAND_FORMAT.matcher(input);
         if (!matcher.matches()) {
             return new HelpCommand(true);
@@ -14,25 +14,29 @@ public class CommandParser {
         String command = matcher.group(1);
         String arguments = matcher.group(2);
 
-        switch (command.toLowerCase()) {
-        case ToDoCommand.COMMAND:
-            return ToDoCommand.parseArguments(arguments);
-        case EventCommand.COMMAND:
-            return EventCommand.parseArguments(arguments);
-        case DeadlineCommand.COMMAND:
-            return DeadlineCommand.parseArguments(arguments);
-        case ListCommand.COMMAND:
-            return new ListCommand();
-        case DoneCommand.COMMAND:
-            return DoneCommand.parseArguments(arguments);
-        case DeleteCommand.COMMAND:
-            return DeleteCommand.parseArguments(arguments);
-        case HelpCommand.COMMAND:
-            return new HelpCommand(false);
-        case ByeCommand.COMMAND:
-            return new ByeCommand();
-        default:
-            return new HelpCommand(true);
+        try {
+            switch (command.toLowerCase()) {
+            case ToDoCommand.COMMAND:
+                return ToDoCommand.parseArguments(arguments);
+            case EventCommand.COMMAND:
+                return EventCommand.parseArguments(arguments);
+            case DeadlineCommand.COMMAND:
+                return DeadlineCommand.parseArguments(arguments);
+            case ListCommand.COMMAND:
+                return new ListCommand();
+            case DoneCommand.COMMAND:
+                return DoneCommand.parseArguments(arguments);
+            case DeleteCommand.COMMAND:
+                return DeleteCommand.parseArguments(arguments);
+            case HelpCommand.COMMAND:
+                return new HelpCommand(false);
+            case ByeCommand.COMMAND:
+                return new ByeCommand();
+            default:
+                return new HelpCommand(true);
+            }
+        } catch (DukeException e) {
+            return new InvalidCommand(e.getMessage());
         }
     }
 }
---
+++ src/main/java/InvalidCommand.java
@@ -0,0 +1,23 @@
+public class InvalidCommand extends Command {
+
+    String errorMessage;
+
+    public InvalidCommand(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public boolean shouldExit() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldSave() {
+        return false;
+    }
+
+    @Override
+    public String execute() {
+        return errorMessage;
+    }
+}
