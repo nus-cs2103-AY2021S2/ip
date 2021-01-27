e8da2c169123217fe9581d5a50c9135d72a65130
Prabhakaran-Gokul <prabhakaran.gokul@gmail.com>
2021-01-27 23:13:46 +0800

    Fix code to follow coding standard

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -13,54 +13,54 @@ public class Parser {
         String firstWord = arr[0];
         String time;
         switch (firstWord) {
-            case "bye":
-                this.isAlive = false;
-                break;
-            case "list":
-                taskList.displayTasks();
-                break;
-            case "done":
-                try {
-                    String num = arr[1];
-                    //TODO exception handling if num is not a number
-                    taskList.markAsDone(Integer.valueOf(num));
-                } catch (NumberFormatException e) {
-                    throw new DukeException("Enter an integer only");
-                }
+        case "bye":
+            this.isAlive = false;
+            break;
+        case "list":
+            taskList.displayTasks();
+            break;
+        case "done":
+            try {
+                String num = arr[1];
+                //TODO exception handling if num is not a number
+                taskList.markAsDone(Integer.valueOf(num));
+            } catch (NumberFormatException e) {
+                throw new DukeException("Enter an integer only");
+            }
 
-                break;
-            case "delete":
-                try {
-                    String num = arr[1];
-                    taskList.deleteTask(Integer.valueOf(num));
-                } catch (NumberFormatException e) {
-                    throw new DukeException("Enter an integer only");
-                }
+            break;
+        case "delete":
+            try {
+                String num = arr[1];
+                taskList.deleteTask(Integer.valueOf(num));
+            } catch (NumberFormatException e) {
+                throw new DukeException("Enter an integer only");
+            }
 
-                break;
-            case "todo":
-                if (arr.length == 1) {
-                    throw new DukeException("Sorry, description of a todo cannot be empty");
-                }
-                String toDo = arr[1];
-                taskList.addTask(new ToDo(toDo));
-                break;
-            case "deadline":
-                if (arr.length == 1) {
-                    throw new DukeException("Sorry description of a deadline cannot be empty");
-                }
-                String deadline = arr[1];
-                taskList.addTask(new Deadline(getMsg(deadline, " /by "), getDateTime(deadline, " /by ")));
-                break;
-            case "event":
-                if (arr.length == 1) {
-                    throw new DukeException("Sorry description of an event cannot be empty");
-                }
-                String event = arr[1];
-                taskList.addTask(new Event(getMsg(event, " /at "), getDateTime(event, " /at ")));
-                break;
-            default:
-                taskList.complain();
+            break;
+        case "todo":
+            if (arr.length == 1) {
+                throw new DukeException("Sorry, description of a todo cannot be empty");
+            }
+            String toDo = arr[1];
+            taskList.addTask(new ToDo(toDo));
+            break;
+        case "deadline":
+            if (arr.length == 1) {
+                throw new DukeException("Sorry description of a deadline cannot be empty");
+            }
+            String deadline = arr[1];
+            taskList.addTask(new Deadline(getMsg(deadline, " /by "), getDateTime(deadline, " /by ")));
+            break;
+        case "event":
+            if (arr.length == 1) {
+                throw new DukeException("Sorry description of an event cannot be empty");
+            }
+            String event = arr[1];
+            taskList.addTask(new Event(getMsg(event, " /at "), getDateTime(event, " /at ")));
+            break;
+        default:
+            taskList.complain();
 
         }
     }
