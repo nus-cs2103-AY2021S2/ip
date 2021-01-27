a87ce68ffcaba3f3ae780aea709cfc857a155fb1
Sidney011100 <sidney011100@gmail.com>
2021-01-27 17:18:17 +0800

    Create more OOP files

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,91 +13,52 @@ public class Duke {
     //Parser
     static String input = " ";
 
-    //Storage
-    // static File fileTest= new File("PreviousTaskList.txt");
     static Storage storage = new Storage(); 
 
-    //TaskList
-    // static ArrayList<Task> list = new ArrayList<>();
     static TaskList list = new TaskList();
+
+    static Parser parser = new Parser();
     
     static Ui userInterface = new Ui(); 
     public static void main(String[] args) throws Exception {
-        // System.out.println("file exists?: " + fileTest.exists());
-        // if(fileTest.exists()) {
-        //     Scanner s = new Scanner(fileTest);
-        //     while (s.hasNext()) {
-        //         String current = s.nextLine().toLowerCase();
-        //         // System.out.println(current);
-        //         if(current.contains("todo")) {
-        //             Task task = Todo.readTask(current);
-        //             list.add(task);
-        //         } else if (current.contains("deadline")) {
-        //             list.add(Deadline.readTask(current));
-        //         } else if (current.contains("event")) {
-        //             list.add(Event.readTask(current));
-        //         } else {
-        //             if(s.hasNext()){
-        //                 current = s.nextLine();
-        //             } else{
-        //                 throw new Exception("History saved corrupted");
-        //             }
-        //         }
-        //     }
-        //     s.close();
-        // }
         if(storage.isSavedHistory()) {
             storage.initialise(list);
         }
 
-        //Ui
-        // String greet = "Hello! I'm Duke \n What can I do for you?";
-        // System.out.println(greet);
-        
         userInterface.welcomeUser();
 
         //Parser
-        Scanner sc = new Scanner(System.in);
-        input = sc.nextLine();
-        cleanInput();
-        Call call;
-        while (!input.contains("bye")) {
-            if(input.contains("list")) {
-                call = Call.LIST;
-            } else if (input.contains("done")){
-                call = Call.DONE;
-            } else if (input.contains("todo")) {
-                call = Call.TODO;
-            } else if (input.contains("event")) {
-                call = Call.EVENT;
-            } else if (input.contains("deadline")) {
-                call = Call.DEADLINE;
-            } else if (input.contains("delete")) {
-                call = Call.DELETE;
-            } else {
-                if(sc.hasNextLine()) {
-                    input = sc.nextLine();
-                } else {
-                    throw new Exception("Please enter a valid command!");
-                }
-                continue;
-            }
-            switch (call) {
-                case LIST -> commandList();
-                case DONE -> commandDone();
-                case TODO -> commandTodo();
-                case DEADLINE -> commandDeadline();
-                case EVENT -> commandEvent();
-                case DELETE -> commandDelete();
-            }
-            input = sc.nextLine();
-            cleanInput();
-        }
+        parser.readInput(list);
+        // Scanner sc = new Scanner(System.in);
+        // input = sc.nextLine();
+        // cleanInput();
+        // while (!input.contains("bye")) {
+        //     if(input.contains("list")) {
+        //         commandList();
+        //     } else if (input.contains("done")){
+        //         commandDone();
+        //     } else if (input.contains("todo")) {
+        //         commandTodo();
+        //     } else if (input.contains("event")) {
+        //         commandEvent();
+        //     } else if (input.contains("deadline")) {
+        //         commandDeadline();
+        //     } else if (input.contains("delete")) {
+        //         commandDelete();
+        //     } else {
+        //         if(sc.hasNextLine()) {
+        //             input = sc.nextLine();
+        //         } else {
+        //             throw new Exception("Please enter a valid command!");
+        //         }
+        //         continue;
+        //     }
+        //     input = sc.nextLine();
+        //     cleanInput();
+        // }
 
-        //Ui
-        // System.out.println("Bye. Hope to see you again soon!");
-        userInterface.userLeaving();
-        sc.close();
+        // userInterface.userLeaving();
+        // sc.close();
 
         //Storage
         try {
@@ -107,98 +68,93 @@ public class Duke {
         }
     }
 
-    //Parser
-    static void commandDone() throws ArrayIndexOutOfBoundsException {
-        try {
-            String value = input.split(" ")[1];
-            int val = Integer.parseInt(value) - 1;
-            list.markDone(val);
-            userInterface.userDoneTask(list.getTaskAtIndex(val).toString());
-        } catch (ArrayIndexOutOfBoundsException e) {
-            throw new ArrayIndexOutOfBoundsException("Please key in which task number to tick off.");
-        }
+    // //Parser
+    // static void commandDone() throws ArrayIndexOutOfBoundsException {
+    //     try {
+    //         int val = parser.inputContainsDoneAtIndex();
+    //         list.markDone(val);
+    //         userInterface.userDoneTask(list.getTaskAtIndex(val).toString());
+    //     } catch (ArrayIndexOutOfBoundsException e) {
+    //         throw new ArrayIndexOutOfBoundsException("Please key in which task number to tick off.");
+    //     }
 
-    }
+    // }
 
-    //Parser
-    static void cleanInput() {
-        input = input.replaceAll("\n", "");
-        input = input.toLowerCase();
-    }
+    // //Parser
+    // static void cleanInput() {
+    //     input = input.replaceAll("\n", "");
+    //     input = input.toLowerCase();
+    // }
 
-    //Parser
-    static void commandTodo() throws Exception{
-        String task = input.replaceFirst("todo", "");
-        task = task.stripTrailing();
-        if (task.isEmpty()) {
-            throw new Exception("☹ OOPS!!! The description of a todo cannot be empty.");
-        }
-        list.addTodo(task);
-        userInterface.userAddTask(list);
-    }
+    // //Parser
+    // static void commandTodo() throws Exception{
+    //     String task = parser.inputTaskToDoIs();
+    //     list.addTodo(task);
+    //     userInterface.userAddTask(list);
+    // }
 
-    //Parser
-    static void commandDeadline() {
-        String deadline = input.replaceFirst("deadline", "");
-        deadline = deadline.stripLeading();
-        String[] deadlineArr = deadline.split("/", 2);
-        list.addDeadline(deadlineArr[0], inputDeadline(deadlineArr[1]));
-        userInterface.userAddTask(list);
-    }
+    // //Parser
+    // static void commandDeadline() {
+    //     String deadline = input.replaceFirst("deadline", "");
+    //     deadline = deadline.stripLeading();
+    //     String[] deadlineArr = deadline.split("/", 2);
+    //     list.addDeadline(deadlineArr[0], inputDeadline(deadlineArr[1]));
+    //     userInterface.userAddTask(list);
+    // }
 
-    //Parser
-    static void commandEvent() {
-        String eventDetails = input.replaceFirst("event", "");
-        eventDetails = eventDetails.stripLeading();
-        String[] eventDeats = eventDetails.split("/", 2);
-        list.addEvent(eventDeats[0], inputDeadline(eventDeats[1]));
-        userInterface.userAddTask(list);
-    }
+    // //Parser
+    // static void commandEvent() {
+    //     String eventDetails = input.replaceFirst("event", "");
+    //     eventDetails = eventDetails.stripLeading();
+    //     String[] eventDeats = eventDetails.split("/", 2);
+    //     list.addEvent(eventDeats[0], inputDeadline(eventDeats[1]));
+    //     userInterface.userAddTask(list);
+    // }
 
-    //Parser
-    static void commandDelete() {
-        String value = input.replaceFirst("delete", "");
-        value = value.strip();
-        int val = Integer.parseInt(value);
-        Task delete = list.deleteTaskAtIndex(val - 1);
-        userInterface.userDeleteTask(delete, list);
-    }
+    // //Parser
+    // static void commandDelete() {
+    //     String value = input.replaceFirst("delete", "");
+    //     value = value.strip();
+    //     int val = Integer.parseInt(value);
+    //     Task delete = list.deleteTaskAtIndex(val - 1);
+    //     userInterface.userDeleteTask(delete, list);
+    // }
 
-    //Parser
-    static void commandList() {
-        list.listAllTasks();
-    }
+    // //Parser
+    // static void commandList() {
+    //     list.listAllTasks();
+    // }
 
     //Parser
-    static LocalDateTime inputDeadline(String inputDate) {
-        String[] dataArray = inputDate.split(" ");
-        LocalDate formatDate = inputDate(dataArray[1]);
-        LocalTime formatTime = inputTime(dataArray[2]);
-        return LocalDateTime.of(formatDate, formatTime);
-    }
+    // static LocalDateTime inputDeadline(String inputDate) {
+    //     String[] dataArray = inputDate.split(" ");
+    //     LocalDate formatDate = inputDate(dataArray[1]);
+    //     LocalTime formatTime = inputTime(dataArray[2]);
+    //     return LocalDateTime.of(formatDate, formatTime);
+    // }
 
-    //Parser
-    static LocalDate inputDate(String input) {
-        String[] dateArray = input.split("/");
-        LocalDate date = LocalDate.of(Integer.parseInt(dateArray[2]), 
-                                            Integer.parseInt(dateArray[1]), 
-                                            Integer.parseInt(dateArray[0]));
-        return date;
-    }
+    // //Parser
+    // static LocalDate inputDate(String input) {
+    //     String[] dateArray = input.split("/");
+    //     LocalDate date = LocalDate.of(Integer.parseInt(dateArray[2]), 
+    //                                         Integer.parseInt(dateArray[1]), 
+    //                                         Integer.parseInt(dateArray[0]));
+    //     return date;
+    // }
 
-    //Parser
-    static LocalTime inputTime(String input) {
-        String hour = input.substring(0, 2);
-        String minutes = input.substring(2); 
-        LocalTime time = LocalTime.of(Integer.parseInt(hour), Integer.parseInt(minutes));
-        return time;
-    }
+    // //Parser
+    // static LocalTime inputTime(String input) {
+    //     String hour = input.substring(0, 2);
+    //     String minutes = input.substring(2); 
+    //     LocalTime time = LocalTime.of(Integer.parseInt(hour), Integer.parseInt(minutes));
+    //     return time;
+    // }
 
-    //Parser
-    static boolean validCommand() {
-        return input.contains("delete") || input.contains("event") || input.contains("done") ||
-                input.contains("todo") || input.contains("deadline") || input.contains("list");
-    }
+    // //Parser
+    // static boolean validCommand() {
+    //     return input.contains("delete") || input.contains("event") || input.contains("done") ||
+    //             input.contains("todo") || input.contains("deadline") || input.contains("list");
+    // }
 
     static void byeCommand() throws IOException {
         storage.saveHistory(list);
---
+++ src/main/java/Parser.java
@@ -0,0 +1,186 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class Parser {
+    private Scanner scanner; 
+    static String input;
+    Ui userInterface;
+
+    Parser(){
+        scanner = new Scanner(System.in);
+        userInterface = new Ui();
+    }
+
+    private void removeExtraLinesAndSpacesFromInput() {
+        input = input.replaceAll("\n", "");
+        input = input.toLowerCase();
+    }
+
+    public void readInput(TaskList list) throws Exception {
+        if(scanner.hasNextLine()) {
+            input = scanner.nextLine(); 
+            removeExtraLinesAndSpacesFromInput();
+            analyseInput(list);
+        } else {
+            userInterface.userLeaving();
+            scanner.close();
+        }
+
+    }
+
+    public Boolean inputDoesNotContainBye() {
+        return !input.contains("bye");
+    }
+
+    private Boolean inputContainsDone() {
+        return input.contains("done");
+    }
+
+    private Boolean inputContainsTodo() {
+        return input.contains("todo");
+    }
+
+    private Boolean inputContainsEvent() {
+        return input.contains("event");
+    }
+
+    private Boolean inputContainsDeadline() {
+        return input.contains("deadline");
+    }
+
+    private Boolean inputContainsDelete() {
+        return input.contains("delete");
+    }
+
+    private Boolean inputContainsList() {
+        return input.contains("list");
+    }
+
+
+/////////////Command Done start ///////////
+    public int inputContainsDoneAtIndex() throws ArrayIndexOutOfBoundsException {
+        String value = input.split(" ")[1];
+        int val = Integer.parseInt(value) - 1;
+        return val;
+    }
+
+    public void markDoneAtIndex(TaskList list) {
+        int index = inputContainsDoneAtIndex();
+        list.markDone(index);
+        userInterface.userDoneTask(list.getTaskAtIndex(index).toString());
+    }
+    
+///////Command Done End ////////////////
+
+//////////////Command ToDo Start ////////////////
+
+    public String inputTaskToDoIs() throws ArrayIndexOutOfBoundsException {
+        String task = input.replaceFirst("todo", "");
+        task = task.stripTrailing();
+        if (task.isEmpty()) {
+            throw new ArrayIndexOutOfBoundsException("☹ OOPS!!! The description of a todo cannot be empty.");
+        }
+        return task;
+    }
+
+    public void addTodoTask(TaskList list) {
+        list.addTodo(inputTaskToDoIs());
+        userInterface.userAddTask(list);
+    }
+
+///////////Command todo End ////////////////////
+
+
+/////Command Deadline and Event Start /////////////
+    public String[] findTaskWithDate() {
+        if(inputContainsEvent()) {
+            input = input.replaceFirst("event", "");
+        } else {
+            input = input.replaceFirst("deadline", "");
+        }
+        input = input.stripLeading();
+        return input.split("/", 2);
+    }
+
+    static LocalDateTime inputDateAndTime(String inputDate) {
+        String[] dataArray = inputDate.split(" ");
+        LocalDate formatDate = inputDate(dataArray[1]);
+        LocalTime formatTime = inputTime(dataArray[2]);
+        return LocalDateTime.of(formatDate, formatTime);
+    }
+
+    static LocalDate inputDate(String input) {
+        String[] dateArray = input.split("/");
+        LocalDate date = LocalDate.of(Integer.parseInt(dateArray[2]), 
+                                            Integer.parseInt(dateArray[1]), 
+                                            Integer.parseInt(dateArray[0]));
+        return date;
+    }
+
+    static LocalTime inputTime(String input) {
+        String hour = input.substring(0, 2);
+        String minutes = input.substring(2); 
+        LocalTime time = LocalTime.of(Integer.parseInt(hour), Integer.parseInt(minutes));
+        return time;
+    }
+
+    public void addEventTask(TaskList list) {
+        String[] taskAndDate = findTaskWithDate();
+        list.addEvent(taskAndDate[0], inputDateAndTime(taskAndDate[1]));
+        userInterface.userAddTask(list);
+    } 
+
+    public void addDeadlineTask(TaskList list) {
+        String[] taskAndDate = findTaskWithDate();
+        list.addDeadline(taskAndDate[0], inputDateAndTime(taskAndDate[1]));
+        userInterface.userAddTask(list);
+    }
+
+////////// command event and deadline end //////////////
+
+////////////// Command delete START ///////////////
+
+    public void commandDelete(TaskList list) {
+        String value = input.replaceFirst("delete", "");
+        value = value.strip();
+        int val = Integer.parseInt(value);
+        Task delete = list.deleteTaskAtIndex(val - 1);
+        userInterface.userDeleteTask(delete, list);
+    }
+
+////////////// Command delete End ///////////////
+
+
+
+
+    private void analyseInput(TaskList list) throws Exception {
+        while(inputDoesNotContainBye()) {
+            if(inputContainsDone()) {
+                markDoneAtIndex(list);
+            } else if (inputContainsTodo()) {
+                addTodoTask(list);
+            } else if (inputContainsDeadline()) {
+                addDeadlineTask(list);
+            } else if (inputContainsEvent()) {
+                addEventTask(list);
+            } else if (inputContainsDelete()) {
+                commandDelete(list);
+            } else if (inputContainsList()) {
+                list.listAllTasks();
+            } else {
+                if(scanner.hasNextLine()) {
+                    input = scanner.nextLine();
+                } else {
+                    throw new Exception("Please enter a valid command!");
+                }
+                continue;
+            }
+            readInput(list);
+        }
+    }
+
+
+
+}
