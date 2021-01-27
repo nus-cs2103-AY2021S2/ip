7b5145c232c77688744c5f6b9726cba6b59a00cb
yuchen <yuchenwang97@gmail.com>
2021-01-28 04:03:19 +0800

    Add support for datetime
    Currently only support for date strings in the format of "yyyy-mm-dd" are supported.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,17 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    private String timing;
+    private LocalDate timing;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-dd");
 
     Event(String name, String timing) {
         super(name);
-        this.timing = timing;
+        this.timing = LocalDate.parse(timing, formatter);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (by: " + this.timing + ")";
+        return "[E]" + super.toString() + " (by: " + this.timing.format(DateTimeFormatter.ofPattern("MMM D YYYY")) + ")";
     }
 }
