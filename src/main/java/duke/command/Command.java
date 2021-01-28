f519c2ba5ace092f8ee202b2f173cb10a4e60fe0
GuoJun <guojunloh1234@gmail.com>
2021-01-29 00:43:20 +0800

    Add JUnit Tests

--- src/main/java/duke/command/Command.java
+++ src/main/java/duke/command/Command.java
@@ -6,6 +6,7 @@ import duke.task.TaskList;
 import duke.Ui;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public abstract class Command {
     protected String command;
@@ -21,9 +22,55 @@ public abstract class Command {
         this.date = date;
         this.isExit = isExit;
     }
+
     public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException;
 
     public boolean isExit() {
         return this.isExit;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        } else if (obj instanceof Command) {
+            Command other = (Command) obj;
+            boolean b1 = checkEquality(this.command, other.command);
+            boolean b2 = checkEquality(this.description, other.description);
+            boolean b3 = checkEquality(this.preposition, other.preposition);
+            boolean b4 = checkEqualityDate(this.date, other.date);
+            boolean b5 = this.isExit == other.isExit;
+
+            return b1 && b2 && b3 && b4 && b5;
+
+        } else {
+            return false;
+        }
+    }
+
+    boolean checkEquality(String s1, String s2) {
+        if(s1 == null) {
+            return s2 == null;
+        } else {
+            if (s2 == null) {
+                return false;
+            } else {
+                return s1.equals(s2);
+            }
+        }
+    }
+
+    boolean checkEqualityDate(LocalDate d1, LocalDate d2) {
+        if(d1 == null) {
+            return d2 == null;
+        } else {
+            if (d2 == null) {
+                return false;
+            } else {
+                String s1 = d1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                String s2 = d2.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                return s1.equals(s2);
+            }
+        }
     }
 }
---
+++ src/test/java/duke/MainTest.java
@@ -0,0 +1,13 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    public void dummyTest(){
+        Main main = new Main("../data");
+    }
+}
+
