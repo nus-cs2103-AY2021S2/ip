524bc2f059413c58092c3eecd742b1ca2f73d3c4
jaredtengsw <jared_teng_@hotmail.com>
2021-01-28 22:21:19 +0800

    Add header comment

--- src/main/java/duke/exceptions/MissingArgumentException.java
+++ src/main/java/duke/exceptions/MissingArgumentException.java
@@ -1,7 +1,12 @@
 package duke.exceptions;
 
+/**
+ * Represents the exception when using the 'done' command
+ * without inputting the task number.
+ */
+
 public class MissingArgumentException extends Exception{
-    public MissingArgumentException(String err) {
-        super(err);
+    public MissingArgumentException(String error) {
+        super(error);
     }
 }
--- src/main/java/duke/exceptions/NoKeywordException.java
+++ src/main/java/duke/exceptions/NoKeywordException.java
@@ -1,7 +1,12 @@
 package duke.exceptions;
 
+/**
+ * Represents the exception from inputting a command
+ * that is non-existing or not recognised by Alfred.
+ */
+
 public class NoKeywordException extends Exception{
-    public NoKeywordException(String err) {
-        super(err);
+    public NoKeywordException(String error) {
+        super(error);
     }
 }
