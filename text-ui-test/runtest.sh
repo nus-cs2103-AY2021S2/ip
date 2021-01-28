97159364a93c8efb2d658c80f2b8827873993d20
Tan Jin <cjtanjin@gmail.com>
2021-01-28 17:21:38 +0800

    Refactor: update gradle build file and runtest script

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -21,7 +21,7 @@ fi
 
 # modified to compile with json-simple
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ~/.gradle/caches/modules-2/files-2.1/com.googlecode.json-simple/json-simple/1.1.1/c9ad4a0850ab676c5c64461a05ca524cdfff59f1/json-simple-1.1.1.jar:./ ../src/main/java/com/tjtanjin/ip/*.java -Xlint:none -d ../bin ../src/main/java/com/tjtanjin/ip/*.java
+if ! javac -cp ~/.gradle/caches/modules-2/files-2.1/com.googlecode.json-simple/json-simple/1.1.1/c9ad4a0850ab676c5c64461a05ca524cdfff59f1/json-simple-1.1.1.jar:./ ../src/main/java/com/tjtanjin/steve/*.java ../src/main/java/com/tjtanjin/steve/*/*.java -Xlint:none -d ../bin ../src/main/java/com/tjtanjin/steve/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
@@ -29,7 +29,7 @@ fi
 
 # modified to run with json-simple
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -cp ~/.gradle/caches/modules-2/files-2.1/com.googlecode.json-simple/json-simple/1.1.1/c9ad4a0850ab676c5c64461a05ca524cdfff59f1/json-simple-1.1.1.jar:./:../bin com.tjtanjin.ip.Duke < input.txt > ACTUAL.TXT
+java -cp ~/.gradle/caches/modules-2/files-2.1/com.googlecode.json-simple/json-simple/1.1.1/c9ad4a0850ab676c5c64461a05ca524cdfff59f1/json-simple-1.1.1.jar:./:../bin com.tjtanjin.steve.Steve < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
