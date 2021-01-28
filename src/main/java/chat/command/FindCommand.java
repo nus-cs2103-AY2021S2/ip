cc73311c83c8f0d8af4ccb6ec3d50bc616fe352d
clarlzx <clarlzx@gmail.com>
2021-01-28 23:55:20 +0800

    Level-9: Fix conflict errors for FindCommand class
    Level-9: Add JavaDoc comments to FindCommand class

--- src/main/java/chat/command/FindCommand.java
+++ src/main/java/chat/command/FindCommand.java
@@ -5,23 +5,39 @@ import chat.task.Task;
 
 import java.util.ArrayList;
 
+/**
+ * A type of command that finds tasks based on keywords given by user
+ */
 public class FindCommand extends Command {
     
     String inputStr;
-    
+
+    /**
+     * Initialises FindCommand object.
+     * 
+     * @param inputStr Inputted command string from user to Chat the Cat.
+     */
     public FindCommand(String inputStr) {
         this.inputStr = inputStr;
     }
-    
+
+    /**
+     * Method that finds tasks from list of tasks from TaskList object.
+     * 
+     * @param tasks TaskList object that contains a list of tasks.
+     * @param ui Ui object that gives responses to user.
+     * @param storage Storage object that interacts with task data in hard disk.
+     * @throws ChatException If no tasks match keyword given by user.
+     */
     public void execute(TaskList tasks, Ui ui, Storage storage) throws ChatException {
         String keyWord = this.inputStr.replace("find","").strip();
         TaskList foundTaskList = new TaskList(new ArrayList<Task>());
-        for (Task task : tasks.getTaskList()) {
+        for (Task task : tasks.getTasks()) {
             if (task.getName().contains(keyWord)) {
-                foundTaskList.getTaskList().add(task);
+                foundTaskList.getTasks().add(task);
             }
         }
-        if (foundTaskList.getTaskList().isEmpty()) {
+        if (foundTaskList.getTasks().isEmpty()) {
             throw new ChatException("Sorry! No results found.");
         } else {
             ui.list(foundTaskList);
