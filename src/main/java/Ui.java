406820f7cc899c6620bc01da80eb888063000729
andrewzhang <zhangguangzesheng@gmail.com>
2021-01-28 23:18:54 +0800

    Add Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,11 @@
+public class Ui {
+    public static void greeting() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+    
+}
