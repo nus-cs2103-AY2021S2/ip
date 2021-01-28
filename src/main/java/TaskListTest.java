7af8a52df6d87b58fa7e5b5b41b81a59793bde37
icytornado <perfecttzp@gmail.com>
2021-01-28 20:35:46 +0800

    A-JUnit

---
+++ src/main/java/TaskListTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TaskListTest {
+    ArrayList<Task> l = new ArrayList<Task>();
+    Task t = new Task("todo borrow book");
+
+    @Test
+     void addToList() {
+        new TaskList().addToList(l, t);
+        assertEquals(1, l.size());
+
+    }
+
+    @Test
+    void deleteFromList() {
+        new TaskList().addToList(l, t);
+        new TaskList().deleteFromList(l,1);
+        assertEquals(0, l.size());
+    }
+}
---
+++ src/main/java/TaskTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    Task task = new Task("deadline return book /by Sunday");
+    int checker = 0;
+
+    @Test
+    void markAsDone() {
+
+        task.markAsDone();
+        if (task.isDone = true) {
+            checker =1;
+        }
+        assertEquals(1, checker);
+
+    }
+}
