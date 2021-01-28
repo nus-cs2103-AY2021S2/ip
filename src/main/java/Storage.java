98b6d7472fc6015d517da836d0d4cca7a645279f
juhaerpasha <juhaeradittya200@gmail.com>
2021-01-28 11:48:01 +0800

    Update Storage

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 public class Storage {
     private File file;
 
+    /**
+     * Constructor to initialize storage
+     */
     public Storage(String filePath) throws IOException {
         file = new File(filePath);
         file.getParentFile().mkdirs();
