f769d7d54e70008155f38f0083ad41dfb9431b0c
Juliet <teohqianying@gmail.com>
2021-01-28 18:14:59 +0800

    Add 2 J-unit test, one that tests the Deadline task and the other tests the Event task

---
+++ src/test/javacode/EventTest.java
@@ -0,0 +1,29 @@
+package javacode;
+
+import duke.exceptions.TaskException;
+
+import duke.task.Event;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EventTest {
+    @Test
+    public void checkStrings() {
+        String output = new Event("test", "NUS school").toString();
+        String output1 = new Event("test", "NUS school").toSaveFormat();
+
+        assertEquals("[E][ ] test (at: NUS school)", output);
+        assertEquals("E|0|test|NUS school", output1);
+
+    }
+
+    @Test
+    public void checkDoneStateChange(){
+        Event event = new Event("testing at", "Now");
+        event.changeTaskToDone();
+
+        assertEquals("[E][X] testing at (at: Now)", event.toString());
+        assertEquals("E|1|testing at|Now", event.toSaveFormat());
+    }
+}
