b4a47a82bb862299ae91496cdd49f913042f225c
Figo Lee Ze Xin <e0420458@u.nus.edu>
2021-01-28 14:34:51 +0800

    Level 9. Find

--- src/main/java/duke/Task.java
+++ src/main/java/duke/Task.java
@@ -10,7 +10,8 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "[\u2718]" : "[ ]"); //return tick or X symbols
+        return (isDone ? "[\u2718]" : "[ ]");
+        //return tick or X symbols
     }
 
     public void markAsDone() {
