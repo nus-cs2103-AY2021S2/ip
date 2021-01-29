7c5428396ea6bb463091188ee0bc0fd04f77fe32
Benedict Khoo <benedictkhoo.mw@u.nus.edu>
2021-01-29 09:08:49 +0800

    Fix FindCommand import order

--- src/main/java/duke/commands/FindCommand.java
+++ src/main/java/duke/commands/FindCommand.java
@@ -1,11 +1,11 @@
 package duke.commands;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import duke.storage.Storage;
 import duke.task.Task;
 import duke.task.TaskList;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Searches (case-sensitive) for tasks containing a search term.
