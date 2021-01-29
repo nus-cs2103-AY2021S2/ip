d8bfafae5c341c4ecda68a2a04eff5711fc11404
Sharptail <kongjianwei@outlook.com>
2021-01-29 11:35:40 +0800

    Updates Coding Standard

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -8,22 +8,22 @@ public class Parser {
     public static Command parse(String commandStr) throws SnomException {
         CommandEnum commandEnum = CommandEnum.getCommand(commandStr);
         switch(commandEnum){
-            case LIST:
-                return new ListCommand(CommandEnum.LIST);
-            case FINISH:
-                return new FinishCommand(CommandEnum.FINISH);
-            case DELETE:
-                return new DeleteCommand(CommandEnum.DELETE);
-            case BYE:
-                return new ExitCommand(CommandEnum.BYE);
-            case TODO:
-                return new AddCommand(CommandEnum.TODO);
-            case DEADLINE:
-                return new AddCommand(CommandEnum.DEADLINE);
-            case EVENT:
-                return new AddCommand(CommandEnum.EVENT);
-            default:
-                throw new SnomException("OOPS!!! I'm sorry, but I don't know what '\" + input + \"' means :-(\"");
+        case LIST:
+            return new ListCommand(CommandEnum.LIST);
+        case FINISH:
+            return new FinishCommand(CommandEnum.FINISH);
+        case DELETE:
+            return new DeleteCommand(CommandEnum.DELETE);
+        case BYE:
+            return new ExitCommand(CommandEnum.BYE);
+        case TODO:
+            return new AddCommand(CommandEnum.TODO);
+        case DEADLINE:
+            return new AddCommand(CommandEnum.DEADLINE);
+        case EVENT:
+            return new AddCommand(CommandEnum.EVENT);
+        default:
+            throw new SnomException("OOPS!!! I'm sorry, but I don't know what '\" + input + \"' means :-(\"");
         }
     }
 }
