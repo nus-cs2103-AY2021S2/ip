a0187891467fc0f738e0cb0658730246cd99f6ef
Jonathan <banchiang98@gmail.com>
2021-01-29 19:02:49 +0800

    Add JUnit tests for Tasklist and Task

---
+++ src/test/java/duke/TaskListTest.java
@@ -0,0 +1,25 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+
+    @Test
+    public void testAddTask() {
+        TaskList tl = new TaskList();
+        Task t1 = new ToDo("read book");
+        tl.add(t1);
+        Task t2 = tl.get(0);
+        assertEquals(t1.getName(), t2.getName());
+    }
+
+    @Test
+    public void testDeleteTask() {
+        TaskList tl = new TaskList();
+        Task t1 = new ToDo("read book");
+        tl.add(t1);
+        Task t2 = tl.remove(0);
+        assertEquals(t1.getName() , t2.getName());
+    }
+}
---
+++ src/test/java/duke/TaskTest.java
@@ -0,0 +1,26 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+
+    @Test
+    public void testGetName() {
+        Task t = new ToDo("read book");
+        assertEquals("read book", t.getName());
+    }
+
+    @Test
+    public void testGetIcon() {
+        Task t = new ToDo("read book");
+        assertEquals(" ", t.getStatusIcon());
+    }
+
+    @Test
+    public void testFinishTask() {
+        Task t = new ToDo("read book");
+        t = t.finishTask();
+        assertEquals("X", t.getStatusIcon());
+    }
+}
