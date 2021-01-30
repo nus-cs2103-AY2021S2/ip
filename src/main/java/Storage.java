00ce37129322394bbf029fb996cd088ed4176855
oeiyiping <yiping426@hotmail.com>
2021-01-30 22:35:43 +0800

    Edit to comply with CS2103 Checkstyle

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-=======
-import java.io.*;
-import java.lang.reflect.Array;
->>>>>>> branch-A-JavaDoc
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
