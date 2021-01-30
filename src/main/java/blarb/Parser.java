afbd52322d4f973590b4d7cce4fb3706c7190f1c
Huang Po-Wei <71061276+georgepwhuang@users.noreply.github.com>
2021-01-30 10:51:23 +0800

    Add Varargs usage
    Used Varargs for parsing.
    Cleaned code and fixed minor bugs.
    Opened nested exception handling blocks in the Processor class.
    
    Enhance code readability.

--- src/main/java/blarb/Parser.java
+++ src/main/java/blarb/Parser.java
@@ -13,12 +13,9 @@ class Parser {
     static CommandLine parse(String input) {
         String[] tokens = input.split(" ", 2);
         Command command = Command.command(tokens[0]);
-        String description;
         if (tokens.length == 1) {
-            description = "";
-        } else {
-            description = tokens[1];
+            return new CommandLine(command);
         }
-        return new CommandLine(command, description);
+        return new CommandLine(command, tokens[1]);
     }
 }
