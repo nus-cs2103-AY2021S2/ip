aaeb2212bf31e007f11166d0c3edde325f508530
lirc572 <admin@lirc572.com>
2021-01-30 23:42:20 +0800

    Docs: add javadoc for TodoTask

--- src/main/java/com/lirc572/ip/TodoTask.java
+++ src/main/java/com/lirc572/ip/TodoTask.java
@@ -1,10 +1,21 @@
 package com.lirc572.ip;
 
 public class TodoTask extends Task {
+
+    /**
+     * Constructs a new TodoTask with the specified name
+     *
+     * @param name The name of the task
+     */
     public TodoTask(String name) {
         super(name);
     }
 
+    /**
+     * Returns the string representation of the TodoTask for storage
+     *
+     * @return The string representation of the TodoTask for storage
+     */
     @Override
     public String toSavedString() {
         return String.format(
