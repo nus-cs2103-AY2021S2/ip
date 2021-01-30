22c0fe45eb8791b52c0d7a5c5cea84fac480fcc9
OhJunMing <ohjunming1998@gmail.com>
2021-01-30 17:57:35 +0800

    No search/find function
    Add SearchList class to print search results.
    
    Amend Ui class to handle find command.

---
+++ src/main/java/Duke/task/SearchList.java
@@ -0,0 +1,39 @@
+package duke.task;
+
+import duke.common.Response;
+
+import java.util.ArrayList;
+
+public class SearchList {
+    private ArrayList<Task> tasks;
+
+    SearchList() {
+        tasks = new ArrayList<Task>();
+    }
+
+    public void add(Task task) {
+        tasks.add(task);
+    }
+
+    public void list() {
+        String msg = "";
+        for(int i = 0; i < tasks.size(); i++) {
+            msg += (i + 1) + "." + tasks.get(i) + "\n";
+        }
+        enclose(Response.LIST.toString() + msg);
+    }
+
+    /**
+     * Prints output to user in generic format.
+     */
+    public void enclose(String reply) {
+        System.out.println("---------------------------------------");
+        System.out.println(reply);
+        System.out.println("---------------------------------------\n");
+    }
+
+    public  String status() {
+        return "Now you have " + tasks.size() + " tasks in the list.\n";
+    }
+}
+
