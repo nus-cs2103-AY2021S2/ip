e192425e23266198bc1b92853808218a6b6fa8fb
zoeykobe <kelvinzhou981209@gmail.com>
2021-01-30 20:18:43 +0800

    Level-10. GUI
    Set up GUI Skeleton

--- src/main/java/kelbot/Kelbot.java
+++ src/main/java/kelbot/Kelbot.java
@@ -1,18 +1,40 @@
 package kelbot;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
-public class Kelbot {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class Kelbot extends Application {
+    private Path path = Paths.get(".", "data", "Kelbot.txt");
     private Storage storage;
     private TaskList taskList;
     private UI ui;
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+    private Image user = new Image(getClass().getResourceAsStream("/images/User.jpg"));
+    private Image kelbot = new Image(getClass().getResourceAsStream("/images/Kelbot.jpg"));
     /**
      * Initializes Kelbot
-     *
-     * @param path The file path that Kelbot will read from to load up previous usage
      */
-    public Kelbot(java.nio.file.Path path) {
+    public Kelbot() {
         ui = new UI();
         storage = new Storage(path);
         try {
@@ -21,6 +43,80 @@ public class Kelbot {
             System.out.println(e.getMessage());
             taskList = new TaskList();
         }
+    }
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+        userInput = new TextField();
+        sendButton = new Button("Send");
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        scene = new Scene(mainLayout);
+        stage.setScene(scene);
+        stage.show();
+        stage.setTitle("Kelbot");
+        stage.setResizable(false);
+        stage.setMinHeight(500.0);
+        stage.setMinWidth(400.0);
+        mainLayout.setPrefSize(400.0, 600.0);
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        userInput.setPrefWidth(325.0);
+        sendButton.setPrefWidth(55.0);
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+    }
+    /**
+     * Iteration 1:
+     * Creates a label with the specified text and adds it to the dialog container.
+     * @param text String containing text to add
+     * @return a label with the specified text that has word wrap enabled.
+     */
+    private Label getDialogLabel(String text) {
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+        return textToAdd;
+    }
+    /**
+     * Iteration 2:
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput() {
+        String userText = userInput.getText();
+        String kelbotText = getResponse(userInput.getText());
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, user),
+                DialogBox.getKelbotDialog(kelbotText, kelbot)
+        );
+        userInput.clear();
+    }
+    /**
+     * Get response from Kelbot
+     * @param input The input from user
+     */
+    public String getResponse(String input) {
+        return "Kelbot has received: " + input;
     }
     /**
      * The main programme that runs after the Kelbot initialization.
@@ -109,7 +205,6 @@ public class Kelbot {
      * Main programme
      */
     public static void main(String[] args) throws KelbotException {
-        java.nio.file.Path path = java.nio.file.Paths.get(".", "data", "Kelbot.txt");
-        new Kelbot(path).run();
+        new Kelbot().run();
     }
 }
---
+++ src/main/java/kelbot/Main.java
@@ -0,0 +1,29 @@
+package kelbot;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Kelbot using FXML.
+ */
+public class Main extends Application {
+    private Kelbot kelbot = new Kelbot();
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setKelbot(kelbot);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
