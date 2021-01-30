1cab1196cb4351248fa55ba7c1bc4ff70cd38ae2
ssoonwee <38690268+ssoonwee@users.noreply.github.com>
2021-01-30 22:47:36 +0800

    Fix missed stash changes for Event

--- src/main/java/soonwee/duke/Event.java
+++ src/main/java/soonwee/duke/Event.java
@@ -3,15 +3,11 @@ package soonwee.duke;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-<<<<<<< Updated upstream
-public class Event extends Task{
-=======
 /**
  * Represents an Event instance. An Event instance will store the task
  * name and its starting time.
  */
 public class Event extends Task {
->>>>>>> Stashed changes
 
     protected LocalDateTime at;
 
