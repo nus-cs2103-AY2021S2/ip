9cf2ed61736ad52899043e15dbe32828d782fc44
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-30 16:38:23 +0800

    Update StorageTest.java to comply with checkstyle

--- src/test/java/duke/storage/StorageTest.java
+++ src/test/java/duke/storage/StorageTest.java
@@ -1,17 +1,18 @@
 package duke.storage;
 
-import duke.exceptions.SaveFileInvalidFormatException;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.FileNotFoundException;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+import duke.exceptions.SaveFileInvalidFormatException;
 
 public class StorageTest {
     @Test
     public void testStorage_invalidFile_throwsFileNotFoundException() {
-       Storage storage = new Storage("invalidFile.abc");
-       assertThrows(FileNotFoundException.class, () -> storage.load());
+        Storage storage = new Storage("invalidFile.abc");
+        assertThrows(FileNotFoundException.class, () -> storage.load());
     }
 
     @Test
