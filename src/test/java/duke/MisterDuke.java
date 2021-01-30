a880303b7d31c6beaf02b82cf82be88d46e21984
shaelynl <shaelyn.lamxy@gmail.com>
2021-01-30 18:37:16 +0800

    Add A-JUnit

---
+++ src/test/java/duke/MisterDuke.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ToDoTest {
+    @Test
+    public void testToDoString(){
+        ToDo toDoTask = new ToDo("read book");
+        assertEquals("[T][ ] read book", toDoTask.toString());
+    }
+}
+
+class EventTest {
+    @Test
+    public void testEventString() {
+        Event eventTask = new Event("proj meeting ", " 2pm");
+        assertEquals("[E][ ] proj meeting (at: 2pm)", eventTask.toString());
+    }
+}
+
+
