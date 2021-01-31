b044ffa15a9ee06c7b80c61cfed1668f7102759f
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-31 17:36:58 +0800

    Update DeleteCommand.java for GUI

--- src/main/java/duke/commands/DeleteCommand.java
+++ src/main/java/duke/commands/DeleteCommand.java
@@ -26,17 +26,19 @@ public class DeleteCommand extends Command {
     }
 
     /**
+     * Returns a message to indicate successful task deletion.
      * Deletes the indicated task from TaskList given by command line
      * and save changes into the save file.
      *
      * @param tasks TaskList to delete task from.
      * @param ui Ui for system outputs.
      * @param storage Storage for saving contents into file.
+     * @return Successful deletion of task string.
      * @throws DukeException If error happens while deleting tasks.
      * @throws IOException If error happens while saving contents into file.
      */
     @Override
-    public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException, IOException {
+    public String execute(TaskList tasks, Ui ui, Storage storage) throws DukeException, IOException {
         if (checkCommands.length == 1 || !isNumber(checkCommands[1])) {
             throw new TaskSelectionInvalidException();
         }
@@ -44,11 +46,11 @@ public class DeleteCommand extends Command {
         if (num > 0 && num <= tasks.size()) {
             Task deleted = tasks.getTask(num - 1);
             tasks.remove(num - 1);
-            ui.showDeleteTask(tasks, deleted);
+            storage.save(tasks);
+            return ui.getDeleteTaskString(tasks, deleted);
         } else {
             throw new TaskNotFoundException();
         }
-        storage.save(tasks);
     }
 
     /**
