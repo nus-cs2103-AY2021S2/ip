7a535f9b155e9d9a865134a3bd59c071d50adaf3
vivegank <77456578+vivegank@users.noreply.github.com>
2021-01-31 22:14:11 +0800

    Implement find task by keyword

--- src/main/java/yoda/command/CommandType.java
+++ src/main/java/yoda/command/CommandType.java
@@ -4,5 +4,5 @@ package yoda.command;
  * CommandType class to enumerate the types of commands available to handle the user's requests.
  */
 public enum CommandType {
-    LIST, TODO, DEADLINE, EVENT, DONE, DELETE, BYE, HELP, ERROR
+    LIST, TODO, DEADLINE, EVENT, DONE, DELETE, FIND, BYE, HELP, ERROR
 }
