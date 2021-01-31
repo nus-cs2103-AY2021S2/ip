b4f840947274992bff40026a1ed6b906b2e416bf
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-31 17:35:28 +0800

    Add Launcher.java for GUI

---
+++ src/main/java/duke/Launcher.java
@@ -0,0 +1,18 @@
+package duke;
+
+import duke.fxml.Main;
+import javafx.application.Application;
+
+/**
+ * A launcher class to start Duke program.
+ */
+public class Launcher {
+    /**
+     * Main method to start the Duke program.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
