e7d4af2a9af69cc198cb8388c76d5d7508e13ee1
Tomashiwa <wongqinliang@gmail.com>
2021-01-31 17:44:08 +0800

    Change display picture for user and duke

--- src/main/java/duke/controller/MainWindow.java
+++ src/main/java/duke/controller/MainWindow.java
@@ -32,8 +32,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/m1.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/w1.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/cat1.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/cat2.png"));
 
     /**
      * Initialize any properties of this UI element and its sub-elements
