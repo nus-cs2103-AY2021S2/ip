c62122005709a80d02b4fb4993238d3d76086015
andrea-twl <tan.wanling.andrea@gmail.com>
2021-01-31 17:37:38 +0800

    Level-2 complete

--- src/com/jetbrains/Duke.java
+++ src/com/jetbrains/Duke.java
@@ -1,6 +1,7 @@
 package com.jetbrains;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -13,12 +14,26 @@ public class Duke {
         System.out.println("Hello! I'm Duke! \n " +
                 "Talk to me. \n" +
                 "********************************");
+        ArrayList<String> list = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+        String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println("Duke: " + input + "\n");
-            input = sc.next();
+            if (input.equals("list")) {
+                displayList(list);
+                System.out.println("\n");
+            } else {
+                list.add(input);
+                System.out.println("added: " + input + "\n");
+            }
+            input = sc.nextLine();
         }
+        sc.close();
         System.out.println("Bye! Come back soon!");
+    }
+
+    static void displayList(ArrayList<String> list) {
+        for(int i = 0; i < list.size(); i++) {
+            System.out.printf("%d. %s%n",i + 1, list.get(i));
+        }
     }
 }
