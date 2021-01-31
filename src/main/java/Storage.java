1591aed4565818a57eb8a9de70eda0417d0782bc
awzhenyi <awzhenyi@github.com>
2021-01-31 22:46:37 +0800

    Add Parser class

---
+++ src/main/java/Storage.java
@@ -0,0 +1,16 @@
+/*package main.java;
+
+import java.util.ArrayList;
+
+public class Storage {
+    private static ArrayList<Task> taskList = new ArrayList<Task>();
+
+    public Storage() {
+
+    }
+
+    public ArrayList<Task> getTaskList() {
+        return this.taskList;
+    }
+
+}*/
