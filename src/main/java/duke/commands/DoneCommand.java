8f54f690c98b5c081c001f1e21070897315f88a9
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-31 17:37:06 +0800

    Update DoneCommand.java for GUI

--- src/main/java/duke/commands/DoneCommand.java
+++ src/main/java/duke/commands/DoneCommand.java
@@ -25,29 +25,30 @@ public class DoneCommand extends Command {
     }
 
     /**
+     * Returns a message to indicate successful task completion.
      * Changes the completion status of indicated task in TaskList
      * given by command line and save changes into the save file.
      *
      * @param tasks TaskList containing the task.
      * @param ui Ui for system outputs.
      * @param storage Storage for saving contents into file.
+     * @return Successful completion of task string.
      * @throws IOException If error happens while saving contents into file.
      * @throws DukeException If error happens while changing status of task.
      */
     @Override
-    public void execute(TaskList tasks, Ui ui, Storage storage) throws IOException, DukeException {
+    public String execute(TaskList tasks, Ui ui, Storage storage) throws IOException, DukeException {
         if (checkCommands.length == 1 || !isNumber(checkCommands[1])) {
             throw new TaskSelectionInvalidException();
         }
         int num = Integer.parseInt(checkCommands[1]);
         if (num > 0 && num <= tasks.size()) {
             tasks.getTask(num - 1).markAsDone();
-            ui.showDoneTask(tasks, num);
-
+            storage.save(tasks);
+            return ui.getDoneTaskString(tasks, num);
         } else {
             throw new TaskNotFoundException();
         }
-        storage.save(tasks);
     }
 
     /**
