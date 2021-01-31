5f9e5138811d860aa7771eb7eee1f9e353e5d9d1
YuHeem <yuuuheem@gmail.com>
2021-01-31 12:01:12 +0800

    feat: add dates and times to tasks

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,20 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    String time;
+    protected LocalDate time;
 
     public Event(String description, String time) {
         super(description);
-        this.time = time;
+        this.time = LocalDate.parse(time, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
+
 
     @Override
     public String toString() {
-        return String.format("[E][%s] %s (at: %s)", this.getStatusIcon(), this.description, this.time);
+        return String.format("[E][%s] %s (at: %s)",
+                this.getStatusIcon(),
+                this.description,
+                this.time.format(DateTimeFormatter.ofPattern("MMM dd yyyy")));
     }
 }
