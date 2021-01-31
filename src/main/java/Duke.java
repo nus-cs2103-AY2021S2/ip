1c3e6f5ab91a31dd0d323eefe8e1f03225fc6c93
kwmiw <pennypolarbear@gmail.com>
2021-01-31 11:53:24 +0800

    Skeletal version of Doge

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        String logo = "░▄▀▄▀▀▀▀▄▀▄░░░░░░░░░\n"
+                + "░█░░░░░░░░▀▄░░░░░░▄░\n"
+                + "█░░▀░░▀░░░░░▀▄▄░░█░█\n"
+                + "█░▄░█▀░▄░░░░░░░▀▀░░█\n"
+                + "█░░▀▀▀▀░░░░░░░░░░░░█\n"
+                + "█░░░░░░░░░░░░░░░░░░█\n"
+                + "█░░░░░░░░░░░░░░░░░░█\n"
+                + "░█░░▄▄░░▄▄▄▄░░▄▄░░█░\n"
+                + "░█░▄▀█░▄▀░░█░▄▀█░▄▀░\n"
+                + "░░▀░░░▀░░░░░▀░░░▀░░░\n";
+
+
+        System.out.println("Greetings from\n" + logo);
+
+        String spacer = "____________________________________________________________\n";
+        String greet = "Woof! I'm Doge\n"
+                + "What do you want me to do?\n"
+                + "Type your request in below!\n";
+        String goodbye = "Bye! Hope I was a good dog, see you again soon!\n";
+        String terminate = "bye";
+
+
+
+        System.out.println(spacer + greet + spacer);
+
+        while (true) {
+            String echo = sc.nextLine();
+            System.out.println(spacer + echo + "\n" + spacer);
+            if (echo.equals(terminate)) {
+                System.out.println(spacer + goodbye + spacer);
+                break;
+            }
+        }
+
+        sc.close();
+
     }
 }
