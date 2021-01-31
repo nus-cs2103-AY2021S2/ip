d1ee9645b34859f6bf43dd6d3f460a6b243a5183
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-31 17:38:35 +0800

    Update SaveFileInvalidFormatException.java for GUI

--- src/main/java/duke/exceptions/SaveFileInvalidFormatException.java
+++ src/main/java/duke/exceptions/SaveFileInvalidFormatException.java
@@ -2,6 +2,6 @@ package duke.exceptions;
 
 public class SaveFileInvalidFormatException extends DukeException {
     public SaveFileInvalidFormatException() {
-        super("\tInvalid content format in save file.\t");
+        super("Invalid content format in save file.");
     }
 }
