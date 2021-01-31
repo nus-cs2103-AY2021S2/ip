14ca474dbd5d86c56838dd7ec4a38081e7075e01
Samuel Fang <samuelfangjw@gmail.com>
2021-02-01 03:30:11 +0800

    Add background image to GUI

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -5,12 +5,19 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.VBox;
 
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
  */
 public class MainWindow extends AnchorPane {
+    @FXML
+    private AnchorPane anchorPane;
     @FXML
     private ScrollPane scrollPane;
     @FXML
@@ -24,11 +31,20 @@ public class MainWindow extends AnchorPane {
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.jpg"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaSquirrel.jpg"));
+//    private Image treeImage = new Image(this.getClass().getResourceAsStream("/images/DaTree.jpg"));
 
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
         displayDukeMessage(Ui.WELCOME_MESSAGE);
+
+//        // new BackgroundSize(width, height, widthAsPercentage, heightAsPercentage, contain, cover)
+//        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+//        // new BackgroundImage(image, repeatX, repeatY, position, size)
+//        BackgroundImage backgroundImage = new BackgroundImage(treeImage, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+//        // new Background(images...)
+//        Background background = new Background(backgroundImage);
+//        anchorPane.setBackground(background);
     }
 
     public void setDuke(Duke d) {
