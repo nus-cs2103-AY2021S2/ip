b1a9135758330837063960c3282f61a504da036a
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-01-31 17:36:41 +0800

    Update Duke.java for GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -34,52 +34,37 @@ public class Duke {
             storage.initialize();
             tasks = new TaskList(storage.load());
         } catch (FileNotFoundException e) {
-            //TODO change all the output to be manged by ui.
-            System.out.println("Error: File does not exists.");
+            ui.showFileNotFound();
             System.exit(1);
         } catch (SaveFileInvalidFormatException e) {
-            System.out.println("Error: Invalid content format in save file");
+            ui.showInvalidSaveFileFormat();
             System.exit(1);
         } catch (DateTimeParseException e) {
-            System.out.println("Error: Invalid date time format in save file or no date time stated");
+            ui.showInvalidSaveFileDateTimeFormat();
             System.exit(1);
         } catch (IOException e) {
-            System.out.println("Error happened while trying to create save file");
+            ui.showFileCreationError();
             System.exit(1);
         }
     }
 
-    /**
-     * Start the program of Duke that edits a lists of task.
-     */
-    public void run() {
-        ui.showWelcome();
-        boolean isExit = false;
-        while (!isExit) {
-            try {
-                String fullCommand = ui.readCommand();
-                Command command = Parser.parse(fullCommand);
-                command.execute(tasks, ui, storage);
-                isExit = command.isExit();
-            } catch (DukeException e) {
-                ui.showError(e.getMessage());
-            } catch (IOException e) {
-                ui.showIoError();
-            } catch (StringIndexOutOfBoundsException e) {
-                ui.showOutOfBoundsError();
-            } catch (DateTimeParseException e) {
-                ui.showDateTimeParseError();
-            }
-        }
-    }
-
-    /**
-     * Main method to start Duke program.
+    /** Returns a response message according to the command given.
      *
-     * @param args
+     * @param fullCommand Command given to Duke bot.
+     * @return Message string according to the command given.
      */
-    public static void main(String[] args) {
-        String dir = System.getProperty("user.dir");
-        new Duke(dir + "/data/Duke.txt").run();
+    public String getResponse(String fullCommand) {
+        try {
+            Command command = Parser.parse(fullCommand);
+            return command.execute(tasks, ui, storage);
+        } catch (DukeException e) {
+            return e.getMessage();
+        } catch (IOException e) {
+            return ui.getIoErrorString();
+        } catch (StringIndexOutOfBoundsException e) {
+            return ui.getOutOfBoundsErrorString();
+        } catch (DateTimeParseException e) {
+            return ui.getDateTimeParseErrorString();
+        }
     }
 }
