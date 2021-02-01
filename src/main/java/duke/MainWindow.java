a17e46a680990ffa2eaafc8ba6ffaa7af188c1aa
= <yongshenswa@gmail.com>
2021-02-01 21:05:57 +0800

    Latest update for GUI

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -24,8 +24,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("../../resources/images/pepe.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("../../resources/images/gavin.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/pepe.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/gavin.png"));
 
     @FXML
     public void initialize() {
