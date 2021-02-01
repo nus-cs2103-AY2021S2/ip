cb75c49cc0fe126f15955e37e51be5cd11782aa8
eksinyue <xinyuehehxy@gmail.com>
2021-02-01 22:43:31 +0800

    Add GUI for Duke

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,5 +1,18 @@
 package duke;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 import duke.command.Command;
 import duke.exception.DukeException;
 import duke.parser.Parser;
@@ -8,6 +21,22 @@ import duke.task.TaskList;
 import duke.ui.Ui;
 
 public class Duke {
+
+    String imageUrl =
+            "https://static.wikia.nocookie.net/moomin/images/0/05/My1.png/revision/latest/top-crop/width/300/height/300?cb=20190914020308";
+    String moominUrl =
+            "https://i.pinimg.com/originals/19/3a/15/193a1552cc00589da96c9c8ce8cc4ba9.png";
+    private Image user = new Image(imageUrl, 160, 60, false, true);
+    private Image duke = new Image(moominUrl, 160, 60, false, true);
+
+//    private Image user = new Image(this.getClass().getResourceAsStream("resources/images/DaUser.png"));
+//    private Image duke = new Image(this.getClass().getResourceAsStream("resources/images/DaDuke.png"));
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
     private TaskList tasks;
     private Ui ui;
@@ -31,7 +60,7 @@ public class Duke {
      * Handles the inputs entered by the user,
      * until the user enters the exit command.
      */
-    public void handleUserInput() {
+    public void handleUserInputs() {
         boolean isRunning = true;
         while (isRunning) {
             try {
@@ -45,12 +74,16 @@ public class Duke {
         }
     }
 
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
     /**
      * Runs the Duke program.
      */
     public void run() {
         ui.printGreeting();
-        handleUserInput();
+        handleUserInputs();
         ui.close();
     }
 
