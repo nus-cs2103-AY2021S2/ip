e916b2816222d8dac6f1df0b81f3967e4df8039b
Justin Gnoh <justingnoh123@gmail.com>
2021-02-02 01:18:18 +0800

    Fix exception to return empty ArrayList on error
    - fetchTask() exception handles by return empty ArrayList

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -218,7 +218,7 @@ public class Duke {
                 lineRead = reader.readLine();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            return result;
         }
         return result;
     }
