57d1cec95881fe77d5b99d89c270186eeab2ce8a
ZhangAnli <zhanganli1998@hotmail.com>
2021-02-01 17:19:34 +0800

    Complete JavaFX tutorial

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,13 @@
 import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.Region;
 
 /**
  * Duke is a AI assistant program that allows users to take note of their tasks.
@@ -9,22 +18,22 @@ import java.io.IOException;
  * - Showing the whole list of tasks
  * - Error checking for IO
  */
-public class Duke {
+public class Duke extends Application {
 
     private static Ui ui = new Ui();
     private Storage storage;
     private TaskList taskList;
+    private ScrollPane scrollPane;
 
     /**
      * Constructor method.
-     * @param filePath to the hard drive file
      * @throws IOException if error in user IO
      */
-    public Duke(String filePath) throws IOException {
-        this.storage = new Storage(filePath);
+    public Duke() {
         try {
+            this.storage = new Storage();
             taskList = new TaskList(storage.load());
-        } catch (IOException e) {
+        } catch (DukeWrongInputException e) {
             ui.showLoadingError(e);
             taskList = new TaskList();
         }
@@ -34,7 +43,7 @@ public class Duke {
      * Runs the Duke Simulation
      * @throws IOException if error in user IO
      */
-    public void run() throws IOException {
+    public void run() throws DukeWrongInputException {
         storage.load();
         ui.showWelcome();
         boolean isExit = false;
@@ -45,9 +54,7 @@ public class Duke {
                 Command c = Parser.parse(fullCommand);
                 c.execute(taskList, ui, storage);
                 isExit = c.isExit();
-            } catch (DukeWrongInputException e) {
-                ui.showError(e.getMessage());
-            } catch (DukeMissingInputException e) {
+            } catch (DukeWrongInputException | DukeMissingInputException e) {
                 ui.showError(e.getMessage());
             } finally {
                 ui.showLine();
@@ -58,11 +65,77 @@ public class Duke {
     }
 
     /**
+     *
+     * @param stage
+     */
+    @Override
+    public void start(Stage stage) {
+        //Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        VBox dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        TextField userInput = new TextField();
+        Button sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        Scene scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        //Scroll down to the end every time dialogContainer's height changes.
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
+    /**
      * Main driver function.
      * @param args command line arguments
-     * @throws IOException if error in user IO
+     * @throws DukeWrongInputException if error in user IO
      */
-    public static void main(String[] args) throws IOException {
-        new Duke("duke.txt").run();
+    public static void main(String[] args) throws DukeWrongInputException {
+        new Duke().run();
     }
 }
