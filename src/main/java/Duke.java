19a299200dc3e0b59121e9e10729d7ef4efc0ec6
weixue123 <weixue@Weis-MacBook-Pro.local>
2021-02-02 03:42:18 +0800

    Added features to add and list tasks (Level-2)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,31 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> inputList = new ArrayList<>();
 
         while (true) {
             String input = sc.nextLine();
+
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                printList(inputList);
+            } else {
+                inputList.add(input);
+                System.out.println("Added: " + input);
             }
-            System.out.println(input);
+        }
+    }
+
+    private static void printList(ArrayList<String> list) {
+        for (int index = 0; index < list.size(); index++) {
+            System.out.print((index + 1) + ". " + list.get(index) + "\n");
         }
     }
 }
