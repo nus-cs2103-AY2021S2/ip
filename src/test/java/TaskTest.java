408fc71a11b92f1322d5d05a67790bb7cef5a53c
Yanneko <joel.wong.xue.rong@gmail.com>
2021-02-01 18:13:57 +0800

    Add 3 JUnit tests for subclasses of Task

---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+    @Test
+    public void toDoCreationTest(){
+        assertEquals(new ToDo("testTask").generateDataString(), "todo notDone testTask");
+    }
+
+    @Test
+    public void deadlineCreationTest(){
+        assertEquals(new Deadline("testDeadline", "2020-02-20 2020").generateDataString(),
+                "deadline 2020-02-20 2020 notDone testDeadline");
+    }
+    @Test
+    public void eventCreationTest(){
+        assertEquals(new Event("testEvent", "2020-02-20 2020").generateDataString(),
+                "event 2020-02-20 2020 notDone testEvent");
+    }
+}
