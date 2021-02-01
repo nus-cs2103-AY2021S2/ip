3159f7201cc4c87bd8895b15659b6853e794bdc2
Icelenaugust <dingyoujia0823@gmail.com>
2021-02-01 16:09:54 +0800

    Change test filepath

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,14 +13,14 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/chatbot/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/duke/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
-# run the program, feed chatbot.commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin ChatBot < input.txt > ACTUAL.TXT
+# run the program, feed duke.commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ../bin Duke < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
