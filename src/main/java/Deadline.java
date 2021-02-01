24ff35618b8952456b1ec166387ae7afeefb30bf
Daniel Ong <ongfishh@gmail.com>
2021-02-01 23:18:11 +0800

    Resolve merge conflicts

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,25 @@
-public class Deadline extends Task {
-    private String time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String taskDescription, String time) {
+public class Deadline extends Task {
+    private LocalDate date;
+    private LocalTime time;
+
+    public Deadline(String taskDescription, String dateAsString, String timeAsString) {
         super(taskDescription);
-        this.time = time;
+        this.date = LocalDate.parse(dateAsString);
+        this.time = LocalTime.parse(timeAsString);
     }
 
-    public Deadline(String taskDescription, boolean isDone, String time) {
+    public Deadline(String taskDescription, boolean isDone, String dateAsString, String timeAsString) {
         super(taskDescription, isDone);
-        this.time = time;
+        this.date = LocalDate.parse(dateAsString);
+        this.time = LocalTime.parse(timeAsString);
     }
 
     @Override
     public String toString() {
-        return "DEADLINE" + Task.TASK_DELIMITER + (isDone ? "done" : " ") + Task.TASK_DELIMITER + taskDescription + Task.TASK_DELIMITER + time;
+        return "DEADLINE" + Task.TASK_DELIMITER + (isDone ? "done" : " ") + Task.TASK_DELIMITER + taskDescription + Task.TASK_DELIMITER + date.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + ", " + time;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,18 +1,25 @@
-public class Event extends Task {
-    private String time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String taskDescription, String time) {
+public class Event extends Task {
+    private LocalDate date;
+    private LocalTime time;
+
+    public Event(String taskDescription, String dateAsString, String timeAsString) {
         super(taskDescription);
-        this.time = time;
+        this.date = LocalDate.parse(dateAsString);
+        this.time = LocalTime.parse(timeAsString);
     }
 
-    public Event(String taskDescription, boolean isDone, String time) {
+    public Event(String taskDescription, boolean isDone, String dateAsString, String timeAsString) {
         super(taskDescription, isDone);
-        this.time = time;
+        this.date = LocalDate.parse(dateAsString);
+        this.time = LocalTime.parse(timeAsString);
     }
 
     @Override
     public String toString() {
-        return "EVENT" + Task.TASK_DELIMITER + (isDone ? "done" : " ") + Task.TASK_DELIMITER + taskDescription + Task.TASK_DELIMITER + time;
+        return "EVENT" + Task.TASK_DELIMITER + (isDone ? "done" : " ") + Task.TASK_DELIMITER + taskDescription + Task.TASK_DELIMITER + date.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + ", " + time;
     }
 }
