ec17516c4c2bf9255be968525503cd1587e5f333
Guan Yang Ze <guanyangze@gmail.com>
2021-02-02 18:07:01 +0800

    add GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,46 +5,29 @@ public class Duke {
     private Ui ui;
     private Storage storage;
     private CommandParser commandParser;
-    private boolean isActive;
-    
+
     public Duke(String filepath) {
         this.ui = new Ui();
         this.storage = new Storage(filepath);
-        this.isActive = true;
 
         try {
             tasks = new TaskList(storage.load());
         } catch (DukeException e) {
-            ui.showError(e);
             this.tasks = new TaskList();
         }
 
         this.commandParser = new CommandParser(tasks, ui);
     }
 
-    public static void main(String[] args) {
-        Duke duke = new Duke("savedata.txt");
-        duke.run();
-    }
-
-    private void run() {
-        // display welcome sequence
-        ui.welcome();
-
-        String userInput;
-        // loop until the user exits
-        while (isActive) {
-            // get user input
-            userInput = ui.readCommand();
-            try {
-                isActive = commandParser.parseCommand(userInput);
-                storage.saveTasks(tasks);
-            } catch (Exception e) {
-                ui.borderPrint(e.getMessage());
-            }
-        }
-        
-        // exit sequence
-        ui.quit();
+    /**
+     * Generates a relevant response to the user input.
+     * @param input
+     * @return
+     * @throws DukeException
+     */
+    public String getResponse(String input) throws DukeException {
+        String response = commandParser.parseCommand(input);
+        storage.saveTasks(tasks);
+        return response;
     }
 }
