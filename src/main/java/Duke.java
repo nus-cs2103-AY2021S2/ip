5decf92557c26e4d100e4586cfbd4c8c6196c500
Lim Li Quan <linliquan1996@Hotmail.com>
2021-02-02 10:59:10 +0800

    Set up for JavaFX tutorial (Part 1)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
-public class Duke {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Duke extends Application {
     private Ui ui;
     private TaskList tasks;
     private Storage storage;
@@ -37,5 +42,14 @@ public class Duke {
 
     public static void main(String[] args) {
         new Duke().run();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
