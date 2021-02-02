4cb5c060460f026227a21f31e507d0f8f05ee31c
chesterhow <chesterhow@gmail.com>
2021-02-02 15:15:23 +0800

    Set up javafx

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,93 @@
-import java.util.Scanner;
 import java.io.IOException;
 import java.time.format.DateTimeParseException;
+import java.util.Scanner;
 
-public class Duke {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.layout.Region;
+
+public class Duke extends Application {
     private static final String DIR_NAME = "data";
     private static final String FILE_NAME = "duke.txt";
 
     private final Ui ui;
     private final Storage storage;
     private final TaskList tasks;
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    /**
+     * Constructor for the Duke class.
+     *
+     * @throws IOException If an error occurs while reading from the file.
+     */
+    public Duke() throws IOException {
+        tasks = new TaskList();
+        ui = new Ui();
+        storage = new Storage(DIR_NAME, FILE_NAME);
+//
+//        storage.readFromFile(tasks, ui);
+//        ui.greet();
+//
+//        Scanner sc = new Scanner(System.in);
+//        while (readInput(sc)) ;
+    }
+
+    @Override
+    public void start(Stage stage) {
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+    }
 
     private int getIndex(String args) {
         return Integer.parseInt(args) - 1;
@@ -163,24 +242,7 @@ public class Duke {
         return true;
     }
 
-    /**
-     * Constructor for the Duke class.
-     *
-     * @throws IOException If an error occurs while reading from the file.
-     */
-    public Duke() throws IOException {
-        tasks = new TaskList();
-        ui = new Ui();
-        storage = new Storage(DIR_NAME, FILE_NAME);
-
-        storage.readFromFile(tasks, ui);
-        ui.greet();
-
-        Scanner sc = new Scanner(System.in);
-        while (readInput(sc)) ;
-    }
-
-    public static void main(String[] args) throws IOException {
-        new Duke();
-    }
+//    public static void main(String[] args) throws IOException {
+//        new Duke();
+//    }
 }
