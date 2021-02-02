5d43e2ab852adfb9ad8ea63ff02671c072d04043
frisciliasultan <frisciliasultan@gmail.com>
2021-02-02 19:54:06 +0800

    Remove Maya constructor

--- src/main/java/Maya.java
+++ src/main/java/Maya.java
@@ -23,16 +23,27 @@ public class Maya extends Application {
     private TaskList taskList;
     private Ui ui;
 
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
+
     /**
-     * Initialises a newly created Maya Object
-     * so that it represents a Bot.
+     * Starts the Maya object to accept user commands.
+     * Processes user commands by utilising Parser object
+     * to make sense of the user command.
      *
      * @param filePath a String of the path
      *                 where the TaskList is stored.
      */
-    public Maya(String filePath) {
+    public void run(String filePath) {
         ui = new Ui();
         storage = new Storage(filePath);
+
         try {
             taskList = storage.load();
         } catch (IOException e) {
@@ -40,16 +51,8 @@ public class Maya extends Application {
         } catch (UnknownCommandException e) {
             ui.showError(e.getMessage());
         }
-    }
 
-    /**
-     * Starts the Maya object to accept user commands.
-     * Processes user commands by utilising Parser object
-     * to make sense of the user command.
-     */
-    public void run() {
         ui.showWelcome();
-
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
             try {
@@ -74,20 +77,10 @@ public class Maya extends Application {
                 ui.showLine();
             }
         }
-
         sc.close();
     }
 
     public static void main(String[] args) {
-        new Maya("data/task.txt").run();
-    }
-
-    @Override
-    public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
+        new Maya().run("data/task.txt");
     }
 }
