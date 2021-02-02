28b7b5bde5a719b7bbff527763ff77cb21cae94b
Sidney011100 <sidney011100@gmail.com>
2021-02-03 00:35:44 +0800

    Write "first program" first half, Add Javafx successful

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,12 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 
-public class Duke {
+public class Duke extends Application {
 
     static Storage storage = new Storage(); 
 
@@ -23,6 +27,15 @@ public class Duke {
         } catch (IOException io) {
             System.out.print("Could not save list!");
         }
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
     /**
