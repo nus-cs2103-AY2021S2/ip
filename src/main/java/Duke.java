8d9088f3ef00a1d8b7d5a8e5a3ec5fc6fd88d6f9
DineshMagesvaran <dineshs97@gmail.com>
2021-02-02 17:10:59 +0800

    Add GUI to the app

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,34 +1,19 @@
+import javafx.application.Application;
+
 /**
  * Class that can create Duke objects that help the user
  * to maintain a list of tasks.
  */
 public class Duke {
-    private Storage storage;
-    private TaskList taskList;
-    private UI ui;
-
-    /**
-     * Constructor that creates a Duke object.
-     *
-     * @param filePath the file path of the file that is used
-     *                 to store the task data.
-     */
-    public Duke(String filePath) {
-        ui = new UI();
-        storage = new Storage(filePath);
-        taskList = storage.readDataFromFile();
-    }
-
     /**
      * Method to run the Duke application.
      */
     public void run() {
-        ui.launchUI(taskList);
-        storage.writeDataIntoFile(taskList);
+        Application.launch(UI.class);
     }
 
     public static void main(String[] args) {
-        new Duke("data/duke.txt").run();
+        new Duke().run();
     }
 
 }
