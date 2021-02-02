1e3655a7991fb1cd12cc51cfb2a1e3fbb143d0d6
Shizheng001 <e0309721@u.nus.edu>
2021-02-02 23:49:02 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    /**
+     * main method to run the program
+     *
+     * @param args command line arguments taken in
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke\n"+ "What can I do for you?");
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                bye();
+                break;
+            } else {
+                echo(input);
+            }
+        }
+    }
+
+    /**
+     * Echos the input message.
+     *
+     * @param msg the user input to be echoed
+     */
+    public static void echo(String msg) {
+        System.out.println(msg);
+    }
+
+    /**
+     * prints Goodbye message.
+     */
+    public static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
