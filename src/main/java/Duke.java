df35bcab6a70e8f12d8e7f6f0da8730333fe2bf6
unknown <varcolac01@gmail.com>
2021-02-02 19:12:28 +0800

    Initial JavaFX testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 /**
  * The Duke class is the main program responsible for instantiating all required classes
  * and methods.
  */
-public class Duke {
+public class Duke extends Application {
     private final Storage ine;
     private final Ui ui;
     private final Parser parser;
@@ -46,5 +51,14 @@ public class Duke {
             ui.ioException();
         }
         ui.bye();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
