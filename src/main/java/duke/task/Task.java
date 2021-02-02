200a91739d92642227c5c09161593dfc922fbb6f
aaronsms <aaronsms@u.nus.edu>
2021-02-02 15:04:54 +0800

    Document Task classes

--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -1,23 +1,47 @@
 package duke.task;
 
+/**
+ * Represents a task item that can be added and listed in Duke.
+ *
+ * @author Aaron Saw Min Sern
+ */
 public abstract class Task {
     protected boolean isDone;
     protected final String description;
     public abstract String encode();
 
+	/**
+	 * Default constructor for subclasses of Task.
+	 *
+	 * @param	isDone      the status of the task item 
+	 * @param	description	the description for the task item
+	 */
     protected Task(final boolean isDone, final String description) {
         this.isDone = isDone;
         this.description = description;
     }
 
+	/**
+	 * Returns a tick if this Task object is finished and a cross otherwise.
+	 *
+     * @return              the status icon based on the status of this Task object.
+	 */
     public String getStatusIcon() {
         return (this.isDone ? "\u2713" : "\u2718");
     }
 
+    /**
+     * Marked the status of this Task object as finished.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+	/**
+	 * Returns the string represenatation of this Task instance
+	 *
+	 * @return		the string representation of this Task instance
+	 */
     @Override
     public String toString() {
         return String.format("[%s] %s", this.getStatusIcon(), this.description);
--- src/main/java/duke/task/ToDo.java
+++ src/main/java/duke/task/ToDo.java
@@ -1,20 +1,42 @@
 package duke.task;
 
+/**
+ * Represents a todo task item without a specific deadline.
+ *
+ * @author Aaron Saw Min Sern
+ */
 public class ToDo extends Task {
+	/**
+	 * Sole constructor for class ToDo.
+	 * @param	description	the description of this ToDo instance
+	 */
 	public ToDo(String description) {
 		super(false, description);
 	}
 
+	/**
+	 * Returns the string representation of this ToDo instance.
+	 * @return		the string representation of this ToDo instance
+	 */
 	@Override
 	public String toString() {
 		return String.format("[T][%s] %s", this.getStatusIcon(), this.description);
 	}
 
+	/**
+	 * Returns the encoded representation for this ToDo instance.
+	 * @return		the encoded representation of this ToDo instance
+	 */
 	@Override
 	public String encode() {
 		return String.format("T | %s | %s", this.isDone ? "1" : "0", this.description);
 	}
 
+	/**
+	 * Returns true if two ToDo items are equivalent. 
+	 * @param	o	the other object to be compared
+	 * @return		true if two ToDo items are equivalent
+	 */
 	@Override
 	public boolean equals(Object o) {
 		if (!(o instanceof ToDo)) {
@@ -24,6 +46,10 @@ public class ToDo extends Task {
 		return this.description.equals(other.description) && this.isDone == other.isDone;
 	}
 
+	/**
+	 * Returns the hashcode of this ToDo instance. 
+	 * @return		hashcode of this ToDo instance	
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
