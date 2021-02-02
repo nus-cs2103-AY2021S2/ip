07fc5cfe510d00bd08ece8317a51322b2b5edbfc
jellymias <51073704+jellymias@users.noreply.github.com>
2021-02-02 15:57:33 +0800

    Revert "Revert "Update FindCommand.java""
    This reverts commit 6ee7c8f95c36b99c3204805fa18be368c6729735.

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -22,13 +22,13 @@ public class FindCommand extends Command {
             if (parser.canParseFindCommand(input)) {
                 String keyword = parser.parseKeyword(input);
                 TaskList tempTaskList = new TaskList();
-                for (int i = 0; i < taskList.size(); i++) {
+                for (int i = 0; i < taskList.getSize(); i++) {
                     Task task = taskList.getTask(i);
                     if (task.hasKeyWord(keyword)) {
                         tempTaskList.addTask(task);
                     }
                 }
-                if (tempTaskList.size() == 0) {
+                if (tempTaskList.getSize() == 0) {
                     throw new NoSuchKeywordException(keyword);
                 } else {
                     ui.printFoundTaskList(tempTaskList);
