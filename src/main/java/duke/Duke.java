b38ab59dc644800e979c3dee18e0386a352066db
jlxw48 <jlxw48@gmail.com>
2021-02-02 20:44:26 +0800

    Edit user interface slightly

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,7 +1,5 @@
 package duke;
 
-import java.util.Scanner;
-
 import duke.commands.Command;
 import duke.dukeexceptions.DukeException;
 import duke.tasks.TaskList;
@@ -15,48 +13,28 @@ public class Duke {
     private static final Storage STORAGE = new Storage(FILE_PATH, UI);
     private static final TaskList TASKLIST = STORAGE.loadFromFile();
 
+    private boolean isExit = false;
+
     /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
+     * Gets input from the user via the GUI and processes it.
      */
     public String getResponse(String input) {
         try {
             Parser p = new Parser(TASKLIST, UI, STORAGE);
             Command c = p.parse(input);
             String output = c.execute();
+            this.isExit = c.isExit();
             return output;
         } catch (DukeException e) {
             return e.getMessage();
         }
-
     }
 
-//    private static void run() {
-//        Ui ui = new Ui();
-//        ui.introduction();
-//
-//        TaskList taskList = storage.loadFromFile();
-//
-//        Scanner scannerInput = new Scanner(System.in);
-//        ui.showMsg("What can I do for you?");
-//
-//        boolean isExit = false;
-//
-//        while (!isExit) {
-//            try {
-//                String input = scannerInput.nextLine();
-//                Parser p = new Parser(taskList, ui, storage);
-//                Command c = p.parse(input);
-//                String output = c.execute();
-//                isExit = c.isExit();
-//            } catch (DukeException e) {
-//                ui.showError(e.getMessage());
-//            }
-//        }
-//        scannerInput.close();
-//    }
-//
-//    public static void main(String[] args) {
-//        run();
-//    }
+    public String introduction() {
+        return "Hello! I'm Duke.";
+    }
+
+    public boolean isExit() {
+        return this.isExit;
+    }
 }
--- src/main/java/duke/window/MainWindow.java
+++ src/main/java/duke/window/MainWindow.java
@@ -35,6 +35,12 @@ public class MainWindow extends AnchorPane {
         duke = d;
     }
 
+    public void displayDukeIntroduction() {
+        dialogContainer.getChildren().addAll(
+                DialogBox.getDukeDialog(duke.introduction(), dukeImage)
+        );
+    }
+
     /**
      * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
      * the dialog container. Clears the user input after processing.
