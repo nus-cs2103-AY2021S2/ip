572141fae5aba02a5624693a7f5d6cb3bf8f22f8
Lim Li Quan <linliquan1996@Hotmail.com>
2021-02-02 10:01:48 +0800

    Add gradle files for JavaFX tutorial

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,8 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class Duke {
     private Ui ui;
     private TaskList tasks;
