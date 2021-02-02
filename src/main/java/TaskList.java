e45ad3a7bae04270b33cecfed78e804f46ed40be
tsh22 <sihuitoh22@gmail.com>
2021-02-03 00:02:10 +0800

    Extract code from TaskList.java and Duke.java to be handled by Ui.java and Parser.java instead

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,32 +1,42 @@
 import java.util.ArrayList;
 
 public class TaskList {
-    protected ArrayList<Task> taskList;
+    public ArrayList<Task> taskList;
+    public static int taskListSize;
 
     public TaskList(ArrayList<Task> taskList) {
         this.taskList = taskList;
     }
 
-    public void addTask(Task task){
+    public Task addTodoTask(Todo task){
         taskList.add(task);
-        System.out.println("Got it. I've added this task:\n"
-                + task.toString()
-                + "\nNow you have " + taskList.size() + " task(s) in the list.");
+        taskListSize = taskList.size();
+        return task;
     }
 
-    public void deleteTask(int index) {
+    public Task addDeadlineTask(Deadline task){
+        taskList.add(task);
+        taskListSize = taskList.size();
+        return task;
+    }
+
+    public Task addEventTask(Event task){
+        taskList.add(task);
+        taskListSize = taskList.size();
+        return task;
+    }
+
+    public Task deleteTask(int index) {
         Task task = taskList.get(index);
         taskList.remove(index);
-        System.out.println("Gotcha. I've removed this task:\n"
-                + task.toString()
-                + "\nNow you have " + taskList.size() + " task(s) in the list.");
+        taskListSize = taskList.size();
+        return task;
     }
 
-    public void taskDone(int index) {
+    public Task taskDone(int index) {
         Task task = taskList.get(index);
         task.markAsDone();
-        System.out.println("Nice job! I've marked this task as done:\n"
-                + task.toString());
+        return task;
     }
 
     public int getListSize() {
@@ -35,17 +45,5 @@ public class TaskList {
 
     public ArrayList<Task> getList() {
         return taskList;
-    }
-
-    public void printList() {
-        if (taskList.size() == 0) {
-            System.out.println("Looks like you have no tasks currently. Add some tasks!");
-        }
-
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < taskList.size(); i++) {
-            Task item = taskList.get(i);
-            System.out.println((i + 1) + "." + item.toString());
-        }
     }
 }
