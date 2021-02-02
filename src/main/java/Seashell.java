16ee7ffcc7ca43bcf312635681fcf78f52ad030f
Jason <jasontay9645@gmail.com>
2021-02-02 13:43:28 +0800

    Fix deadline and event name bugs

--- src/main/java/Seashell.java
+++ src/main/java/Seashell.java
@@ -2,32 +2,39 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Seashell {
-    public static void main(String[] args) {
-        start();
+
+    private ArrayList<Task> taskList;
+
+    public Seashell() {
+        this.taskList = new ArrayList<>();
     }
 
-    public static void start() {
+    public Seashell(String saveFileText) {
+        this.taskList = new ArrayList<>();
+        // add items from saveFile
+    }
+
+    public void start() {
         System.out.println("Hello I'm Seashell, a task manager! What can I do for you?");
         Scanner sc = new Scanner(System.in);
-        ArrayList<Task> list = new ArrayList<>();
         while (true) {
-            String curr = sc.nextLine();
+            String command = sc.nextLine();
             try {
-                if (curr.equals("bye")) {
-                    exit();
+                if (command.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
                     break;
-                } else if (curr.equals("list")) {
-                    listTasks(list);
-                } else if (curr.startsWith("done ")) {
-                    done(curr, list);
-                } else if (curr.startsWith("delete ")) {
-                    delete(curr, list);
-                } else if (curr.startsWith("todo")) {
-                    createTodo(curr, list);
-                } else if (curr.startsWith("deadline")) {
-                    createDeadline(curr, list);
-                } else if (curr.startsWith("event")) {
-                    createEvent(curr, list);
+                } else if (command.equals("list")) {
+                    listTasks(taskList);
+                } else if (command.startsWith("done ")) {
+                    done(command, taskList);
+                } else if (command.startsWith("delete ")) {
+                    delete(command, taskList);
+                } else if (command.startsWith("todo")) {
+                    createTodo(command, taskList);
+                } else if (command.startsWith("deadline")) {
+                    createDeadline(command, taskList);
+                } else if (command.startsWith("event")) {
+                    createEvent(command, taskList);
                 } else {
                     throw new SeashellException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
@@ -37,80 +44,82 @@ public class Seashell {
         }
     }
 
-    public static void exit() {
-        System.out.println("Bye. Hope to see you again soon!");
-    }
-
-    public static void listTasks(ArrayList<Task> list) throws SeashellException {
-        if (list.isEmpty()) {
+    private void listTasks(ArrayList<Task> taskList) throws SeashellException {
+        if (taskList.isEmpty()) {
             throw new SeashellException("OOPS!!! Task list is currently still empty!");
         } else {
-            for (int i = 1; i <= list.size(); i++) {
-                Task t = list.get(i - 1);
+            for (int i = 1; i <= taskList.size(); i++) {
+                Task t = taskList.get(i - 1);
                 System.out.println(i + ". " + t);
             }
         }
     }
 
-    public static void done(String curr, ArrayList<Task> list) throws SeashellException {
-        String num = curr.substring(5);
+    private void done(String command, ArrayList<Task> taskList) throws SeashellException {
+        String num = command.substring(5);
         int taskNum = Integer.parseInt(num);
-        if (list.size() < taskNum) {
+        if (taskList.size() < taskNum) {
             throw new SeashellException("OOPS!!! Task does not exist!");
         } else {
-            Task updated = list.get(taskNum - 1).setDone();
-            list.set(taskNum - 1, updated);
+            Task updated = taskList.get(taskNum - 1).setDone();
+            taskList.set(taskNum - 1, updated);
             System.out.println("Nice! I've marked this task as done: \n" + updated);
         }
     }
 
-    public static void delete(String curr, ArrayList<Task> list) throws SeashellException {
-        String num = curr.substring(7);
+    private void delete(String command, ArrayList<Task> taskList) throws SeashellException {
+        String num = command.substring(7);
         int taskNum = Integer.parseInt(num);
-        if (list.size() < taskNum) {
+        if (taskList.size() < taskNum) {
             throw new SeashellException("OOPS!!! Task does not exist!");
         } else {
-            Task toRemove = list.remove(taskNum - 1);
+            Task toRemove = taskList.remove(taskNum - 1);
             System.out.println("Noted. I have removed " + toRemove);
-            System.out.println("You now have " + list.size() + " items in the list");
+            System.out.println("You now have " + taskList.size() + " items in the task list");
         }
     }
 
-    public static void createTodo(String curr, ArrayList<Task> list) throws SeashellException {
-        String taskName = curr.substring(4).stripLeading();
+    private void createTodo(String command, ArrayList<Task> taskList) throws SeashellException {
+        String taskName = command.substring(4).stripLeading();
         if (taskName.equals("")) {
             throw new SeashellException("OOPS!!! The description of a task cannot be empty.");
         } else {
             Todo newTask = new Todo(taskName);
-            list.add(newTask);
+            taskList.add(newTask);
             System.out.println("Added " + newTask + " to the task list!");
-            System.out.println("You now have " + list.size() + " items in the list");
+            System.out.println("You now have " + taskList.size() + " items in the task list");
         }
     }
 
-    public static void createDeadline(String curr, ArrayList<Task> list) throws SeashellException {
-        String taskName = curr.substring(8).stripLeading();
+    private void createDeadline(String command, ArrayList<Task> taskList) throws SeashellException {
+        String taskName = command.substring(8).stripLeading();
         if (taskName.equals("")) {
             throw new SeashellException("OOPS!!! The description of a task cannot be empty.");
+        } else if (command.indexOf("/by") == -1) {
+            throw new SeashellException("OOPS!!! The syntax of adding a deadline should be [name] /by [date/time]");
         } else {
-            String by = curr.substring(curr.indexOf("/by") + 4);
+            taskName = command.substring(8, command.indexOf("/by") - 1).stripLeading();
+            String by = command.substring(command.indexOf("/by") + 4);
             Deadline newTask = new Deadline(taskName, by);
-            list.add(newTask);
+            taskList.add(newTask);
             System.out.println("Added " + newTask + " to the task list!");
-            System.out.println("You now have " + list.size() + " items in the list");
+            System.out.println("You now have " + taskList.size() + " items in the task list");
         }
     }
 
-    public static void createEvent(String curr, ArrayList<Task> list) throws SeashellException {
-        String taskName = curr.substring(5).stripLeading();
+    private void createEvent(String command, ArrayList<Task> taskList) throws SeashellException {
+        String taskName = command.substring(5).stripLeading();
         if (taskName.equals("")) {
             throw new SeashellException("OOPS!!! The description of a task cannot be empty.");
+        } else if (command.indexOf("/at") == -1) {
+            throw new SeashellException("OOPS!!! The syntax of adding an event should be [name] /at [date/time]");
         } else {
-            String at = curr.substring(curr.indexOf("/at") + 4);
+            taskName = command.substring(5, command.indexOf("/at") - 1).stripLeading();
+            String at = command.substring(command.indexOf("/at") + 4);
             Event newTask = new Event(taskName, at);
-            list.add(newTask);
+            taskList.add(newTask);
             System.out.println("Added " + newTask + " to the task list!");
-            System.out.println("You now have " + list.size() + " items in the list");
+            System.out.println("You now have " + taskList.size() + " items in the task list");
         }
     }
 }
