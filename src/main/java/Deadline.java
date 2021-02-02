0dca6ded124a034fa93fd31c85221276f07c0040
nicholasnge <59093518+nicholasnge@users.noreply.github.com>
2021-02-03 01:55:51 +0800

    Level-7

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,3 +1,7 @@
+import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 public class Deadline extends Task{
     /**
      * Returns a Deadline
@@ -6,15 +10,28 @@ public class Deadline extends Task{
      * **/
     public Deadline(String description, String deadline) {
         super(description);
-        this.deadline = deadline;
+        /* sorted should have 4 args, delimited by spaces
+        1. Day
+        2. Month
+        3. Year
+        4. Hour/Minute
+         */
+        String[] sorted = deadline.split(" ");
+        this.deadline = LocalDateTime.of(Integer.parseInt(sorted[2]), //Day
+                Integer.parseInt(sorted[1]), //Month
+                Integer.parseInt(sorted[0]), //Year
+                Integer.parseInt(sorted[3].substring(0, 2)), //Hour
+                Integer.parseInt(sorted[3].substring(2, 4)));//Minute
     }
 
     @Override
     public String toString() {
         String type = "[D]";
         String doneStatus = "[" + getStatusIcon() + "]";
-        return type + doneStatus + " " + this.description + "(" + this.deadline + ")";
+        String day = deadline.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.forLanguageTag("en"));
+        return String.format("%s%s %s (%s %s:%s, %s %s %s)", type, doneStatus, this.description, day, deadline.getHour(),
+                deadline.getMinute(), deadline.getDayOfMonth(), deadline.getMonth(), deadline.getYear());
     }
 
-    String deadline;
+    LocalDateTime deadline;
 }
