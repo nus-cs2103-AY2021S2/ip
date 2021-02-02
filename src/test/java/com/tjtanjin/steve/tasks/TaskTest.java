5e62e43c6c4cffd06f1a0999459f51eaba0e9561
Tan Jin <cjtanjin@gmail.com>
2021-02-02 19:56:11 +0800

    Refactor: capitalise constant variable names

--- src/test/java/com/tjtanjin/steve/tasks/TaskTest.java
+++ src/test/java/com/tjtanjin/steve/tasks/TaskTest.java
@@ -7,41 +7,41 @@ import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 
 public class TaskTest {
-    private final Task task = new Task("Test Task", "incomplete", "TODO");
+    private final Task TASK = new Task("Test Task", "incomplete", "TODO");
 
     @Test
     void getTaskName_whenInvoke_thenOutputTaskName() {
         String expected = "Test Task";
-        String taskName = task.getTaskName();
+        String taskName = TASK.getTaskName();
         assertEquals(expected, taskName);
     }
 
     @Test
     void getStatus_whenInvoke_thenOutputTaskStatus() {
         String expected = "incomplete";
-        String taskStatus = task.getStatus();
+        String taskStatus = TASK.getStatus();
         assertEquals(expected, taskStatus);
     }
 
     @Test
     void getType_whenInvoke_thenOutputTaskType() {
         String expected = "TODO";
-        String taskType = task.getType();
+        String taskType = TASK.getType();
         assertEquals(expected, taskType);
     }
 
     @Test
     void getDate_whenInvoke_thenOutputTaskDate() {
         LocalDate[] expected = new LocalDate[1];
-        LocalDate[] taskDate = task.getDates();
+        LocalDate[] taskDate = TASK.getDates();
         assertEquals(expected[0], taskDate[0]);
     }
 
     @Test
     void markCompleted() {
         String expected = "complete";
-        task.markCompleted();
-        String taskStatus = task.getStatus();
+        TASK.markCompleted();
+        String taskStatus = TASK.getStatus();
         assertEquals(expected, taskStatus);
     }
 }
