c4b2598215086fa5421d67f94f4dd1b799e6d02a
chesterhow <chesterhow@gmail.com>
2021-02-02 17:01:01 +0800

    Tweak app to use javafx instead of CLI

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,92 +1,25 @@
 import java.io.IOException;
 import java.time.format.DateTimeParseException;
-import java.util.Scanner;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.scene.layout.Region;
-
-public class Duke extends Application {
+public class Duke {
     private static final String DIR_NAME = "data";
     private static final String FILE_NAME = "duke.txt";
 
-    private final Ui ui;
     private final Storage storage;
     private final TaskList tasks;
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-
     /**
      * Constructor for the Duke class.
-     *
-     * @throws IOException If an error occurs while reading from the file.
      */
-    public Duke() throws IOException {
+    public Duke() {
         tasks = new TaskList();
-        ui = new Ui();
         storage = new Storage(DIR_NAME, FILE_NAME);
-//
-//        storage.readFromFile(tasks, ui);
-//        ui.greet();
-//
-//        Scanner sc = new Scanner(System.in);
-//        while (readInput(sc)) ;
-    }
 
-    @Override
-    public void start(Stage stage) {
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
+        try {
+            storage.readFromFile(tasks);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private int getIndex(String args) {
@@ -94,26 +27,28 @@ public class Duke extends Application {
     }
 
     /**
-     * Adds a new Todo to the TaskList.
+     * Adds a new Todo to the TaskList and returns the reply.
      *
      * @param command Command with details about the Todo to be created.
+     * @return Reply to the Todo command.
      * @throws IOException If an error occurs while writing to the file.
      */
-    public void addTodo(Command command) throws IOException {
+    public String addTodo(Command command) throws IOException {
         Task todo = new Todo(command.getArgs());
         tasks.add(todo);
         storage.writeToFile(todo);
-        ui.addedTaskReply(todo, tasks.size());
+        return Ui.addedTaskReply(todo, tasks.size());
     }
 
     /**
-     * Adds a new Deadline to the TaskList
+     * Adds a new Deadline to the TaskList and returns the reply.
      *
      * @param command Command with details about the Deadline to be created.
+     * @return Reply to the Deadline command.
      * @throws IOException If an error occurs while writing to the file.
      * @throws DateTimeParseException If date provided is not of the "dd/mm/yyyy hhmm" or "dd/mm/yyyy" format.
      */
-    public void addDeadline(Command command) throws IOException, DateTimeParseException {
+    public String addDeadline(Command command) throws IOException, DateTimeParseException {
         String[] parts = command.getArgs().split("/by");
 
         if (parts.length != 2) {
@@ -122,17 +57,18 @@ public class Duke extends Application {
             Task deadline = new Deadline(parts[0], parts[1]);
             tasks.add(deadline);
             storage.writeToFile(deadline);
-            ui.addedTaskReply(deadline, tasks.size());
+            return Ui.addedTaskReply(deadline, tasks.size());
         }
     }
 
     /**
-     * Adds a new Event to the TaskList
+     * Adds a new Event to the TaskList and returns the reply.
      *
      * @param command Command with details about the Event to be created.
+     * @return Reply to the Event command.
      * @throws IOException If an error occurs while writing to the file.
      */
-    public void addEvent(Command command) throws IOException {
+    public String addEvent(Command command) throws IOException {
         String[] parts = command.getArgs().split("/at");
 
         if (parts.length != 2) {
@@ -141,17 +77,18 @@ public class Duke extends Application {
             Task event = new Event(parts[0], parts[1]);
             tasks.add(event);
             storage.writeToFile(event);
-            ui.addedTaskReply(event, tasks.size());
+            return Ui.addedTaskReply(event, tasks.size());
         }
     }
 
     /**
-     * Marks a task as done
+     * Marks a task as done and returns the reply.
      *
      * @param command Command containing the index of the task to mark as done.
+     * @return Reply to having marked a task as done.
      * @throws IOException If an error occurs while writing to the file.
      */
-    public void done(Command command) throws IOException {
+    public String done(Command command) throws IOException {
         int index = getIndex(command.getArgs());
 
         if (index < 0 || index >= tasks.size()) {
@@ -159,17 +96,18 @@ public class Duke extends Application {
         } else {
             tasks.getTaskAt(index).markDone();
             storage.updateFile(tasks);
-            ui.markDoneReply(tasks.getTaskAt(index));
+            return Ui.markDoneReply(tasks.getTaskAt(index));
         }
     }
 
     /**
-     * Deletes a task
+     * Deletes a task and returns the reply.
      *
      * @param command Command containing the index of the task to delete.
+     * @return Reply to having deleted a task.
      * @throws IOException If an error occurs while writing to the file.
      */
-    public void delete(Command command) throws IOException {
+    public String delete(Command command) throws IOException {
         int index = getIndex(command.getArgs());
 
         if (index < 0 || index >= tasks.size()) {
@@ -177,72 +115,63 @@ public class Duke extends Application {
         } else {
             Task deletedTask = tasks.remove(index);
             storage.updateFile(tasks);
-            ui.deleteReply(deletedTask, tasks.size());
+            return Ui.deleteReply(deletedTask, tasks.size());
         }
     }
 
     /**
-     * Finds and lists tasks matching the given keyword
+     * Finds and returns a list of tasks matching the given keyword
      *
      * @param command Command containing the keyword to search for
+     * @return Reply to find command
      */
-    public void find(Command command) {
+    public String find(Command command) {
         String keyword = command.getArgs();
 
         TaskList matchingTasks = tasks.findMatchingTasks(keyword);
-        ui.listMatchingTasks(matchingTasks);
+        return Ui.listMatchingTasks(matchingTasks);
     }
 
     /**
      * Processes user's input and executes the respective command
      *
-     * @param sc Scanner object for reading user's input
+     * @param input User's input string
      * @return False if the program should terminate, else true.
      */
-    public boolean readInput(Scanner sc) {
-        String input = sc.nextLine().trim();
-
+    public String parseInputAndReply(String input) {
         try {
-            Command command = Parser.parseInput(input);
+            Command command = Parser.parseInput(input.trim());
 
             switch (command.getInstruction()) {
             case Command.BYE:
-                ui.exit();
-                return false;
+                return Ui.exit();
             case Command.LIST:
-                ui.list(tasks);
-                break;
+                return Ui.list(tasks);
             case Command.DONE:
-                done(command);
-                break;
+                return done(command);
             case Command.DELETE:
-                delete(command);
-                break;
+                return delete(command);
             case Command.FIND:
-                find(command);
-                break;
+                return find(command);
             case Command.TODO:
-                addTodo(command);
-                break;
+                return addTodo(command);
             case Command.EVENT:
-                addEvent(command);
-                break;
+                return addEvent(command);
             case Command.DEADLINE:
-                addDeadline(command);
-                break;
+                return addDeadline(command);
             default:
                 throw new UnknownCommandException();
             }
-        } catch (UnknownCommandException | EmptyDescriptionException | InvalidTaskException | TaskNotFoundException | IOException exception) {
-            ui.reply(ui.formatLine(exception.getMessage()));
+        } catch (UnknownCommandException | EmptyDescriptionException | InvalidTaskException
+                | TaskNotFoundException | IOException exception) {
+            return Ui.formatLine(exception.getMessage());
         } catch (DateTimeParseException exception) {
-            ui.reply(ui.formatLine("☹ Please provide dates in the \"dd/mm/yyyy hhmm\" or \"dd/mm/yyyy\" format"));
+            return Ui.formatLine("☹ Please provide dates in the \"dd/mm/yyyy hhmm\" or \"dd/mm/yyyy\" format");
         }
-
-        return true;
     }
 
-//    public static void main(String[] args) throws IOException {
-//        new Duke();
-//    }
+    public boolean shouldExit(String input) {
+        Command command = Parser.parseInput(input.trim());
+        return command.getInstruction().equals(Command.BYE);
+    }
 }
