6f6eee400e3514d38e80814d9221d770821bb568
tlylt <liu_yongliang@hotmail.com>
2021-02-03 11:49:04 +0800

    Use CheckStyle
    Manual checking of code style violations can be ineffective.
    
    Let's use Gradle to add checkstyle support.
    
    This allows running of checkstyle as a Gradle task.

--- src/test/java/tlylt/haha/UiTest.java
+++ src/test/java/tlylt/haha/UiTest.java
@@ -1,13 +1,13 @@
 package tlylt.haha;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class UiTest {
     private static final ByteArrayOutputStream output = new ByteArrayOutputStream();
