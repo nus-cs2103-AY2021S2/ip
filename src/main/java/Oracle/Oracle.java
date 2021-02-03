4ea02bda2a6718867813ad78547c8854b6bd8605
nicholasnge <59093518+nicholasnge@users.noreply.github.com>
2021-02-04 05:39:27 +0800

    Level-10 A-CheckStyle

--- src/main/java/Oracle/Oracle.java
+++ src/main/java/Oracle/Oracle.java
@@ -1,20 +1,37 @@
 package Oracle;
 
 import Command.Command;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
+import java.io.PrintStream;
 
 public class Oracle {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
     private Parser parser;
-
+    private final String filePath = "./oracle_data.txt";
     /**
      * Create a new Oracle object
-     * @param filePath filePath of the storage file
      */
-    public Oracle(String filePath) {
+    public Oracle() {
+
         ui = new Ui();
         parser = new Parser(ui);
         // storage holds the arraylist used to store all the tasks given to THE ORACLE
@@ -44,6 +61,16 @@ public class Oracle {
     }
 
     public static void main(String[] args) {
-        new Oracle("./oracle_data.txt").run();
+        new Oracle().run();
+    }
+
+    String getResponse(String input) {
+        System.setOut(new PrintStream(outContent));
+        Command c = parser.parse(input);
+        c.execute(ui, tasks);
+        System.setOut(originalOut);
+        String res = outContent.toString();
+        outContent.reset();
+        return res;
     }
 }
