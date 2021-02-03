afa43931bbbf8655de7f3f44bf00e3934016cce8
lirc572 <admin@lirc572.com>
2021-02-04 00:00:38 +0800

    Test: correct testcase

--- src/test/java/com/lirc572/ip/TaskListTest.java
+++ src/test/java/com/lirc572/ip/TaskListTest.java
@@ -16,9 +16,9 @@ public class TaskListTest {
     public void testToSavedString() {
         TaskList tasks = new TaskList();
         tasks.add(new TodoTask("hello"));
-        assertEquals("E | 0 | hello\n", tasks.toSavedString());
+        assertEquals("T | 0 | hello\n", tasks.toSavedString());
         tasks.add(new TodoTask("hi"));
-        assertEquals("E | 0 | hello\nE | 0 | hi\n", tasks.toSavedString());
+        assertEquals("T | 0 | hello\nT | 0 | hi\n", tasks.toSavedString());
     }
 
 }
