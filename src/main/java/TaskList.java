a76f3801723e0e9a6bc815b00cfd02ae7a4dda0f
mabel-kang <e0425446@u.nus.edu>
2021-02-04 01:27:13 +0800

    Update JavaDocs

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -10,6 +10,7 @@ public class TaskList {
 
     /**
      * Creates a TaskList.
+     *
      * @param tasks Tasks to be added into the TaskList.
      */
     public TaskList(List<Task> tasks) {
@@ -18,6 +19,7 @@ public class TaskList {
 
     /**
      * Adds a task to the task list.
+     *
      * @param task Task to be added.
      */
     public void add(Task task) {
@@ -26,6 +28,7 @@ public class TaskList {
 
     /**
      * Deletes a task from the task list.
+     *
      * @param taskNumber Task number of the task that is to be deleted.
      */
     public void delete(int taskNumber) {
@@ -33,7 +36,8 @@ public class TaskList {
     }
 
     /**
-     * Prints out all the tasks in the task list.
+     * Lists out all the tasks in the task list.
+     * @return A string consisting of all the tasks.
      */
     public String list() {
         String output = "";
@@ -45,6 +49,7 @@ public class TaskList {
 
     /**
      * Returns a Task, based on the task number specified by users.
+     *
      * @param taskNumber Task number of selected task.
      * @return Task.
      */
@@ -54,7 +59,9 @@ public class TaskList {
 
     /**
      * Finds the tasks that contain the keyword input by user.
+     *
      * @param keyword Keyword input by user.
+     * @return A String consists of all the matching tasks.
      */
     public String findWithKeyword(String keyword) {
         String output = "";
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -2,23 +2,13 @@
  * Ui handles the interactions with the user.
  */
 public class Ui {
-
     /**
-     * Prints the welcome message for users when they run the app.
-     */
-    public String showWelcome() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        return logo + "\n" + "Hello! I'm Duke\n" + "What can I help you with today! :-)";
-    }
-
-    /**
-     * Prints the response after successfully adding a task.
+     * Creates the response after successfully adding a task.
+     *
      * @param task Task that was added.
      * @param taskListSize Number of tasks in the TaskList.
+     * @return A string including the acknowledgement of the new task, and the total
+     * number of tasks in the list.
      */
     public String responseToAddTask(Task task, int taskListSize) {
         return "Done! One new task:\n" + task.toString() + "\nNow you have "
@@ -26,9 +16,12 @@ public class Ui {
     }
 
     /**
-     * Prints the response after successfully deleting a task.
+     * Creates the response after successfully deleting the task.
+     *
      * @param task Task that was deleted.
      * @param taskListSize Number of tasks in the TaskList.
+     * @return A string including the deletion of the task, and the total
+     * number of tasks in the list.
      */
     public String responseToDelete(Task task, int taskListSize) {
         return "Noted, I've removed this task:\n" + task.toString()
@@ -36,7 +29,8 @@ public class Ui {
     }
 
     /**
-     * Prints the response after user inputs 'bye', before program exits.
+     * Creates the response after user inputs 'bye'.
+     * @return A string.
      */
     public String responseToBye() {
         return "Bye. Hope to see you again soon!";
@@ -45,7 +39,9 @@ public class Ui {
     /**
      * Prints the initial response to the 'list' command, depending on whether
      * the TaskList is empty.
+     *
      * @param taskListSize Number of tasks in the TaskList.
+     * @return A string, depending on whether the list is empty.
      */
     public String responseToList(int taskListSize) {
         if (taskListSize != 0) {
@@ -56,17 +52,27 @@ public class Ui {
     }
 
     /**
-     * Prints the response after marking a task as completed.
+     * Creates the response after marking a task as completed.
+     *
      * @param task Task that was marked completed.
+     * @return A string.
      */
     public String responseToDone(Task task) {
         return "Nice! I've marked this task as done:\n" + task.toString();
     }
 
+    /**
+     * Creates the response after successfully finding matching tasks.
+     * @return A string.
+     */
     public static String responseToFind() {
         return "Here are the matching tasks in your list:";
     }
 
+    /**
+     * Creates the response after being unable to find any matching tasks.
+     * @return A string.
+     */
     public static String responseToNoMatches() {
         return "There are no matching tasks! Time to add one!";
     }
