c416fb3c15ad4602d1e037f047873486c6912480
Jason <jasontay9645@gmail.com>
2021-02-04 02:35:52 +0800

    Add help command

--- src/main/java/Seashell.java
+++ src/main/java/Seashell.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 public class Seashell {
 
     private ArrayList<Task> taskList;
+    private final static String HELP_TEXT = """
+            Welcome to Seashells! You can start by trying out some of these commands
+            
+            todo <task name> - add a todo task
+            deadline <task name> /by <datetime> - add a deadline task
+            event <task name> /at <datetime> - add an event task
+            done <task index> - mark the task at the specified index as done
+            delete <task index> - remove the task at the specified index from the list
+            bye - exits the program""";
 
     public Seashell() {
         this.taskList = new ArrayList<>();
@@ -15,7 +24,8 @@ public class Seashell {
     }
 
     public void start() {
-        System.out.println("Hello I'm Seashell, a task manager! What can I do for you?");
+        System.out.println("Hello I'm Seashell, a task manager! What can I do for you? Type \"help\" for more " +
+                "information on the commands you can give me!");
         Scanner sc = new Scanner(System.in);
         while (true) {
             String command = sc.nextLine();
@@ -35,6 +45,8 @@ public class Seashell {
                     createDeadline(command, taskList);
                 } else if (command.startsWith("event")) {
                     createEvent(command, taskList);
+                } else if (command.stripTrailing().equals("help")) {
+                    System.out.println(HELP_TEXT);
                 } else {
                     throw new SeashellException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
