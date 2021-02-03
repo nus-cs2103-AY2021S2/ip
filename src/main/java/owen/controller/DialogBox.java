d9ba857b237c93bd083da6b451abf5ff0167acb9
Kingsley Kuan <kingsley@kingsleykuan.com>
2021-02-03 17:42:06 +0800

    Modify DialogBox to allow text to grow vertically

--- src/main/java/owen/controller/DialogBox.java
+++ src/main/java/owen/controller/DialogBox.java
@@ -9,14 +9,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 public class DialogBox extends HBox {
     @FXML
-    private Label dialog;
+    private Text dialog;
     @FXML
     private ImageView displayPicture;
 
