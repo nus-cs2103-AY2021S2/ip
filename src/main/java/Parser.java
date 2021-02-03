fff773344881ee7c422149002e757ab621a45f40
RuiXiong2211 <ruixiong8@hotmail.com>
2021-02-03 17:59:07 +0800

    Add checkstyle.xml to detect coding violations

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 import static java.lang.System.exit;
 
 
@@ -16,49 +17,50 @@ public class Parser {
 
     /**
      * method that deals with the user input.
+     *
      * @param tasks the TaskList obj that contains the current list
-     * @param fm the data.txt save file of the current user
+     * @param fm    the data.txt save file of the current user
      */
     void readCommands(TaskList tasks, Storage fm) {
         String cmd = sc.next();
         while (true) {
             try {
                 switch (cmd) {
-                    case "bye":
-                        fm.saveFile(tasks);
-                        terminate();
-                        break;
-                    case "list":
-                        tasks.listItems();
-                        break;
-                    case "todo":
-                        tasks.add(new ToDo(sc.nextLine()));
-                        break;
-                    case "deadline":
-                        String rest = sc.nextLine();
-                        String[] parts = rest.split("/by");
-                        tasks.add(new Deadline(parts[0], parts[1]));
-                        break;
-                    case "event":
-                        String rest1 = sc.nextLine();
-                        String[] parts1 = rest1.split("/at");
-                        tasks.add(new Event(parts1[0], parts1[1]));
-                        break;
-                    case "done":
-                        int number = sc.nextInt();
-                        tasks.done(number);
-                        break;
-                    case "delete":
-                        int no = sc.nextInt();
-                        tasks.delete(no);
-                        break;
-                    case "find":
-                        String keyword = sc.nextLine();
-                        FindCommand fc = new FindCommand();
-                        fc.find(tasks, keyword);
-                        break;
-                    default:
-                        break;
+                case "bye":
+                    fm.saveFile(tasks);
+                    terminate();
+                    break;
+                case "list":
+                    tasks.listItems();
+                    break;
+                case "todo":
+                    tasks.add(new ToDo(sc.nextLine()));
+                    break;
+                case "deadline":
+                    String rest = sc.nextLine();
+                    String[] parts = rest.split("/by");
+                    tasks.add(new Deadline(parts[0], parts[1]));
+                    break;
+                case "event":
+                    String rest1 = sc.nextLine();
+                    String[] parts1 = rest1.split("/at");
+                    tasks.add(new Event(parts1[0], parts1[1]));
+                    break;
+                case "done":
+                    int number = sc.nextInt();
+                    tasks.done(number);
+                    break;
+                case "delete":
+                    int no = sc.nextInt();
+                    tasks.delete(no);
+                    break;
+                case "find":
+                    String keyword = sc.nextLine();
+                    FindCommand fc = new FindCommand();
+                    fc.find(tasks, keyword);
+                    break;
+                default:
+                    break;
                 }
             } catch (DukeException E) {
                 //System.out.println("Caught DukeException.");
@@ -69,7 +71,7 @@ public class Parser {
     }
 
     /**
-     *  Method that exits the program.
+     * Method that exits the program.
      */
     void terminate() {
         System.out.println("GOOD BYE SEE YOU AGAIN!");
