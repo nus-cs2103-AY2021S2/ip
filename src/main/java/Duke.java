4626e50d2045eed9b4fef163a1e4d8bfc540b178
VisnuRavi <visnu9016@gmail.com>
2021-02-03 12:52:19 +0800

    Create a chatbox using JavaFX

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,36 +1,113 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-
-import java.time.format.DateTimeParseException;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.Region;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Class that runs the entire Duke application
  */
 public class Duke extends Application {
 
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
     @Override
     public void start(Stage stage) {
+        /*
         Label helloWorld = new Label("Hello World!"); // Creating a new Label control
         helloWorld.setFont(new Font("Arial", 50));
         Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
         stage.setScene(scene); // Setting the stage to show our screen
         stage.show(); // Render the stage.
+        */
         /*
         Stage s = new Stage();
         Label l = new Label("sdf");
         s.setScene(new Scene((l)));
         s.show();*/
+
+        scrollPane = new ScrollPane();
+        scrollPane.setPrefSize(400,585);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.setVvalue(1.0);
+        //puts scrollbar at the bottom
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer = new VBox();
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        //scroll down to the end every time dialogcontainer's height changes
+        dialogContainer.heightProperty().addListener(
+                observable -> scrollPane.setVvalue(1.0));
+
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        userInput.setPrefWidth(343.0);
+        //handler to handle when press enter
+        userInput.setOnAction((event) -> {
+            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+            userInput.clear();
+        });
+
+
+        sendButton = new Button("Send");
+        sendButton.setPrefWidth(55.0);
+        sendButton.setOnMouseClicked((event) -> {
+            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+            userInput.clear();
+        });
+
+
+        AnchorPane mainLayout = new AnchorPane();
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+        //affects the positioning of the nodes
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+        stage.show();
+    }
+
+    /**
+     * Iteration 1:
+     * Creates a label with the specified text and adds it to the dialog container.
+     * @param text String containing text to add
+     * @return a label with the specified text that has word wrap enabled.
+     */
+    private Label getDialogLabel(String text) {
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+        return textToAdd;
     }
 
     private static final String FOLDER_PATH = "./src/main/java/data/";
