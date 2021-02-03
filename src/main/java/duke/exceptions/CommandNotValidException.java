6f07b3a9fc97796e8bc233ebc9f5afccfa07f73f
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-02-04 01:39:55 +0800

    Update CommandNotValidException.java

--- src/main/java/duke/exceptions/CommandNotValidException.java
+++ src/main/java/duke/exceptions/CommandNotValidException.java
@@ -9,10 +9,10 @@ public class CommandNotValidException extends DukeException {
      */
     public CommandNotValidException() {
         super("Command not valid. Please use \"todo\", \"deadline\""
-                + "or \"event\" followed by task description to add new tasks. "
-                + "Please use \"list\" to view your list of tasks. "
-                + "Please use \"done\" followed by index to mark completed tasks. "
-                + "Please use \"delete\" followed by index to delete tasks. "
+                + "or \"event\" followed by task description to add new tasks.\n"
+                + "Please use \"list\" to view your list of tasks.\n"
+                + "Please use \"done\" followed by index to mark completed tasks.\n"
+                + "Please use \"delete\" followed by index to delete tasks.\n"
                 + "Please use \"bye\" to exit.");
     }
 }
