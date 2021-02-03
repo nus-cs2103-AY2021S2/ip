6b4fcbe615917355a4cc60d5fb7dba23132b4391
ppris <49683462+pPris@users.noreply.github.com>
2021-02-03 19:10:58 +0800

    Add code from part 4 and move javafx related lines outside of Duke.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,37 @@
 import java.util.Scanner;
 
-public class Main {
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    // todo remove tutorial part 1-3 stuff that aren't needed
+    private static Duke duke = new Duke(); //
     /**
      * Entry point of this project
      * @param args Irrelevant argument
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Duke duke = new Duke();
         duke.runDuke(sc);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
