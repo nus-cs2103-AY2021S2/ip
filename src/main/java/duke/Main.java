569e570ef4403a2c64e9690b87f0fe8f48d2881a
Stratostorm <h_weicong@outlook.com>
2021-02-03 13:19:57 +0800

    Add title to GUI

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -24,6 +24,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.setTitle("Duke, Tasklist Extraordinaire");
             stage.show();
             fxmlLoader.<MainWindow>getController().showWelcome();
         } catch (IOException e) {
