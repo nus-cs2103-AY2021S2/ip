3bdaec6261c2f6c1c50e0cab0c7a73af50be56d4
ALZAHRANI-PC\riyad <riyadh-h@outlook.sa>
2021-02-04 20:43:04 +0800

    Add a GUI

--- src/vergil/main/Vergil.java
+++ src/vergil/main/Vergil.java
@@ -1,9 +1,23 @@
 package vergil.main;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 import vergil.components.Ui;
 import vergil.components.Storage;
 import vergil.components.Parser;
 import vergil.components.TaskList;
+import vergil.components.ui.DialogBox;
 
 import vergil.types.VergilException;
 import vergil.types.Command;
@@ -11,7 +25,16 @@ import vergil.types.Todo;
 import vergil.types.Event;
 import vergil.types.Deadline;
 
-public class Vergil {
+public class Vergil extends Application {
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private Image user = new Image(this.getClass().getResourceAsStream("resources/images/user.png"));
+    private Image vergil = new Image(this.getClass().getResourceAsStream("resources/images/vergil.png"));
+
     private Ui ui;
     private Storage storage;
     private Parser parser;
@@ -19,11 +42,10 @@ public class Vergil {
 
     /**
      * Constructs a Vergil object with a specified save file path.
-     * @param filePath the path to the save file.
      */
-    public Vergil(String filePath) {
+    public Vergil() {
         ui = new Ui();
-        storage = new Storage(filePath);
+        storage = new Storage("data/vergil_list.sav");
         parser = new Parser();
 
         try {
@@ -101,7 +123,92 @@ public class Vergil {
         }
     }
 
+    @Override
+    public void start(Stage stage) {
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        stage.setScene(scene);
+        stage.show();
+
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+    }
+
+    public static DialogBox getUserDialog(Label l, ImageView iv) {
+        return new DialogBox(l, iv);
+    }
+
+    public static DialogBox getVergilDialog(Label l, ImageView iv) {
+        var db = new DialogBox(l, iv);
+        db.flip();
+        return db;
+    }
+
+    public String getResponse(String command) {
+        try {
+            return parser.parse(command).execute(taskList, storage);
+        } catch (VergilException e) {
+            return e.getMessage();
+        }
+    }
+
+    private void handleUserInput() {
+        Label userText = new Label(userInput.getText());
+        Label dukeText = new Label(getResponse(userInput.getText()));
+        dialogContainer.getChildren().addAll(
+                getUserDialog(userText, new ImageView(user)),
+                getVergilDialog(dukeText, new ImageView(vergil))
+        );
+        userInput.clear();
+    }
+
     public static void main(String[] args) {
-        new Vergil("data/vergil_list.sav").run();
+        new Vergil().run();
     }
 }
