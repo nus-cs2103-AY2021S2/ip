5a0a8d87e0101451fbd01f9bc4c093002da63ff6
garyljj <gary.lee@u.nus.edu>
2021-02-05 02:19:52 +0800

    Implement varargs in Ui

--- src/main/java/duke/util/Ui.java
+++ src/main/java/duke/util/Ui.java
@@ -7,7 +7,7 @@ import java.util.List;
  */
 public class Ui {
 
-    private String echo(List<String> msgs) {
+    private String echo(String... msgs) {
         String msg = "";
         for (String s : msgs) {
             msg += s + "\n";
@@ -15,24 +15,24 @@ public class Ui {
         return msg;
     }
 
-    private String echoSingle(String msg) {
-        return msg;
-    }
-
     /**
-     * Displays greeting messages when Duke starts.
+     * Returns greeting messages when Duke starts.
+     *
+     * @return Greeting message.
      */
     public String displayGreetings() {
-        return echo(List.of("Hello! I'm Duke",
+        return echo("Hello! I'm Duke",
                 "What can I do for you?",
-                "Enter \"help\" to see list of commands."));
+                "Enter \"help\" to see list of commands.");
     }
 
     /**
      * Lists out commands and their functions.
+     *
+     * @return Help message.
      */
     public String help() {
-        return echo(List.of("List of commands:",
+        return echo("List of commands:",
                 "bye",
                 "- Close Duke",
                 "list",
@@ -55,107 +55,120 @@ public class Ui {
                 "  - Display list of commands",
                 "search [keyword]",
                 "  - Display all task containing the following keyword.",
-                "  - If keyword is in a valid date format(YYYY-MM-DD), display all task on that date."));
+                "  - If keyword is in a valid date format(YYYY-MM-DD), display all task on that date.");
     }
 
     /**
-     * Displays error messages.
-     * 
+     * Returns error messages.
+     *
      * @param e Error to be displayed.
+     * @return Error message.
      */
     public String displayError(DukeException e) {
-        return echoSingle(e.getMessage());
+        return e.getMessage();
     }
 
     /**
-     * Displays goodbye message.
+     * Returns goodbye message.
+     *
+     * @return Goodbye message.
      */
     public String exit() {
-        return echoSingle("Bye. Hope to see you again soon!");
+        return "Bye. Hope to see you again soon!";
     }
 
     /**
-     * Displays list contents.
-     * 
+     * Returns list contents.
+     *
      * @param lst List to be displayed.
+     * @return Content of list.
      */
     public String displayList(List<String> lst) {
         if (lst.size() == 0) {
-            return echoSingle("No task found!");
+            return "No task found!";
         } else {
-            return "Here are the tasks in your list:\n" + echo(lst);
+            return "Here are the tasks in your list:\n" + echo(lst.toArray(new String[0]));
         }
     }
 
     /**
-     * Displays message after task is marked as done.
-     * 
-     * @param task Task  marked as done.
+     * Returns message after task is marked as done.
+     *
+     * @param task Task marked as done.
+     * @return Task done message.
      */
     public String completeTask(String task) {
-        return echo(List.of("Nice! I've marked this task as done:", task));
+        return echo("Nice! I've marked this task as done:", task);
     }
 
     /**
-     * Displays added task and the current number of task in the TaskList.
-     * 
+     * Returns added task and the current number of task in the TaskList.
+     *
      * @param task Task added to TaskList.
      * @param size Current size of TaskList.
+     * @return Add task message.
      */
     public String addTask(String task, int size) {
-        return echo(List.of("Got it. I've added this task:",
+        return echo("Got it. I've added this task:",
                 task,
-                "Now you have " + size + " tasks in the list."));
+                "Now you have " + size + " tasks in the list.");
     }
 
     /**
-     * Displays deleted task and the current number of task in the TaskList.
-     * 
+     * Returns deleted task and the current number of task in the TaskList.
+     *
      * @param task Task deleted from TaskList.
      * @param size Current size of TaskList.
+     * @return Delete task message.
      */
     public String deleteTask(String task, int size) {
-        return echo(List.of("Noted. I've removed this task:",
+        return echo("Noted. I've removed this task:",
                 task,
-                "Now you have " + size + " tasks in the list."));
+                "Now you have " + size + " tasks in the list.");
     }
 
     /**
-     * Displays messages after TaskList has been saved.
+     * Returns messages after TaskList has been saved.
+     *
+     * @return TaskList saved message.
      */
     public String displaySaveMessage() {
-        return echoSingle("TaskList have been saved!");
+        return "TaskList have been saved!";
     }
 
     /**
-     * Displays messages after TaskList has been loaded.
+     * Returns messages after TaskList has been loaded.
+     *
+     * @return TaskList loaded message.
      */
     public String displayLoadMessage() {
-        return echoSingle("TaskList loaded successfully!");
+        return "TaskList loaded successfully!";
     }
 
     /**
      * Prompts user to save file.
-     * 
-     * @return Response from user.
+     *
+     * @return Save file message.
      */
     public String saveFilePrompt() {
-        return echoSingle("Do you want to save the current tasklist? y/n");
+        return "Do you want to save the current tasklist? y/n";
     }
 
     /**
      * Confirms if user want to delete this task.
-     * 
-     * @return Response from user.
+     *
+     * @return Delete task confirmation message.
      */
     public String deleteTaskPrompt() {
-        return echoSingle("Are you sure you want to delete this task? y/n");
+        return "Are you sure you want to delete this task? y/n";
     }
 
     /**
-     * Displays delete aborted message.
+     * Returns delete aborted message.
+     *
+     * @return Cancel delete task message.
      */
     public String abortDelete() {
-        return echoSingle("Deletion cancelled.");
+        return "Deletion cancelled.";
     }
 }
