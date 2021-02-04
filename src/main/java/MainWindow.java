fc2d70d76f23672829f18480a5cd03793ca2bff4
icytornado <perfecttzp@gmail.com>
2021-02-04 20:46:31 +0800

    level 10 closes

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,6 +1,4 @@
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
@@ -10,23 +8,14 @@ import javafx.scene.layout.VBox;
  */
 public class MainWindow extends AnchorPane {
     @FXML
-    private ScrollPane scrollPane;
-    @FXML
     private VBox dialogContainer;
     @FXML
     private TextField userInput;
-    @FXML
-    private Button sendButton;
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("2.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("3.png"));
-
-    @FXML
-    public void initialize() {
-        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
-    }
+    private final Image userImage = new Image(this.getClass().getResourceAsStream("/image/2.png"));
+    private final Image dukeImage = new Image(this.getClass().getResourceAsStream("/image/3.png"));
 
     public void setDuke(Duke d) {
         duke = d;
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,7 +1,6 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 /**
  * Class parser makes sense of the user commands
