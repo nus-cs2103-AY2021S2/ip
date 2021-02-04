669c59b0d2ceb652f925ebfc03d155c44a6d0daa
dlau <danielau88@gmail.com>
2021-02-05 00:25:17 +0800

    Add basic styling

--- src/main/java/duke/controllers/Main.java
+++ src/main/java/duke/controllers/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            scene.getStylesheets().add(Main.class.getResource("/stylesheets/main.css").toExternalForm());
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
