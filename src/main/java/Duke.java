7795417a8ae659537682c06ad82042a1d4367cb2
kwmiw <pennypolarbear@gmail.com>
2021-02-05 14:31:46 +0800

    Add List and Add functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Duke {
     public static void main(String[] args) {
@@ -18,27 +19,40 @@ public class Duke {
 
         System.out.println("Greetings from\n" + logo);
 
-        String spacer = "____________________________________________________________\n";
+        String spacer = "\n____________________________________________________________\n";
         String greet = "Woof! I'm Doge\n"
                 + "What do you want me to do?\n"
                 + "Type your request in below!\n";
-        String goodbye = "Bye! Hope I was a good dog, see you again soon!\n";
+        String goodbye = "Bye! Hope I was a good dog, see you again soon!";
         String terminate = "bye";
-
 
 
         System.out.println(spacer + greet + spacer);
 
+        int ctr = 0;
+        HashMap<Integer, String> commands = new HashMap<Integer, String>();
+
         while (true) {
-            String echo = sc.nextLine();
-            System.out.println(spacer + echo + "\n" + spacer);
-            if (echo.equals(terminate)) {
+            String input = sc.nextLine();
+            if (input.equals(terminate)) {
                 System.out.println(spacer + goodbye + spacer);
                 break;
             }
+
+            if (input.equals("list")) {
+                System.out.println(spacer);
+                for (Integer i : commands.keySet()) {
+                    String key = i.toString();
+                    String value = commands.get(i);
+                    System.out.println(key + ". " + value);
+                }
+                System.out.println(spacer);
+            } else {
+                ctr++;
+                commands.put(ctr, input);
+                System.out.println(spacer + "added: " + input + spacer);
+            }
         }
-
         sc.close();
-
     }
 }
