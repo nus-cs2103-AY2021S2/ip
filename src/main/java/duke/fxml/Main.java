e1288289fd09a786833bd6d8dd9290955376e458
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-02-05 23:49:57 +0800

    Update Main.java

--- src/main/java/duke/fxml/Main.java
+++ src/main/java/duke/fxml/Main.java
@@ -14,7 +14,7 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-    private Duke duke = new Duke(System.getProperty("user.dir") + "/data/Duke.txt");
+    private Duke duke = new Duke("./data/Duke.txt");
 
     @Override
     public void start(Stage stage) {
