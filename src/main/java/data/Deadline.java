050210849e10c4231ce106879ec8daa4aba0a5e7
stein414 <i.s@u.nus.edu>
2021-02-05 17:39:39 +0800

    Update java formatting for vscode

--- src/main/java/data/Deadline.java
+++ src/main/java/data/Deadline.java
@@ -32,7 +32,7 @@ public class Deadline extends Task {
      */
     @Override
     public String toString() {
-        return "[D]" + super.toString()
-                + " (by: " + by.format(DateTimeFormatter.ofPattern(localDateOutputFormat)) + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern(localDateOutputFormat))
+                + ")";
     }
 }
