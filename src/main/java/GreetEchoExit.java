da14f0381856fcee189784f98cdbc15c64e587a2
Jared Lim <jaredlim1998@hotmail.com>
2021-02-05 23:03:07 +0800

    Level-5 Added try-catch blocks to flag errors, and prevent possible incorrect input from created unwanted stops/breakages in the program.

--- src/main/java/GreetEchoExit.java
+++ src/main/java/GreetEchoExit.java
@@ -27,33 +27,52 @@ public class GreetEchoExit {
                 String[] delString = command.split("\\s+");
                 markAsDone(Integer.parseInt(delString[1]));
             } else if (command.contains("todo")) {
-                Todo currentTask = new Todo(command.substring(5));
-                taskList.add(currentTask);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(currentTask);
-                System.out.println(String.format("Now you have %d tasks in the list"
-                        , taskList.size()));
+                try {
+                    Todo currentTask = new Todo(command.substring(5));
+                    taskList.add(currentTask);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(currentTask);
+                    System.out.println(String.format("Now you have %d tasks in the list"
+                            , taskList.size()));
+                } catch (StringIndexOutOfBoundsException indexError) {
+                    System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                }
+
             } else if (command.contains("event")) {
-                String[] splitString = command.split("/at");
-                String eventDesc = splitString[0];
-                String eventDate = splitString[1];
-                Event currentTask = new Event(eventDesc.substring(6), eventDate);
-                taskList.add(currentTask);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(currentTask);
-                System.out.println(String.format("Now you have %d tasks in the list"
-                        , taskList.size()));
+                try {
+                    String[] splitString = command.split("/at");
+                    String eventDesc = splitString[0];
+                    String eventDate = splitString[1];
+                    Event currentTask = new Event(eventDesc.substring(6), eventDate);
+                    taskList.add(currentTask);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(currentTask);
+                    System.out.println(String.format("Now you have %d tasks in the list"
+                            , taskList.size()));
+                } catch (ArrayIndexOutOfBoundsException | StringIndexOutOfBoundsException indexError) {
+                    System.out.println("☹ OOPS!!! The description of an event cannot " +
+                            "be empty.");
+                }
             } else if (command.contains("deadline")) {
-                String[] splitString = command.split("/by");
-                String eventDesc = splitString[0];
-                String eventDate = splitString[1];
-                Deadline currentTask = new Deadline(eventDesc.substring(9), eventDate);
-                taskList.add(currentTask);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(currentTask);
-                System.out.println(String.format("Now you have %d tasks in the list"
-                        , taskList.size()));
+                try {
+                    String[] splitString = command.split("/by");
+                    String eventDesc = splitString[0];
+                    String eventDate = splitString[1];
+                    Deadline currentTask = new Deadline(eventDesc.substring(9), eventDate);
+                    taskList.add(currentTask);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(currentTask);
+                    System.out.println(String.format("Now you have %d tasks in the list"
+                            , taskList.size()));
+                } catch (ArrayIndexOutOfBoundsException | StringIndexOutOfBoundsException indexError) {
+                    System.out.println("☹ OOPS!!! The description of a deadline " +
+                            "cannot be empty.");
+                }
+            } else {
+                // Command is not recognized
+                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
+            
             command = sc.nextLine();
 
         }
