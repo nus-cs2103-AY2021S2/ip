8494ec110bed759f117222bd428679bb126d6fc0
awzhenyi <awzhenyi@github.com>
2021-02-05 13:30:43 +0800

    Add Javafx skeleton code

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,5 +1,22 @@
 package duke;
 
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
+
+
 /**
  * Duke is a chatbot that helps the user to keep track of tasks that needs to be done.
  */
@@ -8,6 +25,7 @@ public class Duke {
 
     /**
      * main method run
+     *
      * @param args
      * @throws DukeException
      * @throws Exception
