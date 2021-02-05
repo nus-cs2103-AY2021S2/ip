272e8e6c9f6d918a87aca3713e1b95b0961b0724
laurenlhy <e0424895@u.nus.edu>
2021-02-05 23:00:35 +0800

    Update build.gradle

--- src/main/java/flamingo/Main.java
+++ src/main/java/flamingo/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setFlamingo(flamingo);
+            stage.setTitle("Flamingo");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
