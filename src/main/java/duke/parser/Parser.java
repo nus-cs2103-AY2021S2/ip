07c4386431239a2bc03f810ac0f52f8345102cb8
yeoutzer <loo_yeou_tzer@hotmail.com>
2021-02-06 22:48:58 +0800

    Update Parser.java to match code quality check

--- src/main/java/duke/parser/Parser.java
+++ src/main/java/duke/parser/Parser.java
@@ -20,22 +20,42 @@ public class Parser {
      */
     public static Command parse(String fullCommand) {
         Command command;
-        if (fullCommand.equals("bye")) {
+        if (isExitCommand(fullCommand)) {
             command = new ExitCommand();
-        } else if (fullCommand.equals("list")) {
+        } else if (isListCommand(fullCommand)) {
             command = new ListCommand();
         } else {
             String[] splitCommands = fullCommand.split(" ");
-            if (splitCommands[0].equals("done")) {
+            if (isDoneCommand(splitCommands[0])) {
                 command = new DoneCommand(fullCommand);
-            } else if (splitCommands[0].equals("delete")) {
+            } else if (isDeleteCommand(splitCommands[0])) {
                 command = new DeleteCommand(fullCommand);
-            } else if (splitCommands[0].equals("find")) {
+            } else if (isFindCommand(splitCommands[0])) {
                 command = new FindCommand(fullCommand);
             } else {
                 command = new AddCommand(fullCommand);
             }
         }
         return command;
+    }
+
+    private static boolean isExitCommand(String fullCommand) {
+        return fullCommand.equals("bye");
+    }
+
+    private static boolean isListCommand(String fullCommand) {
+        return fullCommand.equals("list");
+    }
+
+    private static boolean isDoneCommand(String command) {
+        return command.equals("done");
+    }
+
+    private static boolean isDeleteCommand(String command) {
+        return command.equals("delete");
+    }
+
+    private static boolean isFindCommand(String command) {
+        return command.equals("find");
     }
 }
