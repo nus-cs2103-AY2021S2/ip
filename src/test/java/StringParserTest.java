94885913502df3002b65127bfc27f910122867f3
WANG_YUANZHE <e0411258@u.nus.edu>
2021-02-06 13:50:59 +0800

    Deletes unused methods.

--- src/test/java/StringParserTest.java
+++ src/test/java/StringParserTest.java
@@ -8,41 +8,6 @@ import org.junit.jupiter.api.Test;
 import duke.StringParser;
 
 public class StringParserTest {
-    @Test
-    public void newLiner_shortString() {
-        assertEquals("bye\n",
-                StringParser.newLiner("bye", 5));
-
-    }
-
-    @Test
-    public void newLiner_longString() {
-        assertEquals("11111\n22222\n33333\n44444\n555\n",
-                StringParser.newLiner("11111222223333344444555", 5));
-    }
-
-    @Test
-    public void newLiner_lengthEqualOne() {
-        assertEquals("1\n2\n3\n4\n5\n",
-                StringParser.newLiner("12345", 1));
-    }
-
-    @Test
-    public void newLiner_specialString() {
-        assertEquals("1\n2\n345\n",
-                StringParser.newLiner("1\n2345", 3));
-    }
-
-    @Test
-    public void newLiner_lengthEqualZero_exceptionThrown() {
-        try {
-            assertEquals("1\n2\n3\n4\n5\n",
-                    StringParser.newLiner("12345", 0));
-            fail();
-        } catch (AssertionError e) {
-            assertEquals(AssertionError.class, e.getClass());
-        }
-    }
 
     @Test
     public void underlineGenerator_zeroRepeat() {
