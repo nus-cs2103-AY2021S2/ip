dad25ec16260cc6b5163518522407307b7c892f7
NBH99 <lametiger@hotmail.com>
2021-02-06 22:15:33 +0800

    Add A-Assertions

--- src/main/java/FindCommand.java
+++ src/main/java/FindCommand.java
@@ -7,6 +7,8 @@ public class FindCommand extends Command {
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
         String response;
+
+        assert info != null;
         if (info.equals("")) {
             throw new DukeException("OOPS!!! The description cannot be empty.");
         } else {
