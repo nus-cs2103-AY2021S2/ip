f7670742c5aa281ac6a018f4fbf9a1f0ebaf913c
Yong Kang <skykinetic@stevenkou.xyz>
2021-02-06 14:35:40 +0800

    Extract exception messages into a single class

--- src/main/java/ekud/common/exception/NoTaskDescriptionException.java
+++ src/main/java/ekud/common/exception/NoTaskDescriptionException.java
@@ -4,7 +4,8 @@ package ekud.common.exception;
  * Exception to be thrown when the task description is not given.
  */
 public class NoTaskDescriptionException extends EkudException {
+
     public NoTaskDescriptionException() {
-        super("Where your details?");
+        super(Messages.NO_TASK_DESCRIPTION_EXCEPTION_TEXT);
     }
 }
