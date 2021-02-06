c17364399719657fdd9003cbc22f21838992d434
jlxw48 <jlxw48@gmail.com>
2021-02-07 00:03:43 +0800

    Add assertions to enforce conditions

--- src/main/java/duke/utils/FileTaskStringConverter.java
+++ src/main/java/duke/utils/FileTaskStringConverter.java
@@ -23,8 +23,8 @@ public class FileTaskStringConverter {
      */
     public static List<String> allTaskToAllString(List<Task> list) {
         List<String> result = new ArrayList<>();
-        for (Task t : list) {
-            result.add(taskToString(t));
+        for (Task task : list) {
+            result.add(taskToString(task));
         }
         return result;
     }
@@ -39,8 +39,8 @@ public class FileTaskStringConverter {
      */
     public static List<Task> allStringToAllTask(List<String> list) throws InvalidTaskTypeException {
         List<Task> result = new ArrayList<>();
-        for (String s : list) {
-            result.add(stringToTask(s));
+        for (String stringTask : list) {
+            result.add(stringToTask(stringTask));
         }
         return result;
     }
@@ -59,21 +59,25 @@ public class FileTaskStringConverter {
 
     private static Task stringToTask(String input) throws InvalidTaskTypeException {
         String[] separated = input.split(" \\| ");
+        assert separated.length >= 3;
         char taskType = separated[0].charAt(0);
 
         if (taskType == 'T') {
+            assert separated.length == 3;
             ToDo todo = new ToDo(separated[2]);
             if (separated[1].equals("1")) {
                 todo.markAsDone();
             }
             return todo;
         } else if (taskType == 'D') {
+            assert separated.length == 4;
             Deadline deadline = new Deadline(separated[2], LocalDateTime.parse(separated[3], formatter));
             if (separated[1].equals("1")) {
                 deadline.markAsDone();
             }
             return deadline;
         } else if (taskType == 'E') {
+            assert separated.length == 4;
             Event event = new Event(separated[2], LocalDateTime.parse(separated[3], formatter));
             if (separated[1].equals("1")) {
                 event.markAsDone();
