69fd0ffe0244c161f9f64393ed786db2a750efba
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-02-06 23:22:32 +0800

    Add help command

--- src/main/java/fakebot/command/CommandType.java
+++ src/main/java/fakebot/command/CommandType.java
@@ -4,5 +4,5 @@ package fakebot.command;
  * Command Type Enum.
  */
 public enum CommandType {
-    BYE, LIST, DONE, TODO, EVENT, DEADLINE, DELETE, FIND;
+    BYE, LIST, DONE, TODO, EVENT, DEADLINE, DELETE, FIND, HELP;
 }
