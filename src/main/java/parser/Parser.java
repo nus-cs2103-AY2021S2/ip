a127416aae8f85ae3b9507e65d98f72722cc0aaf
Azeem Vasanwala <azeem.iron@gmail.com>
2021-02-06 19:26:39 +0800

    Improve code quality

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 public class Parser {
     /**
      * Parses a string input from the user and returns a hashmap of command-value pairs.
+     *
      * @param input User entered string.
      * @return Hashmap containing command-value pairs
      */
@@ -22,13 +23,16 @@ public class Parser {
                     Arrays.copyOfRange(firstHalf, 1, firstHalf.length)).trim());
         }
 
-        if (tokens.length > 1) {
-            String[] secondHalf = tokens[1].split(" ");
-            if (secondHalf.length > 0) {
-                String firstWord = secondHalf[0];
-                commandList.put(firstWord, String.join(" ",
-                        Arrays.copyOfRange(secondHalf, 1, secondHalf.length)).trim());
-            }
+        // We don't have a second part to the command, break out
+        if (tokens.length < 2) {
+            return commandList;
+        }
+
+        String[] secondHalf = tokens[1].split(" ");
+        if (secondHalf.length > 0) {
+            String firstWord = secondHalf[0];
+            commandList.put(firstWord, String.join(" ",
+                    Arrays.copyOfRange(secondHalf, 1, secondHalf.length)).trim());
         }
         return commandList;
     }
