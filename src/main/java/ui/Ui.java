7279becb9f90ec1eb17738cbf08b91ca565dc102
Azeem Vasanwala <azeem.iron@gmail.com>
2021-02-06 17:11:16 +0800

    Add support for a next action and remove deprecated methods

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Ui {
     private static Scanner in = new Scanner(System.in);
     private String response;
+    private FurtherAction nextAction;
 
     /**
      * Prints a string to the standard output.
@@ -15,32 +16,6 @@ public class Ui {
         System.out.println("------------------------------");
         System.out.println(str);
         System.out.println("------------------------------");
-    }
-
-    /**
-     * Greets the user.
-     */
-    public static void greet() {
-        String logo = "______                    _       _           _\n"
-                + "|  ___|                  (_)     | |         | |\n"
-                + "| |_ _ __ __ _ _ __   ___ _ ___  | |     ___ | |__\n"
-                + "|  _| '__/ _` | '_ \\ / __| / __| | |    / _ \\| '_ \\\n"
-                + "| | | | | (_| | | | | (__| \\__ \\ | |___| (_) | | | |\n"
-                + "\\_| |_|  \\__,_|_| |_|\\___|_|___/ \\_____/\\___/|_| |_|\n"
-                + "\n"
-                + "\n";
-        System.out.println("Hello from\n" + logo + "(a.k.a Loh Jing Yen)");
-        System.out.println("What can I do for you?");
-        System.out.println("Enter a command below for me to assist you");
-    }
-
-    /**
-     * Reads commands from the standard input
-     *
-     * @return Command string entered by the user
-     */
-    public static String readCommand() {
-        return in.nextLine().toLowerCase();
     }
 
     public String getResponse() {
@@ -55,5 +30,13 @@ public class Ui {
         } else {
             this.response += "\n" + response;
         }
+    }
+
+    public FurtherAction getNextAction() {
+        return nextAction;
+    }
+
+    public void setNextAction(FurtherAction nextAction) {
+        this.nextAction = nextAction;
     }
 }
