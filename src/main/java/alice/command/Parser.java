b1747f5bc5a420da822a882f13b556b783cbc118
lue97 <llue97@hotmail.com>
2021-02-06 11:47:53 +0800

    Add assertions

--- src/main/java/alice/command/Parser.java
+++ src/main/java/alice/command/Parser.java
@@ -60,6 +60,7 @@ public class Parser {
 			}
 		}
 		tmpList.add(0, tokens[0]);
+		assert functionMap.containsKey(tokens[0]);
 		return functionMap.get(tokens[0]).apply(tmpList.toArray(new String[]{}));
 	}
 }
