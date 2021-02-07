3931c0c3642b4386fb2ac682422fb3cf08b4dabf
Jellybeano <lanyuxuan98@gmail.com>
2021-02-07 23:04:27 +0800

    Make changes to Gradle

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -1,5 +1,6 @@
 package duke;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.Collections;
 
@@ -7,12 +8,22 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.effect.BlendMode;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+import javax.sound.sampled.Clip;
 
 /**
  * An example of a custom control using FXML.
@@ -50,12 +61,19 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        DialogBox userMessage = new DialogBox(text, img);
+        userMessage.setBackground(new Background(new BackgroundFill(Color.LAVENDERBLUSH,
+                CornerRadii.EMPTY,
+                Insets.EMPTY)));
+        return userMessage;
     }
 
     public static DialogBox getDukeDialog(String text, Image img) {
         var db = new DialogBox(text, img);
         db.flip();
+        db.setBackground(new Background(new BackgroundFill(Color.LIGHTCYAN,
+                CornerRadii.EMPTY,
+                Insets.EMPTY)));
         return db;
     }
 }
