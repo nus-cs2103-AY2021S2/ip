c87390b8fb7f823baf73787b9476b8a947696d01
IceBear789 <boreman90@gmail.com>
2021-02-07 12:54:06 +0800

    Add: More duke images

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -22,7 +22,7 @@ public class MainWindow extends AnchorPane {
     private Duke duke;
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/iceBear.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/iceBear.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/FrowningIceBear.png"));
 
     @FXML
     public void initialize() {
