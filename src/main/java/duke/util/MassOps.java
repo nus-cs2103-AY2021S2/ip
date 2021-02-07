a9a5a644a93e1aa39e0806729061f13f83adc13a
natosy <natosyy@gmail.com>
2021-02-07 13:58:44 +0800

    Add JavaDocs

--- src/main/java/duke/util/MassOps.java
+++ src/main/java/duke/util/MassOps.java
@@ -6,7 +6,18 @@ import duke.DukeException;
 import duke.storage.Storage;
 import duke.task.Task;
 
+/**
+ * Class which encapsulates behaviour related to mass operations of tasks in the task list.
+ */
 public class MassOps {
+    /**
+     * Identifies range of tasks corresponding to the user's input.
+     *
+     * @param args user input.
+     * @return range of tasks within an integer array of size 2.
+     * @throws DukeException if the first digit of the range provided is larger than the second or if the arguments
+     *                       provided are not integers.
+     */
     public static int[] identifyRange(String args) throws DukeException {
         String[] range = args.split("-");
         int[] taskRange = new int[2];
@@ -32,6 +43,14 @@ public class MassOps {
         return taskRange;
     }
 
+    /**
+     * Gets tasks in the storage which correspond to the range provided.
+     *
+     * @param range integer array where index 0 represents the start of the range and index 1 represents the end of
+     *              the range.
+     * @return ArrayList corresponding to the tasks in the task list matching the range provided.
+     * @throws DukeException if the range provided is out of bounds.
+     */
     public static ArrayList<Task> getTasksWithinRange(int[] range) throws DukeException {
         ArrayList<Task> tasks = Storage.getTasks();
         ArrayList<Task> tasksInRange = new ArrayList<>();
