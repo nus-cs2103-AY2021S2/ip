75020e6120814028949c51366d0148e2848ba62f
ming-00 <e0470728@u.nus.edu>
2021-02-08 23:40:41 +0800

    Add get help extension

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -44,6 +44,8 @@ public class Parser {
         } else if (input.equals("list")) {
             // show everything in the list
             return tasks.iterateList();
+        } else if (input.equals("help")){
+            return Ui.getHelp();
         } else if (input.split(" ", 2)[0].equals("done")) {
             // mark task with the given index as completed
             return tasks.markAsDone(input.split(" ", 2)[1]);
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,9 +1,13 @@
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Ui {
 
     /**
      * Prints a welcome message on the console when Duke is first opened.
      */
-    public void printHello() {
+    public String printHello() {
         String logo = ".------..------..------..------.\n"
                 + "|D.--. ||U.--. ||K.--. ||E.--. |\n"
                 + "| :/\\: || (\\/) || :/\\: || (\\/) |\n"
@@ -12,6 +16,7 @@ public class Ui {
                 + "`------'`------'`------'`------'";
 
         System.out.println("Hello, this is\n" + logo + "\n What can I do for you today?\n");
+        return "Hello, this is\n" + logo + "\n What can I do for you today?\n";
     }
 
     /**
@@ -21,6 +26,24 @@ public class Ui {
     public static String sayGoodbye() {
         System.out.println("Bye, see you soon! Don't miss me too much.");
         return "Bye, see you soon! Don't miss me too much.";
+    }
+
+    /**
+     * Provides commands available to user.
+     * @return list of all usable commands
+     */
+    public static String getHelp() {
+        String introduction = "Hey! Here are the commands you can give me:\n";
+        String addTodo = "To add a todo, write \"todo\" followed by a task description.\n";
+        String addTask = "To add a task with deadline, write \"task\" " +
+                "followed by task description, then \"by\" YYYY-MM-DD.\n";
+        String addEvent = "To add an event, write \"event\" " +
+                "followed by event description, then \"at\" YYYY-MM-DD.\n";
+        String showList = "Use \"list\" to see all your tasks.\n";
+        String markDone = "Use \"done\" and the task's index to mark your task as complete.\n";
+        String deleteItem = "Use \"delete\" and the task's index to delete your task.\n";
+        String bye = "Use \"bye\" to close me!";
+        return introduction + addTodo + addTask + addEvent + showList + markDone + deleteItem + bye;
     }
 
     /**
