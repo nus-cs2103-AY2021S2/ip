ca022428a88e5b91d17ff81fcf4c1e3049b17065
chewwh09 <weishawtyhao9@gmail.com>
2021-02-08 22:18:29 +0800

    Follow 2103 Code Quality

--- src/main/java/duke/logging/TaskList.java
+++ src/main/java/duke/logging/TaskList.java
@@ -51,11 +51,13 @@ public class TaskList {
         Task task;
         if (taskDescription.length() == 0) {
             throw new InvalidDescriptionException("OOPS!!! The description of " + type + " cannot be empty.");
-        } else if (type.equals("todo")) {
-            task = new ToDo(false, taskDescription);
-        } else if ((type.equals("deadline") && !taskDescription.contains("/by"))
+        }else if ((type.equals("deadline") && !taskDescription.contains("/by"))
                 || (type.equals("event")) && !taskDescription.contains("/at")) {
             throw new InvalidDescriptionException("OOPS!!! The description format of " + type + " is wrong.");
+        }
+
+        if (type.equals("todo")) {
+            task = new ToDo(false, taskDescription);
         } else {
             int index = type.equals("deadline")
                     ? taskDescription.indexOf("/by")
