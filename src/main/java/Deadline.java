5f78a3c2ab63317676c048b24510290822bc1deb
shermanyuen <ysm@u.nus.edu>
2021-02-08 20:11:57 +0800

    Add Level-8 requirements: Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,18 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDate.parse(by);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.doneToString() + " (by: " + by + ")";
+        String dateFormatted = by.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        return "[D]" + super.doneToString() + " (by: " + dateFormatted + ")";
     }
 }
