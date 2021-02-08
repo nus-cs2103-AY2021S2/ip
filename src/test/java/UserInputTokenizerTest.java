cd9d75d050c3461dffc2ff563f4437ae5a51b895
pyuxiang <pyxelise@gmail.com>
2021-02-08 23:44:27 +0800

    Clean imports for tokenizer unit test

--- src/test/java/UserInputTokenizerTest.java
+++ src/test/java/UserInputTokenizerTest.java
@@ -1,12 +1,8 @@
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import duke.exceptions.DukeExceptionIllegalArgument;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import duke.exceptions.DukeExceptionIllegalArgument;
 import duke.parser.UserInputTokenSet;
 import duke.parser.UserInputTokenizer;
 
