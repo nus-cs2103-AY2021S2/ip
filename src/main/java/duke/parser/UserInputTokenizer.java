dfd429bcddc217347e3f353ebb51db11fadc4766
pyuxiang <pyxelise@gmail.com>
2021-02-08 23:37:01 +0800

    Fix bugs in tokenization

--- src/main/java/duke/parser/UserInputTokenizer.java
+++ src/main/java/duke/parser/UserInputTokenizer.java
@@ -17,7 +17,7 @@ public final class UserInputTokenizer {
     private UserInputTokenizer() {}
 
     /**
-     * Returns a String of tokens parsed from user input.
+     * Returns a UserInputTokenSet parsed from user input.
      *
      * All commands should strictly follow the following format:
      * <pre>{@code <command> <text> [/<option> <argument>]* }</pre>
@@ -28,12 +28,18 @@ public final class UserInputTokenizer {
      * 3. Options should be strictly prefixed with one '/', and more '/' should not be contained.
      * 4. Leading and trailing whitespaces for each token are stripped.
      * 5. Repeated whitespaces are collapsed.
+     * 6. Duplicate options resolve to last specified
+     *
+     * Commands are stored in '/command' key, and corresponding text in '/text' key,
+     * to disambiguate from potential 'command' and 'text' options, and avoids accidental
+     * overwriting of command.
      *
      * @param userInput user input string from program
      * @return UserInputTokenSet
+     * @see UserInputTokenSet
      */
-    public UserInputTokenSet parse(String userInput) throws DukeExceptionIllegalArgument {
-        String[] t = userInput.strip().split("/s+"); // tokenize and remove duplicate whitespaces
+    public static UserInputTokenSet parse(String userInput) throws DukeExceptionIllegalArgument {
+        String[] t = userInput.strip().split("\\s+"); // tokenize and remove duplicate whitespaces
         Queue<String> tokens = new LinkedList<>(Arrays.asList(t));
         UserInputTokenSet tokenSet = new UserInputTokenSet();
 
@@ -43,8 +49,8 @@ public final class UserInputTokenizer {
         }
 
         // Read command and text
-        tokenSet.set("command", extractOption(tokens));
-        tokenSet.set("text", extractText(tokens));
+        tokenSet.set("/command", extractOption(tokens));
+        tokenSet.set("/text", extractText(tokens));
 
         // Process arguments
         while (!tokens.isEmpty()) {
@@ -54,12 +60,16 @@ public final class UserInputTokenizer {
         return tokenSet;
     }
 
-    private String extractOption(Queue<String> tokens) {
+    private static String extractOption(Queue<String> tokens) {
         assert !tokens.isEmpty();
-        return tokens.poll().toLowerCase();
+        String option = tokens.poll().toLowerCase();
+        if (option.startsWith("/")) { // removes option indicator
+            return option.substring(1);
+        }
+        return option;
     }
 
-    private String extractText(Queue<String> tokens) {
+    private static String extractText(Queue<String> tokens) {
         List<String> textTokens = new ArrayList<>();
         while (!tokens.isEmpty()) { // note: tokens can be empty
             if (isOptionToken(tokens.peek())) {
@@ -71,7 +81,7 @@ public final class UserInputTokenizer {
     }
 
     /** Returns true if only one '/' exists, specifically at start of string */
-    private boolean isOptionToken(String token) {
+    private static boolean isOptionToken(String token) {
         return token.startsWith("/") && (token.lastIndexOf("/") == 0);
     }
 }
