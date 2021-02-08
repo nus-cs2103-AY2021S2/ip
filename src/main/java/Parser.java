8f7aa12eaf4966f29d4be60a9d05974910f4d73a
Ellevy <46534252+Ellevy@users.noreply.github.com>
2021-02-08 20:43:19 +0800

    Improve code quality

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -47,7 +47,7 @@ public class Parser {
     }
 
     /**
-     * Sets a task as done and prints out the task marked as done.
+     * Sets a task as done and return the task marked as done.
      *
      * @param userInput Takes in command from user in the format: done &lt;indexOfItem&gt;
      * @return String representing the output of processing command.
@@ -67,12 +67,12 @@ public class Parser {
     }
 
     /**
-     * Deletes a task in the list and prints out the task deleted with the remaining number of tasks in the list.
+     * Deletes a task in the list and returns the task deleted with the remaining number of tasks in the list.
      *
      * @param userInput Takes in command from user in the format: delete &lt;indexOfItem&gt;
      * @return String representing the output of processing command.
      */
-    protected String deleteFromList(String userInput) {
+    private String deleteFromList(String userInput) {
         String textToReturn = "";
         try {
             int indexToDelete = Integer.parseInt(userInput.substring(7));
@@ -93,7 +93,7 @@ public class Parser {
      * @param userInput Takes in command from user in the format: find &lt;keyword&gt;
      * @return String representing the output of processing command.
      */
-    protected String findFromList(String userInput) {
+    private String findFromList(String userInput) {
         TaskList tasksMatchingKeyword = new TaskList();
         String keyword = userInput.substring(5);
         for (int i = 1; i <= tasks.getSize(); i++) {
@@ -106,7 +106,9 @@ public class Parser {
     }
 
     /**
-     * Adds a task into the list and prints out the task added with the number of tasks in the list.
+     * Adds a task into the list and returns the task added with the number of tasks in the list.
+     * Calls the respective functions to add different tasks into the list.
+     * Else returns an error string describing the error.
      *
      * @param userInput Takes in command from user in the given formats for each task type:
      *                  <p> todo task: todo &lt;task_description&gt; </p>
@@ -118,49 +120,94 @@ public class Parser {
     protected String addTaskToList(String userInput, boolean isDone) {
         String textToReturn = "";
         if (userInput.startsWith("todo ")) {
-            String[] splits = userInput.split("todo ");
-            if (splits.length == 2) {
-                Todo addedTask = new Todo(Arrays.asList(splits).get(1), isDone);
+            textToReturn += addTodo(userInput, isDone);
+        } else if (userInput.startsWith("deadline ")) {
+            textToReturn += addDeadline(userInput, isDone);
+        } else if (userInput.startsWith("event ")) {
+            textToReturn += addEvent(userInput, isDone);
+        } else {
+            textToReturn += new KeywordException().toString();
+        }
+        textToReturn += "\nYou currently have " + tasks.getSize() + " tasks in the list.";
+        return textToReturn;
+    }
+
+    /**
+     * Adds a Todo-task into the list and returns the confirmation of adding the task.
+     * Else returns an error string describing the error.
+     *
+     * @param userInput Takes in command from user in the given format:
+     *                  <p> todo task: todo &lt;task_description&gt; </p>
+     * @param isDone True if task to be added is done, else false.
+     * @return String representing the output of processing command.
+     */
+    private String addTodo(String userInput, boolean isDone) {
+        String[] splits = userInput.split("todo ");
+        String textToReturn = "";
+        if (splits.length == 2) {
+            Todo addedTask = new Todo(Arrays.asList(splits).get(1), isDone);
+            tasks.addTask(addedTask);
+            textToReturn += "Got it, I've added this task to the list: \n";
+            textToReturn += "  " + addedTask;
+        } else {
+            textToReturn = new ArgumentException(1).toString();
+        }
+        return textToReturn;
+    }
+
+    /**
+     * Adds a Deadline-task into the list and returns the confirmation of adding the task.
+     * Else returns an error string describing the error.
+     *
+     * @param userInput Takes in command from user in the given format:
+     *                  <p> deadline task: deadline &lt;task_description&gt; /by &lt;date&gt; </p>
+     * @param isDone True if task to be added is done, else false.
+     * @return String representing the output of processing command.
+     */
+    private String addDeadline(String userInput, boolean isDone) {
+        String[] splits = userInput.split("deadline |/by ");
+        String textToReturn = "";
+        if ((splits.length == 3) && !(splits[1].equals("")) && !(splits[2].equals(""))) {
+            try {
+                Deadline addedTask = new Deadline(Arrays.asList(splits).get(1),
+                    isDone, Arrays.asList(splits).get(2));
                 tasks.addTask(addedTask);
                 textToReturn += "Got it, I've added this task to the list: \n";
                 textToReturn += "  " + addedTask;
-            } else {
-                textToReturn = new ArgumentException(1).toString();
-            }
-        } else if (userInput.startsWith("deadline ")) {
-            String[] splits = userInput.split("deadline |/by ");
-            if ((splits.length == 3) && !(splits[1].equals("")) && !(splits[2].equals(""))) {
-                try {
-                    Deadline addedTask = new Deadline(Arrays.asList(splits).get(1),
-                            isDone, Arrays.asList(splits).get(2));
-                    tasks.addTask(addedTask);
-                    textToReturn += "Got it, I've added this task to the list: \n";
-                    textToReturn += "  " + addedTask;
-                } catch (DukeException ex) {
-                    textToReturn = ex.toString();
-                }
-            } else {
-                textToReturn = new ArgumentException(2).toString();
-            }
-        } else if (userInput.startsWith("event ")) {
-            String[] splits = userInput.split("event | /at ");
-            if ((splits.length == 3) && !(splits[1].equals("")) && !(splits[2].equals(""))) {
-                try {
-                    Event addedTask = new Event(Arrays.asList(splits).get(1),
-                            isDone, Arrays.asList(splits).get(2));
-                    tasks.addTask(addedTask);
-                    textToReturn += "Got it, I've added this task to the list: \n";
-                    textToReturn += "  " + addedTask;
-                } catch (DukeException ex) {
-                    textToReturn = ex.toString();
-                }
-            } else {
-                textToReturn = new ArgumentException(3).toString();
+            } catch (DukeException ex) {
+                textToReturn = ex.toString();
             }
         } else {
-            textToReturn = new KeywordException().toString();
+            textToReturn = new ArgumentException(2).toString();
         }
-        textToReturn += "\nYou currently have " + tasks.getSize() + " tasks in the list.";
+        return textToReturn;
+    }
+
+    /**
+     * Adds a Event-task into the list and returns the confirmation of adding the task.
+     * Else returns an error string describing the error.
+     *
+     * @param userInput Takes in command from user in the given format:
+     *                  <p> event task: event &lt;event_description&gt; /at &lt;date&gt; </p>
+     * @param isDone True if task to be added is done, else false.
+     * @return String representing the output of processing command.
+     */
+    private String addEvent(String userInput, boolean isDone) {
+        String[] splits = userInput.split("event | /at ");
+        String textToReturn = "";
+        if ((splits.length == 3) && !(splits[1].equals("")) && !(splits[2].equals(""))) {
+            try {
+                Event addedTask = new Event(Arrays.asList(splits).get(1),
+                    isDone, Arrays.asList(splits).get(2));
+                tasks.addTask(addedTask);
+                textToReturn += "Got it, I've added this task to the list: \n";
+                textToReturn += "  " + addedTask;
+            } catch (DukeException ex) {
+                textToReturn = ex.toString();
+            }
+        } else {
+            textToReturn = new ArgumentException(3).toString();
+        }
         return textToReturn;
     }
 
