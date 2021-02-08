de9acc136fb7eb24910df11b1ef2f661b0f6bd8c
Assyarul <muhdassyarul@gmail.com>
2021-02-08 16:11:15 +0800

    Add Assertions and more JavaDocs
    No Assertions has been implemented in current version
    and some JavaDocs were incomplete.
    
    Let's add some Assertions in DialogBox and Ui, ensuring
    FXML files are loaded correctly.

--- src/main/java/duke/task/EventFactory.java
+++ src/main/java/duke/task/EventFactory.java
@@ -1,9 +1,19 @@
 package duke.task;
 
+/**
+ * Factory Class responsible for creating Event objects.
+ */
 public class EventFactory extends AbstractTaskFactory {
+    /**
+     * Creates a Event object using the parameters given.
+     * @param parameters Information needed to create event.
+     * @return Event object
+     * @throws IllegalArgumentException thrown when invalid arguments are given.
+     */
     @Override
     public Task createTask(String parameters) throws IllegalArgumentException {
         String[] inputArray = parameters.split("/at", 2);
+        assert inputArray != null : "inputArray should not be null";
 
         if (inputArray[0].isBlank()) {
             throw new IllegalArgumentException("Error: Description of event cannot be empty");
