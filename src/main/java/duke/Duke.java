f4c86035102690a3daed68abf622122e8c5fa9fb
lirc572 <wyharry628@gmail.com>
2021-02-08 21:29:49 +0800

    Level 10 :beer:

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,48 +1,34 @@
 package duke;
-
 import duke.ui.Ui;
 import duke.exception.DukeException;
 import duke.fileSaver.FileSaver;
 import duke.task.TaskList;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-
-
-public class Duke extends Application{
-    private TaskList task;
+public class Duke{
+    private TaskList tasks;
     private Ui ui;
     private FileSaver fs;
 
-    public Duke (String folderName, String fileName) {
-        fs = new FileSaver(folderName, fileName);
+    public Duke() {
+        fs = new FileSaver("data", "duke.txt");
         ui = new Ui();
-        task = new TaskList();
-    }
-    public void run() {
-        ui.greeting();
+        tasks = new TaskList();
         try {
-            fs.load(task);
+            fs.load(tasks);
         } catch (DukeException e) {
-            //TODO: handle exception
             ui.printErrorMessage(e.getMessage());
         }
-        ui.runUi(task, fs);
-        ui.close();
     }
 
-    @Override
-    public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        return ui.getResponse(input, tasks, fs);
     }
 
-    public static void main(String[] args) {
-        new Duke("data", "duke.txt").run();
+    public String greeting() {
+        return ui.greetingMessage();
     }
 }
