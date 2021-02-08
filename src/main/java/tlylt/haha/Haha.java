f6a9d9c5d4d2ffc87d9bf9e3422665d110071a1c
tlylt <liu_yongliang@hotmail.com>
2021-02-09 00:39:15 +0800

    Improve code quality
    Current code requires refactoring due to the newly added GUI.
    
    Let's improve code quality by refactoring some of the methods.
    
    This makes the maintenance of code easier.

--- src/main/java/tlylt/haha/Haha.java
+++ src/main/java/tlylt/haha/Haha.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,22 +12,21 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Haha extends Application {
     private final TaskList database = new TaskList();
     private final Storage storage = new Storage();
     private final Ui ui = new Ui();
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
+    private ScrollPane scrollPane = new ScrollPane();
+    private VBox dialogContainer = new VBox();
+    private TextField userInput = new TextField();
+    private Button sendButton = new Button("Send");
     private Scene scene;
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image hahaImage = new Image(this.getClass().getResourceAsStream("/images/DaHaha.png"));
+    private final Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private final Image hahaImage = new Image(this.getClass().getResourceAsStream("/images/DaHaha.png"));
 
     public Haha() {
         List<String> list = this.storage.getTasks();
@@ -38,92 +38,124 @@ public class Haha extends Application {
     @Override
     public void start(Stage stage) {
         // Step 1 Setting up required components
+        setupComponents(stage);
 
+        // Step 2 Formatting the window to look as expected
+        formatStage(stage);
+        formatScrollPane();
+        formatSmallerComponents();
+        setLayout();
+
+        // Step 3 Add functionality to handle user input
+        configureUserInteraction();
+
+        // Starter
+        new Haha();
+        dialogContainer.getChildren().add(makeHahaDialogBox(ui.welcome()));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //@@author Jeffry Lum-reused
+    //Reused from https://se-education.org/guides/tutorials/javaFx.html with minor modifications
+    private void setupComponents(Stage stage) {
+        stage.getIcons().add(hahaImage);
         // The container for the content of the chat to scroll
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
         scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
 
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        mainLayout.setPrefSize(400.0, 600.0);
+        mainLayout.setBackground(new Background(
+                new BackgroundFill(Color.rgb(231, 239, 248), CornerRadii.EMPTY, Insets.EMPTY)));
+        this.scene = new Scene(mainLayout);
 
-        scene = new Scene(mainLayout);
+    }
 
-        stage.setScene(scene);
-        stage.show();
-
-        // Step 2 Formatting the window to look as expected
+    //@@author Jeffry Lum-reused
+    //Reused from https://se-education.org/guides/tutorials/javaFx.html with minor modifications
+    private void formatStage(Stage stage) {
         stage.setTitle("Haha");
         stage.setResizable(false);
         stage.setMinHeight(600.0);
         stage.setMinWidth(400.0);
+    }
 
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
+    //@@author Jeffry Lum-reused
+    //Reused from https://se-education.org/guides/tutorials/javaFx.html with minor modifications
+    private void formatScrollPane() {
+        scrollPane.setPrefSize(400, 535);
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
         scrollPane.setVvalue(1.0);
         scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+        dialogContainer.setBackground(new Background(
+                new BackgroundFill(Color.rgb(231, 239, 248), CornerRadii.EMPTY, Insets.EMPTY)));
+    }
 
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
+    //@@author Jeffry Lum-reused
+    //Reused from https://se-education.org/guides/tutorials/javaFx.html with minor modifications
+    private void setLayout() {
         AnchorPane.setTopAnchor(scrollPane, 1.0);
-
         AnchorPane.setBottomAnchor(sendButton, 1.0);
         AnchorPane.setRightAnchor(sendButton, 1.0);
-
         AnchorPane.setLeftAnchor(userInput, 1.0);
         AnchorPane.setBottomAnchor(userInput, 1.0);
+    }
 
-        // Step 3 Add functionality to handle user input
+    //@@author Jeffry Lum-reused
+    //Reused from https://se-education.org/guides/tutorials/javaFx.html with minor modifications
+    private void formatSmallerComponents() {
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        userInput.setPrefWidth(325.0);
+        sendButton.setPrefWidth(55.0);
+    }
+
+    //@@author Jeffry Lum-reused
+    //Reused from https://se-education.org/guides/tutorials/javaFx.html with minor modifications
+    private void configureUserInteraction() {
         sendButton.setOnMouseClicked((event) -> handleUserInput());
         userInput.setOnAction((event) -> handleUserInput());
 
         // Scroll down to the end every time dialogContainer's height changes
         dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        // Starter
-        new Haha();
-        Label hahaStarter = new Label(ui.welcome());
-        dialogContainer.getChildren().add(
-                DialogBox.getHahaDialog(hahaStarter, new ImageView(hahaImage))
-        );
-
     }
 
+    private DialogBox makeUserDialogBox(String text) {
+        Label label = new Label(text);
+        return DialogBox.getUserDialog(label, new ImageView(userImage));
+    }
+
+    private DialogBox makeHahaDialogBox(String text) {
+        Label label = new Label(text);
+        return DialogBox.getHahaDialog(label, new ImageView(hahaImage));
+    }
 
     /**
-     * Iteration 2:
      * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
      * the dialog container. Clears the user input after processing.
      */
     //@@author Jeffry Lum-reused
     //Reused from https://se-education.org/guides/tutorials/javaFx.html with minor modifications
     private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label hahaText = new Label(getResponse(userInput.getText()));
+        String rawUserInput = userInput.getText();
+
         dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(userImage)),
-                DialogBox.getHahaDialog(hahaText, new ImageView(hahaImage))
+                makeUserDialogBox(rawUserInput),
+                makeHahaDialogBox(getResponse(rawUserInput))
         );
+
         userInput.clear();
     }
 
+
     /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
+     * Generates a response from user input.
+     *
+     * @param input raw input string from user.
+     * @return String response from the program after commands are executed.
      */
-    //@@author Jeffry Lum-reused
-    //Reused from https://se-education.org/guides/tutorials/javaFx.html with minor modifications
     private String getResponse(String input) {
         String response = "";
         try {
