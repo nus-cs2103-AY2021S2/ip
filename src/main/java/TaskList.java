8cbf526d0a2ae1828c21032ef983cdb65b9c5a39
ming-00 <e0470728@u.nus.edu>
2021-02-08 18:10:30 +0800

    Format UI responses

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -11,6 +11,11 @@ public class TaskList {
     private static final String ADD_DEADLINE_COMMAND = "deadline";
     private static final String ADD_EVENT_COMMAND = "event";
 
+    private static final String ADD_TASK_RESPONSE = "You got it! I added this task:\n   ";
+    private static final String DELETE_TASK_RESPONSE = "Sweet! I have deleted the following task:\n";
+    private static final String WRONG_COMMAND_RESPONSE = "Hey! What is this gibberish?";
+    private static final String WRONG_FORMAT_RESPONSE = "Did you format your request properly? \n This is getting old.";
+
     List<Task> lst = new ArrayList<>();
 
     /**
@@ -71,8 +76,8 @@ public class TaskList {
             if (!command.equals(ADD_TODO_COMMAND) && !command.equals(ADD_DEADLINE_COMMAND)
                     && !command.equals(ADD_EVENT_COMMAND)) {
                 // error: unknown command
-                System.out.println("Hey! What is this gibberish?");
-                return "Hey! What is this gibberish?";
+                System.out.println(WRONG_COMMAND_RESPONSE);
+                return WRONG_COMMAND_RESPONSE;
             } else if (split.length < 2) {
                 // error: command empty
                 System.out.println("You gotta give me a description to work with, buddy:\nCommand \""
@@ -82,10 +87,10 @@ public class TaskList {
             } else if (command.equals("todo")) {
                 Todo todo = new Todo(split[1]);
                 lst.add(todo);
-                System.out.println("You got it! I added this task:\n   "
+                System.out.println(ADD_TASK_RESPONSE
                         + todo.toString());
                 countTasks();
-                return "You got it! I added this task:\n   "
+                return ADD_TASK_RESPONSE
                         + todo.toString() + "\n"
                         + countTasks();
             } else {
@@ -100,25 +105,23 @@ public class TaskList {
                         Deadline deadline = new Deadline(description, localDate);
 
                         lst.add(deadline);
-                        System.out.println("You got it! I added this task:\n   "
+                        System.out.println(ADD_TASK_RESPONSE
                                 + deadline.toString());
-                        return "You got it! I added this task:\n   "
+                        return ADD_TASK_RESPONSE
                                 + deadline.toString() + "\n" + countTasks();
                     } else {
                         // command is ADD_EVENT_COMMAND
                         Event event = new Event(description, localDate);
                         lst.add(event);
-                        System.out.println("You got it! I added this task:\n   "
+                        System.out.println(ADD_TASK_RESPONSE
                                 + event.toString());
-                        return "You got it! I added this task:\n   "
+                        return ADD_TASK_RESPONSE
                                 + event.toString() + "\n" + countTasks();
                     }
                     //countTasks();
                 } catch (ArrayIndexOutOfBoundsException e){
-                    System.err.println("Did you format your request properly? " +
-                            "This is getting old.");
-                    return "Did you format your request properly? " +
-                            "This is getting old.";
+                    System.err.println(WRONG_FORMAT_RESPONSE);
+                    return WRONG_FORMAT_RESPONSE;
                 }
             }
         } catch (DateTimeParseException e) {
@@ -138,12 +141,10 @@ public class TaskList {
             lst.remove(index);
 
             Ui.formatText();
-            System.out.println("Sweet! I have deleted the following task:\n"
-                    + task.toString());
+            System.out.println(DELETE_TASK_RESPONSE + task.toString());
             countTasks();
             Ui.formatText();
-            return "Sweet! I have deleted the following task:\n"
-                    + task.toString() + "\n" + countTasks();
+            return DELETE_TASK_RESPONSE + task.toString() + "\n" + countTasks();
         } catch (final NumberFormatException e) {
             System.err.println("Oof, did you type a valid number or not?");
             return "Oof, did you type a valid number or not?";
