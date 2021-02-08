f7f784b3569b44498ae0fb2e81554c44bc4eafaa
Lim Li Quan <linliquan1996@Hotmail.com>
2021-02-08 20:41:24 +0800

    Edit photo of duke and user to be circle and color user label

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -10,7 +10,11 @@ import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 /**
  * An example of a custom control using FXML.
@@ -22,6 +26,7 @@ public class DialogBox extends HBox {
     private Label dialog;
     @FXML
     private ImageView displayPicture;
+    private final Circle clip = new Circle(50, 50, 50);
 
     private DialogBox(String text, Image img) {
         try {
@@ -35,6 +40,7 @@ public class DialogBox extends HBox {
 
         dialog.setText(text);
         displayPicture.setImage(img);
+        displayPicture.setClip(clip);
     }
 
     /**
@@ -48,7 +54,9 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        var db = new DialogBox(text, img);
+        db.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE,null,null)));
+        return db;
     }
 
     public static DialogBox getDukeDialog(String text, Image img) {
