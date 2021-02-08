864853fd3c6f2489bb3901020044a2328ced5e89
mechastriker3 <33394879+mechastriker3@users.noreply.github.com>
2021-02-08 18:25:44 +0800

    Use FXML

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,7 +19,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class Duke extends Application{
+public class Duke {
     private final Storage storage;
     private TaskList tasks;
     private final Ui ui;
@@ -70,64 +70,64 @@ public class Duke extends Application{
     //     }
     // }
 
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Setting up required components
-
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        //Part 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> handleUserInput());
-        userInput.setOnAction((event) -> handleUserInput());
-
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-    }
+    // @Override
+    // public void start(Stage stage) {
+    //     //Step 1. Setting up required components
+    //
+    //     //The container for the content of the chat to scroll.
+    //     scrollPane = new ScrollPane();
+    //     dialogContainer = new VBox();
+    //     scrollPane.setContent(dialogContainer);
+    //
+    //     userInput = new TextField();
+    //     sendButton = new Button("Send");
+    //
+    //     AnchorPane mainLayout = new AnchorPane();
+    //     mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+    //
+    //     scene = new Scene(mainLayout);
+    //
+    //     stage.setScene(scene);
+    //     stage.show();
+    //
+    //     //Step 2. Formatting the window to look as expected
+    //     stage.setTitle("HELLO BOB");
+    //     stage.setResizable(false);
+    //     stage.setMinHeight(600.0);
+    //     stage.setMinWidth(400.0);
+    //
+    //     mainLayout.setPrefSize(400.0, 600.0);
+    //
+    //     scrollPane.setPrefSize(385, 535);
+    //     scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+    //     scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+    //
+    //     scrollPane.setVvalue(1.0);
+    //     scrollPane.setFitToWidth(true);
+    //
+    //     // You will need to import `javafx.scene.layout.Region` for this.
+    //     dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+    //
+    //     userInput.setPrefWidth(325.0);
+    //
+    //     sendButton.setPrefWidth(55.0);
+    //
+    //     AnchorPane.setTopAnchor(scrollPane, 1.0);
+    //
+    //     AnchorPane.setBottomAnchor(sendButton, 1.0);
+    //     AnchorPane.setRightAnchor(sendButton, 1.0);
+    //
+    //     AnchorPane.setLeftAnchor(userInput , 1.0);
+    //     AnchorPane.setBottomAnchor(userInput, 1.0);
+    //
+    //     //Part 3. Add functionality to handle user input.
+    //     sendButton.setOnMouseClicked((event) -> handleUserInput());
+    //     userInput.setOnAction((event) -> handleUserInput());
+    //
+    //     //Scroll down to the end every time dialogContainer's height changes.
+    //     dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+    //
+    // }
 
     /**
      * Iteration 1:
@@ -145,26 +145,26 @@ public class Duke extends Application{
         return textToAdd;
     }
 
-    /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-    }
+    // /**
+    //  * Iteration 2:
+    //  * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+    //  * the dialog container. Clears the user input after processing.
+    //  */
+    // private void handleUserInput() {
+    //     Label userText = new Label(userInput.getText());
+    //     Label dukeText = new Label(getResponse(userInput.getText()));
+    //     dialogContainer.getChildren().addAll(
+    //             DialogBox.getUserDialog(userText, new ImageView(user)),
+    //             DialogBox.getDukeDialog(dukeText, new ImageView(duke))
+    //     );
+    //     userInput.clear();
+    // }
 
     /**
      * You should have your own function to generate a response to user input.
      * Replace this stub with your completed method.
      */
-    private String getResponse(String input) {
+    String getResponse(String input) {
         try {
             Command c = Parser.parseCommand(input);
             return c.execute(tasks, ui, storage);
