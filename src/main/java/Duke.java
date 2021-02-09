9be54541914578e81c3ceb14f1239769c1f611d2
Lim Li Quan <linliquan1996@Hotmail.com>
2021-02-09 19:40:35 +0800

    Apply SLAP on Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,10 +2,14 @@ public class Duke {
     private boolean isSetUp = false;
     private Ui ui;
 
+    private void setUp() {
+        isSetUp = true;
+        ui = new Ui();
+    }
+
     public String getResponse(String fullCommand) {
         if (!isSetUp) {
-            isSetUp = true;
-            ui = new Ui();
+            setUp();
             return ui.load(fullCommand);
         }
         return ui.parseAndPrint(fullCommand);
