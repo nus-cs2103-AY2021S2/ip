25ead49eee98cb0e293202adcf08a32a08f92c06
ninijiaying <cooolnichole@gmail.com>
2021-02-09 15:27:27 +0800

    delete Parser.java

--- src/main/java/Parser.java
+++
@@ -1,7 +0,0 @@
-public class Parser {
-    //public String commandName;
-
-    public static Command parse(String fullCommand) throws DukeException {
-        return new Command(fullCommand);
-    }
-}
