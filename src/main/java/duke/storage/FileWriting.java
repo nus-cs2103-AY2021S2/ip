3d507a7e9b576f18890e0c81dd5b934638667727
candyhy <candiceyhy@gmail.com>
2021-02-09 14:40:25 +0800

    Refactor writeToFile method in FileWriting class

--- src/main/java/duke/storage/FileWriting.java
+++ src/main/java/duke/storage/FileWriting.java
@@ -25,33 +25,44 @@ public class FileWriting {
     public static void writeToFile(File file, Duke duke) throws IOException {
         FileWriter fw = new FileWriter(file);
         List<Task> taskList = duke.getList().getLst();
-        StringBuilder textToAdd = new StringBuilder();
+        StringBuilder tasks = writeTasks(taskList);
+        fw.write(tasks.toString());
+        fw.close();
+    }
 
+    public static StringBuilder writeTasks(List<Task> taskList) {
+        StringBuilder tasks = new StringBuilder();
         for (Task curr : taskList) {
             if (curr.getStatus()) {
-                if (curr instanceof Todo) {
-                    textToAdd.append(String.format("T | 1 | %s%n", curr.getDescription()));
-                } else if (curr instanceof Deadline) {
-                    textToAdd.append(String.format("D | 1 | %s | %s%n", curr.getDescription(), (
-                            (Deadline) curr).getDeadline()));
-                } else if (curr instanceof Event) {
-                    textToAdd.append(String.format("E | 1 | %s | %s%n", curr.getDescription(), (
-                            (Event) curr).getEventTime()));
-                }
+                textForCompletedTasks(curr, tasks);
             } else {
-                if (curr instanceof Todo) {
-                    textToAdd.append(String.format("T | 0 | %s%n", curr.getDescription()));
-                } else if (curr instanceof Deadline) {
-                    textToAdd.append(String.format("D | 0 | %s | %s%n", curr.getDescription(), (
-                            (Deadline) curr).getDeadline()));
-                } else if (curr instanceof Event) {
-                    textToAdd.append(String.format("E | 0 | %s | %s%n", curr.getDescription(), (
-                            (Event) curr).getEventTime()));
-                }
+                textForUncompletedTasks(curr, tasks);
             }
         }
+        return tasks;
+    }
 
-        fw.write(textToAdd.toString());
-        fw.close();
+    public static void textForCompletedTasks(Task curr, StringBuilder textToAdd) {
+        if (curr instanceof Todo) {
+            textToAdd.append(String.format("T | 1 | %s%n", curr.getDescription()));
+        } else if (curr instanceof Deadline) {
+            textToAdd.append(String.format("D | 1 | %s | %s%n", curr.getDescription(), (
+                    (Deadline) curr).getDeadline()));
+        } else if (curr instanceof Event) {
+            textToAdd.append(String.format("E | 1 | %s | %s%n", curr.getDescription(), (
+                    (Event) curr).getEventTime()));
+        }
+    }
+
+    public static void textForUncompletedTasks(Task curr, StringBuilder textToAdd) {
+        if (curr instanceof Todo) {
+            textToAdd.append(String.format("T | 0 | %s%n", curr.getDescription()));
+        } else if (curr instanceof Deadline) {
+            textToAdd.append(String.format("D | 0 | %s | %s%n", curr.getDescription(), (
+                    (Deadline) curr).getDeadline()));
+        } else if (curr instanceof Event) {
+            textToAdd.append(String.format("E | 0 | %s | %s%n", curr.getDescription(), (
+                    (Event) curr).getEventTime()));
+        }
     }
 }
