572eec38baf4ed2a26e952a744d4263be1b675b2
nicoleang09 <nicoleang09@gmail.com>
2021-02-09 12:29:51 +0800

    Fix ability for GUI to close on "bye" command

--- src/main/java/duke/ui/Main.java
+++ src/main/java/duke/ui/Main.java
@@ -28,6 +28,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setUI(ui);
+            stage.setTitle("Tabby the Task Bot");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
