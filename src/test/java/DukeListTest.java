1bb8b46d60bfc43a5e85185a93c281fcf25451fb
lrj689 <lrj689@hotmail.com>
2021-02-10 00:02:07 +0800

    Add Gradle Support Update DukeListTest.java

--- src/test/java/DukeListTest.java
+++ src/test/java/DukeListTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeListTest {
+
     @Test
     public void addTest() {
         DukeList list = new DukeList();
@@ -12,9 +13,25 @@ public class DukeListTest {
     @Test
     public void deleteTest() {
         DukeList list = new DukeList();
+        list.add(new ToDos(("test")));
+        list.add(new ToDos(("test2")));
+        list.delete(0);
+        assertEquals(1, list.getSize());
+    }
+    @Test
+    public void deleteAllTest() {
+        DukeList list = new DukeList();
         list.add(new ToDos("a"));
         list.add(new ToDos("b"));
         list.deleteAll();
         assertEquals(0, list.getSize());
     }
+    @Test
+    public void markAsDoneTest() {
+        DukeList list = new DukeList();
+        list.add(new ToDos("test"));
+        list.markAsDone(0);
+        assertEquals(true, list.get(0).isTaskDone());
+    }
+
 }
