c77f51132e2bd4760294f367347fa449e1c399f4
tsh22 <sihuitoh22@gmail.com>
2021-02-09 19:14:13 +0800

    Modify Ui.java to print correct message to user

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -37,12 +37,12 @@ public class Ui {
     public void showTaskList(ArrayList<Task> taskList) {
         if (taskList.size() == 0) {
             System.out.println("Looks like you have no tasks currently. Add some tasks!");
-        }
-
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < taskList.size(); i++) {
-            Task item = taskList.get(i);
-            System.out.println((i + 1) + "." + item.toString());
+        } else {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < taskList.size(); i++) {
+                Task item = taskList.get(i);
+                System.out.println((i + 1) + "." + item.toString());
+            }
         }
     }
 }
