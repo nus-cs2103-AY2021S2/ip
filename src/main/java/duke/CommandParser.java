f698214d55a188db49c29ac268a65ae107168ff1
Guan Yang Ze <guanyangze@gmail.com>
2021-02-09 22:50:51 +0800

    implement mass operations on tasks

--- src/main/java/duke/CommandParser.java
+++ src/main/java/duke/CommandParser.java
@@ -1,12 +1,15 @@
 package duke;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Parses user commands and triggers corresponding effects.
  */
 public class CommandParser {
     private static final int SPLIT_LIMIT = 2;
-    private static final String DONE_REGEX_MATCH = "^(do(ne)?|finish(ed)?|completed?) \\d+$";
-    private static final String DELETE_REGEX_MATCH = "^(delete|remove) \\d+$";
+    private static final String DONE_REGEX_MATCH = "^(do(ne)?|finish(ed)?|completed?)( \\d+)+$";
+    private static final String DELETE_REGEX_MATCH = "^(delete|remove)( \\d+)+$";
     private static final String TASK_REGEX_MATCH = "^(todo|deadline|event)( .+)?$";
     private static final String FIND_REGEX_MATCH = "^(find|search) \\d+$";
 
@@ -26,39 +29,57 @@ public class CommandParser {
      */
     public String parseCommand(String userInput) throws DukeException {
         String reply;
+
         if (userInput.toLowerCase().equals("list")) {
             // display list
-            reply = ui.displayList(tasks);
+            reply = ui.showAllTasks(tasks);
 
         } else if (userInput.toLowerCase().matches(DONE_REGEX_MATCH)) {
             // finish a task
+            TaskList doneTasks = new TaskList();
+
             String[] bits = userInput.split(" ");
 
-            int idx = Integer.parseInt(bits[1]);
-            if (idx < 1 || idx > tasks.size()) {
-                throw new DukeException("Oops! That doesn't appear to be a valid task number.");
+            for (int i = 1; i < bits.length; i++) {
+                int idx = Integer.parseInt(bits[i]);
+                if (idx < 1 || idx > tasks.size()) {
+                    throw new DukeException("Oops! Invalid task number included.");
+                }
+
+                Task finishedTask = tasks.get(idx);
+
+                if (!finishedTask.isDone()) {
+                    finishedTask.markAsDone();
+                    doneTasks.add(finishedTask);
+                }
             }
 
-            Task finishedTask = tasks.get(idx);
-
-            if (finishedTask.isDone()) {
-                throw new DukeException("That task's already done!");
-            }
-
-            finishedTask.markAsDone();
-            reply = ui.showDoneTask(finishedTask);
+            reply = ui.showDoneTasks(doneTasks);
 
         } else if (userInput.toLowerCase().matches(DELETE_REGEX_MATCH)) {
             // manually remove task
+            ArrayList<Integer> tasksToRemove = new ArrayList<>();
+            TaskList removedTasks = new TaskList();
+
             String[] bits = userInput.split(" ");
 
-            int idx = Integer.parseInt(bits[1]);
-            if (idx < 1 || idx > tasks.size()) {
-                throw new DukeException("Oops! That doesn't appear to be a valid task number.");
+            for (int i = 1; i < bits.length; i++) {
+                int idx = Integer.parseInt(bits[i]);
+                if (idx < 1 || idx > tasks.size()) {
+                    throw new DukeException("Oops! Invalid task number included.");
+                }
+                tasksToRemove.add(idx);
             }
 
-            Task removedTask = tasks.remove(idx);
-            reply = ui.showRemovedTask(removedTask, tasks.size());
+            Collections.sort(tasksToRemove, Collections.reverseOrder());
+
+            for (int i = 0; i < tasksToRemove.size(); i++) {
+                int idx = tasksToRemove.get(i);
+                Task removedTask = tasks.remove(idx);
+                removedTasks.add(removedTask);
+            }
+
+            reply = ui.showRemovedTasks(removedTasks, tasks.size());
 
         } else if (userInput.toLowerCase().matches(TASK_REGEX_MATCH)) {
             // add task to list
@@ -74,7 +95,7 @@ public class CommandParser {
             }
 
             TaskList matchingTasks = tasks.find(bits[1]);
-            reply = ui.displayList(matchingTasks);
+            reply = ui.showFoundTasks(matchingTasks);
 
         } else {
             throw new DukeException("I don't understand that command!");
--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -6,6 +6,21 @@ import java.util.Scanner;
  * Handles receiving user input and outputting relevant textual information for the user to see.
  */
 public class Ui {
+
+
+    public String showAllTasks(TaskList tasks) {
+        String msg;
+
+        if (tasks.size() > 0) {
+            msg = String.format("Here's the current state of your to-do list:\n%s",
+                    displayList(tasks));
+        } else {
+            msg = "Your to-do list is empty! Congratulations!";
+        }
+
+        return msg;
+    }
+
     /**
      * Shows details of a Task that's been added to the TaskList.
      * @param task Task that's added to the TaskList.
@@ -21,50 +36,74 @@ public class Ui {
 
     /**
      * Shows details of a Task that's been removed from the TaskList.
-     * @param task Task that's been removed from the TaskList.
+     * @param tasks Tasks that are being removed from the list.
      * @param listSize Size of the TaskList, after the Task is removed.
      * @return Message confirming that the task has been removed.
      */
-    public String showRemovedTask(Task task, int listSize) {
-        String msg = String.format("I've removed this task: %s\nYou now have %d items on your todo list.",
-                task.toString(),
-                listSize);
+    public String showRemovedTasks(TaskList tasks, int listSize) {
+        String msg;
+
+        if (tasks.size() > 0) {
+            msg = String.format("Congrats! The following tasks have been marked as done:\n%s"
+                    + "\nYou now have %d tasks on your to-do list.",
+                    displayList(tasks),
+                    listSize);
+        } else {
+            msg = "I couldn't find any of those tasks to remove!";
+        }
+
         return msg;
     }
 
     /**
      * Shows details of a Task that's been marked as done/completed.
-     * @param task Task that's been marked as done/completed.
+     * @param tasks Tasks that are being marked as done/completed.
      * @return Message confirming that the task has been marked as done/completed.
      */
-    public String showDoneTask(Task task) {
-        assert task.isDone() : " Task not properly marked as done";
-        String msg = String.format("Congrats! The following task has been marked as done:\n  %s",
-                task.toString());
+    public String showDoneTasks(TaskList tasks) {
+        String msg;
+
+        if (tasks.size() > 0) {
+            msg = String.format("Congrats! The following tasks have been marked as done:\n%s",
+                    displayList(tasks));
+        } else {
+            msg = "I couldn't find any of those tasks to mark as done!";
+        }
+
         return msg;
     }
+
+
+    public String showFoundTasks(TaskList tasks) {
+        String msg;
+
+        if (tasks.size() > 0) {
+            msg = String.format("Here's all the matches I found:\n%s",
+                    displayList(tasks));
+        } else {
+            msg = "I couldn't find anything matching that!";
+        }
+
+        return msg;
+    }
+
 
     /**
      * Returns the contents of a TaskList in a neat format.
      * @param tasks TaskList containing the tasks to be displayed.
      * @return String summarizing all the tasks in the TaskList.
      */
-    public String displayList(TaskList tasks) {
+    private String displayList(TaskList tasks) {
         String output;
+
+        assert (tasks.size() > 0) : " Calling displayList() on an empty list";
 
         StringBuilder items = new StringBuilder();
         for (int i = 1; i <= tasks.size(); i++) {
             items.append(String.valueOf(i) + ". " + tasks.get(i).toString() + "\n");
         }
 
-        String formattedTasks = items.toString().trim();
-        if (tasks.size() > 0) {
-            output = "Here's all the matches I found:\n" + formattedTasks;
-        } else {
-            assert (tasks.size() == 0) : " Non-empty list displayed as empty";
-            output = "I couldn't find anything!";
-        }
-
+        output = items.toString().trim();
         return output;
     }
 }
