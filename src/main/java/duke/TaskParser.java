b7c6a09b552f21932114deb476dbb873771274d4
Guan Yang Ze <guanyangze@gmail.com>
2021-02-09 20:07:55 +0800

    reorganize TaskParser code for readability

--- src/main/java/duke/TaskParser.java
+++ src/main/java/duke/TaskParser.java
@@ -8,78 +8,89 @@ import java.time.format.DateTimeFormatter;
  */
 public class TaskParser {
     private static final int SPLIT_LIMIT = 2;
+    private static final int NUM_REQUIRED_TIMED_TASK_PARTS = 2;
 
     /**
      * Parses a String representation of a Task into a Task object, and returns the resultant Task object.
      *
-     * @param taskString String representation of the Task to be parsed.
+     * @param fullTaskString String representation of the Task to be parsed.
      * @return Task object parsed from the input String.
      * @throws DukeException If an Exception occurs as a result of the String being malformed.
      */
-    public static Task parseTask(String taskString) throws DukeException {
+    public static Task parseTask(String fullTaskString) throws DukeException {
         Task newTask;
-        String desc;
+        String[] taskParts;
+        String parsableTaskString;
+        String taskTail;
+        String description;
+        String datetimeString;
+        LocalDate datetime;
         boolean isDone = false;
 
-        if (taskString.startsWith("todo")) {
-            String[] taskParts = taskString.split("todo", SPLIT_LIMIT);
+        if (fullTaskString.startsWith("[isDone]")) {
+            parsableTaskString = fullTaskString.split("\\[isDone]", SPLIT_LIMIT)[1].trim();
+            isDone = true;
+        } else {
+            parsableTaskString = fullTaskString;
+        }
+
+        if (parsableTaskString.startsWith("todo")) {
+            taskParts = parsableTaskString.split("todo", SPLIT_LIMIT);
 
             if (taskParts[1].length() == 0) {
                 throw new DukeException("Oops! Usage: todo [desc]");
-            } else {
-                desc = taskParts[1].trim();
             }
+            taskTail = taskParts[1].trim();
 
-            if (desc.startsWith("[isDone]")) {
-                desc = desc.split("\\[isDone\\]", SPLIT_LIMIT)[1].trim();
-                isDone = true;
-            }
+            description = taskTail;
+            newTask = new Todo(description);
 
-            newTask = new Todo(desc);
-        } else if (taskString.startsWith("event")) {
-            desc = taskString.split("event", SPLIT_LIMIT)[1].trim();
-            String[] taskParts = desc.split(" /on ");
+        } else if (parsableTaskString.startsWith("event")) {
+            taskTail = parsableTaskString.split("event", SPLIT_LIMIT)[1].trim();
+            taskParts = taskTail.split(" /on ");
 
-            if (taskParts.length == 1) {
+            if (taskParts.length != NUM_REQUIRED_TIMED_TASK_PARTS) {
                 throw new DukeException("Oops! Usage: event [desc] /on [date]");
-            } else {
-                if (taskParts[0].startsWith("[isDone]")) {
-                    taskParts[0] = taskParts[0].split("\\[isDone\\]", SPLIT_LIMIT)[1].trim();
-                    isDone = true;
-                }
-
-                try {
-                    LocalDate datetime = LocalDate.parse(taskParts[1], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-                    newTask = new Event(taskParts[0], datetime);
-                } catch (Exception e) {
-                    throw new DukeException("Looks like your date's formatted incorrectly! Try this: dd/mm/yyyy");
-                }
             }
-        } else if (taskString.startsWith("deadline")) {
-            desc = taskString.split("deadline", SPLIT_LIMIT)[1].trim();
-            String[] taskParts = desc.split(" /by ");
 
-            if (taskParts.length == 1) {
-                throw new DukeException("Oops! Usage: deadline [desc] /by [date]");
-            } else {
-                if (taskParts[0].startsWith("[isDone]", SPLIT_LIMIT)) {
-                    taskParts[0] = taskParts[0].split("\\[isDone\\]")[1].trim();
-                    isDone = true;
-                }
+            description = taskParts[0];
+            datetimeString = taskParts[1];
 
-                try {
-                    LocalDate datetime = LocalDate.parse(taskParts[1], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-                    newTask = new Deadline(taskParts[0], datetime);
-                } catch (Exception e) {
-                    throw new DukeException("Looks like your date's formatted incorrectly! Try this: dd/mm/yyyy");
-                }
+            try {
+                datetime = LocalDate.parse(datetimeString, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            } catch (Exception e) {
+                throw new DukeException("Looks like your date's formatted incorrectly! Try this: dd/mm/yyyy");
             }
+
+            newTask = new Event(description, datetime);
+
+        } else if (parsableTaskString.startsWith("deadline")) {
+            taskTail = parsableTaskString.split("deadline", SPLIT_LIMIT)[1].trim();
+            taskParts = taskTail.split(" /by ");
+
+            if (taskParts.length != NUM_REQUIRED_TIMED_TASK_PARTS) {
+                throw new DukeException("Oops! Usage: event [desc] /on [date]");
+            }
+
+            description = taskParts[0];
+            datetimeString = taskParts[1];
+
+            try {
+                datetime = LocalDate.parse(datetimeString, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            } catch (Exception e) {
+                throw new DukeException("Looks like your date's formatted incorrectly! Try this: dd/mm/yyyy");
+            }
+
+            newTask = new Deadline(description, datetime);
+
         } else {
             throw new DukeException("Unknown command!");
         }
+
         if (isDone) {
             newTask.markAsDone();
         }
+
         return newTask;
     }
 }
