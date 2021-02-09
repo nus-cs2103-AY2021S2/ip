ee2df78ee1c46d3ea864bfba9f1d8e7e769b89e6
michael <michaelong1998@gmail.com>
2021-02-09 16:04:09 +0800

    Level-9
    Implemented find

---
+++ src/main/java/duke/Parser.java
@@ -0,0 +1,287 @@
+package duke;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+/**
+ *  The Parser class contains methods to handle user's input commands.
+ */
+public class Parser {
+
+    /**
+     *Parses the rest of the input string after todo and updates database
+     *
+     * @param input Scanner
+     * @param taskList the current taskList
+     * @param ui the current ui
+     * @param database the current database
+     * @throws DukeException if the user input a command with invalid format.
+     */
+
+    public static void parseTodoCommand(Scanner input, TaskList taskList, Ui ui, Database database){
+        try {
+            String s1 = input.nextLine();
+            if (s1.equals("")) {
+                throw new DukeException(" Enter a valid todo task");
+            } else {
+                char[] chars = s1.toCharArray();
+                if(chars[1] == ' '){
+                    throw new DukeException(" Enter valid todo task");
+                } else {
+                    try{
+                        String desc = s1.substring(1);
+                        Todo newTodo = new Todo(desc);
+                        taskList.addTask(newTodo);
+                        ui.showSuccessfulAddedMessage(taskList.getSize(), newTodo);
+                        database.writeTaskToFile(taskList.getList());
+                    } catch (Exception e){
+                        System.out.println(" Enter a valid todo task");
+                    }
+                }
+            }
+        } catch (DukeException e){
+            ui.showErrorMessage(e.getMessage());
+        }
+    }
+
+    /**
+     *Parses the rest of the input string after event and updates database
+     *
+     * @param input Scanner
+     * @param taskList the current taskList
+     * @param ui the current ui
+     * @param database the current database
+     * @throws DukeException if the user input a command with invalid format.
+     */
+
+    public static void parseEventCommand(Scanner input, TaskList taskList, Ui ui, Database database){
+        try{
+            String s1 = input.nextLine();
+            if(s1.equals("")){
+                throw new DukeException(" Enter a valid event task.");
+            } else {
+                try {
+                    String wholeString = s1.substring(1);
+                    String[] parts = wholeString.split(" /at ");
+                    String eventDesc = parts[0];
+                    if(parts.length == 1){
+                        throw new DukeException(" Please adhere to convention:\n(event event_name /at event_details)");
+                    } else {
+                        String eventDetails = parts[1];
+                        Event newEvent = new Event(eventDesc, eventDetails);
+                        taskList.addTask(newEvent);
+                        ui.showSuccessfulAddedMessage(taskList.getSize(), newEvent);
+                        database.writeTaskToFile(taskList.getList());
+                    }
+                } catch(DukeException e) {
+                    ui.showErrorMessage(e.getMessage());
+                }
+            }
+
+        } catch (DukeException e){
+            ui.showErrorMessage(e.getMessage());
+        }
+
+    }
+
+    /**
+     *Parses the rest of the input string after deadline and updates database
+     *
+     * @param input Scanner
+     * @param taskList the current taskList
+     * @param ui the current ui
+     * @param database the current database
+     * @throws DukeException if the user input a command with invalid format.
+     */
+
+    public static void parseDeadlineCommand(Scanner input, TaskList taskList, Ui ui, Database database){
+        try{
+            String s1 = input.nextLine();
+            if(s1.equals("")){
+                throw new DukeException(" Enter valid deadline task.");
+            } else {
+                try {
+                    String wholeString = s1.substring(1);
+                    String[] parts = wholeString.split(" /by ");
+                    String deadlineDesc = parts[0];
+                    if(parts.length == 1){
+                        throw new DukeException(" Please adhere to convention:\n(deadline task_name /by deadline date(YYYY-MM-DD))");
+                    } else {
+                        String dl = parts[1];
+                        try{
+                            LocalDate deadline = LocalDate.parse(dl);
+                            Deadline newDeadline = new Deadline(deadlineDesc, deadline);
+                            taskList.addTask(newDeadline);
+                            ui.showSuccessfulAddedMessage(taskList.getSize(), newDeadline);
+                            database.writeTaskToFile(taskList.getList());
+                        } catch (DateTimeParseException e){
+                            ui.showErrorMessage(" Please enter date as follows\n YYYY-MM-DD");
+                        }
+                    }
+                } catch(DukeException e) {
+                    ui.showErrorMessage(e.getMessage());
+                }
+            }
+        } catch (DukeException e){
+            ui.showErrorMessage(e.getMessage());
+        }
+
+    }
+
+    /**
+     *Parses the rest of the input string after done and updates database
+     *
+     * @param input Scanner
+     * @param taskList the current taskList
+     * @param ui the current ui
+     * @param database the current database
+     * @throws DukeException if the user input a command with invalid format.
+     */
+
+    public static void parseDoneCommand(Scanner input, TaskList taskList, Ui ui, Database database){
+        try{
+            String s1 = input.nextLine();
+            if(s1.equals("")){
+                throw new DukeException(" Please specify what task is done");
+            } else {
+                String parts[] = s1.split(" ");
+                if(parts.length> 2){
+                    throw new DukeException(" Please insert valid index to mark as done");
+                } else {
+                    try {
+                        String indexString = parts[1];
+                        int index =Integer.parseInt(indexString);
+                        Task t = taskList.getList().get(index - 1);
+                        t.markAsDone();
+                        ui.markTaskAsDone(t);
+                        database.writeTaskToFile(taskList.getList());
+                    } catch (Exception e){
+                        ui.showErrorMessage(" Please enter a valid index");
+                    }
+                }
+            }
+
+        } catch(DukeException e){
+            ui.showErrorMessage(e.getMessage());
+        }
+    }
+
+    /**
+     *Parses the rest of the input string after find and finds tasks in
+     *TaskList that corresponds to the input keyword
+     * @param input Scanner
+     * @param taskList the current taskList
+     * @param ui the current ui
+     * @throws DukeException if the user input a command with invalid format.
+     */
+    public static void parseFindCommand(Scanner input, TaskList taskList, Ui ui){
+        try{
+            String s1 = input.nextLine();
+            if(s1.equals("")){
+                throw new DukeException("Please specify a keyword you are trying to find");
+            } else {
+                String parts[] = s1.split(" ");
+                if(parts.length> 2){
+                    throw new DukeException(" Please insert only 1 keyword");
+                } else {
+                    try{
+                        String keyWord = parts[1];
+                        ArrayList<String> matchingWithKeyword = new ArrayList<>();
+                        boolean hasMatch = false;
+                        for(int i =0; i< taskList.getSize(); i++){
+                            if(taskList.getList().get(i).hasKeyWord(keyWord)){
+                                matchingWithKeyword.add(taskList.getList().get(i).toString());
+                                hasMatch = true;
+                            }
+                        }
+                        if(!hasMatch){
+                            throw new DukeException("Sorry. None of your tasks contain this keyword");
+                        } else {
+                            ui.showKeyWordMessage(matchingWithKeyword);
+                        }
+                    } catch (ArrayIndexOutOfBoundsException e){
+                        ui.showErrorMessage("Please specify a keyword you are trying to find");
+                    }
+                }
+            }
+        } catch (DukeException e){
+            ui.showErrorMessage(e.getMessage());
+        }
+    }
+
+    /**
+     *Parses the rest of the input string after delete and updates database
+     *
+     * @param input Scanner
+     * @param taskList the current taskList
+     * @param ui the current ui
+     * @param database the current database
+     * @throws DukeException if the user input a command with invalid format.
+     */
+    public static void parseDeleteCommand(Scanner input, TaskList taskList, Ui ui, Database database){
+        try{
+            String s1 = input.nextLine();
+            if(s1.equals("")){
+                throw new DukeException(" Please specify which task to delete");
+            } else {
+                String parts[] = s1.split(" ");
+                if(parts.length> 2){
+                    throw new DukeException(" Please insert valid index to delete");
+                } else {
+                    try {
+                        String indexString = parts[1];
+                        int index =Integer.parseInt(indexString);
+                        Task t = taskList.getList().get(index-1);
+                        taskList.removeTask(index-1);
+                        ui.showTaskAsDeleted(t);
+                        database.writeTaskToFile(taskList.getList());
+
+                    } catch (Exception e){
+                        ui.showErrorMessage(" Please enter a valid index");
+                    }
+                }
+            }
+
+        } catch(DukeException e){
+            ui.showErrorMessage(e.getMessage());
+        }
+    }
+
+    /**
+     *Parses the rest of the input string after List and calls ui to print tasks in TaskList
+     *
+     * @param taskList the current taskList
+     * @param ui the current ui
+     */
+
+    public static void parseListCommand(TaskList taskList, Ui ui){
+        ui.showListContent(taskList);
+
+    }
+
+    /**
+     *
+     * Calls ui to print bye message
+     * Empties buffer is System
+     * @param ui the current ui
+     */
+    public static void parseByeCommand(Ui ui){
+        ui.showByeMessage();
+        System.exit(0);
+    }
+
+    /**
+     *
+     * Calls ui to print default message
+     * @param input the scanner
+     * @param ui the current ui
+     */
+    public static void parseDefault(Scanner input, Ui ui){
+        input.nextLine();
+        ui.showDefaultStatement();
+    }
+
+}
---
+++ src/main/java/duke/Ui.java
@@ -0,0 +1,128 @@
+package duke;
+
+
+import java.util.ArrayList;
+
+/**
+ *  The Ui class contains methods to handle text represention to users via the CLI.
+ */
+public class Ui {
+
+    public static final String BLANK_SPACE_BABY = "";
+    public static final String HORIZONTAL_LINE = "________________________________________________";
+    public static final String FILE_PATH = "data.txt";
+    private static final String greeting =" Hello! I'm duke.\n What can I do for you?\n";
+    private static String farewell = " Bye. Hope to see you again soon!";
+
+
+    /**
+     * Initialize and prints greeting message when a user runs programme.
+     */
+
+    public static void initGreeting() {
+        System.out.println(HORIZONTAL_LINE + "\n" + greeting + HORIZONTAL_LINE + "\n");
+    }
+
+    /**
+     * Prints error message when a user encounters exceptions.
+     */
+
+    public static void showErrorMessage(String error){
+        System.out.println(HORIZONTAL_LINE + "\n"  + error + "\n" +HORIZONTAL_LINE);
+    }
+
+
+    /**
+     * Prints success message when a user successfully adds a task.
+     */
+
+    public static void showSuccessfulAddedMessage(int numTask, Task task){
+        System.out.println(HORIZONTAL_LINE + "\n" + " Got it! I've added this new Task!");
+        System.out.println("  " + task.toString());
+        System.out.println(" Now you have " + numTask + " tasks in your TaskList.");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+
+    /**
+     * Prints TaskList as a string
+     */
+
+    public static void showListContent(TaskList taskList){
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(" Here are your remaining tasks!");
+        System.out.println(BLANK_SPACE_BABY);
+        if(taskList.getSize() ==0){
+            System.out.println(" The list is empty!\n Please add tasks into the list :))");
+        }
+        System.out.println(taskList.toString());
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    /**
+     * Prints message when a user successful marks a task as done.
+     */
+
+    public static void markTaskAsDone(Task task){
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(" Nice! I've marked this task as done");
+        System.out.println(" " + task.toString());
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    /**
+     * Prints message when a user successful deletes a task.
+     */
+    public static void showTaskAsDeleted(Task task){
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(" Noted! I have removed this task from the list.");
+        System.out.println(task.toString());
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    /**
+     * Prints message when a user exits programme.
+     */
+
+    public static void showByeMessage(){
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(farewell);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+
+    /**
+     *
+     * @param matchingTaks
+     */
+    public static void showKeyWordMessage(ArrayList<String> matchingTaks){
+
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Here are the relevant tasks:");
+        System.out.println(BLANK_SPACE_BABY);
+        String output = "";
+        for(int i=0; i< matchingTaks.size(); i++){
+            if(i == matchingTaks.size()-1){
+                output += matchingTaks.get(i);
+            } else {
+                output += matchingTaks.get(i) + "\n";
+            }
+        }
+        System.out.println(output);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    /**
+     * Prints default message when a user inputs invalid arguments.
+     * Prints list of valid arguments
+     */
+
+    public static void showDefaultStatement(){
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(" Please enter a valid command!");
+        System.out.println(" Valid commands include:");
+        System.out.println(" todo\n event\n deadline\n list\n done\n delete\n find\n bye");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+}
