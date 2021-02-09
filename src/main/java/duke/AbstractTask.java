32e5348fa1de5c9cd20511aa7f5ba4ad64c8c804
Ho Jie Feng <hjf3108@gmail.com>
2021-02-09 23:37:47 +0800

    Implement task tagging feature

--- src/main/java/duke/AbstractTask.java
+++ src/main/java/duke/AbstractTask.java
@@ -1,11 +1,14 @@
 package duke;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class AbstractTask implements Serializable {
     protected final String description;
+    protected final List<String> tags;
     protected boolean isDone;
-
     /**
      * Constructs a task using the description
      *
@@ -16,21 +19,41 @@ public abstract class AbstractTask implements Serializable {
             throw new DukeEmptyDescriptionException();
         }
         this.description = description;
+        this.tags = new ArrayList<>();
         this.isDone = false;
     }
 
-    /*
+    /**
      * Mark the task as done
      */
     public void markDone() {
         this.isDone = true;
     }
 
-    /*
+    /**
+     * Add a tag to a task
+     *
+     * @param tag tag name
+     */
+    public void addTag(String tag) {
+        tags.add(tag);
+    }
+
+    /**
+     * Delete a tag from a task
+     *
+     * @param tag tag name
+     */
+    public void deleteTag(String tag) {
+        tags.remove(tag);
+    }
+
+    /**
      * Return a string representation of the task
      */
     @Override
     public String toString() {
-        return String.format("[%s] %s", isDone ? "X" : " ", description);
+        String tagString = tags.stream().map(x -> "#" + x).collect(Collectors.joining());
+        return String.format("[%s] %s %s", isDone ? "X" : " ", description, tagString);
     }
 }
