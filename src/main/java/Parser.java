a0643fc94ce623ddc71bf773f11fefd3807eee1e
unknown <varcolac01@gmail.com>
2021-02-10 18:30:09 +0800

    Clean up code

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -28,94 +28,118 @@ public class Parser {
         if (input.equals("list")) {
             return list.listAll();
         } else if (input.contains("done")) {
-            int number;
-            try {
-                number = (Integer.parseInt(input.substring(5))) - 1;
-                return list.markDone(number);
-            } catch (StringIndexOutOfBoundsException e) {
-                return ui.printErr("No task was input to be marked as done.");
-            } catch (IndexOutOfBoundsException e) {
-                return ui.printErr("No task has been stored at that index yet.");
-            } catch (NumberFormatException e) {
-                return ui.printErr("Input index was not recognised.");
-            }
+            return markDone(input);
         } else if (input.contains("delete")) {
-            int number;
-            try {
-                number = (Integer.parseInt(input.substring(7))) - 1;
-                return list.delete(number);
-            } catch (StringIndexOutOfBoundsException e) {
-                return ui.printErr("No task was input to be deleted.");
-            } catch (IndexOutOfBoundsException e) {
-                return ui.printErr("No task has been stored at that index yet.");
-            } catch (NumberFormatException e) {
-                return ui.printErr("Input index was not recognised.");
-            }
+            return delete(input);
         } else if (input.contains("todo")) {
-            String task = input.substring(4);
-            if (task.length() == 0 || task.equals(" ") || task.contains("  ")) {
-                return ui.printErr("The description of a todo cannot be empty.");
-            } else {
-                return list.add(new Task(task, 0));
-            }
+            return createTodo(input);
         } else if (input.contains("deadline")) {
-            String task = input.substring(8);
-            if (task.length() == 0 || task.equals(" ") || task.contains("  ")) {
-                return ui.printErr("The description of a deadline cannot be empty.");
-            } else {
-                try {
-                    return list.add(new Task(task, 1));
-                } catch (IllegalArgumentException e) {
-                    return ui.printErr("The description or date of a deadline cannot be empty.");
-                } catch (DateTimeParseException e) {
-                    return ui.printErr("The input date was not recognised.");
-                }
-            }
+            return createDeadline(input);
         } else if (input.contains("event")) {
-            String task = input.substring(5);
-            if (task.length() == 0 || task.equals(" ") || task.contains("  ")) {
-                return ui.printErr("The description of an event cannot be empty.");
-            } else {
-                try {
-                    return list.add(new Task(task, 2));
-                } catch (IllegalArgumentException e) {
-                    return ui.printErr("The description or date of an event cannot be empty.");
-                } catch (DateTimeParseException e) {
-                    return ui.printErr("The input time was not recognised.");
-                }
-            }
+            return createEvent(input);
         } else if (input.contains("find")) {
-            String search;
-            try {
-                search = input.substring(5);
-            } catch (StringIndexOutOfBoundsException e) {
-                return ui.printErr("The search query cannot be empty.");
-            }
-            if (search.length() == 0) {
-                return ui.printErr("The search query cannot be empty.");
-            } else {
-                int noOfHits = 0;
-                ArrayDeque<Task> found = new ArrayDeque<>();
-                for (int i = 0; i < list.size(); i++) {
-                    Task currTask = list.get(i);
-                    if (currTask.toString().contains(search)) {
-                        found.add(currTask);
-                        noOfHits++;
-                    }
-                }
-                if (noOfHits == 0) {
-                    return ui.print("Looks like no tasks were found :-(");
-                } else {
-                    String[] output = new String[noOfHits + 1];
-                    output[0] = "Here are the matching tasks in your list:";
-                    for (int i = 1; i <= noOfHits; i++) {
-                        output[i] = i + "." + found.poll().toString();
-                    }
-                    return ui.print(output);
-                }
-            }
+            return find(input);
         } else {
             return ui.print("I'm sorry, but I don't know what that means :-(");
+        }
+    }
+
+    private Label markDone(String input) {
+        int number;
+        try {
+            number = (Integer.parseInt(input.substring(5))) - 1; // Index of input number starts at 5.
+            return list.markDone(number);
+        } catch (StringIndexOutOfBoundsException e) {
+            return ui.printErr("No task was input to be marked as done.");
+        } catch (IndexOutOfBoundsException e) {
+            return ui.printErr("No task has been stored at that index yet.");
+        } catch (NumberFormatException e) {
+            return ui.printErr("Input index was not recognised.");
+        }
+    }
+
+    private Label delete(String input) {
+        int number;
+        try {
+            number = (Integer.parseInt(input.substring(7))) - 1;
+            return list.delete(number);
+        } catch (StringIndexOutOfBoundsException e) {
+            return ui.printErr("No task was input to be deleted.");
+        } catch (IndexOutOfBoundsException e) {
+            return ui.printErr("No task has been stored at that index yet.");
+        } catch (NumberFormatException e) {
+            return ui.printErr("Input index was not recognised.");
+        }
+    }
+
+    private Label createTodo(String input) {
+        String task = input.substring(4); // "todo" is 4 letters long.
+        if (task.length() == 0 || task.equals(" ") || task.contains("  ")) {
+            return ui.printErr("The description of a todo cannot be empty.");
+        } else {
+            return list.add(new Task(task, 0));
+        }
+    }
+
+    private Label createDeadline(String input) {
+        String task = input.substring(8); // "deadline" is 8 letters long.
+        if (task.length() == 0 || task.equals(" ") || task.contains("  ")) {
+            return ui.printErr("The description of a deadline cannot be empty.");
+        } else {
+            try {
+                return list.add(new Task(task, 1));
+            } catch (IllegalArgumentException e) {
+                return ui.printErr("The description or date of a deadline cannot be empty.");
+            } catch (DateTimeParseException e) {
+                return ui.printErr("The input date was not recognised.");
+            }
+        }
+    }
+
+    private Label createEvent(String input) {
+        String task = input.substring(5); // "event" is 5 letters long.
+        if (task.length() == 0 || task.equals(" ") || task.contains("  ")) {
+            return ui.printErr("The description of an event cannot be empty.");
+        } else {
+            try {
+                return list.add(new Task(task, 2));
+            } catch (IllegalArgumentException e) {
+                return ui.printErr("The description or date of an event cannot be empty.");
+            } catch (DateTimeParseException e) {
+                return ui.printErr("The input time was not recognised.");
+            }
+        }
+    }
+
+    private Label find(String input) {
+        String search;
+        try {
+            search = input.substring(5); // Beginning of search query is index 5.
+        } catch (StringIndexOutOfBoundsException e) {
+            return ui.printErr("The search query cannot be empty.");
+        }
+        if (search.length() == 0) {
+            return ui.printErr("The search query cannot be empty.");
+        } else {
+            int noOfHits = 0;
+            ArrayDeque<Task> found = new ArrayDeque<>();
+            for (int i = 0; i < list.size(); i++) {
+                Task currTask = list.get(i);
+                if (currTask.toString().contains(search)) {
+                    found.add(currTask);
+                    noOfHits++;
+                }
+            }
+            if (noOfHits == 0) {
+                return ui.print("Looks like no tasks were found :-(");
+            } else {
+                String[] output = new String[noOfHits + 1];
+                output[0] = "Here are the matching tasks in your list:";
+                for (int i = 1; i <= noOfHits; i++) {
+                    output[i] = i + "." + found.poll().toString();
+                }
+                return ui.print(output);
+            }
         }
     }
 }
