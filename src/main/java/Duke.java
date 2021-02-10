bb3d2ffcc0c93b9a2f0f34250d1cf2b30b62c225
hengyiqun <hengyiqun@gmail.com>
2021-02-11 02:14:38 +0800

    Add a GUI to Duke. Use the JavaFX technology to implement the GUI.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,17 @@
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.layout.Region;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 
 /**
  * Duke allows the user to maintain a list of tasks
@@ -23,7 +34,7 @@ public class Duke {
         }
     }
 
-    public void run() {
+    /*public void run() {
         while (!this.ui.shouldExit() && this.ui.hasNext()) {
             this.tasks = ui.process(this.tasks);
             try {
@@ -55,235 +66,21 @@ public class Duke {
             } catch (IOException e) {
             }
         }
-    }
-
-    public static void main(String[] args) {
-        new Duke("data/duke.txt").run();
-    }
-    /*public static void main(String[] args) {
-        String logo = " ____         _        \n"
-                    + "|  _ \\ _   _| | _____ \n"
-                    + "| | | | | | | |/ / _ \\\n"
-                    + "| |_| | |_| |   <  __/\n"
-                    + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("    ____________________________________________________________");
-        System.out.println("     Hello! I'm Duke");
-        System.out.println("     What can I do for you?");
-        System.out.println("    ____________________________________________________________");
-        List<Task> store = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String filePath = "data/duke.txt";
-        try {
-            File file = new File(filePath);
-            Scanner fileScanner = new Scanner(file);
-            while (fileScanner.hasNext()) {
-                String curRecTask = fileScanner.nextLine();
-                String[] items = curRecTask.split(" \\| ");
-                boolean done = (items[1].equals("1") ? true : false);
-                Task temp = new Todo(items[2]);
-                if (items[0].equals("E")) {
-                    LocalDate date = LocalDate.parse(items[3], formatter);
-                    temp = new Event(items[2], date);
-                } else if (items[0].equals("D")) {
-                    LocalDate date = LocalDate.parse(items[3], formatter);
-                    temp = new Deadline(items[2], date);
-                }
-                if (done) {
-                    temp = temp.markAsDone();
-                }
-                store.add(temp);
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("     There is no existing task list. Please create one.");
-        }
-
-        while (sc.hasNextLine()) {
-            try {
-                String input = sc.nextLine();
-                String[] parts = input.split(" ");
-                System.out.println("    ____________________________________________________________");
-                if (parts[0].equals("bye")) {
-                    System.out.println("     Bye. Hope to see you again soon!");
-                    System.out.println("    ____________________________________________________________");
-                    break;
-                } else if (parts[0].equals("list")) {
-                    System.out.println("     Here are the tasks in your list:");
-                    int count = 1;
-                    for (Task t : store) {
-                        System.out.println("     " + count + "." + t);
-                        count++;
-                    }
-                } else if (parts[0].equals("done")) {
-                    if (parts.length == 1) {
-                        throw new InsufficientArgumentsException("     ☹ OOPS!!! The " +
-                                "description of done cannot be empty.");
-                    }
-                    int taskDone = Integer.parseInt(parts[1]);
-                    int count = 1;
-                    for (Task t : store) {
-                        if (count == taskDone) {
-                            System.out.println("     Nice! I've marked this task as done:");
-                            store.set(count - 1, store.get(count - 1).markAsDone());
-                            System.out.println("     " + store.get(count - 1));
-                        }
-                        count++;
-                    }
-                } else if (parts[0].equals("delete")) {
-                    if (parts.length == 1) {
-                        throw new InsufficientArgumentsException("     ☹ OOPS!!! The " +
-                                "description of delete cannot be empty.");
-                    }
-                    int taskToDelete = Integer.parseInt(parts[1]);
-                    System.out.println("     Noted. I've removed this task: ");
-                    System.out.println("     " + store.get(taskToDelete - 1));
-                    store.remove(taskToDelete - 1);
-                    System.out.println("     Now you have " + store.size() + " tasks in the list.");
-                } else if (parts[0].equals("todo")) {
-                    if (parts.length == 1) {
-                        throw new InsufficientArgumentsException("     ☹ OOPS!!! The " +
-                                "description of a todo cannot be empty.");
-                    }
-                    System.out.println("     Got it. I've added this task:");
-                    String taskDescription = "";
-                    boolean first = true;
-                    for (int i = 1; i < parts.length; ++i) {
-                        if (!first) {
-                            taskDescription += " " + parts[i];
-                        } else {
-                            taskDescription += parts[i];
-                        }
-                        first = false;
-                    }
-                    Todo todo = new Todo(taskDescription);
-                    store.add(todo);
-                    System.out.println("     " + todo);
-                    System.out.println("     Now you have " + store.size() + " tasks in the list.");
-                } else if (parts[0].equals("deadline")) {
-                    if (parts.length == 1) {
-                        throw new InsufficientArgumentsException("     ☹ OOPS!!! The " +
-                                "description of a deadline cannot be empty.");
-                    }
-                    System.out.println("     Got it. I've added this task:");
-                    boolean flag = false, first = true;
-                    String dueDate = "";
-                    for (String s : parts) {
-                        if (flag) {
-                            if (!first) {
-                                dueDate += ' ' + s;
-                            } else {
-                                dueDate += s;
-                            }
-                            first = false;
-                        }
-                        if (s.equals("/by")) {
-                            flag = true;
-                        }
-                    }
-                    LocalDate due = LocalDate.parse(dueDate);
-                    String taskDescription = "";
-                    first = true;
-                    for (int i = 1; i < parts.length; ++i) {
-                        if (!parts[i].equals("/by")) {
-                            if (!first) {
-                                taskDescription += " " + parts[i];
-                            } else {
-                                taskDescription += parts[i];
-                            }
-                            first = false;
-                        } else {
-                            break;
-                        }
-                    }
-                    Deadline deadline = new Deadline(taskDescription, due);
-                    store.add(deadline);
-                    System.out.println("     " + deadline);
-                    System.out.println("     Now you have " + store.size() + " tasks in the list.");
-                } else if (parts[0].equals("event")) {
-                    if (parts.length == 1) {
-                        throw new InsufficientArgumentsException("     ☹ OOPS!!! The " +
-                                "description of an event cannot be empty.");
-                    }
-                    System.out.println("     Got it. I've added this task:");
-                    boolean flag = false, first = true;
-                    String eventDate = "";
-                    for (String s : parts) {
-                        if (flag) {
-                            if (!first) {
-                                eventDate += ' ' + s;
-                            } else {
-                                eventDate += s;
-                            }
-                            first = false;
-                        }
-                        if (s.equals("/at")) {
-                            flag = true;
-                        }
-                    }
-                    LocalDate date = LocalDate.parse(eventDate);
-                    String eventDescription = "";
-                    first = true;
-                    for (int i = 1; i < parts.length; ++i) {
-                        if (!parts[i].equals("/at")) {
-                            if (!first) {
-                                eventDescription += " " + parts[i];
-                            } else {
-                                eventDescription += parts[i];
-                            }
-                            first = false;
-                        } else {
-                            break;
-                        }
-                    }
-                    Event event = new Event(eventDescription, date);
-                    store.add(event);
-                    System.out.println("     " + event);
-                    System.out.println("     Now you have " + store.size() + " tasks in the list.");
-                } else {
-                    throw new WrongArgumentException("     ☹ OOPS!!! I'm sorry, " +
-                            "but I don't know what that means :-(");
-                }
-                System.out.println("    ____________________________________________________________");
-                if (parts[0].equals("event") || parts[0].equals("deadline")
-                    || parts[0].equals("todo") || parts[0].equals("done")
-                        || parts[0].equals("delete")) {
-                    try {
-                        FileWriter fw = new FileWriter(filePath);
-                        for (Task t : store) {
-                            String rmb = "";
-                            if (t instanceof Event) {
-                                rmb += "E";
-                            } else if (t instanceof Deadline) {
-                                rmb += "D";
-                            } else if (t instanceof Todo) {
-                                rmb += "T";
-                            }
-                            rmb += " | ";
-                            if (t.isCompleted()) {
-                                rmb += "1";
-                            } else {
-                                rmb += "0";
-                            }
-                            rmb += " | ";
-                            rmb += t.getDescription();
-                            if (t instanceof DueDate) {
-                                rmb += " | ";
-                                rmb += ((DueDate) t).getDueDate();
-                            }
-                            fw.write(rmb + "\n");
-                        }
-                        fw.close();
-                    } catch (IOException e) {
-                    }
-                }
-            } catch(WrongArgumentException e) {
-                System.out.println(e.getMessage());
-                System.out.println("    ____________________________________________________________");
-            } catch (InsufficientArgumentsException e) {
-                System.out.println(e.getMessage());
-                System.out.println("    ____________________________________________________________");
-            }
-        }
     }*/
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    protected String getResponse(String input) {
+        Parser parser = new Parser();
+        parser = parser.parse(input);
+        String message = this.ui.getOutput(this.tasks, parser);
+        this.tasks = this.ui.process(this.tasks, parser);
+        return "Duke heard: " + message;
+    }
+
+    protected String getGreeting() {
+        return this.ui.greet();
+    }
 }
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -14,24 +14,24 @@ public class Ui {
         this.exit = false;
     }
 
-    public void greet() {
+    public String greet() {
         String logo =  " ____         _        \n"
-                    + "|  _ \\ _   _| | _____ \n"
+                    + "|  _ \\ _   _| |  _____ \n"
                     + "| | | | | | | |/ / _ \\\n"
                     + "| |_| | |_| |   <  __/\n"
                     + "|____/ \\__,_|_|\\_\\___|\n";
         String greeting = "Hello from\n" + logo;
-        greeting += "    ____________________________________________________________\n";
+        greeting += "    _____________________________________\n";
         greeting += "     Hello! I'm Duke\n";
         greeting += "     What can I do for you?\n";
-        greeting += "    ____________________________________________________________\n";
-        System.out.println(greeting);
+        greeting += "    ______________________________________\n";
+        return greeting;
     }
 
-    public TaskList process(TaskList tasks) {
-        String input = this.next();
+    public TaskList process(TaskList tasks, Parser parser) {
+        /*String input = this.next();
         Parser parser = new Parser();
-        parser = parser.parse(input);
+        parser = parser.parse(input);*/
         if (parser.getTaskType().equals("bye")) {
             String msg = this.format("     Bye. Hope to see you again soon!");
             System.out.println(msg);
@@ -124,6 +124,82 @@ public class Ui {
             System.out.println(this.format(parser.getDescription()));
         }
         return tasks;
+    }
+
+    public String getOutput(TaskList tasks, Parser parser) {
+        String msg = "";
+        if (parser.getTaskType().equals("bye")) {
+            msg += this.format("Bye. Hope to see you again soon!");
+        } else if (parser.getTaskType().equals("list")) {
+            msg += "Here are the tasks in your list:\n";
+            ArrayList<Task> taskList = tasks.getList();
+            int count = 1;
+            for (Task t : taskList) {
+                if (count == taskList.size()) {
+                    msg += "     " + count + "." + t.toString();
+                } else {
+                    msg += "     " + count + "." + t.toString() + "\n";
+                }
+                count++;
+            }
+        } else if (parser.getTaskType().equals("done")) {
+            ArrayList<Task> taskList = tasks.getList();
+            msg += "Nice! I've marked this task as done:\n";
+            int taskDone = parser.getTaskIdx();
+            for (int i = 0; i < taskList.size(); ++i) {
+                if (i == taskDone - 1) {
+                    taskList.set(i, taskList.get(i).markAsDone());
+                    msg += taskList.get(i);
+                }
+            }
+        } else if (parser.getTaskType().equals("delete")) {
+            int taskToDelete = parser.getTaskIdx();
+            msg += "Noted. I've removed this task: \n";
+            msg += tasks.get(taskToDelete - 1) + "\n";
+            tasks.remove(taskToDelete - 1);
+            msg += " Now you have " + (tasks.size() - 1 )+ " tasks in the list.";
+        } else if (parser.getTaskType().equals("find")) {
+            String toFind = parser.getDescription();
+            ArrayList<Task> taskList = tasks.getList();
+            msg += "Here are the matching tasks in your list:\n";
+            boolean first = true;
+            for (int i = 0; i < taskList.size(); ++i) {
+                if (taskList.get(i).getDescription().contains(toFind)) {
+                    if (first) {
+                        msg += (i + 1) + "." + taskList.get(i).toString();
+                        first = false;
+                    } else {
+                        msg += "\n" + (i + 1) + "." + taskList.get(i).toString();
+                    }
+                }
+            }
+            if (first) {
+                msg = "There are no matching tasks";
+            }
+        } else if (parser.getTaskType().equals("todo")) {
+            msg += "Got it. I've added this task:\n";
+            String taskDescription = parser.getDescription();
+            Todo todo = new Todo(taskDescription);
+            msg += todo + "\n";
+            msg += "Now you have " + tasks.size() + " tasks in the list.";
+        } else if (parser.getTaskType().equals("deadline")) {
+            msg += "Got it. I've added this task:\n";
+            LocalDate dueDate = parser.getDueDate();
+            String taskDescription = parser.getDescription();
+            Deadline deadline = new Deadline(taskDescription, dueDate);
+            msg += deadline + "\n";
+            msg += "Now you have " + tasks.size() + " tasks in the list.\n";
+        } else if (parser.getTaskType().equals("event")) {
+            msg += "Got it. I've added this task:\n";
+            LocalDate date = parser.getDueDate();
+            String eventDescription = parser.getDescription();
+            Event event = new Event(eventDescription, date);
+            msg += event + "\n";
+            msg += "Now you have " + tasks.size() + " tasks in the list.";
+        } else {
+            msg += this.format(parser.getDescription());
+        }
+        return msg;
     }
 
     public boolean hasNext() {
