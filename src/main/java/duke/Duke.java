e0b8e570062e8f22f46d71c32f8d820c50922a0a
YuHeem <54734556+yuheem@users.noreply.github.com>
2021-02-11 13:18:18 +0800

    refactor: remove dead code

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -27,7 +27,6 @@ public class Duke {
         try {
             tasks = new TaskList(storage.loadTasksFromFile());
         } catch (DukeException e) {
-            ui.showLoadingError();
             tasks = new TaskList();
         }
 
@@ -35,27 +34,6 @@ public class Duke {
 
     public Duke() {
         this("data/tasks.txt");
-    }
-
-    /**
-     * Starts running the Duke program.
-     */
-    public void run() {
-        ui.showWelcome();
-        boolean isExit = false;
-        while (!isExit) {
-            try {
-                String fullCommand = ui.readCommand();
-                ui.showLine();
-                Command command = Parser.parseCommand(fullCommand);
-                command.execute(tasks, ui, storage);
-                isExit = command.isExit();
-            } catch (DukeException e) {
-                ui.showError(e.getMessage());
-            } finally {
-                ui.showLine();
-            }
-        }
     }
 
     public String getWelcomeMessage() {
--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -1,87 +1,19 @@
 package duke;
 
-import java.util.Scanner;
-
 /**
  * The Ui class handles command-line based input from a user and
  * output to be displayed to the user of a running instance of Duke.
  */
 public class Ui {
-    private Scanner scanner;
-    private static final String DIVIDER_LINE = "____________________________________________________________";
-    private static final String LOGO = "                            _       \n" +
-            "                           (_)      \n" +
-            " _ __ ___   __ _ _ ____   ___ _ __  \n" +
-            "| '_ ` _ \\ / _` | '__\\ \\ / / | '_ \\ \n" +
-            "| | | | | | (_| | |   \\ V /| | | | |\n" +
-            "|_| |_| |_|\\__,_|_|    \\_/ |_|_| |_|";
     private static final String WELCOME_MESSAGE = "Hello! I'm Marvin what can I do for you today?";
-    private static final String LOADING_ERROR_MESSAGE = "Sorry was unable to load tasks from storage!";
 
     /**
      * Constructs an instance of a Ui object.
      */
     public Ui() {
-        scanner = new Scanner(System.in);
-    }
-
-    /**
-     * Prints a divider line to be shown to the user.
-     */
-    public void showLine() {
-        System.out.println(DIVIDER_LINE);
-    }
-
-    /**
-     * Prints a welcome message to be shown to the user.
-     */
-    public void showWelcome() {
-        showLine();
-        System.out.println(LOGO);
-        System.out.println(WELCOME_MESSAGE);
-        showLine();
     }
 
     public String getWelcomeMessage() {
         return WELCOME_MESSAGE;
-    }
-
-    /**
-     * Reads a command from the user.
-     */
-    public String readCommand() {
-        System.out.print(">>> ");
-        return scanner.nextLine();
-    }
-
-    /**
-     * Prints an error message when tasks failed to load from file to be shown to the user.
-     */
-    public void showLoadingError() {
-        System.out.println(LOADING_ERROR_MESSAGE);
-    }
-
-    /**
-     * Prints the corresponding error message to be shown to the user.
-     * @param message The corresponding error message.
-     */
-    public void showError(String message) {
-        System.out.println(message);
-    }
-
-    /**
-     * Prints the corresponding message to be shown to the user without a newline.
-     * @param message The corresponding message.
-     */
-    public void show(String message) {
-        System.out.print(message);
-    }
-
-    /**
-     * Prints the corresponding message to be shown to the user with a newline.
-     * @param message The corresponding message.
-     */
-    public void showNewLine(String message) {
-        System.out.println(message);
     }
 }
