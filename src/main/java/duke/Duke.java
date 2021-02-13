7ae2fa8ca7f798610a96c07f1d4275b9cd010e90
rachel <rachel.ljt@gmail.com>
2021-02-13 22:18:05 +0800

    Improve the GUI with new fonts and background colour

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -47,6 +47,6 @@ public class Duke {
      * @return a String containing the welcome message.
      */
     public String welcomeMessage() {
-        return "Hello from Duke!\n" + "\nWhat can I do for you?";
+        return "Hello from Piper!\n" + "\nWhat can I do for you?";
     }
 }
