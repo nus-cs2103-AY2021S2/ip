01fffc3e0f6aec3ef9f7797b26092cc690da2af8
nighoggDatatype <hosherman1965@gmail.com>
2021-02-13 17:52:18 +0800

    Add Material from Tutorial to create MVP for UI

--- src/main/java/duke/ui/MainWindow.java
+++ src/main/java/duke/ui/MainWindow.java
@@ -1,13 +1,16 @@
 package duke.ui;
 
 import duke.Duke;
+import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
@@ -24,6 +27,9 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
@@ -31,7 +37,12 @@ public class MainWindow extends AnchorPane {
 
     public void setDuke(Duke d) {
         duke = d;
-        System.out.println(duke.startUpProcedure());
+        String message = duke.startUpProcedure();
+
+        dialogContainer.getChildren().addAll(
+                DialogBox.getDukeDialog(message, dukeImage)
+        );
+        System.out.println(message);
     }
 
     /**
@@ -41,12 +52,19 @@ public class MainWindow extends AnchorPane {
     @FXML
     private void handleUserInput() {
         String input = userInput.getText();
-        System.out.println(input);
         String output = duke.processInput(input);
+        System.out.println(input);
         System.out.println(output);
+
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(output, dukeImage)
+        );
         userInput.clear();
         if (duke.getIsTerminated()) {
-            Platform.exit();
+            PauseTransition delay = new PauseTransition(Duration.seconds(3));
+            delay.setOnFinished( event ->Platform.exit() );
+            delay.play();
         }
     }
 }
