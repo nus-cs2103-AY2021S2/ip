a05435d6b4cb2bf26a30123ffbbbdb51911df634
BigDoot <glendonchua@gmail.com>
2021-02-14 00:49:43 +0800

    Add ability to save additional info to disk and for 'list' to display an indicator [i] if a task has additional info

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -22,7 +22,7 @@ public class Task {
      * @return task status.
      */
     public String getStatusIcon() {
-        return isDone ? "[X] " : "[ ] ";
+        return isDone ? "[X]" : "[ ]";
     }
 
     /**
@@ -47,6 +47,10 @@ public class Task {
      */
     @Override
     public String toString() {
-        return this.getStatusIcon() + this.description;
+        if (this.moreInfo != null) {
+            return this.getStatusIcon() + "[i] " + this.description;
+        } else {
+            return this.getStatusIcon() + "[ ] " + this.description;
+        }
     }
 }
