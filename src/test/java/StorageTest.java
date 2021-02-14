1840dd925324b2783d4d70bb03f4a9aa96fac827
Jared Lim <jaredlim1998@hotmail.com>
2021-02-14 15:44:45 +0800

    Add JUnit Tests Included the gradle wrapper and the associated dependencies for the Junit test. Added two JUnit Test: One to test the loading of the stored text file, the other to test marking as done utility. Both under src/test/java.

---
+++ src/test/java/StorageTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class DukeTest {
+
+    @Test
+    public void testLoadingData() throws IOException {
+        String[] placeholder = new String[];
+        Duke.main(placeholder);
+
+    }
+
+}
