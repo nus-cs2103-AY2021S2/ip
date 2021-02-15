20e8756364be345beee4cb7e97cee6c098ee82b0
Sharptail <kongjianwei@outlook.com>
2021-02-16 03:43:47 +0800

    Fix gradle test

--- src/main/java/snom/ui/DialogBox.java
+++ src/main/java/snom/ui/DialogBox.java
@@ -13,7 +13,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import snom.common.core.Style;
 
 /**
@@ -36,7 +35,7 @@ public class DialogBox extends HBox {
         }
 
         displayPicture.setImage(img);
-        if(isError){
+        if (isError) {
             dialog.setStyle(Style.STYLE_DIALOG_ERROR);
         }
         dialog.setText(text);
