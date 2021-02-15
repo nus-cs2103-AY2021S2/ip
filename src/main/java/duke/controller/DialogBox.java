e4e86e7bbf05b7d1d323ed991b266d597ea68238
JonahhGohh <jonahgoh@outlook.com>
2021-02-15 18:14:49 +0800

    Improve UI of Duke

--- src/main/java/duke/controller/DialogBox.java
+++ src/main/java/duke/controller/DialogBox.java
@@ -6,12 +6,18 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 /**
  * An example of a custom control using FXML.
@@ -24,6 +30,11 @@ public class DialogBox extends HBox {
     @FXML
     private ImageView displayPicture;
 
+    private static final BackgroundFill BACKGROUND_FILL_USER =
+            new BackgroundFill(Color.DARKGRAY, CornerRadii.EMPTY, Insets.EMPTY);
+    private static final BackgroundFill BACKGROUND_FILL_DUKE =
+            new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY);
+
     private DialogBox(String text, Image img) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
@@ -35,7 +46,9 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
+        final Circle clip = new Circle(50, 50, 45);
         displayPicture.setImage(img);
+        displayPicture.setClip(clip);
     }
 
     /**
@@ -49,12 +62,15 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        DialogBox userDialog = new DialogBox(text, img);
+        userDialog.setBackground(new Background(BACKGROUND_FILL_USER));
+        return userDialog;
     }
 
     public static DialogBox getDukeDialog(String text, Image img) {
         var db = new DialogBox(text, img);
         db.flip();
+        db.setBackground(new Background(BACKGROUND_FILL_DUKE));
         return db;
     }
 }
