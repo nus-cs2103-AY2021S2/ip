6ed62bc2f07454c85fb5536c87f3c46b9ad2c769
andrewzhang <zhangguangzesheng@gmail.com>
2021-02-15 17:33:33 +0800

    Add Missing JavaDoc

--- src/main/java/IDuke.java
+++ src/main/java/IDuke.java
@@ -1,10 +1,45 @@
 interface IDuke {
+
+    /**
+     * Prints greeting message.
+     */
     public void greeting();
+
+    /**
+     * Prints bye message.
+     */
     public void bye();
+
+    /**
+     *
+     * @param input User input
+     * @return IDuke object
+     */
     IDuke processInput(String input);
+
+    /**
+     *
+     * @param task a task
+     * @return new IDuke object with the adding task
+     */
     IDuke addTask(ITask task);
+
+    /**
+     * Prints the task list
+     */
     void display();
+
+    /**
+     *
+     * @param id task index
+     * @return a specific ITask object
+     */
     ITask getTask(int id);
+
+    /**
+     *
+     * @return number of tasks in the list
+     */
     int numOfTasks();
 
 }
