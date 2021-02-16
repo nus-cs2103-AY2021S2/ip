05100e31ec85b5fb9fbf916b975cd9b6015bd100
Winniehyx <e0425411@u.nus.edu>
2021-02-16 20:45:30 +0800

    An improved GUI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -19,7 +20,9 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.getIcons().add(new Image("/images/DaHappy.jpg"));
             fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.setTitle("Duke101");
             stage.show();
         } catch (IOException e) {
 
