19a02329540bb1f8878e3d17dd48ea5a68155462
car155 <carissaying0000@gmail.com>
2021-02-16 23:24:30 +0800

    Edit code to follow SLAP code quality

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -1,6 +1,7 @@
 package duke;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -21,6 +22,7 @@ public class Storage {
 
     /**
      * Construction with specified file path
+     * 
      * @param filePath the file path to load and save from
      */
     public Storage(String filePath) {
@@ -29,60 +31,33 @@ public class Storage {
 
     /**
      * Loads task list from the file path given during initialisation
+     * 
      * @return List of saved tasks
      * @throws IOException
      * @throws DateTimeParseException
      */
     public TaskList load() throws IOException, DateTimeParseException {
-        int lastDelimiterIndex = this.filePath.lastIndexOf("/");
-        if (lastDelimiterIndex < 0) {
-            lastDelimiterIndex = this.filePath.length() + 1;
-        }
-        String dirPath = this.filePath.substring(0, lastDelimiterIndex);
-        File dir = new File(dirPath);
+        String fileDir = getFileDir();
+        boolean fileDirExists = checkDirPath(fileDir);
 
-        if (!dir.exists()) {
-            dir.mkdirs();
+        if (!fileDirExists) {
+            makeFileDir(fileDir);
         }
 
         File f = new File(this.filePath);
-        TaskList memList = new TaskList();
 
         if (!f.exists()) {
             f.createNewFile();
+            return new TaskList();
         } else {
-            // fill with stored data
-            Scanner sc = new Scanner(f);
-            while (sc.hasNext()) {
-                String[] input = sc.nextLine().split(" \\| ");
-                String type = input[0];
-                Task task;
-
-                if (type.equals("T")) { // todo
-                    task = new ToDo(input[2]);
-                } else if (type.equals("D")) { // deadline
-                    LocalDate date = LocalDate.parse(input[3]);
-                    task = new Deadline(input[2], date);
-                } else { // event
-                    LocalDate date = LocalDate.parse(input[3]);
-                    task = new Event(input[2], date);
-                }
-
-                // change status
-                if (input[1].equals("1")) {
-                    task.setCompletion(true);
-                }
-
-                memList.add(task);
-            }
-            sc.close();
+            TaskList memList = readFromMem(f);
+            return memList;
         }
-
-        return memList;
     }
 
     /**
      * Stores task list into file path specified during initialisation
+     * 
      * @param tasks Task list to be saved
      * @throws IOException
      */
@@ -101,4 +76,66 @@ public class Storage {
 
     }
 
+    private String getFileDir() {
+        int lastDelimiterIndex = this.filePath.lastIndexOf("/");
+        if (lastDelimiterIndex < 0) {
+            return "";
+        }
+        String fileDir = this.filePath.substring(0, lastDelimiterIndex);
+        return fileDir;
+    }
+
+    private boolean checkDirPath(String fileDir) {
+        File dir = new File(fileDir);
+        return dir.exists();
+    }
+
+    private void makeFileDir(String fileDir) {
+        File dir = new File(fileDir);
+        dir.mkdirs();
+    }
+
+    private TaskList readFromMem(File f) throws FileNotFoundException {
+        assert(f.exists());
+
+        TaskList memList = new TaskList();
+
+        // fill with stored data
+        Scanner sc = new Scanner(f);
+        while (sc.hasNext()) {
+            String taskString = sc.nextLine();
+            Task parsedTask = parseTask(taskString);
+            memList.add(parsedTask);
+        }
+        sc.close();
+        return memList;
+    }
+
+    private Task parseTask(String taskString) {
+        String[] inputs = taskString.split(" \\| ");
+        String type = inputs[0];
+        Task task;
+
+        if (type.equals("T")) { // todo
+            assert(inputs.length == 3);
+            task = new ToDo(inputs[2]);
+
+        } else if (type.equals("D")) { // deadline
+            assert(inputs.length == 4);
+            LocalDate date = LocalDate.parse(inputs[3]);
+            task = new Deadline(inputs[2], date);
+
+        } else { // event
+            assert(inputs.length == 4);
+            LocalDate date = LocalDate.parse(inputs[3]);
+            task = new Event(inputs[2], date);
+        }
+
+        // change status
+        if (inputs[1].equals("1")) {
+            task.setCompletion(true);
+        }
+
+        return task;
+    }
 }
