e90e3a83324addecd8bf438dd50e40799f8204a3
nighoggDatatype <hosherman1965@gmail.com>
2021-02-16 22:14:31 +0800

    Remove unnecessary field

--- src/main/java/duke/exception/EmptyArgumentException.java
+++ src/main/java/duke/exception/EmptyArgumentException.java
@@ -1,9 +1,4 @@
 package duke.exception;
 
 public class EmptyArgumentException extends Exception {
-
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
 }
--- src/main/java/duke/exception/InvalidCommandException.java
+++ src/main/java/duke/exception/InvalidCommandException.java
@@ -1,7 +1,6 @@
 package duke.exception;
 
 public class InvalidCommandException extends Exception {
-    private static final long serialVersionUID = 1L;
     private String badCommand;
 
     /**
