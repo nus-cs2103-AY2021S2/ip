f8bb668e49a6132d1142cf4fc559afb038cd7edd
rajobasu <rajobasu@gmail.com>
2021-02-16 18:44:45 +0800

    Update TodoTest import statements

--- src/test/java/core/task/TodoTest.java
+++ src/test/java/core/task/TodoTest.java
@@ -3,7 +3,7 @@ package core.task;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TodoTest {
     @Test
