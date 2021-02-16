4aecb0773c9ee983c3343f7d1e61dab035443e4d
deyixtan <deyixtan@gmail.com>
2021-02-16 16:16:52 +0800

    Update command parsing with regex

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -4,6 +4,8 @@ import duke.command.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Represents the parser used by the Duke chat bot.
@@ -11,6 +13,127 @@ import java.time.format.DateTimeFormatter;
  * a suitable Command format for execution.
  */
 public class Parser {
+
+    private static final String INVALID_COMMAND = "I'm not trained with these commands yet...";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    private static final Pattern COMMAND_TYPE_PATTERN = Pattern.compile("usage|bye|list|find|save|todo|deadline|event|delete|done");
+    private static final Pattern USAGE_PATTERN = Pattern.compile("usage");
+    private static final Pattern BYE_PATTERN = Pattern.compile("bye");
+    private static final Pattern LIST_TASKS_PATTERN = Pattern.compile("list");
+    private static final Pattern FIND_TASKS_PATTERN = Pattern.compile("find ([a-zA-Z0-9\\s]+)");
+    private static final Pattern SAVE_TASKS_PATTERN = Pattern.compile("save");
+    private static final Pattern ADD_TODO_PATTERN = Pattern.compile("todo ([a-zA-Z0-9\\s]+)");
+    private static final Pattern ADD_DEADLINE_PATTERN = Pattern.compile("deadline ([a-zA-Z0-9\\s]+) /by (\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2})");
+    private static final Pattern ADD_EVENT_PATTERN = Pattern.compile("event ([a-zA-Z0-9\\s]+) /at (\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2})");
+    private static final Pattern DELETE_TASK_PATTERN = Pattern.compile("delete ([0-9]+)");
+    private static final Pattern MARK_TASK_AS_DONE_PATTERN = Pattern.compile("done ([0-9]+)");
+
+    public static String getCommandType(String input) throws DukeException {
+        String commandType = input.split(" ")[0].toLowerCase();
+        Matcher m = Parser.COMMAND_TYPE_PATTERN.matcher(commandType);
+        if (!m.find()) {
+            throw new DukeException(Parser.INVALID_COMMAND);
+        }
+        return commandType;
+    }
+
+    public static UsageCommand getUsageCommand(String input) throws DukeException {
+        Matcher m = Parser.USAGE_PATTERN.matcher(input);
+        if (!m.find()) {
+            throw new DukeException(Parser.INVALID_COMMAND);
+        }
+        return new UsageCommand();
+    }
+
+    public static ByeCommand getByeCommand(String input) throws DukeException {
+        Matcher m = Parser.BYE_PATTERN.matcher(input);
+        if (!m.find()) {
+            throw new DukeException(Parser.INVALID_COMMAND);
+        }
+        return new ByeCommand();
+    }
+
+    public static ListTasksCommand getListTasksCommand(String input) throws DukeException {
+        Matcher m = Parser.LIST_TASKS_PATTERN.matcher(input);
+        if (!m.find()) {
+            throw new DukeException(Parser.INVALID_COMMAND);
+        }
+        return new ListTasksCommand();
+    }
+
+    public static FindTasksCommand getFindTasksCommand(String input) throws DukeException {
+        Matcher m = Parser.FIND_TASKS_PATTERN.matcher(input);
+        if (!m.find() || m.groupCount() != 1) {
+            throw new DukeException(Parser.INVALID_COMMAND);
+        }
+
+        String keyword = m.group(1);
+        return new FindTasksCommand(keyword);
+    }
+
+
+    public static SaveTasksCommand getSaveTasksCommand(String input) throws DukeException {
+        Matcher m = Parser.SAVE_TASKS_PATTERN.matcher(input);
+        if (!m.find()) {
+            throw new DukeException(Parser.INVALID_COMMAND);
+        }
+        return new SaveTasksCommand();
+    }
+
+
+    public static AddTodoCommand getAddTodoCommand(String input) throws DukeException {
+        Matcher m = Parser.ADD_TODO_PATTERN.matcher(input);
+        if (!m.find() || m.groupCount() != 1) {
+            throw new DukeException(Parser.INVALID_COMMAND);
+        }
+
+        String desc = m.group(1);
+        return new AddTodoCommand(desc);
+    }
+
+    public static AddDeadlineCommand getAddDeadlineCommand(String input) throws DukeException {
+        Matcher m = Parser.ADD_DEADLINE_PATTERN.matcher(input);
+        if (!m.find() || m.groupCount() != 2) {
+            throw new DukeException(Parser.INVALID_COMMAND);
+        }
+
+        String desc = m.group(1);
+        LocalDateTime dateTime = LocalDateTime.parse(m.group(2), Parser.FORMATTER);
+        return new AddDeadlineCommand(desc, dateTime);
+    }
+
+    public static AddEventCommand getAddEventCommand(String input) throws DukeException {
+        Matcher m = Parser.ADD_EVENT_PATTERN.matcher(input);
+        if (!m.find() || m.groupCount() != 2) {
+            throw new DukeException(Parser.INVALID_COMMAND);
+        }
+
+        String desc = m.group(1);
+        LocalDateTime dateTime = LocalDateTime.parse(m.group(2), Parser.FORMATTER);
+        return new AddEventCommand(desc, dateTime);
+    }
+
+    public static DeleteTaskCommand getDeleteTaskCommand(String input) throws DukeException {
+        Matcher m = Parser.DELETE_TASK_PATTERN.matcher(input);
+        if (!m.find() || m.groupCount() != 1) {
+            throw new DukeException(Parser.INVALID_COMMAND);
+        }
+
+        Integer index = Integer.parseInt(m.group(1));
+        return new DeleteTaskCommand(index);
+    }
+
+
+    public static MarkTaskAsDoneCommand getMarkTaskAsDoneCommand(String input) throws DukeException {
+        Matcher m = Parser.MARK_TASK_AS_DONE_PATTERN.matcher(input);
+        if (!m.find() || m.groupCount() != 1) {
+            throw new DukeException(Parser.INVALID_COMMAND);
+        }
+
+        Integer index = Integer.parseInt(m.group(1));
+        return new MarkTaskAsDoneCommand(index);
+    }
 
     /**
      * Processes the raw user input and converts it
@@ -22,64 +145,31 @@ public class Parser {
      * @throws DukeException If command arguments are misused.
      */
     public static Command parseCommand(String input) throws DukeException {
-        String[] inputArr = input.split(" ");
-        String taskType = inputArr[0].toLowerCase();
-        String taskDesc = "";
-        String taskArg = "";
+        String commandType = Parser.getCommandType(input);
 
-        // Get description
-        int i;
-        for (i = 1; i < inputArr.length; i++) {
-            if (taskType.equals("todo") || (!inputArr[i].equalsIgnoreCase("/by")
-                    && !inputArr[i].equalsIgnoreCase("/at"))) {
-                if (!taskDesc.equals("")) {
-                    taskDesc += " ";
-                }
-                taskDesc += inputArr[i];
-            } else {
-                // Ensure no misuse of arguments
-                if (!inputArr[i].equalsIgnoreCase("/by") && taskType.equals("deadline")) {
-                    throw new DukeException("You're confusing me with parameters from other commands...");
-                } else if (!inputArr[i].equalsIgnoreCase("/at") && taskType.equals("event")) {
-                    throw new DukeException("You're confusing me with parameters from other commands...");
-                }
-                break;
-            }
-        }
-
-        // Get argument
-        for (i = i + 1; i < inputArr.length; i++) {
-            if (!taskArg.equals("")) {
-                taskArg += " ";
-            }
-            taskArg += inputArr[i];
-        }
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-
-        switch(taskType.toUpperCase()) {
-        case "TODO":
-            return new AddTodoCommand(taskDesc);
-        case "DEADLINE":
-            return new AddDeadlineCommand(taskDesc, LocalDateTime.parse(taskArg, formatter));
-        case "EVENT":
-            return new AddEventCommand(taskDesc, LocalDateTime.parse(taskArg, formatter));
-        case "DELETE":
-            return new DeleteTaskCommand(Integer.parseInt(taskDesc));
-        case "DONE":
-            return new MarkTaskAsDoneCommand(Integer.parseInt(taskDesc));
-        case "FIND":
-            return new FindTasksCommand(taskDesc);
-        case "LIST":
-            return new ListTasksCommand();
-        case "SAVE":
-            return new SaveTasksCommand();
-        case "USAGE":
-            return new UsageCommand();
-        case "BYE":
-            return new ByeCommand();
-        default:
-            throw new DukeException("I'm not trained with these commands yet...");
+        switch (commandType) {
+            case "usage":
+                return Parser.getUsageCommand(input);
+            case "bye":
+                return Parser.getByeCommand(input);
+            case "list":
+                return Parser.getListTasksCommand(input);
+            case "find":
+                return Parser.getFindTasksCommand(input);
+            case "save":
+                return Parser.getSaveTasksCommand(input);
+            case "todo":
+                return Parser.getAddTodoCommand(input);
+            case "deadline":
+                return Parser.getAddDeadlineCommand(input);
+            case "event":
+                return Parser.getAddEventCommand(input);
+            case "delete":
+                return Parser.getDeleteTaskCommand(input);
+            case "done":
+                return Parser.getMarkTaskAsDoneCommand(input);
+            default:
+                throw new DukeException(Parser.INVALID_COMMAND);
         }
     }
 }
