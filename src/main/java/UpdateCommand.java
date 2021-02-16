7cbd37692784971652cafe155dbf61c04ae76385
Lim Li Quan <linliquan1996@Hotmail.com>
2021-02-16 16:49:23 +0800

    Fix checkStyle errors for UpdateCommand.java

--- src/main/java/UpdateCommand.java
+++ src/main/java/UpdateCommand.java
@@ -1,8 +1,15 @@
-public class UpdateCommand extends Command{
+public class UpdateCommand extends Command {
     private int option;
     private String field;
     private String value;
 
+    /**
+     * Creates a update command.
+     *
+     * @param option , a number indicating which task to update.
+     * @param field , field of task to update.
+     * @param value , value to update to.
+     */
     public UpdateCommand(int option, String field, String value) {
         this.option = option;
         this.field = field;
