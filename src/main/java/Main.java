7748a68e1a0a7934ffe4ee51ddbb5fc605dcbc68
Zeche <zx3110@gmail.com>
2021-02-16 13:13:35 +0800

    Add title to dialog window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
             assert ap != null : "AnchorPane is Null.";
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Mike the Hawk");
             fxmlLoader.<MainWindow>getController().setMike(mike);
             stage.show();
         } catch (IOException e) {
