707efcd60672dbf3daccacf1166c271237765dbe
Yue Yang <lyueyang@u.nus.edu>
2021-02-16 19:20:23 +0800

    Add differentiation for switchblade
    Grey background and white font added for Switchblade

--- src/main/java/switchblade/DialogBox.java
+++ src/main/java/switchblade/DialogBox.java
@@ -1,18 +1,23 @@
 package switchblade;
 
+import java.io.IOException;
+import java.util.Collections;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
-
-import java.io.IOException;
-import java.util.Collections;
+import javafx.scene.paint.Color;
 
 public class DialogBox extends HBox {
 
@@ -21,7 +26,7 @@ public class DialogBox extends HBox {
     @FXML
     private ImageView displayPicture;
 
-    private DialogBox(String text, Image img) {
+    private DialogBox(String text, Image img, Color color) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
             fxmlLoader.setController(this);
@@ -32,6 +37,7 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
+        dialog.setTextFill(color);
         displayPicture.setImage(img);
     }
 
@@ -47,11 +53,15 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getUserDialog(String text, Image iv) {
-        return new DialogBox(text, iv);
+        return new DialogBox(text, iv, Color.BLACK);
     }
 
     public static DialogBox getSbDialog(String text, Image iv) {
-        var db = new DialogBox(text, iv);
+        BackgroundFill backgroundFill = new BackgroundFill(Color.GREY, CornerRadii.EMPTY, Insets.EMPTY);
+        Background bg = new Background(backgroundFill);
+
+        var db = new DialogBox(text, iv, Color.WHITE);
+        db.setBackground(bg);
         db.flip();
         return db;
     }
