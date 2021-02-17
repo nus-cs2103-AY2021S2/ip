57cb1ca07bf4d8476d48c447c62d2d2ca0bf092a
YuHeem <54734556+yuheem@users.noreply.github.com>
2021-02-17 13:19:50 +0800

    docs(message): add missing comments for public methods

--- src/main/java/duke/message/Messages.java
+++ src/main/java/duke/message/Messages.java
@@ -24,18 +24,41 @@ public class Messages {
     private static final String MESSAGE_UNABLE_TO_FIND_TASKS = "No found tasks matching your query.\n";
     private static final String MESSAGE_FIND_TASKS = "Here are the matching tasks in your list:\n" + "%s";
 
+    /**
+     * Returns the message to be shown to the user when a task has been added to the task list.
+     * @param task The task added to the task list.
+     * @param numberOfTasks The updated number of tasks in the task list.
+     * @return Message to be shown to user.
+     */
     public static String getAddTaskMessage(Task task, int numberOfTasks) {
         return String.format(MESSAGE_ADD_TASK, task, numberOfTasks);
     }
 
+    /**
+     * Returns the message to be shown to the user when a task has been deleted from the task list.
+     * @param task The task deleted from the task list.
+     * @param numberOfTasks The updated number of tasks in the task list.
+     * @return Message to be shown to user.
+     */
     public static String getDeleteTaskMessage(Task task, int numberOfTasks) {
         return String.format(MESSAGE_DELETE_TASK, task, numberOfTasks);
     }
 
+    /**
+     * Returns the message to be shown to the user when a task has been marked done in the task list.
+     * @param task The task marked done.
+     * @return Message to be shown to user.
+     */
     public static String getDoneTaskMessage(Task task) {
         return String.format(MESSAGE_DONE_TASK, task);
     }
 
+    /**
+     * Returns the message to be shown to the user when a request to list all tasks in the task list is made.
+     * If no task are in the task list, the appropriate message is also shown to the user.
+     * @param tasks The task list.
+     * @return Message to be shown.
+     */
     public static String getListTaskMessage(TaskList tasks) {
         int numberOfTasks = tasks.size();
         if (numberOfTasks == 0) {
@@ -45,6 +68,12 @@ public class Messages {
         return String.format(MESSAGE_LIST_TASKS, tasks);
     }
 
+    /**
+     * Returns the message to be shown to to the user when a request to find tasks in the task list is made.
+     * If no matching tasks are found in the task list, the appropriate message is also shown to the user.
+     * @param foundTasks The matching tasks found in the task list.
+     * @return Message to be shown.
+     */
     public static String getFindTaskMessage(TaskList foundTasks) {
         int numberOfFoundTasks = foundTasks.size();
         if (numberOfFoundTasks == 0) {
