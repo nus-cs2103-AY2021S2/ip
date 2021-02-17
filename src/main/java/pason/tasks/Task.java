4822bb71d7df711ac9b09a488f968eee949238c5
JasonLim <jasaaanlim@users.noreply.github.com>
2021-02-17 23:36:55 +0800

    Add method JavaDocs

--- src/main/java/pason/tasks/Task.java
+++ src/main/java/pason/tasks/Task.java
@@ -12,18 +12,30 @@ public class Task {
         this.isDone = false;
     }
 
+    /**
+     * Gets Task description.
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Marks Task as done.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Returns done status.
+     */
     public boolean isDone() {
         return this.isDone;
     }
 
+    /**
+     * Returns the Task int he file format.
+     */
     public String toFileFormat() {
         return (isDone ? "1" : "0") + " | " + this.description;
     }
