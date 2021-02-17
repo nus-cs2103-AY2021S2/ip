f35a3677c0b025db75965f33ae1571f1eedda766
ong6 <junxiongong2@gmail.com>
2021-02-18 01:19:49 +0800

    Add Java Docs

--- src/main/java/DukeUi.java
+++ src/main/java/DukeUi.java
@@ -8,6 +8,10 @@ public class DukeUi {
     private TaskList tasks;
     private Parser parser;
 
+    /**
+     * Runs DukeUi with a default file storage called "duke.txt"
+     * @param filePath location of file.
+     */
     public DukeUi(String filePath) {
         try {
             this.storage = new Storage(filePath);
@@ -21,15 +25,5 @@ public class DukeUi {
     public String getResponse(String input) {
         return this.parser.response(input);
     }
-
-    // private void initializeDuke(String filePath) {
-    //     try {
-    //         this.storage = new Storage(filePath);
-    //         this.tasks = new TaskList(storage.load());
-    //     } catch (Exception e) {
-    //         this.tasks = new TaskList();
-    //     }
-    //     this.parser = new Parser(storage, tasks);
-    // }
 
 }
