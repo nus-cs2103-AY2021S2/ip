dade625db77cbcf58c5abdda9b9f216f6b6d88a6
Zen Lee Yijie <iarezenly@gmail.com>
2021-02-18 00:02:07 +0800

    Styling fix
    Place else block in correct position

--- src/main/java/duke/util/Parser.java
+++ src/main/java/duke/util/Parser.java
@@ -170,8 +170,7 @@ public class Parser {
         }
         else if (split.length > 1 && (split[1].equals("/by") || split[1].equals("/at"))) {
             throw new DukeArgumentException("You have not entered a task description!");
-        }
-        else {
+        } else {
             // Do nothing if a keyword or task description is provided.
         }
     }
