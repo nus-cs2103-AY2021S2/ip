e27c5488ff46045b2358bd4ec7ccd1b7952e78ec
YuHeem <54734556+yuheem@users.noreply.github.com>
2021-02-17 13:13:51 +0800

    docs(exception): add missing comment for public methods

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -5,6 +5,10 @@ package duke.exception;
  * encountered when an instance of Duke is running.
  */
 public class DukeException extends Exception {
+    /**
+     * Constructor takes in the message to be shown to the user.
+     * @param message The message to be shown to the user.
+     */
     public DukeException(String message) {
         super(message);
     }
