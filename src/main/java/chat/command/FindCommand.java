e5da47b5a6bdec07cccc172006080859efd9917a
clarlzx <clarlzx@gmail.com>
2021-02-19 03:10:55 +0800

    FindCommand: Add more flexibility to find function
    Keywords can be arranged in any order.
    Keywords are also categorised by keywords such that users will not be
    confused when entering the search terms.

--- src/main/java/chat/command/FindCommand.java
+++ src/main/java/chat/command/FindCommand.java
@@ -1,20 +1,31 @@
 package chat.command;
 
-import chat.*;
+import chat.ChatException;
+import chat.TaskList;
+import chat.Storage;
+import chat.Ui;
 import chat.task.Task;
 
 import java.util.ArrayList;
-import java.util.Objects;
+import java.util.HashMap;
 
 /**
  * A type of command that finds tasks based on keywords given by user
  */
 public class FindCommand extends Command {
+
+    /**
+     * Enumerator class to classify the types of keywords that the user can enter for the find command.
+     */
+    enum KeywordKey { 
+        TASK_TYPE, NAME, IS_COMPLETED, DATES;
+    }
     
     String inputStr;
 
     /**
-     * Initialises FindCommand object.
+     * Initialises FindCommand object with command that includes find keywords given by user. 
+     * 
      * 
      * @param inputStr Inputted command string from user to Chat the Cat.
      */
@@ -36,52 +47,38 @@ public class FindCommand extends Command {
         assert ui != null;
         assert storage != null;
         
-        String keyword = this.inputStr.replace("find","").strip();
-        TaskList foundTaskList = getFoundTaskList(keyword, taskList);
+        TaskList foundTaskList = getFoundTaskList(taskList);
         if (foundTaskList.getTasks().isEmpty()) {
             throw new ChatException("Sorry! No results found.");
         } 
         ui.list(foundTaskList);
     }
-
+    
     /**
-     * Given a particular keyword, this function returns a TaskList object containing list of tasks 
-     * that have that keyword in it's name.
-     * <p>Keyword should follow format: taskType,name,isDone,dates.</p>
-     * <pParameters in keyword can be left black if user wishes to search by only a few parameters.</p>
-     * <p>Task type can be indicated by "T" (todo), "D" (deadline) or "E" (event).</p>
-     * <p>Dates should be inputted with format dd/MM/YYYY or dd/MM or MM/YYYY.</p>
-     * <p>Dates should be separated with a space.</p>
-     * <p></p>
-     * <p>Some examples include:</p>
-     * <p>find T --> lists all todo tasks</p>
-     * <p>find ,return,T --> lists all tasks whose name includes the term 'return' and are not done</p>
-     * <p>find ,,,11/02 12/02 --> lists all tasks with start date 11/02 and end date 12/02</p>
+     * Returns a TaskList object containing list of tasks that match the keywords given by user.
      * 
-     * @param keyword Find tasks based on this keyword.
      * @param taskList TaskList object containing all tasks.
-     * @return TaskList object containing list of tasks that fit the keyword.
+     * @return TaskList object containing list of tasks that fit the keywords.
+     * @throws ChatException
      */
-    public TaskList getFoundTaskList(String keyword, TaskList taskList) throws ChatException {
+    public TaskList getFoundTaskList(TaskList taskList) throws ChatException {
         assert taskList != null;
 
-        String formatStr = "Please find with format:\n" +
-                "find taskType,name,isDone,dates\n\n" +
-                "Parameters can be left blank if you wish to only search by a few parameters.\n" +
-                "Dates should also be inputted with format dd/MM/YYYY or dd/MM or MM/YYYY.\n" +
-                "Dates should be separated with a space.\n" +
-                "* i.e. find T --> lists all todo tasks\n" +
-                "* i.e. find ,return,T --> lists all tasks whose name includes the term 'return'" +
-                "and are not done\n" +
-                "* i.e. find ,,,11/02 12/02 --> lists all tasks with start date 11/02 and end date 12/02\n";
+        String formatStr = "Please utilise this format:\n" +
+                "find t/[TASK_TYPE] n/[NAME] c/[IS_COMPLETED] d/[DATES]\n\n" +
+                "TASK_TYPE: T, D or E\n" +
+                "IS_COMPLETED: T or F\n" +
+                "DATES: dates can be written as dd MMM or MMM YYYY\n\n" +
+                "Examples:\n" +
+                "* find t/T n/book c/T --> lists completed todo tasks of which name includes book\n" +
+                "* find t/D c/F --> lists uncompleted deadline and event tasks\n" +
+                "* find d/13 Mar, 14 Mar 2021 --> lists tasks that start on 13 Mar and end on 14 Mar 2021";
 
         try {
-            String keyWord = this.inputStr.replace("find", "").strip();
-            String[] keys = keyWord.split(",");
-
+            HashMap<KeywordKey, String> keywordMap = getKeywordMap();
             TaskList foundTaskList = new TaskList(new ArrayList<Task>());
             for (Task task : taskList.getTasks()) {
-                if (isFound(task, keys)) {
+                if (isFound(task, keywordMap)) {
                     foundTaskList.getTasks().add(task);
                 }
             }
@@ -92,37 +89,90 @@ public class FindCommand extends Command {
     }
 
     /**
-     * A method that checks if matching task is found.
+     * Returns a hashmap, where keywords have been categorised by their given key. (i.e. task type: t/, name: n/, etc.)
+     * The string behind the keys have been taken as their respective values in the hashmap.
+     * 
+     * @return HashMap 
+     * @throws ChatException 
+     */
+    private HashMap<KeywordKey, String> getKeywordMap() throws ChatException {
+        try {
+            HashMap<KeywordKey, String> keywordMap = new HashMap<>();
+            String keywordStr = this.inputStr.replace("find", "").strip();
+            
+            if (keywordStr.equals("")) {
+                throw new ChatException("There should be at least one keyword.\n");
+            }
+            
+            String[] strings = keywordStr.split("/");
+            String key = strings[0];
+            for (int i = 1; i < strings.length - 1; i++) {
+                keywordMap.put(getKeywordKey(key), strings[i].substring(0, strings[i].length() - 1).strip());
+                key = Character.toString(strings[i].charAt(strings[i].length() - 1));
+            }
+            keywordMap.put(getKeywordKey(key), strings[strings.length - 1]);
+            return keywordMap;
+        } catch (ChatException e) {
+            throw e;
+        }
+    }
+
+    /**
+     * Converts string to KeywordKey constant. 
+     * 
+     * @param s String to be converted to KeywordKey constant.
+     * @return KeywordKey constant.
+     * @throws ChatException ChatException is thrown, if string does not represent any of the possible keywords.
+     */
+    public KeywordKey getKeywordKey(String s) throws ChatException {
+        KeywordKey keywordKey;
+        switch (s) {
+        case "t":
+            keywordKey = KeywordKey.TASK_TYPE;
+            break;
+        case "n":
+            keywordKey = KeywordKey.NAME;
+            break;
+        case "c":
+            keywordKey = KeywordKey.IS_COMPLETED;
+            break;
+        case "d":
+            keywordKey = KeywordKey.DATES;
+            break;
+        default:
+            throw new ChatException("The keyword term " + s + "/ does not exist.\n");
+        }
+        return keywordKey;
+    }
+
+    /**
+     * A method that checks if matching task to keywords is found.
      * 
      * @param task Task being checked.
-     * @param keys Array of parameters derived from keyword given by user.
+     * @param keywordMap HashMap containing keywords with KeywordKey keys.
      * @return Boolean indicating if matching task is found.
-     * @throws ChatException Error is thrown if there are too many search parameters given by keys.
      */
-    public boolean isFound(Task task, String ...keys) throws ChatException {
-        if (keys.length > 4) {
-            throw new ChatException("Too many search parameters.\n");
-        }
+    private boolean isFound(Task task, HashMap<KeywordKey, String> keywordMap) {
         
-        for (int i = 0; i < keys.length; i++) {
-            switch (i) {
-            case 0:
-                if (!isMatchingTaskType(task, keys[0])) {
+        for (KeywordKey key : keywordMap.keySet()) {
+            switch (key) {
+            case TASK_TYPE:
+                if (!isMatchingTaskType(task, keywordMap.get(key))) {
                     return false;
                 }
                 break;
-            case 1:
-                if (!isMatchingName(task, keys[1])) {
+            case NAME:
+                if (!isMatchingName(task, keywordMap.get(key))) {
                     return false;
                 }
                 break;
-            case 2:
-                if (!isMatchingDoneStatus(task, keys[2])) {
+            case IS_COMPLETED:
+                if (!isMatchingDoneStatus(task, keywordMap.get(key))) {
                     return false;
                 }
                 break;
-            case 3:
-                if (!isMatchingDates(task, keys[3])) {
+            case DATES:
+                if (!isMatchingDates(task, keywordMap.get(key))) {
                     return false;
                 }
                 break;
@@ -161,7 +211,7 @@ public class FindCommand extends Command {
     private static boolean isMatchingDates(Task task, String dateStr) {
         String[] dates = dateStr.split(" ");
         for (String date : dates) {
-            if (!task.allParameterStr().contains(date)) {
+            if (!task.toString().contains(date)) {
                 return false;
             }
         }
