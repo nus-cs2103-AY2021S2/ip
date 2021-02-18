ab15c60e46178bc40a5c875158d07982675ac455
Isaiah Koh <zaiah0505@hotmail.com>
2021-02-18 17:28:16 +0800

    Added JavaDocs to Command

--- src/main/java/command/Command.java
+++ src/main/java/command/Command.java
@@ -3,9 +3,28 @@ package command;
 import task.TaskManager;
 import util.DukeException;
 
+/**
+ * Command class representing a future action that can be executed on the supplied
+ * TaskManager object.
+ */
 public abstract class Command {
+    /**
+     * Performs an action on the supplied taskManager and returns a String response
+     * based on the action that was performed.
+     *
+     * @param taskManager TaskManager object to perform the action on.
+     * @return String response of the action that was taken.
+     * @throws DukeException Whenever the supplied action cannot be performed on
+     *                       the given
+     */
     public abstract String execute(TaskManager taskManager) throws DukeException;
 
+    /**
+     * Indicates if the Command object should signal to the application that the
+     * user wants to terminate it.
+     *
+     * @return True, if the Command should telll the application to end.
+     */
     public boolean isQuitCommand() {
         return false;
     }
