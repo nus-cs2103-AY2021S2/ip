bd81374710ba6d0064324927a305d6072646c720
Isaiah Koh <zaiah0505@hotmail.com>
2021-02-18 23:22:18 +0800

    Added rounded image frames

--- src/main/java/nodes/DialogBox.java
+++ src/main/java/nodes/DialogBox.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
@@ -23,8 +24,10 @@ public class DialogBox extends HBox {
 
         text.setWrapText(true);
         text.setFont(Font.font ("Consolas"));
+        Circle clip = new Circle( 50, 50,50);
         displayPicture.setFitWidth(100.0);
         displayPicture.setFitHeight(100.0);
+        displayPicture.setClip(clip);
 
         this.setAlignment(Pos.TOP_RIGHT);
         this.getChildren().addAll(text, displayPicture);
