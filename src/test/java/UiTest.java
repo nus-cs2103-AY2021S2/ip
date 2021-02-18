9d499f518295f972664697ba6df8a68eb0fe1eed
mabel-kang <e0425446@u.nus.edu>
2021-02-19 00:53:47 +0800

    Modify tests

--- src/test/java/UiTest.java
+++ src/test/java/UiTest.java
@@ -1,22 +1,14 @@
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
+import org.junit.jupiter.api.Test;
 
 public class UiTest {
 
     @Test
-    public void testResponseToList() {
-         PrintStream out = System.out;
-         ByteArrayOutputStream content = new ByteArrayOutputStream();
-         System.setOut(new PrintStream(content));
-         Ui ui = new Ui();
-         ui.respondToList(0);
-         assertEquals("Your list is currently empty! Let's start adding tasks!",
-                 content.toString().trim());
-         System.setOut(out);
+    public void respondToList_taskSizeZero_success() {
+        Ui ui = new Ui();
+        String response = ui.respondToList(0);
+        assertEquals("Your list is currently empty! Let's start adding tasks!", response);
     }
 
 }
