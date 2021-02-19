d6ece1567feafe615285b74d91ee01fb1c653d07
Jared Lim <jaredlim1998@hotmail.com>
2021-02-20 00:26:30 +0800

    Updated minor changes to UI class.

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,5 +1,3 @@
-import java.io.File;
-
 public class Ui {
 
 
@@ -20,14 +18,14 @@ public class Ui {
 
     public String outHelp() {
         final String commandTypes = String.format("Here are the list of all commands:" +
-                " \nType help to repeat this commands \n\n" +
+                " \n(Type help again to repeat these commands)\n\n" +
                 "list - lists all existing tasks\n" +
                 "todo [insert task] - creates a todo task\n" +
                 "deadline [insert deadline] - creates a deadline\n" +
                 "event [insert event] - creates an event\n" +
                 "delete [insert index] - deletes event based on index\n" +
                 "find [insert keyword] [search type(optional)] - finds tasks " +
-                "corresponding \n to keyword\n" +
+                "corresponding\n to keyword\n" +
                 "done [insert index] - marks task as done");
         return commandTypes;
     }
@@ -37,53 +35,31 @@ public class Ui {
     }
 
     public String outNotRecognized() {
-        return "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+        return ":( OOPS!!! I'm sorry, but I don't know what that means :-(";
     }
 
     public String outInvalidDeadline() {
-        return "☹ OOPS!!! The description of a deadline " +
+        return ":( OOPS!!! The description of a deadline " +
                 "cannot be empty.";
     }
 
     public String outInvalidEvent() {
-        return "☹ OOPS!!! The description of an event cannot be empty.";
+        return ":( OOPS!!! The description of an event cannot be empty.";
     }
 
     public String outInvalidTodo() {
-        return "☹ OOPS!!! The description of a todo cannot be empty.";
+        return ":( OOPS!!! The description of a todo cannot be empty.";
     }
 
     public String outInvalidDelete() {
-        return "OOPS!!! Please ensure that you have correctly indicated the index of " +
-                "the item " +
-                "you want to find.";
+        return ":( OOPS!!! Please ensure that you have correctly indicated the index " +
+                "of the item you want to find.";
     }
 
     public String outInvalidFind() {
-        return "OOPS!!! Please ensure that you have correctly indicated the keyword " +
-                "of the task(s) you want to search for.";
+        return ":( OOPS!!! Please ensure that you have correctly indicated the " +
+                "keyword of the task(s) you want to search for.";
     }
 
-    public void printBye() {
-        System.out.println("Bye. Hope to see you again soon!");
-    }
-
-    public void commandNotRecognized() {
-        System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-    }
-
-    public void invalidDeadline() {
-        System.out.println("☹ OOPS!!! The description of a deadline " +
-                "cannot be empty.");
-    }
-
-    public void invalidEvent() {
-        System.out.println("☹ OOPS!!! The description of an event cannot " +
-                "be empty.");
-    }
-
-    public void invalidTodo() {
-        System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
-    }
 
 }
