1af4b8f5711c3279a669c8ce6a9910ccaf67d71c
juhaerpasha <juhaeradittya200@gmail.com>
2021-02-19 18:48:58 +0800

    improve code

--- src/main/java/duke/textUi/DukeText.java
+++ src/main/java/duke/textUi/DukeText.java
@@ -11,6 +11,9 @@ import duke.task.TodoTask;
 
 /**
  * The main class where the duke text ui is run
+ * This is slightly outdated from the current versions
+ * Note features not available
+ *
  */
 public class DukeText {
     private final Ui ui;
@@ -65,49 +68,6 @@ public class DukeText {
                 }
             }
         }
-    }
-
-    public String processGUI(String input) {
-        String command = input;
-        Parser p = new Parser(command);
-        p.parse();
-        String TaskType = p.getTaskType();
-        String result = "";
-        if (command.equals("bye") || command.equals("exit")) {
-            ui.goodbye();
-            storage.saveTask(taskList);
-            result = "Goodbye for now.\nHope to see you soon!";
-        } else if (command.equals("list")) {
-            result = taskList.getAllTasks();
-        } else if (p.getCommandLength() > 1) {
-            if (TaskType.equals("done")) {
-                result = taskList.markAsDone(Integer.parseInt(p.getIndex()));
-            } else if (TaskType.equals("delete") || TaskType.equals("remove")) {
-                result = taskList.DeleteTask(Integer.parseInt(p.getIndex()));
-            } else if (TaskType.equals("find") || TaskType.equals("search")) {
-                result = taskList.findTask(p.getTaskName());
-            } else if (TaskType.equals("todo")) {
-                result = taskList.addTask(new TodoTask(command));
-            } else if (TaskType.equals("deadline")) {
-                result = taskList.addTask(new DeadlineTask(command));
-            } else if (TaskType.equals("event")) {
-                result = taskList.addTask(new EventTask(command));
-            }
-        } else {
-            if (TaskType.equals("todo") || TaskType.equals("deadline")
-                    || TaskType.equals("event")) {
-                result = "Oops!!! Incomplete command :(";
-                //    throw new DukeException("Oops!!! Incomplete command :(");
-            } else {
-                result = "Oops!!! Invalid Input :(";
-                //    throw new DukeException("Oops!!! Invalid Input :(");
-            }
-        }
-        return result;
-    }
-
-    public String getResponse(String input) {
-        return processGUI(input);
     }
 
     public void printMessage(String out) {
