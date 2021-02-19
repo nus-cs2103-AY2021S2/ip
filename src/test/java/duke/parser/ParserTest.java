6cd0620a7c68936cc2be0bb24b137a4db232ae70
SiTingST <33442908+SiTingST@users.noreply.github.com>
2021-02-19 20:11:06 +0800

    Fixed codes against CheckStyle

--- src/test/java/duke/parser/ParserTest.java
+++ src/test/java/duke/parser/ParserTest.java
@@ -1,13 +1,13 @@
 package duke.parser;
 
-import duke.exception.DukeException;
-import duke.ui.UI;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import duke.exception.DukeException;
 
 public class ParserTest {
 
@@ -16,9 +16,8 @@ public class ParserTest {
     @Test
     public void isValidTaskNumberTest() throws DukeException {
         Parser parser = new Parser();
-
         String dateInString = "19/02/2025";
-        LocalDate date =  LocalDate.parse(dateInString, DATE_TIME_FORMATTER);
+        LocalDate date = LocalDate.parse(dateInString, DATE_TIME_FORMATTER);
 
         assertEquals(parser.check_valid_date("19/02/2025"), date);
     }
--- src/test/java/duke/tasklist/TaskListTest.java
+++ src/test/java/duke/tasklist/TaskListTest.java
@@ -1,11 +1,13 @@
 package duke.tasklist;
 
-import duke.exception.DukeException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import duke.exception.DukeException;
 
 public class TaskListTest {
 
@@ -15,19 +17,20 @@ public class TaskListTest {
     public void addingTaskTest() {
 
         taskList.addToDo("testing description");
-        taskList.addDeadline("test deadline", LocalDate.of(2021, 1,25),
-                LocalTime.NOON);
 
-        taskList.addEvent("test event", LocalDate.of(2021, 1,26),
-                LocalTime.NOON, LocalTime.MIDNIGHT);
+        LocalDate deadlineDate = LocalDate.of(2021 , 1 , 25);
+        taskList.addDeadline("test deadline" , deadlineDate, LocalTime.NOON);
 
-        assertEquals(" Todo:testing description[" + '\u2718'+ "] ",
+        LocalDate eventDate = LocalDate.of(2021 , 1 , 26);
+        taskList.addEvent("test event" , eventDate , LocalTime.NOON, LocalTime.MIDNIGHT);
+
+        assertEquals(" Todo:testing description[" + '\u2718' + "] ",
                 taskList.getTaskListArray().get(1).toString());
 
-        assertEquals(" Deadline:test deadline[" + '\u2718'+ "]  (by: 2021-01-25 12:00)",
+        assertEquals(" Deadline:test deadline[" + '\u2718' + "]  (by: 2021-01-25 12:00)" ,
                 taskList.getTaskListArray().get(2).toString());
 
-        assertEquals(" Event:test event[" +  '\u2718'+ "] (at: 2021-01-26 12:00 00:00)",
+        assertEquals(" Event:test event[" + '\u2718' + "] (at: 2021-01-26 12:00 00:00)",
                 taskList.getTaskListArray().get(3).toString());
     }
 
@@ -36,13 +39,13 @@ public class TaskListTest {
     public void markAsDoneTest() throws DukeException {
         taskList.addToDo("do ip");
         taskList.markAsDone(0);
-        assertEquals(" Todo:do ip[" + '\u2713'+ "] ", taskList.getTaskListArray().get(0).toString());
-
+        assertEquals(" Todo:do ip[" + '\u2713' + "] ", taskList.getTaskListArray().get(0).toString());
     }
 
     @Test
     public void deleteTaskTest() throws DukeException {
         taskList.deleteTask(0);
-        assertEquals(" Todo:testing description[" +  '\u2718'+ "] ", taskList.getTaskListArray().get(0).toString());
+        assertEquals(" Todo:testing description[" + '\u2718' + "] ",
+                taskList.getTaskListArray().get(0).toString());
     }
 }
