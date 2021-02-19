e4e2868eddd8b6dd91cb810a1713a889f416a9d2
Justin Gnoh <justingnoh123@gmail.com>
2021-02-19 13:33:15 +0800

    Improve Gui by adding CSS styling
    - Add application.css
    - Change user and duke image
    - Change Color scheme

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -21,8 +21,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/modam.jpg"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/modok.jpg"));
 
     @FXML public Button buttonIntro;
 
