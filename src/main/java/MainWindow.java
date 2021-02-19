95ecc24da1c76b8a1bae0886d4e512e222f51ba0
Shaun Loh <sloh97@gmail.com>
2021-02-19 17:24:49 +0800

    implement auto-close feature when user types "bye"

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,3 +1,4 @@
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -19,6 +20,7 @@ public class MainWindow extends AnchorPane {
     private Button sendButton;
 
     private Duke duke;
+    private String CLOSE_COMMAND = "bye";
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
@@ -39,12 +41,18 @@ public class MainWindow extends AnchorPane {
     @FXML
     private void handleUserInput() {
         String input = userInput.getText();
-        String response = duke.getResponse(input);
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(input, userImage),
-                DialogBox.getDukeDialog(response, dukeImage)
-        );
-
         userInput.clear();
+
+        if (input.equalsIgnoreCase(CLOSE_COMMAND)) {
+            Platform.exit();
+
+        } else {
+            String response = duke.getResponse(input);
+            dialogContainer.getChildren().addAll(
+                    DialogBox.getUserDialog(input, userImage),
+                    DialogBox.getDukeDialog(response, dukeImage)
+            );
+
+        }
     }
 }
