9d38c5305cc0e1ce3e6c777984a8a0efdcb1a15a
Jacob-109 <65383310+Jacob-109@users.noreply.github.com>
2021-02-22 02:25:18 +0800

    add JUnit test for Deadline and TaskList

---
+++ src/test/java/DeadlineTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+    @Test
+    public void testGetBy() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate date = LocalDate.parse("2/12/2019",formatter);
+        Deadline test = new Deadline("return book", date);
+        assertEquals(date, test.getBy());
+    }
+
+    @Test
+    public void testToString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate date = LocalDate.parse("2/12/2019",formatter);
+        Deadline test = new Deadline("return book", date);
+        assertEquals("[D][Incompleted] lunch (by:Dec 12 2019)", test.toString());
+    }
+}
---
+++ src/test/java/TaskListTest.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+    TaskList taskList = new TaskList();
+    ToDo todo = new ToDo("run");
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    LocalDate date = LocalDate.parse("2/12/2019",formatter);
+    Deadline deadline = new Deadline("return book", date);
+    Event event = new Event("event project meeting", "Mon 2-4pm") ;
+
+    @Test
+    public void testNumOfTask() {
+        taskList.addTask(todo);
+        taskList.addTask(deadline);
+        taskList.addTask(event);
+        assertEquals(3, taskList.numOfTasks());
+    }
+}
