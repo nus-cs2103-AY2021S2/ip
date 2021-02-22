143a739bfacbd7a0808d178f6e12e91271664c4a
rjeez <77440906+rjeez@users.noreply.github.com>
2021-02-22 22:19:17 +0800

    Create CList.java
    Add CList class for command list

---
+++ src/main/duke/duke/command/CList.java
@@ -0,0 +1,25 @@
+package duke.command;
+import duke.DukeException;
+import duke.DukeList;
+import duke.Ui;
+import duke.io.Storage;
+import duke.tasktype.Task;
+import java.util.List;
+
+public class CList implements ICommand {
+    private String input;
+    public CList(String command){
+        this.input = command;
+    }
+
+    @Override
+    public boolean isBye() {
+        return false;
+    }
+
+    @Override
+    public String run(Ui ui, DukeList dukeList, Storage storage) throws DukeException {
+        List<Task> taskList = dukeList.getTaskList();
+        return ui.showList(taskList);
+    }
+}
