d659477f0ba627941dc3fcc9675259f91a029fcb
rjeez <77440906+rjeez@users.noreply.github.com>
2021-02-23 22:11:36 +0800

    Update MainWindow.java
    Added javadoc

--- src/main/duke/gui/MainWindow.java
+++ src/main/duke/gui/MainWindow.java
@@ -1,16 +1,20 @@
-package duke.gui;
+package main.duke.gui;
 
-import javafx.fml.FXML;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Textfield;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Vbox;
+import javafx.scene.layout.VBox;
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 
-import duke.Duke;
+import main.duke.Duke;
+
+/**
+ * Controller of the MainWindow
+ */
 public class MainWindow extends AnchorPane {
     @FXML
     private ScrollPane scrollPane;
@@ -22,26 +26,35 @@ public class MainWindow extends AnchorPane {
     private Button sendButton;
 
     private Duke duke;
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images.User.jpg"));
-    private Image botImage = new Image(this.getClass().getResourceAsStream("/images.Bot.jpg"));
-    private final String startMessage = "Hi there.\n"
-            + "These are the stuff you can say.\n";
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/User.png"));
+    private Image botImage = new Image(this.getClass().getResourceAsStream("/images/Bot.png"));
+    private final String startMessage = "Hello, I am duke bot.\n"
+            + "If you require any assistance with what I accept use my command help\n";
 
-    public MainWindow(){
+    public MainWindow(){ }
+
+    /**
+     * Initialize the MainWindow with a startMessage
+     */
     @FXML
     public void initialize(){
-        this.scrollPane.vvalueProperty().bind(this,dialogContainer.heightProperty());
-        dialogContainer.getChildren().add(DialogBox.getDukeDialog(startMessage,botImage));
+        this.scrollPane.vvalueProperty().bind(this.dialogContainer.heightProperty());
+        dialogContainer.getChildren().add(DialogBox.getUserDialog(startMessage,botImage));
     }
     public void makeDuke(Duke d){
         duke = d;
         }
+
+    /**
+     * Makes two dialogBoxes, one which echos the user input and the other contains the reply.
+     * Both are appended to the dialog container and cleared after.
+     */
     @FXML
     private void handleUserInput() {
-            String input = userInput.getText();
-            String response = duke.getResponse(input);
-            dialogContainer.getChildren().addAll(DialogBox.getUserDialog(input, userImage), DialogBox.getDukeDialog(response, botImage));
-        };
+            String input = userInput.getText().trim();
+            String response = duke.getReply(input);
+            dialogContainer.getChildren().addAll(DialogBox.getUserDialog(input, userImage),
+                    DialogBox.getBotDialog(response, botImage));
         userInput.clear();
         CompletableFuture.runAsync(() -> {
             try{
