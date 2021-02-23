ad92a540ed5da19650474e095d174671c0737f02
rjeez <77440906+rjeez@users.noreply.github.com>
2021-02-23 22:10:37 +0800

    Update Duke.java
    Added javadoc

--- src/main/duke/Duke.java
+++ src/main/duke/Duke.java
@@ -1,25 +1,29 @@
-package duke;
+package main.duke;
 
-import duke.io.Storage;
-import duke.io.Parser;
-import duke.command.ICommand;
+import main.duke.io.Storage;
+import main.duke.io.Parser;
+import main.duke.DukeList;
+import main.duke.command.ICommand;
 public class Duke {
     private Storage storage;
-    private DukeList DukeList;
+    private DukeList dukeList;
     private Ui ui;
 
-
+    /**
+     * Constructor for duke
+     * @param filePath contains file where the task list is being saved
+     */
     public Duke(String filePath){
         ui = new  Ui();
         storage = new Storage(filePath);
-        DukeList = storage.load();
+        dukeList = storage.load();
     }
 
     public String getReply (String input){
         Parser parser = new Parser();
         try{
             ICommand command = parser.parse(input);
-            String reply = command.run(ui,DukeList, storage);
+            String reply = command.run(ui,dukeList, storage);
             storage.writeFile();
             assert(reply!=null);
             return reply;
