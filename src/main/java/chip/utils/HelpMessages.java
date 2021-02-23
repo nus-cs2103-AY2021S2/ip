253c6a369b5f22ee1cce600c1241ae4e9af61d87
Samuel Fang <samuelfangjw@gmail.com>
2021-02-23 12:36:44 +0800

    Update help messages to be more consistent with user guide

--- src/main/java/chip/utils/HelpMessages.java
+++ src/main/java/chip/utils/HelpMessages.java
@@ -8,36 +8,36 @@ import chip.parser.Parser;
  */
 public class HelpMessages {
     private static final String[] AVAILABLE_COMMANDS =
-        {"bye", "deadline", "delete", "done", "event", "help", "find", "list", "todo"};
+        {"\tbye", "\tdeadline", "\tdelete", "\tdone", "\tevent", "\thelp", "\tfind", "\tlist", "\ttodo"};
     private static final String BYE_MESSAGE =
         "Type \"bye\" to exit the application. Don't worry I'll remember your tasks!";
     private static final String DEADLINE_MESSAGE =
         "Keep track of your deadlines by typing the following:\n"
-            + "\"deadline <description> /by <date> <time>\"\n"
-            + "<date> should be in yyyy-mm-dd format\n"
-            + "<time> is optional and should be in hh:mm format";
+            + "\"deadline DESCRIPTION /by DATE (TIME)\"\n"
+            + "DATE should be in yyyy-mm-dd format\n"
+            + "TIME is optional and should be in hh:mm format";
     private static final String DELETE_MESSAGE =
-        "Type \"delete <task_number>\" to delete a task forever. "
+        "Type \"delete TASK_NUMBER\" to delete a task forever. "
             + "If you are not sure of the task number, try listing out your tasks.";
     private static final String DONE_MESSAGE =
-        "Type \"done <task_number>\" and I'll mark your task as done. "
+        "Type \"done TASK_NUMBER\" and I'll mark your task as done. "
             + "If you are not sure of the task number, try listing out your tasks.";
     private static final String EVENT_MESSAGE =
         "Keep track of your events by typing the following:\n"
-            + "\"event <description> /at <date> <time>\"\n"
-            + "<date> should be in yyyy-mm-dd format\n"
-            + "<time> is optional and should be in hh:mm format";
+            + "\"event DESCRIPTION /at DATE (TIME)\"\n"
+            + "DATE should be in yyyy-mm-dd format\n"
+            + "TIME is optional and should be in hh:mm format";
     private static final String HELP_MESSAGE =
-        "Type \"help <command>\" to see get more information about a command. Available commands:\n";
+        "Type \"help (COMMAND)\" to see get more information about a command. \nAvailable commands:\n";
     private static final String FIND_MESSAGE =
         "Let me know which tasks you are looking for by typing the following:\n"
-            + "\"find <substring>\"\n"
-            + "You may specify more than one space separated substrings to search for.\n"
+            + "\"find SEARCH_PARAMETERS...\"\n"
+            + "You may specify more than one space separated search parameters to search for.\n"
             + "I'll look through the tasks and see if there are any matches.";
     private static final String LIST_MESSAGE = "Type \"list\" to see a list of tasks you currently have.";
     private static final String TODO_MESSAGE =
         "Keep track of your todos by typing the following:\n"
-            + "\"todo <description>\"";
+            + "\"todo DESCRIPTION\"";
     private static final String UNKNOWN_MESSAGE = "I do not understand that command. Try one of these instead:\n";
 
     /**
