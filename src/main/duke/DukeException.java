5bc3889a821b2c4526c65e5373d2ccbb10c20e4b
rjeez <77440906+rjeez@users.noreply.github.com>
2021-02-23 22:10:41 +0800

    Update DukeException.java
    Added javadoc

--- src/main/duke/DukeException.java
+++ src/main/duke/DukeException.java
@@ -1,6 +1,10 @@
-package duke;
+package main.duke;
 
 public class DukeException extends Exception{
+    /**
+     *
+     * @param message Exception message being passed
+     */
     public DukeException (String message){
         super(message);
     }
