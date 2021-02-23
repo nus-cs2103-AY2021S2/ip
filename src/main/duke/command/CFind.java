06d4a0c4d4f6c509d84e5cec19e6b60016fbfe69
rjeez <77440906+rjeez@users.noreply.github.com>
2021-02-23 22:09:41 +0800

    Update CFind.java
    Updated imports

--- src/main/duke/command/CFind.java
+++ src/main/duke/command/CFind.java
@@ -1,10 +1,10 @@
-package duke.command;
+package main.duke.command;
 
-import duke.DukeException;
-import duke.DukeList;
-import duke.Ui;
-import duke.io.Storage;
-import duke.tasktype.Task;
+import main.duke.DukeException;
+import main.duke.DukeList;
+import main.duke.Ui;
+import main.duke.io.Storage;
+import main.duke.tasktype.Task;
 import java.util.List;
 
 public class CFind implements ICommand{
@@ -12,12 +12,21 @@ public class CFind implements ICommand{
     public CFind(String command){
         this.input = command;
     }
+    /**
+     * Check if it is the bye command
+     * @return false as it is not the bye command
+     */
 
     @Override
     public boolean isBye() {
         return false;
     }
-
+    /**
+     *
+     * @param ui UI object that deal with the program output
+     * @param dukeList Collection of tasks in list form
+     * @param storage Storage object that deal with the file system
+     */
     @Override
     public String run(Ui ui, DukeList dukeList, Storage storage) throws DukeException {
         List<Task> taskList = dukeList.getTaskList();
