ff26236857e3111ae39cc57b59004b7250e25367
rjeez <77440906+rjeez@users.noreply.github.com>
2021-02-23 22:09:27 +0800

    Update CDelete.java
    Updated imports

--- src/main/duke/command/CDelete.java
+++ src/main/duke/command/CDelete.java
@@ -1,10 +1,10 @@
-package duke.command;
+package main.duke.command;
 
-import duke.DukeException;
-import duke.DukeList;
-import duke.Ui;
-import duke.io.Storage;
-import duke.tasktype.Task;
+import main.duke.DukeException;
+import main.duke.DukeList;
+import main.duke.Ui;
+import main.duke.io.Storage;
+import main.duke.tasktype.Task;
 import java.util.List;
 
 public class CDelete implements ICommand {
@@ -12,15 +12,25 @@ public class CDelete implements ICommand {
     public CDelete(String command){
         this.input = command;
     }
+    /**
+     * Check if it is the bye command
+     * @return false as it is not the bye command
+     */
     @Override
     public boolean isBye() {
         return false;
     }
+    /**
+     *
+     * @param ui UI object that deal with the program output
+     * @param dukeList Collection of tasks in list form
+     * @param storage Storage object that deal with the file system
+     */
     @Override
     public String run(Ui ui, DukeList dukeList, Storage storage) throws DukeException{
         List<Task> taskList = dukeList.getTaskList();
-        int index = Integer.parseInt(input.substring(7));
-        if(index == 0 || index>= taskList.size() +1){
+        int index = Integer.parseInt(input.substring(7)) -1;
+        if(index+1 == 0 || index>= taskList.size() +1){
             throw new DukeException("This task does not exist");
         }
         Task removedTask = taskList.remove(index);
