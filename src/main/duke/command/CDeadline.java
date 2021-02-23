7e9c75c3a0f6897aea78c23858da0e5683d6f1a8
rjeez <77440906+rjeez@users.noreply.github.com>
2021-02-23 22:09:24 +0800

    Update CDeadline.java
    Updated imports

--- src/main/duke/command/CDeadline.java
+++ src/main/duke/command/CDeadline.java
@@ -1,20 +1,31 @@
-package duke.command;
+package main.duke.command;
 import java.util.List;
-import duke.DukeException;
-import duke.DukeList;
-import duke.Ui;
-import duke.io.Storage;
-import duke.tasktype.Task;
-import duke.tasktype.Deadline;
+import main.duke.DukeException;
+import main.duke.DukeList;
+import main.duke.Ui;
+import main.duke.io.Storage;
+import main.duke.tasktype.Task;
+import main.duke.tasktype.Deadline;
 public class CDeadline implements ICommand {
     private String input;
     public CDeadline(String command){
         this.input = command;
     }
+
+    /**
+     * Check if it is the bye command
+     * @return false as it is not the bye command
+     */
     @Override
     public boolean isBye() {
         return false;
     }
+    /**
+     *
+     * @param ui UI object that deal with the program output
+     * @param dukeList Collection of tasks in list form
+     * @param storage Storage object that deal with the file system
+     */
     @Override
     public String run(Ui ui, DukeList dukeList, Storage storage) throws DukeException{
         List<Task> taskList = dukeList.getTaskList();
