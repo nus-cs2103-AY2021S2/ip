e5124f2a7f39575e6f729175670f4dac0e6edcff
rjeez <77440906+rjeez@users.noreply.github.com>
2021-02-23 22:09:20 +0800

    Update CBye.java
    Updated imports

--- src/main/duke/command/CBye.java
+++ src/main/duke/command/CBye.java
@@ -1,18 +1,27 @@
-package duke.command;
+package main.duke.command;
 
-import duke.DukeException;
-import duke.DukeList;
-import duke.Ui;
-import duke.io.Storage;
+import main.duke.DukeList;
+import main.duke.Ui;
+import main.duke.io.Storage;
 
 public class CBye implements ICommand {
+    /**
+     * Check if it is the bye command
+     * @return true as it is the bye command
+     */
     @Override
     public boolean isBye() {
         return true;
     }
 
+    /**
+     *
+     * @param ui UI object that deal with the program output
+     * @param dukeList Collection of tasks in list form
+     * @param storage Storage object that deal with the file system
+     */
     @Override
-    public String run(Ui ui, DukeList dukeList, Storage storage) throws DukeException {
+    public String run(Ui ui, DukeList dukeList, Storage storage) {
         return ui.sayGoodbye();
     }
 }
