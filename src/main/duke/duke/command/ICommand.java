8da1bee7f59736c3ee7c81980e074644ad97bccd
rjeez <77440906+rjeez@users.noreply.github.com>
2021-02-23 16:18:12 +0800

    Delete ICommand.java

--- src/main/duke/duke/command/ICommand.java
+++
@@ -1,9 +0,0 @@
-package duke.command;
-import duke.DukeList;
-import duke.Ui;
-import duke.DukeException;
-import duke.io.Storage;
-public interface ICommand {
-    public boolean isBye();
-    public String run(Ui ui, DukeList dukeList, Storage storage) throws DukeException;
-}
