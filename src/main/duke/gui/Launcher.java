1f1fb2474e07546f9a26e671fdb0e6dc29d6ec03
rjeez <77440906+rjeez@users.noreply.github.com>
2021-02-23 22:11:05 +0800

    Update Launcher.java
    Added javadoc

--- src/main/duke/gui/Launcher.java
+++ src/main/duke/gui/Launcher.java
@@ -1,9 +1,14 @@
-package duke.gui;
+package main.duke.gui;
 
 import javafx.application.Application;
 
 public class Launcher {
+    /**
+     * Launch javafx application
+     * @param args Args to initialize main method
+     */
     public static void main(String[] args){
+
         Application.launch(Main.class,args);
     }
 }
