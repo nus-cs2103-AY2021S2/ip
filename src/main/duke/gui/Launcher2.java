ba2d5988de6e12eb08a6b12bd050174720d29e28
rjeez <77440906+rjeez@users.noreply.github.com>
2021-02-23 16:19:58 +0800

    Create Launcher2.java
    Update location

---
+++ src/main/duke/gui/Launcher2.java
@@ -0,0 +1,10 @@
+package duke.gui;
+
+import javafx.application.Application;
+
+public class Launcher2 {
+    public static void main(String[] args){
+
+        Application.launch(Main.class,args);
+    }
+}
