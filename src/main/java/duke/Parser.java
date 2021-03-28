afe7462ce09ab7459952406564899b0f30ac3e8e
Jacob-109 <65383310+Jacob-109@users.noreply.github.com>
2021-03-29 01:25:19 +0800

    Cleaned up .class files and fixed Parser.java

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -3,6 +3,7 @@ package duke;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Parser {
     private String command;
@@ -40,118 +41,151 @@ public class Parser {
         return this.isEnd;
     }
 
+    public String ListCommand() {
+        String list = "";
+        if (taskList.numOfTasks() == 0) {
+            return "There are no tasks";
+        }
+        return "Here are the tasks in your list:\n" + taskList.toString();
+    }
+
+    public String ToDoCommand(String command) {
+        String display = "";
+        String[] save = command.split(" ");
+        if (save.length == 1) {
+            return "Please add a description for your ToDo task";
+        }
+        String description = "";
+        ToDo t = new ToDo("", "");
+        if (save.length > 2) {
+            t.setDescription(save[1]);
+            t.setTag(save[2]);
+        } else {
+            t.setDescription(save[1]);
+            t.setTag("");
+        }
+        taskList.addTask(t);
+        taskList.checkForDuplicate();
+        storage.saveTasks(taskList);
+        Integer numOfTask = taskList.numOfTasks();
+        return ("Got it. I've added this task: + \n" + t.toString() + "\n" + "Now you have "
+                + numOfTask.toString() + " tasks in the list.");
+    }
+
+    public String DeadlineCommand(String command) {
+        String[] input = command.split("/by ");
+        int length = input.length;
+        if (length == 1) {
+            return "Please add a description or date for your Deadline task";
+        }
+        input[0] = input[0].replaceAll("deadline", "");
+        LocalDate date = LocalDate.parse(input[1], DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        Deadline d = new Deadline(input[0], date);
+        taskList.addTask(d);
+        taskList.checkForDuplicate();
+        storage.saveTasks(taskList);
+        Integer numOfTask = taskList.numOfTasks();
+        return ("Got it. I've added this task: " + "\n" + d.toString() + "\n"
+                + "Now you have " + numOfTask.toString() + " tasks in the list.");
+    }
+
+    public String EventCommand(String command) {
+        String[] info = command.split("/at");
+        int length = info.length;
+        if (length == 1) {
+            return "Please add a description or date for your Deadline task";
+        }
+        info[0] = info[0].replaceAll("event", "");
+        Event e = new Event(info[0], info[1]);
+        taskList.addTask(e);
+        taskList.checkForDuplicate();
+        storage.saveTasks(taskList);
+        Integer numOfTask = taskList.numOfTasks();
+        return ("Got it. I've added this task: " + "\n" + e.toString() + "\n"
+                + "Now you have " + numOfTask.toString() + " tasks in the list.");
+    }
+
+    public String DoneCommand(String command) {
+        String[] strArray = command.split(" ");
+        int value = Integer.parseInt(strArray[1]);
+        if (value > taskList.numOfTasks()) {
+            return "Invalid Index";
+        }
+        Task complete = taskList.getTask(value - 1);
+        complete.markAsDone();
+        storage.saveTasks(taskList);
+        return (" Nice! I've marked this task as done: " + "\n" + complete.toString());
+    }
+
+    public String DeleteCommand(String command) {
+        String[] arr = command.split(" ");
+        int value = Integer.parseInt(arr[1]);
+        if (value > taskList.numOfTasks()) {
+            return "Invalid Index";
+        }
+        Task remove = taskList.getTask(value - 1);
+        taskList.deleteTask(value - 1);
+        storage.saveTasks(taskList);
+        Integer numOfTask = taskList.numOfTasks();
+        return ("Noted. I've removed this task: " + "\n" + remove.toString() + "Now you have "
+                + numOfTask.toString() + " tasks in the list.");
+    }
+
+    public String FindCommand(String command) {
+        String outcome = "";
+        String[] find = command.split(" ");
+        String keyword = find[1];
+        ArrayList<Task> output = new ArrayList<>();
+        for (int i = 0; i < taskList.numOfTasks(); i++) {
+            Task check = taskList.getTask(i);
+            if (check.getDescription().contains(keyword)) {
+                output.add(check);
+            }
+        } if (output.isEmpty()) {
+            return ("Nothing in the list matches your keyword");
+        } else {
+            for (int i = 1; i < output.size() + 1; i++) {
+                if (i == output.size()) {
+                    outcome += i + ". " + output.get(i - 1);
+                } else {
+                    outcome += i + ". " + output.get(i - 1) + "\n";
+                }
+            }
+        }
+        return ("Here are the matching tasks in your list:" + "\n"
+                + outcome);
+    }
+
+    public String ByeCommand() {
+        isEnd = true;
+        return "Thanks for using Duke.Hope to see you again!";
+    }
+
     /**
      * Processes the commands entered by user
      * @return
      */
     public String process() {
-        String displayed = "";
-        if (command.equals("list")) {
-            String list = "";
-            if (taskList.numOfTasks() == 0) {
-                return "There are no tasks";
-            }
-            return "Here are the tasks in your list:\n" + taskList.toString();
+        String display = "";
+        if (command.contains("list")) {
+            display = ListCommand();
         } else if (command.contains("todo")) {
-            String display = "";
-            String[] save = command.split(" ");
-            if (save.length == 1) {
-                return "Please add a description for your ToDo task";
-            }
-            String description = "";
-            ToDo t = new ToDo("", "");
-            if (save.length > 2) {
-                t.setDescription(save[1]);
-                t.setTag(save[2]);
-            } else {
-                t.setDescription(save[1]);
-                t.setTag("");
-            }
-            taskList.addTask(t);
-            taskList.checkForDuplicate();
-            storage.saveTasks(taskList);
-            Integer numOfTask = taskList.numOfTasks();
-            return ("Got it. I've added this task: + \n" + t.toString() + "\n" + "Now you have "
-                    + numOfTask.toString() + " tasks in the list.");
+            display = ToDoCommand(command);
         } else if (command.contains("deadline")) {
-            String[] input = command.split("/by ");
-            int length = input.length;
-            if (length == 1) {
-                return "Please add a description or date for your Deadline task";
-            }
-            input[0] = input[0].replaceAll("deadline", "");
-            LocalDate date = LocalDate.parse(input[1], DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-            Deadline d = new Deadline(input[0], date);
-            taskList.addTask(d);
-            taskList.checkForDuplicate();
-            storage.saveTasks(taskList);
-            Integer numOfTask = taskList.numOfTasks();
-            return ("Got it. I've added this task: " + "\n" + d.toString() + "\n"
-                    + "Now you have " + numOfTask.toString() + " tasks in the list.");
+            display = DeadlineCommand(command);
         } else if (command.contains("event")) {
-            String[] info = command.split("/at");
-            int length = info.length;
-            if (length == 1) {
-                return "Please add a description or date for your Deadline task";
-            }
-            info[0] = info[0].replaceAll("event", "");
-            Event e = new Event(info[0], info[1]);
-            taskList.addTask(e);
-            taskList.checkForDuplicate();
-            storage.saveTasks(taskList);
-            Integer numOfTask = taskList.numOfTasks();
-            return ("Got it. I've added this task: " + "\n" + e.toString() + "\n"
-                    + "Now you have " + numOfTask.toString() + " tasks in the list.");
+            display = EventCommand(command);
         } else if (command.contains("done")) {
-            String[] strArray = command.split(" ");
-            int value = Integer.parseInt(strArray[1]);
-            if (value > taskList.numOfTasks()) {
-                return "Invalid Index";
-            }
-            Task complete = taskList.getTask(value - 1);
-            complete.markAsDone();
-            storage.saveTasks(taskList);
-            return (" Nice! I've marked this task as done: " + "\n" + complete.toString());
+            display = DoneCommand(command);
         } else if (command.contains("delete")) {
-            String[] arr = command.split(" ");
-            int value = Integer.parseInt(arr[1]);
-            if (value > taskList.numOfTasks()) {
-                return "Invalid Index";
-            }
-            Task remove = taskList.getTask(value - 1);
-            taskList.deleteTask(value - 1);
-            storage.saveTasks(taskList);
-            Integer numOfTask = taskList.numOfTasks();
-            return ("Noted. I've removed this task: " + "\n" + remove.toString() + "Now you have "
-                    + numOfTask.toString() + " tasks in the list.");
+            display = DeleteCommand(command);
         } else if (command.contains("find")) {
-            String outcome = "";
-            String[] find = command.split(" ");
-            String keyword = find[1];
-            ArrayList<Task> output = new ArrayList<>();
-            for (int i = 0; i < taskList.numOfTasks(); i++) {
-                Task check = taskList.getTask(i);
-                if (check.getDescription().contains(keyword)) {
-                    output.add(check);
-                }
-            } if (output.isEmpty()) {
-                return ("Nothing in the list matches your keyword");
-            } else {
-                for (int i = 1; i < output.size() + 1; i++) {
-                    if (i == output.size()) {
-                        outcome += i + ". " + output.get(i - 1);
-                    } else {
-                        outcome += i + ". " + output.get(i - 1) + "\n";
-                    }
-                }
-            }
-            return ("Here are the matching tasks in your list:" + "\n"
-                    + outcome);
-        } else if (command.equals("bye")) {
-            isEnd = true;
-            return "Thanks for using Duke.Hope to see you again!";
+            display = FindCommand(command);
+        } else if (command.contains("bye")) {
+            display = ByeCommand();
         } else {
-            return "OOPS, not sure what you have entered.";
+            display = "OOPS, not sure what you have entered.";
         }
+        return display;
     }
 }
