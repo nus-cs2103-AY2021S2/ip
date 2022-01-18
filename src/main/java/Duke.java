2fab5105cbb994ccda3c5f1a9dbc8a7d25922534
Zheng Teck <zhengteck.goh@comp.nus.edu.sg>
2022-01-18 16:34:29 +0800

    Add Greet Message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        final String greetingMsg = "Hello! I'm DatoDato! Your personal helper bot. :) \n" +
+                "What can I do for you?";
+        final String leavingMsg = "Bye! Checkout another cool bot  @KatoKatoBot on Telegram.\n " +
+                "Hope to see you again soon! \n";
+
+        new ResponePrinter(greetingMsg).print();
     }
 }
---
+++ src/main/java/ResponePrinter.java
@@ -0,0 +1,17 @@
+public class ResponePrinter {
+
+    private static final String LINESEPARATOR = "____________________________________________________________\n";
+
+    private final String msg;
+
+    public ResponePrinter(String msg) {
+        this.msg = msg;
+    }
+
+    /**
+     * The print function is used to print the respone message generated in the required format.
+     */
+    public void print() {
+        System.out.println(LINESEPARATOR + msg + LINESEPARATOR);
+    }
+}
